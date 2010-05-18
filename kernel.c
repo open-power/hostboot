512cc04b8f681a7583fff88bd903f327d549bd3d
Patrick Williams <iawillia@us.ibm.com>
2010-05-18 11:34:54 -0500

    Move stack init to asm stub.

--- kernel.c
+++ kernel.c
@@ -1,12 +1,4 @@
-
-void _main(void)
+void main()
 {
-    static unsigned char kernel_stack[16 * 1024] = {0};
-    register void * stack = &kernel_stack;
-    asm volatile("mr 1,%0" :: "r" (stack) );
-   
-    while(1)
-    {
-	//asm volatile("wait");
-    }
+    while(1);
 }
