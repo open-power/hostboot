90d4e5df06e6108f846013a48b996e936adeffec
Patrick Williams <iawillia@us.ibm.com>
2011-05-24 13:10:03 -0500

    Improve assert debugability.
    Change-Id: Iec3709694ea02ce2c162d310949ad8586da0b539
    Reviewed-on: http://gfwr801.rchland.ibm.com:8080/gerrit/96
    Tested-by: Jenkins Server
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: MATTHEW S. BARTH <msbarth@us.ibm.com>

--- src/lib/assert.C
+++ src/lib/assert.C
@@ -1,8 +1,10 @@
+#include <kernel/console.H>
 
 extern "C" void __assert(bool expr, const char *exprStr, 
 			 const char *file, int line)
 {
-    // TODO
+    // TODO    
+    printk("Assert in %s (%d): %s\n", file, line, exprStr);
     while (true);
 }
 
