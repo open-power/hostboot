c6571028ca3b54ba0a4ec73d1f39fdeba6c79fb9
Patrick Williams <iawillia@us.ibm.com>
2011-08-22 10:34:45 -0500

    Create initial stack-frame from userspace.
    Change-Id: Ie1133bd079e7b9a8f2f82daa06efc426bf0fd0d6
    Reviewed-on: http://gfw160.austin.ibm.com:8080/gerrit/268
    Tested-by: Jenkins Server
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/kernel/taskmgr.C
+++ src/kernel/taskmgr.C
@@ -50,20 +50,20 @@ task_t* TaskManager::_createIdleTask()
     return this->_createTask(&TaskManager::idleTaskLoop, NULL, false);
 }
 
-task_t* TaskManager::_createTask(TaskManager::task_fn_t t, 
+task_t* TaskManager::_createTask(TaskManager::task_fn_t t,
 				 void* p, bool withStack)
 {
     task_t* task = new task_t;
     memset(task, '\0', sizeof(task_t));
 
     task->tid = this->getNextTid();
-    
-    // Set NIP to be userspace_task_entry stub and GPR3 to be the 
+
+    // Set NIP to be userspace_task_entry stub and GPR3 to be the
     // function pointer for the desired task entry point.
     task->context.nip = reinterpret_cast<void*>(&userspace_task_entry);
     task->context.gprs[4] = reinterpret_cast<uint64_t>(t);
 
-    // Set up LR to be the entry point for task_end in case a task 
+    // Set up LR to be the entry point for task_end in case a task
     // 'returns' from its entry point.  By the Power ABI, the entry
     // point address is in (func_ptr)[0].
     task->context.lr = reinterpret_cast<uint64_t*>(&task_end)[0];
@@ -73,19 +73,21 @@ task_t* TaskManager::_createTask(TaskManager::task_fn_t t,
 
     // Set up argument.
     task->context.gprs[3] = (uint64_t) p;
-    
+
     // Setup stack.
     if (withStack)
     {
-	task->context.stack_ptr = 
+	task->context.stack_ptr =
 	    PageManager::allocatePage(TASK_DEFAULT_STACK_SIZE);
-        memset(task->context.stack_ptr, '\0', 
+        memset(task->context.stack_ptr, '\0',
                TASK_DEFAULT_STACK_SIZE * PAGESIZE);
-	task->context.gprs[1] = ((uint64_t)task->context.stack_ptr) + 16320;
+	task->context.gprs[1] = ((uint64_t)task->context.stack_ptr) +
+               TASK_DEFAULT_STACK_SIZE * PAGESIZE - 8;
     }
     else
     {
 	task->context.stack_ptr = NULL;
+        task->context.gprs[1] = NULL;
     }
 
     return task;
