0a6197f1b257f230694bff493bf7239a1a644cd6
CamVan Nguyen <ctnguyen@us.ibm.com>
2011-09-21 14:51:35 -0500

    Added option to use a chiplet level target when flushing L2
    Change-Id: I1a31d574a15f1c40f9ea464776441c28347c06e5
    Reviewed-on: http://gfw160.austin.ibm.com:8080/gerrit/355
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Tested-by: Jenkins Server

--- src/build/vpo/hb-virtdebug.pl
+++ src/build/vpo/hb-virtdebug.pl
@@ -277,7 +277,7 @@ print "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 my $command = "";
 $command = "/afs/awd.austin.ibm.com/projects/eclipz/lab/p8/gsiexe/p8_runso.x86 ";
 $command .= "/afs/awd.austin.ibm.com/projects/eclipz/lab/p8/gsiexe/p8_l2_flush_x86.so ";
-$command .= "@ecmdOpt -debug5.6";
+$command .= "@ecmdOpt -loop ex -debug5.6";
 print "$command\n";
 die if (system("$command") != 0);
 
