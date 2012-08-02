c07b8bb150f1464e17ebf3672460564ec3e8947c
Brian Horton <brianh@linux.ibm.com>
2012-08-02 10:19:13 -0500

    Add node as default parameter to masterProcChipTargetHandle()
    Node will default to 0, so no callers have to change. For hostboot,
    callers will ALWAYS keep this as 0. For FSP code, node could be
    different.
    
    Change-Id: Ib78004dbf89cdee2cfceda6a0c8a54c96380bee1
    RTC: 45742
    Reviewed-on: http://gfw160.austin.ibm.com:8080/gerrit/1465
    Tested-by: Jenkins Server
    Reviewed-by: MIKE J. JONES <mjjones@us.ibm.com>
    Reviewed-by: Andrew J. Geissler <andrewg@us.ibm.com>
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>

--- src/usr/targeting/common/targetservice.C
+++ src/usr/targeting/common/targetservice.C
@@ -1,26 +1,25 @@
-/*  IBM_PROLOG_BEGIN_TAG
- *  This is an automatically generated prolog.
- *
- *  $Source: src/usr/targeting/common/targetservice.C $
- *
- *  IBM CONFIDENTIAL
- *
- *  COPYRIGHT International Business Machines Corp. 2011-2012
- *
- *  p1
- *
- *  Object Code Only (OCO) source materials
- *  Licensed Internal Code Source Materials
- *  IBM HostBoot Licensed Internal Code
- *
- *  The source code for this program is not published or other-
- *  wise divested of its trade secrets, irrespective of what has
- *  been deposited with the U.S. Copyright Office.
- *
- *  Origin: 30
- *
- *  IBM_PROLOG_END_TAG
- */
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/usr/targeting/common/targetservice.C $                    */
+/*                                                                        */
+/* IBM CONFIDENTIAL                                                       */
+/*                                                                        */
+/* COPYRIGHT International Business Machines Corp. 2011,2012              */
+/*                                                                        */
+/* p1                                                                     */
+/*                                                                        */
+/* Object Code Only (OCO) source materials                                */
+/* Licensed Internal Code Source Materials                                */
+/* IBM HostBoot Licensed Internal Code                                    */
+/*                                                                        */
+/* The source code for this program is not published or otherwise         */
+/* divested of its trade secrets, irrespective of what has been           */
+/* deposited with the U.S. Copyright Office.                              */
+/*                                                                        */
+/* Origin: 30                                                             */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
 /**
  *  @file targeting/common/targetservice.C
  *
@@ -319,11 +318,12 @@ Target* TargetService::toTarget(
 }
 
 //******************************************************************************
-// TargetService::masterProcChipTarget
+// TargetService::masterProcChipTargetHandle
 //******************************************************************************
 
 void TargetService::masterProcChipTargetHandle(
-    Target*& o_masterProcChipTargetHandle) const
+    Target*& o_masterProcChipTargetHandle,
+    const uint8_t i_node) const
 {
     #define TARG_FN "masterProcChipTargetHandle(...)"
 
@@ -336,7 +336,7 @@ void TargetService::masterProcChipTargetHandle(
     // PNOR to determine the master chip
     // target; for now, just always report sys0.n0.proc0
     EntityPath l_masterProcChipEntityPath(EntityPath::PATH_PHYSICAL);
-    l_masterProcChipEntityPath.addLast(TYPE_SYS, 0).addLast(TYPE_NODE, 0)
+    l_masterProcChipEntityPath.addLast(TYPE_SYS, 0).addLast(TYPE_NODE, i_node)
         .addLast(TYPE_PROC, 0);
 
     l_pTarget = l_masterProcChipEntityPath.operator->();
