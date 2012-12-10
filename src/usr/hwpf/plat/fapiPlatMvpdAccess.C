aee31b7876297202881b1a17355d3ef1cea34b3d
Dean Sanner <dsanner@us.ibm.com>
2012-12-10 15:19:50 -0600

    Add PB and MK keywords to MVPD
    HW team needs to access PB and MK keywords form the CP00 record.
    Add to mvpd DD and FAPI.
    
    Change-Id: I4e8f186f2f09b950da954dcef91398548bef44e6
    Reviewed-on: http://gfw160.austin.ibm.com:8080/gerrit/2644
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>
    Tested-by: Jenkins Server

--- src/usr/hwpf/plat/fapiPlatMvpdAccess.C
+++ src/usr/hwpf/plat/fapiPlatMvpdAccess.C
@@ -128,7 +128,7 @@ fapi::ReturnCode MvpdKeywordXlate(const fapi::MvpdKeyword i_fapiKeyword,
     // Create a lookup table for converting a FAPI MVPD keyword enumerator to a
     // Hostboot MVPD keyword enumerator. This is a simple array and relies on
     // the FAPI record enumerators starting at zero and incrementing.
-    const uint8_t NUM_MVPD_KEYWORDS = 0x20;
+    const uint8_t NUM_MVPD_KEYWORDS = 0x22;
     static const MVPD::mvpdKeyword
         mvpdFapiKeywordToHbKeyword[NUM_MVPD_KEYWORDS] =
     {
@@ -164,6 +164,8 @@ fapi::ReturnCode MvpdKeywordXlate(const fapi::MvpdKeyword i_fapiKeyword,
         MVPD::FO,
         MVPD::pdI,
         MVPD::pdG,
+        MVPD::MK,
+        MVPD::PB,
     };
     
     fapi::ReturnCode l_rc;
