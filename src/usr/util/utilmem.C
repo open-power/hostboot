7c27703341d61f28b204254ad96c3fea5840bb73
Patrick Williams <iawillia@us.ibm.com>
2013-10-25 12:12:11 -0500

    Clean up BEAM warning in utilmem.
    Change-Id: I417f5d96025c37a7e82703f73d364657ca24fcb2
    RTC: 84072
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/6880
    Tested-by: Jenkins Server
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>

--- src/usr/util/utilmem.C
+++ src/usr/util/utilmem.C
@@ -1,26 +1,25 @@
-/*  IBM_PROLOG_BEGIN_TAG
- *  This is an automatically generated prolog.
- *
- *  $Source: src/usr/util/utilmem.C $
- *
- *  IBM CONFIDENTIAL
- *
- *  COPYRIGHT International Business Machines Corp. 2003-2012
- *
- *  p1
- *
- *  Object Code Only (OCO) source materials
- *  Licensed Internal Code Source Materials
- *  IBM HostBoot Licensed Internal Code
- *
- *  The source code for this program is not published or other-
- *  wise divested of its trade secrets, irrespective of what has
- *  been deposited with the U.S. Copyright Office.
- *
- *  Origin: 30
- *
- *  IBM_PROLOG_END_TAG
- */
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/usr/util/utilmem.C $                                      */
+/*                                                                        */
+/* IBM CONFIDENTIAL                                                       */
+/*                                                                        */
+/* COPYRIGHT International Business Machines Corp. 2003,2013              */
+/*                                                                        */
+/* p1                                                                     */
+/*                                                                        */
+/* Object Code Only (OCO) source materials                                */
+/* Licensed Internal Code Source Materials                                */
+/* IBM HostBoot Licensed Internal Code                                    */
+/*                                                                        */
+/* The source code for this program is not published or otherwise         */
+/* divested of its trade secrets, irrespective of what has been           */
+/* deposited with the U.S. Copyright Office.                              */
+/*                                                                        */
+/* Origin: 30                                                             */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
 /**
  * @file utilmem.C
  *
@@ -312,6 +311,7 @@ uint32_t UtilMem::write(
         }
 
         // Copy memory
+        assert(NULL != iv_memStart); // Impossible condition, but BEAM flags.
         memcpy((iv_memStart + iv_offset), i_buffer, l_rc);
 
         // Set the new current position
