4b0ab464ceaa66fbb9b3d4f833215c26f441b1c9
Mike Jones <mjjones@us.ibm.com>
2013-11-08 08:58:15 -0600

    Update io pre/post trainadv HWPs to match eKB
    Firmware is using a modified version of these empty procedures to get
    past unused variable warnings. I finally updated these procedures in
    eKB to fix the issues. Hostboot needs to sync up to match the eKB HWPs.
    
    Change-Id: Ic91bebb0c4846e04fb172ebf23bf566b74629976
    RTC: 90940
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/7123
    Tested-by: Jenkins Server
    Reviewed-by: Brian H. Horton <brianh@linux.ibm.com>
    Reviewed-by: Donald E. Dahle <dedahle@us.ibm.com>
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>

--- src/usr/hwpf/hwp/bus_training/io_post_trainadv.C
+++ src/usr/hwpf/hwp/bus_training/io_post_trainadv.C
@@ -20,7 +20,7 @@
 /* Origin: 30                                                             */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
-// $Id: io_post_trainadv.C,v 1.1 2013/05/10 20:06:00 thomsen Exp $
+// $Id: io_post_trainadv.C,v 1.2 2013/11/08 14:23:07 mjjones Exp $
 //*!***************************************************************************
 // *! (C) Copyright International Business Machines Corp. 1997, 1998
 // *!           All Rights Reserved -- Property of IBM
@@ -40,76 +40,19 @@
 // Version:|Author: | Date:  | Comment:
 // --------|--------|--------|-------------------------------------------------
 //   1.1   |thomsen |05/10/13| Initial empty shell
+//   1.2   |mjjones |11/07/13| Cleanup
 //-----------------------------------------------------------------------------
 
 #include <fapi.H>
 #include "io_post_trainadv.H"
-#include "gcr_funcs.H"
 
-extern "C" {
+extern "C"
+{
 
-
-using namespace fapi;
-//================================================================================================================================
-// These functions work on a pair of targets. One is the master side of the bus interface, the other the slave side. For eg; in EDI(DMI2)PU is the master and Centaur is the slave
-// In EI4 both sides have pu targets
-ReturnCode io_post_trainadv(const Target& target){
-    ReturnCode rc;
-    //uint32_t master_group=0;
-    FAPI_DBG("Running IO POST TRAINING ADVANCED PROCEDURE");
-    //     ____  __  _______
-    //    / __ \/  |/  /  _/
-    //   / / / / /|_/ // /
-    //  / /_/ / /  / // /
-    // /_____/_/  /_/___/
-    if( (target.getType() == fapi::TARGET_TYPE_MCS_CHIPLET )){
-      FAPI_DBG("This is a Processor DMI bus post training invocation using base DMI scom address");
-      //master_interface=CP_IOMC0_P0; // base scom for MC bus
-      //master_group=3; // Design requires us to do this as per scom map and layout
-      // USER CODE HERE: ex. rc=run_offset_cal(target,master_interface,master_group);if (rc) {return(rc);};
-    }
-    else if( (target.getType() ==  fapi::TARGET_TYPE_MEMBUF_CHIP)){
-      FAPI_DBG("This is a Centaur DMI bus post training invocation using base DMI scom address");
-      //master_interface=CEN_DMI; // base scom for CEN
-      //master_group=0;
-      // USER CODE HERE: ex. rc=run_offset_cal(target,master_interface,master_group);if (rc) {return(rc);};
-    }
-    //
-    //   | |/ // /
-    //   |   // __ \/ / / / ___/
-    //  /   |/ /_/ / /_/ (__  )
-    // /_/|_/_.___/\__,_/____/
-    else if( (target.getType() == fapi::TARGET_TYPE_XBUS_ENDPOINT  )){
-      FAPI_DBG("This is a X Bus post training invocation");
-      //master_interface=CP_FABRIC_X0; // base scom for X bus
-      //master_group=0; // Design requires us to do this as per scom map and layout
-      if(rc.ok()){
-          // No Z cal in EI4/X bus design
-          for(int i=0;i<5;++i){
-             //master_group=i;
-              // USER CODE HERE: ex. rc=run_offset_cal(target,master_interface,master_group);if (rc) {return(rc);};
-          }
-      }
-    }
-    //     ___    __
-    //    /   |  / /_  __  _______
-    //   / /| | / __ \/ / / / ___/
-    //  / ___ |/ /_/ / /_/ (__  )
-    // /_/  |_/_.___/\__,_/____/
-    else if( (target.getType() == fapi::TARGET_TYPE_ABUS_ENDPOINT )){
-      FAPI_DBG("This is an A Bus post training invocation");
-      //master_interface=CP_FABRIC_A0; // base scom for A bus , assume translation to A1 by PLAT
-      //master_group=0; // Design requires us to do this as per scom map and layout
-                // EDI-A bus needs both impedance cal and offset cal
-      // USER CODE HERE: ex. rc=run_offset_cal(target,master_interface,master_group);if (rc) {return(rc);}
-    }
-    else{
-      FAPI_ERR("Invalid io_post_trainadv HWP invocation . Target doesnt belong to DMI/X/A instances");
-      // ADD THIS WHEN IMPLEMENTING FUNCTION: FAPI_SET_HWP_ERROR(rc, IO_post_TRAINADV_INVALID_INVOCATION_RC);
-    }
+fapi::ReturnCode io_post_trainadv(const fapi::Target & i_target)
+{
+    fapi::ReturnCode rc;
     return rc;
 }
-//================================================================================================================================
 
-
-} //end extern C
+} // extern "C"
