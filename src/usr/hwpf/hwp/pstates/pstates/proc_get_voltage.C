2782ebce014e077a16482604044f9ccd4a065078
Thi Tran <thi@us.ibm.com>
2013-12-07 09:30:00 -0600

    INITPROC: Hostboot - SW237453 - Ma set#2 RAS review
    Change-Id: Ib6c8d3fc5183ac3047e60b0ac69b295444690576
    CQ:SW237453
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/7590
    Tested-by: Jenkins Server
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>

--- src/usr/hwpf/hwp/pstates/pstates/proc_get_voltage.C
+++ src/usr/hwpf/hwp/pstates/pstates/proc_get_voltage.C
@@ -20,26 +20,29 @@
 /* Origin: 30                                                             */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
-// $Id: proc_get_voltage.C,v 1.5 2013/05/02 17:33:30 jimyac Exp $
+// $Id: proc_get_voltage.C,v 1.6 2013/12/04 18:54:49 jimyac Exp $
 // $Source: /afs/awd/projects/eclipz/KnowledgeBase/.cvsroot/eclipz/chips/p8/working/procedures/ipl/fapi/proc_get_voltage.C,v $
 //------------------------------------------------------------------------------
 // *! (C) Copyright International Business Machines Corp. 2012
 // *! All Rights Reserved -- Property of IBM
 // *! *** IBM Confidential ***
 //------------------------------------------------------------------------------
-// *! OWNER NAME: Jim Yacynych         Email: jimyac@us.ibm.com
+// *! TITLE       : proc_get_voltage.C
+// *! DESCRIPTION : Get VDD & VCS vid codes for given Frequency (FAPI)
 // *!
-
-/// \file proc_get_voltage.C
-/// \brief
-///
-/// \todo
-///   High-level procedure flow:
-/// \verbatim
-///
-/// Procedure Prereq:
-///   o System clocks are running
-/// \endverbatim
+// *! OWNER NAME  : Jim Yacynych         Email: jimyac@us.ibm.com
+// *! BACKUP NAME :
+// *!
+// *! ADDITIONAL COMMENTS :
+// *!   Procedure Prereq:
+// *!       - System clocks are running
+// *!
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//  Version     Date        Owner       Description
+//------------------------------------------------------------------------------
+//    1.2       11/07/13                RAS review changes
+//    1.1       mm/dd/yy    jimyac      Initial release
 //------------------------------------------------------------------------------
 
 // ----------------------------------------------------------------------
@@ -47,11 +50,10 @@
 // ----------------------------------------------------------------------
 #include <fapi.H>
 
-#include "pstate_tables.h"
-#include "lab_pstates.h"
-#include "pstates.h"
-
-#include "proc_get_voltage.H"
+#include <pstate_tables.h>
+#include <lab_pstates.h>
+#include <pstates.h>
+#include <proc_get_voltage.H>
 
 extern "C" {
 
@@ -60,79 +62,98 @@ using namespace fapi;
 // ----------------------------------------------------------------------
 // Function prototypes
 // ----------------------------------------------------------------------
-//ReturnCode freq2pState(uint32_t freq, PstateSuperStructure *pss, int32_t& pstate);
 
 // ----------------------------------------------------------------------
 // Function definitions
 // ----------------------------------------------------------------------
 /// \param[in]      i_target           Chip Target
 /// \param[in]      i_freq_mhz         frequency in Mhz
-/// \param[out]     *o_vdd_vid         vdd vid value 
+/// \param[out]     *o_vdd_vid         vdd vid value
 /// \param[out]     *o_vcs_vid         vcs vid value
 
 /// \retval FAPI_RC_SUCCESS
 /// \retval ERROR defined in xml
 
-ReturnCode
-proc_get_voltage(const Target& i_target, const uint32_t i_freq_mhz, uint8_t& o_vdd_vid, uint8_t& o_vcs_vid)
+ReturnCode proc_get_voltage(const Target&  i_target,
+                            const uint32_t i_freq_mhz,
+                            uint8_t& o_vdd_vid,
+                            uint8_t& o_vcs_vid)
 {
-  fapi::ReturnCode     l_rc;
-  int                  rc = 0;
-  PstateSuperStructure pss; 
-  gpst_entry_t         entry;
-  Pstate               freq_pstate       = 0;
-  Pstate               pmax              = 0;
-  uint8_t              freq_pstate_index = 0;
+    fapi::ReturnCode     l_rc;
+    int                  l_result = 0;
+    PstateSuperStructure l_pss;
+    gpst_entry_t         l_entry;
+    Pstate               l_freq_pstate       = 0;
+    Pstate               l_pmax              = 0;
+    uint8_t              l_freq_pstate_index = 0;
 
-  // -----------------------------
-  // create pstate super structure
-  // -----------------------------
-  FAPI_INF("Executing p8_build_pstate_datablock");
+    // -----------------------------
+    // create pstate super structure
+    // -----------------------------
+    
+    do
+    {
+        FAPI_IMP("Executing p8_build_pstate_datablock to create Pstate Superstructure");
+        FAPI_EXEC_HWP(l_rc, p8_build_pstate_datablock, i_target, &l_pss);
+        if (!l_rc.ok())
+        {
+            FAPI_ERR("Error calling p8_build_pstate_datablock");
+            break;
+        }
 
-  FAPI_EXEC_HWP(l_rc,  p8_build_pstate_datablock, i_target,  &pss);
-  if (!l_rc.ok()) {
-    FAPI_ERR("Error calling p8_build_pstate_datablock");
+        // ----------------------
+        // convert freq to pstate
+        // ----------------------
+        FAPI_INF("Look up Pstate for given frequency (%u Mhz) in Global Pstate Table in Pstate Superstructure", i_freq_mhz);
+        l_result = freq2pState(&(l_pss.gpst), (i_freq_mhz*1000), &l_freq_pstate);
+
+        if (l_result)
+        {
+            const uint32_t & FREQ_MHZ       = i_freq_mhz;
+            int            & FREQ2PSTATE_RC = l_result;
+            FAPI_ERR("**** ERROR : Pstate for given frequency (%u Mhz) is out of bounds of Pstate Values : max = %d min = %d", i_freq_mhz, PSTATE_MAX, PSTATE_MIN);
+            FAPI_SET_HWP_ERROR(l_rc, RC_PROCPM_GET_VOLTAGE_FREQ2PSTATE_ERROR);
+            break;
+        }
+
+        // ----------------------
+        // pstate bounds checking
+        // ----------------------
+        l_pmax = l_pss.gpst.pmin + l_pss.gpst.entries - 1;
+
+        FAPI_INF("l_freq_pstate = %d l_pmax = %d  pmin = %d entries = %d i_freq_mhz = %d\n", l_freq_pstate, l_pmax, l_pss.gpst.pmin, l_pss.gpst.entries, i_freq_mhz);
+
+        if (l_freq_pstate > l_pmax)
+        {
+            int8_t  & FREQ_PSTATE = l_freq_pstate;
+            int8_t  & PMAX        = l_pmax;
+            int8_t  & PMIN        = l_pss.gpst.pmin;
+            uint8_t & ENTRIES     = l_pss.gpst.entries;
+            FAPI_ERR("**** ERROR : Pstate (%d) for given frequency is greater than l_pmax (%d)", l_freq_pstate, l_pmax);
+            FAPI_SET_HWP_ERROR(l_rc, RC_PROCPM_GET_VOLTAGE_FREQ_PSTATE_GT_PMAX_ERROR);
+            break;
+        }
+
+        // clip l_freq_pstate to pmin if it is lower than pmin
+        if (l_freq_pstate < l_pss.gpst.pmin)
+        {
+            FAPI_INF("Pstate for given frequency is less than pmin, so clipping it to pmin");
+            l_freq_pstate = l_pss.gpst.pmin;
+        }
+
+        // --------------------------------------------------------
+        // convert pstate to pstate table index and lookup voltages
+        // --------------------------------------------------------
+        l_freq_pstate_index = l_freq_pstate - l_pss.gpst.pmin;
+        l_entry.value       = revle64(l_pss.gpst.pstate[l_freq_pstate_index].value);
+        o_vdd_vid           = l_entry.fields.evid_vdd;
+        o_vcs_vid           = l_entry.fields.evid_vcs;
+        
+        FAPI_INF("vid codes for pstate %d : vdd = 0x%02x vcs = 0x%02x", l_freq_pstate, o_vdd_vid, o_vcs_vid);
+        
+    } while(0);
+
     return l_rc;
-  } 
- 
-  // ----------------------
-  // convert freq to pstate
-  // ----------------------
-  rc = freq2pState(&(pss.gpst), (i_freq_mhz*1000), &freq_pstate);
-
-  if (rc) {
-    int & RETURN_CODE = rc;
-    FAPI_ERR("**** ERROR : Procedure freq2pState() returned %d", rc);
-    FAPI_SET_HWP_ERROR(l_rc, RC_PROCPM_GET_VOLTAGE_FREQ2PSTATE_ERROR);
-    return l_rc;
-  }  
-
-  // ----------------------
-  // pstate bounds checking
-  // ----------------------
-  pmax = pss.gpst.pmin + pss.gpst.entries - 1;
-  
-  FAPI_DBG("freq_pstate = %d pmax = %d  pmin = %d entries = %d i_freq_mhz = %d\n", freq_pstate, pmax, pss.gpst.pmin, pss.gpst.entries, i_freq_mhz);
-  
-  if (freq_pstate > pmax) {
-    FAPI_ERR("**** ERROR : Pstate for given frequency is greater than pmax %d > %d (pmax)", freq_pstate, pmax);
-    FAPI_SET_HWP_ERROR(l_rc, RC_PROCPM_GET_VOLTAGE_FREQ_PSTATE_GT_PMAX_ERROR);
-    return l_rc;
-  }
-  
-  if (freq_pstate < pss.gpst.pmin) {
-    freq_pstate = pss.gpst.pmin;
-  }
-
-  // --------------------------------------------------------
-  // convert pstate to pstate table index and lookup voltages
-  // --------------------------------------------------------
-  freq_pstate_index = freq_pstate - pss.gpst.pmin;
-  entry.value       = revle64(pss.gpst.pstate[freq_pstate_index].value);
-  o_vdd_vid         = entry.fields.evid_vdd;
-  o_vcs_vid         = entry.fields.evid_vcs;  
-  return l_rc;
 }
 
 } //end extern C
-
