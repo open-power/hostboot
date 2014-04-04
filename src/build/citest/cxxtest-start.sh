41241a9d96e37ab489c3796c9a938d1c26fc4fd7
Patrick Williams <iawillia@us.ibm.com>
2014-04-04 15:18:12 -0500

    Change autosim parameters to allow parallel builds.
    Change-Id: Ic805267746342c0b4893cc97f79a0b0b3476a4f4
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/10154
    Tested-by: Jenkins Server
    Reviewed-by: Donald E. Dahle <dedahle@us.ibm.com>
    Reviewed-by: Andrew J. Geissler <andrewg@us.ibm.com>
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>

--- src/build/citest/cxxtest-start.sh
+++ src/build/citest/cxxtest-start.sh
@@ -1,33 +1,39 @@
 #!/bin/sh
-#  IBM_PROLOG_BEGIN_TAG
-#  This is an automatically generated prolog.
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
 #
-#  $Source: src/build/citest/cxxtest-start.sh $
+# $Source: src/build/citest/cxxtest-start.sh $
 #
-#  IBM CONFIDENTIAL
+# IBM CONFIDENTIAL
 #
-#  COPYRIGHT International Business Machines Corp. 2011
+# COPYRIGHT International Business Machines Corp. 2011,2014
 #
-#  p1
+# p1
 #
-#  Object Code Only (OCO) source materials
-#  Licensed Internal Code Source Materials
-#  IBM HostBoot Licensed Internal Code
+# Object Code Only (OCO) source materials
+# Licensed Internal Code Source Materials
+# IBM HostBoot Licensed Internal Code
 #
-#  The source code for this program is not published or other-
-#  wise divested of its trade secrets, irrespective of what has
-#  been deposited with the U.S. Copyright Office.
+# The source code for this program is not published or otherwise
+# divested of its trade secrets, irrespective of what has been
+# deposited with the U.S. Copyright Office.
 #
-#  Origin: 30
+# Origin: 30
 #
-#  IBM_PROLOG_END
+# IBM_PROLOG_END_TAG
 #
 export WORKSPACE_DIR=`pwd`
 export COPYRIGHT_CHECK=${WORKSPACE_DIR}/src/build/tools/copyright-check.sh
 export AUTOCITEST=${WORKSPACE_DIR}/src/build/citest/autocitest
 export BACKING_BUILD=`cat ${WORKSPACE_DIR}/src/build/citest/etc/bbuild`
-CXXTEST_REMOTE_SANDBOX="cxxtest_sb"
 HOSTBOOT_IMAGE=img/hbicore_test.bin
+
+if [ -z $JOB_NAME -o -z $BUILD_NUMBER ];
+then
+    CXXTEST_REMOTE_SANDBOX="cxxtest_sb"
+else
+    CXXTEST_REMOTE_SANDBOX=`echo $JOB_NAME $BUILD_NUMBER | md5sum | head -c10`
+fi
 
 #
 #   Validate Copyright blocks
