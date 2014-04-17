06c726df9e0adfab971d7925e0ae0908b9229527
Patrick Williams <iawillia@us.ibm.com>
2014-04-16 22:34:34 -0500

    Reorganize citest scripts.
        Split pieces of the autocitest script out so that we can control the
        order of the pieces, allow some to run in parallel, etc.  This will
        enable us to build the error log parsers as part of Jenkins testing.
    
    Change-Id: I017c7b0516673dc267eb90adf03b4604602281e6
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/10673
    Tested-by: Jenkins Server
    Reviewed-by: Donald E. Dahle <dedahle@us.ibm.com>
    Reviewed-by: STEPHEN M. CPREK <smcprek@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>

--- src/build/citest/cxxtest-start.sh
+++ src/build/citest/cxxtest-start.sh
@@ -22,41 +22,17 @@
 #
 # IBM_PROLOG_END_TAG
 #
-export WORKSPACE_DIR=`pwd`
-export COPYRIGHT_CHECK=${WORKSPACE_DIR}/src/build/tools/copyright-check.sh
-export AUTOCITEST=${WORKSPACE_DIR}/src/build/citest/autocitest
-export BACKING_BUILD=`cat ${WORKSPACE_DIR}/src/build/citest/etc/bbuild`
+if [ -z $HOSTBOOT_CI_ENV_SETUP ];
+then
+    source "$HOSTBOOTROOT/src/build/citest/setup-env"
+fi
+
 HOSTBOOT_IMAGE=img/hbicore_test.bin
 
-if [ -z $JOB_NAME -o -z $BUILD_NUMBER ];
-then
-    CXXTEST_REMOTE_SANDBOX="cxxtest_sb"
-else
-    CXXTEST_REMOTE_SANDBOX=`echo $JOB_NAME $BUILD_NUMBER | md5sum | head -c10`
-fi
-
-#
-#   Validate Copyright blocks
-if [ -f $COPYRIGHT_CHECK ]; then
-    $COPYRIGHT_CHECK
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
-fi
-
-#
 #   Front end to autocitest - script to execute unit tests under simics.
 #
 ##  when jenkins runs it will create a workspace with the built code tree
 ##  and drop us into it.
-
-
-
-##  klog to get access to simics tools
-AFSPASSWD="/gsa/ausgsa/projects/h/hostboot/gerrit-server/afspasswd.sh"
-$AFSPASSWD | klog -cell austin -pipe
-$AFSPASSWD | klog -cell rchland -pipe
-
-$AUTOCITEST ${BACKING_BUILD} ${CXXTEST_REMOTE_SANDBOX} ${HOSTBOOT_IMAGE}
+autocitest ${BACKING_BUILD} ${HOSTBOOT_IMAGE}
 
 exit $?
