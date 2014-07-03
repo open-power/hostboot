38a5b53f55fbccced9b6f4bc0134dcbfe5fe39d0
Patrick Williams <iawillia@us.ibm.com>
2014-07-03 00:58:04 -0500

    Fix compile for endian calls on old host GCC
    Change-Id: I27e18a424ba4bad7dbd33cd17a5c23c65269f51a
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/11958
    Tested-by: Jenkins Server
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>

--- src/build/linker/gensyms.C
+++ src/build/linker/gensyms.C
@@ -5,7 +5,9 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* COPYRIGHT International Business Machines Corp. 2013,2014              */
+/* Contributors Listed Below - COPYRIGHT 2013,2014                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
 /*                                                                        */
 /* Licensed under the Apache License, Version 2.0 (the "License");        */
 /* you may not use this file except in compliance with the License.       */
@@ -33,6 +35,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+
+#ifndef be64toh
+    #include <byteswap.h>
+    #if __BYTE_ORDER == __LITTLE_ENDIAN
+        #define be64toh(x) __bswap_64(x)
+    #else
+        #define be64toh(x) (x)
+    #endif
+#endif
+
 
 using namespace std;
 
