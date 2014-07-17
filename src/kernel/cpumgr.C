cbcc22add05fb4a43faed34edd2b1ffa039dfc23
Patrick Williams <iawillia@us.ibm.com>
2014-07-17 14:01:40 -0500

    Restrict timebase sync to once per core.
        As part of Id3a3bc0b7367e61f2725af17975fe3ba068f69a9, I fixed the
        deferred work queue to not leak work objects if there are multiple
        objects.  When cores wake up, each thread inserts a work object to
        synchronize its timebase.  Now that they are not leaking, we are
        running this 8 times, which is causing enough clock drift that we
        are getting passed the timeout for core wakeups.
    
        Modify deferred work queue to allow us to skip performing work
        if there is already an outstanding deferred work object.  This
        will return us to running the timebase sync just once.
    
    Change-Id: Iccffeb9d0578dcd08d41d41ca6af1b82388e7e34
    RTC: 111512
    Backport: release-fips811
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/12281
    Tested-by: Jenkins Server
    Reviewed-by: Brian Silver <bsilver@us.ibm.com>
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>

--- src/kernel/cpumgr.C
+++ src/kernel/cpumgr.C
@@ -5,7 +5,9 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* COPYRIGHT International Business Machines Corp. 2010,2014              */
+/* Contributors Listed Below - COPYRIGHT 2010,2014                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
 /*                                                                        */
 /* Licensed under the Apache License, Version 2.0 (the "License");        */
 /* you may not use this file except in compliance with the License.       */
@@ -293,7 +295,7 @@ void CpuManager::startSlaveCPU(cpu_t* cpu)
         };
 
         SyncTimebase* deferred = new SyncTimebase();
-        DeferredQueue::insert(deferred);
+        DeferredQueue::insert(deferred, true /* only if empty */);
         DeferredQueue::execute();
     }
 
