31615f402551eaa9d77b94d2c79ed5110e825e85
Dean Sanner <dsanner@us.ibm.com>
2014-07-17 07:52:24 -0500

    Populate actual PVR value instead of architected version
    Change-Id: I5d19166fe949394fae536f5165ce6138be7f820b
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/12277
    Tested-by: Jenkins Server
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>

--- src/include/sys/mmio.h
+++ src/include/sys/mmio.h
@@ -5,7 +5,9 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* COPYRIGHT International Business Machines Corp. 2010,2014              */
+/* Contributors Listed Below - COPYRIGHT 2010,2014                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
 /*                                                                        */
 /* Licensed under the Apache License, Version 2.0 (the "License");        */
 /* you may not use this file except in compliance with the License.       */
@@ -55,6 +57,12 @@ void* mmio_dev_map(void *ra, uint64_t i_devDataSize);
  * @return int - 0 for successful unmap, non-zero otherwise
  */
 int mmio_dev_unmap(void *ea);
+
+/** @fn mmio_pvr_read()
+ *  @brief Reads and returns protected PVR register.
+ *  @return PVR register value
+ */
+uint64_t mmio_pvr_read();
 
 /** @fn mmio_hmer_read()
  *  @brief Reads and returns protected HMER register.
