4f25725547a88037201b86487a1415f4dcdce13e
Patrick Williams <iawillia@us.ibm.com>
2014-07-21 17:30:32 -0500

    Add -ffreestanding for GCC 4.8 compile.
    Change-Id: I4a33f476c79d6816e65875dd3a37883f5a72786f
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/12368
    Tested-by: Jenkins Server
    Reviewed-by: Brian Silver <bsilver@us.ibm.com>
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>

--- src/build/mkrules/cflags.env.mk
+++ src/build/mkrules/cflags.env.mk
@@ -37,7 +37,7 @@ endif
 COMMONFLAGS += $(OPT_LEVEL) -nostdlib
 CFLAGS += $(COMMONFLAGS) -mcpu=power7 -nostdinc -g -mno-vsx -mno-altivec\
           -Wall -Werror -mtraceback=no -pipe \
-	  -ffunction-sections -fdata-sections
+	  -ffunction-sections -fdata-sections -ffreestanding
 ASMFLAGS += $(COMMONFLAGS) -mcpu=power7
 CXXFLAGS += $(CFLAGS) -nostdinc++ -fno-rtti -fno-exceptions -Wall \
 	    -fuse-cxa-atexit
