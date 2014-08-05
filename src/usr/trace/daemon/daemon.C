507f42c384cb8bd8ff42dddb505738eaeb22ab78
Patrick Williams <iawillia@us.ibm.com>
2014-08-05 15:33:12 -0500

    Send final async message for trace extract buf.
    Change-Id: I677463ef481e4cd7fd616f5be5e49c0d4cea3b8a
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/12638
    Tested-by: Jenkins Server
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: A. Patrick Williams III <iawillia@us.ibm.com>

--- src/usr/trace/daemon/daemon.C
+++ src/usr/trace/daemon/daemon.C
@@ -5,7 +5,9 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* COPYRIGHT International Business Machines Corp. 2012,2014              */
+/* Contributors Listed Below - COPYRIGHT 2012,2014                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
 /*                                                                        */
 /* Licensed under the Apache License, Version 2.0 (the "License");        */
 /* you may not use this file except in compliance with the License.       */
@@ -756,6 +758,9 @@ namespace TRACEDAEMON
             sendExtractBuffer(curBuffer, curBufferSize);
             // curBuffer pointer is transfered to mailbox now.
         }
+
+        // Send one last message to the SP to indicate we're done.
+        sendExtractBuffer(NULL, 0);
 
     }
 
