3e1eb47ac126fc9a0c7e36b57fd85b414e14d21d
Brian Silver <bsilver@us.ibm.com>
2015-03-16 07:57:37 -0500

    FFDC class generation changes for caps, errors
      Change from lower-case to upper
      Fix method chaining for empty classes
      -Wall changes
      Rearrange headers to need only error_scope.H
    
    Change-Id: I034fd84d05f53f36079b2b50b4ab338bf8d5bdb9
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/16437
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Matt K. Light <mklight@us.ibm.com>
    Reviewed-by: Brian Silver <bsilver@us.ibm.com>
    Tested-by: Brian Silver <bsilver@us.ibm.com>

--- src/import/hwpf/fapi2/src/error_info.C
+++ src/import/hwpf/fapi2/src/error_info.C
@@ -118,9 +118,9 @@ namespace fapi2
         const CalloutPriorities::CalloutPriority i_priority):
         iv_target(i_target),
         iv_callout(i_callout),
+        iv_calloutPriority(i_priority),
         iv_deconfigure(i_deconfigure),
-        iv_gard(i_gard),
-        iv_calloutPriority(i_priority)
+        iv_gard(i_gard)
     {}
 
     ///
@@ -271,7 +271,7 @@ namespace fapi2
     ///
     void ErrorInfoEntryProcCallout::addErrorInfo(
         std::shared_ptr<ErrorInfo> i_info,
-        const void* const* i_object) const
+        const void* const* ) const
     {
         ErrorInfoProcedureCallout* ei = new ErrorInfoProcedureCallout(
             static_cast<ProcedureCallouts::ProcedureCallout>(iv_procedure),
@@ -396,7 +396,7 @@ namespace fapi2
     ///
     void ErrorInfoEntryCollectTrace::addErrorInfo(
         std::shared_ptr<ErrorInfo> i_info,
-        const void* const* i_object) const
+        const void* const* ) const
     {
         ErrorInfoCollectTrace* ei = new ErrorInfoCollectTrace(
             static_cast<CollectTraces::CollectTrace>(iv_eieTraceId));
