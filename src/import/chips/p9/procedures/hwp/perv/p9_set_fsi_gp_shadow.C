7086551ae356f1823fdfde5be5c7b15fddc5e51a
Abhishek Agarwal <abagarw8@in.ibm.com>
2015-05-05 11:43:04 +0200

    L1 Rev istep 0.(6-8,11,13,14),1.1,2.(1-13,15,18-20,22,26,27,30,32,34) V2
    Change-Id: I96f27016418726b5ddadcb67316d610ed7314ed6
    Original-Change-Id: I3ea0eec08ce479057277524021bfce540d7b63ca
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/17755
    Reviewed-by: Brian Silver <bsilver@us.ibm.com>
    Tested-by: Brian Silver <bsilver@us.ibm.com>
    Reviewed-by: SRINIVAS V. POLISETTY <srinivan@in.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/perv/p9_set_fsi_gp_shadow.C
@@ -0,0 +1,52 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/perv/p9_set_fsi_gp_shadow.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//------------------------------------------------------------------------------
+/// @file  p9_set_fsi_gp_shadow.C
+///
+/// @brief --IPL step 0.8 proc_prep_ipl
+// *!
+// *! OWNER NAME  : Abhishek Agarwal  Email: abagarw8@in.ibm.com
+// *! BACKUP NAME :                   Email:
+//------------------------------------------------------------------------------
+// *HWP HWP Owner   : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP FW Owner    : Brian Silver <bsilver@us.ibm.com>
+// *HWP Team        : Perv
+// *HWP Level       : 1
+// *HWP Consumed by : SBE
+//------------------------------------------------------------------------------
+
+
+//## auto_generated
+#include "p9_set_fsi_gp_shadow.H"
+fapi2::ReturnCode p9_set_fsi_gp_shadow(const
+                                       fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
+{
+    FAPI_DBG("p9_set_fsi_gp_shadow: Entering ...");
+
+    FAPI_DBG("p9_set_fsi_gp_shadow: Exiting ...");
+
+    return fapi2::FAPI2_RC_SUCCESS;
+
+}
