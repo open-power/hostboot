dcf213e3c2f6c0e510eca03195203e74a62b9d35
Patrick Williams <iawillia@us.ibm.com>
2015-08-03 06:53:11 -0500

    Infra: Build FAPI2.
    Change-Id: I2e501defe6f8014a64d4179f00d94146c7af4492
    Original-Change-Id: Id0a9f52204f388d66de41f41bcde78c956969c4f
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/19554
    Tested-by: Jenkins Server
    Reviewed-by: STEPHEN M. CPREK <smcprek@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/hwpf/fapi2/tools/parseErrorInfo.mk
@@ -0,0 +1,55 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/import/hwpf/fapi2/tools/parseErrorInfo.mk $
+#
+# OpenPOWER HostBoot Project
+#
+# Contributors Listed Below - COPYRIGHT 2015,2019
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+# Makefile to run the parseErrorInfo script.
+
+GENERATED = parseErrorInfo
+COMMAND = parseErrorInfo.pl
+
+SOURCES += $(FAPI2_ERROR_XML)
+SOURCES += $(GENPATH)/empty_error.xml
+
+TARGETS += hwp_return_codes.H
+TARGETS += hwp_error_info.H
+TARGETS += hwp_ffdc_classes.H
+TARGETS += collect_reg_ffdc.C
+TARGETS += set_sbe_error.H
+
+define parseErrorInfo_RUN
+		$(C1) $$< --output-dir=$$($(GENERATED)_PATH) $$(filter-out $$<,$$^)
+endef
+
+$(call BUILD_GENERATED)
+
+# Generate an empty error XML file so that the scripts pass if the
+# environment hasn't defined any of its own.
+GENERATED = empty_error_xml
+TARGETS += empty_error.xml
+
+define empty_error_xml_RUN
+		$(C1) echo "<hwpErrors/>" > $$($(GENERATED)_PATH)/empty_error.xml
+endef
+
+$(call BUILD_GENERATED)
