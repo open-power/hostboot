9d3d6f54df8f0a8dac0914d98c083ccc100d8dcd
Patrick Williams <iawillia@us.ibm.com>
2015-08-03 06:53:11 -0500

    Infra: Build FAPI2.
    Change-Id: I5e6ae1892e123beffe438432ff7e452c21c1a6e5
    Original-Change-Id: Id0a9f52204f388d66de41f41bcde78c956969c4f
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/19554
    Tested-by: Jenkins Server
    Reviewed-by: STEPHEN M. CPREK <smcprek@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/hwpf/fapi2/src/fapi2.mk
@@ -0,0 +1,63 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/import/hwpf/fapi2/src/fapi2.mk $
+#
+# OpenPOWER HostBoot Project
+#
+# Contributors Listed Below - COPYRIGHT 2015,2019
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+#
+# Makefile to build the FAPI2 libraries.
+#
+
+
+# Add common and generated parts to object list.
+FAPI2_MODULE_OBJS += array.o
+FAPI2_MODULE_OBJS += error_info.o
+FAPI2_MODULE_OBJS += ffdc.o
+FAPI2_MODULE_OBJS += fapi2_attribute_service.o
+#TODO RTC 134733: Can't find the basic XML for attributes, so
+#                 fapi2_chip_ec_feature.C doesn't compile.
+#FAPI2_MODULE_OBJS += fapi2_chip_ec_feature.o
+FAPI2_MODULE_OBJS += collect_reg_ffdc.o
+
+# Define common source and include paths.
+define FAPI2_MODULE_INCLUDES
+$(call ADD_MODULE_SRCDIR,$(1),$(GENPATH))
+$(call ADD_MODULE_INCDIR,$(1),$(FAPI2_PATH)/include)
+$(call ADD_MODULE_INCDIR,$(1),$(FAPI2_PLAT_INCLUDE))
+endef
+
+# Build real FAPI2 library that uses Cronus platform.
+MODULE = fapi2
+OBJS += $(FAPI2_MODULE_OBJS)
+$(eval $(call FAPI2_MODULE_INCLUDES,$(MODULE)))
+$(call BUILD_MODULE)
+
+# Build test FAPI2 library that uses the reference platform.
+# To do this, we just add the extra 'plat' directories to the srcdir / incdir
+# before the Cronus platform directories.
+MODULE = fapi2_reference
+OBJS += $(FAPI2_MODULE_OBJS)
+OBJS += plat_utils.o
+$(call ADD_MODULE_SRCDIR,fapi2_reference,$(FAPI2_PATH)/src/plat)
+$(call ADD_MODULE_INCDIR,fapi2_reference,$(FAPI2_PATH)/include/plat)
+$(eval $(call FAPI2_MODULE_INCLUDES,$(MODULE)))
+$(call BUILD_MODULE)
