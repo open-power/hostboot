0b52a260cf007f83fc554edc53f0768f30710cb8
Patrick Williams <iawillia@us.ibm.com>
2015-09-04 17:36:20 -0500

    Infra: Regenerate plat_attribute_service.H using Cronus script.
    Change-Id: Ia81dae205bc1d942da403884c8f5c8c775c514d4
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/20340
    Tested-by: Jenkins Server
    Reviewed-by: STEPHEN M. CPREK <smcprek@us.ibm.com>
    Reviewed-by: Matt K. Light <mklight@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/hwpf/fapi2/tools/createPlatAttrService.mk
@@ -0,0 +1,39 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/import/hwpf/fapi2/tools/createPlatAttrService.mk $
+#
+# OpenPOWER HostBoot Project
+#
+# Contributors Listed Below - COPYRIGHT 2015,2019
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+# Makefile to run fapi2CreateAttrGetSetMacros.pl script.
+
+GENERATED = createPlatAttrService
+$(GENERATED)_COMMAND_PATH = $(FAPI2_SCRIPT_PATH)/
+COMMAND = fapi2CreateAttrGetSetMacros.pl
+
+SOURCES += $(GENPATH)/attribute_ids.H
+TARGETS += plat_attribute_service.H
+
+define createPlatAttrService_RUN
+		$(C1) cd $$($(GENERATED)_PATH) && $$<
+endef
+
+$(call BUILD_GENERATED)
