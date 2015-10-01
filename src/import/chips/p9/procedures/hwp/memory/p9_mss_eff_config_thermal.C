f1d06ea7c2bae95c3b8d0413bfc97bff1fa046b9
Brian Silver <bsilver@us.ibm.com>
2015-10-01 08:51:04 -0500

    Change Memory L1 procedures
       Move from chips/centaur
       Add remaining interfaces
    
    Change-Id: I8f0fd2cd50ee57219dc262b8bf1c898eeed0ddf7
    Original-Change-Id: I85f568d5101fb46de031d884cb9aeee3fb32dd2b
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/20902
    Tested-by: Jenkins Server
    Reviewed-by: PRACHI GUPTA <pragupta@us.ibm.com>
    Reviewed-by: Andre A. Marin <aamarin@us.ibm.com>
    Reviewed-by: Richard J. Knight <rjknight@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/memory/p9_mss_eff_config_thermal.C
@@ -0,0 +1,52 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/memory/p9_mss_eff_config_thermal.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file p9_mss_eff_config_thermal.C
+/// @brief Perform thermal calculations as part of the effective configuration
+///
+// *HWP HWP Owner: Luke Mulkey <lwmulkey@us.ibm.com>
+// *HWP HWP Backup: Andre Marin <aamarin@us.ibm.com>
+// *HWP FW Owner: Brian Silver <bsilver@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 1
+// *HWP Consumed by: FSP:HB
+
+#include <fapi2.H>
+
+using fapi2::TARGET_TYPE_MCS;
+using fapi2::FAPI2_RC_SUCCESS;
+
+///
+/// @brief Perform thermal calculations as part of the effective configuration
+/// @param[in] i_target, the controller (e.g., MCS)
+/// @return FAPI2_RC_SUCCESS iff ok
+///
+fapi2::ReturnCode p9_mss_eff_config_thermal( const fapi2::Target<TARGET_TYPE_MCS>& i_target )
+{
+    FAPI_INF("Start effective config thermal");
+    FAPI_INF("End effective config thermal");
+    return FAPI2_RC_SUCCESS;
+}
