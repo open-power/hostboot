034ecbf62a45c149e5c36ba6aedfda339fef7c6f
Anusha Reddy Rangareddygari <anusrang@in.ibm.com>
2015-10-21 11:21:24 +0200

    Level 1 HWP p9_check_slave_sbe_seeprom_complete
    Change-Id: I3eb05475cc1b8b0a996924888816e30aa81069ca
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21366
    Tested-by: Jenkins Server
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: SRINIVAS V. POLISETTY <srinivan@in.ibm.com>
    Reviewed-by: Parvathi Rachakonda
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/23162
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/perv/p9_check_slave_sbe_seeprom_complete.C
@@ -0,0 +1,47 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: chips/p9/procedures/hwp/perv/p9_check_slave_sbe_seeprom_complete.C $ */
+/*                                                                        */
+/* IBM CONFIDENTIAL                                                       */
+/*                                                                        */
+/* EKB Project                                                            */
+/*                                                                        */
+/* COPYRIGHT 2015                                                         */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* The source code for this program is not published or otherwise         */
+/* divested of its trade secrets, irrespective of what has been           */
+/* deposited with the U.S. Copyright Office.                              */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//------------------------------------------------------------------------------
+/// @file  p9_check_slave_sbe_seeprom_complete.C
+///
+/// @brief Check slave SBE complete
+//------------------------------------------------------------------------------
+// *HWP HW Owner        : Anusha Reddy Rangareddygari <anusrang@in.ibm.com>
+// *HWP HW Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner        : Sunil Kumar <skumar8j@in.ibm.com>
+// *HWP Team            : Perv
+// *HWP Level           : 1
+// *HWP Consumed by     : HB
+//------------------------------------------------------------------------------
+
+
+//## auto_generated
+#include "p9_check_slave_sbe_seeprom_complete.H"
+
+
+
+fapi2::ReturnCode p9_check_slave_sbe_seeprom_complete(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
+        i_target_chiplet)
+{
+    FAPI_DBG("Entering ...");
+
+    FAPI_DBG("Exiting ...");
+
+    return fapi2::FAPI2_RC_SUCCESS;
+
+}
