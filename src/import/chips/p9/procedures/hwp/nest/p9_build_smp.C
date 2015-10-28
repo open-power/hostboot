fb0ba13083faea186faed2907a5cabd1a77ca50e
Joe McGill <jmcgill@us.ibm.com>
2015-10-28 16:20:27 -0500

    FBC Level 1 procedures
    Shells for p9_build_smp, p9_fab_iovalid, p9_smp_link_layer
    Supporting attribute definitions
    
    Change-Id: Id51401962caa98f85f4bf9b8bf4692e0b9514419
    Original-Change-Id: I59f7fb0f13ee190cd790ea5771f4a32faaa165d9
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21570
    Tested-by: Jenkins Server
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: CHRISTINA L. GRAVES <clgraves@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/nest/p9_build_smp.C
@@ -0,0 +1,60 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/nest/p9_build_smp.C $      */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p9_build_smp.C
+/// @brief Perform fabric configuration (FAPI2)
+///
+/// @author Joe McGill <jmcgill@us.ibm.com>
+/// @author Christy Graves <clgraves@us.ibm.com>
+///
+
+//
+// *HWP HWP Owner: Joe McGill <jmcgill@us.ibm.com>
+// *HWP FW Owner: Thi Tran <thi@us.ibm.com>
+// *HWP Team: Nest
+// *HWP Level: 1
+// *HWP Consumed by: HB,FSP
+//
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <p9_build_smp.H>
+
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+
+
+fapi2::ReturnCode
+p9_build_smp(std::vector<p9_build_smp_proc_chip>& i_proc_chips,
+             const p9_build_smp_operation i_op)
+
+{
+    FAPI_INF("Start");
+    FAPI_INF("End");
+    return fapi2::current_err;
+}
