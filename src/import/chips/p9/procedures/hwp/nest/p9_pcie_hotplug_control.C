53cebdebc92e0b729443f0d4d6211e22d2ab0ace
Joe McGill <jmcgill@us.ibm.com>
2015-10-29 09:26:10 -0500

    PCIE Level 1 procedures
    Shells for p9_pcie_scominit, p9_pcie_config, p9_pcie_hotplug_control
    Supporting attribute definitions
    
    Change-Id: Icd2c514a0f9b3cffdb64a1613a777bdcb95685bd
    Original-Change-Id: Ifdfee9a0aee08624fdc279355a5f46e0049417e7
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21599
    Tested-by: Jenkins Server
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: CHRISTINA L. GRAVES <clgraves@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/nest/p9_pcie_hotplug_control.C
@@ -0,0 +1,50 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/nest/p9_pcie_hotplug_control.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p9_pcie_hotplug_control.C
+/// @brief Manage slot power on hot-plug controlled slots (FAPI2)
+///
+// *HWP HWP Owner: Christina Graves clgraves@us.ibm.com
+// *HWP FW Owner: Thi Tran thi@us.ibm.com
+// *HWP Team: Nest
+// *HWP Level: 1
+// *HWP Consumed by: HB
+
+//-----------------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------------
+#include <p9_pcie_hotplug_control.H>
+
+
+//-----------------------------------------------------------------------------------
+// Function definitions
+//-----------------------------------------------------------------------------------
+fapi2::ReturnCode p9_pcie_hotplug_control(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target,
+        const bool i_enable_slot_power)
+{
+    FAPI_INF("Start");
+    FAPI_INF("End");
+    return fapi2::current_err;
+}
