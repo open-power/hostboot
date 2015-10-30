3b54a260064b4ccfa23babf4e3f675140fa77670
Thi Tran <thi@us.ibm.com>
2015-10-30 11:10:43 -0500

    p9_psi_scominit procedure (Level 1)
    Change-Id: Ib68958f19a4da373cbc796b76ede8f1bca4c9dc9
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21624
    Tested-by: Jenkins Server
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: CHRISTINA L. GRAVES <clgraves@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/nest/p9_psi_scominit.C
@@ -0,0 +1,71 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/nest/p9_psi_scominit.C $   */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+/// ----------------------------------------------------------------------------
+/// @file  p9_psi_scominit.H
+///
+/// Initializes PSI SCOM of the target proc.
+///
+/// ----------------------------------------------------------------------------
+/// *HWP HWP Owner   : Joe McGill <jmcgill@us.ibm.com>
+/// *HWP FW Owner    : Thi Tran <thi@us.ibm.com>
+/// *HWP Team        : Nest
+/// *HWP Level       : 1
+/// *HWP Consumed by : HB
+/// ----------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <p9_psi_scominit.H>
+
+///----------------------------------------------------------------------------
+/// Constant definitions
+///----------------------------------------------------------------------------
+
+extern "C" {
+
+///----------------------------------------------------------------------------
+/// Function definitions
+///----------------------------------------------------------------------------
+
+///
+/// @brief p9_psi_scominit procedure entry point
+/// See doxygen in p9_psi_scominit.H
+///
+    fapi2::ReturnCode p9_psi_scominit(
+        const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+    {
+        FAPI_DBG("Entering p9_psi_scominit");
+        fapi2::ReturnCode l_rc;
+
+        // TODO: Add code here.
+
+// fapi_try_exit:
+        FAPI_DBG("Exiting p9_psi_scominit");
+
+        return fapi2::current_err;
+    }
+
+} // extern "C"
