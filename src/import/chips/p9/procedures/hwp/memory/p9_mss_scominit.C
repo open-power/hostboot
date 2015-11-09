f6ec4d770c0ca6b4d7bd4686300a28097b9acb1a
Brian Silver <bsilver@us.ibm.com>
2015-11-09 13:40:47 -0600

    Add p9_mss_scominit
    Change-Id: I02408dfd23cdf876b6d4ff8f062a6f19ed72518e
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21909
    Tested-by: Jenkins Server
    Reviewed-by: Craig C. Hamilton <cchamilt@us.ibm.com>
    Reviewed-by: Andre A. Marin <aamarin@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/memory/p9_mss_scominit.C
@@ -0,0 +1,52 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/memory/p9_mss_scominit.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file p9_mss_scominit.C
+/// @brief SCOM inits for PHY, MC
+///
+// *HWP HWP Owner: Brian Silver <bsilver@us.ibm.com>
+// *HWP HWP Backup: Craig Hamilton <cchamilt@us.ibm.com>
+// *HWP FW Owner: Andre Marin <aamarin@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 1
+// *HWP Consumed by: FSP:HB
+
+#include <fapi2.H>
+
+using fapi2::TARGET_TYPE_MCBIST;
+using fapi2::FAPI2_RC_SUCCESS;
+
+///
+/// @brief SCOM inits for PHY, MC
+/// @param[in] i_target, the MCBIST
+/// @return FAPI2_RC_SUCCESS iff ok
+///
+fapi2::ReturnCode p9_mss_scominit( const fapi2::Target<TARGET_TYPE_MCBIST>& i_target )
+{
+    FAPI_INF("Start MSS SCOM init");
+    FAPI_INF("End MSS SCOM init");
+    return FAPI2_RC_SUCCESS;
+}
