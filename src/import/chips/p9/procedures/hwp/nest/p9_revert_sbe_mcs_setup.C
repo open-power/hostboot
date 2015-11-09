2673ee6c96b5239d6eafde10dc34af5729d754e4
Joe McGill <jmcgill@us.ibm.com>
2015-11-09 13:28:05 -0600

    Updates for p9_revert_sbe_mcs_setup, p9_sbe_mcs_setup (Level 2)
      Add p9_revert_sbe_mcs_setup L2 content
      Update p9_sbe_mcs_setup to consider drawer base address, MI target type
      Add common routine in p9_fbc_utils to determine chip base addresses
      Update p9_sbe_bootloader to reference common base address routine
      Change size of fabric system ID attribute to accomodate small sytem memory map
    
    Change-Id: I8fa658249d3fab8075e06dd9f4b0f8823f7ecf0d
    Original-Change-Id: Ifaf016b04c8cf7efbd6e5ee668f22a8059f0ed8d
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/21908
    Tested-by: Jenkins Server
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: CHRISTINA L. GRAVES <clgraves@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/nest/p9_revert_sbe_mcs_setup.C
@@ -0,0 +1,148 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/nest/p9_revert_sbe_mcs_setup.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p9_revert_sbe_mcs_setup.C
+/// @brief Revert MC configuration applied by SBE (FAPI2)
+
+///
+/// @author Joe McGill <jmcgill@us.ibm.com>
+///
+
+//
+// *HWP HWP Owner: Joe McGill <jmcgill@us.ibm.com>
+// *HWP FW Owner: Thi Tran <thi@us.ibm.com>
+// *HWP Team: Nest
+// *HWP Level: 2
+// *HWP Consumed by: HB
+//
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <p9_revert_sbe_mcs_setup.H>
+#include <p9_mc_scom_addresses.H>
+#include <p9_mc_scom_addresses_fld.H>
+
+
+//------------------------------------------------------------------------------
+// Function prototypes
+//------------------------------------------------------------------------------
+
+///
+/// @brief Reset SBE applied hostboot dcbz MC configuration for one unit target
+///
+/// @param[in] i_target Reference to an MC target (MCS/MI)
+/// @return FAPI2_RC_SUCCESS if success, else error code.
+///
+template<fapi2::TargetType T>
+fapi2::ReturnCode revert_hb_dcbz_config(const fapi2::Target<T>& i_target);
+
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+
+
+// specialization for MCS target type
+template<>
+fapi2::ReturnCode revert_hb_dcbz_config(const fapi2::Target<fapi2::TARGET_TYPE_MCS>& i_target)
+{
+    FAPI_DBG("Start");
+    fapi2::buffer<uint64_t> l_mcfgp;
+    fapi2::buffer<uint64_t> l_mcmode1;
+    fapi2::buffer<uint64_t> l_mcfirmask;
+
+    // MCFGP -- mark BAR invalid & reset grouping configuration fields
+    FAPI_TRY(fapi2::getScom(i_target, MCS_MCFGP, l_mcfgp),
+             "Error from getScom (MCS_MCFGP)");
+    l_mcfgp.clearBit<MCS_MCFGP_VALID>();
+    l_mcfgp.clearBit<MCS_MCFGP_MC_CHANNELS_PER_GROUP,
+                     MCS_MCFGP_MC_CHANNELS_PER_GROUP_LEN>();
+    l_mcfgp.clearBit<MCS_MCFGP_CHANNEL_0_GROUP_MEMBER_IDENTIFICATION,
+                     MCS_MCFGP_CHANNEL_0_GROUP_MEMBER_IDENTIFICATION_LEN>();
+    l_mcfgp.clearBit<MCS_MCFGP_GROUP_SIZE, MCS_MCFGP_GROUP_SIZE_LEN>();
+    FAPI_TRY(fapi2::putScom(i_target, MCS_MCFGP, l_mcfgp),
+             "Error from putScom (MCS_MCFGP)");
+
+    // MCMODE1 -- enable speculation
+    FAPI_TRY(fapi2::getScom(i_target, MCS_MCMODE1, l_mcmode1),
+             "Error from getScom (MCS_MCMODE1)");
+    l_mcmode1.clearBit<MCS_MCMODE1_DISABLE_ALL_SPEC_OPS>();
+    l_mcmode1.clearBit<MCS_MCMODE1_DISABLE_SPEC_OP,
+                       MCS_MCMODE1_DISABLE_SPEC_OP_LEN>();
+    FAPI_TRY(fapi2::putScom(i_target, MCS_MCMODE1, l_mcmode1),
+             "Error from putScom (MCS_MCMODE1)");
+
+    // MCFIRMASK -- mask all errors
+    l_mcfirmask.flush<1>();
+    FAPI_TRY(fapi2::putScom(i_target, MCS_MCFIRMASK_OR, l_mcfirmask),
+             "Error from putScom (MCS_MCFIRMASK_OR)");
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
+
+
+// specialization for MI target type
+template<>
+fapi2::ReturnCode revert_hb_dcbz_config(const fapi2::Target<fapi2::TARGET_TYPE_MI>& i_target)
+{
+    // TODO: implement for Cumulus (MI target)
+    return fapi2::current_err;
+}
+
+
+// HWP entry point
+fapi2::ReturnCode
+p9_revert_sbe_mcs_setup(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    FAPI_INF("Start");
+
+    auto l_mcs_chiplets = i_target.getChildren<fapi2::TARGET_TYPE_MCS>();
+
+    if (l_mcs_chiplets.size())
+    {
+        for (auto l_target_mcs : l_mcs_chiplets)
+        {
+            FAPI_TRY(revert_hb_dcbz_config(l_target_mcs),
+                     "Error from revert_hb_dcbz_config (MCS)");
+        }
+    }
+    else
+    {
+        auto l_mi_chiplets = i_target.getChildren<fapi2::TARGET_TYPE_MI>();
+
+        for (auto l_target_mi : l_mi_chiplets)
+        {
+            FAPI_TRY(revert_hb_dcbz_config(l_target_mi),
+                     "Error from revert_hb_dcbz_config (MI)");
+        }
+    }
+
+fapi_try_exit:
+    FAPI_INF("End");
+    return fapi2::current_err;
+}
