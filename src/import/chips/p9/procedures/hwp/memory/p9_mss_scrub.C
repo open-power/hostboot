8e11971cc91b377ff466355bf688bf8c2db86e21
Brian Silver <bsilver@us.ibm.com>
2015-11-30 07:14:46 -0600

    Add p9_mss_scrub level 1
      Remove using declarations from common header
    
    Change-Id: I18554ae892f6cb3f0457702bddb5cb2f29a7ea2f
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/22342
    Tested-by: Jenkins Server
    Reviewed-by: Craig C. Hamilton <cchamilt@us.ibm.com>
    Reviewed-by: WILLIAM G. HOFFA <wghoffa@us.ibm.com>
    Reviewed-by: PRACHI GUPTA <pragupta@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/memory/p9_mss_scrub.C
@@ -0,0 +1,46 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: chips/centaur/procedures/ipl/hwp/p9_mss_scrub.C $       */
+/*                                                                        */
+/* IBM CONFIDENTIAL                                                       */
+/*                                                                        */
+/* EKB Project                                                            */
+/*                                                                        */
+/* COPYRIGHT 2015                                                         */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* The source code for this program is not published or otherwise         */
+/* divested of its trade secrets, irrespective of what has been           */
+/* deposited with the U.S. Copyright Office.                              */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file p9_mss_scrub.C
+/// @brief Begin background scrub
+///
+// *HWP HWP Owner: Brian Silver <bsilver@us.ibm.com>
+// *HWP HWP Backup: Luke Mulkey <lwmulkey@us.ibm.com>
+// *HWP FW Owner: Andre Marin <aamarin@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 1
+// *HWP Consumed by: FSP:HB
+
+#include <fapi2.H>
+
+using fapi2::TARGET_TYPE_MCBIST;
+using fapi2::FAPI2_RC_SUCCESS;
+
+///
+/// @brief Begin background scrub
+/// @param[in] i_target, MCBIST
+/// @return FAPI2_RC_SUCCESS iff ok
+///
+fapi2::ReturnCode p9_mss_scrub( const fapi2::Target<TARGET_TYPE_MCBIST>& i_target )
+{
+    FAPI_INF("Start mss scrub");
+    FAPI_INF("End mss scrub");
+    return FAPI2_RC_SUCCESS;
+}
