a7ca81fa4a0faee18407d26534ee81de3046981c
Chris Steffen <cwsteffen@us.ibm.com>
2015-12-02 09:41:43 -0600

    P9 I/O Xbus Clear Firs L2
    Change-Id: I4d1721d4b47e06bdcf50671e8f52b6c849b61ad5
    Original-Change-Id: I3a796c132f7b34f48f8b356f4459153f2576d764
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/22391
    Tested-by: Jenkins Server
    Reviewed-by: Richard J. Knight <rjknight@us.ibm.com>
    Reviewed-by: Gary A. Peterson <garyp@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/io/p9_io_xbus_clear_firs.C
@@ -0,0 +1,146 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/io/p9_io_xbus_clear_firs.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p9_io_xbus_clear_firs.C
+/// @brief Clears I/O Firs
+///-----------------------------------------------------------------------------
+/// *HWP HWP Owner        : Chris Steffen <cwsteffen@us.ibm.com>
+/// *HWP HWP Backup Owner : Gary Peterson <garyp@us.ibm.com>
+/// *HWP FW Owner         : Jamie Knight <rjknight@us.ibm.com>
+/// *HWP Team             : IO
+/// *HWP Level            : 2
+/// *HWP Consumed by      : FSP:HB
+///-----------------------------------------------------------------------------
+///
+/// @verbatim
+/// High-level procedure flow:
+///
+/// Clears I/O Xbus FIRs on the PHY Rx/Tx.
+///
+/// Clocks must be running.
+///
+/// @endverbatim
+///----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//  Includes
+//-----------------------------------------------------------------------------
+#include <p9_io_xbus_clear_firs.H>
+#include <p9_io_gcr.H>
+#include <p9_io_regs.H>
+
+//-----------------------------------------------------------------------------
+//  Definitions
+//-----------------------------------------------------------------------------
+fapi2::ReturnCode io_rx_fir_reset(
+    const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_target,
+    const uint8_t& i_clock_group);
+
+fapi2::ReturnCode io_tx_fir_reset(
+    const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_target,
+    const uint8_t& i_clock_group);
+
+/**
+ * @brief A HWP that runs on every instance of the XBUS(EDI+)
+ * @param[in] i_target         FAPI2 Target
+ * @param[in] i_clock_group    Clock Group
+ * @retval ReturnCode
+ */
+fapi2::ReturnCode p9_io_xbus_clear_firs(
+    const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_target,
+    const uint8_t& i_clock_group)
+{
+    FAPI_IMP("I/O Start Xbus Clear FIRs");
+
+    FAPI_TRY(io_tx_fir_reset(i_target, i_clock_group), "Tx Reset Failed");
+
+    FAPI_TRY(io_rx_fir_reset(i_target, i_clock_group), "Rx Reset Failed");
+
+fapi_try_exit:
+    FAPI_IMP("I/O End Xbus Clear FIRs");
+    return fapi2::current_err;
+}
+
+/**
+ * @brief This function resets the Rx Firs on a EDI+ Xbus
+ * @param[in] i_target       FAPI2 Target
+ * @param[in] i_clock_group  Clock Group
+ * @retval ReturnCode
+ */
+fapi2::ReturnCode io_rx_fir_reset(
+    const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_target,
+    const uint8_t& i_clock_group)
+{
+    Register < EDIP_RX_GLBSM_CNTL1_EO_PG > rx_fir_reg;
+
+    FAPI_TRY(rx_fir_reg.read(i_target, i_clock_group),
+             "Reading Rx Fir Reg Failed");
+
+    rx_fir_reg.set<EDIP_RX_FIR_RESET>(0);
+    FAPI_TRY(rx_fir_reg.write(i_target, i_clock_group),
+             "Writing Rx Fir Reg Failed");
+
+    rx_fir_reg.set<EDIP_RX_FIR_RESET>(1);
+    FAPI_TRY(rx_fir_reg.write(i_target, i_clock_group),
+             "Writing rx Fir Reg Failed");
+
+    rx_fir_reg.set<EDIP_RX_FIR_RESET>(0);
+    FAPI_TRY(rx_fir_reg.write(i_target, i_clock_group),
+             "Writing Rx Fir Reg Failed");
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+/**
+ * @brief This function resets the Tx Firs on a EDI+ Xbus
+ * @param[in] i_target       FAPI2 Target
+ * @param[in] i_clock_group  Clock Group
+ * @retval ReturnCode
+ */
+fapi2::ReturnCode io_tx_fir_reset(
+    const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_target,
+    const uint8_t& i_clock_group)
+{
+    Register < EDIP_TX_FIR_RESET_PG  > tx_fir_reg;
+
+    FAPI_TRY(tx_fir_reg.read(i_target, i_clock_group),
+             "Reading Tx Fir Reg Failed");
+
+    tx_fir_reg.set<EDIP_TX_FIR_RESET>(0);
+    FAPI_TRY(tx_fir_reg.write(i_target, i_clock_group),
+             "Writing Tx Fir Reg Failed");
+
+    tx_fir_reg.set<EDIP_TX_FIR_RESET>(1);
+    FAPI_TRY(tx_fir_reg.write(i_target, i_clock_group),
+             "Writing Tx Fir Reg Failed");
+
+    tx_fir_reg.set<EDIP_TX_FIR_RESET>(0);
+    FAPI_TRY(tx_fir_reg.write(i_target, i_clock_group),
+             "Writing Tx Fir Reg Failed");
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
