6c329dc6f53f85dfdd81b1ae3afbb28f054d05a5
CHRISTINA L. GRAVES <clgraves@us.ibm.com>
2015-12-04 13:46:39 -0600

    Adding the L1s for p9_tod_init and p9_tod_setup
    Change-Id: I1aa2d0e8351fb0143acea110e36e8d011aef45a2
    Original-Change-Id: I6913fe7c0037368fc2546c5b4959ffc3fb268e66
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/22502
    Tested-by: Jenkins Server
    Reviewed-by: James N. Klazynski <jklazyns@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/29255
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/nest/p9_tod_setup.C
@@ -0,0 +1,65 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/nest/p9_tod_setup.C $      */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2016                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//--------------------------------------------------------------------------
+//
+//
+/// @file p9_tod_setup.C
+/// @brief Procedures to configure the TOD topology
+///
+// *HWP HWP Owner Christina Graves clgraves@us.ibm.com
+// *HWP FW Owner: Thi Tran thi@us.ibm.com
+// *HWP Team: Nest
+// *HWP Level: 1
+// *HWP Consumed by: SBE
+//
+//--------------------------------------------------------------------------
+
+
+//--------------------------------------------------------------------------
+// Includes
+//--------------------------------------------------------------------------
+#include <p9_tod_setup.H>
+
+extern "C" {
+
+//--------------------------------------------------------------------------
+//  HWP entry point
+//--------------------------------------------------------------------------
+    fapi2::ReturnCode p9_tod_setup(
+        tod_topology_node* i_tod_node,
+        const p9_tod_setup_tod_sel i_tod_sel,
+        const p9_tod_setup_osc_sel i_osc_sel)
+    {
+        // mark HWP entry
+        FAPI_DBG("Entering ...\n");
+
+
+        /*fapi_try_exit:*/
+        FAPI_DBG("Exiting...");
+        return fapi2::current_err;
+    }
+
+} // extern "C"
+
---
+++ src/import/chips/p9/procedures/hwp/nest/p9_tod_utils.C
@@ -0,0 +1,77 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/nest/p9_tod_utils.C $      */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2016                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//--------------------------------------------------------------------------
+//
+//
+/// @file p9_tod_utils.C
+/// @brief Utilities and classes for the tod_init and tod_setup procedures
+///
+// *HWP HWP Owner Christina Graves clgraves@us.ibm.com
+// *HWP FW Owner: Thi Tran thi@us.ibm.com
+// *HWP Team: Nest
+// *HWP Level: 1
+// *HWP Consumed by: SBE
+//
+//--------------------------------------------------------------------------
+
+
+//--------------------------------------------------------------------------
+// Includes
+//--------------------------------------------------------------------------
+#include <p9_tod_utils.H>
+
+extern "C" {
+
+//--------------------------------------------------------------------------
+//  HWP entry point
+//--------------------------------------------------------------------------
+    fapi2::ReturnCode p9_tod_utils_get_tfmr_reg(
+        const fapi2::Target<fapi2::TARGET_TYPE_CORE>& i_target,
+        ecmdDataBufferBase& o_tfmr_val)
+    {
+        // mark HWP entry
+        FAPI_DBG("Entering ...\n");
+
+
+        /*fapi_try_exit:*/
+        FAPI_DBG("Exiting...");
+        return fapi2::current_err;
+    }
+
+    fapi2::ReturnCode p9_tod_utils_set_tfmr_reg(
+        const fapi2::Target<fapi2::TARGET_TYPE_CORE>& i_target,
+        ecmdDataBufferBase& i_tfmr_val)
+    {
+        // mark HWP entry
+        FAPI_DBG("Entering ...\n");
+
+
+        /*fapi_try_exit:*/
+        FAPI_DBG("Exiting...");
+        return fapi2::current_err;
+    }
+
+} // extern "C"
+
