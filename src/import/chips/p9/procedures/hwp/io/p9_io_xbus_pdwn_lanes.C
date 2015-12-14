c4c140cabb6c6dacf9485a5e1d20f4b5fe779895
Stephen Cprek <smcprek@us.ibm.com>
2015-12-14 14:19:48 -0600

    Fix all incorrect copyright prologs
    Change-Id: I109eb532fa78f22a245ae7f6deb89de40e663010
    Original-Change-Id: I293e79b5a37bf4180f6dd19d259fac3434327fb3
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/22759
    Tested-by: Jenkins Server
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: PRACHI GUPTA <pragupta@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23244
    Tested-by: FSP CI Jenkins
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/io/p9_io_xbus_pdwn_lanes.C
+++ src/import/chips/p9/procedures/hwp/io/p9_io_xbus_pdwn_lanes.C
@@ -1,7 +1,7 @@
 /* IBM_PROLOG_BEGIN_TAG                                                   */
 /* This is an automatically generated prolog.                             */
 /*                                                                        */
-/* $Source: chips/p9/procedures/hwp/io/p9_io_xbus_pdwn_lanes.C $     */
+/* $Source: chips/p9/procedures/hwp/io/p9_io_xbus_pdwn_lanes.C $          */
 /*                                                                        */
 /* IBM CONFIDENTIAL                                                       */
 /*                                                                        */
