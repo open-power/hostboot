4e5b1ac13de90e247b8c5dbd0f31b4a02d55b0af
Brian Silver <bsilver@us.ibm.com>
2015-12-28 11:26:59 -0600

    Initial commit of memory subsystem
    Change-Id: I451895f69418927dac3ee3486475d0634ad3f29a
    Original-Change-Id: I6b63d2c4eec5d77585c91d905a464962a6153a0a
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/22978
    Tested-by: Jenkins Server
    Reviewed-by: Craig C. Hamilton <cchamilt@us.ibm.com>
    Reviewed-by: Brian Silver <bsilver@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/58297
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/memory/tests/p9_mss_ut.mk
@@ -0,0 +1,34 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/import/chips/p9/procedures/hwp/memory/tests/p9_mss_ut.mk $
+#
+# OpenPOWER HostBoot Project
+#
+# Contributors Listed Below - COPYRIGHT 2015,2018
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+-include 00common.mk
+
+MSS_UT_SOURCE := $(shell find $(ROOTPATH)/chips/p9/procedures/hwp/memory/tests -name '*.C' -exec basename {} \;)
+
+WRAPPER=p9_mss_ut
+OBJS += $(patsubst %.C,%.o,$(MSS_UT_SOURCE))
+$(call ADD_EXE_INCDIR,$(WRAPPER),$(MSS_INCLUDES))
+$(WRAPPER)_DEPLIBS+=mss
+$(call BUILD_WRAPPER)
