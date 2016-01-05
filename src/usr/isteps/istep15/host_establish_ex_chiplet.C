d3b1b0c3058c0a24f6a59148723459e15e2e5673
crgeddes <crgeddes@us.ibm.com>
2016-01-05 09:30:57 -0600

    Integrate HWPs for step 15
    This commit includes work to create the call_HWP wrappers
    for istep15.
    
    RTC:133832
    Change-Id: Ib0db5ac2b9b5d5f5c2967ff97794493d867fb04b
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/774
    Reviewed-by: A. P. Williams III <iawillia@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Richard J. Knight <rjknight@us.ibm.com>
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
    Tested-by: Jenkins Server
    Tested-by: FSP CI Jenkins

---
+++ src/usr/isteps/istep15/host_establish_ex_chiplet.C
@@ -0,0 +1,85 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/usr/isteps/istep15/host_establish_ex_chiplet.C $          */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+//From Hostboot Directory
+////Error handling and traces
+#include    <errl/errlentry.H>
+#include    <errl/errluserdetails.H>
+#include    <errl/errludtarget.H>
+#include    <errl/errlmanager.H>
+#include    <isteps/hwpisteperror.H>
+#include    <initservice/isteps_trace.H>
+
+//HWP Invoker
+#include    <fapi2/plat_hwp_invoker.H>
+
+//Targeting Support
+#include    <targeting/common/utilFilter.H>
+#include    <fapi2/target.H>
+
+//From Import Directory (EKB Repository)
+#include    <p9_update_ec_eq_state.H>
+
+//Namespaces
+using namespace ERRORLOG;
+using namespace TARGETING;
+using namespace fapi2;
+
+namespace ISTEP_15
+{
+void* host_establish_ex_chiplet (void *io_pArgs)
+{
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "host_establish_ex_chiplet entry" );
+    ISTEP_ERROR::IStepError l_StepError;
+    errlHndl_t l_errl = NULL;
+    do {
+        //Use targeting code to get a list of all processors
+        TARGETING::TargetHandleList l_procChips;
+        getAllChips( l_procChips, TARGETING::TYPE_PROC   );
+
+        for (const auto & l_procChip: l_procChips)
+        {
+            const fapi2::Target<TARGET_TYPE_PROC_CHIP>
+                l_fapi_cpu_target(l_procChip);
+            // call p9_update_ec_eq_state.C HWP
+            FAPI_INVOKE_HWP( l_errl,
+                             p9_update_ec_eq_state,
+                             l_fapi_cpu_target);
+
+            if(l_errl)
+            {
+                ErrlUserDetailsTarget(l_procChip).addToLog(l_errl);
+                l_StepError.addErrorDetails( l_errl );
+                errlCommit( l_errl, HWPF_COMP_ID );
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "host_establish_ex_chiplet:: failed on proc with HUID : %d",TARGETING::get_huid(l_procChip)  );
+            }
+        }
+    }while(0);
+
+    // end task, returning any errorlogs to IStepDisp
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "host_establish_ex_chiplet exit" );
+    return l_StepError.getErrorHandle();
+}
+};
---
+++ src/usr/isteps/istep15/host_start_stop_engine.C
@@ -0,0 +1,93 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/usr/isteps/istep15/host_start_stop_engine.C $             */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+//From Hostboot Directory
+////Error handling and traces
+#include    <errl/errlentry.H>
+#include    <errl/errlmanager.H>
+#include    <isteps/hwpisteperror.H>
+#include    <initservice/isteps_trace.H>
+#include    <errl/errluserdetails.H>
+#include    <errl/errludtarget.H>
+
+//HWP Invoker
+#include    <fapi2/plat_hwp_invoker.H>
+
+////Targeting support
+#include    <fapi2/target.H>
+#include    <targeting/common/utilFilter.H>
+
+//From Import Directory (EKB Repository)
+#include <p9_pm.H>
+#include <p9_pm_stop_gpe_init.H>
+
+//Namespaces
+using namespace ERRORLOG;
+using namespace TARGETING;
+using namespace p9pm;
+using namespace fapi2;
+
+namespace ISTEP_15
+{
+void* host_start_stop_engine (void *io_pArgs)
+{
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_host_start_stop_engine entry" );
+    ISTEP_ERROR::IStepError     l_StepError;
+    errlHndl_t l_errl = NULL;
+
+    do {
+        //Use targeting code to get a list of all processors
+        TARGETING::TargetHandleList l_procChips;
+        getAllChips( l_procChips, TARGETING::TYPE_PROC   );
+
+        for (const auto & l_procChip: l_procChips)
+        {
+            //Convert the TARGETING::Target into a fapi2::Target by passing
+            //the const_casted l_procChip into the fapi::Target constructor
+            fapi2::Target<TARGET_TYPE_PROC_CHIP>l_fapi2_cpu_target(
+                                                              (l_procChip));
+
+//RTC:149398 Re-enable when new vector constructor is implemented
+          //call p9_pm_stop_gpe_init.C HWP
+//               FAPI_INVOKE_HWP(l_errl,
+//                               p9_pm_stop_gpe_init,
+//                               l_fapi2_cpu_target,
+//                               PM_INIT);
+            if(l_errl)
+            {
+                ErrlUserDetailsTarget(l_procChip).addToLog(l_errl);
+                l_StepError.addErrorDetails( l_errl );
+                errlCommit( l_errl, HWPF_COMP_ID );
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "host_start_stop_engine:: failed on proc with HUID : %d",TARGETING::get_huid(l_procChip)  );
+            }
+        }
+      }while (0);
+
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_host_start_stop_engine exit" );
+    // end task, returning any errorlogs to IStepDisp
+    return l_StepError.getErrorHandle();
+}
+
+};
