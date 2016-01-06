2486e35b6d1814dddbfca0b2f37eaa1c34ec072e
Thi Tran <thi@us.ibm.com>
2016-01-06 13:31:59 -0600

    Multithread VREF calculations for DDR4
    Change-Id: Iac73ff78118d4e356d6600f18b63bc1d9c545258
    CQ: SW330898
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/22925
    Tested-by: Jenkins Server
    Tested-by: Jenkins OP Build CI
    Tested-by: Jenkins OP HW
    Tested-by: FSP CI Jenkins
    Reviewed-by: William H. Schwartz <whs@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/hwpf/hwp/dram_training/dram_training.C
+++ src/usr/hwpf/hwp/dram_training/dram_training.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2012,2015                        */
+/* Contributors Listed Below - COPYRIGHT 2012,2016                        */
 /* [+] Google Inc.                                                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
@@ -43,6 +43,7 @@
 #include    <errl/errlentry.H>
 
 #include    <initservice/isteps_trace.H>
+#include    <util/threadpool.H>
 
 //  targeting support
 #include    <targeting/common/commontargeting.H>
@@ -90,6 +91,236 @@ using   namespace   ISTEP_ERROR;
 using   namespace   ERRORLOG;
 using   namespace   TARGETING;
 using   namespace   fapi;
+
+typedef fapi::ReturnCode (*mss_FP_t)(const fapi::Target& i_target);
+typedef std::list<errlHndl_t> errList;
+
+class draminitTask;
+
+//Simple multithread task controller
+class dramTPool
+{
+  public:
+
+    /**
+     * @brief ctor
+     *
+     */
+    dramTPool()
+    {
+        iv_errList.clear();
+        iv_taskNum = 0;
+        mutex_init(&iv_mutex);
+        sync_cond_init(&iv_condvar);
+        iv_pool.start();
+    }
+
+    ~dramTPool()
+    {
+        iv_pool.shutdown();
+    }
+
+    /**
+     * @brief addErr():Adds the passed errorlog hndl to the errorlist.
+     *        Used by the task running on the thread to store the error
+     *
+     * @param[in] i_err error log to store
+     */
+
+    void addErr(errlHndl_t i_err)
+    {
+        mutex_lock(&iv_mutex);
+        if(i_err)
+        {
+            iv_errList.push_back(i_err);
+        }
+        mutex_lock(&iv_mutex);
+    }
+
+    /**
+     * @brief getErrors():Retrieve the list of errors.
+     *        Used by the master thread to 
+     *        get the errors generated from the dispatched task
+     *
+     * @param[inout] io_list Reference to list of err handles
+     */
+
+    void getErrors(std::list<errlHndl_t> & io_list)
+    {
+        io_list.clear();
+        io_list = iv_errList;
+        iv_errList.clear();
+    }
+
+    /**
+     * @brief dispatch(): Dispatches the task to a worker thread
+     *
+     * @param[in] i_task task to be dispatched on thread
+     */
+
+    void dispatch(draminitTask * i_task)
+    {
+        mutex_lock(&iv_mutex);
+        iv_taskNum++;
+        iv_pool.insert(i_task);
+        mutex_unlock(&iv_mutex);
+    }
+
+    /**
+     * @brief notifyDone
+     *        Notifies once the dispatched task is done.Outstanding
+     *        task list is updated and signals other threads waiting
+     *        on condition variable. Any error on running the thread
+     *        will be pushed to error list
+     *
+     * @param[in] i_task task currently active 
+     * @param[in] i_err any error while performing the task, 
+     */
+
+
+    void notifyDone(draminitTask & i_task, errlHndl_t i_err)
+    {
+        mutex_lock(&iv_mutex);
+
+        //If there is an error, then save it away
+        if(i_err)
+        {
+            iv_errList.push_back(i_err);
+        }
+
+        iv_taskNum--;
+        sync_cond_signal(&iv_condvar);
+
+
+        mutex_unlock(&iv_mutex);
+    }
+
+    /**
+     * @brief waitForCompletion
+     *        Waits for the completion of dispatched tasks
+     */
+
+    void waitForCompletion()
+    {
+        mutex_lock(&iv_mutex);
+        while(iv_taskNum)
+        {
+            sync_cond_wait(&iv_condvar, &iv_mutex);
+        }
+        mutex_unlock(&iv_mutex);
+    }
+
+  private:
+
+    mutex_t iv_mutex;
+    sync_cond_t iv_condvar;
+    size_t iv_taskNum;
+    errList iv_errList;
+    Util::ThreadPool<draminitTask>  iv_pool;
+
+    /**
+     * @brief copy disabled
+     */
+    dramTPool(dramTPool &);
+
+    /**
+     * @brief assignment disabled
+     */
+    dramTPool & operator=(const dramTPool &);
+};
+
+
+/**
+ * @brief draminit work item
+ */
+class draminitTask
+{
+  public:
+
+    /**
+     * @brief task function, called by threadpool
+     */
+    void operator()()
+    {
+        errlHndl_t l_err = NULL;
+
+        //Do we need to iterate over MBA targets
+        //This is necessary because the MBAs share a common CCS engine in HW
+        if (iv_passMBAs)
+        {
+            // Get this centaurs MBA targets
+            TARGETING::TargetHandleList l_mbaTargetList;
+            getChildChiplets(l_mbaTargetList, iv_target, TYPE_MBA);
+
+            for ( uint8_t l_mbaNum=0; l_mbaNum < l_mbaTargetList.size(); l_mbaNum++ )
+            {
+                //  make a local copy of the target for ease of use
+                const TARGETING::Target*  l_mba_target = l_mbaTargetList[l_mbaNum];
+
+                // Cast to a FAPI type of target.
+                const fapi::Target l_fapi_mba_target( TARGET_TYPE_MBA_CHIPLET,
+                                                      (const_cast<TARGETING::Target*>(l_mba_target)) );
+
+                // Dump current run on target
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                           "Thread on MBA target HUID %.8X", TARGETING::get_huid(l_mba_target));
+
+
+                FAPI_INVOKE_HWP(l_err, iv_func, l_fapi_mba_target);
+                if(l_err)
+                {
+                    ErrlUserDetailsTarget(l_mba_target).addToLog(l_err );
+                    iv_pool->addErr(l_err);
+                    l_err = NULL;
+                }
+            }
+        }
+        else
+        {
+            // Cast to a FAPI type of target.
+            const fapi::Target l_fapi_centaur( TARGET_TYPE_MEMBUF_CHIP,
+                                               (const_cast<TARGETING::Target*>(iv_target)));
+
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                       "Thread on target HUID %.8X", TARGETING::get_huid(iv_target));
+
+            FAPI_INVOKE_HWP(l_err, iv_func, l_fapi_centaur);
+
+            if(l_err)
+            {
+                // capture the target data in the elog
+                ErrlUserDetailsTarget(iv_target).addToLog(l_err );
+            }
+        }
+
+        iv_pool->notifyDone(*this, l_err);
+    }
+
+    /**
+     * @brief draminitTask
+     *
+     * draminit istep task creator that can be dispatched
+     * to worker threads from threadpool
+     *
+     * @param[in] i_pool wrapper to util threadpool
+     * @param[in] i_func fucntion pointer for the draminit task 
+     * @param[in] i_target target input for draminit task
+     * @param[in] i_targetMba Tells to parallelize per MBA (0/1)
+     */
+    draminitTask(dramTPool * i_pool, mss_FP_t i_func,
+                 const TARGETING::Target *  i_target, bool i_targetMba):
+      iv_pool(i_pool), iv_func(i_func), iv_target(i_target), iv_passMBAs(i_targetMba)
+    {};
+
+  private:
+
+    dramTPool * iv_pool;
+    mss_FP_t iv_func;
+    const TARGETING::Target * iv_target;   // Always a Membuf
+    bool iv_passMBAs;
+};
+
+
 
 //
 //  Wrapper function to call host_disable_vddr
@@ -151,21 +382,27 @@ void*    call_mem_pll_initf( void *io_pArgs )
                 "Running cen_mem_pll_initf HWP on "
                 "target HUID %.8X", TARGETING::get_huid(l_pCentaur));
 
-        // Cast to a FAPI type of target.
-        const fapi::Target l_fapi_centaur( TARGET_TYPE_MEMBUF_CHIP,
-                 (const_cast<TARGETING::Target*>(l_pCentaur)));
-
         //  call cen_mem_pll_initf to do pll init
-        FAPI_INVOKE_HWP(l_err, cen_mem_pll_initf, l_fapi_centaur);
+        draminitTask * l_task = new draminitTask(& Singleton<dramTPool>::instance(),
+                                               cen_mem_pll_initf, l_pCentaur, false);
+        Singleton<dramTPool>::instance().dispatch(l_task);
+        
+    }
 
+    //Wait for completion
+    Singleton<dramTPool>::instance().waitForCompletion();
+    errList l_errors;
+    Singleton<dramTPool>::instance().getErrors(l_errors);
+    for(errList::const_iterator errItr= l_errors.begin();
+        errItr != l_errors.end(); ++errItr)
+    {
+        l_err = *errItr;
+    
         if (l_err)
         {
             TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
                       "ERROR 0x%.8X: cen_mem_pll_initf HWP returns error",
                       l_err->reasonCode());
-
-            // capture the target data in the elog
-            ErrlUserDetailsTarget(l_pCentaur).addToLog(l_err );
 
             //Create IStep error log and cross reference to error that occurred
             l_StepError.addErrorDetails(l_err);
@@ -688,43 +925,43 @@ void*    call_mss_draminit_training( void *io_pArgs )
     TRACDCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
                         "call_mss_draminit_training entry" );
 
-    // Get all MBA targets
-    TARGETING::TargetHandleList l_mbaTargetList;
-    getAllChiplets(l_mbaTargetList, TYPE_MBA);
+    // Get all Centaur targets
+    TARGETING::TargetHandleList l_membufTargetList;
+    getAllChips(l_membufTargetList, TYPE_MEMBUF);
 
-    // Limit the number of MBAs to run in VPO environment to save time.
-    uint8_t l_mbaLimit = l_mbaTargetList.size();
-    if (TARGETING::is_vpo() && (VPO_NUM_OF_MBAS_TO_RUN < l_mbaLimit))
-    {
-        l_mbaLimit = VPO_NUM_OF_MBAS_TO_RUN;
-    }
-
-    for ( uint8_t l_mbaNum=0; l_mbaNum < l_mbaLimit; l_mbaNum++ )
+    for (TargetHandleList::const_iterator
+         l_membuf_iter = l_membufTargetList.begin();
+         l_membuf_iter != l_membufTargetList.end();
+         ++l_membuf_iter)
     {
         //  make a local copy of the target for ease of use
-        const TARGETING::Target*  l_mba_target = l_mbaTargetList[l_mbaNum];
+        const TARGETING::Target* l_pCentaur = *l_membuf_iter;
 
         // Dump current run on target
         TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                "Running mss_draminit_training HWP on "
-                "target HUID %.8X", TARGETING::get_huid(l_mba_target));
+                   "Running mss_draminit_training HWP on "
+                   "target HUID %.8X", TARGETING::get_huid(l_pCentaur));
 
-        // Cast to a FAPI type of target.
-        const fapi::Target l_fapi_mba_target( TARGET_TYPE_MBA_CHIPLET,
-                        (const_cast<TARGETING::Target*>(l_mba_target)) );
+        draminitTask * l_task = new draminitTask(& Singleton<dramTPool>::instance(),
+                                                 mss_draminit_training,
+                                                 l_pCentaur, true);
+        Singleton<dramTPool>::instance().dispatch(l_task);
+    }
 
-
-        //  call the HWP with each fapi::Target
-        FAPI_INVOKE_HWP(l_err, mss_draminit_training, l_fapi_mba_target);
+    //Wait for completion
+    Singleton<dramTPool>::instance().waitForCompletion();
+    errList l_errors;
+    Singleton<dramTPool>::instance().getErrors(l_errors);
+    for(errList::const_iterator errItr= l_errors.begin();
+        errItr != l_errors.end(); ++errItr)
+    {
+        l_err = *errItr;
 
         if (l_err)
         {
             TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
                     "ERROR 0x%.8X : mss_draminit_training HWP returns error",
                     l_err->reasonCode());
-
-            // capture the target data in the elog
-            ErrlUserDetailsTarget(l_mba_target).addToLog( l_err );
 
             // Create IStep error log and cross reference to error that occurred
             l_stepError.addErrorDetails( l_err );
@@ -757,43 +994,43 @@ void*    call_mss_draminit_trainadv( void *io_pArgs )
     TRACDCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
             "call_mss_draminit_trainadv entry" );
 
-    // Get all MBA targets
-    TARGETING::TargetHandleList l_mbaTargetList;
-    getAllChiplets(l_mbaTargetList, TYPE_MBA);
+    // Get all Centaur targets
+    TARGETING::TargetHandleList l_membufTargetList;
+    getAllChips(l_membufTargetList, TYPE_MEMBUF);
 
-    // Limit the number of MBAs to run in VPO environment to save time.
-    uint8_t l_mbaLimit = l_mbaTargetList.size();
-    if (TARGETING::is_vpo() && (VPO_NUM_OF_MBAS_TO_RUN < l_mbaLimit))
-    {
-        l_mbaLimit = VPO_NUM_OF_MBAS_TO_RUN;
-    }
-
-    for ( uint8_t l_mbaNum=0; l_mbaNum < l_mbaLimit; l_mbaNum++ )
+    for (TargetHandleList::const_iterator
+         l_membuf_iter = l_membufTargetList.begin();
+         l_membuf_iter != l_membufTargetList.end();
+         ++l_membuf_iter)
     {
         //  make a local copy of the target for ease of use
-        const TARGETING::Target*  l_mba_target = l_mbaTargetList[l_mbaNum];
+        const TARGETING::Target* l_pCentaur = *l_membuf_iter;
 
         // Dump current run on target
         TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                "Running mss_draminit_training_advanced HWP on "
-                "target HUID %.8X", TARGETING::get_huid(l_mba_target));
+                   "Running mss_draminit_training_advanced HWP on "
+                   "target HUID %.8X", TARGETING::get_huid(l_pCentaur));
 
-        // Cast to a FAPI type of target.
-        const fapi::Target l_fapi_mba_target( TARGET_TYPE_MBA_CHIPLET,
-                    (const_cast<TARGETING::Target*>(l_mba_target)) );
+        draminitTask * l_task = new draminitTask(& Singleton<dramTPool>::instance(),
+                                                 mss_draminit_training_advanced,
+                                                 l_pCentaur, true);
+        Singleton<dramTPool>::instance().dispatch(l_task);
+    }
 
-        //  call the HWP with each fapi::Target
-        FAPI_INVOKE_HWP(l_err, mss_draminit_training_advanced,
-                        l_fapi_mba_target);
+    //Wait for completion
+    Singleton<dramTPool>::instance().waitForCompletion();
+    errList l_errors;
+    Singleton<dramTPool>::instance().getErrors(l_errors);
+    for(errList::const_iterator errItr= l_errors.begin();
+        errItr != l_errors.end(); ++errItr)
+    {
+        l_err = *errItr;
 
         if (l_err)
         {
             TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
               "ERROR 0x%.8X : mss_draminit_training_advanced HWP returns error",
                 l_err->reasonCode());
-
-            // capture the target data in the elog
-            ErrlUserDetailsTarget(l_mba_target).addToLog( l_err );
 
             // Create IStep error log and cross reference to error that occurred
             l_stepError.addErrorDetails( l_err );
