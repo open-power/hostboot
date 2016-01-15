b1d81b724f5dbc9bf24074c722db743979bbde7c
Greg Still <stillgs@us.ibm.com>
2016-01-15 11:49:49 -0600

    p9_block_wakeup_intr Level 2
    - Fixed problems with Cronus to ungate testing
    - Ran on Awan and SUET
    - Wrapper has ability to just to -set and -clear but also
      tests all supported options
    - Added a "no special wakeup" option (parm) has there may
      be use cases with it as P8 did NOT perform Special Wake-up
      to set the bit while P9 Hcode is stating this requirement.
    - Has a placeholder for special wakeup inclusion later.
    - Address Gerrit comments (round 2)
    - Rebased (4)
    
    Change-Id: I0722065ce59a2c6ebfdc6c8cf77cb5746c3db7aa
    RTC: 136783
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/23347
    Tested-by: Jenkins Server
    Tested-by: Auto Mirror
    Tested-by: Hostboot CI
    Tested-by: PPE CI
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/24224
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/pm/p9_block_wakeup_intr.C
+++ src/import/chips/p9/procedures/hwp/pm/p9_block_wakeup_intr.C
@@ -17,14 +17,14 @@
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
 ///
-/// @file p8_block_wakeup_intr.C
+/// @file p9_block_wakeup_intr.C
 /// @brief Set/reset the BLOCK_REG_WKUP_SOURCES bit in the PCBS-PM associated
 ///          with an EX chiplet
 ///
 //  *HWP HWP Owner: Amit Kumar <akumar3@us.ibm.com>
 //  *HWP FW Owner: Bilicon Patil <bilpatil@in.ibm.com>
 //  *HWP Team: PM
-//  *HWP Level: 1
+//  *HWP Level: 2
 //  *HWP Consumed by: FSP:HS
 ///
 /// @verbatim
@@ -43,7 +43,7 @@
 // Includes
 // ----------------------------------------------------------------------
 
-#include "p9_block_wakeup_intr.H"
+#include <p9_block_wakeup_intr.H>
 
 
 
@@ -61,57 +61,107 @@ const char* OP_TYPE_STRING[] =
 
 /// @brief @brief Set/reset the BLOCK_INTR_INPUTS bit in the Core PPM
 ///         associated with an EX chiplet
-fapi2::ReturnCode p9_block_wakeup_intr(
+
+fapi2::ReturnCode
+p9_block_wakeup_intr(
     const fapi2::Target<fapi2::TARGET_TYPE_CORE>& i_core_target,
-    const p9pmblockwkup::OP_TYPE i_operation )
-
+    const p9pmblockwkup::OP_TYPE i_operation)
 {
-    FAPI_IMP("p9_block_wakeup_intr start");
+    FAPI_INF("> p9_block_wakeup_intr...");
 
-#if 0
-    ecmdDataBufferBase  data(64);
-
-    uint8_t             attr_chip_unit_pos = 0;
-
-    // CPMMR Bit definitions
-    const uint32_t      BLOCK_INTR_INPUTS = 11;
-
-    FAPI_DBG("Executing with operation %s to Core %s...",
-             p9pmblockwkup::P9_BLKWKUP_OP_STRING[i_operation],
-             i_core_target.toEcmdString());
-
+    fapi2::buffer<uint64_t> l_data64 = 0;
 
     // Get the core number
-    FAPI_TRY(FAPI_ATTR_GET( ATTR_CHIP_UNIT_POS,
-                            &i_core_target,
-                            attr_chip_unit_pos),
+    uint8_t l_attr_chip_unit_pos = 0;
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS,
+                           i_core_target,
+                           l_attr_chip_unit_pos),
              "fapiGetAttribute of ATTR_CHIP_UNIT_POS failed");
 
-    FAPI_DBG("Core number = %d", attr_chip_unit_pos);
-
-    if (i_operation == p9pmblockwkup::SET)
+    // Read for trace
     {
-        FAPI_INF("Setting Block Interrupt Sources...");
+        fapi2::buffer<uint64_t> l_cpmmr = 0;
+        fapi2::buffer<uint64_t> l_gpmmr = 0;
 
+        // Read the CPMMR and GPMMR as a trace
+        FAPI_TRY(fapi2::getScom(i_core_target,
+                                C_CPPM_CPMMR,
+                                l_cpmmr),
+                 "getScom of CPMMR failed");
 
-    }
-    else if (i_operation == p9pmblockwkup::CLEAR)
-    {
+        FAPI_TRY(fapi2::getScom(i_core_target,
+                                C_PPM_GPMMR,
+                                l_gpmmr),
+                 "getScom of GPMMR failed");
 
-        FAPI_INF("Clearing Block Interrupt Sources...");
-
-
-    }
-    else
-    {
-        FAPI_ASSERT(false,
-                    BLOCK_WAKEUP_INTR_OP()
-                    .set_OPERATION(i_operation),
-                    "Invalid operation specified.");
+        FAPI_DBG("Debug: before setting PPM_WRITE_OVERRIDE on Core %d - CPPMR: 0x%016llX GPMMR: 0x%016llX",
+                 l_attr_chip_unit_pos, l_cpmmr, l_gpmmr);
     }
 
+    // Ensure access to the GPMMR is in place using CPMMR Write Access
+    // Override.  This will not affect the CME functionality as only the
+    // Block Wake-up bit is being manipulated -- a bit that the CME does
+    // not control but does react upon.
 
-#endif
-    FAPI_INF("p9_block_wakeup_intr end");
+    FAPI_INF("Set the CPPM PPM Write Override");
+    l_data64.flush<0>().setBit<C_CPPM_CPMMR_PPM_WRITE_OVERRIDE>();
+    FAPI_TRY(fapi2::putScom(i_core_target,
+                            C_CPPM_CPMMR_OR,
+                            l_data64),
+             "putScom of CPMMR to set PMM Write Override failed");
+
+    l_data64.flush<0>().setBit<BLOCK_REG_WKUP_EVENTS>();
+
+    switch (i_operation)
+    {
+        case p9pmblockwkup::SET:
+
+            // @todo RTC 144905 Add Special Wakeup setting here when available
+
+            FAPI_INF("Setting GPMMR[Block Interrupt Sources] on Core %d",
+                     l_attr_chip_unit_pos);
+
+            FAPI_TRY(fapi2::putScom(i_core_target,
+                                    C_PPM_GPMMR_OR,
+                                    l_data64),
+                     "Setting GPMMR failed");
+
+            // @todo RTC 144905 Add Special Wakeup clearing here when available
+
+            break;
+
+        case p9pmblockwkup::SET_NOSPWUP:
+            FAPI_INF("Setting GPMMR[Block Interrupt Sources] without Special Wake-up on Core %d",
+                     l_attr_chip_unit_pos);
+
+            FAPI_TRY(fapi2::putScom(i_core_target,
+                                    C_PPM_GPMMR_OR,
+                                    l_data64),
+                     "Setting GPMMR failed");
+            break;
+
+        case p9pmblockwkup::CLEAR:
+            FAPI_INF("Clearing GPMMR[Block Interrupt Sources] on Core %d",
+                     l_attr_chip_unit_pos);
+
+            FAPI_TRY(fapi2::putScom(i_core_target,
+                                    C_PPM_GPMMR_CLEAR,
+                                    l_data64),
+                     "Clearing GPMMR failed");
+            break;
+
+        default:
+            ;
+    }
+
+    FAPI_INF("Clear the CPPM PPM Write Override");
+    l_data64.flush<0>().setBit<C_CPPM_CPMMR_PPM_WRITE_OVERRIDE>();
+    FAPI_TRY(fapi2::putScom(i_core_target,
+                            C_CPPM_CPMMR_CLEAR,
+                            l_data64),
+             "putScom of CPMMR to clear PMM Write Override failed");
+
+fapi_try_exit:
+    FAPI_INF("< p9_block_wakeup_intr...");
     return fapi2::current_err;
 }
