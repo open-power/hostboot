385f3a98bce1a5be8de70d7aea218605826a08e9
Prem Shanker Jha <premjha2@in.ibm.com>
2016-01-28 07:54:55 -0600

    PM: Added hcode image build support for SGPE and CME.
    In this commit, nested xip structure of hardware image is traversed and hcode
    for following platform is copied to to relevant area in HOMER:
        - SGPE
        - CME
        - P9 Self Restore code
    
    RTC: 145583
    Change-Id: I565ee2f5fe652ae92441c2076d22915f4771031d
    Original-Change-Id: I8f91e34aff81b4c9218be5a380e3336913cbd7c9
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/10675
    Tested-by: Jenkins Server
    Tested-by: Auto Mirror
    Reviewed-by: AMIT KUMAR <akumar3@us.ibm.com>
    Tested-by: Hostboot CI
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

--- src/import/chips/p9/procedures/utils/stopreg/p9_stop_api.mk
+++ src/import/chips/p9/procedures/utils/stopreg/p9_stop_api.mk
@@ -23,5 +23,8 @@
 #
 # IBM_PROLOG_END_TAG
 PROCEDURE=p9_stop_api
+STOP_UTIL=$(ROOTPATH)/chips/p9/procedures/utils/stopreg/
+STOP_UTIL+=$(ROOTPATH)/chips/p9/procedures/hwp/lib
 libp9_stop_util_DEPLIBS += p9_stop_util
+$(call ADD_MODULE_INCDIR,$(PROCEDURE),$(STOP_UTIL))
 $(call BUILD_PROCEDURE)
