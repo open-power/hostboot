a1ddf6f9224993cc9175afcbdb79674542e15304
Anusha Reddy Rangareddygari <anusrang@in.ibm.com>
2016-02-01 08:37:08 +0100

    Level 2 HWP for p9_set_fsi_gp_shadow
    Change-Id: I382f9a0088fafca0c8918299960049066c2e17f4
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/23825
    Tested-by: Jenkins Server
    Reviewed-by: Sunil Kumar <skumar8j@in.ibm.com>
    Reviewed-by: Soma Bhanutej <soma.bhanu@in.ibm.com>
    Reviewed-by: Parvathi Rachakonda
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/24803
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/perv/p9_set_fsi_gp_shadow.C
+++ src/import/chips/p9/procedures/hwp/perv/p9_set_fsi_gp_shadow.C
@@ -7,7 +7,7 @@
 /*                                                                        */
 /* EKB Project                                                            */
 /*                                                                        */
-/* COPYRIGHT 2015                                                         */
+/* COPYRIGHT 2015,2016                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -20,27 +20,86 @@
 /// @file  p9_set_fsi_gp_shadow.C
 ///
 /// @brief --IPL step 0.8 proc_prep_ipl
-// *!
-// *! OWNER NAME  : Abhishek Agarwal  Email: abagarw8@in.ibm.com
-// *! BACKUP NAME :                   Email:
 //------------------------------------------------------------------------------
-// *HWP HWP Owner   : Abhishek Agarwal <abagarw8@in.ibm.com>
-// *HWP FW Owner    : Brian Silver <bsilver@us.ibm.com>
-// *HWP Team        : Perv
-// *HWP Level       : 1
-// *HWP Consumed by : SBE
+// *HWP HW Owner        : Anusha Reddy Rangareddygari <anusrang@in.ibm.com>
+// *HWP HW Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner        : Brian Silver <bsilver@us.ibm.com>
+// *HWP Team            : Perv
+// *HWP Level           : 2
+// *HWP Consumed by     : SBE
 //------------------------------------------------------------------------------
 
 
 //## auto_generated
 #include "p9_set_fsi_gp_shadow.H"
+
+#include "p9_perv_scom_addresses.H"
+
+
 fapi2::ReturnCode p9_set_fsi_gp_shadow(const
                                        fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
 {
-    FAPI_DBG("p9_set_fsi_gp_shadow: Entering ...");
+    FAPI_DBG("Entering ...");
 
-    FAPI_DBG("p9_set_fsi_gp_shadow: Exiting ...");
+    FAPI_INF("Setting flush values for root_ctrl_copy and perv_ctrl_copy registers");
+    //Setting ROOT_CTRL0_COPY register value
+    //CFAM.ROOT_CTRL0_COPY = p9SetFsiGpShadow::ROOT_CTRL0_FLUSHVALUE
+    FAPI_TRY(fapi2::putCfamRegister(i_target_chip, PERV_ROOT_CTRL0_COPY_FSI,
+                                    p9SetFsiGpShadow::ROOT_CTRL0_FLUSHVALUE));
 
-    return fapi2::FAPI2_RC_SUCCESS;
+    //Setting ROOT_CTRL1_COPY register value
+    //CFAM.ROOT_CTRL1_COPY = p9SetFsiGpShadow::ROOT_CTRL1_FLUSHVALUE
+    FAPI_TRY(fapi2::putCfamRegister(i_target_chip, PERV_ROOT_CTRL1_COPY_FSI,
+                                    p9SetFsiGpShadow::ROOT_CTRL1_FLUSHVALUE));
+
+    //Setting ROOT_CTRL2_COPY register value
+    //CFAM.ROOT_CTRL2_COPY = p9SetFsiGpShadow::ROOT_CTRL2_FLUSHVALUE
+    FAPI_TRY(fapi2::putCfamRegister(i_target_chip, PERV_ROOT_CTRL2_COPY_FSI,
+                                    p9SetFsiGpShadow::ROOT_CTRL2_FLUSHVALUE));
+
+    //Setting ROOT_CTRL3_COPY register value
+    //CFAM.ROOT_CTRL3_COPY = p9SetFsiGpShadow::ROOT_CTRL3_FLUSHVALUE
+    FAPI_TRY(fapi2::putCfamRegister(i_target_chip, PERV_ROOT_CTRL3_COPY_FSI,
+                                    p9SetFsiGpShadow::ROOT_CTRL3_FLUSHVALUE));
+
+    //Setting ROOT_CTRL4_COPY register value
+    //CFAM.ROOT_CTRL4_COPY = p9SetFsiGpShadow::ROOT_CTRL4_FLUSHVALUE
+    FAPI_TRY(fapi2::putCfamRegister(i_target_chip, PERV_ROOT_CTRL4_COPY_FSI,
+                                    p9SetFsiGpShadow::ROOT_CTRL4_FLUSHVALUE));
+
+    //Setting ROOT_CTRL5_COPY register value
+    //CFAM.ROOT_CTRL5_COPY = p9SetFsiGpShadow::ROOT_CTRL5_FLUSHVALUE
+    FAPI_TRY(fapi2::putCfamRegister(i_target_chip, PERV_ROOT_CTRL5_COPY_FSI,
+                                    p9SetFsiGpShadow::ROOT_CTRL5_FLUSHVALUE));
+
+    //Setting ROOT_CTRL6_COPY register value
+    //CFAM.ROOT_CTRL6_COPY = p9SetFsiGpShadow::ROOT_CTRL6_FLUSHVALUE
+    FAPI_TRY(fapi2::putCfamRegister(i_target_chip, PERV_ROOT_CTRL6_COPY_FSI,
+                                    p9SetFsiGpShadow::ROOT_CTRL6_FLUSHVALUE));
+
+    //Setting ROOT_CTRL7_COPY register value
+    //CFAM.ROOT_CTRL7_COPY = p9SetFsiGpShadow::ROOT_CTRL7_FLUSHVALUE
+    FAPI_TRY(fapi2::putCfamRegister(i_target_chip, PERV_ROOT_CTRL7_COPY_FSI,
+                                    p9SetFsiGpShadow::ROOT_CTRL7_FLUSHVALUE));
+
+    //Setting ROOT_CTRL8_COPY register value
+    //CFAM.ROOT_CTRL8_COPY = p9SetFsiGpShadow::ROOT_CTRL8_FLUSHVALUE
+    FAPI_TRY(fapi2::putCfamRegister(i_target_chip, PERV_ROOT_CTRL8_COPY_FSI,
+                                    p9SetFsiGpShadow::ROOT_CTRL8_FLUSHVALUE));
+
+    //Setting PERV_CTRL0_COPY register value
+    //CFAM.PERV_CTRL0_COPY = p9SetFsiGpShadow::PERV_CTRL0_FLUSHVALUE
+    FAPI_TRY(fapi2::putCfamRegister(i_target_chip, PERV_PERV_CTRL0_COPY_FSI,
+                                    p9SetFsiGpShadow::PERV_CTRL0_FLUSHVALUE));
+
+    //Setting PERV_CTRL1_COPY register value
+    //CFAM.PERV_CTRL1_COPY = p9SetFsiGpShadow::PERV_CTRL1_FLUSHVALUE
+    FAPI_TRY(fapi2::putCfamRegister(i_target_chip, PERV_PERV_CTRL1_COPY_FSI,
+                                    p9SetFsiGpShadow::PERV_CTRL1_FLUSHVALUE));
+
+    FAPI_DBG("Exiting ...");
+
+fapi_try_exit:
+    return fapi2::current_err;
 
 }
