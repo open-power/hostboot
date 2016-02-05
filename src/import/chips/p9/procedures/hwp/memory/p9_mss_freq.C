cce682cad9acb491b53defecfc43cc334a8c72d3
Jacob Harvey <jlharvey@us.ibm.com>
2016-02-05 15:24:59 -0600

    Fixed doxygen errors and typos
    Change-Id: I5272f825041fb1ab8f001b4a4a5f646e6dd621d6
    Original-Change-Id: I94120c654c32b5c3513740ce8aae4b4cc632fc41
    Reviewed-on: http://gfw160.aus.stglabs.ibm.com:8080/gerrit/24281
    Tested-by: Jenkins Server
    Reviewed-by: Andre A. Marin <aamarin@us.ibm.com>
    Reviewed-by: Brian Silver <bsilver@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/p9_mss_freq.C
+++ src/import/chips/p9/procedures/hwp/memory/p9_mss_freq.C
@@ -42,7 +42,7 @@ using fapi2::FAPI2_RC_SUCCESS;
 
 ///
 /// @brief Calculate and save off DIMM frequencies
-/// @param[in] i_target, the controller (e.g., MCS)
+/// @param[in] i_target the controller (e.g., MCS)
 /// @return FAPI2_RC_SUCCESS iff ok
 ///
 fapi2::ReturnCode p9_mss_freq( const fapi2::Target<TARGET_TYPE_MCS>& i_target )
