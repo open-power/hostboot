69da673c4fab456adc59fa9bba095dac119efc4f
Greg Still <stillgs@us.ibm.com>
2016-02-08 06:53:08 -0600

    Level 2 p9_cpu_special_wakeup
    - Restructured to deal with individual core, EQ and EQ targets
         This is necessary as compound targets really can't apply to this procedure
         as the registers being addressed for an EX target changes based on Fused
         mode.
    - The above moved the main implemented into p9_cpu_special_wakeup_lib
    - Removed tracking attributes
    - Removed FUSED CORE support
    - EX targets will wake-up up to 2 cores within the EX (dependent on
      functionality)
    - Added SUET support
    - Rebased (2)
    - Made HWP attributes writable
    - Added HB platform defaults in hb_temp_defaults.xml (but doesn't compile)
    - Address Gerrit comments (included Cronus noticed bug)
    - Removed some hb_temp attributes to deal with HB Jenkins fails
    
    Change-Id: Id9e739dc18f4984dd0bd1dc01814e0a36a42c6a9
    Original-Change-Id: I84a66b646477627f0178c3c968580ffc435b5300
    RTC: 136795
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/20850
    Dev-Ready: Matt K. Light <mklight@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: Matt K. Light <mklight@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/pm/p9_cpu_special_wakeup_lib.C
@@ -0,0 +1,60 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/pm/p9_cpu_special_wakeup_lib.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include <p9_cpu_special_wakeup_lib.H>
+#include <stdint.h>
+
+namespace p9specialWakeup
+{
+/**
+ * String names for the Entities.  These must match the PROC_SPCWKUP_ENTITY
+ * enum.
+ */
+
+/**
+ * Strings associated with special wake up.
+ */
+const char* SPWK_MSG_LIST[] =
+{
+    "Assert: ",
+    "Deassert",
+    "Clean-up Deassert"
+};
+
+/**
+ * String names for the Entities.  These must match the PROC_SPCWKUP_ENTITY
+ * enum.
+ */
+
+const char* PROC_SPCWKUP_ENTITY_NAMES[] =
+{
+    "OTHER",
+    "FSP",
+    "OCC",
+    "HYP",
+    "HOST",
+    "SPW_ALL"
+};
+
+}
