05039d6225dc91f37643b3ac1fbdb5c03565efff
Andre Marin <aamarin@us.ibm.com>
2016-02-09 13:16:51 -0600

    Modify spd_decoder, eff_config, unit tests. Modify dependent files
    Change-Id: I6fe77b653d6875ec8d207975d8d8000a95db235b
    Original-Change-Id: Ifa4fbf9d452a3e77075bee9ab72b2bde2afe90a5
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/20861
    Tested-by: Jenkins Server
    Tested-by: Auto Mirror
    Reviewed-by: Brian R. Silver <bsilver@us.ibm.com>
    Tested-by: Hostboot CI
    Reviewed-by: CRAIG C. HAMILTON <cchamilt@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/p9_mss_eff_config.C
+++ src/import/chips/p9/procedures/hwp/memory/p9_mss_eff_config.C
@@ -39,7 +39,7 @@
 
 ///
 /// @brief Configure the attributes for each controller
-/// @param[in] i_target, the controller (e.g., MCS)
+/// @param[in] i_target the controller (e.g., MCS)
 /// @return FAPI2_RC_SUCCESS iff ok
 ///
 fapi2::ReturnCode p9_mss_eff_config( const fapi2::Target<fapi2::TARGET_TYPE_MCS>& i_target )
