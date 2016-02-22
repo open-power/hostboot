c82cf18ae1ce7568c9ea1d3b401542f94f86a630
Gou Peng Fei <shgoupf@cn.ibm.com>
2016-02-22 00:33:51 -0500

    PCIE phase1/phase2 initialization procedure.
    Changes included:
    1) p9_pcie_scominit for phase1.
    2) p9_pcie_config for phase2.
    3) Add attribute values for phase1.
    
    Change-Id: Id9afc0edaa716b0fdc22ac9035ad4b8ebfecb38f
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/20885
    Tested-by: Jenkins Server
    Tested-by: Hostboot CI
    Tested-by: PPE CI
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/20841
    Tested-by: FSP CI Jenkins
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/nest/p9_pcie_config.C
+++ src/import/chips/p9/procedures/hwp/nest/p9_pcie_config.C
@@ -7,7 +7,7 @@
 /*                                                                        */
 /* EKB Project                                                            */
 /*                                                                        */
-/* COPYRIGHT 2015                                                         */
+/* COPYRIGHT 2015,2016                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -25,7 +25,7 @@
 // *HWP HWP Owner: Christina Graves clgraves@us.ibm.com
 // *HWP FW Owner: Thi Tran thi@us.ibm.com
 // *HWP Team: Nest
-// *HWP Level: 1
+// *HWP Level: 2
 // *HWP Consumed by: HB
 
 //-----------------------------------------------------------------------------------
@@ -33,6 +33,8 @@
 //-----------------------------------------------------------------------------------
 #include <p9_pcie_config.H>
 
+#include "p9_misc_scom_addresses.H"
+#include "p9_misc_scom_addresses_fld.H"
 
 //-----------------------------------------------------------------------------------
 // Function definitions
@@ -40,6 +42,254 @@
 fapi2::ReturnCode p9_pcie_config(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
 {
     FAPI_INF("Start");
+    // Four BARs
+    const uint8_t NUM_BAR = 4;
+
+    uint64_t l_bar_address[NUM_BAR] = {0};
+    uint64_t l_bar_mask[NUM_BAR] = {0};
+    uint8_t l_bar_enable[NUM_BAR] = {0};
+
+    fapi2::buffer<uint64_t> l_buf = 0;
+    auto l_pec_chiplets_vec = i_target.getChildren<fapi2::TARGET_TYPE_PEC>(fapi2::TARGET_STATE_FUNCTIONAL);
+    auto l_phb_chiplets_vec = i_target.getChildren<fapi2::TARGET_TYPE_PHB>(fapi2::TARGET_STATE_FUNCTIONAL);
+    FAPI_DBG("pec target vec size: %#x\n", l_pec_chiplets_vec.size());
+    FAPI_DBG("phb target vec size: %#x\n", l_phb_chiplets_vec.size());
+    unsigned char l_pec_id = 0;
+    unsigned char l_phb_id = 0;
+
+    for (auto l_pec_chiplets : l_pec_chiplets_vec)
+    {
+        // Get the pec id
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, l_pec_chiplets,
+                               l_pec_id));
+
+        // Phase2 init step 1
+        // NestBase+0x00
+        // Set bits 00:03 = 0b0001Set hang poll scale
+        // Set bits             04:07 = 0b0010 Set data scale
+        // Set bits 08:11 = 0b0001 Set hang pe scale
+        // Set bit 22 = 0b1 Disable out-of-order store behavior
+        FAPI_TRY(fapi2::getScom(l_pec_chiplets, PEC_PBCQHWCFG_REG, l_buf));
+        l_buf.insertFromRight<PEC_PBCQHWCFG_REG_HANG_POLL_SCALE, PEC_PBCQHWCFG_REG_HANG_POLL_SCALE_LEN>(0x1);
+        l_buf.insertFromRight<PEC_PBCQHWCFG_REG_HANG_DATA_SCALE, PEC_PBCQHWCFG_REG_HANG_DATA_SCALE_LEN>(0x2);
+        l_buf.insertFromRight<PEC_PBCQHWCFG_REG_HANG_PE_SCALE, PEC_PBCQHWCFG_REG_HANG_PE_SCALE_LEN>(0x1);
+        l_buf.insertFromRight<PEC_PBCQHWCFG_REG_PE_DISABLE_OOO_MODE, 1>(0x1);
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_PBCQHWCFG_REG, l_buf));
+
+        // Phase2 init step 2
+        // NestBase+0x01
+        // N/A Modify Drop Priority Control Register (DrPriCtl)
+
+        // Phase2 init step 3
+        // NestBase+0x03
+        // Set bits 00:03 = 0b1001 Enable trace, and select
+        // Inbound operations with addr information
+        FAPI_TRY(fapi2::getScom(l_pec_chiplets, PEC_NESTTRC_REG, l_buf));
+        // TODO: no register bit field defined.
+        l_buf.insertFromRight<0, 4>(0x9);
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_NESTTRC_REG, l_buf));
+
+        // Phase2 init step 4
+        // NestBase+0x05
+        // N/A For use of atomics/asb_notify
+
+        // Phase2 init step 5
+        // NestBase+0x06
+        // N/A To override scope prediction
+
+        // Phase2 init step 6
+        // PCIBase +0x00
+        // Set bits 30 = 0b1 Enable Trace
+        l_buf = 0;
+        l_buf.setBit<PEC_PBAIBHWCFG_REG_PE_PCIE_CLK_TRACE_EN>();
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_PBAIBHWCFG_REG, l_buf));
+    }
+
+    for (auto l_phb_chiplets : l_phb_chiplets_vec)
+    {
+        // Get the pec id
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, l_phb_chiplets,
+                               l_phb_id));
+        // Phase2 init step 7_a
+        // NestBase+StackBase+0xA
+        // 0x00000000_00000000
+        // Clear any spurious cerr_rpt0 bits (cerr_rpt0)
+        // TODO: HW363246 this step appears in the v1.0 doc but failed PCB address
+        // error in model e9034
+        //l_buf = (uint64_t)0x0;
+        //FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        //FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_CERR_RPT0_REG, l_buf));
+
+        // Phase2 init step 7_b
+        // NestBase+StackBase+0xB
+        // 0x00000000_00000000
+        // Clear any spurious cerr_rpt1 bits (cerr_rpt1)
+        // TODO: HW363246 this step appears in the v1.0 doc but failed PCB address
+        // error in model e9034
+        //l_buf = (uint64_t)0x0;
+        //FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        //FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_CERR_RPT1_REG, l_buf));
+
+        // Phase2 init step 7_c
+        // NestBase+StackBase+0x0
+        // 0x00000000_00000000
+        // Clear any spurious FIR
+        // bits (NFIR)NFIR
+        l_buf = (uint64_t)0x0;
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_NFIR_REG, l_buf));
+
+        // Phase2 init step 8
+        // NestBase+StackBase+0x8
+        // 0x00000000_00000000
+        // Clear any spurious WOF
+        // bits (NFIRWOF)
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_NFIRWOF_REG, l_buf));
+
+        // Phase2 init step 9
+        // NestBase+StackBase+0x6
+        // 0x5B0F8190_00000000
+        // Set the per FIR Bit Action 0 register
+        l_buf = 0x5B0F819000000000;
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_NFIRACTION0_REG, l_buf));
+
+        // Phase2 init step 10
+        // NestBase+StackBase+0x7
+        // 0x7F0F8190_00000000
+        // Set the per FIR Bit Action 1 register
+        l_buf = 0x7F0F819000000000;
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_NFIRACTION1_REG, l_buf));
+
+        // Phase2 init step 11
+        // NestBase+StackBase+0x3
+        // 0x00000000_00000000
+        // Set FIR Mask Bits to allow errors (NFIRMask)
+        l_buf = 0x0000000000000000;
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_NFIRMASK_REG, l_buf));
+
+        // Phase2 init step 12
+        // NestBase+StackBase+0x15
+        // 0x00000000_00000000
+        // Set Data Freeze Type Register for SUE handling (DFREEZE)
+        l_buf = 0x0000000000000000;
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_PE_DFREEZE_REG, l_buf));
+
+        // Phase2 init step 13_a
+        // PCIBase+StackBase+0xB
+        // 0x00000000_00000000
+        // Clear any spurious pbaib_cerr_rpt bits
+        l_buf = (uint64_t)0x0;
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_PFIR_REG, l_buf));
+
+        // Phase2 init step 13_b
+        // PCIBase+StackBase+0x0
+        // 0x00000000_00000000
+        // Clear any spurious FIR
+        // bits (PFIR)PFIR
+        l_buf = (uint64_t)0x0;
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_PBAIB_CERR_RPT_REG, l_buf));
+
+        // Phase2 init step 14
+        // PCIBase+StackBase+0x8
+        // 0x00000000_00000000
+        // Clear any spurious WOF
+        // bits (PFIRWOF)
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_PFIRWOF_REG, l_buf));
+
+        // Phase2 init step 15
+        // PCIBase+StackBase+0x6
+        // 0x5B0F8190_00000000
+        // Set the per FIR Bit Action 0 register
+        l_buf = 0x5B0F819000000000;
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_PFIRACTION0_REG, l_buf));
+
+        // Phase2 init step 16
+        // PCIBase+StackBase+0x7
+        // 0x7F0F8190_00000000
+        // Set the per FIR Bit Action 1 register
+        l_buf = 0x7F0F819000000000;
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_PFIRACTION1_REG, l_buf));
+
+        // Phase2 init step 17
+        // PCIBase+StackBase+0x3
+        // 0x00000000_00000000
+        // Set FIR Mask Bits to allow errors (PFIRMask)
+        l_buf = 0x0000000000000000;
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_PFIRMASK_REG, l_buf));
+
+        // Get the attribute for BAR address and size.
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_BAR_BASE_ADDR, l_phb_chiplets,
+                               l_bar_address));
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_BAR_SIZE, l_phb_chiplets,
+                               l_bar_mask));
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_BAR_ENABLE, l_phb_chiplets,
+                               l_bar_enable));
+
+        // step 18: NestBase+StackBase+0xE<software programmed>Set MMIO Base
+        // Address Register 0 (MMIOBAR0)
+        FAPI_DBG("phb%i bar0 addr: %#lx", l_phb_id, l_bar_address[0]);
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_MMIOBAR0_REG, l_bar_address[0]));
+
+        // step 19: NestBase+StackBase+0xF<software programmed>Set MMIO BASE
+        // Address Register Mask 0 (MMIOBAR0_MASK)
+        FAPI_DBG("phb%i bar0 mask: %#lx", l_phb_id, l_bar_mask[0]);
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_MMIOBAR0_MASK_REG, l_bar_mask[0]));
+
+        // step 20: NestBase+StackBase+0x10<software programmed>Set MMIO Base
+        // Address Register 1 (MMIOBAR1)
+        FAPI_DBG("phb%i bar1 addr: %#lx", l_phb_id, l_bar_address[1]);
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_MMIOBAR1_REG, l_bar_address[1]));
+
+        // step 21: NestBase+StackBase+0x11<software programmed>Set MMIO Base
+        // Address Register Mask 1 (MMIOBAR1_MASK)
+        FAPI_DBG("phb%i bar1 mask: %#lx", l_phb_id, l_bar_mask[1]);
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_MMIOBAR1_MASK_REG, l_bar_mask[1]));
+
+        // step 22: NestBase+StackBase+0x12<software programmed>Set PHB
+        // Regsiter Base address Register (PHBBAR)
+        FAPI_DBG("phb%i phb addr: %#lx", l_phb_id, l_bar_address[2]);
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_PHBBAR_REG, l_bar_address[2]));
+
+        // step 23: NestBase+StackBase+0x13<software programmed>Set Interrupt
+        // BASEase Address Register (INTBAR)
+        FAPI_DBG("phb%i int addr: %#lx", l_phb_id, l_bar_address[3]);
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_INTBAR_REG, l_bar_address[3]));
+
+        // step 24: NestBase+StackBase+0x14<software programmed>Set Base
+        // addressress Enable Register (BARE)
+        l_buf = (uint64_t)0x0;
+        l_buf.insertFromRight<PHB_BARE_REG_PE_MMIO_BAR0_EN, 1>(l_bar_enable[0]); // bit 0 for BAR0
+        l_buf.insertFromRight<PHB_BARE_REG_PE_MMIO_BAR1_EN, 1>(l_bar_enable[1]); // bit 1 for BAR1
+        l_buf.insertFromRight<PHB_BARE_REG_PE_PHB_BAR_EN, 1>(l_bar_enable[2]); // bit 2 for PHB
+        l_buf.insertFromRight<PHB_BARE_REG_PE_INT_BAR_EN, 1>(l_bar_enable[3]); // bit 3 for INT
+        FAPI_DBG("phb%i bar enable: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_BARE_REG, l_buf));
+
+        // Phase2 init step 25
+        // PCIBase+StackBase +0x0A
+        // 0x00000000_00000000
+        // Remove ETU/AIB bus from reset (PHBReset)
+        l_buf = (uint64_t)0x0;
+        FAPI_DBG("phb%i: %#lx", l_phb_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_phb_chiplets, PHB_PHBRESET_REG, l_buf));
+    }
+
     FAPI_INF("End");
+
+fapi_try_exit:
     return fapi2::current_err;
 }
--- src/import/chips/p9/procedures/hwp/nest/p9_pcie_scominit.C
+++ src/import/chips/p9/procedures/hwp/nest/p9_pcie_scominit.C
@@ -7,7 +7,7 @@
 /*                                                                        */
 /* EKB Project                                                            */
 /*                                                                        */
-/* COPYRIGHT 2015                                                         */
+/* COPYRIGHT 2015,2016                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -24,7 +24,7 @@
 // *HWP HWP Owner: Christina Graves clgraves@us.ibm.com
 // *HWP FW Owner: Thi Tran thi@us.ibm.com
 // *HWP Team: Nest
-// *HWP Level: 1
+// *HWP Level: 2
 // *HWP Consumed by: HB
 
 //-----------------------------------------------------------------------------------
@@ -32,6 +32,53 @@
 //-----------------------------------------------------------------------------------
 #include <p9_pcie_scominit.H>
 
+#include <p9_misc_scom_addresses.H>
+#include <p9_misc_scom_addresses_fixes.H>
+#include <p9_misc_scom_addresses_fld.H>
+
+/// @brief This function configures a buffer with respect to different pec id
+///
+/// @param[in]         in_target The target
+/// @param[in/out]     io_buf  The buffer
+/// @param[in]         in_pec_id The PEC id
+/// @param[in]         in_attr The attribute value to be set
+/// @param[in]         in_pec_id The PEC id
+/// @param[in]         in_pec[0-2]_s The start bit for pec[0-2]
+/// @param[in]         in_pec[0-2]_c The bit count for pec[0-2]
+//
+/// @return  FAPI2_RC_SUCCESS if success, else error code.
+
+fapi2::ReturnCode set_buf(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& in_target,
+                          fapi2::buffer<uint64_t>& io_buf,
+                          const int in_pec_id, const uint8_t in_attr,
+                          const uint32_t in_pec0_s, const uint32_t in_pec0_c,
+                          const uint32_t in_pec1_s, const uint32_t in_pec1_c,
+                          const uint32_t in_pec2_s, const uint32_t in_pec2_c)
+{
+    switch(in_pec_id)
+    {
+        case 0:
+            FAPI_TRY(io_buf.insertFromRight(in_attr, in_pec0_s, in_pec0_c));
+            break;
+
+        case 1:
+            FAPI_TRY(io_buf.insertFromRight(in_attr, in_pec1_s, in_pec1_c));
+            break;
+
+        case 2:
+            FAPI_TRY(io_buf.insertFromRight(in_attr, in_pec2_s, in_pec2_c));
+            break;
+
+        default:
+            FAPI_ASSERT(false,
+                        fapi2::P9_PCIE_SCOMINIT_PECID_ERROR().set_TARGET(in_target),
+                        "Unknown PEC ID: %i!", in_pec_id);
+            break;
+    }
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
 
 //-----------------------------------------------------------------------------------
 // Function definitions
@@ -39,6 +86,239 @@
 fapi2::ReturnCode p9_pcie_scominit(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
 {
     FAPI_INF("Start");
+    uint8_t l_attr_proc_pcie_iop_config = 0;
+    uint8_t l_attr_proc_pcie_iop_swap = 0;
+    uint8_t l_attr_proc_pcie_iovalid_enable = 0;
+    fapi2::buffer<uint64_t> l_buf = 0;
+    unsigned char l_pec_id = 0;
+    auto l_pec_chiplets_vec = i_target.getChildren<fapi2::TARGET_TYPE_PEC>();
+    uint32_t l_pcs_config_mode[NUM_PCS_CONFIG] = {PCS_CONFIG_MODE0, PCS_CONFIG_MODE1, PCS_CONFIG_MODE2, PCS_CONFIG_MODE3};
+    uint8_t l_pcs_cdr_gain[NUM_PCS_CONFIG] = {0};
+    uint16_t l_pcs_loff_control[NUM_PCS_CONFIG] = {0};
+    uint16_t l_pcs_vga_control_register3[NUM_PCS_CONFIG] = {0};
+    uint16_t l_pcs_m_cntl[NUM_M_CONFIG] = {0};
+    uint8_t l_pcs_rot_cntl_cdr_lookahead = 0;
+    uint8_t l_pcs_rot_cntl_cdr_ssc = 0;
+    uint8_t l_attr_8 = 0;
+    uint16_t l_attr_16 = 0;
+
+    FAPI_DBG("target vec size: %#x", l_pec_chiplets_vec.size());
+    FAPI_DBG("l_buf: %#x", l_buf());
+
+    for (auto l_pec_chiplets : l_pec_chiplets_vec)
+    {
+        // Get the pec id
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, l_pec_chiplets,
+                               l_pec_id));
+
+        // Phase1 init step 1 (get VPD, no operation here)
+
+        // Phase1 init step 2a
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_IOP_CONFIG, l_pec_chiplets,
+                               l_attr_proc_pcie_iop_config));
+        FAPI_DBG("attr_proc_pcie_iop_config: %#x", l_attr_proc_pcie_iop_config);
+        l_buf = 0;
+        FAPI_TRY(set_buf(i_target, l_buf, l_pec_id, l_attr_proc_pcie_iop_config,
+                         PEC0_IOP_CONFIG_START_BIT, PEC0_IOP_BIT_COUNT * 2,
+                         PEC1_IOP_CONFIG_START_BIT, PEC1_IOP_BIT_COUNT * 2,
+                         PEC2_IOP_CONFIG_START_BIT, PEC2_IOP_BIT_COUNT * 2));
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_CPLT_CONF1_OR, l_buf));
+
+        // Phase1 init step 2b
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_IOP_SWAP, l_pec_chiplets,
+                               l_attr_proc_pcie_iop_swap));
+        FAPI_DBG("attr_proc_pcie_iop_swap: %#x", l_attr_proc_pcie_iop_swap);
+        l_buf = 0;
+        FAPI_TRY(set_buf(i_target, l_buf, l_pec_id, l_attr_proc_pcie_iop_swap,
+                         PEC0_IOP_SWAP_START_BIT, PEC0_IOP_BIT_COUNT,
+                         PEC1_IOP_SWAP_START_BIT, PEC1_IOP_BIT_COUNT,
+                         PEC2_IOP_SWAP_START_BIT, PEC2_IOP_BIT_COUNT));
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_CPLT_CONF1_OR, l_buf));
+
+        // Phase1 init step 3a
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_IOVALID_ENABLE, l_pec_chiplets,
+                               l_attr_proc_pcie_iovalid_enable));
+        FAPI_DBG("l_attr_proc_pcie_iovalid_enable: %#x", l_attr_proc_pcie_iovalid_enable);
+        l_buf = 0;
+        FAPI_TRY(set_buf(i_target, l_buf, l_pec_id, l_attr_proc_pcie_iovalid_enable,
+                         PEC0_IOP_IOVALID_ENABLE_START_BIT, PEC0_IOP_BIT_COUNT,
+                         PEC1_IOP_IOVALID_ENABLE_START_BIT, PEC1_IOP_BIT_COUNT,
+                         PEC2_IOP_IOVALID_ENABLE_START_BIT, PEC2_IOP_BIT_COUNT));
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_CPLT_CONF1_OR, l_buf));
+
+        // Phase1 init step 3b (enable clock)
+        l_buf = 0;
+        FAPI_TRY(l_buf.insertFromRight(1, PEC_CPLT_CTRL0_TC_REFCLK_DRVR_EN_DC, 1));
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_CPLT_CTRL0_OR, l_buf));
+
+        // Phase1 init step 4 (PMA reset)
+        l_buf = ~0;
+        FAPI_TRY(l_buf.insertFromRight(0, PEC_IOP_PMA_RESET_START_BIT, 1));
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_CPLT_CONF1_CLEAR, l_buf));
+
+        // Phase1 init step 5 (Set FIR action0)
+        l_buf = 0;
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_FIR_ACTION0_REG, l_buf));
+
+        // Phase1 init step 6 (Set FIR action1)
+        l_buf = 0xFFFFFFFFF8000000;
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_FIR_ACTION1_REG, l_buf));
+
+        // Phase1 init step 7 (Set FIR mask)
+        l_buf = 0xFFFFFFFFF8000000;
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_FIR_MASK_REG, l_buf));
+
+        // Phase1 init step 8-11 (Config 0 - 3)
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_PCS_RX_CDR_GAIN, l_pec_chiplets,
+                               l_pcs_cdr_gain));
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_PCS_RX_LOFF_CONTROL, l_pec_chiplets,
+                               l_pcs_loff_control));
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_PCS_RX_VGA_CONTRL_REGISTER3, l_pec_chiplets,
+                               l_pcs_vga_control_register3));
+
+        for (int i = 0; i < NUM_PCS_CONFIG; i++)
+        {
+            // RX Config Mode
+            l_buf = 0;
+            FAPI_TRY(l_buf.insertFromRight(l_pcs_config_mode[i], 48, 16));
+            FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+            FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_PCS_RX_CONFIG_MODE_REG, l_buf));
+
+            // RX CDR GAIN
+            FAPI_TRY(fapi2::getScom(l_pec_chiplets, PEC_PCS_RX_CDR_GAIN_REG, l_buf));
+            FAPI_TRY(l_buf.insertFromRight(l_pcs_cdr_gain[i], 56, 8));
+            FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+            FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_PCS_RX_CDR_GAIN_REG, l_buf));
+
+            // RX LOFF CONTROL
+            l_buf = 0;
+            FAPI_TRY(l_buf.insertFromRight(l_pcs_loff_control[i], 48, 16));
+            FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+            FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_PCS_RX_LOFF_CONTROL_REG, l_buf));
+
+            // RX VGA CONTROL REGISTER3
+            l_buf = 0;
+            FAPI_TRY(l_buf.insertFromRight(l_pcs_vga_control_register3[i], 48, 16));
+            FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+            FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_PCS_RX_VGA_CONTROL3_REG, l_buf));
+        }
+
+        // Phase1 init step 12 (RX Rot Cntl CDR Lookahead Disabled,SSC Disabled)
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_PCS_RX_ROT_CDR_LOOKAHEAD, l_pec_chiplets,
+                               l_pcs_rot_cntl_cdr_lookahead));
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_PCS_RX_ROT_CDR_SSC, l_pec_chiplets,
+                               l_pcs_rot_cntl_cdr_ssc));
+        FAPI_TRY(fapi2::getScom(l_pec_chiplets, PEC_PCS_RX_ROT_CNTL_REG, l_buf));
+        FAPI_TRY(l_buf.insertFromRight(l_pcs_rot_cntl_cdr_lookahead, 55, 1));
+        FAPI_TRY(l_buf.insertFromRight(l_pcs_rot_cntl_cdr_ssc, 63, 1));
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_PCS_RX_ROT_CNTL_REG, l_buf));
+
+        // Phase1 init step 13 (RX Config Mode Enable External Config Control)
+        l_buf = 0;
+        FAPI_TRY(l_buf.insertFromRight(0x8600, 48, 16));
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_PCS_RX_CONFIG_MODE_REG, l_buf));
+
+        // Phase1 init step 14 (PCLCK Control Register - PLLA)
+        SET_REG_RMW_WITH_SINGLE_ATTR_8(fapi2::ATTR_PROC_PCIE_PCS_PCLCK_CNTL_PLLA,
+                                       PEC_PCS_PCLCK_CNTL_PLLA_REG,
+                                       56, 8);
+
+        // Phase1 init step 15 (PCLCK Control Register - PLLB)
+        SET_REG_RMW_WITH_SINGLE_ATTR_8(fapi2::ATTR_PROC_PCIE_PCS_PCLCK_CNTL_PLLB,
+                                       PEC_PCS_PCLCK_CNTL_PLLB_REG,
+                                       56, 8);
+
+        // Phase1 init step 16 (TX DCLCK Rotator Override)
+        SET_REG_WR_WITH_SINGLE_ATTR_16(fapi2::ATTR_PROC_PCIE_PCS_TX_DCLCK_ROT,
+                                       PEC_PCS_TX_DCLCK_ROTATOR_REG,
+                                       48, 16);
+
+        // Phase1 init step 17 (TX FIFO Config Offset)
+        SET_REG_RMW_WITH_SINGLE_ATTR_8(fapi2::ATTR_PROC_PCIE_PCS_TX_FIFO_CONFIG_OFFSET,
+                                       PEC_PCS_TX_FIFO_CONFIG_OFFSET_REG,
+                                       59, 5);
+
+        // Phase1 init step 18 (TX PCIe Receiver Detect Control Register 1)
+        SET_REG_WR_WITH_SINGLE_ATTR_16(fapi2::ATTR_PROC_PCIE_PCS_TX_PCIE_RECV_DETECT_CNTL_REG1,
+                                       PEC_PCS_TX_PCIE_REC_DETECT_CNTL1_REG,
+                                       48, 16);
+
+        // Phase1 init step 19 (TX PCIe Receiver Detect Control Register 2)
+        SET_REG_WR_WITH_SINGLE_ATTR_16(fapi2::ATTR_PROC_PCIE_PCS_TX_PCIE_RECV_DETECT_CNTL_REG2,
+                                       PEC_PCS_TX_PCIE_REC_DETECT_CNTL2_REG,
+                                       48, 16);
+
+        // Phase1 init step 20 (TX Power Sequence Enable)
+        SET_REG_RMW_WITH_SINGLE_ATTR_8(fapi2::ATTR_PROC_PCIE_PCS_TX_POWER_SEQ_ENABLE,
+                                       PEC_PCS_TX_POWER_SEQ_ENABLE_REG,
+                                       56, 7);
+
+        // Phase1 init step 21 (RX Phase Rotator Control)
+        SET_REG_WR_WITH_SINGLE_ATTR_16(fapi2::ATTR_PROC_PCIE_PCS_RX_PHASE_ROTATOR_CNTL,
+                                       PEC_PCS_RX_ROT_CNTL_REG,
+                                       48, 16);
+
+        // Phase1 init step 22 (RX VGA Control Register 1)
+        SET_REG_WR_WITH_SINGLE_ATTR_16(fapi2::ATTR_PROC_PCIE_PCS_RX_VGA_CNTL_REG1,
+                                       PEC_PCS_RX_VGA_CONTROL1_REG,
+                                       48, 16);
+
+        // Phase1 init step 23 (RX VGA Control Register 2)
+        SET_REG_WR_WITH_SINGLE_ATTR_16(fapi2::ATTR_PROC_PCIE_PCS_RX_VGA_CNTL_REG2,
+                                       PEC_PCS_RX_VGA_CONTROL2_REG,
+                                       48, 16);
+
+        // Phase1 init step 24 (RX SIGDET Control)
+        SET_REG_WR_WITH_SINGLE_ATTR_16(fapi2::ATTR_PROC_PCIE_PCS_RX_SIGDET_CNTL,
+                                       PEC_PCS_RX_SIGDET_CONTROL_REG,
+                                       48, 16);
+        // Phase1 init step 25
+        l_buf = ~0;
+        FAPI_TRY(l_buf.insertFromRight(0, PEC_IOP_PIPE_RESET_START_BIT, 1));
+        FAPI_DBG("pec%i: %#lx", l_pec_id, l_buf());
+        FAPI_TRY(fapi2::putScom(l_pec_chiplets, PEC_CPLT_CONF1_CLEAR, l_buf));
+
+        // Phase1 init step 26 (PCS System Control)
+        SET_REG_RMW_WITH_SINGLE_ATTR_16(fapi2::ATTR_PROC_PCIE_PCS_SYSTEM_CNTL,
+                                        PEC_PCS_SYS_CONTROL_REG,
+                                        55, 9);
+
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_PCIE_PCS_M_CNTL, l_pec_chiplets,
+                               l_pcs_m_cntl));
+
+        // Phase1 init step 27 (PCS M1 Control)
+        SET_REG_RMW(l_pcs_m_cntl[0],
+                    PEC_PCS_M1_CONTROL_REG,
+                    55, 9);
+
+        // Phase1 init step 28 (PCS M2 Control)
+        SET_REG_RMW(l_pcs_m_cntl[1],
+                    PEC_PCS_M1_CONTROL_REG,
+                    55, 9);
+
+        // Phase1 init step 29 (PCS M3 Control)
+        SET_REG_RMW(l_pcs_m_cntl[2],
+                    PEC_PCS_M1_CONTROL_REG,
+                    55, 9);
+
+        // Phase1 init step 30 (PCS M4 Control)
+        SET_REG_RMW(l_pcs_m_cntl[3],
+                    PEC_PCS_M1_CONTROL_REG,
+                    55, 9);
+    }
+
     FAPI_INF("End");
+
+fapi_try_exit:
     return fapi2::current_err;
 }
