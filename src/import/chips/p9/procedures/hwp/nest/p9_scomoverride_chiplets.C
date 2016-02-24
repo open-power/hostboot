86d6d607b5134031825f25213dda8059302ce072
Joe McGill <jmcgill@us.ibm.com>
2016-02-24 09:55:00 -0600

    p9_scomoverride_chiplets - drop O,X,PCI syncclk_muxsel for HW354978
    Change-Id: Ib0cf0c76e9315aad88d81dd8ec31dddbb1df086e
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/20878
    Tested-by: Jenkins Server
    Tested-by: Auto Mirror
    Tested-by: Hostboot CI
    Reviewed-by: CHRISTINA L. GRAVES <clgraves@us.ibm.com>
    Reviewed-by: Soma Bhanutej <soma.bhanu@in.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/20839
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/nest/p9_scomoverride_chiplets.C
+++ src/import/chips/p9/procedures/hwp/nest/p9_scomoverride_chiplets.C
@@ -7,7 +7,7 @@
 /*                                                                        */
 /* EKB Project                                                            */
 /*                                                                        */
-/* COPYRIGHT 2015                                                         */
+/* COPYRIGHT 2015,2016                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -23,10 +23,10 @@
 ///
 
 //
-// *HWP HW Owner : Michael Dye <dyem@us.ibm.com>
+// *HWP HW Owner : Joe McGill <jmcgill@us.ibm.com>
 // *HWP FW Owner : Thi N. Tran <thi@us.ibm.com>
 // *HWP Team : Nest
-// *HWP Level : 1
+// *HWP Level : 2
 // *HWP Consumed by : HB
 //
 
@@ -34,6 +34,8 @@
 // Includes
 //------------------------------------------------------------------------------
 #include "p9_scomoverride_chiplets.H"
+#include "p9_perv_scom_addresses.H"
+#include "p9_perv_scom_addresses_fld.H"
 
 //------------------------------------------------------------------------------
 // Function definitions
@@ -43,9 +45,36 @@ fapi2::ReturnCode
 p9_scomoverride_chiplets(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
 {
     FAPI_DBG("Entering ...");
+
+    // HW354978 -- drop syncclock_muxsel for OBUS/XBUS/PCI chiplets
+    fapi2::buffer<uint64_t> l_cplt_ctrl0_clear = 0;
+    l_cplt_ctrl0_clear.setBit<PERV_1_CPLT_CTRL0_TC_UNIT_SYNCCLK_MUXSEL_DC>();
+
+    auto l_perv_functional_vector =
+        i_target.getChildren<fapi2::TARGET_TYPE_PERV>(fapi2::TARGET_STATE_FUNCTIONAL);
+
+    for (auto l_trgt_chplt : l_perv_functional_vector)
+    {
+        uint8_t l_attr_chip_unit_pos = 0; //actual value is read in FAPI_ATTR_GET below
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, l_trgt_chplt,
+                               l_attr_chip_unit_pos));
+
+        if (!((l_attr_chip_unit_pos == 0x09 || l_attr_chip_unit_pos == 0x0A
+               || l_attr_chip_unit_pos == 0x0B
+               || l_attr_chip_unit_pos == 0x0C/* ObusChiplet */) ||
+              (l_attr_chip_unit_pos == 0x0D || l_attr_chip_unit_pos == 0x0E
+               || l_attr_chip_unit_pos == 0x0F/* PcieChiplet */) ||
+              (l_attr_chip_unit_pos == 0x06/* XbusChiplet */)))
+        {
+            continue;
+        }
+
+        FAPI_TRY(fapi2::putScom(l_trgt_chplt, PERV_CPLT_CTRL0_CLEAR, l_cplt_ctrl0_clear));
+    }
+
     FAPI_DBG("Exiting ...");
 
-//fapi_try_exit:
+fapi_try_exit:
     return fapi2::current_err;
 
 }
