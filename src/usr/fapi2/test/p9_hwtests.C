9222ed5052265c819f27125d2d8afc2dffdf3e96
Dan Crowell <dcrowell@us.ibm.com>
2016-03-07 11:40:44 -0600

    Additional fapi2 hw_access support
    Moved function implementations into .C file
    Cleaned up cfam target check
    
    Change-Id: Ib25e6cdb336688eb23e2cfef7f3756781839753f
    RTC: 143118
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/867
    Tested-by: Jenkins Server
    Tested-by: FSP CI Jenkins
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

---
+++ src/usr/fapi2/test/p9_hwtests.C
@@ -0,0 +1,192 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/usr/fapi2/test/p9_hwtests.C $                             */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//------------------------------------------------------------------------------
+/// @file  p9_hwtests.C
+///
+/// @brief These procedures test the fapi2 hw_access interfaces.
+//-----------------------------------------------------------------------------
+
+#include <fapi2.H>
+
+
+fapi2::ReturnCode p9_scomtest_getscom_fail(
+               fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    fapi2::buffer<uint64_t> l_scomdata = 0;
+
+    FAPI_INF("Entering p9_scomtest_getscom_fail...");
+
+    FAPI_INF("Do getscom on proc target");
+    FAPI_TRY(fapi2::getScom(i_target,
+                            0x11223344,
+                            l_scomdata));
+
+ fapi_try_exit:
+
+    FAPI_INF("Exiting p9_scomtest_getscom_fail...");
+
+    return fapi2::current_err;
+
+}
+
+fapi2::ReturnCode p9_scomtest_putscom_fail(
+               fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    fapi2::buffer<uint64_t> l_scomdata = 0;
+
+    FAPI_INF("Entering p9_scomtest_putscom_fail...");
+
+    FAPI_INF("Do getscom on proc target");
+    FAPI_TRY(fapi2::putScom(i_target,
+                            0x22334455,
+                            l_scomdata));
+
+ fapi_try_exit:
+
+    FAPI_INF("Exiting p9_scomtest_putscom_fail...");
+
+    return fapi2::current_err;
+
+}
+
+fapi2::ReturnCode p9_cfamtest_getcfam_fail(
+               fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    fapi2::buffer<uint32_t> l_cfamdata = 0;
+
+    FAPI_INF("Entering p9_cfamtest_getcfam_fail...");
+
+    FAPI_INF("Do getcfam on proc target");
+    FAPI_TRY(fapi2::getCfamRegister(i_target,
+                                    0x11223344,
+                                    l_cfamdata));
+
+ fapi_try_exit:
+
+    FAPI_INF("Exiting p9_cfamtest_getcfam_fail...");
+
+    return fapi2::current_err;
+
+}
+
+fapi2::ReturnCode p9_cfamtest_putcfam_fail(
+               fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    fapi2::buffer<uint32_t> l_cfamdata = 0;
+
+    FAPI_INF("Entering p9_cfamtest_putcfam_fail...");
+
+    FAPI_INF("Do getcfam on proc target");
+    FAPI_TRY(fapi2::putCfamRegister(i_target,
+                                    0x22334455,
+                                    l_cfamdata));
+
+ fapi_try_exit:
+
+    FAPI_INF("Exiting p9_cfamtest_putcfam_fail...");
+
+    return fapi2::current_err;
+
+}
+
+fapi2::ReturnCode p9_scomtest_getscom_pass(
+               fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    fapi2::buffer<uint64_t> l_scomdata = 0;
+
+    FAPI_INF("Entering p9_scomtest_getscom_pass...");
+
+    FAPI_INF("Do getscom on proc target");
+    FAPI_TRY(fapi2::getScom(i_target,
+                            0x02010803, //CXA FIR Mask Register
+                            l_scomdata));
+
+ fapi_try_exit:
+
+    FAPI_INF("Exiting p9_scomtest_getscom_pass...");
+
+    return fapi2::current_err;
+
+}
+
+fapi2::ReturnCode p9_scomtest_putscom_pass(
+               fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    fapi2::buffer<uint64_t> l_scomdata = 0;
+
+    FAPI_INF("Entering p9_scomtest_putscom_pass...");
+
+    FAPI_INF("Do getscom on proc target");
+    FAPI_TRY(fapi2::putScom(i_target,
+                            0x02010803, //CXA FIR Mask Register
+                            l_scomdata));
+
+ fapi_try_exit:
+
+    FAPI_INF("Exiting p9_scomtest_putscom_pass...");
+
+    return fapi2::current_err;
+
+}
+
+fapi2::ReturnCode p9_cfamtest_getcfam_pass(
+               fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    fapi2::buffer<uint32_t> l_cfamdata = 0;
+
+    FAPI_INF("Entering p9_cfamtest_getcfam_pass...");
+
+    FAPI_INF("Do getcfam on proc target");
+    FAPI_TRY(fapi2::getCfamRegister(i_target,
+                                    0x1000, //DATA_0 from FSI2PIB
+                                    l_cfamdata));
+
+ fapi_try_exit:
+
+    FAPI_INF("Exiting p9_cfamtest_getcfam_pass...");
+
+    return fapi2::current_err;
+
+}
+
+fapi2::ReturnCode p9_cfamtest_putcfam_pass(
+               fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    fapi2::buffer<uint32_t> l_cfamdata = 0;
+
+    FAPI_INF("Entering p9_cfamtest_putcfam_pass...");
+
+    FAPI_INF("Do getcfam on proc target");
+    FAPI_TRY(fapi2::putCfamRegister(i_target,
+                                    0x1000, //DATA_0 from FSI2PIB
+                                    l_cfamdata));
+
+ fapi_try_exit:
+
+    FAPI_INF("Exiting p9_cfamtest_putcfam_pass...");
+
+    return fapi2::current_err;
+
+}
