4dea56a10f144a1b0acb8aa9cc027870fd3c8d41
Zane Shelley <zshelle@us.ibm.com>
2016-03-07 17:09:21 -0600

    PRD: moved rule/ out of framework/
    Change-Id: I4ddfa567abb576f7949bf8a9c4ab5ee530eed70e
    RTC: 140081
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/21860
    Tested-by: Jenkins Server
    Reviewed-by: Caleb N. Palmer <cnpalmer@us.ibm.com>
    Reviewed-by: Benjamin J. Weisenbeck <bweisenb@us.ibm.com>
    Reviewed-by: William G. Hoffa <wghoffa@us.ibm.com>
    Reviewed-by: Zane C. Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/22137
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

---
+++ src/usr/diag/prdf/common/rule/tables.mk
@@ -0,0 +1,51 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/usr/diag/prdf/common/rule/tables.mk $
+#
+# OpenPOWER HostBoot Project
+#
+# Contributors Listed Below - COPYRIGHT 2016
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+
+#-------------------------------------------------------------------
+# To add a new chip, modify PRDR_RULE_TABLES line in prdf_rule.mk
+# To change system type, modify VPATH line in rule/Makefile
+#-------------------------------------------------------------------
+
+#------------------------------------------------------------------
+# Change nothing below this line unless you know what you're doing!
+#------------------------------------------------------------------
+
+
+
+# Stuff for errl plugin.
+    # Define required .o's
+PRDR_ERRL_PLUGINS = ${PRDR_RULE_TABLES:S/\.prf/.prf.err.C/g}
+PRDR_ERRL_PLUGINS += ${PRDR_RULE_TABLES:S/\.prf/.prf.reg.C/g}
+PRDR_ERRL_PLUGINS_OFILES = ${PRDR_ERRL_PLUGINS:S/\.C/.o/g}
+    # Ensure that we'll use the latest .C's to build the .o's.
+#${PRDR_ERRL_PLUGINS_OFILES} : ${.TARGET:S/\.o/\.C/g}
+%.prf.err.o: %.prf.err.C
+%.prf.reg.o: %.prf.reg.C
+%.prf.err.C: %.prf
+%.prf.reg.C: %.prf
+%.prf: %.rule
+# end errl plugin.
+
