830a237311e3c256d0d1ed7631d1667f8b9324c2
LiuYangFan <shliuyf@cn.ibm.com>
2016-03-11 03:04:06 -0600

    L2 RAM procedures.
    Changes included:
    1. p9_ram_core: the class for base ramming operations (would reside in SBE)
    2. p9_spr_name_map: the functions to map SPR name to SPR number (for Cronus use)
    3. p9_ram_opcode: the procedure to implement ram an opcode
    4. p9_ram_getspr: the procedure to implement get SPR value
    5. p9_ram_putspr: the procedure to implement put SPR value
    6. p9_ram_getreg: the procedure to implement get GPR/FPR/VSR value
    7. p9_ram_putreg: the procedure to implement put GPR/FPR/VSR value
    8. p9_ram_wrap: the eCMD wrapper to test the procedures
    
    Change-Id: If1cab2741f168390989bd259c37a45afdbe1410b
    Original-Change-Id: I34192e0527a61a48b9c1e69036a3411a1e3d9f6d
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/21933
    Tested-by: Jenkins Server
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/perv/p9_ram_putspr.C
@@ -0,0 +1,67 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/perv/p9_ram_putspr.C $     */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//-----------------------------------------------------------------------------------
+///
+/// @file p9_ram_putspr.C
+/// @brief Utility to implement Put SPR Register by ramming
+///
+//-----------------------------------------------------------------------------------
+// *HWP HWP Owner        : Liu Yang Fan <shliuyf@cn.ibm.com>
+// *HWP HWP Backup Owner : Gou Peng Fei <shgoupf@cn.ibm.com>
+// *HWP FW Owner         : Thi Tran <thi@us.ibm.com>
+// *HWP Team             : Perv
+// *HWP Level            : 2
+// *HWP Consumed by      : None (Cronus test only)
+//-----------------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------------
+#include <p9_ram_putspr.H>
+
+//-----------------------------------------------------------------------------------
+// Function definitions
+//-----------------------------------------------------------------------------------
+fapi2::ReturnCode p9_ram_putspr(const fapi2::Target<fapi2::TARGET_TYPE_CORE>& i_target,
+                                const uint8_t i_thread,
+                                const std::string i_name,
+                                const fapi2::buffer<uint64_t>* i_buffer)
+{
+    FAPI_INF("Start");
+    uint32_t spr_num = 0;
+
+    // instantiate the basic RamCore class
+    RamCore ram(i_target, i_thread);
+    // map spr register name to number
+    FAPI_TRY(p9_spr_name_map_init());
+    FAPI_TRY(p9_spr_name_map(i_name, true, spr_num));
+    // call RamCore put_reg method
+    FAPI_TRY(ram.put_reg(REG_SPR, spr_num, i_buffer));
+
+fapi_try_exit:
+    FAPI_INF("End");
+    return fapi2::current_err;
+}
