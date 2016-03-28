455cfde12db59a37867f0f05b9778314af2b4b5a
Dan Crowell <dcrowell@us.ibm.com>
2016-03-28 16:47:46 -0500

    Fix rounding error in memory allocation code
    A rounding error exposed by a new compiler caused us to write
    over the SPTE entries for the extended memory block.  This
    led to unhandled page faults due to invalid permissions on the
    affected pages.
    
    Change-Id: I53656103252180d37a4291176d45f2cb3f63421d
    Backport: yes
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/22560
    Reviewed-by: Andrew J. Geissler <andrewg@us.ibm.com>
    Tested-by: Jenkins Server
    Tested-by: FSP CI Jenkins
    Reviewed-by: William G. Hoffa <wghoffa@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/22564
    Tested-by: Jenkins OP Build CI
    Tested-by: Jenkins OP HW
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/kernel/basesegment.C
+++ src/kernel/basesegment.C
@@ -5,7 +5,9 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* COPYRIGHT International Business Machines Corp. 2011,2014              */
+/* Contributors Listed Below - COPYRIGHT 2011,2016                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
 /*                                                                        */
 /* Licensed under the Apache License, Version 2.0 (the "License");        */
 /* you may not use this file except in compliance with the License.       */
@@ -263,8 +265,9 @@ int BaseSegment::_mmExtend(void)
     // Now need to take the pages past the SPTE and add them to the heap.
 
     //get the number of pages needed to hold the SPTE entries.
-    uint64_t spte_pages = (ALIGN_PAGE(l_size)/PAGESIZE *
-                           sizeof(ShadowPTE))/PAGESIZE;
+    uint64_t spte_pages = (ALIGN_PAGE
+                           (ALIGN_PAGE(l_size)/PAGESIZE*sizeof(ShadowPTE)))
+                          /PAGESIZE;
 
     printkd("Number of SPTE pages %ld\n", spte_pages);
 
