f07b0603a6130296561c7a71f43ad42477d7d627
Claus Michael Olsen <cmolsen@us.ibm.com>
2016-03-30 14:35:25 -0500

    Relocation of header files to ./tools/imageProcs directory and associated
    changes to various *.mk files to point to new location as well as minor
    changes to xip_tool.
    Removed p9_ring_apply.mk because it isn't needed at this point anyway
    and causes Jenkins to fail. Giri is checking this file plus associated
    corrections to the other  p9_ring_apply.* files.
    Added and updated p9_xip_image.h, and updated p9_scan_compression.H,
    according to commit 22997.
    
    Change-Id: Ic13680ba57f2e6080f90edac528a080d5e3d532d
    Original-Change-Id: I888dc2fccf92d3173ae45870b7cdefeb29757058
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/22682
    Tested-by: Hostboot CI
    Tested-by: Jenkins Server
    Tested-by: PPE CI
    Reviewed-by: Claus M. Olsen <cmolsen@us.ibm.com>
    Reviewed-by: Prachi Gupta <pragupta@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Martin Peschke <mpeschke@de.ibm.com>

--- src/import/chips/p9/xip/p9_xip_image.mk
+++ src/import/chips/p9/xip/p9_xip_image.mk
@@ -22,9 +22,6 @@
 # permissions and limitations under the License.
 #
 # IBM_PROLOG_END_TAG
-
 PROCEDURE = p9_xip_image
-
-lib$(PROCEDURE)_COMMONFLAGS = -DDEBUG_P9_XIP_IMAGE=1
-
+lib$(PROCEDURE)_COMMONFLAGS += -DDEBUG_P9_XIP_IMAGE=1
 $(call BUILD_PROCEDURE)
