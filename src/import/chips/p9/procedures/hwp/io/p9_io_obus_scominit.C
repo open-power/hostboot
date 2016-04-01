f13969f26408a686f4309d79c313f539c88edb80
Sumit Kumar <sumit_kumar@in.ibm.com>
2016-04-01 04:06:57 -0500

    L2 HWP p9_io_obus_scominit
    Change-Id: Ifeb445e8e2c35171c8ea64b0c9095b1ecb9bfece
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/22735
    Tested-by: Jenkins Server
    Tested-by: Hostboot CI
    Reviewed-by: Christopher W. Steffen <cwsteffen@us.ibm.com>
    Reviewed-by: Richard J. Knight <rjknight@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Tested-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/22737
    Tested-by: FSP CI Jenkins
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/io/p9_io_obus_scominit.C
+++ src/import/chips/p9/procedures/hwp/io/p9_io_obus_scominit.C
@@ -7,7 +7,7 @@
 /*                                                                        */
 /* EKB Project                                                            */
 /*                                                                        */
-/* COPYRIGHT 2015                                                         */
+/* COPYRIGHT 2015,2016                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -22,10 +22,10 @@
 ///
 //----------------------------------------------------------------------------
 // *HWP HWP Owner       : Chris Steffen <cwsteffen@us.ibm.com>
-// *HWP HWP Backup Owner:
+// *HWP HWP Backup Owner: Gary Peterson <garyp@us.ibm.com>
 // *HWP FW Owner        : Sumit Kumar <sumit_kumar@in.ibm.com>
 // *HWP Team            : IO
-// *HWP Level           : 1
+// *HWP Level           : 2
 // *HWP Consumed by     : FSP:HB
 //----------------------------------------------------------------------------
 //
@@ -43,7 +43,7 @@
 //------------------------------------------------------------------------------
 //  Includes
 //------------------------------------------------------------------------------
-#include <p9_io_gcr.H>
+#include <p9_io_scom.H>
 #include <p9_io_regs.H>
 #include <p9_io_obus_scominit.H>
 
@@ -64,11 +64,32 @@ fapi2::ReturnCode p9_io_obus_scominit(
 {
     // mark HWP entry
     FAPI_INF("p9_io_obus_scominit: Entering...");
+    fapi2::ReturnCode rc = fapi2::FAPI2_RC_SUCCESS;
 
+    // get system target
+    const fapi2::Target<fapi2::TARGET_TYPE_SYSTEM> l_system_target;
+
+    // assert IO reset to power-up bus endpoint logic
+    // read-modify-write, set single reset bit (HW auto-clears)
+    // on writeback
+    FAPI_TRY(io::rmw(OPT_IORESET_HARD_BUS0, i_target, 0, 0, 1));
+
+    FAPI_INF("Invoke FAPI procedure core: input_target");
+//TODO:
+#if 0
+    FAPI_EXEC_HWP(rc, p9_obus_scom, i_target, l_system_target);
+#endif
+
+    FAPI_INF("Invoke FAPI procedure core: connected_target");
+//TODO:
+#if 0
+    FAPI_EXEC_HWP(rc, p9_obus_scom, i_connected_target, l_system_target);
+#endif
 
     // mark HWP exit
     FAPI_INF("p9_io_obus_scominit: ...Exiting");
 
-    return fapi2::FAPI2_RC_SUCCESS;
+fapi_try_exit:
+    return fapi2::current_err;
 }
 
