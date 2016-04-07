c61dfac62ce9126df524a71f8ebfffda582f2f08
Richard J. Knight <rjknight@us.ibm.com>
2016-04-07 14:14:26 -0500

    Procedure crashes when trying to query an EC feature
        -Updated queryEcFeature to have two parts, base
         attribute reading is now in a library, while
         feature checking logic is now in small individual
         inline functions.
    
    Change-Id: I4b0035ea369ea07b716d6edc00f1eb281c20aa02
    Original-Change-Id: I4c3685d6a85946297af31f7f3da4d918bca88039
    RTC:151184
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23025
    Tested-by: Jenkins Server
    Tested-by: PPE CI
    Tested-by: Hostboot CI
    Reviewed-by: Matt K. Light <mklight@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/hwpf/fapi2/src/fapi2_utils.C
@@ -0,0 +1,62 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/hwpf/fapi2/src/fapi2_utils.C $                     */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+/**
+ *  @file utils.C
+ *  @brief Implements fapi2 utilities
+ */
+#include <fapi2_attribute_service.H>
+#include <attribute_ids.H>
+#include <return_code.H>
+#include <plat_trace.H>
+#include <target.H>
+
+namespace fapi2
+{
+
+ReturnCode queryChipEcAndName(
+    const Target < fapi2::TARGET_TYPE_PROC_CHIP |
+    fapi2::TARGET_TYPE_MEMBUF_CHIP > & i_target,
+    fapi2::ATTR_NAME_Type& o_chipName, fapi2::ATTR_EC_Type& o_chipEc )
+{
+
+    ReturnCode l_rc = FAPI_ATTR_GET_PRIVILEGED(fapi2::ATTR_NAME, i_target, o_chipName);
+
+    if ( l_rc != FAPI2_RC_SUCCESS )
+    {
+        FAPI_ERR("queryChipEcFeature: error getting chip name");
+    }
+    else
+    {
+        l_rc = FAPI_ATTR_GET_PRIVILEGED(fapi2::ATTR_EC, i_target, o_chipEc);
+
+        if ( l_rc != FAPI2_RC_SUCCESS )
+        {
+            FAPI_ERR("queryChipEcFeature: error getting chip ec");
+        }
+    }
+
+    return l_rc;
+}
+};
