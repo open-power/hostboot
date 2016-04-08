c68019c5d93245b479b007b4d7a5209e8017446e
Stephen Cprek <smcprek@us.ibm.com>
2016-04-08 12:50:56 -0500

    Change securerom test to use current hw hash key
    Change-Id: Icb596e6c69fadd2f7b8109876a92db04763f206f
    RTC: 68883
    ForwardPort: yes
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23066
    Tested-by: Jenkins Server
    Tested-by: Jenkins OP Build CI
    Tested-by: Jenkins OP HW
    Tested-by: FSP CI Jenkins
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
    Reviewed-by: Matthew A. Ploetz <maploetz@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/secureboot/base/securerom.C
+++ src/usr/secureboot/base/securerom.C
@@ -279,7 +279,6 @@ errlHndl_t SecureROM::initialize()
 
     TRACDCOMP(g_trac_secure,EXIT_MRK"SecureROM::initialize() - %s",
               ((NULL == l_errl) ? "No Error" : "With Error") );
-
     return l_errl;
 
 }
@@ -314,7 +313,8 @@ errlHndl_t SecureROM::verifyContainer(void * i_container, size_t i_size)
 
         // Now set hw_key_hash, which is of type sha2_hash_t, to iv_hash_key
         memcpy (&l_hw_parms.hw_key_hash, &iv_hash_key, sizeof(sha2_hash_t));
-
+        TRACFBIN(g_trac_secure,"SecureROM::verifyContainer(): hw_key_hash",
+                 l_hw_parms.hw_key_hash, sizeof(sha2_hash_t));
 
         /*******************************************************************/
         /* Call ROM_verify() function via an assembly call                 */
@@ -332,10 +332,20 @@ errlHndl_t SecureROM::verifyContainer(void * i_container, size_t i_size)
                  iv_device_ptr);
 
 
+        ROM_container_raw* l_container = reinterpret_cast<ROM_container_raw*>(i_container);
+        TRACFCOMP(g_trac_secure,"magic_number 0x%X", l_container->magic_number);
+        TRACFCOMP(g_trac_secure,"version 0x%X", l_container->version);
+        TRACFCOMP(g_trac_secure,"container_size 0x%X", l_container->container_size);
+        TRACFCOMP(g_trac_secure,"target_hrmor 0x%X", l_container->target_hrmor);
+        TRACFCOMP(g_trac_secure,"stack_pointer 0x%X", l_container->stack_pointer);
+        TRACFBIN(g_trac_secure,"hw_pkey_a", l_container->hw_pkey_a, SHA512_DIGEST_LENGTH);
+        TRACFBIN(g_trac_secure,"hw_pkey_b", l_container->hw_pkey_b, SHA512_DIGEST_LENGTH);
+        TRACFBIN(g_trac_secure,"hw_pkey_c", l_container->hw_pkey_c, SHA512_DIGEST_LENGTH);
+        TRACFCOMP(g_trac_secure,"prefix 0x%X", l_container->prefix);
+
         l_rc = call_rom_verify(reinterpret_cast<void*>
-                                   (l_rom_verify_startAddr),
-                               reinterpret_cast<ROM_container_raw*>
-                                   (i_container),
+                               (l_rom_verify_startAddr),
+                               l_container,
                                &l_hw_parms);
 
 
