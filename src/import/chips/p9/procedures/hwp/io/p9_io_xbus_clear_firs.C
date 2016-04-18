e1b6fe656f489bf8a367389b7713f393199e4776
Chris Steffen <cwsteffen@us.ibm.com>
2016-04-18 16:20:25 -0500

    io scom access conversion
    Change-Id: I7a0f290c0ea97821af3ad24fa5fed9f0304faaed
    Original-Change-Id: Ifed73825b960fd2e7cbaad65321f016f8dfcf529
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23392
    Tested-by: Jenkins Server
    Tested-by: Hostboot CI
    Reviewed-by: Gary A. Peterson <garyp@us.ibm.com>
    Reviewed-by: Richard J. Knight <rjknight@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/24105
    Tested-by: FSP CI Jenkins
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/io/p9_io_xbus_clear_firs.C
+++ src/import/chips/p9/procedures/hwp/io/p9_io_xbus_clear_firs.C
@@ -42,7 +42,7 @@
 //  Includes
 //-----------------------------------------------------------------------------
 #include <p9_io_xbus_clear_firs.H>
-#include <p9_io_gcr.H>
+#include <p9_io_scom.H>
 #include <p9_io_regs.H>
 
 //-----------------------------------------------------------------------------
@@ -50,30 +50,31 @@
 //-----------------------------------------------------------------------------
 fapi2::ReturnCode io_rx_fir_reset(
     const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_target,
-    const uint8_t& i_clock_group);
+    const uint8_t&                                   i_clock_group );
 
 fapi2::ReturnCode io_tx_fir_reset(
     const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_target,
-    const uint8_t& i_clock_group);
+    const uint8_t&                                   i_clock_group );
 
 /**
- * @brief A HWP that runs on every instance of the XBUS(EDI+)
+ * @brief Clears PHY Rx/Tx FIRs on the XBUS(EDI+) specified target.  The FIRs
+ *   are cleared by toggling a rx & tx fir reset bit.
  * @param[in] i_target         FAPI2 Target
  * @param[in] i_clock_group    Clock Group
  * @retval ReturnCode
  */
 fapi2::ReturnCode p9_io_xbus_clear_firs(
     const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_target,
-    const uint8_t& i_clock_group)
+    const uint8_t&                                   i_clock_group )
 {
-    FAPI_IMP("I/O Start Xbus Clear FIRs");
+    FAPI_IMP( "I/O Start Xbus Clear FIRs" );
 
-    FAPI_TRY(io_tx_fir_reset(i_target, i_clock_group), "Tx Reset Failed");
+    FAPI_TRY( io_tx_fir_reset( i_target, i_clock_group ), "Tx Reset Failed" );
 
-    FAPI_TRY(io_rx_fir_reset(i_target, i_clock_group), "Rx Reset Failed");
+    FAPI_TRY( io_rx_fir_reset( i_target, i_clock_group ), "Rx Reset Failed" );
 
 fapi_try_exit:
-    FAPI_IMP("I/O End Xbus Clear FIRs");
+    FAPI_IMP( "I/O End Xbus Clear FIRs" );
     return fapi2::current_err;
 }
 
@@ -87,21 +88,22 @@ fapi2::ReturnCode io_rx_fir_reset(
     const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_target,
     const uint8_t& i_clock_group)
 {
-    Register < EDIP_RX_GLBSM_CNTLX1_EO_PG > rx_fir_reg;
+    const uint8_t LANE_00 = 0;
+    uint64_t l_data = 0;
 
-    FAPI_TRY(rx_fir_reg.read(i_target, i_clock_group),
-             "Reading Rx Fir Reg Failed");
+    FAPI_TRY( io::read( EDIP_RX_FIR_RESET, i_target, i_clock_group, LANE_00, l_data ),
+              "Reading Rx Fir Reg Failed");
 
-    rx_fir_reg.set<EDIP_RX_FIR_RESET>(0);
-    FAPI_TRY(rx_fir_reg.write(i_target, i_clock_group),
+    io::set (EDIP_RX_FIR_RESET, 0, l_data);
+    FAPI_TRY(io::write( EDIP_RX_FIR_RESET, i_target, i_clock_group, LANE_00, l_data ),
              "Writing Rx Fir Reg Failed");
 
-    rx_fir_reg.set<EDIP_RX_FIR_RESET>(1);
-    FAPI_TRY(rx_fir_reg.write(i_target, i_clock_group),
-             "Writing rx Fir Reg Failed");
+    io::set (EDIP_RX_FIR_RESET, 1, l_data);
+    FAPI_TRY(io::write( EDIP_RX_FIR_RESET, i_target, i_clock_group, LANE_00, l_data ),
+             "Writing Rx Fir Reg Failed");
 
-    rx_fir_reg.set<EDIP_RX_FIR_RESET>(0);
-    FAPI_TRY(rx_fir_reg.write(i_target, i_clock_group),
+    io::set (EDIP_RX_FIR_RESET, 0, l_data);
+    FAPI_TRY(io::write( EDIP_RX_FIR_RESET, i_target, i_clock_group, LANE_00, l_data ),
              "Writing Rx Fir Reg Failed");
 
 fapi_try_exit:
@@ -118,21 +120,22 @@ fapi2::ReturnCode io_tx_fir_reset(
     const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_target,
     const uint8_t& i_clock_group)
 {
-    Register < EDIP_TX_FIR_RESET_PG  > tx_fir_reg;
+    const uint8_t LANE_00 = 0;
+    uint64_t l_data = 0;
 
-    FAPI_TRY(tx_fir_reg.read(i_target, i_clock_group),
-             "Reading Tx Fir Reg Failed");
+    FAPI_TRY( io::read( EDIP_TX_FIR_RESET, i_target, i_clock_group, LANE_00, l_data ),
+              "Reading Tx Fir Reg Failed");
 
-    tx_fir_reg.set<EDIP_TX_FIR_RESET>(0);
-    FAPI_TRY(tx_fir_reg.write(i_target, i_clock_group),
+    io::set (EDIP_TX_FIR_RESET, 0, l_data);
+    FAPI_TRY(io::write( EDIP_TX_FIR_RESET, i_target, i_clock_group, LANE_00, l_data ),
              "Writing Tx Fir Reg Failed");
 
-    tx_fir_reg.set<EDIP_TX_FIR_RESET>(1);
-    FAPI_TRY(tx_fir_reg.write(i_target, i_clock_group),
+    io::set (EDIP_TX_FIR_RESET, 1, l_data);
+    FAPI_TRY(io::write( EDIP_TX_FIR_RESET, i_target, i_clock_group, LANE_00, l_data ),
              "Writing Tx Fir Reg Failed");
 
-    tx_fir_reg.set<EDIP_TX_FIR_RESET>(0);
-    FAPI_TRY(tx_fir_reg.write(i_target, i_clock_group),
+    io::set (EDIP_TX_FIR_RESET, 0, l_data);
+    FAPI_TRY(io::write( EDIP_TX_FIR_RESET, i_target, i_clock_group, LANE_00, l_data ),
              "Writing Tx Fir Reg Failed");
 
 fapi_try_exit:
