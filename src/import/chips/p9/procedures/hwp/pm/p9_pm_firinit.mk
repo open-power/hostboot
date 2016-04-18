ca8d261f951b839ff943d98cf6cade77276bb8ce
Sangeetha T S <sangeet2@in.ibm.com>
2016-04-18 06:10:10 -0500

    p9_pm_reset - Level 2 delivery
    -> Complete design for P9
    -> Transliteration from P8 to P9
    -> SUET test
    -> AWAN test
    
    Change-Id: I3f5298d40d6617ec5f1835fdee368e2693f2e649
    Original-Change-Id: Iab9a8cd7dc7860941263c4fe95814dd259b97a23
    RTC: 140969
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/24313
    Dev-Ready: Brian T. Vanderpool <vanderp@us.ibm.com>
    Tested-by: Jenkins Server
    Tested-by: PPE CI
    Tested-by: Hostboot CI
    Reviewed-by: Brian T. Vanderpool <vanderp@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/pm/p9_pm_firinit.mk
+++ src/import/chips/p9/procedures/hwp/pm/p9_pm_firinit.mk
@@ -23,5 +23,5 @@
 #
 # IBM_PROLOG_END_TAG
 PROCEDURE=p9_pm_firinit
-libp9_pm_firinit_DEPLIBS += p9_pm_utils
+#libp9_pm_firinit_DEPLIBS += p9_pm_occ_firinit p9_pm_pba_firinit p9_pm_ppm_firinit p9_pm_cme_firinit
 $(call BUILD_PROCEDURE)
