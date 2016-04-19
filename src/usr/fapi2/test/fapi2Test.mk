8078862b70a91ec31259d6204062d37f57983014
Corey Swenson <cswenson@us.ibm.com>
2016-04-19 16:24:25 -0500

    Build FAPI2 into HBRT image
    Change-Id: I3567d4d534a26edd33810f6b3a5c033507ffc119
    RTC:145237
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23441
    Tested-by: Jenkins Server
    Tested-by: FSP CI Jenkins
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

---
+++ src/usr/fapi2/test/fapi2Test.mk
@@ -0,0 +1,47 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/usr/fapi2/test/fapi2Test.mk $
+#
+# OpenPOWER HostBoot Project
+#
+# Contributors Listed Below - COPYRIGHT 2016
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+#
+# @file src/usr/fapi2/test/fapi2Test.mk
+#
+# @brief Common makefile for fapi2 and runtime test directory
+#
+
+EXTRAINCDIR += ${ROOTPATH}/src/import/hwpf/fapi2/include/
+EXTRAINCDIR += ${ROOTPATH}/src/include/usr/fapi2/
+EXTRAINCDIR += ${ROOTPATH}/src/usr/fapi2/test/
+EXTRAINCDIR += ${ROOTPATH}/src/import/chips/p9/utils/
+EXTRAINCDIR += ${ROOTPATH}/src/import/chips/p9/common/include/
+EXTRAINCDIR += ${ROOTPATH}/src/import/chips/p9/procedures/hwp/pm/
+EXTRAINCDIR += ${ROOTPATH}/src/include/usr/targeting/common/
+
+# Procedures
+OBJS += p9_sample_procedure.o
+OBJS += p9_hwtests.o
+OBJS += fapi2TestUtils.o
+
+TESTS += ${ROOTPATH}/src/usr/fapi2/test/*Test.H
+TESTS += ${ROOTPATH}/src/usr/fapi2/test/*TestCxx.H
+
+VPATH += ${ROOTPATH}/src/import/chips/p9/procedures/hwp/pm/
