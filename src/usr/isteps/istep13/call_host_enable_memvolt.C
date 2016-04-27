346b5095fac58b8f6d4b98f74faa4ef7d408916f
Andrew Geissler <andrewg@us.ibm.com>
2016-04-27 11:13:20 -0500

    Update ISTEP to IPL Flow v0.93
    Includes a lot of deletes and a few renames.
    
    Change-Id: I36ce351d307d43d67c8dfe4dbe3a94e67380cc20
    RTC: 147800
    CMVC-Prereq:993409
    CMVC-Prereq:992647
    CMVC-Prereq:993648
    CMVC-Prereq:993634
    CMVC-Prereq:993940
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23732
    Tested-by: Jenkins Server
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
    Tested-by: FSP CI Jenkins
    Reviewed-by: William G. Hoffa <wghoffa@us.ibm.com>

--- src/usr/isteps/istep13/call_host_enable_memvolt.C
+++ src/usr/isteps/istep13/call_host_enable_memvolt.C
@@ -1,11 +1,11 @@
 /* IBM_PROLOG_BEGIN_TAG                                                   */
 /* This is an automatically generated prolog.                             */
 /*                                                                        */
-/* $Source: src/usr/isteps/istep11/call_cen_sbe_tp_arrayinit.C $          */
+/* $Source: src/usr/isteps/istep13/call_host_enable_memvolt.C $           */
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015                             */
+/* Contributors Listed Below - COPYRIGHT 2016                             */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -23,16 +23,54 @@
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
 #include <errl/errlentry.H>
+#include <errl/errlmanager.H>
+#include <errl/errludtarget.H>
+#include <isteps/hwpisteperror.H>
+#include <initservice/isteps_trace.H>
 
-using namespace ERRORLOG;
+//  targeting support
+#include <targeting/common/commontargeting.H>
+#include <targeting/common/util.H>
+#include <targeting/common/utilFilter.H>
 
-namespace ISTEP_11
+#include "platform_vddr.H"
+using   namespace   ERRORLOG;
+using   namespace   ISTEP;
+using   namespace   ISTEP_ERROR;
+using   namespace   TARGETING;
+
+namespace ISTEP_13
 {
-void* call_cen_sbe_tp_arrayinit (void *io_pArgs)
+void* call_host_enable_memvolt (void *io_pArgs)
 {
+    TRACDCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+            ENTER_MRK"call_host_enable_memvolt" );
+
     errlHndl_t l_err = NULL;
-    //@TODO call cen_sbe_tp_arrayinit.C HWP
-    return l_err;
+    IStepError l_StepError;
+
+    // This fuction has compile-time binding for different platforms
+    l_err = platform_enable_vddr();
+
+    if( l_err )
+    {
+        TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                  "ERROR 0x%.8X: call_host_enable_memvolt returns error",
+                  l_err->reasonCode());
+
+        l_StepError.addErrorDetails( l_err );
+
+        // Create IStep error log and cross reference to error that occurred
+        l_StepError.addErrorDetails( l_err );
+
+        // Commit Error
+        errlCommit( l_err, HWPF_COMP_ID );
+    }
+
+    TRACDCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+               EXIT_MRK"call_host_enable_memvolt" );
+
+    return l_StepError.getErrorHandle();
 }
 
 };
