504655c3b300fc1b90a9b0cfec3fb07dd36a608f
Brian Silver <bsilver@us.ibm.com>
2016-04-29 07:12:23 -0500

    Change c_str to remove prototype template mechanism
    Change-Id: Ia04fb4b82468763b11ad89444795af10afd803cf
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23858
    Tested-by: Jenkins Server
    Tested-by: Hostboot CI
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: ANDRE A. MARIN <aamarin@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Brian R. Silver <bsilver@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/23861
    Tested-by: FSP CI Jenkins
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/lib/utils/c_str.C
+++ src/import/chips/p9/procedures/hwp/memory/lib/utils/c_str.C
@@ -22,7 +22,7 @@
 /// @brief Storage for the C-string name of a thing
 ///
 // *HWP HWP Owner: Brian Silver <bsilver@us.ibm.com>
-// *HWP HWP Backup: Craig Hamilton <cchamilt@us.ibm.com>
+// *HWP HWP Backup: Andre Marin <aamarin@us.ibm.com>
 // *HWP Team: Memory
 // *HWP Level: 2
 // *HWP Consumed by: HB:FSP
@@ -44,48 +44,23 @@ namespace mss
 // Thread local storage for the string we're going to create.
 thread_local char c_str_storage[fapi2::MAX_ECMD_STRING_LEN];
 
-template< fapi2::TargetType T >
-char const* make_c_str_helper( char const* s, const fapi2::Target<T>& i_target);
-
 template<>
-char const* c_str<DEFAULT_KIND, TARGET_TYPE_DIMM>( const fapi2::Target<TARGET_TYPE_DIMM>& i_target )
+const char* c_str( const fapi2::template Target<fapi2::TARGET_TYPE_DIMM>& i_target )
 {
-    return make_c_str_helper(" unknown dimm", i_target);
-}
+    constexpr auto l_max_gen = 3;
+    constexpr auto l_max_type = 4;
+    static const char* l_map_gen_to_string[l_max_gen] = {"empty", "DDR3", "DDR4"};
+    static const char* l_map_type_to_string[l_max_type] = {"empty", "RDIMM", "UDIMM", "LRDIMM"};
+    char l_buffer[fapi2::MAX_ECMD_STRING_LEN];
 
-template<>
-char const* c_str<KIND_RDIMM_DDR4, TARGET_TYPE_DIMM>( const fapi2::Target<TARGET_TYPE_DIMM>& i_target )
-{
-    return make_c_str_helper(" rdimm (ddr4)", i_target);
-}
-
-template<>
-char const* c_str<KIND_RDIMM_EMPTY, TARGET_TYPE_DIMM>( const fapi2::Target<TARGET_TYPE_DIMM>& i_target )
-{
-    return make_c_str_helper(" rdimm (empty)", i_target);
-}
-
-template<>
-char const* c_str<KIND_LRDIMM_DDR4, TARGET_TYPE_DIMM>( const fapi2::Target<TARGET_TYPE_DIMM>& i_target )
-{
-    return make_c_str_helper(" lrdimm (ddr4)", i_target);
-}
-
-template<>
-char const* c_str<KIND_LRDIMM_EMPTY, TARGET_TYPE_DIMM>( const fapi2::Target<TARGET_TYPE_DIMM>& i_target )
-{
-    return make_c_str_helper(" lrdimm (empty)", i_target);
-}
-
-template<>
-char const* c_str<FORCE_DISPATCH, TARGET_TYPE_DIMM>( const fapi2::Target<TARGET_TYPE_DIMM>& i_target )
-{
     uint8_t l_type = 0;
     uint8_t l_gen = 0;
 
     uint8_t l_value[2][2];
     auto l_mca = i_target.getParent<TARGET_TYPE_MCA>();
     auto l_mcs = l_mca.getParent<TARGET_TYPE_MCS>();
+
+    fapi2::toString( i_target, c_str_storage, fapi2::MAX_ECMD_STRING_LEN );
 
     // Had to unroll FAPI_TRY so that fapi2::current_err doesn't get overwritten, causes errors
     // when calling c_str inside of a function that returns fapi2::ReturnCode
@@ -96,6 +71,11 @@ char const* c_str<FORCE_DISPATCH, TARGET_TYPE_DIMM>( const fapi2::Target<TARGET_
 
     l_type = l_value[mss::index(l_mca)][mss::index(i_target)];
 
+    if (l_type >= l_max_type)
+    {
+        goto fapi_try_exit;
+    }
+
     // Had to unroll FAPI_TRY so that fapi2::current_err doesn't get overwritten, causes errors
     // when calling c_str inside of a function that returns fapi2::ReturnCode
     if (FAPI_ATTR_GET(fapi2::ATTR_EFF_DRAM_GEN, l_mcs, l_value) != FAPI2_RC_SUCCESS)
@@ -105,18 +85,17 @@ char const* c_str<FORCE_DISPATCH, TARGET_TYPE_DIMM>( const fapi2::Target<TARGET_
 
     l_gen = l_value[mss::index(l_mca)][mss::index(i_target)];
 
-    return c_str_dispatch<FORCE_DISPATCH, TARGET_TYPE_DIMM>(dimm_kind( l_type, l_gen ), i_target);
+    if (l_gen >= l_max_gen)
+    {
+        goto fapi_try_exit;
+    }
+
+    snprintf(l_buffer, fapi2::MAX_ECMD_STRING_LEN, " %s (%s)", l_map_type_to_string[l_type], l_map_gen_to_string[l_gen]);
+    return strncat( c_str_storage, l_buffer, fapi2::MAX_ECMD_STRING_LEN - strlen(c_str_storage) );
 
 fapi_try_exit:
     // Probably the best we're going to do ...
-    return "couldn't get dimm type, dram gen";
-}
-
-template< fapi2::TargetType T >
-char const* make_c_str_helper( char const* s, const fapi2::Target<T>& i_target)
-{
-    fapi2::toString( i_target, c_str_storage, fapi2::MAX_ECMD_STRING_LEN );
-    return strncat( c_str_storage, s, fapi2::MAX_ECMD_STRING_LEN - strlen(c_str_storage) );
+    return c_str_storage;
 }
 
 }
