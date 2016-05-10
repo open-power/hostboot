925f5028378adffd8f55b176cc7b5d565acc2d37
Zane Shelley <zshelle@us.ibm.com>
2016-05-10 17:00:25 -0500

    PRD: fixed range issue in Dynamic Memory Deallocation
    Change-Id: I2c79dbedf154ad85a25df85a236e30986bb1be20
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/24354
    Tested-by: Jenkins Server
    Reviewed-by: Caleb N. Palmer <cnpalmer@us.ibm.com>
    Reviewed-by: Benjamin J. Weisenbeck <bweisenb@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Zane C. Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/24353
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/24992
    Tested-by: FSP CI Jenkins

--- src/usr/diag/prdf/plat/prdfPlatServices_rt.C
+++ src/usr/diag/prdf/plat/prdfPlatServices_rt.C
@@ -93,7 +93,7 @@ void sendDynMemDeallocRequest( uint64_t i_startAddr, uint64_t i_endAddr )
         }
 
         int32_t rc = g_hostInterfaces->memory_error( i_startAddr,
-                                                     i_startAddr,
+                                                     i_endAddr,
                                                      MEMORY_ERROR_UE );
         if( SUCCESS != rc )
         {
