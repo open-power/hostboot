040e09048981acad382bf262562ebae752cf35a0
Joe McGill <jmcgill@us.ibm.com>
2016-05-12 16:43:56 -0500

    p9_scomoverride_chiplets -- remove workaround for HW354978
    Change-Id: Ic4f78ede47dee9c419e66eedd37f6074050e13dd
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/24483
    Tested-by: Jenkins Server
    Tested-by: Hostboot CI
    Reviewed-by: Benjamin Gass <bgass@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Prachi Gupta <pragupta@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/24484
    Tested-by: FSP CI Jenkins
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/nest/p9_scomoverride_chiplets.C
+++ src/import/chips/p9/procedures/hwp/nest/p9_scomoverride_chiplets.C
@@ -34,8 +34,6 @@
 // Includes
 //------------------------------------------------------------------------------
 #include "p9_scomoverride_chiplets.H"
-#include "p9_perv_scom_addresses.H"
-#include "p9_perv_scom_addresses_fld.H"
 
 //------------------------------------------------------------------------------
 // Function definitions
@@ -46,36 +44,8 @@ p9_scomoverride_chiplets(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_ta
 {
     FAPI_DBG("Entering ...");
 
-    // HW354978 -- drop syncclock_muxsel for OBUS/XBUS/PCI chiplets
-    fapi2::buffer<uint64_t> l_cplt_ctrl0_clear = 0;
-    l_cplt_ctrl0_clear.setBit<PERV_1_CPLT_CTRL0_TC_UNIT_SYNCCLK_MUXSEL_DC>();
-
-    auto l_perv_functional_vector =
-        i_target.getChildren<fapi2::TARGET_TYPE_PERV>(fapi2::TARGET_STATE_FUNCTIONAL);
-
-    for (auto l_trgt_chplt : l_perv_functional_vector)
-    {
-        uint8_t l_attr_chip_unit_pos = 0; //actual value is read in FAPI_ATTR_GET below
-        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS, l_trgt_chplt,
-                               l_attr_chip_unit_pos));
-
-        if (!((l_attr_chip_unit_pos == 0x09 || l_attr_chip_unit_pos == 0x0A
-               || l_attr_chip_unit_pos == 0x0B
-               || l_attr_chip_unit_pos == 0x0C/* ObusChiplet */) ||
-              (l_attr_chip_unit_pos == 0x0D || l_attr_chip_unit_pos == 0x0E
-               || l_attr_chip_unit_pos == 0x0F/* PcieChiplet */) ||
-              (l_attr_chip_unit_pos == 0x06/* XbusChiplet */)))
-        {
-            continue;
-        }
-
-        FAPI_TRY(fapi2::putScom(l_trgt_chplt, PERV_CPLT_CTRL0_CLEAR, l_cplt_ctrl0_clear));
-    }
-
     FAPI_DBG("Exiting ...");
 
-fapi_try_exit:
     return fapi2::current_err;
-
 }
 
