a1815504911808ad8c2d07726271058af8ff3348
Andre Marin <aamarin@us.ibm.com>
2016-05-16 14:24:56 -0500

    Add initial L2 mss_freq_system procedure.
    Change-Id: Ie6e598e5d24ccfd476ee8686acf80e782748f53c
    Original-Change-Id: Ife86394b2779634f84392f3e0ca22e4f30854ebf
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/25028
    Tested-by: Jenkins Server
    Tested-by: Hostboot CI
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: Brian R. Silver <bsilver@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/memory/p9_mss_freq_system.C
@@ -0,0 +1,105 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/memory/p9_mss_freq_system.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file p9_freq_system.C
+/// @brief Sets synchronous mode an
+///
+// *HWP HWP Owner: Andre A. Marin  <aamarin@us.ibm.com>
+// *HWP FW Owner: Brian Silver <bsilver@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 2
+// *HWP Consumed by: FSP:HB
+#include <vector>
+#include <map>
+
+#include <fapi2.H>
+#include <mss.H>
+
+#include <p9_mss_freq_system.H>
+#include <lib/utils/find.H>
+#include <lib/freq/sync.H>
+
+using fapi2::TARGET_TYPE_SYSTEM;
+using fapi2::TARGET_TYPE_PROC_CHIP;
+using fapi2::TARGET_TYPE_MCBIST;
+using fapi2::TARGET_TYPE_MCS;
+
+extern "C"
+{
+    ///
+    /// @brief Set synchronous mode
+    /// @param[in] i_targets vector of controllers (e.g., MCS)
+    /// @return FAPI2_RC_SUCCESS iff ok
+    ///
+    fapi2::ReturnCode p9_mss_freq_system( const std::vector< fapi2::Target<TARGET_TYPE_MCBIST> >& i_targets )
+    {
+        FAPI_INF("----- In p9_mss_freq_system ----");
+
+        std::map< fapi2::Target<TARGET_TYPE_MCBIST>, uint64_t > l_freq_map;
+        uint32_t l_nest_freq = 0;
+        uint8_t l_required_sync_mode = 0;
+        mss::sync_mode l_mc_in_sync;
+        mss::speed_equality l_equal_dimm_speed;
+
+        // Populate dimm speed map
+        FAPI_TRY( mss::dimm_speed_map(i_targets, l_freq_map, l_equal_dimm_speed),
+                  "Failed to get dimm speed mapping" );
+
+        FAPI_INF("Dimm speed for all MCBISTs are the same : %s",
+                 uint8_t(l_equal_dimm_speed) ? "true" : "false");
+
+        // Get nest freq && F/W attr that tells me if sync mode is required
+        // or if I have to figure that out
+        FAPI_TRY( mss::required_synch_mode(l_required_sync_mode) );
+        FAPI_TRY( FAPI_ATTR_GET( fapi2::ATTR_FREQ_PB, fapi2::Target<TARGET_TYPE_SYSTEM>(), l_nest_freq) );
+
+        FAPI_INF("Retrieved req'd sync mode: %d and nest freq %d", l_required_sync_mode, l_nest_freq);
+
+        // Select SYNCH mode
+        FAPI_TRY( mss::select_sync_mode(l_freq_map,
+                                        l_equal_dimm_speed,
+                                        l_nest_freq,
+                                        l_required_sync_mode,
+                                        l_mc_in_sync) );
+
+        FAPI_INF("Selected SYNC mode : %s", uint8_t(l_mc_in_sync) ? "MC in sync" : "MC NOT in sync");
+
+        // Set attribute
+        for(const auto& l_mcbist : i_targets)
+        {
+            const auto& l_proc_chip = mss::find_target<TARGET_TYPE_PROC_CHIP>(l_mcbist);
+
+            // Cast converts enum class to uint8_t& expected for ATTR_SET
+            FAPI_TRY( FAPI_ATTR_SET( fapi2::ATTR_MC_SYNC_MODE, l_proc_chip, reinterpret_cast<uint8_t(&)>(l_mc_in_sync) ),
+                      "Failed to set ATTR_MC_SYNC_MODE");
+        }
+
+    fapi_try_exit:
+        return fapi2::current_err;
+
+    } // p9_freq_system
+
+} //extern "C"
