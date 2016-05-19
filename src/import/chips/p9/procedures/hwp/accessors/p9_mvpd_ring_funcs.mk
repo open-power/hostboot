b81b09610250ae748f0cd344ea8c5f74dda1504f
Marty Gloff <mgloff@us.ibm.com>
2016-05-19 09:30:00 -0500

    Platform support for FAPI2 winkle/stop ring accessors
    Add getMvpdRing function and supporting utility functions by porting P8
    code and replacing FAPI interfaces with FAPI2 interfaces.
    
    Change-Id: Ice49bb15a5ef71e796c3c24762c852e618ec9d97
    Original-Change-Id: Ied4895f560177a8c68e57ac47db9d9769a04dbdd
    RTC:142622
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/24800
    Tested-by: Jenkins Server
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Claus M. Olsen <cmolsen@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/accessors/p9_mvpd_ring_funcs.mk
@@ -0,0 +1,29 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/import/chips/p9/procedures/hwp/accessors/p9_mvpd_ring_funcs.mk $
+#
+# OpenPOWER HostBoot Project
+#
+# Contributors Listed Below - COPYRIGHT 2016,2019
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+# Include the macros and things for MVPD ring procedures
+PROCEDURE=p9_mvpd_ring_funcs
+$(call ADD_MODULE_INCDIR,$(PROCEDURE),$(ROOTPATH)/chips/p9/utils/imageProcs)
+$(call BUILD_PROCEDURE)
