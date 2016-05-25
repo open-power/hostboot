116d541004c787c15eab84d551594b64dea62888
Joe Dery <dery@us.ibm.com>
2016-05-25 15:10:45 -0400

    update P9 VPD ring names from n10_e9055 RITA scandef
    - initial version from Eric Fluhr/Mike Sgro
    - added n3_np_, n2_psi, expand ob0..3 from e9048+ scandef
    - removed n2_psi_{time,repr} and mc_iomXX_{time,repr} dropped from RITA scandef
    - matched ringIds from p9_ringId.H commit 25695
    * 20160628 further changes from 25695.28 (final): mc_repr 0x76, obX_repr
    * 20160629 further changes from 26388: affected xb_*repr
    - removed RingIdList members ringNameImg and bWcSpace
    
    Change-Id: Id3e150a2fd63036033c8512223ef3de89fcd10a4
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/25027
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/26097
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/utils/imageProcs/p9_ring_identification.C
+++ src/import/chips/p9/utils/imageProcs/p9_ring_identification.C
@@ -20,68 +20,126 @@
 
 const RingIdList RING_ID_LIST_PG[] =
 {
-    /* ringName       ringId  chipletId    ringNameImg            mvpdKeyword    wc */
-    /*                        min   max                                               */
-    {"ab_gptr_ab",     0xA0, 0x08, 0x08, "ab_gptr_ab_ring",     VPD_KEYWORD_PDG, 0},
-    {"ab_gptr_ioa",    0xA1, 0x08, 0x08, "ab_gptr_ioa_ring",    VPD_KEYWORD_PDG, 0},
-    {"ab_gptr_perv",   0xA2, 0x08, 0x08, "ab_gptr_perv_ring",   VPD_KEYWORD_PDG, 0},
-    {"ab_gptr_pll",    0xA3, 0x08, 0x08, "ab_gptr_pll_ring",    VPD_KEYWORD_PDG, 0},
-    {"ab_time",        0xA4, 0x08, 0x08, "ab_time_ring",        VPD_KEYWORD_PDG, 0},
-    {"ex_gptr_core",   0xA5, 0xFF, 0xFF, "ex_gptr_core_ring",   VPD_KEYWORD_PDG, 0},   //Chip specific
-    {"ex_gptr_dpll",   0xA6, 0xFF, 0xFF, "ex_gptr_dpll_ring",   VPD_KEYWORD_PDG, 0},   //Chip specific
-    {"ex_gptr_l2",     0xA7, 0xFF, 0xFF, "ex_gptr_l2_ring",     VPD_KEYWORD_PDG, 0},     //Chip specific
-    {"ex_gptr_l3",     0xA8, 0xFF, 0xFF, "ex_gptr_l3_ring",     VPD_KEYWORD_PDG, 0},     //Chip specific
-    {"ex_gptr_l3refr", 0xA9, 0xFF, 0xFF, "ex_gptr_l3refr_ring", VPD_KEYWORD_PDG, 0}, //Chip specific
-    {"ex_gptr_perv",   0xAA, 0xFF, 0xFF, "ex_gptr_perv_ring",   VPD_KEYWORD_PDG, 0},   //Chip specific
-    {"ex_time_core",   0xAB, 0x10, 0x1F, "ex_time_core_ring",   VPD_KEYWORD_PDG, 0},   //Chiplet specfc
-    {"ex_time_eco",    0xAC, 0x10, 0x1F, "ex_time_eco_ring",    VPD_KEYWORD_PDG, 0},    //Chiplet specfc
-    {"pb_gptr_dmipll", 0xAD, 0x02, 0x02, "pb_gptr_dmipll_ring", VPD_KEYWORD_PDG, 0},
-    {"pb_gptr_mcr",    0xAE, 0x02, 0x02, "pb_gptr_mcr_ring",    VPD_KEYWORD_PDG, 0},
-    {"pb_gptr_nest",   0xAF, 0x02, 0x02, "pb_gptr_nest_ring",   VPD_KEYWORD_PDG, 0},
-    {"pb_gptr_nx",     0xB0, 0x02, 0x02, "pb_gptr_nx_ring",     VPD_KEYWORD_PDG, 0},
-    {"pb_gptr_pcis",   0xB1, 0x02, 0x02, "pb_gptr_pcis_ring",   VPD_KEYWORD_PDG, 0},
-    {"pb_gptr_perv",   0xB2, 0x02, 0x02, "pb_gptr_perv_ring",   VPD_KEYWORD_PDG, 0},
-    {"pb_time",        0xB3, 0x02, 0x02, "pb_time_ring",        VPD_KEYWORD_PDG, 0},
-    {"pb_time_mcr",    0xB4, 0x02, 0x02, "pb_time_mcr_ring",    VPD_KEYWORD_PDG, 0},
-    {"pb_time_nx",     0xB5, 0x02, 0x02, "pb_time_nx_ring",     VPD_KEYWORD_PDG, 0},
-    {"pci_gptr_iopci", 0xB6, 0x09, 0x09, "pci_gptr_iopci_ring", VPD_KEYWORD_PDG, 0},
-    {"pci_gptr_pbf",   0xB7, 0x09, 0x09, "pci_gptr_pbf_ring",   VPD_KEYWORD_PDG, 0},
-    {"pci_gptr_pci0",  0xB8, 0x09, 0x09, "pci_gptr_pci0_ring",  VPD_KEYWORD_PDG, 0},
-    {"pci_gptr_pci1",  0xB9, 0x09, 0x09, "pci_gptr_pci1_ring",  VPD_KEYWORD_PDG, 0},
-    {"pci_gptr_pci2",  0xBA, 0x09, 0x09, "pci_gptr_pci2_ring",  VPD_KEYWORD_PDG, 0},
-    {"pci_gptr_perv",  0xBB, 0x09, 0x09, "pci_gptr_perv_ring",  VPD_KEYWORD_PDG, 0},
-    {"pci_gptr_pll",   0xBC, 0x09, 0x09, "pci_gptr_pll_ring",   VPD_KEYWORD_PDG, 0},
-    {"pci_time",       0xBD, 0x09, 0x09, "pci_time_ring",       VPD_KEYWORD_PDG, 0},
-    {"perv_gptr_net",  0xBE, 0x00, 0x00, "perv_gptr_net_ring",  VPD_KEYWORD_PDG, 0},
-    {"perv_gptr_occ",  0xBF, 0x00, 0x00, "perv_gptr_occ_ring",  VPD_KEYWORD_PDG, 0},
-    {"perv_gptr_perv", 0xC0, 0x00, 0x00, "perv_gptr_perv_ring", VPD_KEYWORD_PDG, 0},
-    {"perv_gptr_pib",  0xC1, 0x00, 0x00, "perv_gptr_pib_ring",  VPD_KEYWORD_PDG, 0},
-    {"perv_gptr_pll",  0xC2, 0x00, 0x00, "perv_gptr_pll_ring",  VPD_KEYWORD_PDG, 0},
-    {"perv_time",      0xC3, 0x00, 0x00, "perv_time_ring",      VPD_KEYWORD_PDG, 0},
-    {"xb_gptr_iopci",  0xC4, 0x04, 0x04, "xb_gptr_iopci_ring",  VPD_KEYWORD_PDG, 0},
-    {"xb_gptr_iox",    0xC5, 0x04, 0x04, "xb_gptr_iox_ring",    VPD_KEYWORD_PDG, 0},
-    {"xb_gptr_pben",   0xC6, 0x04, 0x04, "xb_gptr_pben_ring",   VPD_KEYWORD_PDG, 0},
-    {"xb_gptr_perv",   0xC7, 0x04, 0x04, "xb_gptr_perv_ring",   VPD_KEYWORD_PDG, 0},
-    {"xb_time",        0xC8, 0x04, 0x04, "xb_time_ring",        VPD_KEYWORD_PDG, 0},
-    {"pb_gptr_mcl",    0xC9, 0x02, 0x02, "pb_gptr_mcl_ring",    VPD_KEYWORD_PDG, 0},
-    {"pb_time_mcl",    0xCA, 0x02, 0x02, "pb_time_mcl_ring",    VPD_KEYWORD_PDG, 0},
+    /* ringName           ringId  chipletId  mvpdKeyword    */
+    /*                            min   max                 */
+    {"perv_gptr",          0x01, 0x01, 0x01, VPD_KEYWORD_PDG},
+    {"perv_time",          0x02, 0x01, 0x01, VPD_KEYWORD_PDG},
+    {"occ_gptr",           0x04, 0x01, 0x01, VPD_KEYWORD_PDG},
+    {"occ_time",           0x05, 0x01, 0x01, VPD_KEYWORD_PDG},
+    {"perv_ana_gptr",      0x07, 0x01, 0x01, VPD_KEYWORD_PDG},
+    {"perv_pll_gptr",      0x08, 0x01, 0x01, VPD_KEYWORD_PDG},
+    {"perv_pibnet_gptr",   0x10, 0x01, 0x01, VPD_KEYWORD_PDG},
+    {"perv_pibnet_time",   0x11, 0x01, 0x01, VPD_KEYWORD_PDG},
+    {"n0_gptr",            0x16, 0x02, 0x02, VPD_KEYWORD_PDG},
+    {"n0_time",            0x17, 0x02, 0x02, VPD_KEYWORD_PDG},
+    {"n0_nx_gptr",         0x19, 0x02, 0x02, VPD_KEYWORD_PDG},
+    {"n0_nx_time",         0x1A, 0x02, 0x02, VPD_KEYWORD_PDG},
+    {"n0_cxa0_gptr",       0x1C, 0x02, 0x02, VPD_KEYWORD_PDG},
+    {"n0_cxa0_time",       0x1D, 0x02, 0x02, VPD_KEYWORD_PDG},
+    {"n1_gptr",            0x22, 0x03, 0x03, VPD_KEYWORD_PDG},
+    {"n1_time",            0x23, 0x03, 0x03, VPD_KEYWORD_PDG},
+    {"n1_ioo0_gptr",       0x25, 0x03, 0x03, VPD_KEYWORD_PDG},
+    {"n1_ioo0_time",       0x26, 0x03, 0x03, VPD_KEYWORD_PDG},
+    {"n1_ioo1_gptr",       0x28, 0x03, 0x03, VPD_KEYWORD_PDG},
+    {"n1_ioo1_time",       0x29, 0x03, 0x03, VPD_KEYWORD_PDG},
+    {"n1_mcs23_gptr",      0x2B, 0x03, 0x03, VPD_KEYWORD_PDG},
+    {"n1_mcs23_time",      0x2C, 0x03, 0x03, VPD_KEYWORD_PDG},
+    {"n2_gptr",            0x32, 0x04, 0x04, VPD_KEYWORD_PDG},
+    {"n2_time",            0x33, 0x04, 0x04, VPD_KEYWORD_PDG},
+    {"n2_cxa1_gptr",       0x35, 0x04, 0x04, VPD_KEYWORD_PDG},
+    {"n2_cxa1_time",       0x36, 0x04, 0x04, VPD_KEYWORD_PDG},
+    {"n2_psi_gptr",        0x38, 0x04, 0x04, VPD_KEYWORD_PDG},
+    {"n3_gptr",            0x3F, 0x05, 0x05, VPD_KEYWORD_PDG},
+    {"n3_time",            0x40, 0x05, 0x05, VPD_KEYWORD_PDG},
+    {"n3_mcs01_gptr",      0x42, 0x05, 0x05, VPD_KEYWORD_PDG},
+    {"n3_mcs01_time",      0x43, 0x05, 0x05, VPD_KEYWORD_PDG},
+    {"n3_np_gptr",         0x45, 0x05, 0x05, VPD_KEYWORD_PDG},
+    {"n3_np_time",         0x46, 0x05, 0x05, VPD_KEYWORD_PDG},
+    {"xb_gptr",            0x4C, 0x06, 0x06, VPD_KEYWORD_PDG},
+    {"xb_time",            0x4D, 0x06, 0x06, VPD_KEYWORD_PDG},
+    {"xb_io0_gptr",        0x4F, 0x06, 0x06, VPD_KEYWORD_PDG},
+    {"xb_io0_time",        0x50, 0x06, 0x06, VPD_KEYWORD_PDG},
+    {"xb_io1_gptr",        0x52, 0x06, 0x06, VPD_KEYWORD_PDG},
+    {"xb_io1_time",        0x53, 0x06, 0x06, VPD_KEYWORD_PDG},
+    {"xb_io2_gptr",        0x55, 0x06, 0x06, VPD_KEYWORD_PDG},
+    {"xb_io2_time",        0x56, 0x06, 0x06, VPD_KEYWORD_PDG},
+    {"xb_pll_gptr",        0x57, 0x06, 0x06, VPD_KEYWORD_PDG},
+    {"mc_gptr",            0x66, 0x07, 0xFF, VPD_KEYWORD_PDG},  //0x07, 0x08: multicast group 2
+    {"mc_time",            0x67, 0x07, 0xFF, VPD_KEYWORD_PDG},  //0x07, 0x08: multicast group 2
+    {"mc_iom01_gptr",      0x69, 0x07, 0xFF, VPD_KEYWORD_PDG},  //0x07, 0x08: multicast group 2
+    {"mc_iom23_gptr",      0x6C, 0x07, 0xFF, VPD_KEYWORD_PDG},  //0x07, 0x08: multicast group 2
+    {"mc_pll_gptr",        0x6E, 0x07, 0xFF, VPD_KEYWORD_PDG},  //0x07, 0x08: multicast group 2
+    {"ob0_gptr",           0x7C, 0x09, 0x09, VPD_KEYWORD_PDG},
+    {"ob0_time",           0x7D, 0x09, 0x09, VPD_KEYWORD_PDG},
+    {"ob0_pll_gptr",       0x7E, 0x09, 0x09, VPD_KEYWORD_PDG},
+    {"ob1_gptr",           0x8A, 0x0A, 0x0A, VPD_KEYWORD_PDG},
+    {"ob1_time",           0x8B, 0x0A, 0x0A, VPD_KEYWORD_PDG},
+    {"ob1_pll_gptr",       0x8C, 0x0A, 0x0A, VPD_KEYWORD_PDG},
+    {"ob2_gptr",           0x98, 0x0B, 0x0B, VPD_KEYWORD_PDG},
+    {"ob2_time",           0x99, 0x0B, 0x0B, VPD_KEYWORD_PDG},
+    {"ob2_pll_gptr",       0x9A, 0x0B, 0x0B, VPD_KEYWORD_PDG},
+    {"ob3_gptr",           0xA6, 0x0C, 0x0C, VPD_KEYWORD_PDG},
+    {"ob3_time",           0xA7, 0x0C, 0x0C, VPD_KEYWORD_PDG},
+    {"ob3_pll_gptr",       0xA8, 0x0C, 0x0C, VPD_KEYWORD_PDG},
+    {"pci0_gptr",          0xB4, 0x0D, 0x0D, VPD_KEYWORD_PDG},
+    {"pci0_time",          0xB5, 0x0D, 0x0D, VPD_KEYWORD_PDG},
+    {"pci0_pll_gptr",      0xB7, 0x0D, 0x0D, VPD_KEYWORD_PDG},
+    {"pci1_gptr",          0xBA, 0x0E, 0x0E, VPD_KEYWORD_PDG},
+    {"pci1_time",          0xBB, 0x0E, 0x0E, VPD_KEYWORD_PDG},
+    {"pci1_pll_gptr",      0xBD, 0x0E, 0x0E, VPD_KEYWORD_PDG},
+    {"pci2_gptr",          0xC0, 0x0F, 0x0F, VPD_KEYWORD_PDG},
+    {"pci2_time",          0xC1, 0x0F, 0x0F, VPD_KEYWORD_PDG},
+    {"pci2_pll_gptr",      0xC3, 0x0F, 0x0F, VPD_KEYWORD_PDG},
+    {"eq_gptr",            0xC6, 0x10, 0xFF, VPD_KEYWORD_PDG},  // x10,x15: multicast group 4
+    {"eq_time",            0xC7, 0x10, 0xFF, VPD_KEYWORD_PDG},  // x10,x15: multicast group 4
+    {"ex_l3_gptr",         0xCA, 0x10, 0xFF, VPD_KEYWORD_PDG},  // x10,x1B: multicast groups 5 and 6
+    {"ex_l3_time",         0xCB, 0x10, 0xFF, VPD_KEYWORD_PDG},  // x10,x1B: multicast groups 5 and 6
+    {"ex_l2_gptr",         0xCE, 0x10, 0xFF, VPD_KEYWORD_PDG},  // x10,x1B: multicast groups 5 and 6
+    {"ex_l2_time",         0xCF, 0x10, 0xFF, VPD_KEYWORD_PDG},  // x10,x1B: multicast groups 5 and 6
+    {"ex_l3_refr_gptr",    0xD1, 0x10, 0xFF, VPD_KEYWORD_PDG},  // x10,x1B: multicast groups 5 and 6
+    {"ex_l3_refr_time",    0xD2, 0x10, 0x15, VPD_KEYWORD_PDG},
+    {"eq_ana_gptr",        0xD4, 0x10, 0xFF, VPD_KEYWORD_PDG},  // x10,x15: multicast group 4
+    {"eq_dpll_gptr",       0xD6, 0x10, 0xFF, VPD_KEYWORD_PDG},  // x10,x15: multicast group 4
+    {"ec_gptr",            0xDF, 0x20, 0xFF, VPD_KEYWORD_PDG},  // x20,x37: multicast group 1
+    {"ec_time",            0xE0, 0x20, 0xFF, VPD_KEYWORD_PDG},  // x20,x37: multicast group 1
 };
 
 const RingIdList RING_ID_LIST_PR[] =
 {
-    /*   ringName      ringId chipIdMin chipIdMax     ringNameImg        mvpdKeyword   */
-    {"ab_repr",        0xE0, 0x08, 0x08, "ab_repr_ring",        VPD_KEYWORD_PDR, 0},
-    {"ex_repr_core",   0xE1, 0x10, 0x1F, "ex_repr_core_ring",   VPD_KEYWORD_PDR, 1},
-    {"ex_repr_eco",    0xE2, 0x10, 0x1F, "ex_repr_eco_ring",    VPD_KEYWORD_PDR, 1},
-    {"pb_repr",        0xE3, 0x02, 0x02, "pb_repr_ring",        VPD_KEYWORD_PDR, 0},
-    {"pb_repr_mcr",    0xE4, 0x02, 0x02, "pb_repr_mcr_ring",    VPD_KEYWORD_PDR, 0},
-    {"pb_repr_nx",     0xE5, 0x02, 0x02, "pb_repr_nx_ring",     VPD_KEYWORD_PDR, 0},
-    {"pci_repr",       0xE6, 0x09, 0x09, "pci_repr_ring",       VPD_KEYWORD_PDR, 0},
-    {"perv_repr",      0xE7, 0x00, 0x00, "perv_repr_ring",      VPD_KEYWORD_PDR, 0},
-    {"perv_repr_net",  0xE8, 0x00, 0x00, "perv_repr_net_ring",  VPD_KEYWORD_PDR, 0},
-    {"perv_repr_pib",  0xE9, 0x00, 0x00, "perv_repr_pib_ring",  VPD_KEYWORD_PDR, 0},
-    {"xb_repr",        0xEA, 0x04, 0x04, "xb_repr_ring",        VPD_KEYWORD_PDR, 0},
-    {"pb_repr_mcl",    0xEB, 0x02, 0x02, "pb_repr_mcl_ring",    VPD_KEYWORD_PDR, 0},
+    /* ringName           ringId  chipletId  mvpdKeyword    */
+    /*                            min   max                 */
+    {"perv_repr",          0x12, 0x01, 0x01, VPD_KEYWORD_PDR},
+    {"occ_repr",           0x13, 0x01, 0x01, VPD_KEYWORD_PDR},
+    {"perv_pibnet_repr",   0x14, 0x01, 0x01, VPD_KEYWORD_PDR},
+    {"n0_repr",            0x1E, 0x02, 0x02, VPD_KEYWORD_PDR},
+    {"n0_nx_repr",         0x1F, 0x02, 0x02, VPD_KEYWORD_PDR},
+    {"n0_cxa0_repr",       0x20, 0x02, 0x02, VPD_KEYWORD_PDR},
+    {"n1_repr",            0x2D, 0x03, 0x03, VPD_KEYWORD_PDR},
+    {"n1_ioo0_repr",       0x2E, 0x03, 0x03, VPD_KEYWORD_PDR},
+    {"n1_ioo1_repr",       0x2F, 0x03, 0x03, VPD_KEYWORD_PDR},
+    {"n1_mcs23_repr",      0x30, 0x03, 0x03, VPD_KEYWORD_PDR},
+    {"n2_repr",            0x3A, 0x04, 0x04, VPD_KEYWORD_PDR},
+    {"n2_cxa1_repr",       0x3B, 0x04, 0x04, VPD_KEYWORD_PDR},
+    {"n3_repr",            0x47, 0x05, 0x05, VPD_KEYWORD_PDR},
+    {"n3_mcs01_repr",      0x48, 0x05, 0x05, VPD_KEYWORD_PDR},
+    {"n3_np_repr",         0x49, 0x05, 0x05, VPD_KEYWORD_PDR},
+    {"xb_repr",            0x5F, 0x06, 0x06, VPD_KEYWORD_PDR},
+    {"xb_io0_repr",        0x60, 0x06, 0x06, VPD_KEYWORD_PDR},
+    {"xb_io1_repr",        0x61, 0x06, 0x06, VPD_KEYWORD_PDR},
+    {"xb_io2_repr",        0x62, 0x06, 0x06, VPD_KEYWORD_PDR},
+    {"mc_repr",            0x76, 0x07, 0x08, VPD_KEYWORD_PDR},
+    {"ob0_repr",           0x86, 0x09, 0x09, VPD_KEYWORD_PDR},
+    {"ob1_repr",           0x94, 0x0A, 0x0A, VPD_KEYWORD_PDR},
+    {"ob2_repr",           0xA2, 0x0B, 0x0B, VPD_KEYWORD_PDR},
+    {"ob3_repr",           0xB0, 0x0C, 0x0C, VPD_KEYWORD_PDR},
+    {"pci0_repr",          0xB8, 0x0D, 0x0D, VPD_KEYWORD_PDR},
+    {"pci1_repr",          0xBE, 0x0E, 0x0E, VPD_KEYWORD_PDR},
+    {"pci2_repr",          0xC4, 0x0F, 0x0F, VPD_KEYWORD_PDR},
+    {"eq_repr",            0xDA, 0x10, 0x15, VPD_KEYWORD_PDR},
+    {"ex_l3_repr",         0xDB, 0x10, 0x1B, VPD_KEYWORD_PDR},
+    {"ex_l2_repr",         0xDC, 0x10, 0x1B, VPD_KEYWORD_PDR},
+    {"ex_l3_refr_repr",    0xDD, 0x10, 0x1B, VPD_KEYWORD_PDR},
+    {"ec_repr",            0xE2, 0x20, 0x37, VPD_KEYWORD_PDR},
 };
 
 const uint32_t RING_ID_LIST_PG_SIZE = sizeof(RING_ID_LIST_PG) / sizeof(
@@ -127,8 +185,7 @@ int get_vpd_ring_list_entry(const char* i_ringName,
             // Search for ringName match.
             for (iRing = 0; iRing < ring_id_list_size; iRing++)
             {
-                if ( strcmp((ring_id_list + iRing)->ringName,   i_ringName) == 0  ||
-                     strcmp((ring_id_list + iRing)->ringNameImg, i_ringName) == 0 )
+                if ( strcmp((ring_id_list + iRing)->ringName,   i_ringName) == 0  )
                 {
                     *i_ringIdList = ring_id_list + iRing;
                     return FOUND;
