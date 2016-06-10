dbeea9f0230754aa3419805ed37f302d14a6b6a7
Abhishek Agarwal <abagarw8@in.ibm.com>
2016-06-10 08:49:49 +0200

    Level 1 HWP p9_getecid
    Change-Id: I5ad9c6b693682de1bafb436dac1df0d273b01e97
    Original-Change-Id: I09de2a7a05890f9bb23244989d1a2dc22026e62c
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/25608
    Tested-by: Jenkins Server
    Reviewed-by: PARVATHI RACHAKONDA <prachako@in.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/perv/p9_getecid.C
@@ -0,0 +1,52 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/perv/p9_getecid.C $        */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//------------------------------------------------------------------------------
+/// @file  p9_getecid.C
+///
+/// @brief Get ECID string from target using SCOM
+//------------------------------------------------------------------------------
+// *HWP HW Owner        : Abhishek Agarwal <abagarw8@in.ibm.com>
+// *HWP HW Backup Owner : Srinivas V Naga <srinivan@in.ibm.com>
+// *HWP FW Owner        : sunil kumar <skumar8j@in.ibm.com>
+// *HWP Team            : Perv
+// *HWP Level           : 1
+// *HWP Consumed by     : SBE
+//------------------------------------------------------------------------------
+
+
+//## auto_generated
+#include "p9_getecid.H"
+
+
+fapi2::ReturnCode p9_getecid(const
+                             fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip, const fapi2::variable_buffer& o_fuseString)
+{
+    FAPI_INF("Entering ...");
+
+    FAPI_INF("Exiting ...");
+
+    return fapi2::FAPI2_RC_SUCCESS;
+
+}
