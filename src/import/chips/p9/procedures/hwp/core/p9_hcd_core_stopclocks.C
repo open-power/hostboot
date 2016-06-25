5dddd1788345907e1f6dd2c544d31b84026320fb
Yue Du <daviddu@us.ibm.com>
2016-06-24 23:58:31 -0500

    CORE/CACHE: add Level1 cache/l2/core stopclocks procedures
    Change-Id: I9c7bc5470e09fa456a68b59122be4ab94a6e8e12
    Original-Change-Id: Id407ffa51ea9a7fb9a0056a7faaf1e33e4433e50
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/26276
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Soma Bhanutej <soma.bhanu@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/27581
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/core/p9_hcd_core_stopclocks.C
@@ -0,0 +1,61 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: chips/p9/procedures/hwp/core/p9_hcd_core_stopclocks.C $       */
+/*                                                                        */
+/* IBM CONFIDENTIAL                                                       */
+/*                                                                        */
+/* EKB Project                                                            */
+/*                                                                        */
+/* COPYRIGHT 2015,2016                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* The source code for this program is not published or otherwise         */
+/* divested of its trade secrets, irrespective of what has been           */
+/* deposited with the U.S. Copyright Office.                              */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file  p9_hcd_core_stopclocks.C
+/// @brief Core Clock Stop
+///
+/// Procedure Summary:
+
+// *HWP HWP Owner          : David Du       <daviddu@us.ibm.com>
+// *HWP Backup HWP Owner   : Greg Still     <stillgs@us.ibm.com>
+// *HWP FW Owner           : Sangeetha T S  <sangeet2@in.ibm.com>
+// *HWP Team               : PM
+// *HWP Consumed by        : HB:PREV
+// *HWP Level              : 2
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+
+#include <p9_misc_scom_addresses.H>
+#include <p9_quad_scom_addresses.H>
+#include <p9_hcd_common.H>
+#include "p9_hcd_core_stopclocks.H"
+
+//------------------------------------------------------------------------------
+// Constant Definitions
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Procedure: Core Clock Stop
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p9_hcd_core_stopclocks(
+    const fapi2::Target<fapi2::TARGET_TYPE_CORE>& i_target)
+{
+    FAPI_INF(">>p9_hcd_core_stopclocks");
+
+    FAPI_INF("<<p9_hcd_core_stopclocks");
+    return fapi2::FAPI2_RC_SUCCESS;
+}
+
+
+
+
