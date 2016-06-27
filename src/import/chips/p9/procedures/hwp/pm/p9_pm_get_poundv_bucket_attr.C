6cc7799e780bba4f05eac31a2bb7ea8d676b27e8
Andres Lugo-Reyes <aalugore@us.ibm.com>
2016-06-27 13:07:56 -0500

    Modify #V accessor function for new version 3
    Change-Id: I873cd9d8abd07aa73d6135653bd840569c49074f
    Original-Change-Id: I358109f01732fdb4d75409693d5a1866bcd57866
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/26308
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
    Reviewed-by: Andrew J. Geissler <andrewg@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/27431
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/pm/p9_pm_get_poundv_bucket_attr.C
+++ src/import/chips/p9/procedures/hwp/pm/p9_pm_get_poundv_bucket_attr.C
@@ -25,6 +25,7 @@
 // Includes
 // ----------------------------------------------------------------------
 #include <p9_pm_get_poundv_bucket_attr.H>
+#include <p9_pm_get_poundv_bucket.H>
 #include <mvpd_access_defs.H>
 #include <attribute_ids.H>
 
@@ -32,147 +33,80 @@ fapi2::ReturnCode p9_pm_get_poundv_bucket_attr(
     const fapi2::Target<fapi2::TARGET_TYPE_EQ>& i_target,
     uint8_t* o_data)
 {
-    FAPI_IMP("Entering p9_pm_get_poundv_bucket_attr ....");
+    FAPI_DBG("Entering p9_pm_get_poundv_bucket_attr ....");
     uint8_t* l_prDataPtr = NULL;
     uint8_t* l_fullVpdData = NULL;
     uint8_t l_overridePresent = 0;
+    uint32_t l_tempVpdSize = 0;
     uint32_t l_vpdSize = 0;
     uint8_t l_eqChipUnitPos = 0;
     uint8_t l_bucketId;
-    fapi2::ReturnCode l_rc = fapi2::FAPI2_RC_SUCCESS;
+    uint8_t l_bucketSize = 0;
+    uint32_t l_sysNestFreq = 0;
+    fapi2::voltageBucketData_t* l_currentBucket = NULL;
+    uint8_t l_numMatches = 0;
 
-    do
+    fapi2::MvpdRecord lrpRecord = fapi2::MVPD_RECORD_LAST;
+    //To read MVPD we will need the proc parent of the inputted EQ target
+    fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> l_procParent =
+        i_target.getParent<fapi2::TARGET_TYPE_PROC_CHIP>();
+
+
+    //Need to determine which LRP record to read from depending on which
+    //bucket we are getting the power management data from. FapiPos will
+    //tell us which LRP record to use.
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS,
+                           i_target,
+                           l_eqChipUnitPos));
+
+    FAPI_ASSERT( l_eqChipUnitPos < NUM_BUCKETS ,
+                 fapi2::INVALID_EQ_CHIP_POS().
+                 set_EQ_POSITION( l_eqChipUnitPos ),
+                 "Invalid EQ chip unit position = 0x%X",
+                 l_eqChipUnitPos);
+
+    //The enumeration for the LRPx records are just 3 more
+    //than the EQ chip unit pos. See mvpd_access_defs.H
+    lrpRecord = static_cast<fapi2::MvpdRecord>(l_eqChipUnitPos +
+                fapi2::MVPD_RECORD_LRP0 );
+
+
+    //check if bucket num has been overriden
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_POUNDV_BUCKET_NUM_OVERRIDE,
+                           i_target,
+                           l_overridePresent));
+
+    if(l_overridePresent != 0)
     {
-        //To read MVPD we will need the proc parent of the inputted EQ target
-        fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> l_procParent =
-            i_target.getParent<fapi2::TARGET_TYPE_PROC_CHIP>();
-
-        //check if bucket num has been overriden
-        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_POUNDV_BUCKET_NUM_OVERRIDE,
+        //If it has been overriden then get the override
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_POUNDV_BUCKET_NUM,
                                i_target,
-                               l_overridePresent));
-
-        if(l_overridePresent != 0)
-        {
-            //If it has been overriden then get the override
-            FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_POUNDV_BUCKET_NUM,
-                                   i_target,
-                                   l_bucketId));
-        }
-        else
-        {
-            //Otherwise get the bucket num from MVPD data
-            //First read is to get size of vpd record, note the o_buffer is NULL
-            l_rc = getMvpdField(fapi2::MVPD_RECORD_VINI,
-                                fapi2::MVPD_KEYWORD_PR,
-                                l_procParent,
-                                NULL,
-                                l_vpdSize);
-
-            if(l_rc)
-            {
-                FAPI_ERR("p9_pm_get_poundv_bucket_attr:: Error reading PR keyword size from VINI record");
-                break;
-            }
-
-            l_prDataPtr = reinterpret_cast<uint8_t*>(malloc(l_vpdSize));
-
-            //Second read is to get data of vpd record
-            l_rc = getMvpdField(fapi2::MVPD_RECORD_VINI,
-                                fapi2::MVPD_KEYWORD_PR,
-                                l_procParent,
-                                l_prDataPtr,
-                                l_vpdSize);
-
-            if(l_rc)
-            {
-                FAPI_ERR("p9_pm_get_poundv_bucket_attr:: Error reading PR keyword from VINI record");
-                break;
-            }
-
-            //Bucket ID is byte[4] of the PR keyword
-            memcpy(&l_bucketId, (l_prDataPtr + 4), sizeof(uint8_t));
-        }
-
-
-        //Need to determine which LRP record to read from depending on which
-        //bucket we are getting the power management data from. FapiPos will
-        //tell us which LRP record to use.
-        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_UNIT_POS,
-                               i_target,
-                               l_eqChipUnitPos));
-        fapi2::MvpdRecord lprRecord = fapi2::MVPD_RECORD_LRP0;
-
-        switch(l_eqChipUnitPos)
-        {
-            case 0:
-                lprRecord = fapi2::MVPD_RECORD_LRP0;
-                break;
-
-            case 1:
-                lprRecord = fapi2::MVPD_RECORD_LRP1;
-                break;
-
-            case 2:
-                lprRecord = fapi2::MVPD_RECORD_LRP2;
-                break;
-
-            case 3:
-                lprRecord = fapi2::MVPD_RECORD_LRP3;
-                break;
-
-            case 4:
-                lprRecord = fapi2::MVPD_RECORD_LRP4;
-                break;
-
-            case 5:
-                lprRecord = fapi2::MVPD_RECORD_LRP5;
-                break;
-
-            default:
-                FAPI_ERR("No LRP record found for EQ with fapi pos = %d", l_eqChipUnitPos);
-                fapi2::Assert(0);
-                break;
-        }
-
-        //Reset VPD size because we want to find size of another VPD record
-        l_vpdSize = 0;
-
+                               l_bucketId));
+    }
+    else
+    {
         //First read is to get size of vpd record, note the o_buffer is NULL
-        l_rc = getMvpdField(lprRecord,
-                            fapi2::MVPD_KEYWORD_PDV,
-                            l_procParent,
-                            NULL,
-                            l_vpdSize);
+        FAPI_TRY( getMvpdField(lrpRecord,
+                               fapi2::MVPD_KEYWORD_PDV,
+                               l_procParent,
+                               NULL,
+                               l_tempVpdSize) );
 
-        if(l_rc)
-        {
-            FAPI_ERR("p9_pm_get_poundv_bucket_attr:: Error reading PDV keyword size from LRP%d record", l_eqChipUnitPos);
-            break;
-        }
 
+        //save off the actual vpd size
+        l_vpdSize = l_tempVpdSize;
         //Allocate memory for vpd data
-        l_fullVpdData = reinterpret_cast<uint8_t*>(malloc(l_vpdSize));
+        l_fullVpdData = reinterpret_cast<uint8_t*>(malloc(l_tempVpdSize));
 
-        FAPI_ASSERT(l_vpdSize - 4 - ((l_bucketId - 1) * 0x33) >= sizeof(fapi2::ATTR_POUNDV_BUCKET_DATA_Type),
-                    fapi2::BAD_VPD_READ()
-                    .set_EXPECTED_SIZE(sizeof(fapi2::ATTR_POUNDV_BUCKET_DATA_Type))
-                    .set_ACTUAL_SIZE(l_vpdSize - 4 - ((l_bucketId - 1) * 0x33)),
-                    "#V data read was too small!" );
 
         //Second read is to get data of vpd record
-        l_rc = getMvpdField(lprRecord,
-                            fapi2::MVPD_KEYWORD_PDV,
-                            l_procParent,
-                            l_fullVpdData,
-                            l_vpdSize);
+        FAPI_TRY( getMvpdField(lrpRecord,
+                               fapi2::MVPD_KEYWORD_PDV,
+                               l_procParent,
+                               l_fullVpdData,
+                               l_tempVpdSize) );
 
-        if(l_rc)
-        {
-            FAPI_ERR("p9_pm_get_poundv_bucket_attr:: Error reading PDV keyword from LRP%d record", l_eqChipUnitPos);
-            break;
-        }
-
+        //Version 2:
         //#V record is laid out as follows:
         //Name:     0x2 byte
         //Length:   0x2 byte
@@ -184,46 +118,160 @@ fapi2::ReturnCode p9_pm_get_poundv_bucket_attr(
         //bucket d: 0x33 byte
         //bucket e: 0x33 byte
         //bucket f: 0x33 byte
-        if( *l_fullVpdData == 0x2)
+        if( *l_fullVpdData == POUNDV_VERSION_2)
         {
-            memcpy(o_data,
-                   l_fullVpdData + 4 + (l_bucketId - 1) * 0x33,
-                   sizeof(fapi2::ATTR_POUNDV_BUCKET_DATA_Type));
+            //Set the size of the bucket
+            l_bucketSize = VERSION_2_BUCKET_SIZE;
+
+            //Reset VPD size because we want to find size of another VPD record
+            l_tempVpdSize = 0;
+
+            //First read is to get size of vpd record, note the o_buffer is NULL
+            FAPI_TRY( getMvpdField(fapi2::MVPD_RECORD_VINI,
+                                   fapi2::MVPD_KEYWORD_PR,
+                                   l_procParent,
+                                   NULL,
+                                   l_tempVpdSize) );
+
+            l_prDataPtr = reinterpret_cast<uint8_t*>(malloc(l_tempVpdSize));
+
+            //Second read is to get data of vpd record
+            FAPI_TRY( getMvpdField(fapi2::MVPD_RECORD_VINI,
+                                   fapi2::MVPD_KEYWORD_PR,
+                                   l_procParent,
+                                   l_prDataPtr,
+                                   l_tempVpdSize) );
+
+            //Bucket ID is byte[4] of the PR keyword
+            memcpy(&l_bucketId, (l_prDataPtr + 4), sizeof(uint8_t));
+
+        }
+        //Version 3:
+        //#V record is laid out as follows:
+        //Name:     0x2 byte
+        //Length:   0x2 byte
+        //Version:  0x1 byte **buffer starts here
+        //PNP:      0x3 byte
+        //bucket a: 0x3D byte
+        //bucket b: 0x3D byte
+        //bucket c: 0x3D byte
+        //bucket d: 0x3D byte
+        //bucket e: 0x3D byte
+        //bucket f: 0x3D byte
+        else if( *l_fullVpdData == POUNDV_VERSION_3 )
+        {
+            // Set the size of the bucket
+            l_bucketSize = VERSION_3_BUCKET_SIZE;
+
+            // Version 3 uses the nest frequency to choose the bucket Id
+            // get the system target to find the NEST_FREQ_MHZ
+            fapi2::Target<fapi2::TARGET_TYPE_SYSTEM> l_sysParent;
+
+
+            FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_FREQ_PB_MHZ,
+                                   l_sysParent,
+                                   l_sysNestFreq));
+            //cast the voltage data into an array of buckets
+            fapi2::voltageBucketData_t* l_buckets = reinterpret_cast
+                                                    <fapi2::voltageBucketData_t*>
+                                                    (l_fullVpdData + POUNDV_BUCKET_OFFSET);
+
+            for(int i = 0; i < NUM_BUCKETS; i++)
+            {
+                if(l_buckets[i].pbFreq == l_sysNestFreq)
+                {
+                    l_numMatches++;
+
+                    /*
+                     * TODO RTC: 157341
+                     * Modify function to fail if multiple buckets have same nest
+                     */
+                    if(l_numMatches > 1)
+                    {
+                        FAPI_ERR("p9_pm_get_poundv_bucket_attr::"
+                                 " Multiple buckets (%d) reporting the same nest frequency",
+                                 l_numMatches);
+                    }
+
+                    /*
+                    else
+                    {
+                        l_currentBucket = &l_buckets[i];
+                    }
+                    */
+                    l_currentBucket = &l_buckets[i];
+                }
+            }
+
+            /*
+             * TODO RTC: 157341
+             * Modify function to fail if multiple buckets have same nest
+             if(l_numMatches == 1)
+             {
+                 l_bucketId = l_currentBucket->bucketId;
+             }
+             else
+             {
+
+                 FAPI_ERR("p9_pm_get_poundv_bucket_attr::Invalid number of matching nest freqs found. Matches found = %d",
+                          l_numMatches );
+                 FAPI_ASSERT(0,
+                             fapi2::INVALID_MATCHING_FREQ_NUMBER().
+                             set_MATCHES_FOUND(l_numMatches),
+                             "Matches found is NOT 1" );
+             }
+             */
+            l_bucketId = l_currentBucket->bucketId;
         }
         else
         {
-            FAPI_ERR("p9_pm_get_poundv_bucket_attr::Invalid #V record version: 0x%x", *l_fullVpdData);
-            FAPI_ASSERT(0,
-                        fapi2::INVALID_POUNDV_VERSION()
-                        .set_POUNDV_VERSION(*l_fullVpdData),
-                        "#V is of an invalid version!" );
+            FAPI_ASSERT( false,
+                         fapi2::INVALID_POUNDV_VERSION()
+                         .set_POUNDV_VERSION(*l_fullVpdData),
+                         "p9_pm_get_poundv_bucket_attr::Invalid #V record version: 0x%x",
+                         *l_fullVpdData);
         }
 
+        // This assert ensures the size of the calculated data is correct
+        FAPI_ASSERT(l_vpdSize - POUNDV_BUCKET_OFFSET - ((l_bucketId - 1) *
+                    l_bucketSize) >= l_bucketSize,
+                    fapi2::BAD_VPD_READ().set_EXPECTED_SIZE(sizeof(l_bucketSize))
+                    .set_ACTUAL_SIZE(l_vpdSize - POUNDV_BUCKET_OFFSET -
+                                     ((l_bucketId - 1) * l_bucketSize)),
+                    "#V data read was too small!" );
 
-    }
-    while(0);
+    }// else no override
+
+    // Ensure we got a valid bucket id
+    // NOTE: Bucket IDs range from 1-6
+    FAPI_ASSERT( (l_bucketId <= NUM_BUCKETS) && (l_bucketId != 0),
+                 fapi2::INVALID_BUCKET_ID()
+                 .set_TARGET(i_target)
+                 .set_BUCKET_ID(l_bucketId),
+                 "Invalid Bucket Id = %d",
+                 l_bucketId );
+
+
+    // Use the selected bucket id to populate the output data
+    memcpy(o_data,
+           l_fullVpdData + POUNDV_BUCKET_OFFSET + (l_bucketId - 1) * l_bucketSize,
+           l_bucketSize);
 
 fapi_try_exit:
 
     if(l_fullVpdData != NULL)
     {
         free(l_fullVpdData);
+        l_fullVpdData = NULL;
     }
 
     if(l_prDataPtr != NULL)
     {
         free(l_prDataPtr);
+        l_prDataPtr = NULL;
     }
 
-    FAPI_IMP("Exiting p9_pm_get_poundv_bucket_attr ....");
-
-    //If there is no issue in the current err, check
-    //the local rc to see if the mvpd access methods caught anything
-    if((fapi2::current_err == fapi2::FAPI2_RC_SUCCESS) &&
-       (l_rc != fapi2::FAPI2_RC_SUCCESS))
-    {
-        fapi2::current_err = l_rc;
-    }
+    FAPI_DBG("Exiting p9_pm_get_poundv_bucket_attr ....");
 
     return fapi2::current_err;
 }
