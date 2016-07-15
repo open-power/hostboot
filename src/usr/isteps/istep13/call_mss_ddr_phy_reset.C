1073b2b23651fc2683d91add819213eab8b5b21e
crgeddes <crgeddes@us.ibm.com>
2016-07-15 08:53:18 -0500

    Remove IS_SIMULATION hack needed for memory hwps
    Change-Id: Icf4e2511097d3834f4425fa0a9d3ac83d23ccd72
    RTC:155373
    CMVC-Coreq: 1000150
    CMVC-Prereq: 1005045
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/27086
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
    Reviewed-by: Brian R. Silver <bsilver@us.ibm.com>
    Reviewed-by: Martin Gloff <mgloff@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep13/call_mss_ddr_phy_reset.C
+++ src/usr/isteps/istep13/call_mss_ddr_phy_reset.C
@@ -58,9 +58,6 @@ void* call_mss_ddr_phy_reset (void *io_pArgs)
     TARGETING::Target * sys = NULL;
     TARGETING::targetService().getTopLevelTarget( sys );
 
-//  TODO: RTC 155373 Need to remove hack that is setting IS_SIMULATION to 1 for this substep
-    sys->setAttr<TARGETING::ATTR_IS_SIMULATION>(1);
-
     TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
                           "call_mss_ddr_phy_reset entry" );
 
@@ -103,9 +100,6 @@ void* call_mss_ddr_phy_reset (void *io_pArgs)
         }
 
     } // end l_mcbist loop
-
-//  TODO: RTC 155373 Need to remove hack that is setting IS_SIMULATION to 1 for this substep
-    sys->setAttr<TARGETING::ATTR_IS_SIMULATION>(0);
 
     TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
             "call_mss_ddr_phy_reset exit" );
