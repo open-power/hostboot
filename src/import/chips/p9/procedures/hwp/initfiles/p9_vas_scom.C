a0fdd124e5625b1ffef5608d206c5331f2865c67
Juan Medina <jr.medina@us.ibm.com>
2016-07-22 14:02:54 -0500

    Changes based on Jenny's review of ffdc
    Change-Id: I5ba474b49ea4ee9d16b683af8e72553c26c6af89
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/27380
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Jenny Huynh <jhuynh@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/initfiles/p9_vas_scom.C
+++ src/import/chips/p9/procedures/hwp/initfiles/p9_vas_scom.C
@@ -29,9 +29,9 @@
 
 using namespace fapi2;
 
-constexpr auto literal_0x000001000001FFFF = 0x000001000001FFFF;
-constexpr auto literal_0xFF00FDFC0003FFFF = 0xFF00FDFC0003FFFF;
-constexpr auto literal_0xFFFFFEFFFFFF8000 = 0xFFFFFEFFFFFF8000;
+constexpr auto literal_0x00000100000D7FFF = 0x00000100000D7FFF;
+constexpr auto literal_0x0000000000000FFF = 0x0000000000000FFF;
+constexpr auto literal_0x00FF0203C00C0FFF = 0x00FF0203C00C0FFF;
 constexpr auto literal_0x1 = 0x1;
 
 fapi2::ReturnCode p9_vas_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& TGT0,
@@ -52,7 +52,7 @@ fapi2::ReturnCode p9_vas_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
             }
 
             {
-                l_scom_buffer.insert<uint64_t> (literal_0x000001000001FFFF, 0, 54, 0 );
+                l_scom_buffer.insert<uint64_t> (literal_0x00000100000D7FFF, 0, 54, 0 );
             }
 
             l_rc = fapi2::putScom(TGT0, 0x3011803ull, l_scom_buffer);
@@ -73,7 +73,7 @@ fapi2::ReturnCode p9_vas_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
             }
 
             {
-                l_scom_buffer.insert<uint64_t> (literal_0xFF00FDFC0003FFFF, 0, 54, 0 );
+                l_scom_buffer.insert<uint64_t> (literal_0x0000000000000FFF, 0, 54, 0 );
             }
 
             l_rc = fapi2::putScom(TGT0, 0x3011806ull, l_scom_buffer);
@@ -94,7 +94,7 @@ fapi2::ReturnCode p9_vas_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
             }
 
             {
-                l_scom_buffer.insert<uint64_t> (literal_0xFFFFFEFFFFFF8000, 0, 54, 0 );
+                l_scom_buffer.insert<uint64_t> (literal_0x00FF0203C00C0FFF, 0, 54, 0 );
             }
 
             l_rc = fapi2::putScom(TGT0, 0x3011807ull, l_scom_buffer);
