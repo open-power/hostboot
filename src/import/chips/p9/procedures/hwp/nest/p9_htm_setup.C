2c184a964937a9f36b33c2e6009ed96899c5d875
Stephen Cprek <smcprek@us.ibm.com>
2016-08-05 16:17:13 -0500

    Remove IBM confidential from files mirrored to hostboot currently
    Change-Id: I320f326f12f2f0ce26f2dff1416f2102fbaefdaa
    Original-Change-Id: I840eee8a063c0eb7ae195060a85da189d5e19e82
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/27944
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Matthew A. Ploetz <maploetz@us.ibm.com>
    Reviewed-by: Stephen M. Cprek <smcprek@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/nest/p9_htm_setup.C
+++ src/import/chips/p9/procedures/hwp/nest/p9_htm_setup.C
@@ -22,21 +22,6 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
-/* This is an automatically generated prolog.                             */
-/*                                                                        */
-/* $Source: chips/p9/procedures/hwp/nest/p9_htm_setup.C $                 */
-/*                                                                        */
-/* IBM CONFIDENTIAL                                                       */
-/*                                                                        */
-/* EKB Project                                                            */
-/*                                                                        */
-/* [+] International Business Machines Corp.                              */
-/*                                                                        */
-/*                                                                        */
-/* The source code for this program is not published or otherwise         */
-/* divested of its trade secrets, irrespective of what has been           */
-/* deposited with the U.S. Copyright Office.                              */
-/*                                                                        */
 ///----------------------------------------------------------------------------
 /// @file  p9_htm_setup.C
 ///
