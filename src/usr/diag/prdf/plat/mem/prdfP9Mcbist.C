c6ba29a554017fbf3bfa22f3fc1ba24506ed4107
Caleb Palmer <cnpalmer@us.ibm.com>
2016-08-15 16:36:33 -0500

    PRD: Maint Cmd Complete Nimbus DD1.0 Workaround
    Change-Id: I814f53adc0a6a7243719cada21d9aca44fb1f2ca
    RTC: 158755
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/28392
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Benjamin J. Weisenbeck <bweisenb@us.ibm.com>
    Reviewed-by: Zane C. Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/29059
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/diag/prdf/plat/mem/prdfP9Mcbist.C
+++ src/usr/diag/prdf/plat/mem/prdfP9Mcbist.C
@@ -34,6 +34,7 @@
 #include <prdfPluginMap.H>
 
 // Platform includes
+#include <prdfMemExtraSig.H>
 #include <prdfPlatServices.H>
 #include <prdfP9McbistDataBundle.H>
 
@@ -129,6 +130,117 @@ int32_t McbistCmdComplete( ExtensibleChip * i_mcbChip,
     #undef PRDF_FUNC
 }
 PRDF_PLUGIN_DEFINE( p9_mcbist, McbistCmdComplete );
+
+/**
+ * @brief  MCBIST[12] - MCBIST Command Complete Nimbus DD1.0 workaround.
+ * @param  i_mcbChip An MCBIST chip.
+ * @param  io_sc     The step code data struct.
+ * @return SUCCESS or PRD_NO_CLEAR_FIR_BITS
+ */
+int32_t CmdCompleteDd1Workaround( ExtensibleChip * i_mcbChip,
+                                  STEP_CODE_DATA_STRUCT & io_sc )
+{
+    #define PRDF_FUNC "[p9_mcbist::CmdCompleteDd1Workaround] "
+
+    TARGETING::TargetHandle_t mcbTrgt = i_mcbChip->getTrgt();
+
+    // This workaround should only be seen during super fast MCBIST commands,
+    // which are only run during Memory Diagnostics. Also, the workaround only
+    // applies to P9 Nimbus DD1.0.
+    PRDF_ASSERT( isInMdiaMode() );
+    PRDF_ASSERT( (TARGETING::MODEL_NIMBUS == getChipModel(mcbTrgt)) &&
+                 (0x10                    == getChipLevel(mcbTrgt)) );
+
+    int32_t o_rc = SUCCESS; // Returned to rule code.
+    int32_t l_rc = SUCCESS; // For local rc handling.
+
+    SCAN_COMM_REGISTER_CLASS * mcbmcat = i_mcbChip->getRegister("MCBMCAT");
+
+    do
+    {
+        //read MCBMCAT from cache first then from HW
+        l_rc = mcbmcat->Read();
+        if (SUCCESS != l_rc)
+        {
+            PRDF_ERR(PRDF_FUNC "Read() failed on MCBMCAT");
+            break;
+        }
+        uint64_t tmp1 = mcbmcat->GetBitFieldJustified(0, 64);
+
+        l_rc = mcbmcat->ForceRead();
+        if (SUCCESS != l_rc)
+        {
+            PRDF_ERR(PRDF_FUNC "ForceRead() failed on MCBMCAT");
+            break;
+        }
+        uint64_t tmp2 = mcbmcat->GetBitFieldJustified(0, 64);
+
+        //compare the two values to determine if the command is completed
+
+        //if the values are the same the command has completed
+        if (tmp1 == tmp2)
+        {
+            // This plugin is only called when MCBISTFIR[12] is on by itself,
+            // however, there is a small window where MCBISTFIR[10] could be set
+            // after the initial register capture, but before this point in the
+            // code. So clear MCBISTFIR[10] just in case. Also, do this before
+            // handling the command complete attention to ease complication in
+            // error path handling.
+            SCAN_COMM_REGISTER_CLASS * mcbFirAnd =
+                i_mcbChip->getRegister("MCBISTFIR_AND");
+            mcbFirAnd->setAllBits();
+
+            mcbFirAnd->ClearBit(10);
+
+            l_rc = mcbFirAnd->Write();
+            if ( SUCCESS != l_rc )
+            {
+                PRDF_ERR( PRDF_FUNC "Write() failed on MCBISTFIR" );
+                break;
+            }
+
+            //handle like a normal command complete
+            o_rc = McbistCmdComplete( i_mcbChip, io_sc );
+        }
+        //if the values are different the command has not completed yet
+        else
+        {
+            //sleep for a bit - hopefully will be enough for the cmd to complete
+            PlatServices::milliSleep(0, 20);
+
+            //do not commit the errl - it won't have useful data
+            io_sc.service_data->setDontCommitErrl();
+
+            //return PRD_NO_CLEAR_FIR_BITS so PRD is called again
+            o_rc = PRD_NO_CLEAR_FIR_BITS;
+        }
+
+    } while(0);
+
+    // There is some error path handling that needs to be done if there is a
+    // local error.
+    if ( SUCCESS != l_rc )
+    {
+        PRDF_ERR( PRDF_FUNC "Failed on 0x%08x", i_mcbChip->getHuid() );
+
+        // Change signature indicating there was an error in analysis.
+        io_sc.service_data->SetErrorSig( PRDFSIG_CmdComplete_ERROR );
+
+        // Something definitely failed, so callout 2nd level support.
+        io_sc.service_data->SetCallout( NextLevelSupport_ENUM, MRU_HIGH );
+        io_sc.service_data->setServiceCall();
+
+        // Tell MDIA to skip further analysis on this target.
+        l_rc = mdiaSendEventMsg( mcbTrgt, MDIA::STOP_TESTING );
+        if ( SUCCESS != l_rc )
+            PRDF_ERR( PRDF_FUNC "mdiaSendEventMsg(STOP_TESTING) failed" );
+    }
+
+    return o_rc;
+
+    #undef PRDF_FUNC
+}
+PRDF_PLUGIN_DEFINE( p9_mcbist, CmdCompleteDd1Workaround );
 
 } // end namespace p9_mcbist
 
