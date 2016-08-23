9566d67feb51b93b340c9651996df320f059a075
Nick Bofferding <bofferdn@us.ibm.com>
2016-08-23 10:01:44 -0500

    Purge L2 at same time as L3 to support secureboot
    Change-Id: I6cda6470770d884392df940956c4629c25072b1d
    RTC: 125297
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/28679
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Stephen M. Cprek <smcprek@us.ibm.com>
    Reviewed-by: William G. Hoffa <wghoffa@us.ibm.com>

--- src/usr/secureboot/base/coreops.C
+++ src/usr/secureboot/base/coreops.C
@@ -119,128 +119,173 @@ namespace SECUREBOOT
 
     errlHndl_t issueBlindPurge()
     {
-        static const uint64_t PURGE_REG = 0x1001080e;
+        static const uint64_t PURGE_REG[] = {
+            EX_L2_PURGE_REGISTER,
+            EX_L3_PURGE_REGISTER
+        };
 
-            // Bits : Value
-            // 0    : 0b1 = Initiate Purge
-            // 1:4  : 0b0100 = Full Blind Purge
-            // 13:28 : 0x1000 = CGC is (512k / 128 byte line size)
+        static const uint64_t PURGE_VALUE[] = {
+            // L2, full purge.
+                0x8000000000000000,
+            // L3, blind purge.
+            // Bits  : Value
+            // 0     : 0b1 = Initiate Purge
+            // 1:4   : 0b0100 = Full Blind Purge
+            // 13:15 : 0b000 = Member value (ignored for full blind purge)
+            // 16:28 : 0x8000 = CGC is (512k / 128 byte line size)
             //          CGC means "Congruence Class", ie. cache row.
-        static const uint64_t PURGE_VALUE = 0xa000800000000000;
+                0xa000800000000000
+        };
 
-            // Bit 0 - Purge Pending.
-        static const uint64_t PURGE_PENDING = 0x8000000000000000;
+        static const uint64_t PURGE_PENDING[] = {
+            // L2 bit 9,10,11 - purge pending.
+                0x0070000000000000,
+            // L3 bit 0 - purge pending.
+                0x8000000000000000
+        };
 
         static const size_t RETRY_COUNT = 100;
         static const size_t RETRY_WAIT_NS = ONE_CTX_SWITCH_NS;
 
-        errlHndl_t l_errl = NULL;
-        do
+        errlHndl_t pError = NULL;
+
+        for (size_t reg = 0;
+             reg < (sizeof(PURGE_REG)/sizeof(PURGE_REG[0]));
+             ++reg)
         {
-            size_t coreId = getCoreId();
-            uint64_t regAddr = PURGE_REG + 0x01000000 * coreId;
+            const size_t coreId = getCoreId();
+            const uint64_t regAddr = PURGE_REG[reg] +
+                (EX_CHILPLET_MULTIPLIER * coreId);
 
             uint64_t data = 0;
             size_t size = sizeof(data);
 
             // Read purge register to ensure no operation is pending.
-            l_errl =
-                deviceRead(TARGETING::MASTER_PROCESSOR_CHIP_TARGET_SENTINEL,
-                           &data, size,
-                           DEVICE_SCOM_ADDRESS(regAddr));
-            if (l_errl)
+            pError = deviceRead(
+                TARGETING::MASTER_PROCESSOR_CHIP_TARGET_SENTINEL,
+                &data, size,
+                DEVICE_SCOM_ADDRESS(regAddr));
+            if (pError)
             {
                 break;
             }
 
-            if (data & PURGE_PENDING)
+            assert(size == sizeof(data),
+                   "BUG! Returned SCOM size was %d, expected %d",
+                   size,sizeof(data));
+
+            if (data & PURGE_PENDING[reg])
             {
                 /*@
                  * @errortype
-                 * @moduleid    SECUREBOOT::MOD_SECURE_BLINDPURGE
-                 * @reasoncode  SECUREBOOT::RC_PURGEOP_PENDING
-                 * @userdata1   SCOM value.
-                 * @userdata2   CPU ID (PIR) encountering failure.
-                 * @devdesc     Attempted to purge cache while purge operation
-                 *              was pending.
+                 * @moduleid          SECUREBOOT::MOD_SECURE_BLINDPURGE
+                 * @reasoncode        SECUREBOOT::RC_PURGEOP_PENDING
+                 * @userdata1         SCOM value.
+                 * @userdata2[0:31]   CPU ID (PIR) encountering failure.
+                 * @userdata2[32:63]  SCOM address read.
+                 * @devdesc           Attempted to purge cache while purge
+                 *                    operation was pending.
+                 * @custdesc          Failed to initialize boot core cache
                  */
-                l_errl =
-                    new ERRORLOG::ErrlEntry(ERRORLOG::ERRL_SEV_UNRECOVERABLE,
-                                            MOD_SECURE_BLINDPURGE,
-                                            RC_PURGEOP_PENDING,
-                                            data, task_getcpuid());
-                // Probably a code bug
-                l_errl->addProcedureCallout( HWAS::EPUB_PRC_HB_CODE,
-                                             HWAS::SRCI_PRIORITY_HIGH );
-                // But there might be something wrong with the chip
-                //  or the SBE image
-                l_errl->addHwCallout(
-                          TARGETING::MASTER_PROCESSOR_CHIP_TARGET_SENTINEL,
-                          HWAS::SRCI_PRIORITY_LOW,
-                          HWAS::NO_DECONFIG,
-                          HWAS::GARD_NULL );
+                pError = new ERRORLOG::ErrlEntry(
+                    ERRORLOG::ERRL_SEV_UNRECOVERABLE,
+                    MOD_SECURE_BLINDPURGE,
+                    RC_PURGEOP_PENDING,
+                    data,
+                    TWO_UINT32_TO_UINT64(
+                        task_getcpuid(),
+                        regAddr),
+                    true); // Add high priority HB callout
+
+                // There is a small chance something is wrong with the chip
+                // or the SBE image
+                pError->addHwCallout(
+                    TARGETING::MASTER_PROCESSOR_CHIP_TARGET_SENTINEL,
+                    HWAS::SRCI_PRIORITY_LOW,
+                    HWAS::NO_DECONFIG,
+                    HWAS::GARD_NULL );
                 break;
             }
 
             // Initiate purge operation
-            data = PURGE_VALUE;
-            l_errl =
-                deviceWrite(TARGETING::MASTER_PROCESSOR_CHIP_TARGET_SENTINEL,
-                            &data, size,
-                            DEVICE_SCOM_ADDRESS(regAddr));
-            if (l_errl)
+            data = PURGE_VALUE[reg];
+            pError = deviceWrite(
+                TARGETING::MASTER_PROCESSOR_CHIP_TARGET_SENTINEL,
+                &data, size,
+                DEVICE_SCOM_ADDRESS(regAddr));
+            if (pError)
             {
                 break;
             }
 
-            // Wait for purge to complete.
-            for(size_t i = 0; i < RETRY_COUNT; i++)
-            {
-                l_errl =
-                    deviceRead(TARGETING::MASTER_PROCESSOR_CHIP_TARGET_SENTINEL,
-                               &data, size,
-                               DEVICE_SCOM_ADDRESS(regAddr));
+            assert(size == sizeof(data),
+                   "BUG! After SCOM write, SCOM size was %d, expected %d",
+                   size,sizeof(data));
 
-                if ((l_errl) || !(data & PURGE_PENDING))
+            // Wait for purge to complete.
+            for(size_t i = 0; i < RETRY_COUNT; ++i)
+            {
+                data = 0;
+                pError = deviceRead(
+                    TARGETING::MASTER_PROCESSOR_CHIP_TARGET_SENTINEL,
+                    &data, size,
+                    DEVICE_SCOM_ADDRESS(regAddr));
+
+                // Error or not, size should not change
+                assert(size == sizeof(data),
+                       "BUG! Returned SCOM size was %d, expected %d",
+                       size,sizeof(data));
+
+                if ((pError) || !(data & PURGE_PENDING[reg]))
                 {
                     break;
                 }
 
                 nanosleep(0, RETRY_WAIT_NS);
             }
-            if (l_errl)
+
+            if (pError)
             {
                 break;
             }
-            if (data & PURGE_PENDING) // Ensure op did complete.
+
+            if (data & PURGE_PENDING[reg]) // Ensure purge completed.
             {
                 /*@
                  * @errortype
-                 * @moduleid    SECUREBOOT::MOD_SECURE_BLINDPURGE
-                 * @reasoncode  SECUREBOOT::RC_PURGEOP_FAIL_COMPLETE
-                 * @userdata1   SCOM value of Reg 0x1001080E
-                 * @devdesc     Purge operation never completed.
+                 * @moduleid          SECUREBOOT::MOD_SECURE_BLINDPURGE
+                 * @reasoncode        SECUREBOOT::RC_PURGEOP_FAIL_COMPLETE
+                 * @userdata1         SCOM value
+                 * @userdata2[0:31]   CPU ID (PIR) encountering failure.
+                 * @userdata2[32:63]  SCOM address read.
+                 * @devdesc           Purge operation never completed.
+                 * @custdesc          Failed to initialize boot core cache
                  */
-                l_errl =
-                    new ERRORLOG::ErrlEntry(ERRORLOG::ERRL_SEV_UNRECOVERABLE,
-                                            MOD_SECURE_BLINDPURGE,
-                                            RC_PURGEOP_FAIL_COMPLETE,
-                                            data);
+                pError = new ERRORLOG::ErrlEntry(
+                    ERRORLOG::ERRL_SEV_UNRECOVERABLE,
+                    MOD_SECURE_BLINDPURGE,
+                    RC_PURGEOP_FAIL_COMPLETE,
+                    data,
+                    TWO_UINT32_TO_UINT64(
+                        task_getcpuid(),
+                        regAddr));
+
                 // Most likely a hardware issue of some sort
-                l_errl->addHwCallout(
-                          TARGETING::MASTER_PROCESSOR_CHIP_TARGET_SENTINEL,
-                          HWAS::SRCI_PRIORITY_HIGH,
-                          HWAS::NO_DECONFIG,
-                          HWAS::GARD_NULL );
+                pError->addHwCallout(
+                    TARGETING::MASTER_PROCESSOR_CHIP_TARGET_SENTINEL,
+                    HWAS::SRCI_PRIORITY_HIGH,
+                    HWAS::NO_DECONFIG,
+                    HWAS::GARD_NULL );
+
                 // Could also be a code bug
-                l_errl->addProcedureCallout( HWAS::EPUB_PRC_HB_CODE,
+                pError->addProcedureCallout( HWAS::EPUB_PRC_HB_CODE,
                                              HWAS::SRCI_PRIORITY_MED );
                 break;
             }
 
-        } while(0);
+        }
 
-        return l_errl;
+        return pError;
     }
 
 }
