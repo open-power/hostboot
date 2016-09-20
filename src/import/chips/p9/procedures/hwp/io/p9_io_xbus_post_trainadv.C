336b64615c3146723ff65cf9bf72e98e82c149ff
Chris Steffen <cwsteffen@us.ibm.com>
2016-09-20 14:33:30 -0500

    IO Xbus Post Training Mfg Check
    - Added Mfg Min Eye Width Checking based upon
        attribute flag & min eye width value
    
    Change-Id: I4099e20d1efb22fe51266567d7bc39c9fefbca8b
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/29985
    Reviewed-by: Richard J. Knight <rjknight@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Gary A. Peterson <garyp@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/29987
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/io/p9_io_xbus_post_trainadv.C
+++ src/import/chips/p9/procedures/hwp/io/p9_io_xbus_post_trainadv.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2016                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2017                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -52,12 +52,76 @@
 // ----------------------------------------------------------------------------
 // Includes
 // ----------------------------------------------------------------------------
-
 #include "p9_io_xbus_post_trainadv.H"
+#include <p9_io_scom.H>
+#include <p9_io_regs.H>
 
 // ----------------------------------------------------------------------------
 // Procedure Function
 // ----------------------------------------------------------------------------
+
+
+fapi2::ReturnCode getDebugInfo(
+    const fapi2::Target<fapi2::TARGET_TYPE_XBUS> i_tgt,
+    const uint8_t i_grp )
+{
+    FAPI_IMP("Entering...");
+
+
+    FAPI_IMP("Exiting...");
+    return fapi2::current_err;
+}
+
+fapi2::ReturnCode checkEyeWidth(
+    const fapi2::Target<fapi2::TARGET_TYPE_XBUS> i_tgt,
+    const uint8_t i_grp )
+{
+    FAPI_IMP("Entering...");
+
+    const uint32_t ONE_MS        = 1000000; // 1,000,000ns = 1ms
+    const uint32_t DELAY_NS      = 100 * ONE_MS; // Delay for 100ms
+    const uint32_t DELAY_CYCLES  = 1; // We won't be using this feature in sim.
+    const uint8_t LN0            = 0;
+    uint64_t      data64         = 0;
+    uint8_t       minMfgEyeWidth = 0;
+    char tgt_str[fapi2::MAX_ECMD_STRING_LEN];
+
+    fapi2::toString(i_tgt, tgt_str, fapi2::MAX_ECMD_STRING_LEN);
+
+    // Get the minimum manufacturing eye width
+    FAPI_TRY( FAPI_ATTR_GET( fapi2::ATTR_IO_X_MFG_MIN_EYE_WIDTH, i_tgt, minMfgEyeWidth ) );
+
+    // We need to wait for each lane to get through recal before the historical eye
+    //   width values will be valid.  At 2ms per lane * 17 lanes = 34ms.  To be safe
+    //   we want this number to get through a few times.  We will wait 100ms
+    FAPI_TRY(fapi2::delay(DELAY_NS, DELAY_CYCLES));
+
+
+    // Read the historical minimum eye width
+    FAPI_TRY( io::read( EDIP_RX_CTL_CNTL13_EO_PG, i_tgt, i_grp, LN0, data64 ),
+              "Reading EDI+ RX CTL CNTL13 EO PG Failed" );
+
+    FAPI_DBG( "tgt(%s:g%d) Min Eye Width(%d) Lane(%d) Valid(%d) :: MinMfgEyeWidth(%d)",
+              tgt_str, i_grp,
+              io::get( EDIP_RX_HIST_MIN_EYE_WIDTH      , data64 ),
+              io::get( EDIP_RX_HIST_MIN_EYE_WIDTH_LANE , data64 ),
+              io::get( EDIP_RX_HIST_MIN_EYE_WIDTH_VALID, data64 ),
+              minMfgEyeWidth );
+
+    // Check if the historical eye width is less then the manufacturing minimum eye width
+    FAPI_ASSERT( ( io::get( EDIP_RX_HIST_MIN_EYE_WIDTH, data64 ) >= minMfgEyeWidth ),
+                 fapi2::IO_XBUS_MFG_RX_EYE_WIDTH_FAILURE().set_TARGET( i_tgt ).set_GROUP( i_grp )
+                 .set_EYE_WIDTH( io::get( EDIP_RX_HIST_MIN_EYE_WIDTH, data64 ) )
+                 .set_EYE_WIDTH_LANE( io::get( EDIP_RX_HIST_MIN_EYE_WIDTH_LANE, data64 ) )
+                 .set_EYE_WIDTH_VALID( io::get( EDIP_RX_HIST_MIN_EYE_WIDTH_VALID, data64 ) )
+                 .set_MIN_EYE_WIDTH( minMfgEyeWidth ),
+                 "I/O EDI+ Xbus Manufacturing Eye Width Failure." );
+
+
+fapi_try_exit:
+    FAPI_IMP("Exiting...");
+    return fapi2::current_err;
+}
 
 /**
  * @brief A simple HWP that runs after io_run_trainig.
@@ -69,28 +133,38 @@
  * @retval ReturnCode
  */
 fapi2::ReturnCode p9_io_xbus_post_trainadv(
-    const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_target,
-    const uint8_t& i_group,
-    const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_ctarget,
-    const uint8_t& i_cgroup)
+    const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_tgt,
+    const uint8_t& i_grp,
+    const fapi2::Target < fapi2::TARGET_TYPE_XBUS >& i_ctgt,
+    const uint8_t& i_cgrp)
 {
     FAPI_IMP("Entering...");
     uint8_t l_status = 0x0;
-    char target_string[fapi2::MAX_ECMD_STRING_LEN];
+    char tgt_str[fapi2::MAX_ECMD_STRING_LEN];
 
-    fapi2::toString(i_target, target_string, fapi2::MAX_ECMD_STRING_LEN);
+    fapi2::toString(i_tgt, tgt_str, fapi2::MAX_ECMD_STRING_LEN);
 
-    FAPI_INF("Checking %s:g%d Debug Status.", target_string, i_group);
-    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_IO_X_DEBUG, i_target, l_status));
+    FAPI_INF("Checking %s:g%d Post Link Training.", tgt_str, i_grp);
+
+    // Get Debug Info
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_IO_X_DEBUG, i_tgt, l_status));
 
     if(l_status == fapi2::ENUM_ATTR_IO_X_DEBUG_TRUE)
     {
-        FAPI_INF("Debug True.");
+        FAPI_TRY( getDebugInfo( i_tgt, i_grp ) );
+        FAPI_TRY( getDebugInfo( i_ctgt, i_cgrp ) );
     }
-    else
+
+    // Run Manufacturing Eye Width Check
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_IO_X_MFG_CHK, i_tgt, l_status));
+
+    if(l_status == fapi2::ENUM_ATTR_IO_X_MFG_CHK_TRUE)
     {
-        FAPI_INF("Debug False.");
+        FAPI_TRY( checkEyeWidth( i_tgt, i_grp ) );
+        FAPI_TRY( checkEyeWidth( i_ctgt, i_cgrp ) );
     }
+
+
 
 fapi_try_exit:
     FAPI_IMP("Exiting...");
