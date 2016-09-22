2ca97f1fd9776aba2e82bc786953c07cc12c0133
Dan Crowell <dcrowell@us.ibm.com>
2016-09-22 09:53:55 -0500

    Fixes for MSS_INTERLEAVE_ENABLE
    Added a better default value to attempt maximum interleaving
    Added MRW parsing logic to attempt to pull data if it is available
    
    Change-Id: I9def8f51a3bb39d2868e9903fae68a1affe4b44e
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/30121
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Matt Derksen <v2cibmd@us.ibm.com>
    Reviewed-by: Andres A. Lugo-Reyes <aalugore@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/targeting/common/genHwsvMrwXml.pl
+++ src/usr/targeting/common/genHwsvMrwXml.pl
@@ -343,6 +343,14 @@ push @systemAttr,
 ];
 
 
+# Note - if below attribute is specified with im-id, it will not get
+#  set into the output
+if( exists $reqPol->{'mss_interleave_enable'} )
+{
+    push @systemAttr, ['MSS_INTERLEAVE_ENABLE',
+      $reqPol->{'mss_interleave_enable'}];
+}
+
 
 if ($reqPol->{'supports_dynamic_mem_volt'} eq 'true')
 {
