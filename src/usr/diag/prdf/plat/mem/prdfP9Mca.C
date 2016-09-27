6c8eba571369f2df68aae86d9997a97341f55bcc
Zane Shelley <zshelle@us.ibm.com>
2016-09-27 09:25:58 -0500

    PRD: add support for RCD parity errors
    Change-Id: Ibba87b4efeb8533f74954d652356d80f94c5da65
    RTC: 161894
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/30565
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Caleb N. Palmer <cnpalmer@us.ibm.com>
    Reviewed-by: Brian J. Stegmiller <bjs@us.ibm.com>
    Reviewed-by: Benjamin J. Weisenbeck <bweisenb@us.ibm.com>
    Reviewed-by: Zane C. Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/30990
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>

---
+++ src/usr/diag/prdf/plat/mem/prdfP9Mca.C
@@ -0,0 +1,110 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/usr/diag/prdf/plat/mem/prdfP9Mca.C $                      */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+// Framework includes
+#include <iipServiceDataCollector.h>
+#include <prdfExtensibleChip.H>
+#include <prdfPluginMap.H>
+
+// Platform includes
+#include <prdfP9McbistDataBundle.H>
+#include <prdfPlatServices.H>
+#ifdef __HOSTBOOT_RUNTIME
+  #include <prdfMemTps_rt.H>
+#endif
+
+
+using namespace TARGETING;
+
+namespace PRDF
+{
+
+using namespace PlatServices;
+
+namespace p9_mca
+{
+
+//##############################################################################
+//
+//                               MCACALFIR
+//
+//##############################################################################
+
+/**
+ * @brief  MCACALFIR[4] - RCD Parity Error.
+ * @param  i_mcaChip A P9 MCA chip.
+ * @param  io_sc     The step code data struct.
+ * @return SUCCESS
+ */
+int32_t RcdParityError( ExtensibleChip * i_mcaChip,
+                        STEP_CODE_DATA_STRUCT & io_sc )
+{
+    #define PRDF_FUNC "[p9_mca::RcdParityError] "
+
+    // The callouts have already been made in the rule code. All we need to do
+    // now is start TPS on all slave ranks behind the MCA. This can only be done
+    // at runtime because it is too complicated to handle during Memory
+    // Diagnostics and we don't have time to complete the procedures at any
+    // other point during the IPL. The DIMMs will be deconfigured during the IPL
+    // anyways. So not really much benefit except for extra FFDC.
+
+    #ifdef __HOSTBOOT_RUNTIME // TPS only supported at runtime.
+
+    if ( io_sc.service_data->IsAtThreshold() )
+    {
+        ExtensibleChip * mcbChip = getConnectedParent( i_mcaChip, TYPE_MCBIST );
+        PRDF_ASSERT( nullptr != mcbChip );
+
+        McbistDataBundle * mcbdb = getMcbistDataBundle( mcbChip );
+
+        std::vector<MemRank> list;
+        getSlaveRanks<TYPE_MCA>( i_mcaChip->getTrgt(), list );
+
+        for ( auto & r : list )
+        {
+            TdEntry * entry = new TpsEvent<TYPE_MCA>( i_mcaChip, r );
+            uint32_t rc = mcbdb->getTdCtlr()->handleTdEvent( io_sc, entry );
+            if ( SUCCESS != rc )
+            {
+                PRDF_ERR( PRDF_FUNC "handleTdEvent(m%ds%s) failed on 0x%08x",
+                          i_mcaChip->getHuid() );
+
+                continue; // Try the other ranks.
+            }
+        }
+    }
+
+    #endif
+
+    return SUCCESS;
+
+    #undef PRDF_FUNC
+}
+PRDF_PLUGIN_DEFINE( p9_mca, RcdParityError );
+
+} // end namespace p9_mca
+
+} // end namespace PRDF
+
