2e0cf2d9aa1a44a364eb052b0d752ec247c5c217
xiaozq <shxiaozq@cn.ibm.com>
2016-09-29 10:46:54 -0500

    Centaur SBE -> FAPI2:
        Finished procedures:
        - cen_tp_chiplet_init1
        - cen_pll_setup
        - cen_tp_chiplet_init2
        - cen_tp_arrayinit
        - cen_tp_chiplet_init3
        - cen_chiplet_init
        - cen_arrayinit
        - cen_dts_init
        - cen_do_manual_inits
        - cen_nest_startclocks
        - cen_scominits
    
        All tests are done and passed against tul87fp.
    
    Change-Id: I151e19e6a055b97b24404020906b2c94a888cf81
    Original-Change-Id: I1e536bea079c0f256f9276505065477373c72514
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/30513
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: LUCAS W. MULKEY <lwmulkey@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/43842
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/centaur/procedures/hwp/perv/cen_pll_setup.C
@@ -0,0 +1,152 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/centaur/procedures/hwp/perv/cen_pll_setup.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2017                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file cen_pll_setup.C
+/// @brief Centaur PLL setup (FAPI2)
+///
+/// @author Peng Fei GOU <shgoupf@cn.ibm.com>
+///
+
+//
+// *HWP HWP Owner: Peng Fei GOU <shgoupf@cn.ibm.com>
+// *HWP FW Owner: Thi Tran <thi@us.ibm.com>
+// *HWP Team: Perv
+// *HWP Level: 2
+// *HWP Consumed by: HB
+//
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <cen_pll_setup.H>
+#include <centaur_misc_scom_addresses.H>
+#include <centaur_misc_constants.H>
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+cen_pll_setup(const fapi2::Target<fapi2::TARGET_TYPE_MEMBUF_CHIP>& i_target)
+{
+    FAPI_DBG("Start");
+    fapi2::buffer<uint32_t> l_cfam_status_data = 0;
+    fapi2::buffer<uint32_t> l_fsi_gp3_data = 0;
+    fapi2::buffer<uint32_t> l_fsi_gp4_data = 0;
+    fapi2::buffer<uint32_t> l_perv_gp3_data = 0;
+    fapi2::Target<fapi2::TARGET_TYPE_SYSTEM> FAPI_SYSTEM;
+    bool l_poll_succeed = false;
+
+    // SBE Address Base Register Setups
+    FAPI_DBG("Reset PLL test enable");
+    FAPI_TRY(fapi2::getCfamRegister(i_target, CFAM_FSI_GP4, l_fsi_gp4_data),
+             "Error from getCfamRegister (CFAM_FSI_GP4)");
+    l_fsi_gp4_data.clearBit<24>();
+    FAPI_TRY(fapi2::putCfamRegister(i_target, CFAM_FSI_GP4, l_fsi_gp4_data),
+             "Error from putCfamRegister (CFAM_FSI_GP4)");
+
+    FAPI_DBG( "PLL Leave Reset State" );
+    FAPI_TRY(fapi2::getCfamRegister(i_target, CFAM_FSI_GP3, l_fsi_gp3_data),
+             "Error from getCfamRegister (CFAM_FSI_GP3)");
+    l_fsi_gp3_data.clearBit<28>();
+    FAPI_TRY(fapi2::putCfamRegister(i_target, CFAM_FSI_GP3, l_fsi_gp3_data),
+             "Error from putCfamRegister (CFAM_FSI_GP3)");
+
+    FAPI_DBG( "Centaur only: Nest PLL Leave Reset State" );
+    FAPI_TRY(fapi2::getCfamRegister(i_target, CFAM_FSI_GP4, l_fsi_gp4_data),
+             "Error from getCfamRegister (CFAM_FSI_GP4)");
+    l_fsi_gp4_data.clearBit<16>();
+    FAPI_TRY(fapi2::putCfamRegister(i_target, CFAM_FSI_GP4, l_fsi_gp4_data),
+             "Error from putCfamRegister (CFAM_FSI_GP4)");
+
+    FAPI_DBG( "Drop Nest PLL bypass GP3MIR(5)=0  tp_pllbyp_dc" );
+    FAPI_TRY(fapi2::getCfamRegister(i_target, PERV_GP3, l_perv_gp3_data),
+             "Error from getCfamRegister (PERV_GP3)");
+    l_perv_gp3_data.clearBit<5>();
+    FAPI_TRY(fapi2::putCfamRegister(i_target, PERV_GP3, l_perv_gp3_data),
+             "Error from putCfamRegister (PERV_GP3)");
+
+    FAPI_DBG( "Poll FSI2PIB-Status(24) for (nest) pll lock bits." );
+
+    for (uint32_t i = 0; i < MAX_FLUSH_LOOPS; i++)
+    {
+        FAPI_TRY(fapi2::getCfamRegister(i_target, FSI2PIB_STATUS, l_cfam_status_data),
+                 "Error from getCfamRegister (FSI2PIB_STATUS)");
+
+        FAPI_DBG( "Polling... FSI2PIB-Status(24)." );
+
+        if (l_cfam_status_data.getBit<24>())
+        {
+            l_poll_succeed = true;
+            break;
+        }
+
+        FAPI_TRY(fapi2::delay(NANO_FLUSH_DELAY, SIM_FLUSH_DELAY));
+    }
+
+    FAPI_ASSERT(l_poll_succeed,
+                fapi2::CEN_PLL_SETUP_POLL_NEST_PLL_LOCK_TIMEOUT().
+                set_TARGET(i_target),
+                "NEST PLL LOCK TIMEOUT!");
+
+    // Chiplet Init bring-up MEM PLL
+
+    FAPI_DBG( "bring-up the MEM PLL for ARRAYINIT closure" );
+
+    FAPI_DBG( "Drop bypass mode before LOCK (tp_pllmem_bypass_en_dc)." );
+    FAPI_TRY(fapi2::getCfamRegister(i_target, PERV_GP3, l_perv_gp3_data),
+             "Error from getCfamRegister (PERV_GP3)");
+    l_perv_gp3_data.clearBit<17>();
+    FAPI_TRY(fapi2::putCfamRegister(i_target, PERV_GP3, l_perv_gp3_data),
+             "Error from putCfamRegister (PERV_GP3)");
+
+    FAPI_DBG( "Poll FSI2PIB-Status(25) for (mem) pll lock bits." );
+    l_poll_succeed = false;
+
+    for (uint32_t i = 0; i < MAX_FLUSH_LOOPS; i++)
+    {
+        FAPI_TRY(fapi2::getCfamRegister(i_target, FSI2PIB_STATUS, l_cfam_status_data),
+                 "Error from getCfamRegister (FSI2PIB_STATUS)");
+
+        FAPI_DBG( "Polling... FSI2PIB-Status(25)." );
+
+        if (l_cfam_status_data.getBit<25>())
+        {
+            l_poll_succeed = true;
+            break;
+        }
+
+        FAPI_TRY(fapi2::delay(NANO_FLUSH_DELAY, SIM_FLUSH_DELAY));
+    }
+
+    FAPI_ASSERT(l_poll_succeed,
+                fapi2::CEN_PLL_SETUP_POLL_MEM_PLL_LOCK_TIMEOUT().
+                set_TARGET(i_target),
+                "MEM PLL LOCK TIMEOUT!");
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
