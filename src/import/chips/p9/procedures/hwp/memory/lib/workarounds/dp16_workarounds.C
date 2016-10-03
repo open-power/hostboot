53653cb7e8f21dc4715e400ea726ebdfca2f27a2
Stephen Glancy <sglancy@us.ibm.com>
2016-10-03 14:04:35 -0500

    Added WR VREF latch command
    Change-Id: I11938e71982aad0b5b5f3b389247cbd6b46f3a02
    Original-Change-Id: Ibb8ce5b66b0b2359c6da8066b99cbb6381c7ff92
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/30697
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Brian R. Silver <bsilver@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/lib/workarounds/dp16_workarounds.C
+++ src/import/chips/p9/procedures/hwp/memory/lib/workarounds/dp16_workarounds.C
@@ -43,6 +43,7 @@
 #include <lib/utils/pos.H>
 #include <lib/workarounds/dp16_workarounds.H>
 #include <lib/phy/dp16.H>
+#include <lib/dimm/rank.H>
 
 namespace mss
 {
@@ -177,6 +178,303 @@ fapi2::ReturnCode error_dram23( const fapi2::Target<fapi2::TARGET_TYPE_MCA>& i_t
     FAPI_TRY((mss::dp16::write_wr_vref_error_mask<DP2, DRAM23>(i_target, DISABLE_ERRORS)));
     FAPI_TRY((mss::dp16::write_wr_vref_error_mask<DP3, DRAM23>(i_target, DISABLE_ERRORS)));
     FAPI_TRY((mss::dp16::write_wr_vref_error_mask<DP4, DRAM23>(i_target, DISABLE_ERRORS)));
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+///
+/// @brief DP16 big step/small step check and modify workaround - MCA specialization
+/// In DD1 Nimbus in the WR VREF algorithm, the out of bounds checks are broken.
+/// One aspect of fixing this bug is to ensure that the big step is divisible by the small step
+/// This function converts the small step value over to the closest allowable value as to what was entered
+/// @param[in] i_big_step - WR VREF big step value
+/// @param[in,out] io_small_step - WR VREF small step value
+/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS if ok
+///
+template<  >
+fapi2::ReturnCode modify_small_step_for_big_step<fapi2::TARGET_TYPE_MCA>(const uint8_t i_big_step,
+        uint8_t& io_small_step)
+{
+    // TODO RTC:160353 - Need module/chip rev EC support for workarounds
+
+    // Step size constants
+    constexpr uint8_t MAX_BIG_STEP = 0x10;
+    constexpr uint8_t MAX_SMALL_STEP = 0x08;
+
+    // Conversion over to the nearest allowable small step
+    // The algorithm takes the register value + 1 when it runs.
+    // This avoids cases where it would infinite loop if it had a 0 value in the register
+    // As such, the math to figure out if a big/small step combination is allowed is ((big_step + 1) % (small_step + 1)) == 0
+    // This below chart takes the allowable combinations or the next smallest allowable value.
+    constexpr uint8_t SMALL_STEP_CONVERSION[MAX_BIG_STEP][MAX_SMALL_STEP] =
+    {
+        {0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 1, 1, 1, 1, 1, 1, 1,},
+        {0, 0, 2, 2, 2, 2, 2, 2,},
+        {0, 1, 1, 3, 3, 3, 3, 3,},
+        {0, 0, 0, 0, 4, 4, 4, 4,},
+        {0, 1, 2, 2, 2, 5, 5, 5,},
+        {0, 0, 0, 0, 0, 0, 6, 6,},
+        {0, 1, 1, 3, 3, 3, 3, 7,},
+        {0, 0, 2, 2, 2, 2, 2, 2,},
+        {0, 1, 1, 1, 4, 4, 4, 4,},
+        {0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 1, 2, 3, 3, 5, 5, 5,},
+        {0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 1, 1, 1, 1, 1, 6, 6,},
+        {0, 0, 2, 2, 4, 4, 4, 4,},
+        {0, 1, 1, 3, 3, 3, 3, 7,},
+    };
+
+    // Makes sure that the values passed in were not out of range
+    if(MAX_BIG_STEP <= i_big_step)
+    {
+        FAPI_TRY(fapi2::FAPI2_RC_INVALID_PARAMETER, "WR VREF %s step is out of range. %s step 0x%02x max", "big", "big",
+                 i_big_step);
+    }
+
+    if(MAX_SMALL_STEP <= io_small_step)
+    {
+        FAPI_TRY(fapi2::FAPI2_RC_INVALID_PARAMETER, "WR VREF %s step is out of range. %s step 0x%02x max", "small", "small",
+                 io_small_step);
+    }
+
+    // Converts the value
+    io_small_step = SMALL_STEP_CONVERSION[i_big_step][io_small_step];
+
+    return fapi2::FAPI2_RC_SUCCESS;
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+///
+/// @brief DP16 writes config 0 overriding bad small step/big step values
+/// In DD1 Nimbus in the WR VREF algorithm, the out of bounds checks are broken.
+/// One aspect of fixing this bug is to ensure that the big step is divisible by the small step
+/// This function converts the small step value over to the closest allowable value as to what was entered
+/// @param[in] i_target the fapi2 target type MCA of the port
+/// @param[out] o_big_step - WR VREF big step value
+/// @param[out] o_small_step - WR VREF small step value
+/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS if ok
+///
+fapi2::ReturnCode write_config0( const fapi2::Target<fapi2::TARGET_TYPE_MCA>& i_target, uint8_t& o_big_step,
+                                 uint8_t& o_small_step)
+{
+    // TODO RTC:160353 - Need module/chip rev EC support for workarounds
+
+    // Traits declaration
+    typedef dp16Traits<fapi2::TARGET_TYPE_MCA> TT;
+
+    // Variable declaration
+    fapi2::buffer<uint64_t> l_data;
+
+    // Reads the register - note assumes ALL DP's have the same value. Will overwrite different values
+    FAPI_TRY(mss::dp16::read_wr_vref_config0<0>(i_target, l_data));
+
+    // Sets these values to 0's to avoid undesired values
+    o_small_step = 0;
+    o_big_step = 0;
+
+    // Gets out the current values of the big and small step
+    l_data.extractToRight<TT::WR_VREF_CONFIG0_2D_SMALL_STEP_VAL, TT::WR_VREF_CONFIG0_2D_SMALL_STEP_VAL_LEN>(o_small_step)
+    .extractToRight<TT::WR_VREF_CONFIG0_2D_BIG_STEP_VAL, TT::WR_VREF_CONFIG0_2D_BIG_STEP_VAL_LEN>(o_big_step);
+
+    // Does the workaround conversion
+    FAPI_TRY(modify_small_step_for_big_step<fapi2::TARGET_TYPE_MCA>(o_big_step, o_small_step));
+
+    // Sets up the good values
+    l_data.insertFromRight<TT::WR_VREF_CONFIG0_2D_SMALL_STEP_VAL, TT::WR_VREF_CONFIG0_2D_SMALL_STEP_VAL_LEN>(o_small_step)
+    .insertFromRight<TT::WR_VREF_CONFIG0_2D_BIG_STEP_VAL, TT::WR_VREF_CONFIG0_2D_BIG_STEP_VAL_LEN>(o_big_step);
+
+    // Blasts em out
+    FAPI_TRY(scom_blastah(i_target, TT::WR_VREF_CONFIG0_REG, l_data));
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+///
+/// @brief DP16 converts the VREF training values to start calibration
+/// In DD1 Nimbus in the WR VREF algorithm, the out of bounds checks are broken.
+/// One aspect of fixing this bug is to ensure the WR VREF is an integer number of big steps away from the 0 value and one big step from the top of the range
+/// This function converts values over to the required rules
+/// @tparam T fapi2 Target Type - just here to ensure that this won't be called on a non-Nimbus system
+/// @param[in] i_big_step - WR VREF big step value
+/// @param[in,out] io_train_range - VREF train range converted value
+/// @param[in,out] io_train_value - VREF train value converted value
+/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS if ok
+///
+template <>
+fapi2::ReturnCode convert_train_values<fapi2::TARGET_TYPE_MCA>( const uint8_t i_big_step,
+        uint8_t& io_train_range,
+        uint8_t& io_train_value)
+{
+    // Number of big steps
+    constexpr uint8_t NUM_BIG_STEP = 16;
+
+    // Value at which Range 2 equals Range 1's 0 value
+    constexpr uint8_t CROSSOVER_RANGE = 0b011000;
+
+    // List of allowable maximum ranges.  The math is max = (absolute_max - (big_step_val + 1)) - (absolute_max - (big_step_val + 1)) % (big_step_val + 1)
+    constexpr uint8_t l_max_allowable_values[NUM_BIG_STEP] =
+    {
+        73, 72, 69, 68,
+        65, 66, 63, 64,
+        63, 60, 55, 60,
+        52, 56, 45, 48,
+    };
+
+    // Declares temporary variable
+    uint8_t l_continuous_range = 0;
+    const uint8_t l_big_step_alg = i_big_step + 1;
+
+    // Errors out if the big step is out of range
+    FAPI_TRY(i_big_step < NUM_BIG_STEP ? fapi2::FAPI2_RC_SUCCESS : fapi2::FAPI2_RC_INVALID_PARAMETER,
+             "Big step of %d passed in. Max allowable value is %d", i_big_step, NUM_BIG_STEP - 1);
+
+    // Converts the values over to the algorithm's continuous range
+    l_continuous_range = io_train_range == fapi2::ENUM_ATTR_EFF_VREF_DQ_TRAIN_RANGE_RANGE1 ?
+                         (io_train_value + CROSSOVER_RANGE) : (io_train_value);
+
+    // Does the math for what makes a good continuous range
+    // At least 1 big step away from the top of the range
+    l_continuous_range = std::min(l_continuous_range, l_max_allowable_values[i_big_step]);
+
+    // At least 1 big step away from the bottom of the range
+    l_continuous_range = std::max(l_continuous_range, l_big_step_alg);
+
+    // Moves to be an even number of big steps away
+    l_continuous_range -= (l_continuous_range % l_big_step_alg);
+
+    // Now converts the range/value from the continuous range - defaults to range 1
+    // If above the crossover range, use range1. If not, use range 2.
+    io_train_range = (l_continuous_range >= CROSSOVER_RANGE) ? fapi2::ENUM_ATTR_EFF_VREF_DQ_TRAIN_RANGE_RANGE1 :
+                     fapi2::ENUM_ATTR_EFF_VREF_DQ_TRAIN_RANGE_RANGE2;
+    // If above the crossover range, convert to range 1. If not use range 2
+    io_train_value = (l_continuous_range >= CROSSOVER_RANGE) ? (l_continuous_range - CROSSOVER_RANGE) :
+                     (l_continuous_range);
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+///
+/// @brief DP16 gets the VREF training values to start calibration
+/// In DD1 Nimbus in the WR VREF algorithm, the out of bounds checks are broken.
+/// One aspect of fixing this bug is to ensure that the big step is divisible by the small step
+/// This function converts the small step value over to the closest allowable value as to what was entered
+/// @param[in] i_target the fapi2 target type MCA of the port
+/// @param[in] i_big_step - WR VREF big step value
+/// @param[out] o_train_range - JEDEC MR6 WR VREF training range
+/// @param[out] o_train_value - JEDEC MR6 WR VREF training value
+/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS if ok
+///
+fapi2::ReturnCode get_train_values( const fapi2::Target<fapi2::TARGET_TYPE_MCA>& i_target,
+                                    const uint64_t i_rp,
+                                    const uint8_t i_big_step,
+                                    uint8_t& o_train_range,
+                                    uint8_t& o_train_value)
+{
+    // Declares variables
+    std::vector<uint64_t> l_ranks;
+    uint64_t l_rank = 0;
+    uint8_t l_vrefdq_train_value[MAX_DIMM_PER_PORT][MAX_RANK_PER_DIMM] = {0};
+    uint8_t l_vrefdq_train_range[MAX_DIMM_PER_PORT][MAX_RANK_PER_DIMM] = {0};
+
+    // Gets the ranks on which to latch the VREF's
+    FAPI_TRY(mss::rank::get_ranks_in_pair( i_target, i_rp, l_ranks));
+
+    // Only uses the first rank in the rank pair, as there is only one set of registers per rank pair
+    // If no ranks are configured, exit
+    l_rank = l_ranks[0];
+
+    if(l_rank == NO_RANK)
+    {
+        return fapi2::FAPI2_RC_SUCCESS;
+    }
+
+    // Now gets the train value and range for the given rank
+    FAPI_TRY( mss::eff_vref_dq_train_value(i_target, &(l_vrefdq_train_value[0][0])) );
+    FAPI_TRY( mss::eff_vref_dq_train_range(i_target, &(l_vrefdq_train_range[0][0])) );
+
+    o_train_range = l_vrefdq_train_range[l_rank / MAX_RANK_PER_DIMM][l_rank % MAX_RANK_PER_DIMM];
+    o_train_value = l_vrefdq_train_value[l_rank / MAX_RANK_PER_DIMM][l_rank % MAX_RANK_PER_DIMM];
+
+    // Converts over the values
+    FAPI_TRY(convert_train_values<fapi2::TARGET_TYPE_MCA>(i_big_step, o_train_range, o_train_value));
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+///
+/// @brief DP16 sets up the VREF train range and value
+/// In DD1 Nimbus in the WR VREF algorithm, the out of bounds checks are broken.
+/// One aspect of fixing this bug is to ensure the WR VREF is an integer number of big steps away from the 0 value and one big step from the top of the range
+/// This function converts the WR VREF values over to be good values
+/// @param[in] i_target the fapi2 target type MCA of the port
+/// @param[in] i_rp - rank pair to check and modify
+/// @param[out] o_train_range - JEDEC MR6 WR VREF training range
+/// @param[out] o_train_value - JEDEC MR6 WR VREF training value
+/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS if ok
+///
+fapi2::ReturnCode setup_values( const fapi2::Target<fapi2::TARGET_TYPE_MCA>& i_target,
+                                const uint64_t i_rp,
+                                uint8_t& o_train_range,
+                                uint8_t& o_train_value)
+{
+    // TODO RTC:160353 - Need module/chip rev EC support for workarounds
+
+    // Traits declaration
+    typedef dp16Traits<fapi2::TARGET_TYPE_MCA> TT;
+
+    // Declares variables
+    uint8_t l_big_step = 0;
+    uint8_t l_small_step = 0;
+
+    // Sets up the wr_vref_config0 register and returns out the big/small step values
+    FAPI_TRY(write_config0(i_target, l_big_step, l_small_step));
+
+    // Gets the train values
+    FAPI_TRY(get_train_values(i_target, i_rp, l_big_step, o_train_range, o_train_value));
+
+    // Sets up the WR VREF values in the RP register
+    {
+        // Creates and sets up the buffer
+        fapi2::buffer<uint64_t> l_buff;
+
+        l_buff.writeBit<TT::WR_VREF_VALUE_RANGE_DRAM_EVEN>(o_train_range)
+        .writeBit<TT::WR_VREF_VALUE_RANGE_DRAM_ODD>(o_train_range)
+        .insertFromRight<TT::WR_VREF_VALUE_VALUE_DRAM_EVEN, TT::WR_VREF_VALUE_VALUE_DRAM_EVEN_LEN>(o_train_value)
+        .insertFromRight<TT::WR_VREF_VALUE_VALUE_DRAM_ODD, TT::WR_VREF_VALUE_VALUE_DRAM_ODD_LEN>(o_train_value);
+
+        // Hits only the desired RP
+        switch(i_rp)
+        {
+            case 0:
+                FAPI_TRY(mss::scom_blastah(i_target, TT::WR_VREF_VALUE_RP0_REG, l_buff));
+                break;
+
+            case 1:
+                FAPI_TRY(mss::scom_blastah(i_target, TT::WR_VREF_VALUE_RP1_REG, l_buff));
+                break;
+
+            case 2:
+                FAPI_TRY(mss::scom_blastah(i_target, TT::WR_VREF_VALUE_RP2_REG, l_buff));
+                break;
+
+            case 3:
+                FAPI_TRY(mss::scom_blastah(i_target, TT::WR_VREF_VALUE_RP3_REG, l_buff));
+                break;
+
+            // Weird. There shouldn't be more than RP3. Error out
+            default:
+                FAPI_TRY(fapi2::FAPI2_RC_INVALID_PARAMETER, "%s RP%lu was passed in. Maximum allowable value is 3.",
+                         mss::c_str(i_target), i_rp);
+                break;
+        }
+    }
 
 fapi_try_exit:
     return fapi2::current_err;
