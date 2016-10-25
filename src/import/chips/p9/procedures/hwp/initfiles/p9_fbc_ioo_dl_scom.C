459af56ef4cca21355a4d2b21f5521250ec7d5ef
Richard J. Knight <rjknight@us.ibm.com>
2016-10-25 13:49:21 -0500

    Modify initCompiler to use template version of buffer insert
    Change-Id: Ib2e652ad78971ac12084ce1053101c081c34b590
    RTC:165584
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/33566
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/initfiles/p9_fbc_ioo_dl_scom.C
+++ src/import/chips/p9/procedures/hwp/initfiles/p9_fbc_ioo_dl_scom.C
@@ -29,21 +29,21 @@
 
 using namespace fapi2;
 
-constexpr auto literal_0xFFFFFFFFFFFFFFFF = 0xFFFFFFFFFFFFFFFF;
+constexpr uint64_t literal_0xFFFFFFFFFFFFFFFF = 0xFFFFFFFFFFFFFFFF;
 
 fapi2::ReturnCode p9_fbc_ioo_dl_scom(const fapi2::Target<fapi2::TARGET_TYPE_OBUS>& TGT0)
 {
     {
         fapi2::ATTR_OPTICS_CONFIG_MODE_Type l_TGT0_ATTR_OPTICS_CONFIG_MODE;
         FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_OPTICS_CONFIG_MODE, TGT0, l_TGT0_ATTR_OPTICS_CONFIG_MODE));
-        auto l_def_OBUS_FBC_ENABLED = (l_TGT0_ATTR_OPTICS_CONFIG_MODE == ENUM_ATTR_OPTICS_CONFIG_MODE_SMP);
+        uint64_t l_def_OBUS_FBC_ENABLED = (l_TGT0_ATTR_OPTICS_CONFIG_MODE == ENUM_ATTR_OPTICS_CONFIG_MODE_SMP);
         fapi2::buffer<uint64_t> l_scom_buffer;
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x9010803ull, l_scom_buffer ));
 
             if (l_def_OBUS_FBC_ENABLED)
             {
-                l_scom_buffer.insert<uint64_t> (literal_0xFFFFFFFFFFFFFFFF, 0, 64, 0 );
+                l_scom_buffer.insert<0, 64, 0, uint64_t>(literal_0xFFFFFFFFFFFFFFFF );
             }
 
             FAPI_TRY(fapi2::putScom(TGT0, 0x9010803ull, l_scom_buffer));
@@ -54,7 +54,7 @@ fapi2::ReturnCode p9_fbc_ioo_dl_scom(const fapi2::Target<fapi2::TARGET_TYPE_OBUS
             if (l_def_OBUS_FBC_ENABLED)
             {
                 constexpr auto l_PB_IOO_LL0_CONFIG_LINK_PAIR_ON = 0x1;
-                l_scom_buffer.insert<uint64_t> (l_PB_IOO_LL0_CONFIG_LINK_PAIR_ON, 0, 1, 63 );
+                l_scom_buffer.insert<0, 1, 63, uint64_t>(l_PB_IOO_LL0_CONFIG_LINK_PAIR_ON );
             }
 
             FAPI_TRY(fapi2::putScom(TGT0, 0x901080aull, l_scom_buffer));
