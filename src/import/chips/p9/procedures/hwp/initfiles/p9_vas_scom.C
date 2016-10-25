b04c3feecb187b77f524cfdce9a8e05f1b6ce0a6
Richard J. Knight <rjknight@us.ibm.com>
2016-10-25 13:49:21 -0500

    Modify initCompiler to use template version of buffer insert
    Change-Id: I487c32c7050c3a69bde50e73c6fe5735d7758239
    Original-Change-Id: Ib2e652ad78971ac12084ce1053101c081c34b590
    RTC:165584
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/33566
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/initfiles/p9_vas_scom.C
+++ src/import/chips/p9/procedures/hwp/initfiles/p9_vas_scom.C
@@ -29,10 +29,10 @@
 
 using namespace fapi2;
 
-constexpr auto literal_0x00000100000D7FFF = 0x00000100000D7FFF;
-constexpr auto literal_0x0000000000000FFF = 0x0000000000000FFF;
-constexpr auto literal_0x00FF0203C00C0FFF = 0x00FF0203C00C0FFF;
-constexpr auto literal_0x1 = 0x1;
+constexpr uint64_t literal_0x00000100000D7FFF = 0x00000100000D7FFF;
+constexpr uint64_t literal_0x0000000000000FFF = 0x0000000000000FFF;
+constexpr uint64_t literal_0x00FF0203C00C0FFF = 0x00FF0203C00C0FFF;
+constexpr uint64_t literal_0x1 = 0x1;
 
 fapi2::ReturnCode p9_vas_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& TGT0,
                               const fapi2::Target<fapi2::TARGET_TYPE_SYSTEM>& TGT1)
@@ -48,25 +48,25 @@ fapi2::ReturnCode p9_vas_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x3011803ull, l_scom_buffer ));
 
-            l_scom_buffer.insert<uint64_t> (literal_0x00000100000D7FFF, 0, 54, 0 );
+            l_scom_buffer.insert<0, 54, 0, uint64_t>(literal_0x00000100000D7FFF );
             FAPI_TRY(fapi2::putScom(TGT0, 0x3011803ull, l_scom_buffer));
         }
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x3011806ull, l_scom_buffer ));
 
-            l_scom_buffer.insert<uint64_t> (literal_0x0000000000000FFF, 0, 54, 0 );
+            l_scom_buffer.insert<0, 54, 0, uint64_t>(literal_0x0000000000000FFF );
             FAPI_TRY(fapi2::putScom(TGT0, 0x3011806ull, l_scom_buffer));
         }
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x3011807ull, l_scom_buffer ));
 
-            l_scom_buffer.insert<uint64_t> (literal_0x00FF0203C00C0FFF, 0, 54, 0 );
+            l_scom_buffer.insert<0, 54, 0, uint64_t>(literal_0x00FF0203C00C0FFF );
             FAPI_TRY(fapi2::putScom(TGT0, 0x3011807ull, l_scom_buffer));
         }
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x301184dull, l_scom_buffer ));
 
-            l_scom_buffer.insert<uint64_t> (literal_0x1, 19, 1, 63 );
+            l_scom_buffer.insert<19, 1, 63, uint64_t>(literal_0x1 );
             FAPI_TRY(fapi2::putScom(TGT0, 0x301184dull, l_scom_buffer));
         }
         {
@@ -75,12 +75,12 @@ fapi2::ReturnCode p9_vas_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
             if ((l_TGT1_ATTR_PROC_FABRIC_ADDR_BAR_MODE == fapi2::ENUM_ATTR_PROC_FABRIC_ADDR_BAR_MODE_SMALL_SYSTEM))
             {
                 constexpr auto l_VA_VA_SOUTH_VA_EG_EG_SCF_ADDR_BAR_MODE_ON = 0x1;
-                l_scom_buffer.insert<uint64_t> (l_VA_VA_SOUTH_VA_EG_EG_SCF_ADDR_BAR_MODE_ON, 13, 1, 63 );
+                l_scom_buffer.insert<13, 1, 63, uint64_t>(l_VA_VA_SOUTH_VA_EG_EG_SCF_ADDR_BAR_MODE_ON );
             }
             else if ((l_TGT1_ATTR_PROC_FABRIC_ADDR_BAR_MODE == fapi2::ENUM_ATTR_PROC_FABRIC_ADDR_BAR_MODE_LARGE_SYSTEM))
             {
                 constexpr auto l_VA_VA_SOUTH_VA_EG_EG_SCF_ADDR_BAR_MODE_OFF = 0x0;
-                l_scom_buffer.insert<uint64_t> (l_VA_VA_SOUTH_VA_EG_EG_SCF_ADDR_BAR_MODE_OFF, 13, 1, 63 );
+                l_scom_buffer.insert<13, 1, 63, uint64_t>(l_VA_VA_SOUTH_VA_EG_EG_SCF_ADDR_BAR_MODE_OFF );
             }
 
             FAPI_TRY(fapi2::putScom(TGT0, 0x301184eull, l_scom_buffer));
@@ -88,7 +88,7 @@ fapi2::ReturnCode p9_vas_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x301184full, l_scom_buffer ));
 
-            l_scom_buffer.insert<uint64_t> (literal_0x1, 0, 1, 63 );
+            l_scom_buffer.insert<0, 1, 63, uint64_t>(literal_0x1 );
             FAPI_TRY(fapi2::putScom(TGT0, 0x301184full, l_scom_buffer));
         }
 
