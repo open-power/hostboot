9c8141ec796a5cc63ec70b71c5ab1d79a91ba922
Zane Shelley <zshelle@us.ibm.com>
2016-11-10 15:13:01 -0600

    PRD: combine RT/IPL imp of MemTdCtlr::handleCmdComplete to common file
    The functions was more common than not so it made sense to combine the
    functions and use macros to control the small differences.
    
    Change-Id: I9d2afbd8390bd806fd0fbc42341565da0772ca03
    RTC: 157892
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/32508
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Caleb N. Palmer <cnpalmer@us.ibm.com>
    Reviewed-by: Benjamin J. Weisenbeck <bweisenb@us.ibm.com>
    Reviewed-by: Zane C. Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/33078
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>

--- src/usr/diag/prdf/plat/mem/prdfMemTdCtlr_ipl.C
+++ src/usr/diag/prdf/plat/mem/prdfMemTdCtlr_ipl.C
@@ -31,7 +31,6 @@
 
 // Platform includes
 #include <prdfMemScrubUtils.H>
-#include <prdfP9McbistExtraSig.H>
 
 using namespace TARGETING;
 
@@ -39,93 +38,6 @@ namespace PRDF
 {
 
 using namespace PlatServices;
-
-//------------------------------------------------------------------------------
-
-template <TARGETING::TYPE T>
-uint32_t MemTdCtlr<T>::handleCmdComplete( STEP_CODE_DATA_STRUCT & io_sc )
-{
-    #define PRDF_FUNC "[MemTdCtlr::handleCmdComplete] "
-
-    PRDF_ASSERT( isInMdiaMode() ); // MDIA must be running.
-
-    uint32_t o_rc = SUCCESS;
-
-    TargetHandle_t trgt = iv_chip->getTrgt();
-
-    do
-    {
-        // Inform MDIA the command has completed and PRD is starting analysis.
-        o_rc = mdiaSendEventMsg( trgt, MDIA::RESET_TIMER );
-        if ( SUCCESS != o_rc )
-        {
-            PRDF_ERR( PRDF_FUNC "mdiaSendEventMsg(RESET_TIMER) failed" );
-            break;
-        }
-
-        if ( nullptr == iv_curProcedure )
-        {
-            // There is nothing currently in progress, which means the command
-            // either stopped at the end of memory successfully (nothing to do,
-            // so call nextStep() to initiate defaultStep()) or stopped at the
-            // end of a rank due to an error (will need to add procedures to the
-            // queue to handle the errors and finish the pattern test to the end
-            // of memory).
-
-            // Keep track of where the command stopped.
-            o_rc = initStoppedRank();
-            if ( SUCCESS != o_rc )
-            {
-                PRDF_ERR( PRDF_FUNC "initStoppedRank() failed" );
-                break;
-            }
-
-            // TODO: RTC 157892 Check why the command stopped and take actions
-            //       appropriately. Note that since nothing is happening here at
-            //       the moment, the code will simply assume the command stopped
-            //       at the end of memory with no errors.
-
-            // If the command completed successfully with no error, do not
-            // commit the error log. This is done to suppress unnecessary
-            // information error logs.
-            io_sc.service_data->setDontCommitErrl();
-        }
-
-        // Move onto the next step in the state machine.
-        o_rc = nextStep( io_sc );
-        if ( SUCCESS != o_rc )
-        {
-            PRDF_ERR( PRDF_FUNC "nextStep() failed" );
-            break;
-        }
-
-    } while (0);
-
-    if ( SUCCESS != o_rc )
-    {
-        PRDF_ERR( PRDF_FUNC "Failed on 0x%08x", iv_chip->getHuid() );
-
-        // Just in case it was a legitimate command complete (error log not
-        // committed) but something else failed.
-        io_sc.service_data->clearDontCommitErrl();
-
-        // Change signature indicating there was an error in analysis.
-        io_sc.service_data->SetErrorSig( PRDFSIG_CmdComplete_ERROR );
-
-        // Something definitely failed, so callout 2nd level support.
-        io_sc.service_data->SetCallout( NextLevelSupport_ENUM, MRU_HIGH );
-        io_sc.service_data->setServiceCall();
-
-        // Tell MDIA to skip further analysis on this target.
-        uint32_t l_rc = mdiaSendEventMsg( trgt, MDIA::STOP_TESTING );
-        if ( SUCCESS != l_rc )
-            PRDF_ERR( PRDF_FUNC "mdiaSendEventMsg(STOP_TESTING) failed" );
-    }
-
-    return o_rc;
-
-    #undef PRDF_FUNC
-}
 
 //------------------------------------------------------------------------------
 
