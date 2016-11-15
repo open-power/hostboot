1d3335a4799f2523fa0b186dd9c58053319b0095
Caleb Palmer <cnpalmer@us.ibm.com>
2016-11-15 15:09:05 -0600

    PRD: DRAM Repairs IPL VCM Procedure (MCBIST)
    Change-Id: I8fd205308c547e113c6e3d152c321c9719f5c529
    RTC: 157608
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/32394
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Benjamin J. Weisenbeck <bweisenb@us.ibm.com>
    Reviewed-by: Zane C. Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/33082
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>

--- src/usr/diag/prdf/plat/mem/prdfMemVcm_ipl.C
+++ src/usr/diag/prdf/plat/mem/prdfMemVcm_ipl.C
@@ -27,15 +27,19 @@
 
 // Platform includes
 #include <prdfMemVcm.H>
+#include <prdfMemScrubUtils.H>
+#include <prdfPlatServices.H>
+#include <prdfP9McaExtraSig.H>
 
 using namespace TARGETING;
 
 namespace PRDF
 {
 
+using namespace PlatServices;
+
 //------------------------------------------------------------------------------
 
-// TODO: RTC 157608 Actual implementation of this procedure will be done later.
 template<>
 uint32_t VcmEvent<TYPE_MCA>::nextStep( STEP_CODE_DATA_STRUCT & io_sc,
                                        bool & o_done )
@@ -43,10 +47,161 @@ uint32_t VcmEvent<TYPE_MCA>::nextStep( STEP_CODE_DATA_STRUCT & io_sc,
     #define PRDF_FUNC "[VcmEvent<TYPE_MCA>::nextStep] "
 
     uint32_t o_rc = SUCCESS;
+    o_done = false;
 
-    o_done = true;
+    // add iv_mark to the callout list
+    MemoryMru memmru( iv_chip->getTrgt(), iv_rank, iv_mark.getSymbol() );
+    io_sc.service_data->SetCallout( memmru );
 
-    PRDF_ERR( PRDF_FUNC "function not implemented yet" );
+    do
+    {
+        //phase 0
+        if ( TD_PHASE_0 == iv_phase )
+        {
+            //start VCM phase 1
+            o_rc = startVcmPhase1<TYPE_MCA>( iv_chip, iv_rank );
+            if ( SUCCESS != o_rc )
+            {
+                PRDF_ERR( PRDF_FUNC "Call to 'startVcmPhase1 failed on chip: "
+                          "0x%08x", iv_chip->getHuid() );
+                break;
+            }
+            iv_phase = TD_PHASE_1;
+        }
+        //phase 1
+        else if ( TD_PHASE_1 == iv_phase )
+        {
+            //get the ecc attentions
+            uint32_t eccAttns;
+            o_rc = checkEccFirs<TYPE_MCA>( iv_chip, eccAttns );
+            if ( SUCCESS != o_rc )
+            {
+                PRDF_ERR( PRDF_FUNC "Call to 'checkEccFirs' failed on chip: "
+                          "0x%08x", iv_chip->getHuid() );
+                break;
+            }
+
+            //if there was a UE or IUE
+            if ( (eccAttns & MAINT_UE) || (eccAttns & MAINT_IUE) )
+            {
+                //UE
+                if ( eccAttns & MAINT_UE )
+                {
+                    io_sc.service_data->setSignature( iv_chip->getHuid(),
+                                                      PRDFSIG_MaintUE );
+                }
+                //IUE
+                else
+                {
+                    io_sc.service_data->setSignature( iv_chip->getHuid(),
+                                                      PRDFSIG_MaintIUE );
+                }
+
+                //Add the rank to the callout list (via MemoryMru)
+                MemoryMru memmru(iv_chip->getTrgt(), iv_rank,
+                        MemoryMruData::CALLOUT_RANK);
+                io_sc.service_data->SetCallout( memmru );
+
+                //Make the error log predictive
+                io_sc.service_data->setServiceCall();
+
+                //Leave the mark in place
+                //Abort this procedure
+                o_done = true;
+            }
+            else
+            {
+                //Start VCM phase 2
+                o_rc = startVcmPhase2<TYPE_MCA>( iv_chip, iv_rank );
+                if ( SUCCESS != o_rc )
+                {
+                    PRDF_ERR( PRDF_FUNC "Call to 'startVcmPhase2 failed on "
+                              "chip: 0x%08x", iv_chip->getHuid() );
+                    break;
+                }
+                iv_phase = TD_PHASE_2;
+            }
+        }
+        //phase 2
+        else if ( TD_PHASE_2 == iv_phase )
+        {
+            //get the ecc attentions
+            uint32_t eccAttns;
+            o_rc = checkEccFirs<TYPE_MCA>( iv_chip, eccAttns );
+            if ( SUCCESS != o_rc )
+            {
+                PRDF_ERR( PRDF_FUNC "Call to 'checkEccFirs' failed on chip: "
+                          "0x%08x", iv_chip->getHuid() );
+                break;
+            }
+
+            //If DRAM repairs is disabled (via MNFG policy)
+            if ( areDramRepairsDisabled() )
+            {
+                //Make the error log predictive
+                io_sc.service_data->setServiceCall();
+            }
+
+            //if there was a UE or IUE
+            if ( (eccAttns & MAINT_UE) || (eccAttns & MAINT_IUE) )
+            {
+                //UE
+                if ( eccAttns & MAINT_UE )
+                {
+                    io_sc.service_data->setSignature( iv_chip->getHuid(),
+                                                      PRDFSIG_MaintUE );
+                }
+                //IUE
+                else
+                {
+                    io_sc.service_data->setSignature( iv_chip->getHuid(),
+                                                      PRDFSIG_MaintIUE );
+                }
+
+                //Add the rank to the callout list (via MemoryMru)
+                MemoryMru memmru(iv_chip->getTrgt(), iv_rank,
+                        MemoryMruData::CALLOUT_RANK);
+                io_sc.service_data->SetCallout( memmru );
+
+                //Make the error log predictive
+                io_sc.service_data->setServiceCall();
+
+                //Leave the mark in place
+            }
+            //else if there was a MCE
+            else if ( eccAttns & MAINT_MCE )
+            {
+                //The chip mark is verified
+                io_sc.service_data->setSignature( iv_chip->getHuid(),
+                                                  PRDFSIG_VcmVerified );
+
+                //Update the DRAM repairs VPD to indicate the entire DRAM is bad
+                //TODO ?????? - updating VPD not yet supported
+
+            }
+            //else - verification failed
+            else
+            {
+                io_sc.service_data->setSignature( iv_chip->getHuid(),
+                                                  PRDFSIG_VcmFalseAlarm );
+
+                //Remove the chip mark
+                o_rc = MarkStore::clearChipMark<TYPE_MCA>( iv_chip, iv_rank );
+                if ( SUCCESS != o_rc )
+                {
+                    PRDF_ERR( PRDF_FUNC "Failure to clear chip mark on chip: "
+                              "0x%08x", iv_chip->getHuid() );
+                }
+
+            }
+            //Abort the procedure
+            o_done = true;
+        }
+        else
+        {
+            PRDF_ASSERT( false ); //invalid value in iv_phase
+        }
+    }while(0);
 
     return o_rc;
 
