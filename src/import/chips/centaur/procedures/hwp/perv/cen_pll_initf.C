86e63c64cbb24796f1c7e36416226ac166405a5c
Thi Tran <thi@us.ibm.com>
2016-11-16 09:31:03 -0600

    Centaur istep 11 support
    initCompiler
      generate procedures & wrapper for Centaur SCAN initfiles
      porting Centaur initfiles into p9 for Centaur EC 21
      update Initfile Compiler to generate wrapper and C/H Scan files for Centaur
      generate rings data to be pulled in Centaur HW image.
    
    centaur_ec_attributes
      remove all attributes previously used by SBE code -- only DD2 is supported
    
    cen_common_funcs
      cleanup trace messages
      add common clockstart module for NEST/MEM
    
    centaur_misc_constants
      define constants for PLL scan0 operations
      define MEM scan chiplet constant
    
    cen_tp_chiplet_init1
      remove EC feature attribute qualifications -- only DD2 is supported
      scan0 flush TP PLL GPTR BNDY and FUNC ring
    
    cen_pll_initf
    cen_initf
      directly invoke generated scan HWPs until putRing support exists
    
    cen_chiplet_init
      remove EC feature attribute qualifications -- only DD2 is supported
    
    cen_startclocks
      initial release
    
    centaur.cleanup.pll.scan.initfile
      right justify spy value to match p9 initCompiler expectations
    
    centaur.dmi.scan.initfile
      update owner
    
    centaur.mba.scan.initfile
      remove EC feature attribute qualifications -- only DD2 is supported
    
    centaur.mbs.scan.initfile
      remove EC feature attribute qualifications -- only DD2 is supported
      add content missing from original port
    
    centaur.mem.pll.scan.initfile
      remove dummy settings
    
    centaur.nest.pll.scan.initfile
      adjust freq attributes to match P9 NEST attribute content
      remove dummy settings
    
    centaur.thermal.scan.initfile
      update owner
      add content missing from original port
    
    cen_mem_pll_setup
    cen_nest_startclocks
      remove, not required for P9 IPL flow
    
    add missing object depedencies
    create wrappers for all planned isteps
    
    RTC 163457
    
    Change-Id: I7f0640d37c567c2d870a26903dd6d2ebaeb7e97c
    Original-Change-Id: Idd0972207f356315f1e2f52f911e1cb27f72a737
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/32714
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Richard J. Knight <rjknight@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/43840
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/centaur/procedures/hwp/perv/cen_pll_initf.C
@@ -0,0 +1,115 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/centaur/procedures/hwp/perv/cen_pll_initf.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2017                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file cen_pll_initf.C
+/// @brief Centaur PLL initf (FAPI2)
+///
+/// @author Peng Fei GOU <shgoupf@cn.ibm.com>
+///
+
+//
+// *HWP HWP Owner: Peng Fei GOU <shgoupf@cn.ibm.com>
+// *HWP FW Owner: Thi Tran <thi@us.ibm.com>
+// *HWP Team: Perv
+// *HWP Level: 2
+// *HWP Consumed by: HB
+//
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <cen_pll_initf.H>
+#include <cen_gen_scom_addresses.H>
+#include <cen_gen_scom_addresses_fixes.H>
+#include <centaur_misc_constants.H>
+
+#ifndef __HOSTBOOT_MODULE
+    #include <centaur_cleanup_pll_scan.H>
+    #include <centaur_nest_pll_scan.H>
+    #include <centaur_mem_pll_scan.H>
+#endif
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+cen_pll_initf(const fapi2::Target<fapi2::TARGET_TYPE_MEMBUF_CHIP>& i_target)
+{
+    FAPI_DBG("Start");
+    fapi2::buffer<uint64_t> l_clk_region = 0;
+
+#ifndef __HOSTBOOT_MODULE
+    fapi2::Target<fapi2::TARGET_TYPE_SYSTEM> FAPI_SYSTEM;
+    fapi2::ReturnCode l_rc;
+    // apply initfiles
+    ecmdChipTarget l_ecmd_target;
+    fapiTargetToEcmdTarget(i_target, l_ecmd_target);
+    ecmdEnableRingCache(l_ecmd_target);
+    FAPI_EXEC_HWP(l_rc, centaur_cleanup_pll_scan, i_target, FAPI_SYSTEM);
+
+    if (l_rc)
+    {
+        FAPI_ERR("Error from centaur_cleanup_pll_scan");
+        fapi2::current_err = l_rc;
+        goto fapi_try_exit;
+    }
+
+    FAPI_EXEC_HWP(l_rc, centaur_mem_pll_scan, i_target, FAPI_SYSTEM);
+
+    if (l_rc)
+    {
+        FAPI_ERR("Error from centaur_mem_pll_scan");
+        fapi2::current_err = l_rc;
+        goto fapi_try_exit;
+    }
+
+    FAPI_EXEC_HWP(l_rc, centaur_nest_pll_scan, i_target, FAPI_SYSTEM);
+
+    if (l_rc)
+    {
+        FAPI_ERR("Error from centaur_nest_pll_scan");
+        fapi2::current_err = l_rc;
+        goto fapi_try_exit;
+    }
+
+    ecmdDisableRingCache(l_ecmd_target);
+#endif
+
+    // issue setpulse
+    l_clk_region.setBit<0, 2>();  // CLOCK_CMD = pulse
+    l_clk_region.setBit<4>();     // CLOCK_REGION_PERV
+    l_clk_region.setBit<11>();    // CLOCK_REGION_PLL
+    l_clk_region.setBit<21>();    // SEL_THOLD_NSL
+    FAPI_TRY(fapi2::putScom(i_target, CEN_CLK_REGION_PCB, l_clk_region),
+             "Error from putScom (CEN_CLK_REGION_PCB, setpulse)");
+    l_clk_region.flush<0>();
+    FAPI_TRY(fapi2::putScom(i_target, CEN_CLK_REGION_PCB, l_clk_region),
+             "Error from putScom (CEN_CLK_REGION_PCB, clear)");
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
