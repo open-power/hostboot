0fef5f9c47fb8d38bc7d81000f00063b4ca2b286
Andre Marin <aamarin@us.ibm.com>
2016-12-01 03:56:02 -0600

    Add common functionality between RCD and data buffer control word API
    Change-Id: I7c43ecd6247ff50a4fede86bd8370bf764e0a03b
    Original-Change-Id: I601f143578a796f30ad582c1581f73ebf413840c
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/33247
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: Brian R. Silver <bsilver@us.ibm.com>
    Reviewed-by: JACOB L. HARVEY <jlharvey@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/lib/dimm/rcd_load_ddr4.C
+++ src/import/chips/p9/procedures/hwp/memory/lib/dimm/rcd_load_ddr4.C
@@ -37,6 +37,7 @@
 
 #include <mss.H>
 #include <lib/dimm/rcd_load_ddr4.H>
+#include <lib/dimm/ddr4/control_word_ddr4.H>
 
 using fapi2::TARGET_TYPE_MCBIST;
 using fapi2::TARGET_TYPE_MCA;
@@ -51,100 +52,59 @@ namespace mss
 ///
 /// @brief Perform the rcd_load_ddr4 operations - TARGET_TYPE_DIMM specialization
 /// @param[in] i_target, a fapi2::Target<TARGET_TYPE_DIMM>
-/// @param[in] a vector of CCS instructions we should add to
+/// @param[in,out] a vector of CCS instructions we should add to
 /// @return FAPI2_RC_SUCCESS if and only if ok
 ///
 fapi2::ReturnCode rcd_load_ddr4( const fapi2::Target<TARGET_TYPE_DIMM>& i_target,
-                                 std::vector< ccs::instruction_t<TARGET_TYPE_MCBIST> >& i_inst)
+                                 std::vector< ccs::instruction_t<TARGET_TYPE_MCBIST> >& io_inst)
 {
     FAPI_INF("rcd_load_ddr4 %s", mss::c_str(i_target));
 
     // Per DDR4RCD02, tSTAB is us. We want this in cycles for the CCS.
     const uint64_t tSTAB = mss::us_to_cycles(i_target, mss::tstab());
+    constexpr uint8_t FS0 = 0; // Function space 0
 
-    static const std::vector< rcd_data > l_rcd_4bit_data =
+    // RCD 4-bit data - integral represents rc#
+    static const std::vector< cw_data > l_rcd_4bit_data =
     {
-        {  0, eff_dimm_ddr4_rc00, mss::tmrd()    }, {  1, eff_dimm_ddr4_rc01, mss::tmrd()   },
-        {  2, eff_dimm_ddr4_rc02, tSTAB          }, {  3, eff_dimm_ddr4_rc03, mss::tmrd_l() },
-        {  4, eff_dimm_ddr4_rc04, mss::tmrd_l()  }, {  5, eff_dimm_ddr4_rc05, mss::tmrd_l() },
-        {  6, eff_dimm_ddr4_rc06_07, mss::tmrd() }, {  8, eff_dimm_ddr4_rc08, mss::tmrd()   },
-        {  9, eff_dimm_ddr4_rc09, mss::tmrd()    }, { 10, eff_dimm_ddr4_rc10, tSTAB         },
-        { 11, eff_dimm_ddr4_rc11, mss::tmrd()    }, { 12, eff_dimm_ddr4_rc12, mss::tmrd()   },
-        { 13, eff_dimm_ddr4_rc13, mss::tmrd()    }, { 14, eff_dimm_ddr4_rc14, mss::tmrd()   },
-        { 15, eff_dimm_ddr4_rc15, mss::tmrd()    },
+        {  FS0, 0, eff_dimm_ddr4_rc00, mss::tmrd()     },
+        {  FS0, 1, eff_dimm_ddr4_rc01, mss::tmrd()     },
+        {  FS0, 2, eff_dimm_ddr4_rc02, tSTAB           },
+        {  FS0, 3, eff_dimm_ddr4_rc03, mss::tmrd_l()   },
+        {  FS0, 4, eff_dimm_ddr4_rc04, mss::tmrd_l()   },
+        {  FS0, 5, eff_dimm_ddr4_rc05, mss::tmrd_l()   },
+        {  FS0, 6, eff_dimm_ddr4_rc06_07, mss::tmrd()  },
+        {  FS0, 8, eff_dimm_ddr4_rc08, mss::tmrd()     },
+        {  FS0, 9, eff_dimm_ddr4_rc09, mss::tmrd()     },
+        {  FS0, 10, eff_dimm_ddr4_rc10, tSTAB          },
+        {  FS0, 11, eff_dimm_ddr4_rc11, mss::tmrd()    },
+        {  FS0, 12, eff_dimm_ddr4_rc12, mss::tmrd()    },
+        {  FS0, 13, eff_dimm_ddr4_rc13, mss::tmrd()    },
+        {  FS0, 14, eff_dimm_ddr4_rc14, mss::tmrd()    },
+        {  FS0, 15, eff_dimm_ddr4_rc15, mss::tmrd()    },
     };
 
-    static const std::vector< rcd_data > l_rcd_8bit_data =
+    // RCD 8-bit data - integral represents rc#
+    static const std::vector< cw_data > l_rcd_8bit_data =
     {
-        {  1, eff_dimm_ddr4_rc_1x, mss::tmrd()  }, {  2, eff_dimm_ddr4_rc_2x, mss::tmrd()  },
-        {  3, eff_dimm_ddr4_rc_3x, tSTAB        }, {  4, eff_dimm_ddr4_rc_4x, mss::tmrd()  },
-        {  5, eff_dimm_ddr4_rc_5x, mss::tmrd()  }, {  6, eff_dimm_ddr4_rc_6x, mss::tmrd()  },
-        {  7, eff_dimm_ddr4_rc_7x, mss::tmrd()  }, {  8, eff_dimm_ddr4_rc_8x, mss::tmrd()  },
-        {  9, eff_dimm_ddr4_rc_9x, mss::tmrd()  }, { 10, eff_dimm_ddr4_rc_ax, mss::tmrd()  },
-        { 11, eff_dimm_ddr4_rc_bx, mss::tmrd_l() }
+        {  FS0, 1, eff_dimm_ddr4_rc_1x, mss::tmrd()     },
+        {  FS0, 2, eff_dimm_ddr4_rc_2x, mss::tmrd()     },
+        {  FS0, 3, eff_dimm_ddr4_rc_3x, tSTAB           },
+        {  FS0, 4, eff_dimm_ddr4_rc_4x, mss::tmrd()     },
+        {  FS0, 5, eff_dimm_ddr4_rc_5x, mss::tmrd()     },
+        {  FS0, 6, eff_dimm_ddr4_rc_6x, mss::tmrd()     },
+        {  FS0, 7, eff_dimm_ddr4_rc_7x, mss::tmrd()     },
+        {  FS0, 8, eff_dimm_ddr4_rc_8x, mss::tmrd()     },
+        {  FS0, 9, eff_dimm_ddr4_rc_9x, mss::tmrd()     },
+        {  FS0, 10, eff_dimm_ddr4_rc_ax, mss::tmrd()    },
+        {  FS0, 11, eff_dimm_ddr4_rc_bx, mss::tmrd_l()  }
     };
 
-    fapi2::buffer<uint8_t> l_value;
+    // Load 4-bit data
+    FAPI_TRY( control_word_engine<RCW_4BIT>(i_target, l_rcd_4bit_data, io_inst) );
 
-    // A little 4bit RCD love ...
-    for (const auto& d : l_rcd_4bit_data)
-    {
-        // Keep in mind that swizzles count back from SWIZZLE_START for the apprporiate length in bits
-        constexpr uint64_t DATA_LEN = 4;
-        constexpr uint64_t WORD_LEN = 4;
-        constexpr uint64_t SWIZZLE_START = 7;
-
-        // Note: this isn't general - assumes Nimbus via MCBIST instruction here BRS
-        ccs::instruction_t<TARGET_TYPE_MCBIST> l_inst = ccs::rcd_command<TARGET_TYPE_MCBIST>(i_target);
-        FAPI_TRY( d.iv_func(i_target, l_value) );
-
-        // Don't care if the value is 0 - send it anyway
-        // Data to be written into the 4-bit configuration registers need to be presented on DA0 .. DA3
-        mss::swizzle<MCBIST_CCS_INST_ARR0_00_DDR_ADDRESS_0_13,
-            DATA_LEN, SWIZZLE_START>(l_value, l_inst.arr0);
-
-        // Selection of each word of 4-bit control bits is presented on inputs DA4 through DA12
-        mss::swizzle < MCBIST_CCS_INST_ARR0_00_DDR_ADDRESS_0_13 + DATA_LEN,
-            WORD_LEN, SWIZZLE_START > (d.iv_rcd, l_inst.arr0);
-
-        // For changes to the control word setting [...] the controller needs to wait tMRD[tSTAB] after
-        // the last control word access, before further access to the DRAM can take place.
-        l_inst.arr1.insertFromRight<MCBIST_CCS_INST_ARR1_00_IDLES, MCBIST_CCS_INST_ARR1_00_IDLES_LEN>(d.iv_delay);
-
-        FAPI_INF("RCD%02d value 0x%x (%d) 0x%016llx:0x%016llx %s", uint8_t(d.iv_rcd), l_value, d.iv_delay,
-                 l_inst.arr0, l_inst.arr1, mss::c_str(i_target));
-        i_inst.push_back(l_inst);
-    }
-
-    // 8bit's turn
-    for (auto d : l_rcd_8bit_data)
-    {
-        // Keep in mind that swizzles count back from SWIZZLE_START for the apprporiate length in bits
-        constexpr uint64_t DATA_LEN = 8;
-        constexpr uint64_t WORD_LEN = 5;
-        constexpr uint64_t SWIZZLE_START = 7;
-
-        // Note: this isn't general - assumes Nimbus via MCBIST instruction here BRS
-        ccs::instruction_t<TARGET_TYPE_MCBIST> l_inst = ccs::rcd_command<TARGET_TYPE_MCBIST>(i_target);
-        FAPI_TRY( d.iv_func(i_target, l_value) );
-
-        // Don't care if the value is 0 - send it anyway
-        // Data to be written into the 8-bit configuration registers need to be presented on DA0 .. DA7
-        mss::swizzle<MCBIST_CCS_INST_ARR0_00_DDR_ADDRESS_0_13,
-            DATA_LEN, SWIZZLE_START>(l_value, l_inst.arr0);
-
-        // Selection of each word of 8-bit control bits is presented on inputs DA8 through DA12.
-        mss::swizzle < MCBIST_CCS_INST_ARR0_00_DDR_ADDRESS_0_13 + DATA_LEN,
-            WORD_LEN, SWIZZLE_START > (d.iv_rcd, l_inst.arr0);
-
-        // For changes to the control word setting [...] the controller needs to wait tMRD[tSTAB] after
-        // the last control word access, before further access to the DRAM can take place.
-        l_inst.arr1.insertFromRight<MCBIST_CCS_INST_ARR1_00_IDLES, MCBIST_CCS_INST_ARR1_00_IDLES_LEN>(d.iv_delay);
-
-        FAPI_INF("RCD%XX value 0x%x (%d) 0x%016llx:0x%016llx %s", uint8_t(d.iv_rcd), l_value, d.iv_delay,
-                 l_inst.arr0, l_inst.arr1, mss::c_str(i_target));
-        i_inst.push_back(l_inst);
-    }
+    // Load 8-bit data
+    FAPI_TRY( control_word_engine<RCW_8BIT>(i_target, l_rcd_8bit_data, io_inst) );
 
 fapi_try_exit:
     return fapi2::current_err;
