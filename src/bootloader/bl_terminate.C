9a5f582e7b98764ec90b1cb015429b14d3326ead
Marty Gloff <mgloff@us.ibm.com>
2016-12-28 15:11:49 -0600

    Bootloader - Error handling - Add exception handling
    As a second step for Bootloader error handling add support for various
    exception vectors and handle these exceptions by calling the terminate
    function.
    
    Change-Id: Ib39818fff377da3fa93d32112f629238a82625ad
    RTC:135746
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/34239
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Nicholas E. Bofferding <bofferdn@us.ibm.com>
    Reviewed-by: Elizabeth K. Liner <eliner@us.ibm.com>
    Reviewed-by: Corey V. Swenson <cswenson@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/bootloader/bl_terminate.C
+++ src/bootloader/bl_terminate.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2016                             */
+/* Contributors Listed Below - COPYRIGHT 2016,2017                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -23,9 +23,122 @@
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
 #include <bootloader/bootloader.H>
+#include <bootloader/hbblreasoncodes.H>
+#include <arch/ppc.H>
 
 #define bl_terminate_C
 
 #include <../kernel/terminate.C>
+
+#define UINT64_HIGH(data) ((data & 0xFFFFFFFF00000000) >> 32)
+#define UINT64_LOW(data) (data & 0x00000000FFFFFFFF)
+#define WORD7_WORD8(data) UINT64_HIGH(data), UINT64_LOW(data)
+
+
+extern "C"
+void kernel_std_exception()
+{
+    uint64_t exception = getSPRG2();
+    uint64_t nip = getSRR0();
+
+    /*@
+     * @errortype
+     * @moduleid     MOD_BOOTLOADER_TERMINATE
+     * @reasoncode   RC_STD_EXCEPTION
+     * @userdata1[0:15]   TI_WITH_SRC
+     * @userdata1[16:31]  TI_BOOTLOADER
+     * @userdata1[32:63]  Exception vector address
+     * @userdata2    NIP / SRR0
+     * @devdesc      Exception occurred during execution of HBBL
+     * @custdesc     A problem occurred while running processor
+     *               boot code.
+     */
+    bl_terminate(Bootloader::MOD_BOOTLOADER_TERMINATE,
+                 Bootloader::RC_STD_EXCEPTION,
+                 WORD7_WORD8(nip),
+                 true,
+                 exception);
+}
+
+extern "C"
+void kernel_std_exception_w_dsisr()
+{
+    uint64_t exception = getSPRG2();
+    uint64_t nip = getSRR0();
+    uint64_t dsisr = getDSISR();
+
+    /*@
+     * @errortype
+     * @moduleid     MOD_BOOTLOADER_TERMINATE
+     * @reasoncode   RC_STD_EX_W_DSISR
+     * @userdata1[0:15]   TI_WITH_SRC
+     * @userdata1[16:31]  TI_BOOTLOADER
+     * @userdata1[32:63]  Exception vector address
+     * @userdata2    NIP / SRR0
+     * @devdesc      Exception occurred during execution of HBBL
+     * @custdesc     A problem occurred while running processor
+     *               boot code.
+     */
+    kernel_TIDataArea.src.SRCword3 = UINT64_HIGH(dsisr);
+    kernel_TIDataArea.src.SRCword4 = UINT64_LOW(dsisr);
+    bl_terminate(Bootloader::MOD_BOOTLOADER_TERMINATE,
+                 Bootloader::RC_STD_EX_W_DSISR,
+                 WORD7_WORD8(nip),
+                 true,
+                 exception);
+}
+
+extern "C"
+void kernel_std_exception_w_srr1()
+{
+    uint64_t exception = getSPRG2();
+    uint64_t nip = getSRR0();
+    uint64_t srr1 = getSRR1();
+
+    /*@
+     * @errortype
+     * @moduleid     MOD_BOOTLOADER_TERMINATE
+     * @reasoncode   RC_STD_EX_W_SRR1
+     * @userdata1[0:15]   TI_WITH_SRC
+     * @userdata1[16:31]  TI_BOOTLOADER
+     * @userdata1[32:63]  Exception vector address
+     * @userdata2    NIP / SRR0
+     * @devdesc      Exception occurred during execution of HBBL
+     * @custdesc     A problem occurred while running processor
+     *               boot code.
+     */
+    kernel_TIDataArea.src.SRCword3 = UINT64_HIGH(srr1);
+    kernel_TIDataArea.src.SRCword4 = UINT64_LOW(srr1);
+    bl_terminate(Bootloader::MOD_BOOTLOADER_TERMINATE,
+                 Bootloader::RC_STD_EX_W_SRR1,
+                 WORD7_WORD8(nip),
+                 true,
+                 exception);
+}
+
+extern "C"
+void kernel_hype_exception()
+{
+    uint64_t exception = getSPRG2();
+    uint64_t nip = getHSRR0();
+
+    /*@
+     * @errortype
+     * @moduleid     MOD_BOOTLOADER_TERMINATE
+     * @reasoncode   RC_HYPE_EXCEPTION
+     * @userdata1[0:15]   TI_WITH_SRC
+     * @userdata1[16:31]  TI_BOOTLOADER
+     * @userdata1[32:63]  Exception vector address
+     * @userdata2    NIP / HSRR0
+     * @devdesc      Exception occurred during execution of HBBL
+     * @custdesc     A problem occurred while running processor
+     *               boot code.
+     */
+    bl_terminate(Bootloader::MOD_BOOTLOADER_TERMINATE,
+                 Bootloader::RC_HYPE_EXCEPTION,
+                 WORD7_WORD8(nip),
+                 true,
+                 exception);
+}
 
 #undef bl_terminate_C
