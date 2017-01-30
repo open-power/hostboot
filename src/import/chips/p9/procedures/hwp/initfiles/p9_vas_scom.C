99e91f7b8149e6fbefa211c989bb2fcbabb3989c
Juan Medina <jr.medina@us.ibm.com>
2017-01-30 10:17:10 -0600

    reverting FIRs to master values, setting only bit 8
    Change-Id: I7a78e01239f827fb94f62a0aa5ce4971f248e603
    Original-Change-Id: I9ff37faffad1c6c2323c501a5c55992a81fc9fd8
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/35575
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Matt K. Light <mklight@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/initfiles/p9_vas_scom.C
+++ src/import/chips/p9/procedures/hwp/initfiles/p9_vas_scom.C
@@ -32,6 +32,8 @@ using namespace fapi2;
 constexpr uint64_t literal_0x00200102000D7FFF = 0x00200102000D7FFF;
 constexpr uint64_t literal_0x0000000000000000 = 0x0000000000000000;
 constexpr uint64_t literal_0x00DF0201C0000000 = 0x00DF0201C0000000;
+constexpr uint64_t literal_0x80000000 = 0x80000000;
+constexpr uint64_t literal_0x8000000 = 0x8000000;
 constexpr uint64_t literal_0x1 = 0x1;
 
 fapi2::ReturnCode p9_vas_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& TGT0,
@@ -60,6 +62,18 @@ fapi2::ReturnCode p9_vas_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
 
             l_scom_buffer.insert<0, 54, 0, uint64_t>(literal_0x00DF0201C0000000 );
             FAPI_TRY(fapi2::putScom(TGT0, 0x3011807ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x301180aull, l_scom_buffer ));
+
+            l_scom_buffer.insert<8, 31, 33, uint64_t>(literal_0x80000000 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x301180aull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x301180bull, l_scom_buffer ));
+
+            l_scom_buffer.insert<8, 28, 36, uint64_t>(literal_0x8000000 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x301180bull, l_scom_buffer));
         }
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x301184dull, l_scom_buffer ));
