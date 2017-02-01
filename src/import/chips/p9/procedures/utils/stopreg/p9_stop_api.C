162af3e4c2614a47a70e2cceb0b38c11c50e85bd
Prem Shanker Jha <premjha2@in.ibm.com>
2017-01-31 23:16:59 -0600

    PM: Added changes in SCOM restore region.
      - Added BLR as first instruction in Self-Restore region
      - Modified handling of STOP API SCOM operation RESET.
    
    Change-Id: I84096355b520f4c9cae98860ccf9ebf1c56229e4
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/35724
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: AMIT KUMAR <akumar3@us.ibm.com>
    Dev-Ready: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

--- src/import/chips/p9/procedures/utils/stopreg/p9_stop_api.C
+++ src/import/chips/p9/procedures/utils/stopreg/p9_stop_api.C
@@ -526,7 +526,8 @@ StopReturnCode_t p9_stop_save_cpureg(  void* const i_pImage,
                 &(chipHomer->coreThreadRestore[coreId][threadId].coreArea[0]);
         }
 
-        if( SWIZZLE_4_BYTE(ATTN_OPCODE) == *(uint32_t*)pThreadLocation )
+        if( ( SWIZZLE_4_BYTE(BLR_INST) == *(uint32_t*)pThreadLocation ) ||
+            ( SWIZZLE_4_BYTE(ATTN_OPCODE) == *(uint32_t*) pThreadLocation ) )
         {
             // table for given core id doesn't exit. It needs to be
             // defined.
@@ -864,6 +865,7 @@ StopReturnCode_t p9_stop_save_scom( void* const   i_pImage,
         uint32_t swizzleAttn = SWIZZLE_4_BYTE(ATTN_OPCODE);
         uint32_t swizzleEntry = SWIZZLE_4_BYTE(SCOM_ENTRY_START);
         uint32_t index = 0;
+        uint32_t swizzleBlr = SWIZZLE_4_BYTE(BLR_INST);
 
         for( index = 0; index < entryLimit; ++index )
         {
@@ -876,8 +878,8 @@ StopReturnCode_t p9_stop_save_scom( void* const   i_pImage,
             }
 
             if( (( nopInst == entrySwzAddress ) ||
-                 ( swizzleAttn == entrySwzAddress )) &&
-                ( !pNopLocation ) )
+                 ( swizzleAttn == entrySwzAddress ) ||
+                 ( swizzleBlr == entrySwzAddress )) && ( !pNopLocation ) )
             {
                 pNopLocation = &pScomEntry[index];
             }
@@ -962,7 +964,12 @@ StopReturnCode_t p9_stop_save_scom( void* const   i_pImage,
                 break;
 
             case P9_STOP_SCOM_RESET:
-                memset( pScomEntry, nopInst, SCOM_SIZE_PER_CORE  );
+
+                if( P9_STOP_SECTION_CORE_SCOM ==  i_section )
+                {
+                    memset( pScomEntry, 0x00, SCOM_SIZE_PER_CORE );
+                }
+
                 break;
 
             case P9_STOP_SCOM_OR_APPEND:
