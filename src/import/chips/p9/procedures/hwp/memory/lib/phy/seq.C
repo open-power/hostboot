7acbc7a767e8ed47b1de53df60fcb2c193305201
Andre Marin <aamarin@us.ibm.com>
2017-02-03 04:00:10 -0600

    Add c_str generic API and update makefiles
    Change-Id: Ib4748ee25c0903d27572e4be2dcb6461492a6f90
    Original-Change-Id: I95e3b9013d3ab0c352d3614c12ee4ef0d26965d0
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/35924
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Brian R. Silver <bsilver@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/lib/phy/seq.C
+++ src/import/chips/p9/procedures/hwp/memory/lib/phy/seq.C
@@ -36,7 +36,7 @@
 #include <fapi2.H>
 #include <lib/phy/seq.H>
 #include <lib/utils/scom.H>
-#include <lib/utils/c_str.H>
+#include <c_str.H>
 #include <lib/utils/bit_count.H>
 #include <lib/eff_config/timing.H>
 
