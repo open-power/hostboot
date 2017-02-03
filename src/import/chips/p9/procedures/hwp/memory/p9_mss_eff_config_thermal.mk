1d0ffdd5ad98056b3873710844275e075e148d9b
Andre Marin <aamarin@us.ibm.com>
2017-02-03 04:00:10 -0600

    Add c_str generic API and update makefiles
    Change-Id: I60fb21733de4815486eaced78ce541780b195e22
    Original-Change-Id: I95e3b9013d3ab0c352d3614c12ee4ef0d26965d0
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/35924
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Brian R. Silver <bsilver@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/p9_mss_eff_config_thermal.mk
+++ src/import/chips/p9/procedures/hwp/memory/p9_mss_eff_config_thermal.mk
@@ -22,6 +22,11 @@
 # permissions and limitations under the License.
 #
 # IBM_PROLOG_END_TAG
+
+# Include the macros and things for MSS procedures
+-include 00common.mk
+
 PROCEDURE=p9_mss_eff_config_thermal
 lib$(PROCEDURE)_DEPLIBS += p9_mss_bulk_pwr_throttles
+$(eval $(call ADD_MEMORY_INCDIRS,$(PROCEDURE)))
 $(call BUILD_PROCEDURE)
