ceb14fe26ad395b5ce5b6f0eb99ad4cd0cfe3475
Prasad Bg Ranganath <prasadbgr@in.ibm.com>
2017-02-08 07:53:13 -0600

    p9_pstate_param_blk: Define VFRT table and initialize the data
    Change-Id: If517d23014f40bc3f07563fc4009ff25b59f7745
    RTC:167141
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/36131
    Dev-Ready: Gregory S. Still <stillgs@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: AMIT KUMAR <akumar3@us.ibm.com>
    Reviewed-by: Sachin Gupta <sgupta2m@in.ibm.com>
    Reviewed-by: Matt K. Light <mklight@us.ibm.com>
    Reviewed-by: Prem Shanker Jha <premjha2@in.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/38209
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>

--- src/usr/isteps/istep15/host_build_stop_image.C
+++ src/usr/isteps/istep15/host_build_stop_image.C
@@ -346,7 +346,7 @@ void* host_build_stop_image (void *io_pArgs)
     TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "host_build_stop_image entry" );
 
     // allocate three temporary work buffers
-    void* l_temp_buffer0 = malloc(MAX_SEEPROM_IMAGE_SIZE);
+    void* l_temp_buffer0 = malloc(HW_IMG_RING_SIZE);
     void* l_temp_buffer1 = malloc(MAX_RING_BUF_SIZE);
     void* l_temp_buffer2 = malloc(MAX_RING_BUF_SIZE);
 
@@ -482,7 +482,7 @@ void* host_build_stop_image (void *io_pArgs)
                                  PHASE_IPL,
                                  img_type,
                                  l_temp_buffer0,
-                                 MAX_SEEPROM_IMAGE_SIZE,
+                                 HW_IMG_RING_SIZE,
                                  l_temp_buffer1,
                                  MAX_RING_BUF_SIZE,
                                  l_temp_buffer2,
