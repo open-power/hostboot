61a53e411873196590c2cf97b7b97e8e1418460d
William A. Kennington III <wak@google.com>
2017-02-14 14:18:01 -0800

    stdint: Add least and fast types
    Also cleanup the spacing on some of the old declarations.
    
    Change-Id: I0c0422effb11729b28b77a7363138663d59009c6
    Signed-off-by: William A. Kennington III <wak@google.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/36504
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/include/stdint.h
+++ src/include/stdint.h
@@ -5,7 +5,10 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* COPYRIGHT International Business Machines Corp. 2010,2014              */
+/* Contributors Listed Below - COPYRIGHT 2010,2017                        */
+/* [+] Google Inc.                                                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
 /*                                                                        */
 /* Licensed under the Apache License, Version 2.0 (the "License");        */
 /* you may not use this file except in compliance with the License.       */
@@ -25,18 +28,39 @@
 
 #include <stddef.h>
 
-typedef signed char 		int8_t;
-typedef short int 		    int16_t;
-typedef int			        int32_t;
-typedef long int	        int64_t;
+typedef signed char         int8_t;
+typedef short int           int16_t;
+typedef int                 int32_t;
+typedef long int            int64_t;
 
-typedef unsigned char 		uint8_t;
-typedef unsigned short int 	uint16_t;
-typedef unsigned int	 	uint32_t;
-typedef unsigned long int 	uint64_t;
+typedef signed char         int_least8_t;
+typedef short int           int_least16_t;
+typedef int                 int_least32_t;
+typedef long int            int_least64_t;
 
-typedef uint64_t 	        size_t;
-typedef int64_t 	        ssize_t;
+typedef signed char         int_fast8_t;
+typedef long int            int_fast16_t;
+typedef long int            int_fast32_t;
+typedef long int            int_fast64_t;
+
+typedef unsigned char       uint8_t;
+typedef unsigned short int  uint16_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long int   uint64_t;
+
+typedef unsigned char       uint_least8_t;
+typedef unsigned short int  uint_least16_t;
+typedef unsigned int        uint_least32_t;
+typedef unsigned long int   uint_least64_t;
+
+typedef unsigned char       uint_fast8_t;
+typedef unsigned long int   uint_fast16_t;
+typedef unsigned long int   uint_fast32_t;
+typedef unsigned long int   uint_fast64_t;
+
+
+typedef uint64_t            size_t;
+typedef int64_t             ssize_t;
 
 typedef ssize_t             ptrdiff_t;
 
