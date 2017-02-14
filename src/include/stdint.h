4e6386dba348cb53d1a9651c7c7f6bba22f2c5f0
William A. Kennington III <wak@google.com>
2017-02-14 14:18:01 -0800

    stdint: Add least and fast types
    Also cleanup the spacing on some of the old declarations.
    
    Signed-off-by: William A. Kennington III <wak@google.com>

--- src/include/stdint.h
+++ src/include/stdint.h
@@ -25,18 +25,39 @@
 
 #include <stddef.h>
 
-typedef signed char 		int8_t;
-typedef short int 		    int16_t;
-typedef int			        int32_t;
-typedef long int	        int64_t;
+typedef signed char         int8_t;
+typedef short int           int16_t;
+typedef int                 int32_t;
+typedef long int            int64_t;
 
-typedef unsigned char 		uint8_t;
-typedef unsigned short int 	uint16_t;
-typedef unsigned int	 	uint32_t;
-typedef unsigned long int 	uint64_t;
+typedef signed char         int_least8_t;
+typedef short int           int_least16_t;
+typedef int                 int_least32_t;
+typedef long int            int_least64_t;
 
-typedef uint64_t 	        size_t;
-typedef int64_t 	        ssize_t;
+typedef signed char         int_fast8_t;
+typedef long int            int_fast16_t;
+typedef long int            int_fast32_t;
+typedef long int            int_fast64_t;
+
+typedef unsigned char       uint8_t;
+typedef unsigned short int  uint16_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long int   uint64_t;
+
+typedef unsigned char       uint_least8_t;
+typedef unsigned short int  uint_least16_t;
+typedef unsigned int        uint_least32_t;
+typedef unsigned long int   uint_least64_t;
+
+typedef unsigned char       uint_fast8_t;
+typedef unsigned long int   uint_fast16_t;
+typedef unsigned long int   uint_fast32_t;
+typedef unsigned long int   uint_fast64_t;
+
+
+typedef uint64_t            size_t;
+typedef int64_t             ssize_t;
 
 typedef ssize_t             ptrdiff_t;
 
