315745680169f4500128cf1368087be89c6a332f
Prasad Bg Ranganath <prasadbgr@in.ibm.com>
2017-05-02 04:52:24 -0500

    P9 Pstate: VREF calibration suport
    Change-Id: I8d7bf4be0537e28fa68fc894b299ceaf2e41aafb
    Original-Change-Id: Ic1265ee9756b2253032a54d381a016315e8176d0
    RTC:160976
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/39931
    Reviewed-by: Michael S. Floyd <mfloyd@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Juan R. Medina <jrmedina@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/pm/p9_pm_stop_gpe_init.C
+++ src/import/chips/p9/procedures/hwp/pm/p9_pm_stop_gpe_init.C
@@ -204,8 +204,44 @@ fapi2::ReturnCode p9_pm_stop_gpe_init(
                      "ERROR: Failed to assert DPLL in mode 1 and set slew rate to 1");
         }
 
+        uint8_t l_ivrms_attrval = 0;
+        uint8_t l_vdm_attrval = 0;
+
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_SYSTEM_IVRMS_ENABLED,
+                               FAPI_SYSTEM,
+                               l_ivrms_attrval),
+                 "Error from FAPI_ATTR_GET for attribute ATTR_SYSTEM_IVRMS_ENABLED" );
+
+
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_VDM_ENABLE,
+                               FAPI_SYSTEM,
+                               l_vdm_attrval),
+                 "Error from FAPI_ATTR_GET for attribute ATTR_VDM_ENABLE" );
+
+        if((l_vdm_attrval || l_ivrms_attrval))
+        {
+            //check vref calibration bit set or not
+            //PERV_TP_KVREF_AND_VMEAS_MODE_STATUS_REG
+            //--> 0x01020007
+            FAPI_TRY(fapi2::getScom(i_target, 0x01020007, l_data64));
+
+            if ( !(l_data64.getBit<16>()))
+            {
+                FAPI_ERR("VDMs/IVRM are enabled but necessary VREF calibration failed");
+                FAPI_ASSERT(false,
+                            fapi2::STOP_GPE_VREF_CALIBRATION_FAILED()
+                            .set_CHIP(i_target)
+                            .set_VREF_CALIBRATION_ADDRESS(0x01020007)
+                            .set_IS_VDM_ENABLED(l_vdm_attrval)
+                            .set_IS_IVRM_ENABLED(l_ivrms_attrval),
+                            "ERROR; VREF calibration bit is not set %x",
+                            (uint32_t)l_data64);
+            }
+        }
+
         // Boot the STOP GPE
         FAPI_TRY(stop_gpe_init(i_target), "ERROR: failed to initialize Stop GPE");
+
 
     }
 
