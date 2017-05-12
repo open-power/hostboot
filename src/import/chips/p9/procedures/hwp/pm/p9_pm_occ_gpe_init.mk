484d53beba7bfe554b479a83c800c728fc7fa9e6
Sangeetha T S <sangeet2@in.ibm.com>
2017-05-12 00:59:39 -0500

    Level 3: For various PM HWP
    Commit provides level 3 readiness for following HWP :
        - p9_pm_init
        - p9_pm_occ_gpe_init
        - p9_pm_pba_init
        - p9_pm_pss_init
        - p9_pm_reset
    
    Change-Id: Ifce6961327e2cde625c2ff4962e55f0652253b9a
    Original-Change-Id: I5066a664a6c42198fb3d6fccaf4bfb641d79b890
    RTC: 172617
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/40431
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: AMIT J. TENDOLKAR <amit.tendolkar@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/pm/p9_pm_occ_gpe_init.mk
+++ src/import/chips/p9/procedures/hwp/pm/p9_pm_occ_gpe_init.mk
@@ -23,4 +23,6 @@
 #
 # IBM_PROLOG_END_TAG
 PROCEDURE=p9_pm_occ_gpe_init
+$(call ADD_MODULE_SRCDIR,$(PROCEDURE),$(ROOTPATH)/chips/p9/procedures/hwp/lib)
+$(call ADD_MODULE_INCDIR,$(PROCEDURE),$(ROOTPATH)/chips/p9/procedures/hwp/lib)
 $(call BUILD_PROCEDURE)
