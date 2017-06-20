03f9b6c4cb3a05d8d630461a8a70405ade923199
Marty Gloff <mgloff@us.ibm.com>
2017-06-20 16:03:23 -0500

    Detect lack of proc0 memory and force reboot on new config / Detect
    Implement the detection of no memory or of memory configuration
    mismatches for proc0.
    
    Change-Id: I5b7700bfaf0b0d5f9cd6164cd0252fa47907e8ab
    RTC: 176066
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/42167
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Matt Derksen <mderkse1@us.ibm.com>
    Reviewed-by: Corey V. Swenson <cswenson@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep07/call_mss_attr_update.C
+++ src/usr/isteps/istep07/call_mss_attr_update.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2016                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2017                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -31,6 +31,8 @@
 // Includes
 /******************************************************************************/
 #include    <stdint.h>
+#include    <map>
+#include    <arch/pirformat.H>
 
 #include    <trace/interface.H>
 #include    <initservice/taskargs.H>
@@ -41,6 +43,7 @@
 
 #include    <errl/errludtarget.H>
 #include    <initservice/isteps_trace.H>
+#include    <initservice/initserviceif.H>
 
 //  targeting support
 #include    <targeting/common/commontargeting.H>
@@ -64,6 +67,203 @@ using   namespace   ISTEP_ERROR;
 using   namespace   ERRORLOG;
 using   namespace   TARGETING;
 
+
+typedef struct procIds
+{
+    Target* proc;                                  // Proc
+    ATTR_FABRIC_GROUP_ID_type groupIdDflt;         // Default Group ID
+    ATTR_PROC_EFF_FABRIC_GROUP_ID_type groupIdEff; // Effective Group ID
+    ATTR_FABRIC_GROUP_ID_type groupId;             // Desired Group ID
+    ATTR_FABRIC_CHIP_ID_type chipIdDflt;           // Default Chip ID
+    ATTR_PROC_EFF_FABRIC_CHIP_ID_type chipIdEff;   // Effective Chip ID
+    ATTR_FABRIC_CHIP_ID_type chipId;               // Desired Chip ID
+} procIds_t;
+
+const uint8_t INVALID_PROC = 0xFF;
+
+
+/**
+ * @brief   check_proc0_memory_config   Check memory config for proc0
+ *
+ *  Loops through all processors gathering group ID and chip ID information for
+ *  each one and determining which is proc0 based on having the smallest group
+ *  ID / chip Id.
+ *
+ *  Checks that proc0 has associated functional dimms.  If it does not have any
+ *  functional dimms behind it, look for a proc that does have functional dimms
+ *  and swap IDs for these processors.
+ *
+ *  Loops through all processors again making sure that the effective group and
+ *  chip IDs are the desired IDs.  If the IDs are not as desired, update the
+ *  effective ID attributes and flag that an SBE Update is needed.
+ *
+ *  If SBE Update is needed, sync all attributes to the FSP and perform update.
+ *
+ * @param  none
+ *
+ * @return errlHndl_t       valid errlHndl_t handle if there was an error
+ *                          NULL if no errors;
+ */
+errlHndl_t check_proc0_memory_config()
+{
+    errlHndl_t l_err = nullptr;
+    bool l_updateNeeded = false;
+
+    TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+              "check_proc0_memory_config entry");
+
+    // Get all procs
+    TargetHandleList l_procsList;
+    getAllChips(l_procsList, TYPE_PROC);
+
+    // Loop through all procs getting IDs
+    procIds_t l_procIds[l_procsList.size()];
+    uint8_t i = 0;
+    uint8_t l_proc0 = INVALID_PROC;
+    uint8_t l_victim = INVALID_PROC;
+    for (const auto & l_procChip : l_procsList)
+    {
+        l_procIds[i].proc = l_procChip;
+
+        // Get Group IDs
+        l_procIds[i].groupIdDflt =
+            l_procChip->getAttr<ATTR_FABRIC_GROUP_ID>();
+        l_procIds[i].groupIdEff =
+            l_procChip->getAttr<ATTR_PROC_EFF_FABRIC_GROUP_ID>();
+        l_procIds[i].groupId = l_procIds[i].groupIdDflt;
+
+        // Get Chip IDs
+        l_procIds[i].chipIdDflt =
+            l_procChip->getAttr<ATTR_FABRIC_CHIP_ID>();
+        l_procIds[i].chipIdEff =
+            l_procChip->getAttr<ATTR_PROC_EFF_FABRIC_CHIP_ID>();
+        l_procIds[i].chipId = l_procIds[i].chipIdDflt;
+
+        // Check if this proc should be tracked as proc0
+        if(l_proc0 == INVALID_PROC)
+        {
+            // No proc0, make initial assignment
+            l_proc0 = i;
+        }
+        else if ( PIR_t::createChipId(l_procIds[i].groupId,
+                                      l_procIds[i].chipId) <
+                  PIR_t::createChipId(l_procIds[l_proc0].groupId,
+                                      l_procIds[l_proc0].chipId) )
+        {
+            // Smaller group ID / chip ID, replace assignment
+            l_proc0 = i;
+        }
+
+        // Increment index
+        i++;
+    }
+
+    // Get the functional DIMMs for proc0
+    PredicateHwas l_functional;
+    l_functional.functional(true);
+    TargetHandleList l_dimms;
+    PredicateCTM l_dimm(CLASS_LOGICAL_CARD, TYPE_DIMM);
+    PredicatePostfixExpr l_checkExprFunctional;
+    l_checkExprFunctional.push(&l_dimm).push(&l_functional).And();
+    targetService().getAssociated(l_dimms,
+                                  l_procIds[l_proc0].proc,
+                                  TargetService::CHILD_BY_AFFINITY,
+                                  TargetService::ALL,
+                                  &l_checkExprFunctional);
+
+    TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+              "check_proc0_memory_config: %d functional dimms behind proc0 "
+              "%.8X",
+              l_dimms.size(), get_huid(l_procIds[l_proc0].proc) );
+
+    // Check if proc0 does not have memory
+    if(l_dimms.empty())
+    {
+        TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                  "check_proc0_memory_config: proc0 %.8X has no functional "
+                  "dimms behind it",
+                  get_huid(l_procIds[l_proc0].proc) );
+
+        // Loop through all procs to find ones for memory remap
+        for (i = 0; i < l_procsList.size(); i++)
+        {
+            // If proc0, then continue
+            if(i == l_proc0)
+            {
+                continue;
+            }
+
+            // Get the functional DIMMs for the proc
+            targetService().getAssociated(l_dimms,
+                                          l_procIds[i].proc,
+                                          TargetService::CHILD_BY_AFFINITY,
+                                          TargetService::ALL,
+                                          &l_checkExprFunctional);
+
+            // If proc does not have memory, then continue
+            if(l_dimms.empty())
+            {
+                TRACDCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                          "check_proc0_memory_config: Proc %.8X has no  "
+                          "functional dimms behind it",
+                          get_huid(l_procIds[i].proc) );
+
+                continue;
+            }
+
+            // Use this proc for swapping memory with proc0
+            l_victim = i;
+
+            // Set the desired proc0 IDs from swapped proc's default IDs
+            l_procIds[l_proc0].groupId = l_procIds[l_victim].groupIdDflt;
+            l_procIds[l_proc0].chipId = l_procIds[l_victim].chipIdDflt;
+            TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                      "check_proc0_memory_config: proc0 %.8X is set to use "
+                      "groupId %d and chipId %d",
+                      get_huid(l_procIds[l_proc0].proc),
+                      l_procIds[l_proc0].groupId,
+                      l_procIds[l_proc0].chipId);
+
+            // Set the desired IDs for the swapped proc from proc0 defaults
+            l_procIds[l_victim].groupId = l_procIds[l_proc0].groupIdDflt;
+            l_procIds[l_victim].chipId = l_procIds[l_proc0].chipIdDflt;
+            TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                      "check_proc0_memory_config: Proc %.8X is set to use "
+                      "groupId %d and chipId %d",
+                      get_huid(l_procIds[l_victim].proc),
+                      l_procIds[l_victim].groupId,
+                      l_procIds[l_victim].chipId);
+
+            // Leave loop after swapping memory
+            break;
+        }
+
+        // Check that a victim was found
+        assert( l_victim < l_procsList.size(), "No swap match found" );
+    }
+
+    // Loop through all procs detecting that IDs are set correctly
+    for (i = 0; i < l_procsList.size(); i++)
+    {
+        if((l_procIds[i].groupId != l_procIds[i].groupIdEff) ||
+           (l_procIds[i].chipId != l_procIds[i].chipIdEff) )
+        {
+            // Update attributes @TODO RTC: 176068
+
+            l_updateNeeded = true;
+        }
+    }
+
+    if(l_updateNeeded)
+    {
+        // Sync all attributes to the FSP @TODO RTC: 176068
+        // Rebuild SBE image and trigger reconfig loop @TODO RTC: 176068
+    }
+
+    return l_err;
+} // end check_proc0_memory_config()
+
+
 //
 //  Wrapper function to call mss_attr_update
 //
@@ -74,27 +274,49 @@ void*    call_mss_attr_update( void *io_pArgs )
     TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_mss_attr_update entry");
     errlHndl_t l_err = NULL;
 
-    // Get all functional MCS chiplets
-    TARGETING::TargetHandleList l_mcsTargetList;
-    getAllChiplets(l_mcsTargetList, TYPE_MCS);
+    // Check the memory on proc0 chip
+    l_err = check_proc0_memory_config();
 
-    for (const auto & l_mcsTarget: l_mcsTargetList)
+    if (l_err)
     {
-        const fapi2::Target<fapi2::TARGET_TYPE_MCS>
-            l_fapi2_mcs_target(l_mcsTarget);
+        TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                   "ERROR 0x%.8X:  check_proc0_memory_config",
+                   l_err->reasonCode());
 
-        TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-            "Running p9_mss_attr_update HWP on "
-            "MCS target HUID %.8X", TARGETING::get_huid(l_mcsTarget));
-        FAPI_INVOKE_HWP(l_err, p9_mss_attr_update, l_fapi2_mcs_target);
-        if(l_err)
+        // Ensure istep error created and has same plid as this error
+        l_StepError.addErrorDetails( l_err );
+        errlCommit( l_err, HWPF_COMP_ID );
+    }
+    else
+    {
+        TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                   "SUCCESS:  check_proc0_memory_config");
+    }
+
+    if (l_StepError.isNull())
+    {
+        // Get all functional MCS chiplets
+        TARGETING::TargetHandleList l_mcsTargetList;
+        getAllChiplets(l_mcsTargetList, TYPE_MCS);
+
+        for (const auto & l_mcsTarget: l_mcsTargetList)
         {
+            const fapi2::Target<fapi2::TARGET_TYPE_MCS>
+                l_fapi2_mcs_target(l_mcsTarget);
+
             TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-                "ERROR 0x%.8X : p9_mss_attr_update HWP returned "
-                "error for HUID %.8x",
-                l_err->reasonCode(), TARGETING::get_huid(l_mcsTarget));
-            l_StepError.addErrorDetails(l_err);
-            errlCommit( l_err, HWPF_COMP_ID );
+                "Running p9_mss_attr_update HWP on "
+                "MCS target HUID %.8X", TARGETING::get_huid(l_mcsTarget));
+            FAPI_INVOKE_HWP(l_err, p9_mss_attr_update, l_fapi2_mcs_target);
+            if(l_err)
+            {
+                TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                    "ERROR 0x%.8X : p9_mss_attr_update HWP returned "
+                    "error for HUID %.8x",
+                    l_err->reasonCode(), TARGETING::get_huid(l_mcsTarget));
+                l_StepError.addErrorDetails(l_err);
+                errlCommit( l_err, HWPF_COMP_ID );
+            }
         }
     }
 
