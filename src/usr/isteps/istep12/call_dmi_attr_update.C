70047c0b3b9461a5f15da3d3bd1ec1435f38b8b7
Swathi Madhuri Bhattiprolu <bhmadhur@in.ibm.com>
2017-07-12 02:52:01 -0500

    Framework (plumbing work) in HB for Cumulus HW procedures
    Change-Id: I0bc7b56d75f404176268cfbd3948e6c24ef35b4d
    RTC: 175996
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/43020
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: VENKATESH SAINATH <venkatesh.sainath@in.ibm.com>
    Reviewed-by: Corey V. Swenson <cswenson@us.ibm.com>
    Reviewed-by: Sakethan R. Kotta <sakkotta@in.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

---
+++ src/usr/isteps/istep12/call_dmi_attr_update.C
@@ -0,0 +1,52 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/usr/isteps/istep12/call_dmi_attr_update.C $               */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2017                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include    <errl/errlentry.H>
+
+#include    <initservice/isteps_trace.H>
+
+#include    <isteps/hwpisteperror.H>
+#include    <errl/errludtarget.H>
+
+//  targeting support.
+#include    <targeting/common/commontargeting.H>
+#include    <targeting/common/utilFilter.H>
+
+using   namespace   ISTEP;
+using   namespace   ISTEP_ERROR;
+using   namespace   ERRORLOG;
+using   namespace   TARGETING;
+
+
+namespace ISTEP_12
+{
+void* call_dmi_attr_update (void *io_pArgs)
+{
+    IStepError l_StepError;
+    
+    // end task, returning any errorlogs to IStepDisp
+    return l_StepError.getErrorHandle();
+}
+
+};
