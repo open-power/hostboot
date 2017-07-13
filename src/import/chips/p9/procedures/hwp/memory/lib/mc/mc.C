9fc9612463c7c2ac748c00ab8d03081576e8c74c
Jacob Harvey <jlharvey@us.ibm.com>
2017-07-13 15:06:13 -0500

    L3 draminit and mss_lib
    Change-Id: I3b48278463c4d14ebb3e04632b6170f6f4927000
    Original-Change-Id: If5cae63291864da0b87d6a1e82407da9358d62d2
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/43121
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: ANDRE A. MARIN <aamarin@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/lib/mc/mc.C
+++ src/import/chips/p9/procedures/hwp/memory/lib/mc/mc.C
@@ -27,10 +27,10 @@
 /// @file mc.C
 /// @brief Subroutines to manipulate the memory controller
 ///
-// *HWP HWP Owner: Brian Silver <bsilver@us.ibm.com>
+// *HWP HWP Owner: Andre Marin <aamarin@us.ibm.com>
 // *HWP HWP Backup: Jacob Harvey <jlharvey@us.ibm.com>
 // *HWP Team: Memory
-// *HWP Level: 2
+// *HWP Level: 3
 // *HWP Consumed by: FSP:HB
 
 #include <fapi2.H>
@@ -88,8 +88,8 @@ fapi2::ReturnCode set_pwr_cntrl_reg(const fapi2::Target<fapi2::TARGET_TYPE_MCA>&
     uint8_t l_pwr_cntrl = 0;
     fapi2::buffer<uint64_t> l_data;
 
-    FAPI_TRY(mrw_power_control_requested(l_pwr_cntrl));
-    FAPI_TRY(mss::getScom(i_target, MCA_MBARPC0Q, l_data));
+    FAPI_TRY(mrw_power_control_requested(l_pwr_cntrl), "Error in set_pwr_cntrl_reg");
+    FAPI_TRY(mss::getScom(i_target, MCA_MBARPC0Q, l_data), "Error in set_pwr_cntrl_reg");
 
     l_data.insertFromRight<TT::CFG_MIN_MAX_DOMAINS, TT::CFG_MIN_MAX_DOMAINS_LEN>(MAXALL_MINALL);
 
@@ -117,7 +117,7 @@ fapi2::ReturnCode set_pwr_cntrl_reg(const fapi2::Target<fapi2::TARGET_TYPE_MCA>&
     l_data.insertFromRight<TT::MIN_DOMAIN_REDUCTION_TIME, TT::MIN_DOMAIN_REDUCTION_TIME_LEN>
     (MIN_DOMAIN_REDUCTION_TIME);
 
-    FAPI_TRY(mss::putScom(i_target, MCA_MBARPC0Q, l_data) );
+    FAPI_TRY(mss::putScom(i_target, MCA_MBARPC0Q, l_data), "Error in set_pwr_cntrl_reg" );
 
     return fapi2::FAPI2_RC_SUCCESS;
 fapi_try_exit:
@@ -137,8 +137,8 @@ fapi2::ReturnCode set_str_reg(const fapi2::Target<fapi2::TARGET_TYPE_MCA>& i_tar
     uint8_t l_str_enable = 0;
     fapi2::buffer<uint64_t> l_data;
 
-    FAPI_TRY(mrw_power_control_requested(l_str_enable));
-    FAPI_TRY(mss::getScom(i_target, MCA_MBASTR0Q, l_data));
+    FAPI_TRY(mrw_power_control_requested(l_str_enable), "Error in set_pwr_cntrl_reg");
+    FAPI_TRY(mss::getScom(i_target, MCA_MBASTR0Q, l_data), "Error in set_pwr_cntrl_reg");
 
     //Write bit if STR should be enabled
     switch (l_str_enable)
@@ -169,7 +169,7 @@ fapi2::ReturnCode set_str_reg(const fapi2::Target<fapi2::TARGET_TYPE_MCA>& i_tar
 
     l_data.insertFromRight<TT::ENTER_STR_TIME_POS, TT::ENTER_STR_TIME_LEN>(ENTER_STR_TIME);
 
-    FAPI_TRY(mss::putScom(i_target, MCA_MBASTR0Q, l_data) );
+    FAPI_TRY(mss::putScom(i_target, MCA_MBASTR0Q, l_data), "Error in set_str_reg" );
 
     return fapi2::FAPI2_RC_SUCCESS;
 fapi_try_exit:
@@ -195,15 +195,15 @@ fapi2::ReturnCode set_nm_support (const fapi2::Target<fapi2::TARGET_TYPE_MCA>& i
     fapi2::buffer<uint64_t> l_data;
 
     //runtime should be calculated in eff_config_thermal, which is called before scominit in ipl
-    FAPI_TRY(runtime_mem_throttled_n_commands_per_port(i_target, l_run_port));
-    FAPI_TRY(runtime_mem_throttled_n_commands_per_slot(i_target, l_run_slot));
-    FAPI_TRY(mss::mrw_mem_m_dram_clocks(l_throttle_denominator) );
+    FAPI_TRY(runtime_mem_throttled_n_commands_per_port(i_target, l_run_port), "Error in set_nm_support");
+    FAPI_TRY(runtime_mem_throttled_n_commands_per_slot(i_target, l_run_slot), "Error in set_nm_support");
+    FAPI_TRY(mss::mrw_mem_m_dram_clocks(l_throttle_denominator), "Error in set_nm_support" );
     FAPI_INF("For target %s throttled n commands per port are %d, per slot are %d, and dram m clocks are %d",
              mss::c_str(i_target),
              l_run_port,
              l_run_slot,
              l_throttle_denominator);
-    FAPI_TRY(mss::getScom(i_target, MCA_MBA_FARB3Q, l_data));
+    FAPI_TRY(mss::getScom(i_target, MCA_MBA_FARB3Q, l_data), "Error in set_nm_support");
 
     l_data.insertFromRight<TT::RUNTIME_N_SLOT, TT::RUNTIME_N_SLOT_LEN>(l_run_slot);
     l_data.insertFromRight<TT::RUNTIME_N_PORT, TT::RUNTIME_N_PORT_LEN>(l_run_port);
@@ -215,7 +215,7 @@ fapi2::ReturnCode set_nm_support (const fapi2::Target<fapi2::TARGET_TYPE_MCA>& i
     // Set to disable permanently due to hardware design bug (HW403028) that won't be changed
     l_data.writeBit<TT::NM_CHANGE_AFTER_SYNC>(CHANGE_AFTER_SYNC_OFF);
 
-    FAPI_TRY(mss::putScom(i_target, MCA_MBA_FARB3Q, l_data) );
+    FAPI_TRY(mss::putScom(i_target, MCA_MBA_FARB3Q, l_data), "Error in set_nm_support" );
 
     return fapi2::FAPI2_RC_SUCCESS;
 fapi_try_exit:
@@ -238,13 +238,13 @@ fapi2::ReturnCode set_safemode_throttles(const fapi2::Target<fapi2::TARGET_TYPE_
     uint16_t l_throttle_per_slot = 0;
     uint32_t l_throttle_denominator = 0;
 
-    FAPI_TRY(mss::mrw_mem_m_dram_clocks(l_throttle_denominator) );
-    FAPI_TRY(mss::mrw_safemode_mem_throttled_n_commands_per_port(l_throttle_per_slot) );
-    FAPI_TRY(mss::getScom(i_target, MCA_MBA_FARB4Q, l_data) );
+    FAPI_TRY(mss::mrw_mem_m_dram_clocks(l_throttle_denominator), "Error in set_safemode_throttles" );
+    FAPI_TRY(mss::mrw_safemode_mem_throttled_n_commands_per_port(l_throttle_per_slot), "Error in set_safemode_throttles" );
+    FAPI_TRY(mss::getScom(i_target, MCA_MBA_FARB4Q, l_data), "Error in set_safemode_throttles" );
 
     l_data.insertFromRight<TT::EMERGENCY_M, TT::EMERGENCY_M_LEN>(l_throttle_denominator);
     l_data.insertFromRight<TT::EMERGENCY_N, TT::EMERGENCY_N_LEN>(l_throttle_per_slot);
-    FAPI_TRY(mss::putScom(i_target, MCA_MBA_FARB4Q, l_data) );
+    FAPI_TRY(mss::putScom(i_target, MCA_MBA_FARB4Q, l_data), "Error in set_safemode_throttles" );
     return fapi2::FAPI2_RC_SUCCESS;
 
 fapi_try_exit:
@@ -268,16 +268,16 @@ fapi2::ReturnCode set_runtime_throttles_to_safe(const fapi2::Target<fapi2::TARGE
     uint16_t l_throttle_per_port = 0;
     uint32_t l_throttle_denominator = 0;
 
-    FAPI_TRY(mss::mrw_mem_m_dram_clocks(l_throttle_denominator) );
-    FAPI_TRY(mss::mrw_safemode_mem_throttled_n_commands_per_port(l_throttle_per_port) );
-    FAPI_TRY(mss::getScom(i_target, MCA_MBA_FARB3Q, l_data) );
+    FAPI_TRY(mss::mrw_mem_m_dram_clocks(l_throttle_denominator), "Error in set_safemode_throttles" );
+    FAPI_TRY(mss::mrw_safemode_mem_throttled_n_commands_per_port(l_throttle_per_port), "Error in set_safemode_throttles" );
+    FAPI_TRY(mss::getScom(i_target, MCA_MBA_FARB3Q, l_data), "Error in set_safemode_throttles" );
 
     //Same value for both throttles
     l_data.insertFromRight<TT::RUNTIME_N_SLOT, TT::RUNTIME_N_SLOT_LEN>(l_throttle_per_port);
     l_data.insertFromRight<TT::RUNTIME_N_PORT, TT::RUNTIME_N_PORT_LEN>(l_throttle_per_port);
     l_data.insertFromRight<TT::RUNTIME_M, TT::RUNTIME_M_LEN>(l_throttle_denominator);
 
-    FAPI_TRY(mss::putScom(i_target, MCA_MBA_FARB3Q, l_data) );
+    FAPI_TRY(mss::putScom(i_target, MCA_MBA_FARB3Q, l_data), "Error in set_safemode_throttles" );
     return fapi2::FAPI2_RC_SUCCESS;
 
 fapi_try_exit:
@@ -293,10 +293,10 @@ fapi_try_exit:
 ///
 fapi2::ReturnCode thermal_throttle_scominit (const fapi2::Target<fapi2::TARGET_TYPE_MCA>& i_target)
 {
-    FAPI_TRY(set_pwr_cntrl_reg(i_target));
-    FAPI_TRY(set_str_reg(i_target));
-    FAPI_TRY(set_nm_support(i_target));
-    FAPI_TRY(set_safemode_throttles(i_target));
+    FAPI_TRY(set_pwr_cntrl_reg(i_target), "Error in thermal_throttle_scominit");
+    FAPI_TRY(set_str_reg(i_target), "Error in thermal_throttle_scominit");
+    FAPI_TRY(set_nm_support(i_target), "Error in thermal_throttle_scominit");
+    FAPI_TRY(set_safemode_throttles(i_target), "Error in thermal_throttle_scominit");
 
     return fapi2::FAPI2_RC_SUCCESS;
 fapi_try_exit:
