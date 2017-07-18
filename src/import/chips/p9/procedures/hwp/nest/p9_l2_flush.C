8870aa8179783ec0952c15a9538dc5f7f584a3d0
Thi Tran <thi@us.ibm.com>
2017-07-18 07:47:10 -0500

    L3 Update - p9_l2/l3_flush.C
    Change-Id: I0f1f61df99d8538bfed5d56e88f49af2f3d24abb
    Original-Change-Id: I665dcc102cd019be1856a3c7e9ccbe3a99fb43f2
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/43264
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Benjamin Gass <bgass@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/47177
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/nest/p9_l2_flush.C
+++ src/import/chips/p9/procedures/hwp/nest/p9_l2_flush.C
@@ -22,49 +22,28 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
+
 ///
 /// @file p9_l2_flush.H
 /// @brief Flush the P9 L2 cache (FAPI)
 ///
 /// *HWP HWP Owner   : Benjamin Gass <bgass@us.ibm.com>
-/// *HWP FW Owner    : Bilicon Patil  <bilpatil@in.ibm.com>
+/// *HWP FW Owner    : Thi Tran <thi@us.ibm.com>
 /// *HWP Team        : Quad
-/// *HWP Consumed by : FSP
-/// *HWP Level       : 2
+/// *HWP Consumed by : FSP and SBE
+/// *HWP Level       : 3
 ///
 
 //------------------------------------------------------------------------------
 // Includes
 //------------------------------------------------------------------------------
-#include <p9_quad_scom_addresses.H>
 #include <p9_l2_flush.H>
+#include <p9_quad_scom_addresses.H>
+#include <p9_quad_scom_addresses_fld.H>
 
 //------------------------------------------------------------------------------
 // Constant definitions
 //------------------------------------------------------------------------------
-
-// L2 Purge Engine Command Register bit/field definitions
-enum
-{
-    PURGE_CMD_TRIGGER_BIT            = 0,
-    PURGE_CMD_REG_BUSY               = 9,
-    PURGE_CMD_PRGSM_BUSY_ON_THIS_BIT = 10,
-    PURGE_CMD_PRGSM_BUSY_BIT         = 11,
-    PURGE_CMD_ERR_BIT                = 29,
-    PURGE_CMD_TYPE_BIT               = 1,
-    PURGE_CMD_MEM_BIT                = 17,
-    PURGE_CMD_BANK_BIT               = 28,
-    PURGE_CMD_CGC_BIT                = 20
-};
-
-// L2 Purge Engine Command Register bit/field Lengths
-enum
-{
-    PURGE_CMD_TYPE_BIT_LENGTH   = 4,
-    PURGE_CMD_MEM_BIT_LENGTH    = 3,
-    PURGE_CMD_BANK_BIT_LENGTH   = 1,
-    PURGE_CMD_CGC_BIT_LENGTH    = 8
-};
 
 // polling constants
 enum
@@ -78,19 +57,19 @@ enum
 // Function definitions
 //------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
+///-----------------------------------------------------------------------------
+/// @brief Utility subroutine to initiate L2 cache flush via purge engine.
 ///
-/// @brief l2_flush_start: Utility subroutine to initiate L2 cache flush
-///                        via purge engine
-/// @param[in]  i_target Ex target
-/// @param[in]  i_regAddr The scom address to use
+/// @param[in]  i_target    EX target
+/// @param[in]  i_regAddr   The scom address to use
 /// @param[in]  i_purgeData Structure having values for MEM, CGC, BANK
 ///                         passed by the user
+///
 /// @return  FAPI2_RC_SUCCESS if purge operation was started,
 ///          RC_P9_L2_FLUSH_PURGE_REQ_OUTSTANDING if a prior purge
 ///          operation has not yet completed
 ///          else FAPI getscom/putscom return code for failing operation
-//------------------------------------------------------------------------------
+///-----------------------------------------------------------------------------
 fapi2::ReturnCode l2_flush_start(
     const fapi2::Target<fapi2::TARGET_TYPE_EX>& i_target,
     const uint32_t i_regAddr,
@@ -99,7 +78,7 @@ fapi2::ReturnCode l2_flush_start(
     fapi2::buffer<uint64_t> l_cmdReg;
     fapi2::buffer<uint64_t> l_purgeCmd;
 
-    FAPI_INF("l2_flush_start: Enter");
+    FAPI_DBG("l2_flush_start: Enter");
 
     // ensure that purge engine is idle before starting flush
     // poll Purge Engine status
@@ -107,7 +86,7 @@ fapi2::ReturnCode l2_flush_start(
     FAPI_TRY(fapi2::getScom(i_target, i_regAddr, l_cmdReg));
 
     // check to see if this reg is idle and ready to accept a new command
-    FAPI_ASSERT(!l_cmdReg.getBit<PURGE_CMD_REG_BUSY>(),
+    FAPI_ASSERT(!l_cmdReg.getBit<EX_PRD_PURGE_CMD_REG_BUSY>(),
                 fapi2::P9_L2_FLUSH_PURGE_REQ_OUTSTANDING()
                 .set_TARGET(i_target)
                 .set_CMD_REG(l_cmdReg)
@@ -118,34 +97,33 @@ fapi2::ReturnCode l2_flush_start(
     // ensure PURGE_CMD_TYPE/MEM/CGC/BANK are clear to specify flush
     // of entire cache
     FAPI_DBG("Write L2 Purge Engine Command Register to initiate cache flush");
-    l_purgeCmd.insert<PURGE_CMD_TYPE_BIT,
-                      PURGE_CMD_TYPE_BIT_LENGTH>(i_purgeData.iv_cmdType);
+    l_purgeCmd.insert<EX_PRD_PURGE_CMD_REG_TYPE,
+                      EX_PRD_PURGE_CMD_REG_TYPE_LEN>(i_purgeData.iv_cmdType);
 
-    l_purgeCmd.insert<PURGE_CMD_MEM_BIT,
-                      PURGE_CMD_MEM_BIT_LENGTH>(i_purgeData.iv_cmdMem);
+    l_purgeCmd.insert<EX_PRD_PURGE_CMD_REG_MEM,
+                      EX_PRD_PURGE_CMD_REG_MEM_LEN>(i_purgeData.iv_cmdMem);
 
-    l_purgeCmd.insert<PURGE_CMD_BANK_BIT,
-                      PURGE_CMD_BANK_BIT_LENGTH>(i_purgeData.iv_cmdBank);
+    l_purgeCmd.insert<EX_PRD_PURGE_CMD_REG_BANK, 1>(i_purgeData.iv_cmdBank);
 
-    l_purgeCmd.insert<PURGE_CMD_CGC_BIT,
-                      PURGE_CMD_CGC_BIT_LENGTH>(i_purgeData.iv_cmdCGC);
+    l_purgeCmd.insert<EX_PRD_PURGE_CMD_REG_CGC,
+                      EQ_PRD_PURGE_CMD_REG_CGC_LEN>(i_purgeData.iv_cmdCGC);
 
-    l_purgeCmd.setBit<PURGE_CMD_TRIGGER_BIT>();
+    l_purgeCmd.setBit<EX_PRD_PURGE_CMD_REG_TRIGGER>();
 
     FAPI_TRY(fapi2::putScom(i_target, i_regAddr, l_purgeCmd));
 
 fapi_try_exit:
+    FAPI_DBG("l2_flush_start: Exit");
     return fapi2::current_err;
 }
 
-
-//------------------------------------------------------------------------------
+///-----------------------------------------------------------------------------
+/// @brief Utility subroutine to poll L2 purge engine status, looking for
+///        clean idle state.
 ///
-/// @brief l2_flush_check_status: Utility subroutine to poll L2 purge
-///                                    engine status, looking for clean idle
-///                                    state
 /// @param[in] i_target EX chiplet target
 /// @param[in] i_regAddr Purge engine register SCOM address
+///
 /// @return FAPI2_RC_SUCCESS if engine status returns as idle (with no errors)
 ///         before maximum number of polls has been reached
 ///         RC_P9_L2_FLUSH_CMD_ERROR
@@ -153,8 +131,7 @@ fapi_try_exit:
 ///         RC_P9_L2_FLUSH_CMD_TIMEOUT
 ///              if purge operation did not complete prior to polling limit,
 ///         else FAPI getscom/putscom return code for failing operation
-///
-//------------------------------------------------------------------------------
+///-----------------------------------------------------------------------------
 fapi2::ReturnCode l2_flush_check_status(
     const fapi2::Target<fapi2::TARGET_TYPE_EX>& i_target,
     const uint32_t i_regAddr)
@@ -162,7 +139,7 @@ fapi2::ReturnCode l2_flush_check_status(
     fapi2::buffer<uint64_t> l_cmdReg;
     uint32_t l_polls = 1;
 
-    FAPI_INF("l2_flush_check_status: Enter");
+    FAPI_DBG("l2_flush_check_status: Enter");
 
     while(1)
     {
@@ -171,15 +148,15 @@ fapi2::ReturnCode l2_flush_check_status(
         FAPI_TRY(fapi2::getScom(i_target, i_regAddr, l_cmdReg));
 
         // check state of PURGE_CMD_ERR
-        FAPI_ASSERT(!l_cmdReg.getBit<PURGE_CMD_ERR_BIT>(),
+        FAPI_ASSERT(!l_cmdReg.getBit<EX_PRD_PURGE_CMD_REG_ERR>(),
                     fapi2::P9_L2_FLUSH_CMD_ERROR()
                     .set_TARGET(i_target)
                     .set_CMD_REG(l_cmdReg)
                     .set_CMD_REG_ADDR(i_regAddr),
-                    "Purge failed. PURGE_CMD_ERR_BIT set");
+                    "Purge failed. EX_PRD_PURGE_CMD_REG_ERR set");
 
         // check to see if this reg is idle and ready to accept a new command
-        if (!l_cmdReg.getBit<PURGE_CMD_REG_BUSY>())
+        if (!l_cmdReg.getBit<EX_PRD_PURGE_CMD_REG_BUSY>())
         {
             FAPI_DBG("Purge engine idle");
             break;
@@ -188,9 +165,9 @@ fapi2::ReturnCode l2_flush_check_status(
         // engine busy, dump status
         FAPI_DBG("Purge engine busy (reg_busy = %d, busy_on_this = %d,"
                  " sm_busy = %d)",
-                 l_cmdReg.getBit<PURGE_CMD_REG_BUSY>(),
-                 l_cmdReg.getBit<PURGE_CMD_PRGSM_BUSY_ON_THIS_BIT>(),
-                 l_cmdReg.getBit<PURGE_CMD_PRGSM_BUSY_BIT>());
+                 l_cmdReg.getBit<EX_PRD_PURGE_CMD_REG_BUSY>(),
+                 l_cmdReg.getBit<EX_PRD_PURGE_CMD_REG_PRGSM_BUSY_ON_THIS>(),
+                 l_cmdReg.getBit<EX_PRD_PURGE_CMD_REG_PRGSM_BUSY>());
 
         // check if loop count has expired
         FAPI_ASSERT((l_polls < P9_L2_FLUSH_MAX_POLLS),
@@ -205,26 +182,28 @@ fapi2::ReturnCode l2_flush_check_status(
         FAPI_DBG("%d loops done, delaying before next poll", l_polls);
 
         FAPI_TRY(fapi2::delay(P9_L2_FLUSH_HW_NS_DELAY,
-                              P9_L2_FLUSH_SIM_CYCLE_DELAY),
-                 "fapi delay Error");
+                              P9_L2_FLUSH_SIM_CYCLE_DELAY));
 
         l_polls++;
     }
 
 fapi_try_exit:
+    FAPI_DBG("l2_flush_check_status: Exit");
     return fapi2::current_err;
 }
 
 //------------------------------------------------------------------------------
 // Hardware Procedure
+// See doxygen in header file.
 //------------------------------------------------------------------------------
-fapi2::ReturnCode p9_l2_flush(const fapi2::Target < fapi2::TARGET_TYPE_EX >
-                              & i_target,
-                              const p9core::purgeData_t& i_purgeData)
+fapi2::ReturnCode p9_l2_flush(
+    const fapi2::Target <fapi2::TARGET_TYPE_EX>& i_target,
+    const p9core::purgeData_t& i_purgeData)
 {
-    FAPI_DBG("i_purgeData [iv_cmdType: 0x%x] [iv_cmdMem : 0x%x] "
-             "[iv_cmdBank: 0x%x] [iv_cmdCGC : 0x%x]", i_purgeData.iv_cmdType,
-             i_purgeData.iv_cmdMem, i_purgeData.iv_cmdBank, i_purgeData.iv_cmdCGC);
+    FAPI_DBG("Entering p9_l2_flush: i_purgeData [iv_cmdType: 0x%x] "
+             "[iv_cmdMem : 0x%x] [iv_cmdBank: 0x%x] [iv_cmdCGC : 0x%x]",
+             i_purgeData.iv_cmdType, i_purgeData.iv_cmdMem,
+             i_purgeData.iv_cmdBank, i_purgeData.iv_cmdCGC);
 
     uint32_t l_regAddr = EX_PRD_PURGE_CMD_REG;
 
@@ -236,7 +215,7 @@ fapi2::ReturnCode p9_l2_flush(const fapi2::Target < fapi2::TARGET_TYPE_EX >
     FAPI_TRY(l2_flush_check_status(i_target, l_regAddr));
 
 fapi_try_exit:
-    FAPI_INF("l2_flush, Ex: Exit");
+    FAPI_DBG("p9_l2_flush: Exit");
     return fapi2::current_err;
 }
 
