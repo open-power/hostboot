2415c1b6a66ac8778fc3d04cafe76e766bd21a89
Shelton Leung <sleung@us.ibm.com>
2017-07-18 12:03:44 -0500

    dis spec ops dcbf HW414958
    Change-Id: I17c325727c6c6c48d6963f988ed7588f616fa589
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/43277
    Dev-Ready: Joseph J. McGill <jmcgill@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Jenny Huynh <jhuynh@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/initfiles/p9c_mi_scom.C
+++ src/import/chips/p9/procedures/hwp/initfiles/p9c_mi_scom.C
@@ -32,6 +32,7 @@ using namespace fapi2;
 constexpr uint64_t literal_7 = 7;
 constexpr uint64_t literal_1 = 1;
 constexpr uint64_t literal_0x19 = 0x19;
+constexpr uint64_t literal_0b0000000000001000000 = 0b0000000000001000000;
 constexpr uint64_t literal_0b0000000000001000 = 0b0000000000001000;
 
 fapi2::ReturnCode p9c_mi_scom(const fapi2::Target<fapi2::TARGET_TYPE_MI>& TGT0,
@@ -86,6 +87,12 @@ fapi2::ReturnCode p9c_mi_scom(const fapi2::Target<fapi2::TARGET_TYPE_MI>& TGT0,
             }
 
             FAPI_TRY(fapi2::putScom(TGT0, 0x5010811ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x5010812ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<33, 19, 45, uint64_t>(literal_0b0000000000001000000 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x5010812ull, l_scom_buffer));
         }
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x5010813ull, l_scom_buffer ));
