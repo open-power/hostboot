3a33d7a5fd07047bbf94498f6187c41acaf048ba
Joe McGill <jmcgill@us.ibm.com>
2017-07-26 10:27:12 -0500

    p9.int.scom.initfile -- mask SUE FIR for Nimbus DD2
    Change-Id: I8c921e4d6adf441daa6892e56fc1208081a6fb48
    CQ: HW411637
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/43637
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: DAVID M. KAUER <dmkauer@us.ibm.com>
    Reviewed-by: Matt K. Light <mklight@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/43645
    Reviewed-by: Hostboot Team <hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/initfiles/p9_int_scom.C
+++ src/import/chips/p9/procedures/hwp/initfiles/p9_int_scom.C
@@ -33,6 +33,7 @@ constexpr uint64_t literal_0 = 0;
 constexpr uint64_t literal_1 = 1;
 constexpr uint64_t literal_0x0070000072040140 = 0x0070000072040140;
 constexpr uint64_t literal_0x0000004004028000 = 0x0000004004028000;
+constexpr uint64_t literal_0x2000004004008000 = 0x2000004004008000;
 constexpr uint64_t literal_0x0000004004008000 = 0x0000004004008000;
 constexpr uint64_t literal_0x0000000000000000 = 0x0000000000000000;
 constexpr uint64_t literal_0x9554021F80110FCF = 0x9554021F80110FCF;
@@ -61,6 +62,8 @@ fapi2::ReturnCode p9_int_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
         FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_FABRIC_PUMP_MODE, TGT1, l_TGT1_ATTR_PROC_FABRIC_PUMP_MODE));
         fapi2::ATTR_CHIP_EC_FEATURE_INT_DD1_Type l_TGT0_ATTR_CHIP_EC_FEATURE_INT_DD1;
         FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_EC_FEATURE_INT_DD1, TGT0, l_TGT0_ATTR_CHIP_EC_FEATURE_INT_DD1));
+        fapi2::ATTR_CHIP_EC_FEATURE_HW411637_Type l_TGT0_ATTR_CHIP_EC_FEATURE_HW411637;
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_EC_FEATURE_HW411637, TGT0, l_TGT0_ATTR_CHIP_EC_FEATURE_HW411637));
         fapi2::buffer<uint64_t> l_scom_buffer;
         {
             FAPI_TRY(fapi2::getScom( TGT0, 0x501300aull, l_scom_buffer ));
@@ -107,6 +110,10 @@ fapi2::ReturnCode p9_int_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
             if ((l_TGT0_ATTR_CHIP_EC_FEATURE_INT_DD1 == literal_1))
             {
                 l_scom_buffer.insert<0, 64, 0, uint64_t>(literal_0x0000004004028000 );
+            }
+            else if (((l_TGT0_ATTR_CHIP_EC_FEATURE_INT_DD1 == literal_0) && (l_TGT0_ATTR_CHIP_EC_FEATURE_HW411637 != literal_0)))
+            {
+                l_scom_buffer.insert<0, 64, 0, uint64_t>(literal_0x2000004004008000 );
             }
             else if ((l_TGT0_ATTR_CHIP_EC_FEATURE_INT_DD1 == literal_0))
             {
