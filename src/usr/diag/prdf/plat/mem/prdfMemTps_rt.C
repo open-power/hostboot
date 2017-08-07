3f8f25c0900a47b8d53d22b316bd5f2ae76ca35a
Caleb Palmer <cnpalmer@us.ibm.com>
2017-08-07 14:31:12 -0500

    PRD: TPS Analyze ECC attentions
    Change-Id: I2326623087bf8d8b1c6d3a4d79472837e5c5857e
    RTC: 171914
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/44368
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Benjamin J. Weisenbeck <bweisenb@us.ibm.com>
    Reviewed-by: Brian J. Stegmiller <bjs@us.ibm.com>
    Reviewed-by: Zane C. Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/44765
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>

--- src/usr/diag/prdf/plat/mem/prdfMemTps_rt.C
+++ src/usr/diag/prdf/plat/mem/prdfMemTps_rt.C
@@ -26,15 +26,20 @@
 /** @file prdfMemTps_rt.C */
 
 // Platform includes
+#include <prdfMemEccAnalysis.H>
+#include <prdfMemScrubUtils.H>
 #include <prdfMemTdFalseAlarm.H>
 #include <prdfMemTps.H>
 #include <prdfP9McaExtraSig.H>
 #include <prdfP9McaDataBundle.H>
+#include <prdfTargetServices.H>
 
 using namespace TARGETING;
 
 namespace PRDF
 {
+
+using namespace PlatServices;
 
 //------------------------------------------------------------------------------
 
@@ -86,15 +91,140 @@ uint32_t TpsEvent<T>::analyzeTpsPhase1_rt( STEP_CODE_DATA_STRUCT & io_sc,
     uint32_t o_rc = SUCCESS;
 
     // TODO RTC 171914
-    // Analyze Ecc Attentions
-    // Analyze CEs
+    do
+    {
+        // Analyze Ecc Attentions
+        uint32_t eccAttns;
+        o_rc = checkEccFirs<T>( iv_chip, eccAttns );
+        if ( SUCCESS != o_rc )
+        {
+            PRDF_ERR( PRDF_FUNC "checkEccFirs(0x%08x) failed",
+                    iv_chip->getHuid() );
+            break;
+        }
 
-    o_done = true;
-    PRDF_ERR( PRDF_FUNC "function not implemented yet" );
+        o_rc = analyzeEcc( eccAttns, io_sc, o_done );
+        if ( SUCCESS != o_rc )
+        {
+            PRDF_ERR( PRDF_FUNC "analyzeEcc() failed." );
+            break;
+        }
+        if ( o_done ) break;
+
+        // Analyze CEs
+
+        o_done = true;
+        PRDF_ERR( PRDF_FUNC "function not implemented yet" );
+
+    }while(0);
 
     return o_rc;
 
     #undef PRDF_FUNC
+}
+
+//------------------------------------------------------------------------------
+
+template <>
+uint32_t TpsEvent<TYPE_MCA>::analyzeEcc( const uint32_t & i_eccAttns,
+                                         STEP_CODE_DATA_STRUCT & io_sc,
+                                         bool & o_done )
+{
+    #define PRDF_FUNC "[TpsEvent<TYPE_MCA>::analyzeEcc] "
+
+    uint32_t o_rc = SUCCESS;
+
+    do
+    {
+        // If there was a UE.
+        if ( i_eccAttns & MAINT_UE )
+        {
+            PRDF_TRAC( PRDF_FUNC "UE Detected: 0x%08x,0x%02x",
+                       iv_chip->getHuid(), getKey() );
+
+            io_sc.service_data->setSignature( iv_chip->getHuid(),
+                                              PRDFSIG_MaintUE );
+
+            // At this point we don't actually have an address for the UE. The
+            // best we can do is get the address in which the command stopped.
+            MemAddr addr;
+            o_rc = getMemMaintAddr<TYPE_MCA>( iv_chip, addr );
+            if ( SUCCESS != o_rc )
+            {
+                PRDF_ERR( PRDF_FUNC "getMemMaintAddr(0x%08x) failed",
+                          iv_chip->getHuid() );
+                break;
+            }
+
+            o_rc = MemEcc::handleMemUe<TYPE_MCA>( iv_chip, addr,
+                                                  UE_TABLE::SCRUB_UE, io_sc );
+            if ( SUCCESS != o_rc )
+            {
+                PRDF_ERR( PRDF_FUNC "handleMemUe(0x%08x,0x%02x) failed",
+                          iv_chip->getHuid(), getKey() );
+                break;
+            }
+
+            // Abort this procedure because additional repairs will likely
+            // not help (also avoids complication of having UE and MPE at
+            // the same time).
+            o_done = true; break;
+        }
+
+        // If there was an IUE (MNFG only).
+        if ( mfgMode() && (i_eccAttns & MAINT_IUE) )
+        {
+            io_sc.service_data->setSignature( iv_chip->getHuid(),
+                                              PRDFSIG_MaintIUE );
+
+            o_rc = MemEcc::handleMemIue<TYPE_MCA, McaDataBundle *>( iv_chip,
+                                                                    iv_rank,
+                                                                    io_sc );
+            if ( SUCCESS != o_rc )
+            {
+                PRDF_ERR( PRDF_FUNC "handleMemIue(0x%08x,0x%02x) failed",
+                          iv_chip->getHuid(), getKey() );
+                break;
+            }
+
+            // If service call is set, then IUE threshold was reached.
+            if ( io_sc.service_data->queryServiceCall() )
+            {
+                PRDF_TRAC( PRDF_FUNC "IUE threshold detected: 0x%08x,0x%02x",
+                           iv_chip->getHuid(), getKey() );
+
+                // Abort this procedure because port failure will be triggered
+                // after analysis is complete.
+                o_done = true; break;
+            }
+        }
+
+        // If there was an MPE.
+        if ( i_eccAttns & MAINT_MPE )
+        {
+            io_sc.service_data->setSignature( iv_chip->getHuid(),
+                                              PRDFSIG_MaintMPE );
+
+            o_rc = MemEcc::handleMpe<TYPE_MCA, McaDataBundle *>( iv_chip,
+                iv_rank, io_sc );
+            if ( SUCCESS != o_rc )
+            {
+                PRDF_ERR( PRDF_FUNC "handleMpe<T>(0x%08x, 0x%02x) failed",
+                          iv_chip->getHuid(), iv_rank.getKey() );
+                break;
+            }
+
+            // Abort this procedure because the chip mark may have fixed the
+            // symbol that triggered TPS
+            o_done = true; break;
+        }
+
+    }while(0);
+
+    return o_rc;
+
+    #undef PRDF_FUNC
+
 }
 
 //------------------------------------------------------------------------------
