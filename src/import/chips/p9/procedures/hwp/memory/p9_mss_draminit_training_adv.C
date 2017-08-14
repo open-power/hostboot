65953c7d38afeb9c99dbeab195d6cc978cf2519d
Jacob Harvey <jlharvey@us.ibm.com>
2017-08-14 15:16:43 -0500

    Implementing draminit_training_adv
        Set default pattern to john's new one and backup to supernova 2.0
    
    Change-Id: Iefe5991e4cf5d262118d3af1462a86247819601e
    Original-Change-Id: I406bb5c5652cff9fe4690e5bd9b03cc431d75f61
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/44709
    Dev-Ready: JACOB L. HARVEY <jlharvey@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Matt K. Light <mklight@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/p9_mss_draminit_training_adv.C
+++ src/import/chips/p9/procedures/hwp/memory/p9_mss_draminit_training_adv.C
@@ -24,7 +24,7 @@
 /* IBM_PROLOG_END_TAG                                                     */
 ///
 /// @file p9_mss_draminit_training_adv.C
-/// @brief Train dram
+/// @brief Perform read_centering again but with a custom pattern for better eye
 ///
 // *HWP HWP Owner: Jacob Harvey <jlharvey@us.ibm.com>
 // *HWP HWP Backup: Andre Marin <aamarin@us.ibm.com>
@@ -36,30 +36,164 @@
 #include <mss.H>
 #include <vector>
 
-#include <p9_mss_draminit_training.H>
+#include <p9_mss_draminit_training_adv.H>
 #include <lib/utils/count_dimm.H>
 #include <lib/shared/mss_const.H>
-#include <lib/workarounds/dp16_workarounds.H>
-#include <lib/workarounds/dqs_align_workarounds.H>
-#include <lib/fir/unmask.H>
-#include <lib/dimm/ddr4/zqcal.H>
+#include <lib/phy/dp16.H>
+#include <lib/phy/seq.H>
+#include <lib/phy/ddr_phy.H>
 
 using fapi2::TARGET_TYPE_MCBIST;
 using fapi2::TARGET_TYPE_MCA;
 
 extern "C"
 {
-    ///
-    /// @brief Train dram
-    /// @param[in] i_target, the McBIST of the ports of the dram you're training
-    /// @param[in] i_special_training, optional CAL_STEP_ENABLE override. Used in sim, debug
-    /// @param[in] i_abort_on_error, optional CAL_ABORT_ON_ERROR override. Used in sim, debug
-    /// @return FAPI2_RC_SUCCESS iff ok
-    ///
+///
+/// @brief Perform training_adv on the dimms (perform read centering with custom pattern)
+/// @param[in] i_target, the McBIST of the ports of the dram you're training
+/// @param[in] i_abort_on_error, optional CAL_ABORT_ON_ERROR override. Used in sim, debug
+/// @return FAPI2_RC_SUCCESS iff ok
+/// @note turn off rd cntr periodic cal if training_adv is run. Add OFF value for CUSTOM_PATTERN?
+///
+
     fapi2::ReturnCode p9_mss_draminit_training_adv( const fapi2::Target<TARGET_TYPE_MCBIST>& i_target,
-            const uint32_t i_special_training,
             const uint8_t i_abort_on_error)
     {
+        std::vector<fapi2::ReturnCode> l_fails;
+        uint8_t l_cal_abort_on_error = i_abort_on_error;
+
+        FAPI_INF("Start draminit training advance %s", mss::c_str(i_target));
+
+        // If there are no DIMMs installed, we don't need to bother. In fact, we can't as we didn't setup
+        // attributes for the PHY, etc.
+        if (mss::count_dimm(i_target) == 0)
+        {
+            FAPI_INF("... skipping draminit_training_adv %s - no DIMM ...", mss::c_str(i_target));
+            return fapi2::FAPI2_RC_SUCCESS;
+        }
+
+        if (i_abort_on_error == CAL_ADV_ABORT_SENTINAL)
+        {
+            FAPI_TRY( mss::cal_abort_on_error(l_cal_abort_on_error) );
+        }
+
+        // If Custom Read Centering or Custom Write Centering was performed,
+        // an Initial Pattern Write of 5555 hex, the reset value of the registers described in,
+        // must be performed before any periodic calibration routines are performed.
+        // Clean out any previous calibration results, set bad-bits and configure the ranks.
+        for( const auto& p : mss::find_targets<TARGET_TYPE_MCA>(i_target))
+        {
+            fapi2::buffer<uint32_t> l_cal_steps_enabled;
+            FAPI_TRY( mss::cal_step_enable(p, l_cal_steps_enabled), "Error in p9_mss_draminit_training %s", mss::c_str(i_target) );
+
+            if (!l_cal_steps_enabled.getBit<mss::TRAINING_ADV>())
+            {
+                FAPI_INF("ATTR_MSS_CAL_STEPS_ENABLED says to skip draminit training advance, so skipping %s",
+                         mss::c_str(i_target));
+                continue;
+            }
+
+            // If we see the bit to for training_adv, let's set up our cal config.
+            // As per the PHY spec, only INITIAL_PAT_WRITE and the CUSTOM_RD_PATTERN bits should be set
+            // TRAINING_ADV == CUSTOM_RD_PATTERN
+            l_cal_steps_enabled = 0;
+            l_cal_steps_enabled.setBit<mss::INITIAL_PAT_WR>().setBit<mss::TRAINING_ADV>();
+
+            // Keep track of the last error seen by a rank pair
+            fapi2::ReturnCode l_rank_pair_error(fapi2::FAPI2_RC_SUCCESS);
+
+            // Clear all of the errors before we start
+            FAPI_TRY(mss::clear_initial_cal_errors(p), "%s error resetting errors prior to init cal", mss::c_str(p));
+
+            // Returned from set_rank_pairs, it tells us how many rank pairs
+            // we configured on this port.
+            std::vector<uint64_t> l_pairs;
+
+            // Let's set the pattern to run
+            FAPI_TRY( mss::configure_custom_pattern(p) );
+
+            // Set staggered mode
+            FAPI_TRY( mss::rc::change_staggered_pattern(p) );
+
+            // Set custom read centering mode
+            FAPI_TRY( mss::dp16::setup_custom_read_centering_mode(p), "Error in p9_mss_draminit_training %s",
+                      mss::c_str(i_target) );
+
+            // Get our rank pairs.
+            FAPI_TRY( mss::rank::get_rank_pairs(p, l_pairs), "Error in p9_mss_draminit_training" );
+
+            // For each rank pair we need to calibrate, pop a ccs instruction in an array and execute it.
+            // NOTE: IF YOU CALIBRATE MORE THAN ONE RANK PAIR PER CCS PROGRAM, MAKE SURE TO CHANGE
+            // THE PROCESSING OF THE ERRORS. (it's hard to figure out which DIMM failed, too) BRS.
+            for (const auto& rp : l_pairs)
+            {
+                bool l_cal_fail = false;
+
+                std::vector<fapi2::ReturnCode> l_temp_fails;
+                FAPI_TRY( mss::setup_and_execute_cal(p, rp, l_cal_steps_enabled, l_cal_abort_on_error) );
+                FAPI_TRY( mss::find_and_log_cal_errors(p, rp, l_cal_abort_on_error, l_cal_fail, l_temp_fails) );
+
+                // If we got a fail, let's ignore the previous fails and run backup pattern
+                if (l_cal_fail)
+                {
+                    l_cal_fail = false;
+
+                    // Clear the disable bits from last run.
+                    // This function restores bad_bits to how the attribute has them
+                    // (which was updated at the end of regular training)
+                    // So we won't run on known bad bits, but will rerun on the iffy bits from last custom_pattern run
+                    FAPI_TRY( mss::dp16::reset_bad_bits( p ) );
+                    FAPI_INF("%s rp%x running back up pattern for draminit_training_adv", mss::c_str(p), rp);
+
+                    // Clear the cal errors so we can retry
+                    FAPI_TRY(mss::clear_initial_cal_errors(p), "%s error resetting errors prior to init cal", mss::c_str(p));
+
+                    fapi2::buffer<uint32_t> l_backup;
+                    FAPI_TRY( mss::custom_training_adv_backup_patterns( p, l_backup) );
+
+                    // Put the backup into the registers
+                    FAPI_TRY( mss::seq::setup_rd_wr_data( p, l_backup) );
+
+                    // Rerun the training for this rp
+                    FAPI_TRY( mss::setup_and_execute_cal(p, rp, l_cal_steps_enabled, l_cal_abort_on_error) );
+                    FAPI_TRY( mss::find_and_log_cal_errors(p, rp, l_cal_abort_on_error, l_cal_fail, l_fails) );
+
+                    // If l_cal_fail == true here, error out
+                    if (l_cal_fail)
+                    {
+                        fapi2::Target<fapi2::TARGET_TYPE_DIMM> l_dimm;
+
+                        // Let's get the DIMM since we train per rank pair (primary rank pair)
+                        FAPI_TRY( mss::rank_pair_primary_to_dimm(p,
+                                  rp,
+                                  l_dimm),
+                                  "Failed getting the DIMM for %s rp %x", mss::c_str(p), rp);
+
+                        FAPI_ASSERT( l_cal_fail == false,
+                                     fapi2::MSS_DRAMINIT_TRAINING_CUSTOM_PATTERN_RD_ERROR()
+                                     .set_PORT_POSITION(mss::index(p))
+                                     .set_RANKGROUP_POSITION(rp)
+                                     .set_MCA_TARGET(p)
+                                     .set_DIMM_TARGET(l_dimm),
+                                     "%s rp%x Failed Draminit_training_adv", mss::c_str(p), rp);
+                    }
+                }
+            }// rank pairs
+
+            // Resetting current_err.
+            // The error has either already been "logged" or we have exited and returned the error up the call stack.
+            fapi2::current_err = fapi2::FAPI2_RC_SUCCESS;
+        }
+
+        // If we fail draminit_training_adv, we should exit out the ipl / reconfigure
+        // Shouldn't really get here but let's do it anyways
+        if (l_fails.size() != 0)
+        {
+            FAPI_TRY(l_fails[0]);
+        }
+
         return fapi2::FAPI2_RC_SUCCESS;
+    fapi_try_exit:
+        return fapi2::current_err;
     }
 }
