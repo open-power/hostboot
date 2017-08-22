289062fda679d0ff937a98f961872c7e880423c8
Sakethan R Kotta <sakkotta@in.ibm.com>
2017-08-22 10:08:41 -0500

    Cumulus HWPs for istep11
    Change-Id: I18b8eb5595b6c83c05eae52f2d870898b28f1116
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/44967
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Prachi Gupta <pragupta@us.ibm.com>
    Reviewed-by: Corey V. Swenson <cswenson@us.ibm.com>
    Reviewed-by: Thomas R. Sand <trsand@us.ibm.com>
    Reviewed-by: SWATHI M. BHATTIPROLU <bhmadhur@in.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep11/call_cen_initf.C
+++ src/usr/isteps/istep11/call_cen_initf.C
@@ -22,17 +22,90 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
-#include <errl/errlentry.H>
+#include    <stdint.h>
 
-using namespace ERRORLOG;
+#include    <trace/interface.H>
+#include    <initservice/taskargs.H>
+#include    <errl/errlentry.H>
+
+#include    <isteps/hwpisteperror.H>
+#include    <errl/errludtarget.H>
+
+#include    <initservice/isteps_trace.H>
+
+//  targeting support
+#include    <targeting/common/commontargeting.H>
+#include    <targeting/common/utilFilter.H>
+
+//Fapi Support
+#include    <config.h>
+#include    <fapi2.H>
+#include    <fapi2/plat_hwp_invoker.H>
+#include    <util/utilmbox_scratch.H>
+
+// HWP
+#include    <cen_initf.H>
+
+using   namespace   ERRORLOG;
+using   namespace   ISTEP;
+using   namespace   ISTEP_ERROR;
+using   namespace   TARGETING;
 
 namespace ISTEP_11
 {
 void* call_cen_initf (void *io_pArgs)
 {
+    IStepError l_StepError;
     errlHndl_t l_err = NULL;
 
-    return l_err;
+    TRACDCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_cen_initf entry" );
+
+    TARGETING::TargetHandleList l_membufTargetList;
+    getAllChips(l_membufTargetList, TYPE_MEMBUF);
+
+    TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace, "call_cen_initf: %d membufs found",
+            l_membufTargetList.size());
+
+    for (const auto & l_membuf_target : l_membufTargetList)
+    {
+        TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+            "cen_initf HWP target HUID %.8x",
+            TARGETING::get_huid(l_membuf_target));
+
+        //  call the HWP with each target
+        fapi2::Target <fapi2::TARGET_TYPE_MEMBUF_CHIP> l_fapi_membuf_target
+                (l_membuf_target);
+
+        FAPI_INVOKE_HWP(l_err, cen_initf, l_fapi_membuf_target);
+
+        //  process return code.
+        if ( l_err )
+        {
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                "ERROR 0x%.8X:  cen_initf HWP on target HUID %.8x",
+                l_err->reasonCode(), TARGETING::get_huid(l_membuf_target) );
+
+            // capture the target data in the elog
+            ErrlUserDetailsTarget(l_membuf_target).addToLog( l_err );
+
+            // Create IStep error log and cross reference to error that occurred
+            l_StepError.addErrorDetails( l_err );
+
+            // Commit Error
+            errlCommit( l_err, ISTEP_COMP_ID );
+        }
+        else
+        {
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                     "SUCCESS :  cen_initf HWP");
+        }
+
+    }
+
+
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_cen_initf exit" );
+
+    return l_StepError.getErrorHandle();
 }
 
 };
