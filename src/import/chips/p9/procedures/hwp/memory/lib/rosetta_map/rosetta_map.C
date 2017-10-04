fd207a3412f2320077add14351a2f69f27b8f052
Andre Marin <aamarin@us.ibm.com>
2017-10-04 10:39:39 -0500

    Update HPW Level for MSS API library
    Change-Id: Ic2803754615e3af2d18bc8d2f7741d4c06be7ef9
    Original-Change-Id: I33f36a6cc647d66c39ac46ba2922dcd1c49d86b5
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/47150
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/52087
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/lib/rosetta_map/rosetta_map.C
+++ src/import/chips/p9/procedures/hwp/memory/lib/rosetta_map/rosetta_map.C
@@ -33,7 +33,7 @@
 // *HWP HWP Owner: Louis Stermole <stermole@us.ibm.com>
 // *HWP HWP Backup: Andre Marin <aamarin@us.ibm.com>
 // *HWP Team: Memory
-// *HWP Level: 2
+// *HWP Level: 3
 // *HWP Consumed by: FSP:HB
 
 #include <rosetta_map.H>
