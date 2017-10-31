6f0334dafbc915700ac6d1603d0ebd7f84dea266
Stephen Cprek <smcprek@us.ibm.com>
2017-10-31 13:01:30 -0500

    Create runtime_utils so both IPL and runtime can use common functions
    Change-Id: I130f87acdcb841640de7768f1c0abe38e45ce677
    RTC: 180063
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/49754
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Nicholas E. Bofferding <bofferdn@us.ibm.com>
    Reviewed-by: Marshall J. Wilks <mjwilks@us.ibm.com>
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

---
+++ src/usr/runtime/common/runtime_utils.C
@@ -0,0 +1,61 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/usr/runtime/common/runtime_utils.C $                      */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2017                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include <runtime/common/runtime_utils.H>
+
+namespace RUNTIME
+{
+
+// -- Verified Images
+//   -- OCC
+//   -- WOFDATA
+//   -- HCODE
+// -- Non-verified Images
+///  -- RINGOVD
+const PreVerifyVector preVerifiedPnorSections {
+    {PNOR::OCC, true},
+    {PNOR::WOFDATA, true},
+    {PNOR::HCODE, true},
+    {PNOR::RINGOVD, false},
+};
+
+bool isPreVerifiedSection(const PNOR::SectionId i_section)
+{
+    bool l_result = false;
+    auto it = find_if(preVerifiedPnorSections.begin(),
+                      preVerifiedPnorSections.end(),
+                      [&i_section](const PreVerifyPair& p)
+                      {
+                          return p.first == i_section;
+                      });
+
+    if (it != preVerifiedPnorSections.end())
+    {
+        l_result = true;
+    }
+
+    return l_result;
+}
+
+}
