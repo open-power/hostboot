e35c1c33d1aa45d18b7e95c93312b68463c30c75
Claus Michael Olsen <cmolsen@us.ibm.com>
2018-01-04 22:51:20 -0600

    p9_dd_container: Introducing DD container API lib to PPE repo
    Change-Id: I488c09c98383c287b9812c59e92cf9647ac9c042
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/51517
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Richard J. Knight <rjknight@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/utils/imageProcs/p9_dd_container.c
+++ src/import/chips/p9/utils/imageProcs/p9_dd_container.c
@@ -48,7 +48,7 @@ int p9_dd_validate(struct p9_dd_cont* i_cont)
         return DDCO_FAILURE_MAGIC_NOT_FOUND;
     }
 
-    // may want to check here for holes or overlap as to stored blocks
+    // TBD: We can do more checking here, if the container is populated
 
     return DDCO_SUCCESS;
 }
