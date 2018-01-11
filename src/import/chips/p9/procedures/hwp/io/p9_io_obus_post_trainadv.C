78c8b54bee4d7dbc5624f5d553e9b27a97c904d5
Joe McGill <jmcgill@us.ibm.com>
2018-01-11 14:28:32 -0600

    create shells for IO OBUS pre, post training HWPs
      currently empty
    
    Change-Id: I1d51500aa5434a0fc590c88a0fcd6104ed368570
    Original-Change-Id: I324720af79af6a4a3a23dedcbc4bad804829a84d
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/51799
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Christopher W. Steffen <cwsteffen@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/io/p9_io_obus_post_trainadv.C
@@ -0,0 +1,73 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/io/p9_io_obus_post_trainadv.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2017,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p9_io_obus_post_trainadv.H
+/// @brief Post-Training PHY Status Function.
+///
+///-----------------------------------------------------------------------------
+/// *HWP HWP Owner        : Chris Steffen <cwsteffen@us.ibm.com>
+/// *HWP HWP Backup Owner : Gary Peterson <garyp@us.ibm.com>
+/// *HWP FW Owner         : Jamie Knight <rjknight@us.ibm.com>
+/// *HWP Team             : IO
+/// *HWP Level            : 3
+/// *HWP Consumed by      : FSP:HB
+///-----------------------------------------------------------------------------
+///
+/// @verbatim
+/// High-level procedure flow:
+///
+/// Post-Training PHY Status Function.
+///
+/// Procedure Prereq:
+///   - System clocks are running.
+///   - Scominit Procedure is completed.
+///   - IO DCCAL Procedure is completed.
+///   - IO Run Training Procedure is completed.
+/// @endverbatim
+///----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+// Includes
+// ----------------------------------------------------------------------------
+#include <p9_io_obus_post_trainadv.H>
+
+// ----------------------------------------------------------------------------
+// Procedure Function
+// ----------------------------------------------------------------------------
+
+/**
+ * @brief A simple HWP that runs after io_run_training.
+ *  This function is called on every Obus.
+ * @param[in] i_target Fapi2 OBUS Target
+ * @retval ReturnCode
+ */
+fapi2::ReturnCode p9_io_obus_post_trainadv(
+    const fapi2::Target<fapi2::TARGET_TYPE_OBUS>& i_tgt)
+{
+    FAPI_IMP("Entering...");
+    FAPI_IMP("Exiting...");
+    return fapi2::current_err;
+}
