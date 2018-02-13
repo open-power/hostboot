fb663d7186c1b2e9120c6ad3449f27555b716784
Rahul Batra <rbatra@us.ibm.com>
2018-02-12 22:09:23 -0600

    PSTATE: Remove Already ON/OFF PGPE-OCC IPC Error RC
        Key_Cronus_Test=PM_REGRESS
    
    Change-Id: I009bb7307468139529e8325eb8ad86c1706a6117
    CQ:SW417260
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/53919
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Martha Broyles <mbroyles@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Tested-by: Cronus HW CI <cronushw-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/53924
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/common/pmlib/include/pstate_pgpe_occ_api.h
+++ src/import/chips/p9/common/pmlib/include/pstate_pgpe_occ_api.h
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2017                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2018                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -62,12 +62,8 @@ enum MESSAGE_ID_IPI2HI
 #define PGPE_RC_SUCCESS                         0x01
 #define PGPE_WOF_RC_NOT_ENABLED                 0x10
 #define PGPE_RC_PSTATES_NOT_STARTED             0x11
-#define PGPE_RC_REQ_PSTATE_ALREADY_STARTED      0x12
-#define PGPE_RC_REQ_PSTATE_ALREADY_STOPPED      0x13
 #define PGPE_RC_OCC_NOT_PMCR_OWNER              0x14
 #define PGPE_RC_PM_COMPLEX_SUSPEND_SAFE_MODE    0x15
-#define PGPE_RC_WOF_ALREADY_ON                  0x16
-#define PGPE_RC_WOF_ALREADY_OFF                 0x17
 // Active quad mismatch with requested active quads.  PGPE did not switch
 // to using the new VFRT.  The original VFRT is still being used.
 #define PGPE_WOF_RC_VFRT_QUAD_MISMATCH  0x20
