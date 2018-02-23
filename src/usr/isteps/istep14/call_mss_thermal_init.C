0dbb063085655ee322b26b282faab6f3ad73301e
Roland Veloz <rveloz@us.ibm.com>
2018-02-22 18:10:58 -0600

    Fixed both NIMBUS and CUMULUS. They are now making the call to mss_thermal_init
    Neither NIMBUS or Cumulus was making the call to mss_thermal_init.  For
    Cumulus, the issue was trying to create a target list by getting a
    list of chiplets when MEMBUF is a chip.  For NIMBUS, well, the code
    was not even there.  For Nimbus a target list for the MCS, which is
    chiplet in this case, is created and used during the call.
    
    Change-Id: I17c4e1b12435c4dcfaa528b905d67a59962b3bca
    CQ: SW416904
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/54621
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep14/call_mss_thermal_init.C
+++ src/usr/isteps/istep14/call_mss_thermal_init.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2017                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2018                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -38,6 +38,7 @@
 #include    <fapi2.H>
 #include    <fapi2/plat_hwp_invoker.H>
 #include    <p9c_mss_thermal_init.H>
+#include    <p9_mss_thermal_init.H>
 #include    <p9_throttle_sync.H>
 
 
@@ -56,23 +57,13 @@ void* call_mss_thermal_init (void *io_pArgs)
     // -- Cumulus only ---
     // Get all Centaur targets
     TARGETING::TargetHandleList l_memBufTargetList;
-    getAllChiplets(l_memBufTargetList, TYPE_MEMBUF );
+    getAllChips(l_memBufTargetList, TYPE_MEMBUF);
 
     //  --------------------------------------------------------------------
-    //  run mss_thermal_init on all Centaurs
+    //  run mss_thermal_init on all functional Centaur chips
     //  --------------------------------------------------------------------
-    for (TargetHandleList::const_iterator
-         l_iter = l_memBufTargetList.begin();
-         l_iter != l_memBufTargetList.end();
-         ++l_iter)
+    for (auto l_pCentaur : l_memBufTargetList)
     {
-        //  make a local copy of the target for ease of use
-        TARGETING::Target*  l_pCentaur = *l_iter;
-
-        //  write HUID of target
-        TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-                  "target HUID %.8X", TARGETING::get_huid(l_pCentaur));
-
         fapi2::Target<fapi2::TARGET_TYPE_MEMBUF_CHIP> l_fapi_pCentaur
           (l_pCentaur);
 
@@ -86,7 +77,7 @@ void* call_mss_thermal_init (void *io_pArgs)
         if ( l_errl )
         {
             TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-                      "ERROR 0x%.8X: mss_thermal_init HWP returns error",
+                      "ERROR 0x%.8X: p9c_mss_thermal_init HWP returns error",
                       l_errl->reasonCode());
 
             // capture the target data in the elog
@@ -103,8 +94,53 @@ void* call_mss_thermal_init (void *io_pArgs)
         }
         else
         {
-            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                       "SUCCESS : mss_thermal_init HWP( )" );
+            TRACDCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                       "SUCCESS : p9c_mss_thermal_init HWP( )" );
+        }
+    }
+
+    // -- Nimbus only ---
+    // Get all MCS targets
+    TARGETING::TargetHandleList l_mcsTargetList;
+    getAllChiplets(l_mcsTargetList, TYPE_MCS);
+
+    //  --------------------------------------------------------------------
+    //  run mss_thermal_init on all functional MCS chiplets
+    //  --------------------------------------------------------------------
+    for (auto l_pMcs : l_mcsTargetList)
+    {
+        fapi2::Target<fapi2::TARGET_TYPE_MCS> l_fapi_pMcs
+          (l_pMcs);
+
+        // Current run on target
+        TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                   "Running call_mss_thermal_init HWP on "
+                   "target HUID %.8X", TARGETING::get_huid(l_pMcs));
+
+        FAPI_INVOKE_HWP( l_errl, p9_mss_thermal_init, l_fapi_pMcs );
+
+        if ( l_errl )
+        {
+            TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                      "ERROR 0x%.8X: p9_mss_thermal_init HWP returns error",
+                      l_errl->reasonCode());
+
+            // capture the target data in the elog
+            ErrlUserDetailsTarget(l_pMcs).addToLog( l_errl );
+
+            // Create IStep error log and cross reference
+            // to error that occurred
+            l_StepError.addErrorDetails( l_errl );
+
+            // Commit Error
+            errlCommit( l_errl, HWPF_COMP_ID );
+
+            break;
+        }
+        else
+        {
+            TRACDCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                       "SUCCESS : p9_mss_thermal_init HWP( )" );
         }
     }
 
@@ -120,7 +156,8 @@ void* call_mss_thermal_init (void *io_pArgs)
         {
             //Convert the TARGETING::Target into a fapi2::Target by passing
             //l_procChip into the fapi2::Target constructor
-            fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>l_fapi2CpuTarget((l_procChip));
+            fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>
+                                               l_fapi2CpuTarget((l_procChip));
 
             TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
                        "Running p9_throttle_sync HWP on "
