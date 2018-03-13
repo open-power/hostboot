1f77e4b1f4f67abc4f1bc2dcab3eb40283794715
Stephen Glancy <sglancy@us.ibm.com>
2018-03-13 13:55:57 -0500

    Moves count_dimm to be in the memory generic folder
    Change-Id: I187788ea920d6c5930a72aa385ae32bdefd0c5a4
    Original-Change-Id: I6ff967f0f7d8f337f3e9b2011ba62e5554843f95
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/55871
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: ANDRE A. MARIN <aamarin@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/p9_mss_memdiag.C
+++ src/import/chips/p9/procedures/hwp/memory/p9_mss_memdiag.C
@@ -38,7 +38,7 @@
 
 #include <lib/utils/poll.H>
 #include <generic/memory/lib/utils/find.H>
-#include <lib/utils/count_dimm.H>
+#include <generic/memory/lib/utils/count_dimm.H>
 #include <lib/mcbist/address.H>
 #include <lib/mcbist/memdiags.H>
 #include <lib/mcbist/mcbist.H>
