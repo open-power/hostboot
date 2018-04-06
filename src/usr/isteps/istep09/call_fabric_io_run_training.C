c830e8f23be1758f56a836d352db8c00dbfeb0ce
Roland Veloz <rveloz@us.ibm.com>
2018-04-06 13:36:07 -0500

    Continuing support, in IStep 09, to facilitate the testing of SMP wrap
    Continuing the support, in IStep 09 to make the call to
    p9_io_obus_linktrain and p9_io_obus_post_trainadv.
    
    Change-Id: I3d070bfc6c55224b77b6342362d5d008d0f0a61b
    RTC:190096
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/56886
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: William G. Hoffa <wghoffa@us.ibm.com>
    Disable-CI: Roland Veloz <rveloz@us.ibm.com>
    Reviewed-by: Prachi Gupta <pragupta@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep09/call_fabric_io_run_training.C
+++ src/usr/isteps/istep09/call_fabric_io_run_training.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2016                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2018                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -22,6 +22,7 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
+
 /**
  *  @file call_fabric_io_run_training.C
  *
@@ -35,160 +36,153 @@
 /******************************************************************************/
 // Includes
 /******************************************************************************/
-#include    <stdint.h>
-#include    <map>
 
-#include    <trace/interface.H>
-#include    <initservice/taskargs.H>
-#include    <errl/errlentry.H>
+//  Integral and component ID support
+#include <stdint.h>                     // uint32_t
+#include <hbotcompid.H>                 // HWPF_COMP_ID
 
-#include    <isteps/hwpisteperror.H>
-#include    <errl/errludtarget.H>
+//  Tracing support
+#include <trace/interface.H>            // TRACFCOMP
+#include <initservice/isteps_trace.H>   // g_trac_isteps_trace
 
-#include    <initservice/isteps_trace.H>
+//  Targeting support
+#include <fapi2_target.H>               // fapi2::Target
+#include <target.H>                     // TARGETING::Target
 
-#include    <hwas/common/deconfigGard.H>
-#include    <hwas/common/hwasCommon.H>
+//  Error handling support
+#include <errl/errlentry.H>             // errlHndl_t
+#include <isteps/hwpisteperror.H>       // IStepError
 
-#include    <sbe/sbeif.H>
+//  Pbus link service support
+#include <pbusLinkSvc.H>                // TargetPairs_t, PbusLinkSvc
 
-//  targeting support
-#include   <targeting/common/attributes.H>
-#include   <targeting/common/targetservice.H>
-
-#include    <targeting/common/commontargeting.H>
-#include    <targeting/common/utilFilter.H>
-#include    <targeting/common/trace.H>
-
-#include  <pbusLinkSvc.H>
-#include  <fapi2/target.H>
-#include  <fapi2/plat_hwp_invoker.H>
-
-// HWP
-#include    <p9_io_xbus_linktrain.H>
+//  HWP call support
+#include <istepHelperFuncs.H>           // captureError
+#include <istep09/istep09HelperFuncs.H> // trainBusHandler
+#include <p9_io_xbus_linktrain.H>       // p9_io_xbus_linktrain
 
 namespace   ISTEP_09
 {
-
-using   namespace   ISTEP;
 using   namespace   ISTEP_ERROR;
-using   namespace   ERRORLOG;
+using   namespace   ISTEPS_TRACE;
 using   namespace   TARGETING;
-using   namespace   HWAS;
 
 // helper function prototypes
 uint8_t run_linktraining(
                 const fapi2::Target<fapi2::TARGET_TYPE_XBUS> &i_master_target,
                 const fapi2::Target<fapi2::TARGET_TYPE_XBUS> &i_slave_target,
                 ISTEP_ERROR::IStepError & o_step_error );
-//
-//  Wrapper function to call fabric_io_run_training
-//
-void*    call_fabric_io_run_training( void    *io_pArgs )
+
+//******************************************************************************
+// Wrapper function to call fabric_io_run_training
+//******************************************************************************
+void* call_fabric_io_run_training( void *io_pArgs )
 {
+    errlHndl_t  l_err(nullptr);
+    IStepError  l_stepError;
 
-    IStepError  l_StepError;
-    errlHndl_t  l_errl  =   NULL;
+    TRACFCOMP(g_trac_isteps_trace,ENTER_MRK"call_fabric_io_run_training entry");
 
-    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-               "call_fabric_io_run_training entry" );
+    uint32_t l_numberOfTrainFailures(0);
+    EDI_EI_INITIALIZATION::TargetPairs_t l_pbusConnections;
+    TYPE l_busSet[] = { TYPE_XBUS, TYPE_OBUS };
+    constexpr uint32_t l_maxBusSet(sizeof(l_busSet)/sizeof(TYPE));
 
-    uint8_t l_linktrain_failures = 0;
-    EDI_EI_INITIALIZATION::TargetPairs_t l_PbusConnections;
-    const uint32_t MaxBusSet = 1;
-    TYPE busSet[MaxBusSet] = { TYPE_XBUS }; // TODO RTC:152304 - add TYPE_OBUS
-
-    for (uint32_t ii = 0; (!l_errl) && (ii < MaxBusSet); ii++)
+    for (uint32_t ii = 0; ii < l_maxBusSet; ++ii)
     {
-        l_errl = EDI_EI_INITIALIZATION::PbusLinkSvc::getTheInstance().
-                    getPbusConnections(l_PbusConnections, busSet[ii]);
-        if (l_errl)
+        l_err = EDI_EI_INITIALIZATION::PbusLinkSvc::getTheInstance().
+                    getPbusConnections(l_pbusConnections, l_busSet[ii]);
+        if (l_err)
         {
-            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+            TRACFCOMP(g_trac_isteps_trace,
                 "ERROR 0x%.8X : getPbusConnections TYPE_%cBUS returns error",
-                l_errl->reasonCode(), (ii ? 'X':'O') );
+                l_err->reasonCode(), (ii ? 'O':'X') );
 
-            // Create IStep error log and cross reference to error that occurred
-            l_StepError.addErrorDetails( l_errl );
-
-            // Commit the error log
-            // Log should be deleted and set to NULL in errlCommit.
-            errlCommit(l_errl, HWPF_COMP_ID);
+            // Capture error and then
+            captureError(l_err,
+                         l_stepError,
+                         HWPF_COMP_ID);
 
             // Don't continue with a potential bad connection set
             break;
         }
 
-        for (const auto & l_PbusConnection: l_PbusConnections)
+        if (TYPE_XBUS == l_busSet[ii])
         {
-            const fapi2::Target <fapi2::TARGET_TYPE_XBUS>
-                l_thisPbusFapi2Target(
-                (const_cast<TARGETING::Target*>(l_PbusConnection.first)));
-
-            const fapi2::Target <fapi2::TARGET_TYPE_XBUS>
-                l_connectedPbusFapi2Target(
-                (const_cast<TARGETING::Target*>(l_PbusConnection.second)));
-
-
-            const TARGETING::ATTR_IO_XBUS_MASTER_MODE_type l_master_mode =
-                l_PbusConnection.first->getAttr
-                <TARGETING::ATTR_IO_XBUS_MASTER_MODE>();
-
-            TARGETING::ATTR_FAPI_NAME_type  l_master_target_name = {0};
-            TARGETING::ATTR_FAPI_NAME_type  l_slave_target_name = {0};
-            if (l_master_mode == fapi2::ENUM_ATTR_IO_XBUS_MASTER_MODE_TRUE)
+            for (const auto & l_pbusConnection: l_pbusConnections)
             {
-                l_linktrain_failures = run_linktraining(
-                                            l_thisPbusFapi2Target,
-                                            l_connectedPbusFapi2Target,
-                                            l_StepError);
-                fapi2::toString(l_thisPbusFapi2Target,
+                // Default the master and slave fapi2 targets
+                fapi2::Target <fapi2::TARGET_TYPE_XBUS>
+                l_masterFapi2Target(
+                (const_cast<TARGETING::Target*>(l_pbusConnection.first)));
+
+                fapi2::Target <fapi2::TARGET_TYPE_XBUS>
+                l_slaveFapi2Target(
+                (const_cast<TARGETING::Target*>(l_pbusConnection.second)));
+
+                //Swap master and slave fapi2 targets if master mode is NOT true
+                if (l_pbusConnection.first->getAttr
+                                  <TARGETING::ATTR_IO_XBUS_MASTER_MODE>() !=
+                    fapi2::ENUM_ATTR_IO_XBUS_MASTER_MODE_TRUE)
+                {
+                    l_masterFapi2Target =
+                    (const_cast<TARGETING::Target*>(l_pbusConnection.second));
+
+                    l_slaveFapi2Target =
+                    (const_cast<TARGETING::Target*>(l_pbusConnection.first));
+                }
+
+                TARGETING::ATTR_FAPI_NAME_type  l_master_target_name = {0};
+                TARGETING::ATTR_FAPI_NAME_type  l_slave_target_name = {0};
+
+                l_numberOfTrainFailures = run_linktraining(l_masterFapi2Target,
+                                                           l_slaveFapi2Target,
+                                                           l_stepError);
+                fapi2::toString(l_masterFapi2Target,
                                 l_master_target_name,
                                 sizeof(l_master_target_name));
-                fapi2::toString(l_connectedPbusFapi2Target,
+
+                fapi2::toString(l_slaveFapi2Target,
                                 l_slave_target_name,
                                 sizeof(l_slave_target_name));
-            }
-            else
-            {
-                l_linktrain_failures = run_linktraining(
-                                            l_connectedPbusFapi2Target,
-                                            l_thisPbusFapi2Target,
-                                            l_StepError);
-                fapi2::toString(l_connectedPbusFapi2Target,
-                                l_master_target_name,
-                                sizeof(l_master_target_name));
-                fapi2::toString(l_thisPbusFapi2Target,
-                                l_slave_target_name,
-                                sizeof(l_slave_target_name));
-            }
 
-            if (l_linktrain_failures)
+                if (l_numberOfTrainFailures)
+                {
+                    TRACFCOMP(g_trac_isteps_trace,
+                        "%d attempts to linktrain XBUS targets failed. "
+                        "Master: %s, Slave: %s", l_numberOfTrainFailures,
+                        l_master_target_name, l_slave_target_name);
+                }
+                else
+                {
+                    TRACFCOMP(g_trac_isteps_trace,
+                        "Successfully linktrained XBUS targets. "
+                        "Master: %s, Slave: %s",
+                        l_master_target_name, l_slave_target_name);
+                }
+            }  // end for (const auto & l_pbusConnection: l_pbusConnections)
+        }  // end if (TYPE_XBUS == l_busSet[ii])
+#ifdef CONFIG_SMP_WRAP_TEST
+        else if (TYPE_OBUS == l_busSet[ii])
+        {
+            // Make the FAPI call to p9_io_obus_linktrain
+            if (!trainBusHandler(l_busSet[ii],
+                                 P9_IO_OBUS_LINKTRAIN,
+                                 l_stepError,
+                                 HWPF_COMP_ID,
+                                 l_pbusConnections))
             {
-                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                    "%d attempts to linktrain XBUS targets failed. "
-                    "Master: %s, Slave: %s", l_linktrain_failures,
-                    l_master_target_name, l_slave_target_name);
+                break;
             }
-            else
-            {
-                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                    "Successfully linktrained XBUS targets. "
-                    "Master: %s, Slave: %s",
-                    l_master_target_name, l_slave_target_name);
-            }
-        }
-    } // end for MaxBusSet
+        }  // end else if (TYPE_OBUS == l_busSet[ii])
+#endif
+    } // end for (uint32_t ii = 0; ii < l_maxBusSet; ++ii)
 
-    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-               "call_fabric_io_run_training exit" );
+    TRACFCOMP(g_trac_isteps_trace, EXIT_MRK"call_fabric_io_run_training exit" );
 
     // end task, returning any errorlogs to IStepDisp
-    return l_StepError.getErrorHandle();
+    return l_stepError.getErrorHandle();
 }
-
-
-
 
 /**
  *  @brief Try to train link between target's endpoints
@@ -208,7 +202,7 @@ uint8_t run_linktraining(
                 ISTEP_ERROR::IStepError & o_step_error)
 {
     uint8_t o_failures = 0;
-    errlHndl_t l_errl = NULL;
+    errlHndl_t l_err(nullptr);
 
     // group is either 0 or 1
     std::vector<uint8_t> l_groups = {0,1};
@@ -216,7 +210,7 @@ uint8_t run_linktraining(
     for (auto l_group : l_groups)
     {
 
-        TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+        TRACFCOMP(g_trac_isteps_trace,
                  "Running p9_io_xbus_linktrain HWP on "
                  "master target %.8X and "
                  "slave target %.8X on group %d.",
@@ -224,39 +218,36 @@ uint8_t run_linktraining(
                  TARGETING::get_huid(i_slave_target),
                  l_group );
 
-        FAPI_INVOKE_HWP(l_errl,
+        FAPI_INVOKE_HWP(l_err,
                         p9_io_xbus_linktrain,
                         i_master_target,
                         i_slave_target,
                         l_group);
 
-        if (l_errl)
+        if (l_err)
         {
             o_failures++;
-            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+            TRACFCOMP(g_trac_isteps_trace,
                  "Failure #%d) ERROR 0x%.8X : p9_io_xbus_linktrain "
                  "HWP on master target %.8X and "
                  "slave target %.8X on group %d.",
-                 o_failures, l_errl->reasonCode(),
+                 o_failures, l_err->reasonCode(),
                  TARGETING::get_huid(i_master_target),
                  TARGETING::get_huid(i_slave_target),
                  l_group );
 
-            // capture the target data in the elog
-            ErrlUserDetailsTarget(i_master_target).addToLog( l_errl );
-            ErrlUserDetailsTarget(i_slave_target).addToLog( l_errl );
+            // Create a target list out of the master and slave target
+            TargetHandleList l_targetList =
+                  { i_master_target.get(),
+                    i_slave_target.get() };
 
-            // Create IStep error log and cross ref error that occurred
-            o_step_error.addErrorDetails( l_errl);
-
-            // Commit Error
-            errlCommit(l_errl, HWPF_COMP_ID);
-            l_errl = NULL;
+            // Capture error and continue
+            captureError(l_err,
+                         o_step_error,
+                         HWPF_COMP_ID,
+                         l_targetList);
         }
     }
     return o_failures;
 }
-
-
-
-};
+};   // end namespace   ISTEP_09
