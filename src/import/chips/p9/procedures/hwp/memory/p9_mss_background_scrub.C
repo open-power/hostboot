7b8e60462b27343ed743e947f35abcac52a4ec58
Matthew Hickman <Matthew.Hickman@ibm.com>
2018-04-10 15:58:46 -0500

    Added p9_mss_memdiag for cronus ipl and modified scrub for step 16
    Change-Id: I2fb279f10c75d6fd3cab66d54a7bdac1382a6b9e
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/44428
    Dev-Ready: Brent Wieman <bwieman@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: ANDRE A. MARIN <aamarin@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/44433
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/p9_mss_background_scrub.C
+++ src/import/chips/p9/procedures/hwp/memory/p9_mss_background_scrub.C
@@ -22,3 +22,69 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file p9_mss_background_scrub.C
+/// @brief Begin background scrub
+///
+// *HWP HWP Owner: Matthew Hickman <Matthew.Hickman@ibm.com>
+// *HWP HWP Backup: Andre Marin <aamarin@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 2
+// *HWP Consumed by: FSP:HB
+
+#include <fapi2.H>
+#include <p9_mss_background_scrub.H>
+
+#include <lib/dimm/rank.H>
+#include <lib/mcbist/address.H>
+#include <lib/mcbist/mcbist.H>
+#include <lib/mcbist/patterns.H>
+#include <lib/mcbist/memdiags.H>
+#include <lib/mcbist/sim.H>
+#include <generic/memory/lib/utils/count_dimm.H>
+#include <lib/fir/memdiags_fir.H>
+
+using fapi2::TARGET_TYPE_MCBIST;
+using fapi2::TARGET_TYPE_MCA;
+using fapi2::TARGET_TYPE_SYSTEM;
+using fapi2::FAPI2_RC_SUCCESS;
+
+///
+/// @brief Begin background scrub
+/// @param[in] i_target MCBIST
+/// @return FAPI2_RC_SUCCESS iff ok
+///
+fapi2::ReturnCode p9_mss_background_scrub( const fapi2::Target<TARGET_TYPE_MCBIST>& i_target )
+{
+    FAPI_INF("Start mss background scrub on: %s", mss::c_str( i_target ));
+
+    // If there are no DIMM we don't need to bother. In fact, we can't as we didn't setup
+    // attributes for the PHY, etc.
+    if (mss::count_dimm(i_target) == 0)
+    {
+        FAPI_INF("... skipping background scrub for %s - no DIMM ...", mss::c_str(i_target));
+        return fapi2::FAPI2_RC_SUCCESS;
+    }
+
+    // If we're running in the simulator, we want to only touch the addresses which training touched
+    uint8_t l_sim = 0;
+    FAPI_TRY( mss::is_simulation(l_sim) );
+
+    // Kick off background scrub if we are not running in sim
+    if (!(l_sim))
+    {
+        // Start background scrub
+        FAPI_TRY ( mss::memdiags::background_scrub( i_target,
+                   mss::mcbist::stop_conditions(),
+                   mss::mcbist::speed::BG_SCRUB,
+                   mss::mcbist::address() ) );
+
+    }
+
+    // Unmask firs after background scrub is started
+    FAPI_TRY ( mss::unmask::after_background_scrub( i_target ) );
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
