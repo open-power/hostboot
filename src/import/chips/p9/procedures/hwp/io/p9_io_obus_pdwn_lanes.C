c61808ab22708b371ac8884c014a0eeddd4bf7de
Chris Steffen <cwsteffen@us.ibm.com>
2018-05-15 09:19:28 -0500

    P9C Abus Procedure
    - Power Down Lanes for Erepair
    - Update for Dccal
    
    Change-Id: I902542d03b4527c8d9ef78c41d5aaf46bca783e6
    Original-Change-Id: Ie3a526d0165373a9f946c8db2b07c7006dcbec50
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/58836
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: John G. Rell III <jgrell@us.ibm.com>
    Reviewed-by: Richard J. Knight <rjknight@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/chips/p9/procedures/hwp/io/p9_io_obus_pdwn_lanes.C
@@ -0,0 +1,94 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p9/procedures/hwp/io/p9_io_obus_pdwn_lanes.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p9_io_obus_pdwn_lanes.C
+/// @brief Train the Link.
+///-----------------------------------------------------------------------------
+/// *HWP HWP Owner        : Chris Steffen <cwsteffen@us.ibm.com>
+/// *HWP HWP Backup Owner : Gary Peterson <garyp@us.ibm.com>
+/// *HWP FW Owner         : Jamie Knight <rjknight@us.ibm.com>
+/// *HWP Team             : IO
+/// *HWP Level            : 3
+/// *HWP Consumed by      : FSP:HB
+///-----------------------------------------------------------------------------
+///
+/// @verbatim
+/// High-level procedure flow:
+///
+/// Run Dccal
+///
+/// Procedure Prereq:
+///     - System clocks are running.
+///
+/// @endverbatim
+///----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//  Includes
+//-----------------------------------------------------------------------------
+#include <p9_io_obus_pdwn_lanes.H>
+#include <p9_io_scom.H>
+#include <p9_io_regs.H>
+
+//-----------------------------------------------------------------------------
+// Function Declarations
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+//  Function Definitions
+//-----------------------------------------------------------------------------
+
+
+/**
+ * @brief A I/O Obus Procedure to power down lanes
+ * on every instance of the OBUS.
+ * @param[in] i_tgt         FAPI2 Target
+ * @param[in] i_lane_vector Lanve Vector
+ * @retval ReturnCode
+ */
+fapi2::ReturnCode p9_io_pdwn_lanes(const OBUS_TGT i_tgt, const uint32_t i_lane_vector)
+{
+    FAPI_IMP("p9_io_pdwn_lanes: I/O Obus Entering");
+    const uint8_t GRP0  = 0;
+    const uint8_t LANES = 24;
+    char l_tgtStr[fapi2::MAX_ECMD_STRING_LEN];
+    fapi2::toString(i_tgt, l_tgtStr, fapi2::MAX_ECMD_STRING_LEN);
+    FAPI_DBG("I/O Obus Pdwn Lanes %s, Lane Vector(0x%X)", l_tgtStr, i_lane_vector);
+
+    // Power down Per-Lane Registers
+    for(uint8_t lane = 0; lane < LANES; ++lane)
+    {
+        if(((0x1 << lane) & i_lane_vector) != 0)
+        {
+            FAPI_TRY(io::rmw(OPT_RX_LANE_ANA_PDWN, i_tgt, GRP0, lane, 0x01));
+            FAPI_TRY(io::rmw(OPT_RX_LANE_DIG_PDWN, i_tgt, GRP0, lane, 0x01));
+            FAPI_TRY(io::rmw(OPT_TX_LANE_PDWN    , i_tgt, GRP0, lane, 0x01));
+        }
+    }
+
+fapi_try_exit:
+    FAPI_IMP("p9_io_pdwn_lanes: I/O Obus Exiting");
+    return fapi2::current_err;
+}
