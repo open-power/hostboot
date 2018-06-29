2b3766ca5e27818697607d76360289e349940f8c
Jennifer A. Stofer <stofer@us.ibm.com>
2018-06-29 15:08:49 -0400

    Revert "Cen Too Many Bus Errors"
    This reverts commit 62357b8b2fc9c8f394d93031865b9786e4e6cb9e.
    
        -Copyright dates were updated to 2018 to bypass ci fialure
    
    Change-Id: I1d5ff47032b22673c073fecd72e07926ae480424
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/61683
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Nicholas E. Bofferding <bofferdn@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/61696
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/import/chips/centaur/procedures/hwp/io/p9_io_cen_scominit.C
+++ src/import/chips/centaur/procedures/hwp/io/p9_io_cen_scominit.C
@@ -51,11 +51,6 @@
 #include <p9_io_cen_scominit.H>
 #include <centaur_dmi_scom.H>
 
-#include <cen_gen_scom_addresses.H>
-#include <cen_gen_scom_addresses_fixes.H>
-#include <cen_gen_scom_addresses_fld.H>
-
-
 //------------------------------------------------------------------------------
 //  Constant definitions
 //------------------------------------------------------------------------------
@@ -83,57 +78,6 @@ fapi2::ReturnCode p9_io_cen_scominit(const CEN_TGT& i_tgt)
         FAPI_ERR("P9 I/O Cen Scominit Failed");
         fapi2::current_err = rc;
     }
-
-
-
-    // Centaur DMI FIR setup
-    {
-        fapi2::buffer<uint64_t> l_cen_fir_mask;
-        fapi2::buffer<uint64_t> l_cen_fir_action0;
-        fapi2::buffer<uint64_t> l_cen_fir_action1; // Recoverable
-        l_cen_fir_mask.flush<1>();
-        l_cen_fir_action0.flush<0>();
-        l_cen_fir_action1.flush<0>();
-
-        // 0    Rx Invalid State or Parity Error    mask
-        // 1    Tx Invalid State or Parity Error    mask
-        // 2    GCR Hange Error                     recoverable
-        l_cen_fir_mask.clearBit<2>();
-        l_cen_fir_action1.setBit<2>();
-        // 3:7  Reserved                            mask
-        // 8    Training Error                      mask
-        // 9    Spare Lane Deployed                 recoverable
-        l_cen_fir_mask.clearBit<9>();
-        l_cen_fir_action1.setBit<9>();
-        // 10   Max Spares Exceeded                 recoverable
-        l_cen_fir_mask.clearBit<10>();
-        l_cen_fir_action1.setBit<10>();
-        // 11   Recal or Dynamic Repair Error       recoverable
-        l_cen_fir_mask.clearBit<11>();
-        l_cen_fir_action1.setBit<11>();
-        // 12   Too Many Bus Errors                 unit_cs
-        l_cen_fir_mask.clearBit<12>();
-        // 13:15 Reserved                           mask
-        // 16:23 Bus 1 - Unused                     mask
-        // 24:31 Bus 2 - Unused                     mask
-        // 32:39 Bus 3 - Unused                     mask
-        // 40:47 Bus 4 - Unused                     mask
-        // 48    Scom Error                         recoverable
-        l_cen_fir_mask.clearBit<48>();
-        l_cen_fir_action1.setBit<48>();
-        // 49    Scom Error                         recoverable
-        l_cen_fir_mask.clearBit<49>();
-        l_cen_fir_action1.setBit<49>();
-        // 50:63 Unused                             mask
-
-
-        FAPI_TRY(fapi2::putScom(i_tgt, CEN_FIR_ACTION0_REG, l_cen_fir_action0));
-        FAPI_TRY(fapi2::putScom(i_tgt, CEN_FIR_ACTION1_REG, l_cen_fir_action1));
-        FAPI_TRY(fapi2::putScom(i_tgt, CEN_FIR_MASK_REG, l_cen_fir_mask));
-    }
-
-
-fapi_try_exit:
 
     // mark HWP exit
     FAPI_INF("p9_io_cen_scominit: ...Exiting");
