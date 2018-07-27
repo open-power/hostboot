4dfcdd4a9e59308aa24a94673d5d16341b383dea
Prachi Gupta <pragupta@us.ibm.com>
2018-07-27 16:36:57 -0500

    p9_fab_iovalid: process errors returned by this HWP
    When partial links fail to train, this procedure will
    return a vector of RCs. This commit adds in code to
    process those errors and add appropriate BUS callouts
    
    CQ:SW439810
    Change-Id: Ie3465bbac2736dbcc20c9cf9a71e7c91ee618335
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/63506
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Reviewed-by: ILYA SMIRNOV <ismirno@us.ibm.com>
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
    Reviewed-by: Nicholas E. Bofferding <bofferdn@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep09/call_proc_fab_iovalid.C
+++ src/usr/isteps/istep09/call_proc_fab_iovalid.C
@@ -61,6 +61,7 @@
 #include <fapi2/target.H>
 #include <fapi2/plat_hwp_invoker.H>
 #include <errl/errlmanager.H>
+#include <errl/errlentry.H>
 
 // HWP
 #include <p9_fab_iovalid.H>
@@ -77,6 +78,74 @@ using   namespace   ISTEP_ERROR;
 using   namespace   ERRORLOG;
 using   namespace   TARGETING;
 using   namespace   HWAS;
+
+typedef std::map<ATTR_HUID_type, ATTR_LINK_TRAIN_type > OBUSHuidAttrMap_t;
+
+void fillAttrMap (OBUSHuidAttrMap_t& o_map)
+{
+    TARGETING::TargetHandleList l_obuses;
+    getAllChiplets(l_obuses, TYPE_OBUS);
+    for (const auto& l_obus : l_obuses)
+    {
+        o_map[get_huid(l_obus)] = l_obus->getAttr<ATTR_LINK_TRAIN>();
+    }
+}
+
+void calloutOBUS(TargetHandle_t i_proc,
+                 OBUSHuidAttrMap_t& i_attrMapBeforeHWP,
+                 errlHndl_t& io_errl)
+{
+    TargetHandleList l_obuses;
+    getChildChiplets(l_obuses, i_proc, TYPE_OBUS);
+    for (const auto& l_obus : l_obuses)
+    {
+        auto l_trainVal = l_obus->getAttr<ATTR_LINK_TRAIN>();
+        auto l_trainValBefore = -1;
+
+        auto l_itr = i_attrMapBeforeHWP.find(get_huid(l_obus));
+        if (l_itr != i_attrMapBeforeHWP.end())
+        {
+            l_trainValBefore = l_itr->second;
+        }
+
+        TRACDCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                "calloutOBUS: HUID=0x%x BeforeTrainVal=0x%x CurrTrainVal=0x%x",
+                get_huid(l_obus), l_trainValBefore, l_trainVal);
+
+        if (l_trainVal == l_trainValBefore)
+        {
+            //This OBUS link is fine. Value is as exepected after running HWP
+            continue;
+        }
+
+        TargetHandleList l_smpGroupTargets;
+        getChildAffinityTargets(l_smpGroupTargets,
+                                l_obus,
+                                CLASS_UNIT,
+                                TYPE_SMPGROUP,
+                                false);
+        for (const auto& l_smpGrp : l_smpGroupTargets)
+        {
+            auto l_smpGrpPos = l_smpGrp->getAttr<ATTR_REL_POS>();
+            //if smpGrpPos%2==0, then ODD links are functional, callout even
+            //if smpGrpPos%2==1, then even are functional, callout odd
+
+            //In the rc vector we get back, we only get training errors for
+            //odd or even links or none of links train error. However, if
+            //none of the links were trained, then the OBUS target is already
+            //deconfigured. Therefore, we will never get here if LINK_TRAIN_NONE
+            if (((l_smpGrpPos % 2 == 0) && (l_trainVal == LINK_TRAIN_ODD_ONLY))
+            || ((l_smpGrpPos % 2 == 1) && (l_trainVal == LINK_TRAIN_EVEN_ONLY)))
+            {
+                auto l_smp     = l_smpGrp->getAttr<ATTR_PHYS_PATH>();
+                auto l_peerSMP = l_smpGrp->getAttr<ATTR_PEER_PATH>();
+                io_errl->addBusCallout (l_smp, l_peerSMP, HWAS::O_BUS_TYPE,
+                        SRCI_PRIORITY_MED);
+            }
+        }
+    }
+
+}
 //
 //  Wrapper function to call proc_fab_iovalid
 //
@@ -114,6 +183,10 @@ void*    call_proc_fab_iovalid( void    *io_pArgs )
     TARGETING::TargetHandleList l_cpuTargetList;
     getAllChips(l_cpuTargetList, TYPE_PROC);
 
+    //Save off the state of ATTR_LINK_TRAIN
+    OBUSHuidAttrMap_t l_attrMapBeforeHWP;
+    fillAttrMap(l_attrMapBeforeHWP);
+
     // Loop through all processors including master
     for (const auto & l_cpu_target: l_cpuTargetList)
     {
@@ -128,6 +201,51 @@ void*    call_proc_fab_iovalid( void    *io_pArgs )
         {
             FAPI_INVOKE_HWP(l_errl, p9_fab_iovalid, l_fapi2_proc_target,
                         true, true, true, l_fapiRcs);
+
+            if ((l_errl == nullptr) && (l_fapiRcs.size()))
+            {
+                //if HWP did not return an error, then create a new
+                //error to link all the RCs with
+                /*@
+                 * @errortype  ERRL_SEV_UNRECOVERABLE
+                 * @moduleid   MOD_SMP_WRAP_PROC_IOVALID
+                 * @reasoncode RC_LINK_TRAIN_ERRORS_FROM_HWP
+                 * @userdata0  PROC HUID
+                 * @userdata1  Number of RCs from HWP
+                 * @devdesc    p9_fab_iovalid HWP returned errors on odd and
+                 *             even links. Creating an error to link all the
+                 *             individual link RCs together
+                 * @custdesc   There was an error training the SMP cables.
+                 */
+                l_errl = new ERRORLOG::ErrlEntry(
+                        ERRORLOG::ERRL_SEV_UNRECOVERABLE,
+                        MOD_SMP_WRAP_PROC_IOVALID,
+                        RC_LINK_TRAIN_ERRORS_FROM_HWP,
+                        get_huid(l_cpu_target),
+                        l_fapiRcs.size());
+            }
+
+            //Something bad happened during the hwp run
+            for (auto& l_rc : l_fapiRcs)
+            {
+                errlHndl_t l_tempErr = rcToErrl(l_rc);
+                if (l_tempErr)
+                {
+                    l_tempErr->plid(l_errl->plid());
+
+                    // capture the target data in the elog
+                    ErrlUserDetailsTarget(l_cpu_target).addToLog
+                        (l_tempErr);
+
+                    l_StepError.addErrorDetails(l_tempErr);
+
+                    //callout any buses we need to.
+                    calloutOBUS(l_cpu_target, l_attrMapBeforeHWP, l_tempErr);
+
+                    errlCommit(l_tempErr, HWPF_COMP_ID);
+                    l_tempErr = nullptr;
+                }
+            }
         }
         else
         {
