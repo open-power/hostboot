e112cf5c675dbbdcedb2269a45fc3d505a982e38
Stephen Glancy <sglancy@us.ibm.com>
2018-07-27 14:49:59 -0500

    Adds Explorer OMI setup - step 12.8a
    Change-Id: I4e448a7ecc04b6f169b1688c7e8ffde2451f5f9e
    Original-Change-Id: Ic3a15e25faf465c4beb2b722c79c12d4a79ae33b
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/63492
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: ANDRE A. MARIN <aamarin@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/71349
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>

--- src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_omi_setup.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_omi_setup.C
@@ -22,3 +22,57 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file exp_omi_setup.H
+/// @brief Contains the explorer OMI setup
+///
+// *HWP HWP Owner: Andre Marin <aamarin@us.ibm.com>
+// *HWP HWP Backup: Stephen Glancy <sglancy@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 3
+// *HWP Consumed by: Memory
+
+#include <fapi2.H>
+#include <generic/memory/lib/utils/c_str.H>
+#include <lib/exp_attribute_accessors_manual.H>
+#include <lib/omi/exp_omi_utils.H>
+
+extern "C"
+{
+
+    ///
+    /// @brief Setup the OCMB for enterprise and half-DIMM modes as desired
+    /// @param[in] i_target the OCMB target to operate on
+    /// @return FAPI2_RC_SUCCESS iff ok
+    ///
+    fapi2::ReturnCode exp_omi_setup( const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
+    {
+        // Declares variables
+        fapi2::buffer<uint64_t> l_data;
+        bool l_is_enterprise = false;
+        bool l_is_half_dimm = false;
+
+        // Gets the configuration information
+        FAPI_TRY(mss::enterprise_mode(i_target, l_is_enterprise));
+        FAPI_TRY(mss::half_dimm_mode(i_target, l_is_half_dimm));
+
+        // Prints out the data
+        FAPI_INF("%s %s enterprise mode %s-DIMM mode", mss::c_str(i_target), l_is_enterprise ? "is" : "isn't",
+                 l_is_half_dimm ? "half" : "full");
+
+        // Sets up the register
+        mss::exp::omi::set_enterprise_set_bit(l_data, l_is_enterprise);
+        mss::exp::omi::set_half_dimm_mode(l_data, l_is_half_dimm);
+
+        // Writes the data to the register
+        FAPI_TRY(mss::exp::omi::write_enterprise_config(i_target, l_data));
+
+        // Checks that the chip is configured correctly
+        FAPI_TRY(mss::exp::omi::read_enterprise_config(i_target, l_data));
+        FAPI_TRY(mss::exp::omi::check_enterprise_mode(i_target, l_is_enterprise, l_data));
+
+    fapi_try_exit:
+        return fapi2::current_err;
+    }
+}
