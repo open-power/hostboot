b2cf0aa44b39ba6ff6f09b2ebc3ec3942b020f24
Christian Geddes <crgeddes@us.ibm.com>
2018-07-31 10:47:34 -0500

    Make processing of hrmor value in MemStateInfo consistent
    There was some confusion on whether the value in core scratch 1
    that told us where Host HRMOR lives was in MB or just Bytes. Going
    forward this value will be in bytes.
    
    CQ: SW440425
    Change-Id: I5e7e5efdd584c3a8c050091b32d5443dff3cd417
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/63627
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Nicholas E. Bofferding <bofferdn@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/build/debug/ecmd-debug-framework.pl
+++ src/build/debug/ecmd-debug-framework.pl
@@ -178,15 +178,11 @@ sub getHRMOR
     {
         # read Core Scratch 1 (multicast to all cores)
         my $scratch1 = readScom( 0x41010A87, 8 );
-        # Bits 4:51 are the current HRMOR in MB (see memstate.H)
+        # Bits 4:51 are the current HRMOR in Bytes (see memstate.H)
         my $hrmor = ($scratch1 & 0x0FFFFFFFFFFFF000)>>12;
         if( $hrmor == 0 )
         {
             $hrmor = DEFAULT_HRMOR + ($node * PER_NODE_OFFSET);
-        }
-        else
-        {
-            $hrmor = $hrmor*1024; #value is in MB
         }
 
         return $hrmor;
