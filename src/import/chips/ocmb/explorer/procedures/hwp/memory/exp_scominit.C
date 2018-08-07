3cc33bb24572d0e14e4f6846cc796d24b78f34c7
Ben Gass <bgass@us.ibm.com>
2018-08-07 14:59:09 -0500

    Adds initfile for Explorer
    Change-Id: I8dc18bbe6850199de4a0fee2bc88e275ae484a82
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/64063
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Reviewed-by: Benjamin Gass <bgass@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_scominit.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_scominit.C
@@ -22,3 +22,61 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file exp_scominit.C
+/// @brief Contains explorer scominits
+///
+// *HWP HWP Owner: Andre Marin <aamarin@us.ibm.com>
+// *HWP HWP Backup: Stephen Glancy <sglancy@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 3
+// *HWP Consumed by: Memory
+
+#include <fapi2.H>
+#include <generic/memory/lib/utils/c_str.H>
+#include <generic/memory/lib/utils/count_dimm.H>
+#include <generic/memory/lib/utils/find.H>
+#include <explorer_scom.H>
+
+extern "C"
+{
+
+    ///
+    /// @brief Scominit for Explorer
+    /// @param[in] i_target the OCMB target to operate on
+    /// @return FAPI2_RC_SUCCESS iff ok
+    ///
+    fapi2::ReturnCode exp_scominit( const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
+    {
+        if (mss::count_dimm(i_target) == 0)
+        {
+            FAPI_INF("... skipping mss_scominit %s - no DIMM ...", mss::c_str(i_target));
+            return fapi2::FAPI2_RC_SUCCESS;
+        }
+
+        // We need to make sure we hit all ports
+        const auto& l_port_targets = mss::find_targets<fapi2::TARGET_TYPE_MEM_PORT>(i_target);
+
+        fapi2::Target<fapi2::TARGET_TYPE_SYSTEM> FAPI_SYSTEM;
+        const auto& l_mc = i_target.getParent<fapi2::TARGET_TYPE_OMI>()
+                           .getParent<fapi2::TARGET_TYPE_MCC>()
+                           .getParent<fapi2::TARGET_TYPE_MI>()
+                           .getParent<fapi2::TARGET_TYPE_MC>();
+
+        for(const auto& l_port : l_port_targets)
+        {
+            fapi2::ReturnCode l_rc;
+            FAPI_INF("phy scominit for %s", mss::c_str(l_port));
+            FAPI_EXEC_HWP(l_rc, explorer_scom, i_target, l_port, FAPI_SYSTEM, l_mc);
+
+            FAPI_TRY(l_rc, "Error from explorer.scom.initfile %s", mss::c_str(l_port));
+        }
+
+        return fapi2::FAPI2_RC_SUCCESS;
+
+    fapi_try_exit:
+        FAPI_INF("End MSS SCOM init");
+        return fapi2::current_err;
+    }
+}
