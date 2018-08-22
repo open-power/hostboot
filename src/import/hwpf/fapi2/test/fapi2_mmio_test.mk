88f8e3bb26560e52e56f0481d5f9fc86965d6e42
Dan Crowell <dcrowell@us.ibm.com>
2018-08-22 17:37:38 -0500

    New FAPI2 interfaces to read and write MMIO ranges
    Adding 2 new fapi2 interfaces for platforms to implement:
    - getMMIO
    - putMMIO
    
    These functions will read/write a data buffer into a known
    memory-mapped i/o space based on an address that is relative
    to the input target.
    
    Change-Id: I6ff0b19d179cb8c8a8ef52a807c84137d66e2ece
    Original-Change-Id: Id24888e355cccfd462a57868eabc2af7cebd4ed4
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/65037
    Reviewed-by: Benjamin Gass <bgass@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Matt K. Light <mklight@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: ANDRE A. MARIN <aamarin@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/hwpf/fapi2/test/fapi2_mmio_test.mk
@@ -0,0 +1,29 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/import/hwpf/fapi2/test/fapi2_mmio_test.mk $
+#
+# OpenPOWER HostBoot Project
+#
+# Contributors Listed Below - COPYRIGHT 2018,2019
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+FAPI2_TEST_INCLUDES := $(GENPATH)
+FAPI2_TEST_INCLUDES+=$(ROOTPATH)/hwpf/fapi2/include/plat/
+WRAPPER=fapi2_mmio_test
+$(call ADD_EXE_INCDIR,$(WRAPPER),$(FAPI2_TEST_INCLUDES))
+$(call BUILD_WRAPPER)
