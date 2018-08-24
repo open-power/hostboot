4671a510b5b77cf54fe369e46efd569574f71f65
Stephen Glancy <sglancy@us.ibm.com>
2018-08-24 13:51:23 -0500

    Moves CAS latency algorithm to generic folder
    Change-Id: I9cc3c262fc6578928e0bae4f9d362eb5e86055c3
    Original-Change-Id: Ie2a7e7f7b1f1e0f78716d458531715016a539ec0
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/65187
    Dev-Ready: STEPHEN GLANCY <sglancy@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: ANDRE A. MARIN <aamarin@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ src/import/generic/memory/lib/utils/freq/cas_latency.C
@@ -0,0 +1,64 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/generic/memory/lib/utils/freq/cas_latency.C $      */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file cas_latency.C
+/// @brief CAS latency class implementation
+///
+// *HWP HWP Owner: Andre Marin <aamarin@us.ibm.com>
+// *HWP HWP Backup: Stephen Glancy <sglancy@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 3
+// *HWP Consumed by: HB:FSP
+
+// std lib
+#include <limits.h>
+#include <algorithm>
+
+// fapi2
+#include <fapi2.H>
+
+// mss lib
+#include <generic/memory/lib/utils/freq/cas_latency.H>
+
+namespace mss
+{
+
+const std::vector< uint32_t > CasLatencyTraits<mc_type::NIMBUS>::SUPPORTED_FREQS =
+{
+    DIMM_SPEED_1866,
+    DIMM_SPEED_2133,
+    DIMM_SPEED_2400,
+    DIMM_SPEED_2666,
+};
+
+const std::vector< uint32_t > CasLatencyTraits<mc_type::EXPLORER>::SUPPORTED_FREQS =
+{
+    DIMM_SPEED_2666,
+    DIMM_SPEED_2933,
+    DIMM_SPEED_3200,
+};
+
+}// mss
