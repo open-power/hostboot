ada2c16282bb05e67d277417096f76ebc462a7f6
Zane Shelley <zshelle@us.ibm.com>
2018-08-24 14:02:03 -0500

    PRD: RX trgt used for TX trgt in XBUS lane repair VPD write
    Change-Id: I5eace3a79d9fd8fe8e30ddc12276813dd61b4205
    CQ: SW443246
    Backport: release-fips921
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/65188
    Reviewed-by: Caleb N. Palmer <cnpalmer@us.ibm.com>
    Reviewed-by: Nicholas E. Bofferding <bofferdn@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Zane C. Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/65200
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/65230
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>

--- src/usr/diag/prdf/common/plat/prdfPlatServices_common.C
+++ src/usr/diag/prdf/common/plat/prdfPlatServices_common.C
@@ -230,7 +230,7 @@ int32_t setVpdFailedLanesXbus(TargetHandle_t i_rxBusTgt,
     errlHndl_t err = nullptr;
 
     fapi2::Target<fapi2::TARGET_TYPE_XBUS> fapiRxTrgt (i_rxBusTgt);
-    fapi2::Target<fapi2::TARGET_TYPE_XBUS> fapiTxTrgt (i_rxBusTgt);
+    fapi2::Target<fapi2::TARGET_TYPE_XBUS> fapiTxTrgt (i_txBusTgt);
 
     FAPI_INVOKE_HWP(err,
                     erepairSetFailedLanes,
