open-power/hostboot

commit 529bf938bf0ee224f01ed8a2c28fe74729042fd1

Use Attr to Save/Fetch MDRT Count
MDRT count is saved out into an attribute in istep14 and the same
is fetched out in istep21 for Phyp as well as Opal(ZZ-L) system.

Added libruntime.so to istep14, Since Dump relies upon runtime module

Moved 'useRelocatedPayloadAddr' setting to call_host_mpipl_service(),
So that we have everything set before calling doDumpCollect()

Change-Id: I5d30135d86a1477a68509aaff0516efb4d620410
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/58636
Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

src/usr/isteps/istep21/call_host_runtime_setup.C
@@ -785,6 +785,18 @@ void* call_host_runtime_setup (void *io_pArgs)
                 break;
             }
         }
+        
+        // Update the MDRT Count from Attribute
+        TargetService& l_targetService = targetService();
+        Target* l_sys = nullptr;
+        l_targetService.getTopLevelTarget(l_sys);
+        if(l_sys->getAttr<ATTR_IS_MPIPL_HB>())
+        {
+            uint32_t l_mdrtCount =
+                l_sys->getAttr<TARGETING::ATTR_MPIPL_HB_MDRT_COUNT>();
+            //Update actual count in RUNTIME
+            RUNTIME::saveActualCount(RUNTIME::MS_DUMP_RESULTS_TBL, l_mdrtCount);
+        }
 
         //Update the MDRT value (for MS Dump)
         l_err = RUNTIME::writeActualCount(RUNTIME::MS_DUMP_RESULTS_TBL);

src/include/usr/isteps/istep14list.H
@@ -197,6 +197,7 @@ const DepModInfo g_istep14Dependancies = {
     {
         DEP_LIB(libistep14.so),
         DEP_LIB(libdump.so),
+        DEP_LIB(libruntime.so),
         DEP_LIB(libisteps_mss.so),
         DEP_LIB(libpm.so),
         DEP_LIB(libimageprocs.so),

src/usr/dump/dumpCollect.C
@@ -59,10 +59,6 @@ namespace DUMP
 errlHndl_t doDumpCollect(void)
 {
     TRACFCOMP(g_trac_dump, "doDumpCollect - start ");
-
-    // Use relocated payload base to get MDST, MDDT, MDRT details
-    RUNTIME::useRelocatedPayloadAddr(true);
-
     errlHndl_t l_err = NULL;
 
     // Table Sizes
@@ -102,8 +98,6 @@ errlHndl_t doDumpCollect(void)
         }
 
     }while (0);
-
-    RUNTIME::useRelocatedPayloadAddr(false);
 
     return (l_err);
 }
@@ -608,10 +602,15 @@ errlHndl_t copySrcToDest(dumpEntry *srcTableEntry,
             break;
         }
 
+        // Update the MDRT Count to Attribute to be fetched in istep 21
+        TARGETING::TargetService& l_targetService = TARGETING::targetService();
+        TARGETING::Target* l_sys = NULL;
+        l_targetService.getTopLevelTarget(l_sys);
+        l_sys->setAttr<TARGETING::ATTR_MPIPL_HB_MDRT_COUNT>(l_resultCount);
+
         //Update actual count in RUNTIME
         RUNTIME::saveActualCount(RUNTIME::MS_DUMP_RESULTS_TBL,
                                  l_resultCount);
-
 
         //Write actual count into memory as well
         // We know this will get whacked when FSP reloads the PHYP

src/usr/isteps/istep14/call_host_mpipl_service.C
@@ -35,6 +35,8 @@
 #include <targeting/common/utilFilter.H>
 #include <targeting/targplatutil.H>
 
+#include <runtime/runtime.H>
+
 #include <p9_mpipl_chip_cleanup.H>
 #include <fapi2/plat_hwp_invoker.H>
 
@@ -150,99 +152,80 @@ void* call_host_mpipl_service (void *io_pArgs)
 
             errlHndl_t l_errMsg = NULL;
 
-            // Dump relies upon the runtime module
-            // Not declaring in istep DEP list cause if we load it
-            // we want it to stay loaded
-            if (  !VFS::module_is_loaded( "libruntime.so" ) )
+            // Use relocated payload base to get MDST, MDDT, MDRT details
+            RUNTIME::useRelocatedPayloadAddr(true);
+
+            do
             {
-                l_err = VFS::module_load( "libruntime.so" );
+                // send the start message
+                l_errMsg = DUMP::sendMboxMsg(DUMP::DUMP_MSG_START_MSG_TYPE);
 
-                if ( l_err )
-                {
-                    //  load module returned with errl set
-                    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                               "Could not load runtime module" );
-                }
-            }
-
-            // If dump module successfully loaded then continue with DumpCollect
-            // and messaging
-            if (!l_err)
-            {
-                do
-                {
-                    // send the start message
-                    l_errMsg = DUMP::sendMboxMsg(DUMP::DUMP_MSG_START_MSG_TYPE);
-
-                    // If error, commit and send error message.
-                    if (l_errMsg)
-                    {
-                        TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-                       "ERROR : returned from DUMP::sendMboxMsg - dump start" );
-
-                        errlCommit( l_errMsg, HWPF_COMP_ID );
-
-                        // don't break in this case because we not want to fail
-                        // the istep on the dump collect so we will continue
-                        // after we log the errhandle that we can't send a
-                        // message.
-                    }
-
-                    // Call the dump collect
-                    l_err = DUMP::doDumpCollect();
-
-                    // Got a Dump Collect error.. Commit the dumpCollect
-                    // errorlog and then send an dump Error mbox message
-                    // and FSP will decide what to do.
-                    // We do not want dump Collect failures to terminate the
-                    // istep.
-                    if (l_err)
-                    {
-                        TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-                             "ERROR : returned from DUMP::HbDumpCopySrcToDest");
-
-                        break;
-                    }
-
-                } while(0);
-
-                DUMP::DUMP_MSG_TYPE msgType = DUMP::DUMP_MSG_END_MSG_TYPE;
-
-                // Send dumpCollect success trace
-                if (!l_err)
-                {
-                    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                               "SUCCESS : doDumpCollect" );
-                }
-                // got an error that we need to send a ERROR message to FSP
-                // and commit the errorlog from dumpCollect.
-                else
-                {
-                    msgType = DUMP::DUMP_MSG_ERROR_MSG_TYPE;
-
-                    // Commit the dumpCollect errorlog from above as
-                    // we dont want dump collect to kill the istep
-                    errlCommit( l_err, HWPF_COMP_ID );
-
-                }
-
-                // Send an Error mbox msg to FSP (either end or error)
-                l_errMsg = DUMP::sendMboxMsg(msgType);
-
+                // If error, commit and send error message.
                 if (l_errMsg)
                 {
                     TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-                              "ERROR : returned from DUMP::sendMboxMsg" );
+                   "ERROR : returned from DUMP::sendMboxMsg - dump start" );
 
                     errlCommit( l_errMsg, HWPF_COMP_ID );
+
+                    // don't break in this case because we not want to fail
+                    // the istep on the dump collect so we will continue
+                    // after we log the errhandle that we can't send a
+                    // message.
                 }
 
+                // Call the dump collect
+                l_err = DUMP::doDumpCollect();
+
+                // Got a Dump Collect error.. Commit the dumpCollect
+                // errorlog and then send an dump Error mbox message
+                // and FSP will decide what to do.
+                // We do not want dump Collect failures to terminate the
+                // istep.
+                if (l_err)
+                {
+                    TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                         "ERROR : returned from DUMP::HbDumpCopySrcToDest");
+
+                    break;
+                }
+
+            } while(0);
+
+            DUMP::DUMP_MSG_TYPE msgType = DUMP::DUMP_MSG_END_MSG_TYPE;
+
+            // Send dumpCollect success trace
+            if (!l_err)
+            {
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                           "SUCCESS : doDumpCollect" );
             }
+            // got an error that we need to send a ERROR message to FSP
+            // and commit the errorlog from dumpCollect.
             else
             {
-                TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-                      "ERROR : returned from VFS::module_load (libruntime.so)");
+                msgType = DUMP::DUMP_MSG_ERROR_MSG_TYPE;
+
+                // Commit the dumpCollect errorlog from above as
+                // we dont want dump collect to kill the istep
+                errlCommit( l_err, HWPF_COMP_ID );
+
             }
+
+            // Send an Error mbox msg to FSP (either end or error)
+            l_errMsg = DUMP::sendMboxMsg(msgType);
+
+            if (l_errMsg)
+            {
+                TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                          "ERROR : returned from DUMP::sendMboxMsg" );
+
+                errlCommit( l_errMsg, HWPF_COMP_ID );
+            }
+
+             RUNTIME::useRelocatedPayloadAddr(false);
+             // Wipe out our cache of the NACA/SPIRA pointers
+             RUNTIME::rediscover_hdat();
         }
 
         // If got an error in the procedure or collection of the dump kill the

src/usr/targeting/common/xmltohb/attribute_types_hb.xml
@@ -119,6 +119,20 @@
 </attribute>
 
 <attribute>
+    <id>MPIPL_HB_MDRT_COUNT</id>
+    <description>Actual MDRT count in Memory Preserving IPL mode.</description>
+    <simpleType>
+        <uint32_t>
+            <default>0</default>
+        </uint32_t>
+    </simpleType>
+    <persistency>volatile-zeroed</persistency>
+    <readable/>
+    <writeable/>
+    <hbOnly/>
+</attribute>
+
+<attribute>
     <id>IS_SLAVE_DRAWER</id>
     <description>0 = is master node, 1 = is slave node</description>
     <simpleType>

src/usr/targeting/common/xmltohb/target_types_hb.xml
@@ -34,6 +34,7 @@
 <targetTypeExtension>
     <id>sys-sys-power9</id>
     <attribute><id>IS_MPIPL_HB</id></attribute>
+    <attribute><id>MPIPL_HB_MDRT_COUNT</id></attribute>
     <attribute><id>IBSCOM_ENABLE_OVERRIDE</id></attribute>
     <attribute><id>HB_MUTEX_TEST_LOCK</id></attribute>
     <attribute><id>HB_EXISTING_IMAGE</id></attribute>

commit e30bf32f696010969d242d95531f494deb4e8150

MPIPL: Update MDRT count only if its valid
Commit 529bf93 added new attribute to save MDRT count (MPIPL_HB_MDRT_COUNT).
This commit un-conditionally updates MDRT count in call_host_runtime_setup().

At present OPAL doesn't support MPIPL and hence its not updating MDRT ntuple
in SPIRAH...resulting in writeActualCount() to  fail. And we endup in system
terminate state (at least in FSP system).

Lets make sure attribute contains valid MDRT count before calling
saveActualCount().

Change-Id: I8d25a274d65034bca392137e4f48b8a127fa4ee0
Fixes: 529bf93 (Use Attr to Save/Fetch MDRT Count)
CC: Raja Das <rajadas2@in.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/66474
Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
Reviewed-by: Dean Sanner <dsanner@us.ibm.com>
Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

src/usr/isteps/istep21/call_host_runtime_setup.C
@@ -830,7 +830,11 @@ void* call_host_runtime_setup (void *io_pArgs)
             uint32_t l_mdrtCount =
                 l_sys->getAttr<TARGETING::ATTR_MPIPL_HB_MDRT_COUNT>();
             //Update actual count in RUNTIME
-            RUNTIME::saveActualCount(RUNTIME::MS_DUMP_RESULTS_TBL, l_mdrtCount);
+            if(l_mdrtCount)
+            {
+                RUNTIME::saveActualCount( RUNTIME::MS_DUMP_RESULTS_TBL,
+                                          l_mdrtCount);
+            }
         }
 
         //Update the MDRT value (for MS Dump)