2fa477b66a39481998be7b97924552307df0ce20
Jenny Huynh <jhuynh@us.ibm.com>
2018-11-01 13:04:37 -0500

    Tune xbus packet_delay_limit for various systems
    For both low end and high end systems, increase the packet_delay_limit
    from 11->15. Setting to 15 across the board for consistency.
    
    Change-Id: Idcf8b81e465ff395ceff1f2f42f8484d4a95f4ab
    CQ:SW449875
    CQ:SW448334
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/68301
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: DANIEL C. HOWE <dchowe@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/initfiles/p9_fbc_ioe_dl_scom.C
+++ src/import/chips/p9/procedures/hwp/initfiles/p9_fbc_ioe_dl_scom.C
@@ -30,7 +30,7 @@
 using namespace fapi2;
 
 constexpr uint64_t literal_1 = 1;
-constexpr uint64_t literal_0x0B = 0x0B;
+constexpr uint64_t literal_0x0F = 0x0F;
 constexpr uint64_t literal_0xF = 0xF;
 constexpr uint64_t literal_0b111 = 0b111;
 constexpr uint64_t literal_0x6 = 0x6;
@@ -69,12 +69,12 @@ fapi2::ReturnCode p9_fbc_ioe_dl_scom(const fapi2::Target<fapi2::TARGET_TYPE_XBUS
                 || ((l_chip_id == 0x6) && (l_chip_ec == 0x11)) || ((l_chip_id == 0x6) && (l_chip_ec == 0x12)) || ((l_chip_id == 0x6)
                         && (l_chip_ec == 0x13)) || ((l_chip_id == 0x7) && (l_chip_ec == 0x10)) )
             {
-                l_scom_buffer.insert<11, 5, 59, uint64_t>(literal_0x0B );
+                l_scom_buffer.insert<11, 5, 59, uint64_t>(literal_0x0F );
             }
 
             if (((l_chip_id == 0x5) && (l_chip_ec == 0x10)) )
             {
-                l_scom_buffer.insert<12, 4, 60, uint64_t>(literal_0x0B );
+                l_scom_buffer.insert<12, 4, 60, uint64_t>(literal_0x0F );
             }
 
             l_scom_buffer.insert<28, 4, 60, uint64_t>(literal_0xF );
