746ee0ff5d9c2c2adcb9ee868229638c88f0d0b3
Andre Marin <aamarin@us.ibm.com>
2018-11-02 08:33:30 -0500

    Add exp_draminit and fix data_structs constants
    Change-Id: Iab828da19ec77dd2b342ea5e0fa42a99fc7ee84f
    Original-Change-Id: Ic8011ed9b6261ce2849b2c51f79c9533261222bf
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/67837
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_draminit.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_draminit.C
@@ -22,3 +22,57 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file exp_draminit.C
+/// @brief Procedure definition to initialize DRAM
+///
+// *HWP HWP Owner: Andre Marin <aamarin@us.ibm.com>
+// *HWP HWP Backup: Stephen Glancy <sglancy@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 2
+// *HWP Consumed by: FSP:HB
+
+#include <exp_inband.H>
+#include <generic/memory/lib/utils/c_str.H>
+#include <lib/exp_draminit_utils.H>
+
+///
+/// @brief Initializes DRAM
+/// @param[in] i_target the controller
+/// @return FAPI2_RC_SUCCESS iff ok
+///
+fapi2::ReturnCode exp_draminit(const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
+{
+    uint32_t l_crc = 0;
+
+    user_input_msdg l_phy_params;
+    FAPI_TRY(mss::exp::setup_phy_params(i_target, l_phy_params),
+             "Failed setup_phy_params() for %s", mss::c_str(i_target));
+
+    // Copy the PHY initialization parameters into the internal buffer of Explorer
+    FAPI_TRY( mss::exp::ib::putUserInputMsdg(i_target, l_phy_params, l_crc),
+              "Failed putUserInputMsdg() for %s", mss::c_str(i_target) );
+
+    // Issue full boot mode cmd though EXP-FW REQ buffer
+    {
+        host_fw_command_struct l_cmd;
+        mss::exp::setup_cmd_params(l_crc, l_cmd);
+        FAPI_TRY( mss::exp::ib::putCMD(i_target, l_cmd),
+                  "Failed putCMD() for  %s", mss::c_str(i_target) );
+    }
+
+    // Read the response message from EXP-FW RESP buffer
+    {
+        host_fw_response_struct l_response;
+        FAPI_TRY( mss::exp::ib::getRSP(i_target, l_response),
+                  "Failed getRSP() for  %s", mss::c_str(i_target) );
+
+        // Check if cmd was successful
+        FAPI_TRY(mss::exp::check::response(i_target, l_response),
+                 "Failed check::response() for  %s", mss::c_str(i_target) );
+    }
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
