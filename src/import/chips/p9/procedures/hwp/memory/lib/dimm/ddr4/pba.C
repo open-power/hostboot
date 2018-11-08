dd9caf484f5fb17f9ed758b28b7d69eb74194c6c
Stephen Glancy <sglancy@us.ibm.com>
2018-11-07 19:29:06 -0600

    Updates PBA to use RCW's to issue BCW's
    During LDIMM bringup it was found that function space
    changes do not work during PBA. This fix uses
    RCW's to issue BCW's avoiding function space changes
    
    Change-Id: Ia671e5a373949707657dde71808e931db86c9e2a
    Original-Change-Id: I86518269447e14fda386c780ec7a9ba99c90cdcc
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/68528
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: ANDRE A. MARIN <aamarin@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Devon A. Baughen <devon.baughen1@ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/memory/lib/dimm/ddr4/pba.C
+++ src/import/chips/p9/procedures/hwp/memory/lib/dimm/ddr4/pba.C
@@ -205,8 +205,7 @@ fapi2::ReturnCode execute_commands( const fapi2::Target<fapi2::TARGET_TYPE_DIMM>
         l_program.iv_instructions.push_back(mss::ccs::des_command<fapi2::TARGET_TYPE_MCBIST>());
 
         // Makes a copy of the vector, so we can do the function space swaps correctly
-        auto l_bcws = i_bcws;
-        FAPI_TRY(insert_function_space_select(l_bcws));
+        auto l_bcws = convert_to_rcws(i_bcws, commands::BCW_DELAY);
 
 
         FAPI_TRY(control_word_engine(i_target,
