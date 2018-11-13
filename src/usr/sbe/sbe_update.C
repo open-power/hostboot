d23603024bc431b2d63e31eca2e3ece75bc9519d
Nick Bofferding <bofferdn@us.ibm.com>
2018-11-13 00:35:35 -0600

    Only allow key transition in istep 10.2
    In certain cases, if a key transition driver was booted and the nest frequency
    had to be updated, the key transition flow would erroneously activate in istep
    7.3 (call_mss_freq).  This change confines key transitioning to istep 10.2
    
    Change-Id: I450703e21bf68644298f77fcdfca62eae5c667e4
    CQ: SW451376
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/68690
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/sbe/sbe_update.C
+++ src/usr/sbe/sbe_update.C
@@ -113,7 +113,8 @@ using namespace TARGETING;
 
 namespace SBE
 {
-    errlHndl_t updateProcessorSbeSeeproms()
+    errlHndl_t updateProcessorSbeSeeproms(
+        const KEY_TRANSITION_PERM i_keyTransPerm)
     {
         errlHndl_t err = NULL;
         errlHndl_t err_cleanup = NULL;
@@ -258,8 +259,12 @@ namespace SBE
                  err = NULL;
             }
 
-            // Check if a key transition is needed
-            err = secureKeyTransition();
+            // Check if a key transition is allowed/needed
+            if(i_keyTransPerm == ALLOW_KEY_TRANSITION)
+            {
+                err = secureKeyTransition();
+            }
+
             if (err)
             {
                 TRACFCOMP( g_trac_sbe, ERR_MRK"updateProcessorSbeSeeproms() - failed secureKeyTransition");
