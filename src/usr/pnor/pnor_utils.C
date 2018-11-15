9de9d8f7c5b5c73247dc69925a594fcd07ce060c
Ilya Smirnov <ismirno@us.ibm.com>
2018-11-15 08:34:52 -0600

    SMF: Create New UVBWLIST Partition
    A new partition was added for the contents of the XSCOM
    white/blacklist called UVBWLIST. When the time comes,
    this partition will hold the binary representation of
    the aforementioned lists. The partition is dumped into
    hostboot reserved memory for Ultravisor consumption.
    
    Change-Id: I06ebce74aae3c0df987e5a057967842042db2bae
    RTC: 192422
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/68869
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Nicholas E. Bofferding <bofferdn@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/pnor/pnor_utils.C
+++ src/usr/pnor/pnor_utils.C
@@ -378,7 +378,8 @@ bool PNOR::isEnforcedSecureSection(const uint32_t i_section)
                i_section == MEMD ||
                i_section == CAPP ||
                i_section == TESTLOAD ||
-               i_section == VERSION;
+               i_section == VERSION ||
+               i_section == UVBWLIST;
     #endif
 #else
     return false;
@@ -450,6 +451,7 @@ const char * PNOR::SectionIdToString( uint32_t i_secIdIndex )
         "CENHWIMG",    /**< PNOR::CENTAUR_HW_IMG : Centaur HCODE ref image   */
         "HDAT",        /**< PNOR::HDAT           : Hdat Data */
         "EECACHE",     /**< PNOR::EECACHE        : Cached data from various EEPROMs */
+        "UVBWLIST",    /**< PNOR::UVBWLIST       : Ultravisor XSCOM white/blacklist */
 #endif
     };
 
