bb0bb96d8a49f6d8711d3c143677d2353e2f09c8
Thi Tran <thi@us.ibm.com>
2018-12-10 16:27:10 -0600

    Temporarily fix master-p10 build
    To temporarily fix Jenkins failure because of target filter splits
    in commit https://rchgit01.rchland.ibm.com/gerrit1/#/c/69887/
    Permanent fix will be done by Infrastructure team by stopping
    building chips/p9 directory in master-p10 branch.
    
    Change-Id: I8f3b59077903a9a95f554360f07ea83e09f1f805
    Original-Change-Id: I9516a47aa8d42384d2f1bfce86edde4b14b3f37b
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/70276
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>

--- src/import/chips/p9/procedures/hwp/nest/p9_chiplet_scominit.mk
+++ src/import/chips/p9/procedures/hwp/nest/p9_chiplet_scominit.mk
@@ -24,4 +24,5 @@
 # IBM_PROLOG_END_TAG
 PROCEDURE=p9_chiplet_scominit
 $(call ADD_MODULE_INCDIR,$(PROCEDURE),$(ROOTPATH)/chips/p9/procedures/hwp/initfiles)
+$(call ADD_MODULE_INCDIR,$(PROCEDURE),$(ROOTPATH)/chips/p9/common/include/)
 $(call BUILD_PROCEDURE)
