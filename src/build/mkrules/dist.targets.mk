24f66ecb7882be36eba5ef13cbc414acb642c2af
Matt Raybuck <mraybuc@us.ibm.com>
2018-12-21 10:47:04 -0600

    Better logging for Partial Good Issues (1.5/2)
    Add necessary files for HWSV team to be able to make changes for (2/2)
    
    Change-Id: I1cc0c799d9fb743f9ab19dd0393015f16a3d4c01
    RTC:180365
    CMVC-Prereq:1074242
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/70003
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Reviewed-by: Roland Veloz <rveloz@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/build/mkrules/dist.targets.mk
+++ src/build/mkrules/dist.targets.mk
@@ -5,7 +5,7 @@
 #
 # OpenPOWER HostBoot Project
 #
-# Contributors Listed Below - COPYRIGHT 2013,2018
+# Contributors Listed Below - COPYRIGHT 2013,2019
 # [+] International Business Machines Corp.
 #
 #
