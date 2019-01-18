51206826999dfe85dda405257b0d0cc2182e9aac
Dan Crowell <dcrowell@us.ibm.com>
2019-01-18 13:50:07 -0600

    Add msgsync to doorbell wakeup logic to avoid weak consistency bug
    POWER9 added a new sync mode called 'msgsync' that is required
    to avoid weak consistency issues when you are using doorbell
    (msgsnd) functions.
    
    See POWER ISA Section 5.9.2 for details, excerpt here:
    The ordering done by sync (and ptesync) provides
    the appearance of "causality" across a sequence of
    msgsnd instructions, as in the following example.
    "msgsnd->T1" means "msgsnd instruction target-
    ting thread T1". "<DHDI 0>" means "occurrence of
    Directed Hypervisor Doorbell interrupt caused by
    msgsnd executed on T0". On T0, register r1 is
    assumed to contain the value 1.
      T0           T1           T2
      std r1,X     <DHDI 0>     <DHDI 1>
      sync         msgsnd->T2   msgsync
      msgsnd->T1                ld r1,X
    In this example, T2's load from X must return 1.
    
    The change here adds the msgsync call to the code that executes
    any time we handle a doorbell interrupt.  In addition there is a
    POWER9 DD2 errata that indicates we also require a lwsync to
    ensure consistency.
    
    Change-Id: Ib0f3571926d71efcbffa205093278e2a1d58df85
    CQ: SW454611
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/70650
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: William G. Hoffa <wghoffa@us.ibm.com>

--- src/kernel/syscall.C
+++ src/kernel/syscall.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2010,2018                        */
+/* Contributors Listed Below - COPYRIGHT 2010,2019                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -55,6 +55,12 @@ namespace KernelIpc
 extern "C"
 void kernel_execute_hype_doorbell()
 {
+    // Per POWER ISA Section 5.9.2, to avoid any weak consistency
+    //  issues we must use a msgsync instruction before consuming
+    //  any data set by a different thread following a doorbell
+    //  wakeup.
+    msgsync();
+
     task_t* t = TaskManager::getCurrentTask();
     doorbell_clear();
 
