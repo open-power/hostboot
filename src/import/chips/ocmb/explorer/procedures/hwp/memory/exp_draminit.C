97517fa73c61b025b7a44367e10753fdeef12827
Stephen Glancy <sglancy@us.ibm.com>
2019-02-14 11:33:59 -0500

    Adds explorer training response display functions
    Change-Id: I2e5c8ef08cda7abf83a322d2a3b0e01de2aaee41
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/71902
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: Mark Pizzutillo <mark.pizzutillo@ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/76117
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
    Tested-by: Christian R. Geddes <crgeddes@us.ibm.com>

--- src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_draminit.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_draminit.C
@@ -35,6 +35,7 @@
 
 #include <lib/shared/exp_consts.H>
 #include <exp_inband.H>
+#include <lib/shared/exp_consts.H>
 #include <generic/memory/lib/utils/c_str.H>
 #include <generic/memory/lib/utils/mss_bad_bits.H>
 #include <lib/exp_draminit_utils.H>
