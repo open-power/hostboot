85d4b2dd5405427449be9776674378b75218016e
Christian Geddes <crgeddes@us.ibm.com>
2019-03-01 12:04:28 -0600

    Compile and add calls to exp_scominit in istep 13.8
    Since this code is ready and seemingly passing in axone simics okay
    we will pull it in to cross another item off the list.
    
    Change-Id: Id02b1fae825d5e601312251f75af21120f9fa2ba
    RTC: 195556
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/72723
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: William G. Hoffa <wghoffa@us.ibm.com>
    Reviewed-by: Corey V. Swenson <cswenson@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep13/call_mss_scominit.C
+++ src/usr/isteps/istep13/call_mss_scominit.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2017                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -44,6 +44,7 @@
 #include    <p9_mss_scominit.H>
 #include    <p9_throttle_sync.H>
 #include    <p9c_mss_scominit.H>
+#include    <exp_scominit.H>
 
 using   namespace   ERRORLOG;
 using   namespace   ISTEP;
@@ -152,6 +153,50 @@ void* call_mss_scominit (void *io_pArgs)
                 TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
                       "SUCCESS running p9c_mss_scominit HWP on "
                       "target HUID %.8X", TARGETING::get_huid(l_membuf_target));
+            }
+        }
+
+         // Get all OCMB targets
+        TARGETING::TargetHandleList l_ocmbTargetList;
+        getAllChips(l_ocmbTargetList, TYPE_OCMB_CHIP);
+
+        for (const auto & l_ocmb_target : l_ocmbTargetList)
+        {
+            // Dump current run on target
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                    "Running exp_scominit HWP on "
+                    "target HUID %.8X",
+                    TARGETING::get_huid(l_ocmb_target));
+
+            fapi2::Target <fapi2::TARGET_TYPE_OCMB_CHIP> l_fapi_ocmb_target
+                (l_ocmb_target);
+
+            //  call the HWP with each fapi2::Target
+            FAPI_INVOKE_HWP(l_err, exp_scominit, l_fapi_ocmb_target);
+
+            if (l_err)
+            {
+                TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                          "ERROR 0x%.8X: exp_scominit HWP returns error",
+                          l_err->reasonCode());
+
+                // capture the target data in the elog
+                ErrlUserDetailsTarget(l_fapi_ocmb_target).addToLog(l_err);
+
+                // Create IStep error log and cross reference to error that
+                // occurred
+                l_stepError.addErrorDetails( l_err );
+
+                // Commit Error
+                errlCommit( l_err, HWPF_COMP_ID );
+
+                break;
+            }
+            else
+            {
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                      "SUCCESS running exp_scominit HWP on "
+                      "target HUID %.8X", TARGETING::get_huid(l_ocmb_target));
             }
         }
 
