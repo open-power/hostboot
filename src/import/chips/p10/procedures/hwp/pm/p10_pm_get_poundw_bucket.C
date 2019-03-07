ff9e526d5a730398c552afb13605d608471cd8e5
Prasad Bg Ranganath <prasadbgr@in.ibm.com>
2019-03-07 11:09:33 -0600

    P10: #V and #W attribute read support
    RTC:200999
    Change-Id: If1a020ec2074aabe944f2c5d2f4460aacc9e2b7f
    Original-Change-Id: I65e7975bf576d3f5bdef9fa2087daeda98ae6acd
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/73020
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Prem Shanker Jha <premjha2@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/83353
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Daniel M Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/pm/p10_pm_get_poundw_bucket.C
@@ -0,0 +1,66 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/pm/p10_pm_get_poundw_bucket.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019                             */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+/// @file p10_pm_get_poundw_bucket.C
+/// @brief Grab PM data from attribute
+///
+/// @brief Provide structure for DDSCdata so that info from #W keyword can be
+/// stored inside of this struct.
+/// Also define prototype for p10_pm_get_poundw_bucket
+///
+/// *HWP HW Owner    : Greg Stills(stillgs@us.ibm.com)
+/// *HWP FW Owner    : Prasad Bg Ranganath(prasadbgr@in.ibm.com)
+/// *HWP Team        : PM - Calling this function.
+/// *HWP Consumed by : HB
+/// *HWP Level       : 2
+///
+
+// ----------------------------------------------------------------------
+// Includes
+// ----------------------------------------------------------------------
+#include <p10_pm_get_poundw_bucket.H>
+#include <attribute_ids.H>
+
+// See doxygen in header file
+fapi2::ReturnCode p10_pm_get_poundw_bucket(
+    const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target,
+    fapi2::ddscData_t& o_data)
+{
+    FAPI_DBG("Entering p10_pm_get_poundw_bucket ....");
+
+    //clear the output variable
+    memset (&o_data, 0, sizeof(fapi2::ddscData_t));
+    //Set up a char array to hold the bucket data from an attr read
+    fapi2::ATTR_POUNDW_BUCKET_DATA_Type l_bucketAttr;
+
+    //Perform an ATTR_GET for POUNDW_BUCKET data on the proc target
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_POUNDW_BUCKET_DATA,
+                           i_target,
+                           l_bucketAttr));
+
+    memcpy(&o_data, l_bucketAttr, sizeof(o_data));
+
+fapi_try_exit:
+    FAPI_DBG("Exiting p10_pm_get_poundw_bucket ....");
+    return fapi2::current_err;
+}
---
+++ src/import/chips/p10/procedures/hwp/pm/p10_pm_get_poundw_bucket_attr.C
@@ -0,0 +1,147 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/pm/p10_pm_get_poundw_bucket_attr.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019                             */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p10_pm_get_poundw_bucket_attr.C
+/// @brief Grab PM data from certain bucket in #W keyword in CRP0 record
+///
+/// *HWP HW Owner    : Greg Stills(stillgs@us.ibm.com)
+/// *HWP FW Owner    : Prasad Bg Ranganath(prasadbgr@in.ibm.com)
+/// *HWP Team        : PM - Calling this function.
+/// *HWP Consumed by : HB
+/// *HWP Level       : 2
+///
+
+// ----------------------------------------------------------------------
+// Includes
+// ----------------------------------------------------------------------
+#include <p10_pm_get_poundw_bucket_attr.H>
+#include <p10_pm_get_poundw_bucket.H>
+#include <p10_pm_get_poundv_bucket.H>
+#include <mvpd_access_defs.H>
+#include <attribute_ids.H>
+
+// See doxygen in header file
+fapi2::ReturnCode p10_pm_get_poundw_bucket_attr(
+    const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target,
+    uint8_t* o_data)
+{
+    FAPI_DBG("Entering p10_pm_get_poundw_bucket_attr ....");
+    uint8_t* l_fullVpdData = nullptr;
+    uint32_t l_vpdSize = 0;
+    uint8_t l_bucketId;
+    uint16_t l_bucketSize = 0;
+    *o_data = 0;
+
+    //Get the #V bucket associated with proc target
+    fapi2::voltageBucketData_t l_voltageBucket;
+
+    FAPI_TRY( p10_pm_get_poundv_bucket(i_target,
+                                       l_voltageBucket) );
+
+    //Use the bucket ID from the #V bucket
+    l_bucketId = l_voltageBucket.bucketId;
+
+    //First read is to get size of vpd record, note the o_buffer is NULL
+    FAPI_TRY( getMvpdField(fapi2::MVPD_RECORD_CRP0,
+                           fapi2::MVPD_KEYWORD_PDW,
+                           i_target,
+                           NULL,
+                           l_vpdSize) );
+
+    //Allocate memory for vpd data
+    l_fullVpdData = reinterpret_cast<uint8_t*>(malloc(l_vpdSize));
+
+    //Second read is to get data of vpd record
+    FAPI_TRY( getMvpdField(fapi2::MVPD_RECORD_CRP0,
+                           fapi2::MVPD_KEYWORD_PDW,
+                           i_target,
+                           l_fullVpdData,
+                           l_vpdSize) );
+
+    //#W record is laid out as follows:
+    //Name:                  0x2 byte
+    //Length:                0x2 byte
+    //Version:               0x1 byte **buffer starts here
+    //#W Bucket ID #1:       0x1 byte
+    //DDSC Data for bucket 1: varies by version
+    //#W Bucket ID #2:       0x1 byte
+    //DDSC Data for bucket 2: varies by version
+
+    if( *l_fullVpdData == POUNDW_VERSION_1)
+    {
+        //Set the size of the bucket
+        l_bucketSize = POUNDW_BUCKETID_SIZE + PW_VER_1_DDSCDATA_SIZE;
+    }
+    else
+    {
+        FAPI_ASSERT( false,
+                     fapi2::INVALID_POUNDW_VERSION()
+                     .set_CHIP_TARGET(i_target)
+                     .set_POUNDW_VERSION(*l_fullVpdData),
+                     "p10_pm_get_poundw_bucket_attr::Invalid #W record "
+                     "version: 0x%x",
+                     *l_fullVpdData);
+    }
+
+    //Copy version into output data
+    *o_data = *l_fullVpdData;
+
+    l_vpdSize = l_vpdSize - POUNDW_VERSION_SIZE - ((l_bucketId - 1) *
+                l_bucketSize);
+    // This assert ensures the size of the calculated data is correct
+    FAPI_ASSERT(l_vpdSize >= l_bucketSize,
+                fapi2::BAD_POUNDW_VPD_READ()
+                .set_CHIP_TARGET(i_target)
+                .set_EXPECTED_SIZE(l_bucketSize)
+                .set_ACTUAL_SIZE(l_vpdSize),
+                "#W data read was too small!" );
+
+    // Use the selected bucket id to populate the output data
+    memcpy(o_data + POUNDW_VERSION_SIZE,
+           l_fullVpdData + POUNDW_VERSION_SIZE +
+           ((l_bucketId - 1) * l_bucketSize),
+           l_bucketSize);
+
+    // This assert ensures the output data bucket ID matches what we looked for
+    FAPI_ASSERT( (l_bucketId == ((fapi2::ddscData_t*)o_data)->bucketId),
+                 fapi2::INCORRECT_POUNDW_BUCKET_ID()
+                 .set_CHIP_TARGET(i_target)
+                 .set_BUCKET_ID(((fapi2::ddscData_t*)o_data)->bucketId)
+                 .set_EXP_BUCKET_ID(l_bucketId),
+                 "Incorrect Bucket Id = %d, expected %d",
+                 ((fapi2::ddscData_t*)o_data)->bucketId,
+                 l_bucketId );
+
+fapi_try_exit:
+
+    if(l_fullVpdData != NULL)
+    {
+        free(l_fullVpdData);
+        l_fullVpdData = NULL;
+    }
+
+    FAPI_DBG("Exiting p10_pm_get_poundw_bucket_attr ....");
+
+    return fapi2::current_err;
+}
