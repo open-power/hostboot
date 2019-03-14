76a4f5d86930611ddb4be8d273e9fbd279ff3f53
Louis Stermole <stermole@us.ibm.com>
2019-03-14 15:49:18 -0400

    Move explorer specialization of eff_memory_size into explorer lib
      Also change explorer specialization to work around mss::index until fixed
    
    Change-Id: Idd2f98e23d3431771d49f36829b3d7cd9ce57417
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/73372
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: ANDRE A. MARIN <aamarin@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/73472
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>

--- src/import/chips/ocmb/explorer/procedures/hwp/memory/lib/eff_config/explorer_memory_size.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/lib/eff_config/explorer_memory_size.C
@@ -22,3 +22,60 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file explorer_memory_size.C
+/// @brief Return the effective memory size behind a target
+///
+// *HWP HWP Owner: Andre Marin <aamarin@us.ibm.com>
+// *HWP HWP Backup: Louis Stermole <stermole@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 3
+// *HWP Consumed by: HB:FSP
+
+#include <fapi2.H>
+
+#include <lib/shared/exp_consts.H>
+#include <mss_generic_attribute_getters.H>
+#include <generic/memory/lib/utils/memory_size.H>
+
+namespace mss
+{
+
+///
+/// @brief Return the total memory size behind a DIMM target
+/// @param[in] i_target the DIMM target
+/// @param[out] o_size the size of memory in GB behind the target
+/// @return FAPI2_RC_SUCCESS if ok
+/// @note The purpose of this specialization is to bridge the gap between different accessor functions
+///
+template<>
+fapi2::ReturnCode eff_memory_size<mss::mc_type::EXPLORER>(
+    const fapi2::Target<fapi2::TARGET_TYPE_DIMM>& i_target,
+    uint64_t& o_size )
+{
+#ifdef ACCESSORS_FIXED
+    uint32_t l_size = 0;
+    o_size = 0;
+    FAPI_TRY( mss::attr::get_dimm_size(i_target, l_size) );
+    o_size = l_size;
+
+fapi_try_exit:
+    return fapi2::current_err;
+#endif
+
+    // Temporarily unrolling attribute accessor here until mss library is fixed
+    uint32_t l_value[2] = {};
+    const auto l_port = i_target.getParent<fapi2::TARGET_TYPE_MEM_PORT>();
+    uint8_t l_pos;
+
+    FAPI_TRY( FAPI_ATTR_GET(fapi2::ATTR_REL_POS, i_target, l_pos) );
+    FAPI_TRY( FAPI_ATTR_GET(fapi2::ATTR_MEM_EFF_DIMM_SIZE, l_port, l_value) );
+    o_size = l_value[l_pos];
+
+fapi_try_exit:
+    return fapi2::current_err;
+
+}
+
+} // ns mss
