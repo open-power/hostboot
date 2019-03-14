7063dd994e9a392e69b1d4b9e72512998d004937
Yue Du <daviddu@us.ibm.com>
2019-03-13 21:02:31 -0500

    CORECACHE: startclocks and stopclocks
    - Add multicast groups to wrappers
    - Remove polling of CLOCK_STAT_[SL/NSL/ARY] registers as they presently don't
      change values on 16u01.  Applicable only the QME (SIMICS) for now.
    
    Change-Id: I5e7874b50918e57c26fcc0a4b07330901c7ec965
    Original-Change-Id: I497be977b256ad70f91fea1ea6ebb5f8c402623c
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/73296
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/86235
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/corecache/p10_hcd_cache_stopclocks.C
+++ src/import/chips/p10/procedures/hwp/corecache/p10_hcd_cache_stopclocks.C
@@ -1,7 +1,7 @@
 /* IBM_PROLOG_BEGIN_TAG                                                   */
 /* This is an automatically generated prolog.                             */
 /*                                                                        */
-/* $Source: src/import/chips/p10/procedures/hwp/corecache/p10_hcd_core_stopclocks.C $ */
+/* $Source: src/import/chips/p10/procedures/hwp/corecache/p10_hcd_cache_stopclocks.C $ */
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
@@ -26,7 +26,7 @@
 
 
 ///
-/// @file  p10_hcd_core_stopclocks.C
+/// @file  p10_hcd_cache_stopclocks.C
 /// @brief
 ///
 
@@ -43,7 +43,7 @@
 // Includes
 //------------------------------------------------------------------------------
 
-#include "p10_hcd_core_stopclocks.H"
+#include "p10_hcd_cache_stopclocks.H"
 #include "p10_hcd_corecache_clock_control.H"
 #include "p10_hcd_common.H"
 
@@ -51,74 +51,74 @@
 // Constant Definitions
 //------------------------------------------------------------------------------
 
-enum P10_HCD_CORE_STOPCLOCKS_CONSTANTS
+enum P10_HCD_CACHE_STOPCLOCKS_CONSTANTS
 {
-    HCD_ECL2_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS        = 10000,   // 10^4ns = 10us timeout
-    HCD_ECL2_CLK_SYNC_DROP_POLL_DELAY_HW_NS          = 100,     // 100ns poll loop delay
-    HCD_ECL2_CLK_SYNC_DROP_POLL_DELAY_SIM_CYCLE      = 3200,    // 3.2k sim cycle delay
+    HCD_L3_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS        = 10000,   // 10^4ns = 10us timeout
+    HCD_L3_CLK_SYNC_DROP_POLL_DELAY_HW_NS          = 100,     // 100ns poll loop delay
+    HCD_L3_CLK_SYNC_DROP_POLL_DELAY_SIM_CYCLE      = 3200,    // 3.2k sim cycle delay
 };
 
 //------------------------------------------------------------------------------
-// Procedure: p10_hcd_core_stopclocks
+// Procedure: p10_hcd_cache_stopclocks
 //------------------------------------------------------------------------------
 
 fapi2::ReturnCode
-p10_hcd_core_stopclocks(
+p10_hcd_cache_stopclocks(
     const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_OR > & i_target,
     uint32_t i_regions)
 {
-
-    fapi2::buffer<uint64_t> l_data64  = 0;
-    uint32_t                l_timeout = 0;
-
 // @todo RTC 207921
 //     fapi2::Target < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > eq_target =
 //         i_target.getParent < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST > ();
     fapi2::Target < fapi2::TARGET_TYPE_EQ > eq_target =
         i_target.getParent < fapi2::TARGET_TYPE_EQ > ();
 
-    FAPI_INF(">>p10_hcd_core_stopclocks");
 
-    FAPI_DBG("Disable ECL2 Regional PSCOMs via CPLT_CTRL3[5-8:ECL2_REGIONS]");
+    fapi2::buffer<uint64_t> l_data64  = 0;
+    uint32_t                l_timeout = 0;
+
+    FAPI_INF(">>p10_hcd_cache_stopclocks");
+
+    FAPI_DBG("Disable L3 Regional PSCOMs via CPLT_CTRL3[9-12:L3_REGIONS]");
     FAPI_TRY( putScom( eq_target, G_CPLT_CTRL3_CLR, MASK_H32(i_regions) ) );
 
-    FAPI_DBG("Enable ECL2 Regional Fences via CPLT_CTRL1[5-8:ECL2_FENCES]");
+    FAPI_DBG("Enable L3 Regional Fences via CPLT_CTRL1[9-12:L3_FENCES]");
     FAPI_TRY( putScom( eq_target, G_CPLT_CTRL1_OR, MASK_H32(i_regions) ) );
 
-    FAPI_TRY( p10_hcd_corecache_clock_control(eq_target, i_regions, HCD_CLK_STOP ) );
+    FAPI_TRY( p10_hcd_corecache_clock_control( eq_target, i_regions, HCD_CLK_STOP ) );
 
-    FAPI_DBG("Disable ECL2 Skewadjust via CPMS_CGCSR_[1:CL2_CLK_SYNC_ENABLE]");
-    FAPI_TRY( putScom( i_target, G_QME_CPMS_CGCSR_CLR, MASK_SET(1) ) );
+    FAPI_DBG("Disable L3 Skewadjust via CPMS_CGCSR_[0:L3_CLK_SYNC_ENABLE]");
+    FAPI_TRY( putScom( i_target, G_QME_CPMS_CGCSR_CLR, MASK_SET(0) ) );
 
-    FAPI_DBG("Check ECL2 Skewadjust Removed via CPMS_CGCSR[33:CL2_CLK_SYNC_DONE]");
-    l_timeout = HCD_ECL2_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS /
-                HCD_ECL2_CLK_SYNC_DROP_POLL_DELAY_HW_NS;
+    FAPI_DBG("Check L3 Skewadjust Removed via CPMS_CGCSR[32:L3_CLK_SYNC_DONE]");
+    l_timeout = HCD_L3_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS /
+                HCD_L3_CLK_SYNC_DROP_POLL_DELAY_HW_NS;
 
     do
     {
         FAPI_TRY( getScom( i_target, G_QME_CPMS_CGCSR, l_data64 ) );
 
         // use multicastOR to check 0
-        if( DATA_GET(33) == 0 )
+        if( DATA_GET(32) == 0 )
         {
             break;
         }
 
-        fapi2::delay(HCD_ECL2_CLK_SYNC_DROP_POLL_DELAY_HW_NS,
-                     HCD_ECL2_CLK_SYNC_DROP_POLL_DELAY_SIM_CYCLE);
+        fapi2::delay(HCD_L3_CLK_SYNC_DROP_POLL_DELAY_HW_NS,
+                     HCD_L3_CLK_SYNC_DROP_POLL_DELAY_SIM_CYCLE);
     }
     while( (--l_timeout) != 0 );
 
     FAPI_ASSERT((l_timeout != 0),
-                fapi2::ECL2_CLK_SYNC_DROP_TIMEOUT()
-                .set_ECL2_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS(HCD_ECL2_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS)
+                fapi2::L3_CLK_SYNC_DROP_TIMEOUT()
+                .set_L3_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS(HCD_L3_CLK_SYNC_DROP_POLL_TIMEOUT_HW_NS)
                 .set_CPMS_CGCSR(l_data64)
                 .set_CORE_TARGET(i_target),
-                "ECL2 Clock Sync Drop Timeout");
+                "L3 Clock Sync Drop Timeout");
 
 fapi_try_exit:
 
-    FAPI_INF("<<p10_hcd_core_stopclocks");
+    FAPI_INF("<<p10_hcd_cache_stopclocks");
 
     return fapi2::current_err;
 
---
+++ src/import/chips/p10/procedures/hwp/corecache/p10_hcd_corecache_clock_control.C
@@ -0,0 +1,216 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/corecache/p10_hcd_corecache_clock_control.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file  p10_hcd_corecache_clock_control.C
+/// @brief
+///
+
+
+// *HWP HWP Owner          : David Du         <daviddu@us.ibm.com>
+// *HWP Backup HWP Owner   : Greg Still       <stillgs@us.ibm.com>
+// *HWP FW Owner           : Prem Shanker Jha <premjha2@in.ibm.com>
+// *HWP Team               : PM
+// *HWP Consumed by        : SBE:QME
+// *HWP Level              : 2
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+
+#include "p10_hcd_corecache_clock_control.H"
+#include <p10_hcd_common.H>
+
+//------------------------------------------------------------------------------
+// Constant Definitions
+//------------------------------------------------------------------------------
+
+enum P10_HCD_CORECACHE_CLOCK_CONTROL_CONSTANTS
+{
+    HCD_CORECACHE_CLK_CTRL_POLL_TIMEOUT_HW_NS              = 1000000, // 10^6ns = 1ms timeout
+    HCD_CORECACHE_CLK_CTRL_POLL_DELAY_HW_NS                = 10000,   // 10us poll loop delay
+    HCD_CORECACHE_CLK_CTRL_POLL_DELAY_SIM_CYCLE            = 32000    // 320k sim cycle delay
+};
+
+//------------------------------------------------------------------------------
+// Procedure: p10_hcd_corecache_clock_control
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p10_hcd_corecache_clock_control(
+    const fapi2::Target < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > & i_target,
+    // 1) The following parameters assume bits already line up with bits in first or
+    //    second half word of the scom register, in another word, they are 32-bit-mask,
+    //    enum P10_HCD_CLK_CTRL_CONSTANTS in p10_hcd_common.h can be used in the caller
+    // 2) Due to PPE compatiablity/cost, number of parameters is minimalized; thus
+    //    i_command should OR-in Master/Slave bit should they be used(not common usage)
+    //    Example: HCD_CLK_STOP|HCD_CLK_SLAVE|HCD_CLK_MASTER as i_command
+    // 3) As conventional usage, this module always assume to operate on all 3 tholds
+    //    Therefore, no type parameter is provided as they were never used
+    uint32_t i_regions,
+    uint32_t i_command)
+
+{
+
+    fapi2::buffer<uint64_t> l_data64                = 0;
+#ifdef __PPE_QME
+    fapi2::Target < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST > l_mc_or = i_target;//default OR
+    uint32_t                l_timeout               = 0;
+    uint32_t                l_clk_stat              = 0;
+    uint32_t                l_clk_stat_expected     = 0;
+#endif
+    FAPI_INF(">>p10_hcd_corecache_clock_control[%s] on regions[0x%08X]",
+             (i_command & HCD_CLK_STOP) ? "STOP" : "START", i_regions);
+
+    FAPI_DBG("Exit Flush (set flushmode inhibit) via CPLT_CTRL4[REGIONS]");
+    FAPI_TRY( putScom( i_target, G_CPLT_CTRL4_OR, MASK_H32(i_regions) ) );
+
+    FAPI_DBG("Enable Alignment via CPLT_CTRL0[3:CTRL_CC_FORCE_ALIGN]");
+    FAPI_TRY( putScom( i_target, G_CPLT_CTRL0_OR, MASK_SET(3) ) );
+
+    FAPI_DBG("Disable Alignment via CPLT_CTRL0[3:CTRL_CC_FORCE_ALIGN]");
+    FAPI_TRY( putScom( i_target, G_CPLT_CTRL0_CLR, MASK_SET(3) ) );
+
+    FAPI_DBG("Clear SCAN_REGION_TYPE Register");
+    FAPI_TRY( putScom( i_target, G_SCAN_REGION_TYPE, 0 ) );
+
+    FAPI_DBG("Start/Stop ECL2 Clocks via CLK_REGION_TYPE");
+    FAPI_TRY( putScom( i_target, G_CLK_REGION_TYPE, MASK_HL( (i_command | i_regions), HCD_CLK_THOLD_ALL ) ) );
+
+#ifdef __PPE_QME
+    FAPI_DBG("Poll OPCG done bit to check for completeness via CPLT_STAT0[8:CC_CTRL_OPCG_DONE_DC]");
+    l_timeout = HCD_CORECACHE_CLK_CTRL_POLL_TIMEOUT_HW_NS /
+                HCD_CORECACHE_CLK_CTRL_POLL_DELAY_HW_NS;
+
+    do
+    {
+        FAPI_TRY( getScom( i_target, G_CPLT_STAT0, l_data64 ) );
+
+        //use multicastAND to check 1
+        if( DATA_GET(8) == 1 )
+        {
+            break;
+        }
+
+        fapi2::delay(HCD_CORECACHE_CLK_CTRL_POLL_DELAY_HW_NS,
+                     HCD_CORECACHE_CLK_CTRL_POLL_DELAY_SIM_CYCLE);
+    }
+    while( (--l_timeout) != 0 );
+
+    FAPI_ASSERT((l_timeout != 0),
+                fapi2::CORECACHE_CLK_CTRL_TIMEOUT()
+                .set_CLK_CTRL_POLL_TIMEOUT_HW_NS(HCD_CORECACHE_CLK_CTRL_POLL_TIMEOUT_HW_NS)
+                .set_CPLT_STAT0(l_data64)
+                .set_CLK_COMMAND(i_command)
+                .set_CLK_REGIONS(i_regions)
+                .set_QUAD_TARGET(i_target),
+                "Core/Cache Clock Control Timeout");
+
+    // IF clocks are stopped, check for stat bits ON, use MC_AND target
+    // Otherwise, check for bits OFF, use MC_OR target
+
+    // Do not blame me, fapi forces me to write this ugly code below
+    // Note, since mc target can either be AND/OR, which is
+    // treated as different variables in C++ as different types
+    // while you are allowed to name the target with the same name
+    // which doesnt really help because compiler doesnt care that
+    // 1) Therefore, inline i_command?i_target:l_mc did not work
+    //    as compiler disallow different type in comparison outcome
+    // 2) Also try to create a "common" target container failed
+    //    as there is no generic mc target, the default is always OR
+    // 3) and of course, fapi disallow you to change mc type
+    //    on the given target, you HAVE to create different target
+
+    FAPI_DBG("Check CLOCK_STAT_SL[bit OFF for clock Started]");
+
+    if( i_command & HCD_CLK_STOP )
+    {
+        l_clk_stat_expected = i_regions;
+        FAPI_TRY( getScom( i_target , G_CLOCK_STAT_SL, l_data64 ) );
+    }
+    else
+    {
+        // stat expected 0s in this case
+        FAPI_TRY( getScom( l_mc_or , G_CLOCK_STAT_SL, l_data64 ) );
+    }
+
+    DATA_H32R(l_clk_stat);
+    FAPI_ASSERT( ( ( l_clk_stat & i_regions ) == l_clk_stat_expected ),
+                 fapi2::CORECACHE_CLK_CTRL_SL_FAILED()
+                 .set_CLK_STAT_SL(l_clk_stat)
+                 .set_CLK_COMMAND(i_command)
+                 .set_CLK_REGIONS(i_regions)
+                 .set_QUAD_TARGET(i_target),
+                 "Core/Cache Clock Control Failed in SL thold");
+
+    FAPI_DBG("Check CLOCK_STAT_NSL[bit OFF for clock Started]");
+
+    if( i_command & HCD_CLK_STOP )
+    {
+        FAPI_TRY( getScom( i_target , G_CLOCK_STAT_NSL, l_data64 ) );
+    }
+    else
+    {
+        FAPI_TRY( getScom( l_mc_or , G_CLOCK_STAT_NSL, l_data64 ) );
+    }
+
+    DATA_H32R(l_clk_stat);
+    FAPI_ASSERT( ( ( l_clk_stat & i_regions ) == l_clk_stat_expected ),
+                 fapi2::CORECACHE_CLK_CTRL_NSL_FAILED()
+                 .set_CLK_STAT_NSL(l_clk_stat)
+                 .set_CLK_COMMAND(i_command)
+                 .set_CLK_REGIONS(i_regions)
+                 .set_QUAD_TARGET(i_target),
+                 "Core/Cache Clock Control Failed in NSL thold");
+
+    FAPI_DBG("Check CLOCK_STAT_ARY[bit OFF for clock Started]");
+
+    if( i_command & HCD_CLK_STOP )
+    {
+        FAPI_TRY( getScom( i_target , G_CLOCK_STAT_NSL, l_data64 ) );
+    }
+    else
+    {
+        FAPI_TRY( getScom( l_mc_or , G_CLOCK_STAT_NSL, l_data64 ) );
+    }
+
+    DATA_H32R(l_clk_stat);
+    FAPI_ASSERT( ( ( l_clk_stat & i_regions ) == l_clk_stat_expected ),
+                 fapi2::CORECACHE_CLK_CTRL_ARY_FAILED()
+                 .set_CLK_STAT_ARY(l_clk_stat)
+                 .set_CLK_REGIONS(i_regions)
+                 .set_QUAD_TARGET(i_target),
+                 "Core/Cache Clock Control Failed in ARY thold");
+#endif
+
+    FAPI_DBG("Enter Flush (clear flushmode inhibit) via CPLT_CTRL4[REGIONS]");
+    FAPI_TRY( putScom( i_target, G_CPLT_CTRL4_CLR, MASK_H32(i_regions) ) );
+
+fapi_try_exit:
+
+    FAPI_INF("<<p10_hcd_corecache_clock_control");
+
+    return fapi2::current_err;
+
+}
