7d235200c6e17cfac8337e914bae5f626e81cb20
Yue Du <daviddu@us.ibm.com>
2019-03-15 16:57:05 -0500

    CORECACHE: poweron and poweroff../lib/p10_hcd_common.H
    - Fix issues with Cronus build and early testing
    - Temp work-around for HW487147
    - Condition out the PFET sense polling for HW486979
    - Fix VBU operational bugs
    - For Cronus to work on Awan, removed TARGET_TYPE_MULTICAST from
      eq_target made from core.getParent call.
    
    Change-Id: I7482a3d3036670bbad2f59ea8970dcd32de86984
    Original-Change-Id: I072dac91767ae24e12397eca6a3df21a3490148f
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/73464
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/86236
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/corecache/p10_hcd_corecache_power_control.C
@@ -0,0 +1,212 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/corecache/p10_hcd_corecache_power_control.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file  p10_hcd_corecache_power_control.C
+/// @brief
+///
+
+// *HWP HWP Owner          : David Du               <daviddu@us.ibm.com>
+// *HWP Backup HWP Owner   : Greg Still             <stillgs@us.ibm.com>
+// *HWP FW Owner           : Prasad Brahmasamurdra  <prasadbgr@in.ibm.com>
+// *HWP Team               : PM
+// *HWP Consumed by        : SBE:QME
+// *HWP Level              : 2
+
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+
+#include "p10_hcd_corecache_power_control.H"
+#include "p10_hcd_common.H"
+
+//------------------------------------------------------------------------------
+// Constant Definitions
+//------------------------------------------------------------------------------
+
+enum P10_HCD_CORECACHE_POWER_CONTROL_CONSTANTS
+{
+    HCD_CORECACHE_POW_CTRL_POLL_TIMEOUT_HW_NS   = 100000,  // 10^5ns = 100us timeout
+    HCD_CORECACHE_POW_CTRL_POLL_DELAY_HW_NS     = 500,     // 500ns poll loop delay
+    HCD_CORECACHE_POW_CTRL_POLL_DELAY_SIM_CYCLE = 32000,   // 32k sim cycle delay
+};
+
+const uint32_t HCD_PFET_OVERRIDES[2] =
+{
+    (BITS32(4, 2) | BIT32(8)),     //VDD.val/sel/regulation_finger
+    BITS32(6, 2)                  //VCS.val/sel
+};
+
+const uint32_t HCD_PFET_SEQ_STATES[2][2] =
+{
+    { BIT32(1),    BIT32(3)    },  //VDD_OFF, VCS_OFF
+    { BITS32(0, 2), BITS32(2, 2) } //VDD_ON,  VCS_ON
+};
+
+const uint32_t HCD_PFET_SENSE_BITS[2][2] =
+{
+    { BIT32(1),    BIT32(3)    },  //VDD_OFF, VCS_OFF
+    { BIT32(0),    BIT32(2)    }   //VDD_ON,  VCS_ON
+};
+
+const uint32_t HCD_CPMS_PFETCNTL[2] =
+{
+    0x200E0E28, //ECL2
+    0x200E0E20  //L3
+};
+
+// TODO: HW487147 - CLEAR and OR addresses switched
+// Needed be switched back once the hardware is fixed.
+const uint32_t HCD_CPMS_PFETCNTL_OR[2] =
+//const uint32_t HCD_CPMS_PFETCNTL_CLR[2] =
+{
+    0x200E0E2A, //ECL2
+    0x200E0E22  //L3
+};
+
+const uint32_t HCD_CPMS_PFETCNTL_CLR[2] =
+//const uint32_t HCD_CPMS_PFETCNTL_OR[2] =
+{
+    0x200E0E2B, //ECL2
+    0x200E0E23  //L3
+};
+// End HW487147
+
+const uint32_t HCD_CPMS_PFETSTAT[2] =
+{
+    0x200E0E2C, //ECL2
+    0x200E0E24  //L3
+};
+
+//------------------------------------------------------------------------------
+// Procedure: p10_hcd_corecache_power_control
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p10_hcd_corecache_power_control(
+    const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > & i_target,
+    uint32_t i_command)
+{
+    fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST > l_mc_or = i_target;//default OR
+    fapi2::buffer<uint64_t> l_data64          = 0;
+#ifndef PFET_SENSE_POLL_DISABLE
+    uint32_t                l_timeout         = 0;
+    uint32_t                l_pfet_senses     = 0;
+#endif
+    uint32_t                l_pfet_seq_states = 0;
+    uint32_t                l_isL3            = (i_command & HCD_PFET_L3_MASK)  >> 1;
+    uint32_t                l_isON            = (i_command & HCD_PFET_ON_MASK);
+    uint32_t                l_isVCS           = l_isON;
+
+    FAPI_INF(">>p10_hcd_corecache_power_control[%x]", i_command);
+
+    do
+    {
+        // l_isVCS is initialized to be same as l_isON value
+        // then it is fliped between VDD and VCS in every processing loop
+        // The goal is to only process two iterations of loops
+        // VDD then VCS or VCS then VDD depends on the ON/OFF operation
+        //
+        // Here is how this logic works
+        //           ON=1   OFF=0  while(On^Vcs)
+        // 1st flip: VDD=0  VCS=1  True  -> next loop
+        // 2nd flip: VCS=1  VDD=0  False -> terminate
+        l_isVCS = l_isVCS ^ 1;
+
+        FAPI_DBG("Make sure that we are not forcing PFET while the state isnt idle");
+        FAPI_TRY( getScom(l_mc_or, HCD_CPMS_PFETCNTL[l_isL3], l_data64 ) ); // use Multicast_OR to check 0s
+
+        DATA_EXTRACT(0, 4, l_pfet_seq_states);
+        FAPI_ASSERT((l_pfet_seq_states == 0),
+                    fapi2::CORECACHE_PFET_SEQ_STATE_ERROR()
+                    .set_PFET_SEQ_STATES(l_pfet_seq_states)
+                    .set_POW_COMMAND(l_isON)
+                    .set_POW_HEADERS(l_isVCS)
+                    .set_POW_DOMAINS(l_isL3)
+                    .set_CORE_TARGET(i_target),
+                    "PFET_SEQ_STATE not 0");
+
+        FAPI_DBG("Clear %s PFET stage select and value override bits via PFETCNTL[4,5/6,7/8]", l_isL3 ? "L3" : "ECL2" );
+        FAPI_TRY( putScom( i_target, HCD_CPMS_PFETCNTL_CLR[l_isL3], MASK_H32( HCD_PFET_OVERRIDES[l_isVCS] ) ) );
+
+        FAPI_DBG("Turn %s %s %s", l_isL3 ? "L3" : "ECL2", l_isVCS ? "VCS" : "VDD", l_isON ? "ON" : "OFF");
+        FAPI_TRY( putScom( i_target, HCD_CPMS_PFETCNTL_OR[l_isL3], MASK_H32( HCD_PFET_SEQ_STATES[l_isON][l_isVCS] ) ) );
+
+#ifndef PFET_SENSE_POLL_DISABLE
+        FAPI_DBG("Poll for PFET senses to be proper in PFETSTAT[]");
+        l_timeout = HCD_CORECACHE_POW_CTRL_POLL_TIMEOUT_HW_NS /
+                    HCD_CORECACHE_POW_CTRL_POLL_DELAY_HW_NS;
+
+        do
+        {
+            FAPI_TRY( getScom( i_target, HCD_CPMS_PFETSTAT[l_isL3], l_data64 ) );
+
+            DATA_H32R(l_pfet_senses);
+
+            //use multicastAND to check 1
+            if( l_pfet_senses & HCD_PFET_SENSE_BITS[l_isON][l_isVCS] )
+            {
+                break;
+            }
+
+            // Debug read
+            FAPI_TRY( getScom( i_target, HCD_CPMS_PFETCNTL[l_isL3], l_data64 ) );
+
+            fapi2::delay(HCD_CORECACHE_POW_CTRL_POLL_DELAY_HW_NS,
+                         HCD_CORECACHE_POW_CTRL_POLL_DELAY_SIM_CYCLE);
+        }
+        while( (--l_timeout) != 0 );
+
+        FAPI_ASSERT((l_timeout != 0),
+                    fapi2::CORECACHE_POW_CTRL_TIMEOUT()
+                    .set_POW_CTRL_POLL_TIMEOUT_HW_NS(HCD_CORECACHE_POW_CTRL_POLL_TIMEOUT_HW_NS)
+                    .set_PFET_SENSES(l_pfet_senses)
+                    .set_POW_COMMAND(l_isON)
+                    .set_POW_HEADERS(l_isVCS)
+                    .set_POW_DOMAINS(l_isL3)
+                    .set_CORE_TARGET(i_target),
+                    "Core/Cache PFET Control Timeout");
+#endif
+
+        FAPI_DBG("Reset PFET Sequencer State via PFETCNTL[0,1/2,3]");
+        FAPI_TRY( putScom( i_target, HCD_CPMS_PFETCNTL_CLR[l_isL3], MASK_H32( HCD_PFET_SEQ_STATES[1][l_isVCS] ) ) );
+
+        FAPI_DBG("Check PFET Sequencer State via PFETCNTL[0,1/2,3]");
+        FAPI_TRY( getScom( i_target, HCD_CPMS_PFETCNTL[l_isL3], l_data64 ) );
+
+        DATA_EXTRACT(0, 4, l_pfet_seq_states);
+        FAPI_DBG("Current PFET Sequencer State is %x, clear value is %x", l_pfet_seq_states, HCD_PFET_SEQ_STATES[1][l_isVCS]);
+
+    }
+    while( l_isON ^ l_isVCS );
+
+fapi_try_exit:
+
+    FAPI_INF("<<p10_hcd_corecache_power_control");
+
+    return fapi2::current_err;
+
+}
