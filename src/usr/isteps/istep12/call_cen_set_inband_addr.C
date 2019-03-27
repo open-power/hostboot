625a082c5e39460925c868a6ae27233fecc029dd
Christian Geddes <crgeddes@us.ibm.com>
2019-03-27 11:53:49 -0500

    Add p9a_omi_init and exp_omi_init calls to istep 12.13
    As per P9A IPL Flow document exp_omi_init followed by p9a_omi_init
    must be called during istep 12 prior to firmware update of explorer
    card.
    
    Change-Id: I00b51f075152d2120143661404755ec5791c1691
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/75097
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Matt Derksen <mderkse1@us.ibm.com>
    Reviewed-by: Glenn Miles <milesg@ibm.com>
    Reviewed-by: William G. Hoffa <wghoffa@us.ibm.com>

--- src/usr/isteps/istep12/call_cen_set_inband_addr.C
+++ src/usr/isteps/istep12/call_cen_set_inband_addr.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2018                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -47,6 +47,11 @@
 //HWP
 #include    <p9c_set_inband_addr.H>
 
+#ifdef CONFIG_AXONE
+#include    <exp_omi_init.H>
+#include    <p9a_omi_init.H>
+#endif
+
 //Inband SCOM
 #include    <ibscom/ibscomif.H>
 
@@ -62,50 +67,55 @@ void* call_cen_set_inband_addr (void *io_pArgs)
 {
     IStepError l_StepError;
     errlHndl_t l_err = NULL;
+    auto l_procModel = TARGETING::targetService().getProcessorModel();
 
-    TRACDCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_cen_set_inband_addr entry" );
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_cen_set_inband_addr entry" );
 
-    TARGETING::TargetHandleList l_procTargetList;
-    getAllChips(l_procTargetList, TYPE_PROC);
-
-    TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace, "call_cen_set_inband_addr: %d proc chips found",
-            l_procTargetList.size());
-
-    for (const auto & l_proc_target : l_procTargetList)
+    if(l_procModel == TARGETING::MODEL_CUMULUS)
     {
-        TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-            "p9c_set_inband_addr HWP target HUID %.8x",
-            TARGETING::get_huid(l_proc_target));
+        TARGETING::TargetHandleList l_procTargetList;
+        getAllChips(l_procTargetList, TYPE_PROC);
 
-        //  call the HWP with each target
-        fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> l_fapi_proc_target
-                (l_proc_target);
+        TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace, "call_cen_set_inband_addr: %d proc chips found",
+                l_procTargetList.size());
 
-        FAPI_INVOKE_HWP(l_err, p9c_set_inband_addr, l_fapi_proc_target);
-
-        //  process return code.
-        if ( l_err )
+        for (const auto & l_proc_target : l_procTargetList)
         {
+
             TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                "ERROR 0x%.8X:  p9c_set_inband_addr HWP on target HUID %.8x",
-                l_err->reasonCode(), TARGETING::get_huid(l_proc_target) );
+                "p9c_set_inband_addr HWP target HUID %.8x",
+                TARGETING::get_huid(l_proc_target));
 
-            // capture the target data in the elog
-            ErrlUserDetailsTarget(l_proc_target).addToLog( l_err );
+            //  call the HWP with each target
+            fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> l_fapi_proc_target
+                    (l_proc_target);
 
-            // Create IStep error log and cross reference to error that occurred
-            l_StepError.addErrorDetails( l_err );
+            FAPI_INVOKE_HWP(l_err, p9c_set_inband_addr, l_fapi_proc_target);
 
-            // Commit Error
-            errlCommit( l_err, ISTEP_COMP_ID );
+            //  process return code.
+            if ( l_err )
+            {
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                    "ERROR 0x%.8X:  p9c_set_inband_addr HWP on target HUID %.8x",
+                    l_err->reasonCode(), TARGETING::get_huid(l_proc_target) );
+
+                // capture the target data in the elog
+                ErrlUserDetailsTarget(l_proc_target).addToLog( l_err );
+
+                // Create IStep error log and cross reference to error that occurred
+                l_StepError.addErrorDetails( l_err );
+
+                // Commit Error
+                errlCommit( l_err, ISTEP_COMP_ID );
+
+            }
+            else
+            {
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                        "SUCCESS :  p9c_set_inband_addr HWP");
+            }
 
         }
-        else
-        {
-            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                     "SUCCESS :  p9c_set_inband_addr HWP");
-        }
-
     }
 
     // @todo RTC 187913 inband centaur scom in P9
@@ -115,6 +125,98 @@ void* call_cen_set_inband_addr (void *io_pArgs)
         //Now enable Inband SCOM for all membuf chips.
         IBSCOM::enableInbandScoms();
     }
+
+#ifdef CONFIG_AXONE
+    if(l_procModel == TARGETING::MODEL_AXONE)
+    {
+
+        TARGETING::TargetHandleList l_ocmbTargetList;
+        getAllChips(l_ocmbTargetList, TYPE_OCMB_CHIP);
+
+        TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace, "call_cen_set_inband_addr: %d ocmb chips found",
+                l_ocmbTargetList.size());
+
+        for (const auto & l_ocmb_target : l_ocmbTargetList)
+        {
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                "exp_omi_init HWP target HUID %.8x",
+                TARGETING::get_huid(l_ocmb_target));
+
+            //  call the HWP with each target
+            fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP> l_fapi_ocmb_target
+                    (l_ocmb_target);
+
+            FAPI_INVOKE_HWP(l_err, exp_omi_init , l_fapi_ocmb_target);
+
+            //  process return code.
+            if ( l_err )
+            {
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                    "ERROR 0x%.8X:  exp_omi_init HWP on target HUID 0x%.8x",
+                    l_err->reasonCode(), TARGETING::get_huid(l_ocmb_target) );
+
+                // capture the target data in the elog
+                ErrlUserDetailsTarget(l_ocmb_target).addToLog( l_err );
+
+                // Create IStep error log and cross reference to error that occurred
+                l_StepError.addErrorDetails( l_err );
+
+                // Commit Error
+                errlCommit( l_err, ISTEP_COMP_ID );
+
+            }
+            else
+            {
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                          "SUCCESS :  exp_omi_init HWP on target HUID 0x%.8x",
+                          TARGETING::get_huid(l_ocmb_target));
+            }
+
+        }
+
+        TARGETING::TargetHandleList l_mccTargetList;
+        getAllChiplets(l_mccTargetList, TYPE_MCC);
+
+        for (const auto & l_mcc_target : l_mccTargetList)
+        {
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                "p9a_omi_init HWP target HUID %.8x",
+                TARGETING::get_huid(l_mcc_target));
+
+            //  call the HWP with each target
+            fapi2::Target<fapi2::TARGET_TYPE_MCC> l_fapi_mcc_target
+                    (l_mcc_target);
+
+            FAPI_INVOKE_HWP(l_err, p9a_omi_init, l_fapi_mcc_target);
+
+            //  process return code.
+            if ( l_err )
+            {
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                    "ERROR 0x%.8X:  p9a_omi_init HWP on target HUID %.8x",
+                    l_err->reasonCode(), TARGETING::get_huid(l_mcc_target) );
+
+                // capture the target data in the elog
+                ErrlUserDetailsTarget(l_mcc_target).addToLog( l_err );
+
+                // Create IStep error log and cross reference to error that occurred
+                l_StepError.addErrorDetails( l_err );
+
+                // Commit Error
+                errlCommit( l_err, ISTEP_COMP_ID );
+
+            }
+            else
+            {
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                        "SUCCESS :  p9a_omi_init HWP on target HUID 0x%.8x",
+                          TARGETING::get_huid(l_mcc_target));
+            }
+
+        }
+    }
+#endif // CONFIG_AXONE
+
 
     TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_cen_set_inband_addr exit" );
 
