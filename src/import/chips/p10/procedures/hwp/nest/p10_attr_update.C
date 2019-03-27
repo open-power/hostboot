a7cd860881f7f7b1f94dfd02119df378434da804
Nicholas Landi <nlandi@ibm.com>
2019-03-27 13:45:15 -0400

    Create placeholder for attr_update procedure
    Change-Id: I3986dfa76083130d96e46be7d4e3977bde5a8e67
    RTC: 202053
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/75102
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Christopher M. Riedl <cmr@ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/81111
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/nest/p10_attr_update.C
@@ -0,0 +1,53 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/nest/p10_attr_update.C $  */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p10_attr_update.C
+/// @brief Stub HWP for FW to override attributes programmatically (FAPI2)
+///
+/// @author Joe McGill <jmcgill@us.ibm.com>
+///
+
+///
+/// *HW HW Maintainer: Nicholas Landi <nlandi@ibm.com>
+/// *HW FW Maintainer: Ilya Smirnov <ismirno@us.ibm.com>
+/// *HW Consumed by  : HB
+///
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <p10_attr_update.H>
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p10_attr_update(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    FAPI_DBG("Start");
+    FAPI_DBG("End");
+    return fapi2::FAPI2_RC_SUCCESS;
+}
