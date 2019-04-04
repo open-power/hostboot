e7cb511c12ec648942258df0a37701c7c9715bc8
Nicholas Landi <nlandi@ibm.com>
2019-04-04 16:26:52 -0400

    Implement p10_sbe_mcs_setup procedure
     - port to p10 (mcs removal)
     - update fbc_utils to include p10 specific base address accessor
     - stores register states in new attribute to be used in
       p10_revert_sbe_mcs_setup
     - stores which MC was written to in new attribute
     - add configuration for MCMODE2 (subchannel enable)
     - update to use new scom headers and accessors
    
    Change-Id: I6b95f48c018683d0cb1b101631ad20a1f6ed906e
    Original-Change-Id: Id3830bc7568d94d3180f4d30918fe01112d9b408
    RTC: 202057
    RTC: 208347
    RTC: 209749
    RTC: 209497
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/75621
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Jenny Huynh <jhuynh@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/83627
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>
    Tested-by: Nicholas E Bofferding <bofferdn@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/nest/p10_fbc_utils.C
+++ src/import/chips/p10/procedures/hwp/nest/p10_fbc_utils.C
@@ -40,17 +40,6 @@
 // FIXME @RTC206386 #include <p10_misc_scom_addresses_fld.H>
 
 //------------------------------------------------------------------------------
-// Constant definitions
-//------------------------------------------------------------------------------
-
-// FBC base address determination constants
-const uint8_t FABRIC_ADDR_SMF_BIT = 12;
-const uint8_t FABRIC_ADDR_MSEL_START_BIT = 13;
-const uint8_t FABRIC_ADDR_MSEL_END_BIT = 14;
-const uint8_t FABRIC_ADDR_TID_INDEX_START_BIT = 15;
-const uint8_t FABRIC_ADDR_TID_INDEX_END_BIT = 19;
-
-//------------------------------------------------------------------------------
 // Function definitions
 //------------------------------------------------------------------------------
 
@@ -120,6 +109,99 @@ fapi2::ReturnCode p10_fbc_utils_override_fbc_stop(
     l_pmisc_mode_data.setBit<PU_SND_MODE_REG_MANUAL_CLR_PB_STOP>();
     FAPI_TRY(fapi2::putScom(i_target, PU_SND_MODE_REG, l_pmisc_mode_data),
              "Error writing ADU PMisc Mode register to manually clear FBC stop control");
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
+
+fapi2::ReturnCode p10_fbc_utils_get_chip_base_address(
+    const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target,
+    const p10_fbc_utils_addr_mode i_addr_mode,
+    uint64_t& o_base_address_nm0,
+    uint64_t& o_base_address_nm1,
+    uint64_t& o_base_address_m,
+    uint64_t& o_base_address_mmio)
+{
+    fapi2::ATTR_PROC_FABRIC_TOPOLOGY_ID_Type l_fabric_topology_id = 0;
+    uint8_t l_fabric_topology_id_mask = 0x0;
+    fapi2::ATTR_PROC_FABRIC_TOPOLOGY_MODE_Type l_fabric_topology_mode;
+    fapi2::ATTR_MEM_MIRROR_PLACEMENT_POLICY_Type l_fabric_mirror_policy = 0;
+    fapi2::buffer<uint64_t> l_base_address;
+    const fapi2::Target<fapi2::TARGET_TYPE_SYSTEM> FAPI_SYSTEM;
+
+    FAPI_DBG("Start");
+
+    //Get Topology Mode and Mirror Policy
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_FABRIC_TOPOLOGY_MODE,
+                           FAPI_SYSTEM,
+                           l_fabric_topology_mode),
+             "Error from FAPI_ATTR_GET (ATTR_PROC_FABRIC_TOPOLOGY_MODE)");
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_MEM_MIRROR_PLACEMENT_POLICY,
+                           FAPI_SYSTEM,
+                           l_fabric_mirror_policy),
+             "Error from FAPI_ATTR_GET (ATTR_MEM_MIRROR_PLACEMENT_POLICY)");
+
+    //Determine which Topology ID to use (EFF vs HB)
+    if(i_addr_mode == HB_BOOT_ID)
+    {
+        l_fabric_topology_id_mask =
+            (l_fabric_topology_mode == fapi2::ENUM_ATTR_PROC_FABRIC_TOPOLOGY_MODE_MODE0)
+            ? 0x0E : 0x0C;
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_FABRIC_TOPOLOGY_ID,
+                               i_target,
+                               l_fabric_topology_id),
+                 "Error from FAPI_ATTR_GET (ATTR_PROC_FABRIC_TOPOLOGY_ID)");
+        l_fabric_topology_id &= l_fabric_topology_id_mask;//assume Chip field 0;
+    }
+    else//EFF_TOPOLOGY_ID
+    {
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_FABRIC_EFF_TOPOLOGY_ID,
+                               i_target,
+                               l_fabric_topology_id),
+                 "Error from FAPI_ATTR_GET (ATTR_PROC_FABRIC_EFF_TOPOLOGY_ID)");
+    }
+
+    //ensure that topology ID is in GGG_CC format
+    //avoids the instance where TOP_MODE=0 with chip ID 1 which
+    //then becomes GGG_10 (needs to be GGG_01)
+    if(l_fabric_topology_mode == fapi2::ENUM_ATTR_PROC_FABRIC_TOPOLOGY_MODE_MODE0)
+    {
+        l_fabric_topology_id = (((l_fabric_topology_id & 0x0E) << 1) //group ID GGG
+                                | (l_fabric_topology_id & 0x01));     //chip ID C
+    }
+
+
+    l_base_address.insertFromRight < FABRIC_ADDR_TOPO_ID_INDEX_START_BIT,
+                                   (FABRIC_ADDR_TOPO_ID_INDEX_END_BIT
+                                    - FABRIC_ADDR_TOPO_ID_INDEX_START_BIT
+                                    + 1 ) > (l_fabric_topology_id);
+
+    if(l_fabric_mirror_policy
+       == fapi2::ENUM_ATTR_MEM_MIRROR_PLACEMENT_POLICY_NORMAL)
+    {
+        //normal => nm = 0b00/0b01 ; m = 0b10 ; mmio = 0b11
+        o_base_address_nm0 = l_base_address();
+        l_base_address.setBit<FABRIC_ADDR_MSEL_END_BIT>();
+        o_base_address_nm1 = l_base_address();
+        l_base_address.setBit<FABRIC_ADDR_MSEL_START_BIT>();
+        l_base_address.clearBit<FABRIC_ADDR_MSEL_END_BIT>();
+        o_base_address_m = l_base_address();
+        l_base_address.setBit<FABRIC_ADDR_MSEL_END_BIT>();
+        o_base_address_mmio = l_base_address();
+    }
+    else
+    {
+        //mirrored start => m = 0b00 ; nm = 0b01/0b10 ; mmi0 = 0b11
+        o_base_address_m = l_base_address();
+        l_base_address.setBit<FABRIC_ADDR_MSEL_END_BIT>();
+        o_base_address_nm0 = l_base_address();
+        l_base_address.clearBit<FABRIC_ADDR_MSEL_END_BIT>();
+        l_base_address.setBit<FABRIC_ADDR_MSEL_START_BIT>();
+        o_base_address_nm1 = l_base_address();
+        l_base_address.setBit<FABRIC_ADDR_MSEL_END_BIT>();
+        o_base_address_mmio = l_base_address();
+    }
 
 fapi_try_exit:
     FAPI_DBG("End");
