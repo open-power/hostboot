cf78a44375d116bb1ad43ab71192d935298b0a3c
Ilya Smirnov <ismirno@us.ibm.com>
2019-04-08 09:53:09 -0500

    Flush Errlmanager Queue Before Fetching Gard Records
    When an error occurs or is injected into an EX target,
    the gard record for the target does not make it into
    the GUARD partition in time for the gard processing
    in istep 6.9 because the associated error log is sent
    asynchronously. This change flushes the errlmanager
    message queue right before the processing of the
    gard records to insure all gard records make it into
    GUARD partition.
    
    Change-Id: I06625672df7e926d6d080673241ad2ee4d87a7c9
    CQ: SW461565
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/75684
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Reviewed-by: Roland Veloz <rveloz@us.ibm.com>
    Reviewed-by: William G. Hoffa <wghoffa@us.ibm.com>

--- src/usr/hwas/common/deconfigGard.C
+++ src/usr/hwas/common/deconfigGard.C
@@ -43,6 +43,7 @@
 
 #ifdef __HOSTBOOT_MODULE
 #include <config.h>
+#include <errl/errlmanager.H>
 #if (!defined(CONFIG_CONSOLE_OUTPUT_TRACE) && defined(CONFIG_CONSOLE))
 #include <console/consoleif.H>
 #endif
@@ -551,6 +552,11 @@ errlHndl_t DeconfigGard::deconfigureTargetsFromGardRecordsForIpl(
             }
             break;
         }
+
+#ifdef __HOSTBOOT_MODULE
+        HWAS_INF("deconfigureTargetsFromGardRecordsForIpl: flushing the error log queue");
+        ErrlManager::callFlushErrorLogs();
+#endif
 
         // Get all GARD Records
         GardRecords_t l_gardRecords;
