e45c5702e5b7689d9fb86c4c88e415601b832b2b
Thi Tran <thi@us.ibm.com>
2019-04-12 15:52:49 -0400

    P10 - Unit test wrapper for ADU setup and access functions
    - Write test wrapper and tested on P9 system.
    - Fix bug in p10_adu_setup.C
    - Add error bit to catch invalid ADU flag arguments
    
    Change-Id: Id3e5a074ff2788c2cc241c21c3e6819d6cf4ba92
    Original-Change-Id: I3929f9c6d0ed2bba1a2e7ce5a8ec7b01aa5fc2c1
    RTC:208091
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/75939
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: Jenny Huynh <jhuynh@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/88394
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/nest/p10_putmemproc.C
+++ src/import/chips/p10/procedures/hwp/nest/p10_putmemproc.C
@@ -38,7 +38,7 @@
 #include <p10_putmemproc.H>
 #include <p10_adu_setup.H>
 #include <p10_adu_access.H>
-#include <p10_adu_utils.H>
+#include <p10_getputmemproc_utils.H>
 
 //------------------------------------------------------------------------------
 // Function definitions
@@ -57,139 +57,18 @@ fapi2::ReturnCode p10_putmemproc(
     uint64_t l_end_address = i_address + i_bytes;
     uint32_t l_granules_before_setup = 0;
     uint32_t l_granule = 0;
-    uint8_t l_data[1];
     bool l_first_access = true;
 
     FAPI_DBG("Start");
 
-    FAPI_ASSERT(((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_TARGET_PROC) == fapi2::SBE_MEM_ACCESS_FLAGS_TARGET_PROC) &&
-                ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_TARGET_PBA)  == 0) &&
-                ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_LCO_MODE) == 0) &&
-                ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_CACHE_INJECT_MODE) == 0),
-                fapi2::P10_PUTMEMPROC_INVALID_FLAGS().
-                set_TARGET(i_target).
-                set_ADDRESS(i_address).
-                set_BYTES(i_bytes).
-                set_FLAGS(i_mem_flags).
-                set_FLAG_CHECK_OP_TYPE(((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_TARGET_PROC) ==
-                                        fapi2::SBE_MEM_ACCESS_FLAGS_TARGET_PROC) &&
-                                       ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_TARGET_PBA)  == 0)).
-                set_FLAG_CHECK_LCO_MODE((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_LCO_MODE) == 0).
-                set_FLAG_CHECK_CACHE_INJECT_MODE((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_CACHE_INJECT_MODE) == 0),
-                "Invalid flag specified for ADU access");
+    // Validate flags
+    FAPI_TRY(p10_validateSbeMemoryAccessFlagsADU(i_target, i_address, i_bytes, i_mem_flags),
+             "Error returned from p10_validateSbeMemoryAccessFlagsADU()");
 
-    FAPI_ASSERT(((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_ECC_OVERRIDE) == 0) &&
-                ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_TAG) == 0) &&
-                ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_HOST_PASS_THROUGH) == 0),
-                fapi2::P10_PUTMEMPROC_UNSUPPORTED_FLAGS().
-                set_TARGET(i_target).
-                set_ADDRESS(i_address).
-                set_BYTES(i_bytes).
-                set_FLAGS(i_mem_flags).
-                set_FLAG_CHECK_ECC_OVERRIDE((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_ECC_OVERRIDE) == 0).
-                set_FLAG_CHECK_TAG((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_TAG) == 0).
-                set_FLAG_CHECK_HOST_PASS_THROUGH((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_HOST_PASS_THROUGH) == 0),
-                "Unsupported flag specified for ADU access");
+    FAPI_TRY(p10_getADUFlags(i_target, i_address, i_bytes, i_mem_flags, l_flags),
+             "Error returned from p10_getADUFlags()");
 
-    // set auto-increment
-    l_flags.setAutoIncrement((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_AUTO_INCR_ON) ==
-                             fapi2::SBE_MEM_ACCESS_FLAGS_AUTO_INCR_ON);
-
-    // set fast mode
-    l_flags.setFastMode((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_FAST_MODE_ON) ==
-                        fapi2::SBE_MEM_ACCESS_FLAGS_FAST_MODE_ON);
-
-    // set operation type and transaction size
-    if ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_CACHE_INHIBITED_MODE) ==
-        fapi2::SBE_MEM_ACCESS_FLAGS_CACHE_INHIBITED_MODE)
-    {
-        l_flags.setOperationType(adu_operationFlag::CACHE_INHIBIT);
-
-        if (i_bytes == 4)
-        {
-            l_flags.setTransactionSize(adu_operationFlag::TSIZE_4);
-        }
-        else if (i_bytes == 2)
-        {
-            l_flags.setTransactionSize(adu_operationFlag::TSIZE_2);
-        }
-        else if (i_bytes == 1)
-        {
-            l_flags.setTransactionSize(adu_operationFlag::TSIZE_1);
-        }
-        else if (i_bytes == 8)
-        {
-            l_flags.setTransactionSize(adu_operationFlag::TSIZE_8);
-        }
-        else
-        {
-            FAPI_ASSERT(false,
-                        fapi2::P10_PUTMEMPROC_INVALID_SIZE_CI().
-                        set_TARGET(i_target).
-                        set_ADDRESS(i_address).
-                        set_BYTES(i_bytes).
-                        set_FLAGS(i_mem_flags),
-                        "Invalid byte count specified for cache-inhibited access");
-        }
-    }
-    else if ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_PB_DIS_MODE) == fapi2::SBE_MEM_ACCESS_FLAGS_PB_DIS_MODE)
-    {
-        l_flags.setOperationType(adu_operationFlag::PB_DIS_OPER);
-    }
-    else if ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_SWITCH_MODE) == fapi2::SBE_MEM_ACCESS_FLAGS_SWITCH_MODE)
-    {
-        l_flags.setOperationType(adu_operationFlag::PMISC_OPER);
-    }
-    else if ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_PB_INIT_MODE) == fapi2::SBE_MEM_ACCESS_FLAGS_PB_INIT_MODE)
-    {
-        l_flags.setOperationType(adu_operationFlag::PB_INIT_OPER);
-    }
-    else if ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_PRE_SWITCH_CD_MODE) ==
-             fapi2::SBE_MEM_ACCESS_FLAGS_PRE_SWITCH_CD_MODE)
-    {
-        l_flags.setOperationType(adu_operationFlag::PRE_SWITCH_CD);
-    }
-    else if ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_PRE_SWITCH_AB_MODE) ==
-             fapi2::SBE_MEM_ACCESS_FLAGS_PRE_SWITCH_AB_MODE)
-    {
-        l_flags.setOperationType(adu_operationFlag::PRE_SWITCH_AB);
-    }
-    else if ((i_mem_flags & fapi2::SBE_MEM_ACCESS_FLAGS_POST_SWITCH_MODE) == fapi2::SBE_MEM_ACCESS_FLAGS_POST_SWITCH_MODE)
-    {
-        l_flags.setOperationType(adu_operationFlag::POST_SWITCH);
-    }
-    else
-    {
-        l_flags.setOperationType(adu_operationFlag::DMA_PARTIAL);
-
-        if (i_bytes == 4)
-        {
-            l_flags.setTransactionSize(adu_operationFlag::TSIZE_4);
-        }
-        else if (i_bytes == 2)
-        {
-            l_flags.setTransactionSize(adu_operationFlag::TSIZE_2);
-        }
-        else if (i_bytes == 1)
-        {
-            l_flags.setTransactionSize(adu_operationFlag::TSIZE_1);
-        }
-        else if ((i_bytes % 8) == 0)
-        {
-            l_flags.setTransactionSize(adu_operationFlag::TSIZE_8);
-        }
-        else
-        {
-            FAPI_ASSERT(false,
-                        fapi2::P10_PUTMEMPROC_INVALID_SIZE_DMA().
-                        set_TARGET(i_target).
-                        set_ADDRESS(i_address).
-                        set_BYTES(i_bytes).
-                        set_FLAGS(i_mem_flags),
-                        "Invalid byte count specified for DMA partial write access");
-        }
-    }
-
+    // Write data
     while (l_target_address < l_end_address)
     {
         // invoke ADU setup HWP to prepare current stream of contiguous granules
@@ -207,7 +86,18 @@ fapi2::ReturnCode p10_putmemproc(
         while (l_granules_before_setup && (l_target_address < l_end_address))
         {
             // invoke ADU access HWP to move one granule (8B)
-            l_data[0] = i_data[l_granule];
+            uint8_t l_data[8];
+            memset(l_data, 0, 8);
+
+            // Prepare data array for writing, 8 bytes at a time
+            // Depends on TSIZE, data in array needs to be right aligned.
+            uint8_t l_startIndex = l_target_address % 8;
+
+            for (uint32_t ii = 0; ii < l_flags.getTransactionSize(); ii++)
+            {
+                l_data[l_startIndex + ii] = i_data[( (l_granule * l_flags.getTransactionSize()) + ii)];
+            }
+
             FAPI_TRY(p10_adu_access(i_target,
                                     l_target_address,
                                     false,
@@ -221,7 +111,7 @@ fapi2::ReturnCode p10_putmemproc(
 
             l_first_access = false;
             l_granules_before_setup--;
-            l_target_address += 8;
+            l_target_address += l_flags.getTransactionSize();
             l_granule++;
         }
     }
