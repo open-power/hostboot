cd8e6a339185ff990d12bb1f0feb61bea5ef6f3d
Thi Tran <thi@us.ibm.com>
2019-04-16 07:24:42 -0500

    Port 'p9_mss_eff_grouping HWP to P10 (Part 1 of 2).
    - Port all p9_mss_eff_grouping HWP source code, unit test, and wrapper files.
    
    Change-Id: If62889f8cd728ecf6a5dac24e19807c0bc853aee
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/76012
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Jenny Huynh <jhuynh@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/81118
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/nest/p10_mss_eff_grouping.mk
@@ -0,0 +1,31 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: src/import/chips/p10/procedures/hwp/nest/p10_mss_eff_grouping.mk $
+#
+# OpenPOWER HostBoot Project
+#
+# Contributors Listed Below - COPYRIGHT 2019
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+# Include the macros and things for MSS procedures
+PROCEDURE=p10_mss_eff_grouping
+$(call ADD_MODULE_INCDIR,$(PROCEDURE),$(ROOTPATH)/chips/p10/procedures/hwp/memory/)
+# lib$(PROCEDURE)_DEPLIBS+=cen
+OBJS+=p10_fbc_utils.o
+$(call ADD_MODULE_INCDIR,$(PROCEDURE),$(ROOTPATH))
+$(call BUILD_PROCEDURE)
