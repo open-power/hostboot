0a0e889ae4d71044eb0eb6814b3dd23cf85b91ff
Mike Baiocchi <mbaiocch@us.ibm.com>
2019-04-19 08:49:20 -0500

    Combine 'generic' and 'explorer' OCMB chip targets
    This commit moves the Explorer-specific OCMB_CHIP target into the
    'generic' OCMB_CHIP target so that there is only one target.  This
    target will also be used for Gemini.  Changes were also made to look
    for Gemini vs Explorer where appropriate based on ATTR_CHIP_ID.
    
    Change-Id: I91b79195bf997a6af4e2ae0a3326ed5a1c7887ec
    RTC:205563
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/78390
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Zane C. Shelley <zshelle@us.ibm.com>

--- src/usr/fapi2/fapi2.mk
+++ src/usr/fapi2/fapi2.mk
@@ -59,6 +59,7 @@ EXTRAINCDIR += ${HWP_PATH_2}/hwp/memory/lib/shared/
 EXTRAINCDIR += ${HWP_PATH_2}/hwp/memory/lib/utils/
 EXTRAINCDIR += ${HWP_PATH_2}/vpd_accessors/
 EXTRAINCDIR += ${ROOTPATH}/src/usr/scom/
+EXTRAINCDIR += ${ROOTPATH}/src/import/chips/common/utils/
 
 include ${ROOTPATH}/src/build/mkrules/verbose.rules.mk
 define __CLEAN_TARGET
