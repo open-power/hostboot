b632f2cf6a4c7c522954bb6c226b8e0c66eab0c0
Christian Geddes <crgeddes@us.ibm.com>
2019-04-23 10:37:21 -0500

    Update simbuild to latest simics release
    Simics release updates include new VPD, omi io dc calibration
    action and .chip workarounds, and it correctly sets the IDEC
    value for the OCMB in the explorer.por file.
    
    Change-Id: I038b7e4839c83937d48888faee1c1dd453b4e73d
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/76393
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Matt Derksen <mderkse1@us.ibm.com>
    Reviewed-by: Matthew Raybuck <matthew.raybuck@ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/build/citest/cxxtest-start.sh
+++ src/build/citest/cxxtest-start.sh
@@ -38,7 +38,7 @@ if [ "$MACHINE" != "NIMBUS" ] && [ "$MACHINE" != "CUMULUS" ] && \
    [ "$MACHINE" != "CUMULUS_CDIMM" ] && [ "$MACHINE" != "FSPBUILD" ];
 then
     export PATH=$PATH:$SANDBOXBASE/simics/
-    export START_SIMICS_CMD="runsim -m $MACHINE hb_script_to_run=$SANDBOXBASE/obj/ppc/simu/scripts/hbfw/startup.simics pnor_img=$SANDBOXBASE/obj/ppc/hbfw/img/axone.pnor sbe_seeprom_img=$SANDBOXBASE/images/ppc/lab/flash/sbe_seeprom_p9a_10.bin.ecc num_procs=1 vpd_proc=vpd/images/99a8c3fe4e5c74798f5bd4212f3d9a2a"
+    export START_SIMICS_CMD="runsim -m $MACHINE hb_script_to_run=$SANDBOXBASE/obj/ppc/simu/scripts/hbfw/startup.simics pnor_img=$SANDBOXBASE/obj/ppc/hbfw/img/axone.pnor sbe_seeprom_img=$SANDBOXBASE/images/ppc/lab/flash/sbe_seeprom_p9a_10.bin.ecc num_procs=1 vpd_proc=vpd/images/11a4199cd205aaad34566b1832e3ef6a"
 fi
 
 #   Front end to autocitest - script to execute unit tests under simics.
