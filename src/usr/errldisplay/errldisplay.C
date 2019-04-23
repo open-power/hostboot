0f03814d62a6693df5b0c3118662e00a5b79b24a
Nick Bofferding <bofferdn@us.ibm.com>
2019-04-23 08:46:10 -0500

    Only display ERRL user details callouts/strings when dumping error to console
    This commit fixes a bug wherein, when Hostboot dumped an error log to the
    console that contained user details with the same subsection ID as an ERRL
    string but for a different component ID, the information, which was often in
    binary format, would appear garbled, and would additionally terminate the BMC's
    SOL session due to invalid characters.  The change narrows the scope of what
    gets emitted to just ERRL component strings/callouts.
    
    Change-Id: Ifbe83448a56955d44f0e775e123ffb95a6330b8a
    CQ: SW463437
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/76381
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/errldisplay/errldisplay.C
+++ src/usr/errldisplay/errldisplay.C
@@ -709,7 +709,7 @@ void ErrLogDisplay::msgDisplay (const errlHndl_t &i_err,
                 displayHwpf( user_data->iv_pData, user_data->iv_Size,
                              user_data->iv_header.iv_sst);
             }
-            else
+            else if(user_data->iv_header.iv_compId == ERRL_COMP_ID)
             {
                 switch ( user_data->iv_header.iv_sst )
                 {
