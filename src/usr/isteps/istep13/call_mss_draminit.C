6c39a01069eceea95e0637d5d943aa80f8b3e0a0
Matt Derksen <mderkse1@us.ibm.com>
2019-04-25 13:21:38 -0500

    Compile Gemini hardware procedures in hostboot
    This is just the start of importing/compiling
    the Gemini hardware procedures.
    This commit compiles the helper functions in mss library,
    and then compiles/calls gem_draminit in istep 13.
    
    Change-Id: I837a5d8507c882c41650d06bccfcf25cd11688c8
    RTC:204647
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/76522
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Christian R. Geddes <crgeddes@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Glenn Miles <milesg@ibm.com>
    Reviewed-by: Daniel M. Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep13/call_mss_draminit.C
+++ src/usr/isteps/istep13/call_mss_draminit.C
@@ -47,13 +47,21 @@
 
 //From Import Directory (EKB Repository)
 #include  <fapi2.H>
-#include  <p9_mss_draminit.H>
-#include  <p9c_mss_draminit.H>
+#include  <config.h>
+#ifndef CONFIG_AXONE
+    #include  <p9_mss_draminit.H>
+    #include  <p9c_mss_draminit.H>
+#else
+#include <chipids.H>
+// @todo RTC 207856   #include  <exp_draminit.H>
+    #include  <gem_draminit.H>
+#endif
 
 #ifdef CONFIG_NVDIMM
 // NVDIMM support
 #include    <isteps/nvdimm/nvdimm.H>
 #endif
+
 
 using namespace ERRORLOG;
 using namespace ISTEP;
@@ -62,8 +70,49 @@ using namespace TARGETING;
 
 namespace ISTEP_13
 {
+// Declare local functions
+void nimbus_mss_draminit(IStepError & io_istepError);
+void cumulus_mss_draminit(IStepError & io_istepError);
+void axone_mss_draminit(IStepError & io_istepError);
+void mss_post_draminit( IStepError & io_stepError );
 
-void   mss_post_draminit( IStepError & io_stepError )
+void* call_mss_draminit (void *io_pArgs)
+{
+    IStepError l_stepError;
+
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_mss_draminit entry" );
+    auto l_procModel = TARGETING::targetService().getProcessorModel();
+
+    switch (l_procModel)
+    {
+        case TARGETING::MODEL_CUMULUS:
+            cumulus_mss_draminit(l_stepError);
+            break;
+        case TARGETING::MODEL_AXONE:
+            axone_mss_draminit(l_stepError);
+            break;
+        case TARGETING::MODEL_NIMBUS:
+            nimbus_mss_draminit(l_stepError);
+            break;
+        default:
+            assert(0, "call_mss_draminit: Unsupported model type 0x%04X",
+                l_procModel);
+            break;
+    }
+
+    // call POST_DRAM_INIT function, if nothing failed above
+    if( INITSERVICE::spBaseServicesEnabled() &&
+        (l_stepError.getErrorHandle() == nullptr) )
+    {
+        mss_post_draminit(l_stepError);
+    }
+
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_mss_draminit exit" );
+
+    return l_stepError.getErrorHandle();
+}
+
+void mss_post_draminit( IStepError & io_stepError )
 {
     errlHndl_t l_err = NULL;
     bool rerun_vddr = false;
@@ -144,13 +193,10 @@ void   mss_post_draminit( IStepError & io_stepError )
     return;
 }
 
-void* call_mss_draminit (void *io_pArgs)
+#ifndef CONFIG_AXONE
+void nimbus_mss_draminit(IStepError & io_istepError)
 {
     errlHndl_t l_err = NULL;
-
-    IStepError l_stepError;
-
-    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_mss_draminit entry" );
 
     // Get all MCBIST targets
     TARGETING::TargetHandleList l_mcbistTargetList;
@@ -191,12 +237,12 @@ void* call_mss_draminit (void *io_pArgs)
             ErrlUserDetailsTarget(l_mcbist_target).addToLog(l_err);
 
             // Create IStep error log and cross reference to error that occurred
-            l_stepError.addErrorDetails( l_err );
-
-            break;
+            io_istepError.addErrorDetails( l_err );
 
             // Commit Error
             errlCommit( l_err, HWPF_COMP_ID );
+
+            break;
         }
         else
         {
@@ -206,85 +252,147 @@ void* call_mss_draminit (void *io_pArgs)
         }
 
     }   // endfor   mcbist's
+}
 
 
+void cumulus_mss_draminit(IStepError & io_istepError)
+{
+    errlHndl_t l_err = NULL;
 
-    if(l_stepError.getErrorHandle() == NULL)
+    // Get all Centaur targets
+    TARGETING::TargetHandleList l_membufTargetList;
+    getAllChips(l_membufTargetList, TYPE_MEMBUF);
+
+    for (const auto & l_membufTarget : l_membufTargetList )
     {
-        // Get all Centaur targets
-        TARGETING::TargetHandleList l_membufTargetList;
-        getAllChips(l_membufTargetList, TYPE_MEMBUF);
+        TARGETING::TargetHandleList l_mbaTargetList;
+        getChildChiplets(l_mbaTargetList, l_membufTarget, TYPE_MBA);
 
-        for (TargetHandleList::const_iterator
-             l_membuf_iter = l_membufTargetList.begin();
-             l_membuf_iter != l_membufTargetList.end();
-             ++l_membuf_iter)
+        for (const auto & l_mbaTarget : l_mbaTargetList )
         {
-            //  make a local copy of the target for ease of use
-            TARGETING::Target* l_pCentaur = *l_membuf_iter;
+            // Dump current run on target
+             TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                "Running p9c_mss_draminit HWP on "
+                "target HUID %.8X", TARGETING::get_huid(l_mbaTarget));
 
-            TARGETING::TargetHandleList l_mbaTargetList;
-            getChildChiplets(l_mbaTargetList,
-                            l_pCentaur,
-                            TYPE_MBA);
+            //  call the HWP with each target
+            fapi2::Target <fapi2::TARGET_TYPE_MBA_CHIPLET> l_fapi_mba_target(l_mbaTarget);
 
-            for (TargetHandleList::const_iterator
-                 l_mba_iter = l_mbaTargetList.begin();
-                 l_mba_iter != l_mbaTargetList.end();
-                 ++l_mba_iter)
-             {
-                //  Make a local copy of the target for ease of use
-                TARGETING::Target*  l_mbaTarget = *l_mba_iter;
+            FAPI_INVOKE_HWP(l_err, p9c_mss_draminit, l_fapi_mba_target);
 
-                // Dump current run on target
-                 TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                    "Running p9c_mss_draminit HWP on "
-                    "target HUID %.8X", TARGETING::get_huid(l_mbaTarget));
+            //  process return code.
+            if ( l_err )
+            {
+                TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                    "ERROR 0x%.8X : p9c_mss_draminit HWP returns error",
+                    l_err->reasonCode());
 
-                //  call the HWP with each target
-                fapi2::Target <fapi2::TARGET_TYPE_MBA_CHIPLET> l_fapi_mba_target(l_mbaTarget);
+                // capture the target data in the elog
+                ErrlUserDetailsTarget(l_mbaTarget).addToLog(l_err);
 
-                FAPI_INVOKE_HWP(l_err, p9c_mss_draminit, l_fapi_mba_target);
+                // Create IStep error log and cross reference to error that occurred
+                io_istepError.addErrorDetails( l_err );
 
-                //  process return code.
-                if ( l_err )
-                {
-                    TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-                        "ERROR 0x%.8X : p9c_mss_draminit HWP returns error",
-                        l_err->reasonCode());
+                // Commit Error
+                errlCommit( l_err, HWPF_COMP_ID );
 
-                    // capture the target data in the elog
-                    ErrlUserDetailsTarget(l_mbaTarget).addToLog(l_err);
+                break;
+            }
+            else
+            {
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                       "SUCCESS running p9c_mss_draminit HWP on "
+                       "target HUID %.8X", TARGETING::get_huid(l_mbaTarget));
+            }
+         } // end MBA loop
+    } // end MEMBUF loop
+}
 
-                    // Create IStep error log and cross reference to error that occurred
-                    l_stepError.addErrorDetails( l_err );
+#else
+void nimbus_mss_draminit(IStepError & io_istepError)
+{
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+              "Error: Trying to call 'p9_mss_draminit' but Nimbus code is not compiled in");
+    assert(0, "Calling wrong Model's HWPs");
+}
 
-                    // Commit Error
-                    errlCommit( l_err, HWPF_COMP_ID );
+void cumulus_mss_draminit(IStepError & io_istepError)
+{
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+              "Error: Trying to call 'p9c_mss_draminit' but Cumulus code is not compiled in");
+    assert(0, "Calling wrong Model's HWPs");
+}
+#endif
 
-                    break;
-                }
-                else
-                {
-                    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                           "SUCCESS running p9c_mss_draminit HWP on "
-                           "target HUID %.8X", TARGETING::get_huid(l_mbaTarget));
-                }
+#ifdef CONFIG_AXONE
+void axone_mss_draminit(IStepError & io_istepError)
+{
+    errlHndl_t l_err = NULL;
 
-             }
+    // Get all OCMB targets
+    TARGETING::TargetHandleList l_ocmbTargetList;
+    getAllChips(l_ocmbTargetList, TYPE_OCMB_CHIP);
+
+    bool isGeminiChip = false;
+    for ( const auto & l_ocmb : l_ocmbTargetList )
+    {
+        fapi2::Target <fapi2::TARGET_TYPE_OCMB_CHIP> l_fapi_ocmb_target(l_ocmb);
+
+        // check EXPLORER first as this is most likely the configuration
+        uint32_t chipId = l_ocmb->getAttr< TARGETING::ATTR_CHIP_ID>();
+        if (chipId == POWER_CHIPID::EXPLORER_16)
+        {
+            isGeminiChip = false;
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                "Running exp_draminit HWP on target HUID 0x%.8X",
+                TARGETING::get_huid(l_ocmb) );
+            //@todo RTC 207856: FAPI_INVOKE_HWP(l_err, exp_draminit, l_fapi_ocmb_target);
+        }
+        else
+        {
+            isGeminiChip = true;
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                "Running gem_draminit HWP on target HUID 0x%.8X, chipId 0x%.4X",
+                TARGETING::get_huid(l_ocmb), chipId );
+            FAPI_INVOKE_HWP(l_err, gem_draminit, l_fapi_ocmb_target);
         }
 
-    }
+        //  process return code.
+        if ( l_err )
+        {
+            TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                "ERROR 0x%.8X : %s_draminit HWP returned error",
+                l_err->reasonCode(), isGeminiChip?"gem":"exp");
 
-    // call POST_DRAM_INIT function
-    if(INITSERVICE::spBaseServicesEnabled())
-    {
-        mss_post_draminit(l_stepError);
-    }
+            // capture the target data in the elog
+            ErrlUserDetailsTarget(l_ocmb).addToLog(l_err);
 
-    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, "call_mss_draminit exit" );
+            // Create IStep error log and cross reference to error that occurred
+            io_istepError.addErrorDetails( l_err );
 
-    return l_stepError.getErrorHandle();
+            // Commit Error
+            errlCommit( l_err, HWPF_COMP_ID );
+
+            break;
+        }
+        else
+        {
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+                "SUCCESS running %s_draminit HWP on target HUID 0x%.8X",
+                isGeminiChip?"gem":"exp", TARGETING::get_huid(l_ocmb) );
+        }
+    } // end of OCMB loop
 }
+
+#else
+
+void axone_mss_draminit(IStepError & io_istepError)
+{
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+              "Error: Trying to call 'exp_draminit' or 'gem_draminit' but Axone code is not compiled in");
+    assert(0, "Calling wrong Model's HWPs");
+}
+
+#endif
 
 };
