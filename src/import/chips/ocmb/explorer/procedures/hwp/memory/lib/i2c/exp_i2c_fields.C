f2c6b655ac192abf64ccd1188cb3e13dcb875dfd
Andre A. Marin <aamarin@us.ibm.com>
2019-05-30 09:45:54 -0500

    Add snapshot of ocmb/explorer for master-p10 branch
    Had to fix mss incorrect use of p9 attribute enums
    used in generic.  Made a list of fixes to fix in
    ekb/master.
    
    Sets #5-7: Fix Jenkins build failure.
    
    Change-Id: I18d05f89a5b741da7150ad3a6d139cd84f915a87
    Original-Change-Id: I556a2e117d4dab2276ecd122650d253782c52e52
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/78083
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>

--- src/import/chips/ocmb/explorer/procedures/hwp/memory/lib/i2c/exp_i2c_fields.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/lib/i2c/exp_i2c_fields.C
@@ -23,6 +23,7 @@
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
 #include <lib/i2c/exp_i2c_fields.H>
+#include <generic/memory/lib/utils/find.H>
 
 namespace mss
 {
@@ -33,7 +34,7 @@ namespace i2c
 
 // If a constexpr static data member (since C++11) is odr-used,
 // a definition at namespace scope is still required, but it cannot have an initializer.
-constexpr mss::field_t<mss::endian::BIG> fields::BOOT_MODE;
+constexpr mss::field_t<mss::endian::BIG> fields::DFE_DISABLE;
 constexpr mss::field_t<mss::endian::BIG> fields::LANE_MODE;
 constexpr mss::field_t<mss::endian::BIG> fields::SERDES_FREQ;
 constexpr mss::field_t<mss::endian::BIG> fields::FW_MODE;
@@ -43,6 +44,49 @@ constexpr mss::field_t<mss::endian::BIG> fields::DL_LAYER_BOOT_MODE;
 constexpr mss::field_t<mss::endian::BIG> fields::CMD_ID;
 constexpr mss::field_t<mss::endian::BIG> fields::STATUS_CODE;
 constexpr mss::field_t<mss::endian::BIG> fields::BOOT_STAGE;
+
+namespace boot_cfg
+{
+
+///
+/// @brief SERDES_FREQ setter
+/// @param[in] i_target the OCMB target
+/// @param[in,out] io_data the buffer as a reference to a vector
+/// @param[in] i_freq frequency to set
+/// @return FAPI2_RC_SUCCESS iff okay
+///
+fapi2::ReturnCode set_serdes_freq(const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target,
+                                  std::vector<uint8_t>& io_data,
+                                  const uint32_t i_freq)
+{
+    static const std::vector< std::pair<uint32_t, uint8_t> > OMI_FREQ_MAP =
+    {
+        {fapi2::ENUM_ATTR_FREQ_OMI_MHZ_21330, 1},
+        {fapi2::ENUM_ATTR_FREQ_OMI_MHZ_23460, 2},
+        {fapi2::ENUM_ATTR_FREQ_OMI_MHZ_25600, 3},
+        // All others reserved or not supported
+    };
+
+    uint8_t l_setting = 0;
+    const bool l_is_val_found = mss::find_value_from_key(OMI_FREQ_MAP, i_freq, l_setting);
+
+    FAPI_ASSERT( l_is_val_found,
+                 fapi2::MSS_LOOKUP_FAILED()
+                 .set_KEY(i_freq)
+                 .set_DATA(l_setting)
+                 .set_FUNCTION(SET_SERDES_FREQ)
+                 .set_TARGET(i_target),
+                 "Failed to find a BOOT_CONFIG setting for OMI value %d on %s",
+                 i_freq,
+                 mss::c_str(i_target) );
+
+    return set_field<fields::SERDES_FREQ>(i_target, io_data, l_setting);
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+}// boot_cfg
 
 }// i2c
 }// exp
