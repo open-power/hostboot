de9dd1d1be34e4af142f206ef970fb36f7e9c936
Andre A. Marin <aamarin@us.ibm.com>
2019-05-30 09:45:54 -0500

    Add snapshot of ocmb/explorer for master-p10 branch
    Had to fix mss incorrect use of p9 attribute enums
    used in generic.  Made a list of fixes to fix in
    ekb/master.
    
    Sets #5-7: Fix Jenkins build failure.
    
    Change-Id: Ib5b7f0beb5a050fd4944088733a1518ca885fcaf
    Original-Change-Id: I556a2e117d4dab2276ecd122650d253782c52e52
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/78083
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>

--- src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_draminit_mc.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_draminit_mc.C
@@ -22,3 +22,83 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file exp_mss_draminit_mc.C
+/// @brief Initialize the memory controller to take over the DRAM
+///
+// *HWP HWP Owner: Louis Stermole <stermole@us.ibm.com>
+// *HWP HWP Backup: Andre Marin <aamarin@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 3
+// *HWP Consumed by: FSP:HB
+
+#include <fapi2.H>
+
+#include <lib/shared/exp_defaults.H>
+#include <generic/memory/lib/utils/c_str.H>
+#include <generic/memory/lib/utils/find.H>
+#include <generic/memory/lib/utils/count_dimm.H>
+
+#include <lib/mc/exp_port.H>
+#include <generic/memory/mss_git_data_helper.H>
+
+extern "C"
+{
+///
+/// @brief Initialize the MC now that DRAM is up
+/// @param[in] i_target, the MC of the ports
+/// @return FAPI2_RC_SUCCESS iff ok
+///
+    fapi2::ReturnCode exp_draminit_mc( const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target )
+    {
+        mss::display_git_commit_info("exp_draminit_mc");
+
+
+        FAPI_INF("%s Start exp_draminit MC", mss::c_str(i_target));
+
+        //skip this ocmb_chip if we have no DIMM's configured
+        if(mss::count_dimm(i_target) == 0)
+        {
+            FAPI_INF("No DIMM's configured on %s. Skipping this OCMB_CHIP.", mss::c_str(i_target));
+            return fapi2::FAPI2_RC_SUCCESS;
+        }
+
+        // Enable Power management based off of mrw_power_control_requested
+        FAPI_TRY( mss::enable_power_management(i_target), "%s Failed to enable power management",
+                  mss::c_str(i_target) );
+
+        // Set the IML Complete bit. Steve Powell to find a bit in the SRQ to use for this purpose
+        FAPI_TRY( mss::change_iml_complete(i_target, mss::HIGH), "%s Failed to set_ipm_complete",
+                  mss::c_str(i_target));
+
+        // Set DFI init start requested from Stephen Powell
+        FAPI_TRY( mss::change_dfi_init_start(i_target, mss::ON ), "%s Failed to change_dfi_init_start",
+                  mss::c_str(i_target));
+
+        // Re-enable port fails.
+        FAPI_TRY( mss::change_port_fail_disable(i_target, mss::OFF ),
+                  "%s Failed to change_port_fail_disable", mss::c_str(i_target) );
+
+        // Start the refresh engines by setting MBAREF0Q(0) = “1”. Note that the remaining bits in
+        // MBAREF0Q should retain their initialization values.
+        FAPI_TRY( mss::change_refresh_enable(i_target, mss::HIGH), "%s Failed change_refresh_enable",
+                  mss::c_str(i_target) );
+
+        // Enable periodic short zq cal
+        FAPI_TRY( mss::enable_zq_cal(i_target), "%s Failed enable_zq_cal", mss::c_str(i_target) );
+
+        // Enable ecc checking
+        FAPI_TRY( mss::enable_read_ecc(i_target), "%s Failed enable_read_ecc", mss::c_str(i_target) );
+
+        // Apply marks from OCMB VPD
+        FAPI_TRY( mss::apply_mark_store(i_target), "%s Failed enable_read_ecc", mss::c_str(i_target) );
+
+        // TODO: Move mss::unmask::after_draminit_mc to generic and call it
+        // At this point the DDR interface must be monitored for memory errors. Memory related FIRs should be unmasked.
+        //FAPI_TRY( mss::unmask::after_draminit_mc(i_target), "%s Failed after_draminit_mc", mss::c_str(i_target) );
+
+    fapi_try_exit:
+        FAPI_INF("%s End exp_draminit MC", mss::c_str(i_target));
+        return fapi2::current_err;
+    }
+}
