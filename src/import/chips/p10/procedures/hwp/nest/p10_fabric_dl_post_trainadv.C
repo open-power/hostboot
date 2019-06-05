7719f5c994f74484497478aa59fa621c99293395
Joe McGill <jmcgill@us.ibm.com>
2019-06-05 18:15:00 -0400

    p10_fabric_dl_pre_trainadv, p10_fabric_dl_post_trainadv
    HWPs stubs to permit overrides for pre,post training workarounds
    or IO characterization hooks
    
    Change-Id: If0b554114c5a26ece33e8f3a39b4673e6a47b4b4
    RTC: 202114
    RTC: 202116
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/78417
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Brent Wieman <bwieman@us.ibm.com>
    Dev-Ready: Brent Wieman <bwieman@us.ibm.com>
    Reviewed-by: Jenny Huynh <jhuynh@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/81123
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/nest/p10_fabric_dl_post_trainadv.C
@@ -0,0 +1,55 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/nest/p10_fabric_dl_post_trainadv.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p10_fabric_dl_post_trainadv.C
+/// @brief Stub HWP for post-training overrides/IO characterization (FAPI2)
+///
+/// @author Joe McGill <jmcgill@us.ibm.com>
+///
+
+///
+/// *HW HW Maintainer: Joe McGill <jmcgill@us.ibm.com>
+/// *HW FW Maintainer: Ilya Smirnov <ismirno@us.ibm.com>
+/// *HW Consumed by  : HB
+///
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <p10_fabric_dl_post_trainadv.H>
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p10_fabric_dl_post_trainadv(
+    const fapi2::Target<fapi2::TARGET_TYPE_IOHS>& i_targetA,
+    const fapi2::Target<fapi2::TARGET_TYPE_IOHS>& i_targetB)
+{
+    FAPI_DBG("Start");
+    FAPI_DBG("End");
+    return fapi2::FAPI2_RC_SUCCESS;
+}
