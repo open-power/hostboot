c597bcdbefd2e8c7c35d265d34bbeccb7ef1016d
Mark Pizzutillo <Mark.Pizzutillo@ibm.com>
2019-07-10 17:23:23 -0500

    Add PRBS training sequence to exp_omi_setup
    P10 port. Removed p9a_omi_setup and lib changes
    since they are being reimplemented for p10,
    mss team doesn't own them.
    Changes not in original commit: makefile update,
    and add of mss_file_io.H needed for UTs.
    
    Change-Id: I390dfb23476da9fe2054c7825fef760a8aca258f
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/83548
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Mark Pizzutillo <mark.pizzutillo@ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/83555
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>

--- src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_omi_setup.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_omi_setup.C
@@ -37,8 +37,11 @@
 #include <generic/memory/lib/utils/c_str.H>
 #include <lib/exp_attribute_accessors_manual.H>
 #include <lib/omi/exp_omi_utils.H>
+#include <lib/workarounds/exp_omi_workarounds.H>
 #include <lib/i2c/exp_i2c.H>
 #include <generic/memory/mss_git_data_helper.H>
+#include <generic/memory/lib/mss_generic_attribute_getters.H>
+#include <generic/memory/lib/mss_generic_system_attribute_getters.H>
 
 extern "C"
 {
@@ -54,8 +57,11 @@ extern "C"
 
         // Declares variables
         fapi2::buffer<uint64_t> l_data;
+        fapi2::buffer<uint64_t> dlx_config1_data;
+        uint8_t l_edpl_disable = 0;
         bool l_is_enterprise = false;
         bool l_is_half_dimm = false;
+        bool l_workaround_required = false;
         std::vector<uint8_t> l_boot_config_data;
         uint8_t l_dl_layer_boot_mode = fapi2::ENUM_ATTR_MSS_OCMB_EXP_BOOT_CONFIG_DL_LAYER_BOOT_MODE_NON_DL_TRAINING;
 
@@ -72,6 +78,7 @@ extern "C"
         // Gets the configuration information from attributes
         FAPI_TRY(mss::enterprise_mode(i_target, l_is_enterprise));
         FAPI_TRY(mss::half_dimm_mode(i_target, l_is_half_dimm));
+        FAPI_TRY(mss::attr::get_mss_omi_edpl_disable(l_edpl_disable));
 
         // Prints out the data
         FAPI_INF("%s is %s enterprise mode, and %s-DIMM mode", mss::c_str(i_target), l_is_enterprise ? "" : "non",
@@ -87,6 +94,25 @@ extern "C"
         // Checks that the chip is configured correctly
         FAPI_TRY(mss::exp::omi::read_enterprise_config(i_target, l_data));
         FAPI_TRY(mss::exp::omi::check_enterprise_mode(i_target, l_is_enterprise, l_data));
+
+        // Set the EDPL according the attribute
+        FAPI_TRY(mss::exp::omi::read_dlx_config1(i_target, dlx_config1_data));
+        mss::exp::omi::set_edpl_enable_bit(dlx_config1_data, l_edpl_disable);
+        FAPI_TRY(mss::exp::omi::write_dlx_config1(i_target, dlx_config1_data));
+        FAPI_INF("%s EDPL enable: ", mss::c_str(i_target), l_edpl_disable ? "false" : "true");
+
+        // Run the workaround if it's needed
+        FAPI_TRY(mss::exp::workarounds::omi::is_prbs_ocmb_required(i_target, l_workaround_required));
+
+        if (l_workaround_required)
+        {
+            uint8_t l_dl_x4_backoff_en = 0;
+
+            FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_EC_FEATURE_OMI_DL_X4_BACKOFF_ENABLE, i_target, l_dl_x4_backoff_en),
+                     "Error getting ATTR_CHIP_EC_FEATURE_OMI_DL_X4_BACKOFF_ENABLE");
+
+            FAPI_TRY(mss::exp::workarounds::omi::prbs_ocmb(i_target, l_dl_x4_backoff_en));
+        }
 
     fapi_try_exit:
         return fapi2::current_err;
