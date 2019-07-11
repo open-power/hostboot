033024ffb787a4d5c2cf83ffcc78fe64a1d1415d
Ben Gass <bgass@us.ibm.com>
2019-07-11 19:13:18 -0400

    ridi enable updates
    - add p10_enable_ridi function to p10_perv_sbe_cmn
    - add p10_chiplet_enable_ridi to enable MC/PCIe
    - add p10_iosh_enable_ridi to enable IOHS
    - update p10_sbe_nest_enable_ridi to use p10_enable_ridi
    
    Change-Id: Ie9eb42a7bc28d334dfc4795d1eead76e0625729a
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/80321
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/81131
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/perv/p10_chiplet_enable_ridi.C
@@ -0,0 +1,63 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/perv/p10_chiplet_enable_ridi.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//------------------------------------------------------------------------------
+/// @file  p10_chiplet_enable_ridi.C
+///
+/// @brief Enable ridi controls for PCIE/MC logic
+//------------------------------------------------------------------------------
+// *HWP HW Maintainer   : Anusha Reddy (anusrang@in.ibm.com)
+// *HWP FW Maintainer   : Raja Das (rajadas2@in.ibm.com)
+// *HWP Consumed by     : HB
+//------------------------------------------------------------------------------
+
+#include <p10_chiplet_enable_ridi.H>
+#include <p10_scom_perv.H>
+#include <p10_perv_sbe_cmn.H>
+#include <target_filters.H>
+
+fapi2::ReturnCode p10_chiplet_enable_ridi(const
+        fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip)
+{
+
+    FAPI_INF("p10_chiplet_enable_ridi: Entering ...");
+
+    auto l_perv_nest = i_target_chip.getChildren<fapi2::TARGET_TYPE_PERV>(
+                           static_cast<fapi2::TargetFilter>(fapi2::TARGET_FILTER_ALL_MC |
+                                   fapi2::TARGET_FILTER_ALL_PCI),
+                           fapi2::TARGET_STATE_FUNCTIONAL);
+
+    FAPI_DBG("Enabling Nest drivers and receivers");
+
+    for (auto& targ : l_perv_nest)
+    {
+        FAPI_TRY(p10_enable_ridi(targ));
+    }
+
+    FAPI_DBG("p10_chiplet_enable_ridi: Exiting ...");
+
+fapi_try_exit:
+    return fapi2::current_err;
+
+}
