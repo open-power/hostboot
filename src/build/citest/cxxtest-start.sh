52e44970090b7a381d51bf57e8dcaa294c4daf72
Luis Fernandez <Luis.Fernandez@ibm.com>
2019-07-12 15:00:33 -0500

    P10 Standalone SIMICS: HB CI integration w/ ODE-less standalone build
    CI is able to start Simics for ODE-less standalone Hostboot.
    CI is able to gather FFDC on failure and other artifacts whether build
    fails or not.
    An ODE SB is still created, but only used to run the tightly integrated
    autosim and similar sim tools.
    
    Change-Id: I586957d84b4f4814f7ce236c750e8b0f3028de27
    RTC:212829
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/80386
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>
    Reviewed-by: Glenn Miles <milesg@ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/build/citest/cxxtest-start.sh
+++ src/build/citest/cxxtest-start.sh
@@ -24,32 +24,62 @@
 #
 # IBM_PROLOG_END_TAG
 #
-if [ -z $HOSTBOOT_CI_ENV_SETUP ];
-then
+
+if [ -z $HOSTBOOT_CI_ENV_SETUP ];then
     unset $SANDBOXBASE
     unset $SANDBOXNAME
     source "$PROJECT_ROOT/src/build/citest/setup-env"
 fi
 
-HOSTBOOT_IMAGE=img/hbicore_test.bin
+HOSTBOOT_IMG=/img/hbicore_test.bin
+SBE_SEEPROM_IMG=/gsa/ausgsa/projects/h/hostboot/simbuild/SBE_19b2530_HB_c391a8a_sbe_seeprom_p10.bin.ecc
 
-# do not set this for FSP build
-if [ "$MACHINE" != "FSPBUILD" ];
-then
-    export PATH=$PATH:$SANDBOXBASE/simics/
+echo "autocitest setup"
+
+if [[ $SETUP_FOR_STANDALONE -eq 1 ]];then
+
+    echo "autocitest setup for P10 STANDALONE"
+
+    # Env. vars. for startup.simics and hb-tools scripts
+    export HBBLPATH=${STANDALONE}/pnor/hbbl.bin
+    export HBICORE_EXTENDED_PATH=${STANDALONE}/staging/hbicore_extended.bin
+    export STARTUPSIMICS=${STANDALONE_SIMICS}/hbfw/startup.simics
+
+    export PATH=${STANDALONE_SIMICS}:${PATH}
+
     export START_SIMICS_CMD="\
-        runsim -m $MACHINE \
-        hb_script_to_run=$SANDBOXBASE/obj/ppc/simu/scripts/hbfw/startup.simics \
-        pnor_img=$SANDBOXBASE/obj/ppc/hbfw/img/p10.pnor \
-        sbe_seeprom_img=/gsa/ausgsa/projects/h/hostboot/simbuild/SBE_19b2530_HB_c391a8a_sbe_seeprom_p10.bin.ecc \
+        runsim -m ${MACHINE} \
+        hb_script_to_run=${STARTUPSIMICS} \
+        pnor_img=${STANDALONE}/pnor/P10.pnor \
+        sbe_seeprom_img=${SBE_SEEPROM_IMG} \
         sbe_boot_mem=seeprom \
         enable_lpc_console=TRUE"
+
+else
+
+    # do not set this for FSP build
+    if [ "$MACHINE" != "FSPBUILD" ];then
+        echo "autocitest setup for non-FSP and non-P10 Standalone"
+        # Env. vars. for startup.simics and hb-tools scripts
+        export HBBLPATH=${SANDBOXBASE}/obj/ppc/hbfw/img/hbbl.bin
+        export HBICORE_EXTENDED_PATH=${SANDBOXBASE}/src/hbfw/img/hostboot_extended.bin
+        export STARTUPSIMICS=$SANDBOXBASE/obj/ppc/simu/scripts/hbfw/startup.simics
+        export PATH=$PATH:$SANDBOXBASE/simics/
+        export START_SIMICS_CMD="\
+            runsim -m $MACHINE \
+            hb_script_to_run=${STARTUPSIMICS} \
+            pnor_img=$SANDBOXBASE/obj/ppc/hbfw/img/p10.pnor \
+            sbe_seeprom_img=${SBE_SEEPROM_IMG} \
+            sbe_boot_mem=seeprom \
+            enable_lpc_console=TRUE"
+    fi
+
 fi
 
 #   Front end to autocitest - script to execute unit tests under simics.
 #
 ##  when jenkins runs it will create a workspace with the built code tree
 ##  and drop us into it.
-autocitest ${BACKING_BUILD} ${HOSTBOOT_IMAGE}
+autocitest ${BACKING_BUILD} ${HOSTBOOT_IMG}
 
 exit $?
