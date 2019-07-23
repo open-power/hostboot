39178ef83d4038bbf5482e83fbe7727463da7093
Douglas Holtsinger <Douglas.Holtsinger@ibm.com>
2019-07-23 12:52:35 -0400

    Implement p10_tod_setup and p10_tod_init procedures (1/2)
    Straight import from P9 version with a bunch of P9-specific
    things commented out for a clean build.
    
    Also included is some test code to verify the PC timebase load:
    - p10_tod_move_tod_to_tb.[CH]
    And some auxillary routines for FW:
    - p10_tod_save_config.[CH]
    - p10_tod_cleanup.[CH]
    
    Change-Id: Iecd68cceed0ab2abfe38831b4195dfe8be1f0cf0
    RTC:202074
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/80824
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Thi N Tran <thi@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/83536
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/nest/p10_tod_save_config.C
@@ -0,0 +1,107 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/nest/p10_tod_save_config.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2019                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+//------------------------------------------------------------------------------
+//
+// @file p10_tod_save_config.C
+// @brief Saves TOD configuration registers to topology structure
+//
+// *HWP HW Maintainer    : Douglas Holtsinger <Douglas.Holtsinger@ibm.com>
+// *HWP FW Maintainer    : Ilya Smirnov <ismirno@us.ibm.com>
+// *HWP Consumed by      : FSP
+//
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <p10_tod_save_config.H>
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+
+// NOTE: description in header
+fapi2::ReturnCode p10_tod_save_config(tod_topology_node* i_tod_node)
+{
+    FAPI_DBG("Start");
+
+    FAPI_TRY(fapi2::getScom(*(i_tod_node->i_target),
+                            PERV_TOD_M_PATH_CTRL_REG,
+                            i_tod_node->o_todRegs.tod_m_path_ctrl_reg),
+             "Error from getScom (PERV_TOD_M_PATH_CTRL_REG)!");
+
+    FAPI_TRY(fapi2::getScom(*(i_tod_node->i_target),
+                            PERV_TOD_PRI_PORT_0_CTRL_REG,
+                            i_tod_node->o_todRegs.tod_pri_port_0_ctrl_reg),
+             "Error from getScom (PERV_TOD_PRI_PORT_0_CTRL_REG)!");
+
+    FAPI_TRY(fapi2::getScom(*(i_tod_node->i_target),
+                            PERV_TOD_PRI_PORT_1_CTRL_REG,
+                            i_tod_node->o_todRegs.tod_pri_port_1_ctrl_reg),
+             "Error from getScom (PERV_TOD_PRI_PORT_1_CTRL_REG)!");
+
+    FAPI_TRY(fapi2::getScom(*(i_tod_node->i_target),
+                            PERV_TOD_SEC_PORT_0_CTRL_REG,
+                            i_tod_node->o_todRegs.tod_sec_port_0_ctrl_reg),
+             "Error from getScom (PERV_TOD_SEC_PORT_0_CTRL_REG)!");
+
+    FAPI_TRY(fapi2::getScom(*(i_tod_node->i_target),
+                            PERV_TOD_SEC_PORT_1_CTRL_REG,
+                            i_tod_node->o_todRegs.tod_sec_port_1_ctrl_reg),
+             "Error from getScom (PERV_TOD_SEC_PORT_1_CTRL_REG)!");
+
+    FAPI_TRY(fapi2::getScom(*(i_tod_node->i_target),
+                            PERV_TOD_S_PATH_CTRL_REG,
+                            i_tod_node->o_todRegs.tod_s_path_ctrl_reg),
+             "Error from getScom (PERV_TOD_S_PATH_CTRL_REG)!");
+
+    FAPI_TRY(fapi2::getScom(*(i_tod_node->i_target),
+                            PERV_TOD_I_PATH_CTRL_REG,
+                            i_tod_node->o_todRegs.tod_i_path_ctrl_reg),
+             "Error from getScom (PERV_TOD_I_PATH_CTRL_REG)!");
+
+    FAPI_TRY(fapi2::getScom(*(i_tod_node->i_target),
+                            PERV_TOD_PSS_MSS_CTRL_REG,
+                            i_tod_node->o_todRegs.tod_pss_mss_ctrl_reg),
+             "Error from getScom (PERV_TOD_PSS_MSS_CTRL_REG)!");
+
+    FAPI_TRY(fapi2::getScom(*(i_tod_node->i_target),
+                            PERV_TOD_CHIP_CTRL_REG,
+                            i_tod_node->o_todRegs.tod_chip_ctrl_reg),
+             "Error from getScom (PERV_TOD_CHIP_CTRL_REG)!");
+
+    // Recurse to save configuration of children
+    for (auto l_child = (i_tod_node->i_children).begin();
+         l_child != (i_tod_node->i_children).end();
+         ++l_child)
+    {
+        FAPI_TRY(p10_tod_save_config(*l_child),
+                 "Failure saving downstream configurations!");
+    }
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
