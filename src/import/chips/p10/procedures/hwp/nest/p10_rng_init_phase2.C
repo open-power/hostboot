7821f958a6aab96a0d3b4370741ab9d94faa89ef
Nicholas Landi <nlandi@ibm.com>
2019-07-29 17:13:06 -0400

    Port p10_rng_init_phase2 (2/2)
    - Finish initialization of the RNG unit that began in
      p10_rng_init_phase1 procedure.
    - Update to wrap mk file to include scom header DEPLIB
    
    Change-Id: I091dc31430945cb9de2a6623e5c11f3be169b2a1
    RTC: 213400
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/81361
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Dev-Ready: Brent Wieman <bwieman@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-by: Thi N Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/85715
    Reviewed-by: William G Hoffa <wghoffa@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/nest/p10_rng_init_phase2.C
+++ src/import/chips/p10/procedures/hwp/nest/p10_rng_init_phase2.C
@@ -37,13 +37,14 @@
 // Includes
 //------------------------------------------------------------------------------
 #include <p10_rng_init_phase2.H>
-//#include <p10_misc_scom_addresses.H>
-//#include <p10_misc_scom_addresses_fld.H>
+#include <p10_scom_proc.H>
+#include <p10_scom_perv_8.H>
 #include <p10_fbc_utils.H>
 
 //------------------------------------------------------------------------------
 // Constant definitions
 //------------------------------------------------------------------------------
+const uint8_t ADDR_SHIFT = 12;
 
 //------------------------------------------------------------------------------
 // Function definitions
@@ -52,40 +53,43 @@
 fapi2::ReturnCode
 p10_rng_init_phase2(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
 {
+    using namespace scomt;
+    using namespace scomt::proc;
+
     FAPI_INF("Start");
     fapi2::buffer<uint64_t> l_rng_cfg_data;
     fapi2::buffer<uint64_t> l_rng_bar_data;
     fapi2::buffer<uint64_t> l_rng_failed_int_data;
     fapi2::buffer<uint64_t> l_security_switch_data;
 
-//    uint16_t l_rng_cfg_self_test_hard_fail_status = 0;
+    uint64_t l_rng_cfg_self_test_hard_fail_status = 0;
     uint8_t  l_nx_rng_bar_enable = 0;
     uint64_t l_nx_rng_bar_addr = 0;
     uint64_t l_nx_rng_bar_base_addr_offset = 0;
     uint8_t  l_nx_rng_failed_int_enable = 0;
     uint64_t l_nx_rng_failed_int_addr = 0;
-    std::vector<uint64_t> l_base_addr_nm0, l_base_addr_nm1, l_base_addr_m;
-    uint64_t l_base_addr_mmio;
+    uint64_t l_base_addr_nm0 = 0;
+    uint64_t l_base_addr_nm1 = 0;
+    uint64_t l_base_addr_m = 0;
+    uint64_t l_base_addr_mmio = 0;
 
     // 5. RNG is allowed to run for M cycles (M = enough time to complete init;
     // recommend 1 second of time).
     // NOTE: accomplished by delay in execution time between phase1/phase2 HWPs
 
-    //FAPI_TRY(fapi2::getScom(i_target, PU_NX_RNG_CFG, l_rng_cfg_data),
-    //         "Error from getScom (NX RNG Status and Control Register)");
+    FAPI_TRY(GET_NX_PBI_RNG_CFG(i_target, l_rng_cfg_data));
 
     // 6. Host boot checks RNG fail bits again and if a fail is detected
     // then RNG is declared broken
 
     FAPI_DBG("Checking RNG fail status...");
     // exit if failure is reported in self test hard fail status field
-    //l_rng_cfg_data.extractToRight<PU_NX_RNG_CFG_FAIL_REG,
-    //                              PU_NX_RNG_CFG_FAIL_REG_LEN>(l_rng_cfg_self_test_hard_fail_status);
-    //FAPI_ASSERT(!l_rng_cfg_self_test_hard_fail_status,
-    //            fapi2::P10_RNG_INIT_SELF_TEST_FAILED_ERR().
-    //            set_TARGET(i_target).
-    //            set_SELF_TEST_HARD_FAIL_STATUS(l_rng_cfg_self_test_hard_fail_status),
-    //            "Self test hard fail status indicates failure");
+    GET_NX_PBI_RNG_CFG_FAIL_REG(l_rng_cfg_data, l_rng_cfg_self_test_hard_fail_status);
+    FAPI_ASSERT(!l_rng_cfg_self_test_hard_fail_status,
+                fapi2::P10_RNG_INIT_SELF_TEST_FAILED_ERR().
+                set_TARGET(i_target).
+                set_SELF_TEST_HARD_FAIL_STATUS(l_rng_cfg_self_test_hard_fail_status),
+                "Self test hard fail status indicates failure");
 
     // 7. Host boot maps RNG BARs (see Section 5.31 RNG BAR on page 185).
     // • NX RNG BAR (not mapped/enabled if RNG is broken)
@@ -101,13 +105,13 @@ p10_rng_init_phase2(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
                            l_nx_rng_bar_enable),
              "Error from FAPI_ATTR_GET (ATTR_PROC_NX_BAR_ENABLE)");
 
-    //FAPI_TRY(p10_fbc_utils_get_chip_base_address(i_target,
-    //         EFF_FBC_GRP_CHIP_IDS,
-    //         l_base_addr_nm0,
-    //         l_base_addr_nm1,
-    //         l_base_addr_m,
-    //         l_base_addr_mmio),
-    //         "Error from p10_fbc_utils_get_chip_base_address");
+    FAPI_TRY(p10_fbc_utils_get_chip_base_address(i_target,
+             EFF_TOPOLOGY_ID,
+             l_base_addr_nm0,
+             l_base_addr_nm1,
+             l_base_addr_m,
+             l_base_addr_mmio),
+             "Error from p10_fbc_utils_get_chip_base_address");
 
     // get RNG BAR addr
     FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_NX_RNG_BAR_BASE_ADDR_OFFSET,
@@ -115,18 +119,22 @@ p10_rng_init_phase2(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
                            l_nx_rng_bar_base_addr_offset),
              "Error from FAPI_ATTR_GET (ATTR_PROC_NX_BAR_BASE_ADDR_OFFSET)");
 
-    // caculate the NX RNG BAR ADDR based on the bar adddr offset
-    l_nx_rng_bar_addr = l_base_addr_mmio;
-    l_nx_rng_bar_addr += l_nx_rng_bar_base_addr_offset;
+    // caculate the NX RNG BAR ADDR based on the bar addr offset
+    l_nx_rng_bar_addr = l_base_addr_mmio + l_nx_rng_bar_base_addr_offset;
+
+    FAPI_DBG("FBC_Util returned mmio base address of: 0x%016llx", l_base_addr_mmio);
+    FAPI_DBG("Base nx offset:                         0x%016llx", l_nx_rng_bar_base_addr_offset);
+    FAPI_DBG("Calculated offset:                      0x%016llx", l_nx_rng_bar_addr);
 
     if (l_nx_rng_bar_enable == fapi2::ENUM_ATTR_PROC_NX_RNG_BAR_ENABLE_ENABLE)
     {
+        FAPI_TRY(PREP_NX_PBI_CQ_WRAP_NXCQ_SCOM_MMIO_BAR(i_target));
         // map NX RNG MMIO BAR
-        //l_rng_bar_data.setBit<PU_NX_MMIO_BAR_ENABLE>();
-        //l_rng_bar_data.insert<PU_NX_MMIO_BAR_BAR,
-        //                      PU_NX_MMIO_BAR_BAR_LEN, PU_NX_MMIO_BAR_BAR>(l_nx_rng_bar_addr);
-        //FAPI_TRY(fapi2::putScom(i_target, PU_NX_MMIO_BAR, l_rng_bar_data),
-        //         "Error from putScom (PU_NX_MMIO_BAR)");
+
+        SET_NX_PBI_CQ_WRAP_NXCQ_SCOM_MMIO_BAR_MMIO_BAR((l_nx_rng_bar_addr >> ADDR_SHIFT), l_rng_bar_data);
+        SET_NX_PBI_CQ_WRAP_NXCQ_SCOM_MMIO_BAR_MMIO_BAR_ENABLE(l_rng_bar_data);
+
+        FAPI_TRY(PUT_NX_PBI_CQ_WRAP_NXCQ_SCOM_MMIO_BAR(i_target, l_rng_bar_data));
 
         // map NX RNG failed interrupt address
         FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_PROC_NX_RNG_FAILED_INT_ENABLE,
@@ -138,23 +146,20 @@ p10_rng_init_phase2(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
                                l_nx_rng_failed_int_addr),
                  "Error from FAPI_ATTR_GET (ATTR_PROC_NX_RNG_FAILED_INT_ADDR)");
 
-        //if (l_nx_rng_failed_int_enable ==
-        //    fapi2::ENUM_ATTR_PROC_NX_RNG_FAILED_INT_ENABLE_ENABLE)
-        //{
-        //    //l_rng_failed_int_data.setBit<PU_RNG_FAILED_INT_ENABLE>();
-        //    //l_rng_failed_int_data.insert<PU_RNG_FAILED_INT_ADDRESS,
-        //    //                             PU_RNG_FAILED_INT_ADDRESS_LEN, PU_RNG_FAILED_INT_ADDRESS>
-        //    //                             (l_nx_rng_failed_int_addr);
+        if (l_nx_rng_failed_int_enable ==
+            fapi2::ENUM_ATTR_PROC_NX_RNG_FAILED_INT_ENABLE_ENABLE)
+        {
+            FAPI_TRY(PREP_NX_PBI_UMAC_RNG_FAILED_INT(i_target));
 
-        //    //FAPI_TRY(fapi2::putScom(i_target,
-        //    //                        PU_RNG_FAILED_INT,
-        //    //                        l_rng_failed_int_data),
-        //    //         "Error from putScom (NX RNG Failed Interrupt Address Register");
-        //}
-        //else
-        //{
-        //    FAPI_DBG("Skipping setup of NX RNG Failed Interrupt Address Register");
-        //}
+            SET_NX_PBI_UMAC_RNG_FAILED_INT_ENABLE(l_rng_failed_int_data);
+            SET_NX_PBI_UMAC_RNG_FAILED_INT_ADDRESS((l_nx_rng_failed_int_addr >> ADDR_SHIFT), l_rng_failed_int_data);
+
+            FAPI_TRY(PUT_NX_PBI_UMAC_RNG_FAILED_INT(i_target, l_rng_failed_int_data));
+        }
+        else
+        {
+            FAPI_DBG("Skipping setup of NX RNG Failed Interrupt Address Register");
+        }
     }
     else
     {
@@ -162,9 +167,8 @@ p10_rng_init_phase2(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
     }
 
     // set NX RNG enable
-    //l_rng_cfg_data.setBit<PU_NX_RNG_CFG_ENABLE>();
-    //FAPI_TRY(fapi2::putScom(i_target, PU_NX_RNG_CFG, l_rng_cfg_data),
-    //         "Error from putScom (NX RNG Status and Control Register)");
+    SET_NX_PBI_RNG_CFG_RNG_ENABLE(l_rng_cfg_data);
+    FAPI_TRY(PUT_NX_PBI_RNG_CFG(i_target, l_rng_cfg_data));
 
     // 8. Host boot sets the NX “sticky bit” that asserts
     // tc_nx_block_rng_scom_wr. If tc_nx_block_rng_scom_wr = 1 writes to RNG
@@ -174,17 +178,11 @@ p10_rng_init_phase2(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
 
     // set NX sticky bit to block future RNG SCOM writes
     // (tc_nx_block_rng_scom_wr)
-    //FAPI_TRY(fapi2::getScom(i_target,
-    //                        PU_SECURITY_SWITCH_REGISTER_SCOM,
-    //                        l_security_switch_data),
-    //         "Error from getScom (Security Switch Register");
+    FAPI_TRY(scomt::perv::GET_OTPC_M_SECURITY_SWITCH_REGISTER(i_target, l_security_switch_data));
 
-    //l_security_switch_data.setBit<PU_SECURITY_SWITCH_REGISTER_NX_RAND_NUM_GEN_LOCK>();
+    scomt::perv::SET_OTPC_M_SECURITY_SWITCH_REGISTER_NX_RAND_NUM_GEN_LOCK(l_security_switch_data);
 
-    //FAPI_TRY(fapi2::putScom(i_target,
-    //                        PU_SECURITY_SWITCH_REGISTER_SCOM,
-    //                        l_security_switch_data),
-    //         "Error from putScom (Security Switch Register");
+    FAPI_TRY(scomt::perv::PUT_OTPC_M_SECURITY_SWITCH_REGISTER(i_target, l_security_switch_data));
 
 fapi_try_exit:
     FAPI_INF("End");
