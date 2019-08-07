b91f23acfe17399623fe92e745298ddb59616b3f
Caleb Palmer <cnpalmer@us.ibm.com>
2019-08-07 09:29:09 -0500

    PRD: Add Axone background scrubbing framework
    Change-Id: I7516acf512f5a24fb3edacd966a35dbfed6eafe0
    CQ: SW471952
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/81897
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Benjamen G Tyner <ben.tyner@ibm.com>
    Reviewed-by: Zane C Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/81988
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/87499
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/87625
    Tested-by: Zane C Shelley <zshelle@us.ibm.com>

--- src/usr/diag/prdf/prdfMain_ipl.C
+++ src/usr/diag/prdf/prdfMain_ipl.C
@@ -153,6 +153,8 @@ errlHndl_t startScrub( const TargetHandle_t i_trgt )
         switch ( chip->getType() )
         {
             case TYPE_MCBIST: startInitialBgScrub<TYPE_MCBIST>(chip); break;
+            case TYPE_OCMB_CHIP:
+                startInitialBgScrub<TYPE_OCMB_CHIP>(chip); break;
             default:
                 PRDF_ERR( PRDF_FUNC "Unsupported maintenance target type "
                           "0x%02x", chip->getType() );
