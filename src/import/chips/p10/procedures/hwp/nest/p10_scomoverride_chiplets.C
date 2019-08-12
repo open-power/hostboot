8b65ca3a2e1234778434b1f957cb3bb2e8bf6a81
Joe McGill <jmcgill@us.ibm.com>
2019-08-12 14:59:21 -0400

    p10_scomoverride_chiplets -- create placeholder for IPL flow
    invoked at istep 10.10 as a placeholder for any sequence driven
    SCOM overrides -- currently empty
    
    Change-Id: Ie522f3ebc75124f90891f646bfc7a64c7c1cca19
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/82093
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Dev-Ready: Brent Wieman <bwieman@us.ibm.com>
    Reviewed-by: Brent Wieman <bwieman@us.ibm.com>
    Reviewed-by: Thi N Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/90623
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/nest/p10_scomoverride_chiplets.C
@@ -0,0 +1,51 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/nest/p10_scomoverride_chiplets.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019,2020                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p10_scomoverride_chiplets.C
+/// @brief Apply any sequence driven SCOM overrides to chiplets (FAPI2)
+///
+
+///
+/// *HWP HW Maintainer: Joe McGill <jmcgill@us.ibm.com>
+/// *HWP FW Maintainer: Ilya Smirnov <ismirno@us.ibm.com>
+/// *HWP Consumed by  : HB
+///
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <p10_scomoverride_chiplets.H>
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p10_scomoverride_chiplets(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    FAPI_DBG("Start");
+    FAPI_DBG("End");
+    return fapi2::FAPI2_RC_SUCCESS;
+}
