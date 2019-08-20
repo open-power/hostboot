87b1f2f29a18a13db1424fdf21752ea9334d9478
Thi Tran <thi@us.ibm.com>
2019-08-20 11:43:16 -0400

    HWP supports for istep 14.3 p10_load_iop_xram
    Set #2: Update code from review comments and test results
    Set #3: Hacks/workarounds added to test IOP XRAM write/read back.
    Set #6: Update to accomodate HW changes for drop X
    Set #7: Update PHY reset sequence
    
    Change-Id: I4cbd730f4006bd37fb9e0c54afe0b5f8820a7f5c
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/82525
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: CHRISTOPHER D HANUDEL <chanudel@us.ibm.com>
    Reviewed-by: Benjamin Gass <bgass@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/91727
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/p10/procedures/hwp/nest/p10_iop_xram_utils.C
@@ -0,0 +1,310 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/nest/p10_iop_xram_utils.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019,2020                        */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p10_iop_xram_utils.C
+/// @brief Common code/definitions to support xram read/write HWPs.
+///
+/// *HWP HW Maintainer: Thi Tran <thi@us.ibm.com>
+/// *HWP FW Maintainer: Ilya Smirnov <ismirno@us.ibm.com>
+/// *HWP Consumed by: HB, Cronus
+///
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <p10_iop_xram_utils.H>
+#include <p10_scom_pec.H>
+
+//@TODO: RTC 214852 - Use SCOM accessors
+
+//------------------------------------------------------------------------------
+// scomt name spaces
+//------------------------------------------------------------------------------
+using namespace scomt;
+using namespace scomt::pec;
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+
+/// ############################################################
+/// See doxygen in header file
+fapi2::buffer<uint64_t> getXramAddress(const uint32_t i_offset)
+{
+    uint64_t l_bank = 0;
+    uint64_t l_row = 0;
+    fapi2::buffer<uint64_t> l_buffer(0);
+    l_bank = i_offset / NUM_OF_BYTES_PER_BANK;
+    l_row  = (i_offset % NUM_OF_BYTES_PER_BANK) / NUM_OF_BYTES_PER_ROW;
+    // Bank = bits 0:1
+    // Row  = bits 2:11
+    l_buffer = (l_bank << 62) | (l_row << 52);
+    return l_buffer;
+}
+
+/// ############################################################
+/// See doxygen in header file
+fapi2::ReturnCode validateXramAccessParms(const uint32_t i_offset,
+        const xramIopTopNum_t i_top,
+        const xramPhyNum_t i_phy)
+{
+    // Validate offset is within range and 8-byte aligned
+    FAPI_ASSERT( (i_offset < MAX_XRAM_IMAGE_SIZE) &&
+                 (!(i_offset & 0x7)),
+                 fapi2::P10_IOP_XRAM_OFFSET_ERROR().set_OFFSET(i_offset),
+                 "validateXramAccessParms: XRAM offset must be < 32K and 8-byte aligned: Offset 0x%.16llX.",
+                 i_offset);
+    // Validate Top and Phy numbers
+    FAPI_ASSERT( (i_top <= 1) && (i_phy <= 1),
+                 fapi2::P10_IOP_TOP_PHY_ERROR()
+                 .set_IOP_TOP(i_top)
+                 .set_XRAM_PHY(i_phy),
+                 "validateXramAccessParms: XRAM Iop_top and/or Phy values are invalid. i_top %d, i_phy %d.",
+                 i_top, i_phy);
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+/// ############################################################
+/// See doxygen in header file
+fapi2::ReturnCode doPhyReset(
+    const fapi2::Target < fapi2::TARGET_TYPE_PEC |
+    fapi2::TARGET_TYPE_MULTICAST > & i_target)
+{
+    FAPI_DBG("Start");
+    fapi2::buffer<uint64_t> l_CPLT_CTRL0_reg(0);
+    fapi2::buffer<uint64_t> l_arrayModeReg(0);
+    uint64_t l_xramBaseReg = 0;
+    uint32_t l_pollCount = 0;
+    char l_target_str[fapi2::MAX_ECMD_STRING_LEN];
+
+    // display target information for this chip
+    fapi2::toString(i_target, l_target_str, sizeof(l_target_str));
+    FAPI_INF("Target: %s", l_target_str);
+
+
+    // Note: Reset CPLT_CTRL0 bit 55 feeds to both iop_top instances and both PHYs
+    //    1. Drive PHY reset to 1
+    //    2. Drive PHY reset to 0
+    //    3. Poll for SRAM init done
+
+    // 1. Drive PHY reset to 1
+    FAPI_TRY(PREP_CPLT_CTRL0_WO_OR(i_target),
+             "doPhyReset: PREP_CPLT_CTRL0_WO_OR returns an error.");
+    SET_CPLT_CTRL0_TC_CCFG_FUNC_PHY_RESET_DC(l_CPLT_CTRL0_reg); // Set bit 55 to reset
+    FAPI_TRY(PUT_CPLT_CTRL0_WO_OR(i_target, l_CPLT_CTRL0_reg),
+             "doPhyReset: PUT_CPLT_CTRL0_WO_OR returns an error.");
+
+    // 2. Drive PHY reset to 0
+    FAPI_TRY(PREP_CPLT_CTRL0_WO_CLEAR(i_target),
+             "doPhyReset: PREP_CPLT_CTRL0_WO_CLEAR returns an error.");
+    FAPI_TRY(PUT_CPLT_CTRL0_WO_CLEAR(i_target, l_CPLT_CTRL0_reg), // Clear bit 55
+             "doPhyReset: PUT_CPLT_CTRL0_WO_CLEAR returns an error..");
+
+    // 3. Poll for SRAM init done (on both tops and both PHYs of each PEC)
+    for (auto l_pec : i_target.getChildren<fapi2::TARGET_TYPE_PEC>())
+    {
+        for (uint8_t l_top = 0; l_top < NUM_OF_IO_TOPS; l_top++)
+        {
+            fapi2::toString(i_target, l_target_str, sizeof(l_target_str));
+            FAPI_INF("doPhyReset: Check SRAM init done on %s, top %d",
+                     l_target_str, l_top);
+
+            l_pollCount = 0;
+
+            while (l_pollCount <= SRAM_INIT_DONE_POLLS)
+            {
+                l_xramBaseReg = getXramBaseReg(static_cast<xramIopTopNum_t>(l_top));
+                FAPI_TRY(fapi2::getScom(l_pec,
+                                        l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET,
+                                        l_arrayModeReg),
+                         "Error from getScom 0x%.16llX", l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET);
+
+                if ( l_arrayModeReg.getBit<ARRAY_MODE_REG_PHY0_SRAM_INIT_DONE_BIT>() &&
+                     l_arrayModeReg.getBit<ARRAY_MODE_REG_PHY1_SRAM_INIT_DONE_BIT>() )
+                {
+                    break;
+                }
+
+                l_pollCount++;
+                fapi2::delay(SRAM_INIT_DONE_POLL_DELAY_HW_NS,
+                             SRAM_INIT_DONE_POLL_DELAY_SIM_CYCLE);
+            }
+
+            FAPI_ASSERT( (l_pollCount < SRAM_INIT_DONE_POLLS),
+                         fapi2::P10_IOP_XRAM_INIT_TIMEOUT_ERROR()
+                         .set_TARGET(l_pec)
+                         .set_TOP(l_top)
+                         .set_ARRAY_MODE_REG(l_arrayModeReg),
+                         "doPhyReset: SRAM init_done timeout error on %s, top %d.",
+                         l_target_str, l_top);
+
+            FAPI_INF("doPhyReset: SRAM init done set for %s", l_target_str);
+        }
+    }
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
+
+/// ############################################################
+/// See doxygen in header file
+uint64_t getXramBaseReg(xramIopTopNum_t i_top)
+{
+    uint64_t l_xramBaseReg = 0;
+
+    if (i_top == XRAM_TOP_0)
+    {
+        l_xramBaseReg = IOP_TOP0_XRAM_BASE_REG;
+    }
+    else
+    {
+        l_xramBaseReg = IOP_TOP1_XRAM_BASE_REG;
+    }
+
+    return l_xramBaseReg;
+}
+
+/// ############################################################
+/// See doxygen in header file
+fapi2::ReturnCode setPhyLoadDone(
+    const fapi2::Target < fapi2::TARGET_TYPE_PEC |
+    fapi2::TARGET_TYPE_MULTICAST > & i_target,
+    const xramIopTopNum_t i_top,
+    const xramPhyNum_t i_phy)
+{
+    FAPI_DBG("Start");
+    fapi2::buffer<uint64_t> l_xramArrayMode_reg(0);
+    uint64_t l_xramBaseReg = getXramBaseReg(i_top);
+
+    FAPI_DBG("setPhyLoadDone: i_top %d, i_phy %d", i_top, i_phy);
+
+    // Need to do RMW on individual PEC
+    for (auto l_pec : i_target.getChildren<fapi2::TARGET_TYPE_PEC>())
+    {
+        FAPI_TRY(fapi2::getScom(l_pec,
+                                l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET,
+                                l_xramArrayMode_reg),
+                 "Error from getScom 0x%.16llX", l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET);
+
+        l_xramArrayMode_reg.setBit(ARRAY_MODE_REG_PHY0_SRAM_EXT_LOAD_DONE_BIT + i_phy);
+
+        FAPI_TRY(fapi2::putScom(l_pec, l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET,
+                                l_xramArrayMode_reg),
+                 "Error from putScom 0x%.16llX", l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET);
+    }
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
+
+/// ############################################################
+/// See doxygen in header file
+fapi2::ReturnCode enableXramScrubber(
+    const fapi2::Target < fapi2::TARGET_TYPE_PEC |
+    fapi2::TARGET_TYPE_MULTICAST > & i_target,
+    const xramIopTopNum_t i_top)
+{
+    FAPI_DBG("Start");
+    fapi2::buffer<uint64_t> l_xramArrayMode_reg(0);
+    uint64_t l_xramBaseReg = getXramBaseReg(i_top);
+
+    FAPI_DBG("enableXramScrubber: i_top %d", i_top);
+
+    // Need to do RMW on individual PEC
+    for (auto l_pec : i_target.getChildren<fapi2::TARGET_TYPE_PEC>())
+    {
+        // Set scrubber bit in mode reg
+        FAPI_TRY(fapi2::getScom(l_pec,
+                                l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET,
+                                l_xramArrayMode_reg),
+                 "Error from getScom 0x%.16llX", l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET);
+        l_xramArrayMode_reg.setBit<ARRAY_MODE_REG_ENABLE_SCRUBBER_BIT>();
+
+        FAPI_TRY(fapi2::putScom(l_pec, l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET,
+                                l_xramArrayMode_reg),
+                 "Error from putScom 0x%.16llX", l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET);
+    }
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
+
+/// ############################################################
+/// See doxygen in header file
+fapi2::ReturnCode autoIncrementControl(
+    const fapi2::Target < fapi2::TARGET_TYPE_PEC |
+    fapi2::TARGET_TYPE_MULTICAST > & i_target,
+    const xramIopTopNum_t i_top,
+    const bool i_enable,
+    const bool i_write_op)
+{
+    FAPI_DBG("Start");
+    fapi2::buffer<uint64_t> l_xramArrayMode_reg(0);
+    uint64_t l_xramBaseReg = getXramBaseReg(i_top);
+
+    FAPI_DBG("enableAutoIncrement: i_top %d, Action %s, Oper %s", i_top,
+             i_enable ? "Enable" : "Disable",
+             i_write_op ? "Write" : "Read");
+
+    // Need to do RMW on individual PEC
+    for (auto l_pec : i_target.getChildren<fapi2::TARGET_TYPE_PEC>())
+    {
+        FAPI_TRY(fapi2::getScom(l_pec,
+                                l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET,
+                                l_xramArrayMode_reg),
+                 "Error from getScom 0x%.16llX", l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET);
+
+        // Enable autoinc
+        if (i_enable)
+        {
+            l_xramArrayMode_reg.setBit<ARRAY_MODE_REG_SCOM_ACCESS_MODE>();
+
+            if (i_write_op) // Write
+            {
+                l_xramArrayMode_reg.setBit<ARRAY_MODE_REG_SCOM_AUTOINC_RW>();
+            }
+            else // Read
+            {
+                l_xramArrayMode_reg.clearBit<ARRAY_MODE_REG_SCOM_AUTOINC_RW>();
+            }
+        }
+        // Disable autoinc
+        else
+        {
+            l_xramArrayMode_reg.clearBit<ARRAY_MODE_REG_SCOM_ACCESS_MODE>();
+        }
+
+        FAPI_TRY(fapi2::putScom(l_pec, l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET,
+                                l_xramArrayMode_reg),
+                 "Error from putScom 0x%.16llX", l_xramBaseReg + XRAM_ARRAY_MODE_REG_OFFSET);
+    }
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
---
+++ src/import/chips/p10/procedures/hwp/nest/p10_write_xram.C
@@ -0,0 +1,128 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/p10/procedures/hwp/nest/p10_write_xram.C $   */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019,2020                        */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file p10_write_xram.C
+/// @brief Write data to IOP XRAM
+/// *HWP HW Maintainer: Thi Tran <thi@us.ibm.com>
+/// *HWP FW Maintainer: Ilya Smirnov <ismirno@us.ibm.com>
+/// *HWP Consumed by: HB, Cronus
+///
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+#include <p10_write_xram.H>
+#include <p10_scom_pec.H>
+
+//------------------------------------------------------------------------------
+// scomt name spaces
+//------------------------------------------------------------------------------
+using namespace scomt;
+using namespace scomt::pec;
+
+//@TODO: RTC 214852 - Use SCOM accessors, remove workaround code
+
+//------------------------------------------------------------------------------
+// Function definitions
+//------------------------------------------------------------------------------
+
+/// NOTE: doxygen in header
+fapi2::ReturnCode p10_write_xram(
+    const fapi2::Target < fapi2::TARGET_TYPE_PEC |
+    fapi2::TARGET_TYPE_MULTICAST > & i_target,
+    const uint32_t i_offset,
+    const uint32_t i_bytes,
+    const xramIopTopNum_t i_top,
+    const xramPhyNum_t i_phy,
+    uint8_t* i_data)
+{
+    FAPI_DBG("Start");
+    fapi2::buffer<uint64_t> l_dataBuf(0);
+    fapi2::buffer<uint64_t> l_offsetBuf(0);
+    uint8_t* l_dataPtr = i_data;
+    uint64_t l_xramBaseReg = getXramBaseReg(i_top);
+    uint64_t l_data = 0;
+
+    FAPI_INF("p10_write_xram: i_offset %p, i_bytes %lu, i_top %d, i_phy %d.",
+             i_offset, i_bytes, i_top, i_phy);
+
+    // Validate input parameters
+    FAPI_TRY(validateXramAccessParms(i_offset, i_top, i_phy),
+             "validateXramAccessParms returns an error.");
+
+    // Enable auto-inc mode for write
+    FAPI_TRY(autoIncrementControl(i_target, i_top, true, true),
+             "autoIncrementControl Enable returns an error.");
+
+    // Setup Array Address Register for write
+    l_offsetBuf = getXramAddress(i_offset);
+    FAPI_TRY(fapi2::putScom(i_target,
+                            l_xramBaseReg + XRAM_ARRAY_ADDR_REG_PHY0_OFFSET + i_phy,
+                            l_offsetBuf),
+             "Error from putScom 0x%.16llX",
+             l_xramBaseReg + XRAM_ARRAY_ADDR_REG_PHY0_OFFSET + i_phy);
+
+    // Write data, 8-byte chunk at a time
+    FAPI_DBG("p10_write_xram: Start writing data...");
+
+
+    while (l_dataPtr < (i_data + i_bytes))
+    {
+        // Load 8 bytes into 64-bit word
+        l_data = 0;
+
+        for (uint8_t ii = 0; ii < 8; ii++)
+        {
+            l_data |= ( static_cast<uint64_t>(*l_dataPtr++) << (56 - (8 * ii)) );
+
+            // Exit if size has reached. Remaining data in double words are zeroes
+            if (l_dataPtr >= i_data + i_bytes)
+            {
+                break;
+            }
+        }
+
+        // Write data to Array Data Register
+        l_dataBuf = l_data;
+        FAPI_TRY(fapi2::putScom(i_target,
+                                l_xramBaseReg + XRAM_ARRAY_DATA_REG_PHY0_OFFSET + i_phy,
+                                l_dataBuf),
+                 "Error from putScom 0x%.16llX",
+                 l_xramBaseReg + XRAM_ARRAY_DATA_REG_PHY0_OFFSET + i_phy);
+    }
+
+    FAPI_DBG("p10_write_xram: Done writing data.");
+
+    // Disable auto-inc mode
+    FAPI_TRY(autoIncrementControl(i_target, i_top, false, true),
+             "autoIncrementControl disable returns an error.");
+
+    // Set XRAM load done
+    FAPI_TRY(setPhyLoadDone(i_target, i_top, i_phy),
+             "p10_write_xram: setPhyLoadDone returns an error.");
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
