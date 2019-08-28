b7c4af200f89786106e986767ed9f293291f3bdf
Christian Geddes <crgeddes@us.ibm.com>
2019-08-28 17:13:58 -0500

    Add wrapper to exp_getidec HWP so we can call it in platform code
    There has been a trend to create more HWPs that process raw data.
    This allows Hostboot and Cronus to share more code. The trick is
    that we generally want to call these HWPs inside traditionally
    hostboot-only code. This new fapiwrap directory will help encapsulate
    all of the calls to ekb code in one place. That way if we ever need
    to remove HWPs/EKB code it will be easier to stub out the calls. In the
    past we have contaminated a lot of Hostboot-only code with calls out
    to EKB code. This new new directory should help avoid this issue.
    
    Change-Id: I2b2d0d1e62c97f0976c9c9ede3fe2eac30c7a40f
    RTC: 214627
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/83029
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

---
+++ src/usr/fapiwrap/fapiWrap.C
@@ -0,0 +1,58 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/usr/fapiwrap/fapiWrap.C $                                 */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2019                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+#include <fapiwrap/fapiWrapif.H> // interface definitions
+#include <fapi2/plat_hwp_invoker.H> // FAPI_INVOKE_HWP
+#include <trace/interface.H> // tracing includes
+
+#include <exp_getidec.H> // exp_getidec
+
+trace_desc_t* g_trac_fapiwrap;
+TRAC_INIT(&g_trac_fapiwrap, FAPIWRAP_COMP_NAME, 6*KILOBYTE, TRACE::BUFFER_SLOW);
+
+
+namespace FAPIWRAP
+{
+    errlHndl_t explorer_getidec( TARGETING::Target * i_ocmbChip,
+                                 uint16_t& o_chipId,
+                                 uint8_t&  o_ec)
+    {
+        errlHndl_t l_errl = nullptr;
+
+        //assert type of i_ocmbChip == TARGETING::TYPE_OCMB_CHIP
+        assert(i_ocmbChip->getAttr<TARGETING::ATTR_TYPE>() == TARGETING::TYPE_OCMB_CHIP,
+               "exp_getidec_wrap: error expected type OCMB_CHIP");
+
+        fapi2::Target <fapi2::TARGET_TYPE_OCMB_CHIP> l_fapi_ocmb_target(i_ocmbChip);
+
+        FAPI_INVOKE_HWP(l_errl,
+                        exp_getidec,
+                        l_fapi_ocmb_target,
+                        o_chipId,
+                        o_ec);
+
+        return l_errl;
+    }
+}
