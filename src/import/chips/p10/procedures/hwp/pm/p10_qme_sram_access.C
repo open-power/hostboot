f89bb47f326f1d38d17e9c69d163a8ce539c0045
Greg Still <stillgs@us.ibm.com>
2019-09-20 13:31:54 -0400

    PM: fix to QME get/putsram functions
    - Remove auto-increment work-around for HW501715
    
    Change-Id: I2c5d5cfea3112004b8e68abf968eb2d050ee6a1e
    RTC: 211508
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/84136
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Dev-Ready: Gregory S Still <stillgs@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/91726
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/pm/p10_qme_sram_access.C
+++ src/import/chips/p10/procedures/hwp/pm/p10_qme_sram_access.C
@@ -97,18 +97,8 @@ fapi2::ReturnCode p10_qme_sram_access(
     // Not doing an initial assignment to 0 to save space on the SBE.
     uint32_t        l_norm_address;
     uint32_t        l_words_to_access;
-    // Clear the core selects so that multicast to the QMEs will work
-    uint32_t l_core_select = 0;
 
     FAPI_DBG("> p10_qme_sram_access");
-
-    fapi2::Target< fapi2::TARGET_TYPE_PROC_CHIP > l_chip =
-        i_qme_target.getParent< fapi2::TARGET_TYPE_PROC_CHIP >();
-
-    fapi2::Target < fapi2::TARGET_TYPE_EQ |
-    fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > l_target =
-        l_chip.getMulticast<fapi2::MULTICAST_AND>(fapi2::MCGROUP_GOOD_EQ,
-                static_cast<fapi2::MulticastCoreSelect>(l_core_select));
 
     // Ensure the address is between 0xFFFF0000 and 0xFFFFFFFF.
     // No need to check the upper limit, since that will overflow the uint32_t data type.
@@ -132,9 +122,9 @@ fapi2::ReturnCode p10_qme_sram_access(
 
     // Set the QME SRAM address as defined by 16:28 (64k)
     l_norm_address = i_start_address & 0x0000FFF8;
-    l_data64.flush<0>().insertFromRight<0, 32>(l_norm_address);
-    FAPI_DBG("   QME Setting address to 0x%016llX", l_data64);
-    FAPI_TRY(fapi2::putScom(l_target, QME_QSAR, l_data64));
+    l_qsar.flush<0>().insertFromRight<0, 32>(l_norm_address);
+    FAPI_DBG("   QME Setting address to 0x%016llX", l_qsar);
+    FAPI_TRY(fapi2::putScom(i_qme_target, QME_QSAR, l_qsar));
 
     // Compute the number of words
     if ((l_norm_address + i_length_dword * 8) > QME_SRAM_SIZE)
@@ -150,16 +140,17 @@ fapi2::ReturnCode p10_qme_sram_access(
     // Increment after each access.
     o_dwords_accessed = 0;
 
-    // RTC 211508 Get around HW501715 where auto-increment doesn't work
+    // This switch is here to allow for each switching between auto-increment
+    // and non-autoincrement (NOAUTOINC) modes.
     if (i_operation == qmesram::GET)
     {
-        l_op = qmesram::GET_NOAUTOINC;
-//        l_op = qmesram::GET;
+        // l_op = qmesram::GET_NOAUTOINC;  // only needed for work-arounds
+        l_op = qmesram::GET;
     }
     else if (i_operation == qmesram::PUT)
     {
-        l_op = qmesram::PUT_NOAUTOINC;
-//        l_op = qmesram::PUT;
+        // l_op = qmesram::PUT_NOAUTOINC;  // only needed for work-arounds
+        l_op = qmesram::PUT;
     }
     else
     {
@@ -169,18 +160,12 @@ fapi2::ReturnCode p10_qme_sram_access(
     switch (l_op)
     {
         case qmesram::GET:
-//             if (l_target.isMulticast())
-//             {
-//                 FAPI_ERR("   ERROR:  Multicast GET is not supported");
-//                 fapi2::current_err = fapi2::FAPI2_RC_INVALID_PARAMETER;
-//                 goto fapi_try_exit;
-//             }
-
-            l_data64.flush<0>().setBit<QSCR_SRAM_ACCESS_MODE_BIT>();
-            FAPI_TRY(fapi2::putScom(l_target, QME_QSCR_WO_OR, l_data64));
-
-            FAPI_DBG("   Reading %d words from 0x%.8X through 0x%.8X in autoinc mode", l_words_to_access, l_norm_address,
+            FAPI_DBG("   Reading %d words from 0x%.8X through 0x%.8X in autoinc mode",
+                     l_words_to_access, l_norm_address,
                      l_norm_address + l_words_to_access * 8);
+
+            l_data64.flush<0>().setBit<QME_QSCR_SRAM_ACCESS_MODE>();
+            FAPI_TRY(fapi2::putScom(i_qme_target, QME_QSCR_WO_OR, l_data64)); // set autoincrement
 
             for (uint32_t x = 0; x < l_words_to_access; x++)
             {
@@ -195,16 +180,12 @@ fapi2::ReturnCode p10_qme_sram_access(
             break;
 
         case qmesram::GET_NOAUTOINC:
-//             if (l_target.isMulticast())
-//             {
-//                 FAPI_ERR("   ERROR:  Multicast GET is not supported");
-//                 fapi2::current_err = fapi2::FAPI2_RC_INVALID_PARAMETER;
-//                 goto fapi_try_exit;
-//             }
-
             FAPI_DBG("   Reading %d words from 0x%.8X through 0x%.8X in loop mode",
                      l_words_to_access, l_norm_address,
                      l_norm_address + l_words_to_access * 8);
+
+            l_data64.flush<0>().setBit<QME_QSCR_SRAM_ACCESS_MODE>();
+            FAPI_TRY(fapi2::putScom(i_qme_target, QME_QSCR_WO_CLEAR, l_data64));  // clear autoincrement
 
             for (uint32_t x = 0; x < l_words_to_access; x++)
             {
@@ -214,27 +195,26 @@ fapi2::ReturnCode p10_qme_sram_access(
                 FAPI_TRY(fapi2::getScom(i_qme_target, QME_QSDR, l_data64));
                 io_data[x] = l_data64();
                 o_dwords_accessed++;
-//                l_qsar += 0x0000000800000000;
-                l_qsar += 0x0000001000000000;
+                l_qsar += 0x0000000800000000;
                 FAPI_TRY(fapi2::putScom(i_qme_target, QME_QSAR, l_qsar));
             }
 
             break;
 
         case qmesram::PUT:
-            l_data64.flush<0>().setBit<QSCR_SRAM_ACCESS_MODE_BIT>();
-            FAPI_TRY(fapi2::putScom(l_target, QME_QSCR_WO_OR, l_data64));
-
             FAPI_DBG("   Writing %d words to 0x%.8X through 0x%.8X in autoinc mode",
                      l_words_to_access, l_norm_address,
                      l_norm_address + l_words_to_access * 8);
 
+            l_data64.flush<0>().setBit<QME_QSCR_SRAM_ACCESS_MODE>();
+            FAPI_TRY(fapi2::putScom(i_qme_target, QME_QSCR_WO_OR, l_data64)); // set autoincrement
+
             for (uint32_t x = 0; x < l_words_to_access; x++)
             {
-                FAPI_TRY(qme_sram_read_qsar(l_target));
+                FAPI_TRY(qme_sram_read_qsar(i_qme_target));
 
                 l_data64() = io_data[x];
-                FAPI_TRY(fapi2::putScom(l_target, QME_QSDR, l_data64));
+                FAPI_TRY(fapi2::putScom(i_qme_target, QME_QSDR, l_data64));
                 o_dwords_accessed++;
             }
 
@@ -245,16 +225,18 @@ fapi2::ReturnCode p10_qme_sram_access(
                      l_words_to_access, l_norm_address,
                      l_norm_address + l_words_to_access * 8);
 
+            l_data64.flush<0>().setBit<QME_QSCR_SRAM_ACCESS_MODE>();
+            FAPI_TRY(fapi2::putScom(i_qme_target, QME_QSCR_WO_CLEAR, l_data64));  // clear autoincrement
+
             for (uint32_t x = 0; x < l_words_to_access; x++)
             {
-                FAPI_TRY(qme_sram_read_qsar(l_target));
+                FAPI_TRY(qme_sram_read_qsar(i_qme_target));
 
                 l_data64() = io_data[x];
-                FAPI_TRY(fapi2::putScom(l_target, QME_QSDR, l_data64));
+                FAPI_TRY(fapi2::putScom(i_qme_target, QME_QSDR, l_data64));
                 o_dwords_accessed++;
-//                l_qsar += 0x0000000800000000;
-                l_qsar += 0x0000001000000000;
-                FAPI_TRY(fapi2::putScom(l_target, QME_QSAR, l_qsar));
+                l_qsar += 0x0000000800000000;
+                FAPI_TRY(fapi2::putScom(i_qme_target, QME_QSAR, l_qsar));
             }
 
             break;
