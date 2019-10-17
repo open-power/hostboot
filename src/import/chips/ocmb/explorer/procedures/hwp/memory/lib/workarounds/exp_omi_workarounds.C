d9b2f153e4126f1854d3104b37d354900ddfe07a
Andre A. Marin <aamarin@us.ibm.com>
2019-10-17 14:08:42 -0400

    Snapshot explorer hwp and xml from ekb/master
    Change-Id: I6a35a4be580185700268cbc64072de6a985314fa
    Original-Change-Id: I76cbce908831ac632c7728710518ed5fef6cff54
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/85445
    Dev-Ready: ANDRE A MARIN <aamarin@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/90120
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Christian R Geddes <crgeddes@us.ibm.com>

--- src/import/chips/ocmb/explorer/procedures/hwp/memory/lib/workarounds/exp_omi_workarounds.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/lib/workarounds/exp_omi_workarounds.C
@@ -33,12 +33,15 @@
 // *HWP Level: 2
 // *HWP Consumed by: Memory
 
+#include <fapi2.H>
 #include <generic/memory/lib/utils/find.H>
 #include <lib/workarounds/exp_omi_workarounds.H>
 #include <lib/shared/exp_consts.H>
 #include <lib/omi/exp_omi_utils.H>
 #include <generic/memory/lib/mss_generic_attribute_getters.H>
 #include <generic/memory/lib/mss_generic_system_attribute_getters.H>
+#include <lib/inband/exp_inband.H>
+#include <exp_oc_regs.H>
 
 namespace mss
 {
@@ -48,75 +51,6 @@ namespace workarounds
 {
 namespace omi
 {
-///
-/// @brief Determine if OCMB PRBS workaround needs to be performed
-///
-/// @param[in] i_ocmb_chip OCMB target
-/// @param[out] o_required workaround needs to be performed
-/// @return FAPI2_RC_SUCCESS iff success
-///
-fapi2::ReturnCode is_prbs_ocmb_required(
-    const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP> i_ocmb_chip,
-    bool& o_required)
-{
-    // Check chip type
-    uint8_t l_proc_type = 0;
-    const auto& l_proc_chip = mss::find_target<fapi2::TARGET_TYPE_PROC_CHIP>(i_ocmb_chip);
-    FAPI_TRY(FAPI_ATTR_GET_PRIVILEGED(fapi2::ATTR_NAME, l_proc_chip, l_proc_type),
-             "Error getting ATTR_NAME of %s", mss::c_str(l_proc_chip));
-
-    // OCMB Workaround Logic:
-    // Non-P10 (Apollo): OCMB workaround on
-    // P10 - No workaround required (no enum for this yet, so we must revisit this when the time comes)
-    o_required = l_proc_type != fapi2::ENUM_ATTR_NAME_P10;
-
-fapi_try_exit:
-    return fapi2::current_err;
-}
-
-///
-/// @brief Performs OCMB PRBS workaround
-///
-/// @param[in] i_ocmb_chip OCMB chip
-/// @param[in] i_dl_x4_backoff_en backoff enable
-/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS iff success
-///
-fapi2::ReturnCode prbs_ocmb(
-    const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP> i_ocmb_chip,
-    const uint8_t i_dl_x4_backoff_en)
-{
-    FAPI_DBG("Performing PRBS OCMB workaround on %s", mss::c_str(i_ocmb_chip));
-
-    uint8_t l_sim = 0;
-    uint32_t l_prbs_time = 0;
-    uint64_t l_prbs_time_scaled = 0;
-
-    FAPI_TRY( mss::attr::get_is_simulation( l_sim) );
-
-    FAPI_TRY(mss::attr::get_omi_dl_preipl_prbs_time(mss::find_target<fapi2::TARGET_TYPE_OMI>(i_ocmb_chip), l_prbs_time),
-             "Error from FAPI_ATTR_GET (ATTR_OMI_DL_PREIPL_PRBS_TIME)");
-    l_prbs_time_scaled = l_prbs_time * mss::common_timings::DELAY_1MS;
-
-    // State 6
-    FAPI_TRY(mss::exp::omi::setup_omi_dl0_config0(i_ocmb_chip,
-             mss::omi::train_mode::TX_TRAINING_STATE3,
-             i_dl_x4_backoff_en));
-
-    // Set configurable delay based on the PRBS ATTR and SIM mode
-    FAPI_TRY(fapi2::delay(l_prbs_time_scaled, mss::common_timings::DELAY_1US));
-    FAPI_DBG("OMI Training Pre-ipl PRBS Time = %dns",
-             (l_sim ? mss::common_timings::DELAY_1US : l_prbs_time_scaled));
-
-    // Enable training state 1 to send Pattern A
-    FAPI_TRY(mss::exp::omi::setup_omi_dl0_config0(i_ocmb_chip,
-             mss::omi::train_mode::TX_PATTERN_A,
-             i_dl_x4_backoff_en));
-
-    // Not calling with ENABLE_AUTO_TRAINING for explorer
-
-fapi_try_exit:
-    return fapi2::current_err;
-}
 
 ///
 /// @brief Determine if we need to bypass MENTERP register reads/writes
@@ -131,6 +65,40 @@ fapi2::ReturnCode gem_menterp(const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP> 
     o_workaround = 0;
     FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_EC_FEATURE_GEMINI_MENTERP_WORKAROUND, i_target, o_workaround),
              "Error getting ATTR_CHIP_EC_FEATURE_GEMINI_MENTERP_WORKAROUND");
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+/// @brief Determine if / perform the gemini workaround to setup the OMI config registers
+///
+/// @param[in] i_target OCMB (gemini)
+/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS iff success, else error
+/// @note Gemini workaround to setup METADATA and TEMPLATE bits before doing reads
+///
+fapi2::ReturnCode gem_setup_config(const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
+{
+    uint8_t l_gemini_config_workaround = 0;
+
+    // Check if gemini workaround should be performed
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_CHIP_EC_FEATURE_GEMINI_OMI_SETUP_CONFIG_WORKAROUND, i_target,
+                           l_gemini_config_workaround),
+             "Error getting ATTR_CHIP_EC_FEATURE_GEMINI_OMI_SETUP_CONFIG_WORKAROUND");
+
+    if (l_gemini_config_workaround)
+    {
+        // Set metadata bits
+        fapi2::buffer<uint32_t> l_value;
+        l_value.setBit<EXPLR_OC_OCTRLPID_MSB_METADATA_SUPPORTED>();
+        l_value.setBit<EXPLR_OC_OCTRLPID_MSB_METADATA_ENABLED>();
+        FAPI_TRY(mss::exp::ib::putOCCfg(i_target, EXPLR_OC_OCTRLPID_MSB, l_value));
+
+        // Set template bits
+        l_value.flush<0>();
+        l_value.setBit<EXPLR_OC_OTTCFG_MSB_TEMPLATE_0>();
+        l_value.setBit<EXPLR_OC_OTTCFG_MSB_TEMPLATE_5>();
+        FAPI_TRY(mss::exp::ib::putOCCfg(i_target, EXPLR_OC_OTTCFG_MSB, l_value));
+    }
 
 fapi_try_exit:
     return fapi2::current_err;
