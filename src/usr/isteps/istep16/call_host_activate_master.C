22b39d874ddd07de74470b20d6b4a66ebb52c6ee
Christian Geddes <crgeddes@us.ibm.com>
2019-11-05 10:33:43 -0600

    Adjust deadman timeout request time to be 10.5 seconds
    We see intermittent deadman timeouts occuring in our testing. Upon
    further inspection it was determined that value we were passing
    to the SBE was being cast to a uint32_t and multiplied by up to
    a factor of 400000 , which was resulting in an overflow of the
    uint32_t. This was giving us a timeout lower than desired. This
    commit lowers the requested timeout to be 10.5 seconds which will
    not overflow the uint32_t.
    
    Change-Id: I747db46a5d4a99d273b5604971d3db51f077be50
    CQ: SW477557
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/86513
    Reviewed-by: Glenn Miles <milesg@ibm.com>
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: William G Hoffa <wghoffa@us.ibm.com>

--- src/usr/isteps/istep16/call_host_activate_master.C
+++ src/usr/isteps/istep16/call_host_activate_master.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2018                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -204,7 +204,8 @@ void* call_host_activate_master (void *io_pArgs)
                     TARGETING::get_huid(l_proc_target));
 
         //In the future possibly move default "waitTime" value to SBEIO code
-        uint64_t waitTime = 1000000; // bump the wait time to 1 sec
+        uint64_t waitTime = 10500; // wait time 10.5 sec, anything larger than 10737 ms can cause
+                                   // overflow on SBE side of the tiemout calculations
         l_errl = SBEIO::startDeadmanLoop(waitTime);
 
         if ( l_errl )
