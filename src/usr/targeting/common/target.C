54f692b2d30ad2665222bc67cb9c29978d345a10
Dean Sanner <dsanner@us.ibm.com>
2019-11-19 07:48:31 -0600

    Add missing target types for P10/OCMB
    Need to add missing unit numbers for FAPI ATTR Dump
    
    Change-Id: Icb6a332e7f7e6a7177dc2efc2d5e87e552f06f2c
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/87389
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>
    Reviewed-by: William G Hoffa <wghoffa@us.ibm.com>

--- src/usr/targeting/common/target.C
+++ src/usr/targeting/common/target.C
@@ -617,7 +617,9 @@ void Target::getAttrTankTargetPosData(uint16_t & o_pos,
             }
             else if ((l_element.type == TYPE_PROC) ||
                      (l_element.type == TYPE_MEMBUF) ||
-                     (l_element.type == TYPE_DIMM))
+                     (l_element.type == TYPE_DIMM) ||
+                     (l_element.type == TYPE_OCMB_CHIP) ||
+                     (l_element.type == TYPE_PMIC))
             {
                 o_pos = l_element.instance;
             }
@@ -635,7 +637,15 @@ void Target::getAttrTankTargetPosData(uint16_t & o_pos,
                      (l_element.type == TYPE_PPE) ||
                      (l_element.type == TYPE_PERV) ||
                      (l_element.type == TYPE_PEC) ||
-                     (l_element.type == TYPE_PHB))
+                     (l_element.type == TYPE_PHB) ||
+                     (l_element.type == TYPE_OMI) ||
+                     (l_element.type == TYPE_MCC) ||
+                     (l_element.type == TYPE_OMIC) ||
+                     (l_element.type == TYPE_MEM_PORT) ||
+                     (l_element.type == TYPE_NMMU) ||
+                     (l_element.type == TYPE_PAU) ||
+                     (l_element.type == TYPE_IOHS) ||
+                     (l_element.type == TYPE_PAUC))
             {
                 o_unitPos = l_element.instance;
             }
