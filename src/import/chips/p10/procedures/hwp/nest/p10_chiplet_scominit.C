3d817185b21c3a0d38b48053792eb374f108f35c
Jenny Huynh <jhuynh@us.ibm.com>
2019-12-30 16:20:11 -0500

    Apply switch_cd in p10_chiplet_scominit instead of p10_build_smp
    p10_build_smp (phase2 mode) is not called in single-drawer configs, so
    the switch_cd command needs to be issued in a different procedure after
    p10_ipl_customize (istep 10.2) to guarantee that it is always applied.
    
    Patch 1: Implement switch_cd in p10_chiplet_scominit
    Patch 2: Remove switch_cd implementation from p10_build_smp
    
    Change-Id: I7106f2d9c01983f165df933467f16bcd4390d78d
    Original-Change-Id: Ibbe97a527195a587335872002ce976010d11c961
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/89137
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Thi N Tran <thi@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/90447
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/nest/p10_chiplet_scominit.C
+++ src/import/chips/p10/procedures/hwp/nest/p10_chiplet_scominit.C
@@ -24,13 +24,12 @@
 /* IBM_PROLOG_END_TAG                                                     */
 ///
 /// @file p10_chiplet_scominit.C
-///
 /// @brief SCOM inits to all chiplets (sans Quad/fabric)
 ///
-
-// *HWP HW Maintainer: Joe McGill <jmcgill@us.ibm.com>
-// *HWP FW Maintainer: Ilya Smirnov <ismirno@us.ibm.com>
-// *HWP Consumed by: HB
+/// *HWP HW Maintainer: Joe McGill <jmcgill@us.ibm.com>
+/// *HWP FW Maintainer: Ilya Smirnov <ismirno@us.ibm.com>
+/// *HWP Consumed by: HB
+///
 
 //------------------------------------------------------------------------------
 // Includes
@@ -39,24 +38,105 @@
 #include <p10_mi_omi_scom.H>
 #include <p10_mcc_omi_scom.H>
 
-//------------------------------------------------------------------------------
-// Constant definitions
-//------------------------------------------------------------------------------
+#include <p10_fbc_utils.H>
+#include <p10_putmemproc.H>
+#include <fapi2_subroutine_executor.H>
+#include <fapi2_mem_access.H>
+
+#include <p10_scom_proc_8.H>
+#include <p10_scom_proc_e.H>
 
 //------------------------------------------------------------------------------
 // Function definitions
 //------------------------------------------------------------------------------
 
-fapi2::ReturnCode p10_chiplet_scominit(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+///
+/// @brief Activate fabric async configuration via switch_cd
+///        Note that the performance optimized settings are preconfigured
+///        into the fabric Mode D registers via dynamic inits
+///
+/// @param[in] i_target         Reference to processor chip target
+/// @return fapi::ReturnCode    FAPI2_RC_SUCCESS if success, else error code.
+///
+fapi2::ReturnCode p10_chiplet_scominit_fbc_cd(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
 {
-    fapi2::ReturnCode l_rc;
-    fapi2::Target<fapi2::TARGET_TYPE_SYSTEM> FAPI_SYSTEM;
-    std::vector<fapi2::Target<fapi2::TARGET_TYPE_OMI>> l_omi_targets;
+    using namespace scomt;
+    using namespace scomt::proc;
 
     FAPI_DBG("Start");
 
+    uint32_t l_flags = fapi2::SBE_MEM_ACCESS_FLAGS_TARGET_PROC;
+    uint64_t l_addr_unused = 0x0ULL;
+    uint32_t l_bytes_unused = 1;
+    uint8_t l_data_unused[1];
+    fapi2::ReturnCode l_rc;
+
+    fapi2::buffer<uint64_t> l_station_mode(0x0);
+    fapi2::buffer<uint64_t> l_station_cfg3(0x0);
+
+    // configure fbc to abide by switch_cd signal
+    GET_PB_COM_SCOM_EQ0_STATION_MODE(i_target, l_station_mode);
+    SET_PB_COM_SCOM_EQ0_STATION_MODE_PB_CFG_SWITCH_CD_GATE_ENABLE_EQ0(l_station_mode);
+    FAPI_TRY(p10_fbc_utils_set_racetrack_regs(i_target, PB_COM_SCOM_EQ0_STATION_MODE, l_station_mode),
+             "Error from p10_fbc_utils_set_racetrack_regs (PB_COM_SCOM_EQ0_STATION_MODE)");
+
+    // select to apply mode D on switch_cd signal
+    GET_PB_COM_SCOM_EQ0_STATION_CFG3(i_target, l_station_cfg3);
+    SET_PB_COM_SCOM_EQ0_STATION_CFG3_PB_CFG_PBIASY_UNIT0_SELCD(l_station_cfg3);
+    SET_PB_COM_SCOM_EQ0_STATION_CFG3_PB_CFG_PBIASY_UNIT1_SELCD(l_station_cfg3);
+    SET_PB_COM_SCOM_EQ0_STATION_CFG3_PB_CFG_PBIASY_LINK0_SELCD(l_station_cfg3);
+    SET_PB_COM_SCOM_EQ0_STATION_CFG3_PB_CFG_PBIASY_LINK1_SELCD(l_station_cfg3);
+    FAPI_TRY(p10_fbc_utils_set_racetrack_regs(i_target, PB_COM_SCOM_EQ0_STATION_CFG3, l_station_cfg3),
+             "Error from p10_fbc_utils_set_racetrack_regs (PB_COM_SCOM_EQ0_STATION_CFG3)");
+
+    // issue switch_cd to apply new configuration
+    FAPI_CALL_SUBROUTINE(l_rc,
+                         p10_putmemproc,
+                         i_target,
+                         l_addr_unused,
+                         l_bytes_unused,
+                         l_data_unused,
+                         l_flags | fapi2::SBE_MEM_ACCESS_FLAGS_PRE_SWITCH_CD_MODE);
+    FAPI_TRY(l_rc, "Error from p10_putmemproc when setting switch_cd config bit pre-switch");
+
+    FAPI_CALL_SUBROUTINE(l_rc,
+                         p10_putmemproc,
+                         i_target,
+                         l_addr_unused,
+                         l_bytes_unused,
+                         l_data_unused,
+                         l_flags | fapi2::SBE_MEM_ACCESS_FLAGS_SWITCH_MODE);
+    FAPI_TRY(l_rc, "Error from p10_putmemproc when issuing switch_cd command");
+
+    FAPI_CALL_SUBROUTINE(l_rc,
+                         p10_putmemproc,
+                         i_target,
+                         l_addr_unused,
+                         l_bytes_unused,
+                         l_data_unused,
+                         l_flags | fapi2::SBE_MEM_ACCESS_FLAGS_POST_SWITCH_MODE);
+    FAPI_TRY(l_rc, "Error from p10_putmemproc when clearing switch_cd config bit post-switch");
+
+fapi_try_exit:
+    FAPI_DBG("End");
+    return fapi2::current_err;
+}
+
+/// See doxygen comments in header file
+fapi2::ReturnCode p10_chiplet_scominit(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
+{
+    FAPI_DBG("Start");
+
+    fapi2::ReturnCode l_rc;
+    fapi2::Target<fapi2::TARGET_TYPE_SYSTEM> FAPI_SYSTEM;
+    std::vector<fapi2::Target<fapi2::TARGET_TYPE_OMI>> l_omi_targets;
     auto l_mi_targets = i_target.getChildren<fapi2::TARGET_TYPE_MI>();
 
+    // activate fabric async configuration (switch_cd)
+    FAPI_TRY(p10_chiplet_scominit_fbc_cd(i_target),
+             "Error from p10_build_smp_set_fbc_cd");
+
+    // apply omi scom initfiles
     for (const auto& l_mi_target : l_mi_targets)
     {
         auto l_mcc_targets = l_mi_target.getChildren<fapi2::TARGET_TYPE_MCC>();
