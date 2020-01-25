d02ab4eb12704e76d2fdc3767e903f463ff8f211
Yue Du <daviddu@us.ibm.com>
2020-01-24 21:05:23 -0600

    Istep4: enable Skew Adjust Polling and Clock Stat check
    - removed bypass switches to make the HWP check
      clock stat and skew adjust done
    
    Change-Id: Ia8974b91d5fb2b54812d4a93a3e0597e8092a5aa
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/90327
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Christopher M Riedl <cmr@ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/90329
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/corecache/p10_hcd_corecache_clock_control.mk
+++ src/import/chips/p10/procedures/hwp/corecache/p10_hcd_corecache_clock_control.mk
@@ -5,7 +5,7 @@
 #
 # OpenPOWER HostBoot Project
 #
-# Contributors Listed Below - COPYRIGHT 2019
+# Contributors Listed Below - COPYRIGHT 2019,2020
 # [+] International Business Machines Corp.
 #
 #
@@ -25,6 +25,4 @@
 PROCEDURE=p10_hcd_corecache_clock_control
 $(call ADD_MODULE_SRCDIR,$(PROCEDURE),$(ROOTPATH)/chips/p10/common/include)
 $(call ADD_MODULE_SRCDIR,$(PROCEDURE),$(ROOTPATH)/chips/p10/procedures/hwp/lib)
-# TODO:  remove when clock stat works.
-lib$(PROCEDURE)_COMMONFLAGS += -DEQ_CLOCK_STAT_DISABLE
 $(call BUILD_PROCEDURE)
