0e91cddd1db1039b0270eab6d740539248e554fe
Jenny Huynh <jhuynh@us.ibm.com>
2020-03-16 19:19:20 -0400

    Add placeholders for IOO PHY FIR settings in p10_smp_link_firs
    - add placeholders to apply link PHY FIR settings
    - remove dlr psave configuration (leave to ptl initfiles)
    
    Change-Id: Id6ec329a90997a5d8466a241db24877bd1481006
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/93437
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Christopher W Steffen <cwsteffen@us.ibm.com>
    Reviewed-by: Benjamin Gass <bgass@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/93440
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/nest/p10_smp_link_firs.C
+++ src/import/chips/p10/procedures/hwp/nest/p10_smp_link_firs.C
@@ -70,10 +70,16 @@ struct fir_registers
     uint64_t DLP_FIR_ACTION1;
     uint64_t DLP_FIR_MASK[NUM_SUBLINK_OPTS][NUM_MASK_OPTS];
 
+    // PHY FIR Constants
+    uint64_t PHY_FIR_ACTION0;
+    uint64_t PHY_FIR_ACTION1;
+    uint64_t PHY_FIR_MASK[NUM_SUBLINK_OPTS][FABRIC_NUM_IOHS_LINKS][NUM_MASK_OPTS];
+
     // Selected masks from above arrays, populated by functions
     uint64_t FBC_EXT_FIR_MASK_SEL[NUM_MASK_OPTS];
     uint64_t PB_PTL_FIR_MASK_SEL[NUM_MASK_OPTS];
     uint64_t DLP_FIR_MASK_SEL[NUM_MASK_OPTS];
+    uint64_t PHY_FIR_MASK_SEL[NUM_MASK_OPTS];
 };
 
 // @TODO RTC209314 Fill in actual FIR mask/action values
@@ -142,6 +148,47 @@ struct fir_registers firs_inactive =
         { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // both halves
         { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // even only
         { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // odd only
+    },
+    .PHY_FIR_ACTION0     = 0x0000000000000000,
+    .PHY_FIR_ACTION1     = 0x0000000000000000,
+    .PHY_FIR_MASK        =
+    {
+        // both even/odd
+        {
+            // bits to modify   , value to program
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs0
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs1
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs2
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs3
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs4
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs5
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs6
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs7
+        },
+        // even only
+        {
+            // bits to modify   , value to program
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs0
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs1
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs2
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs3
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs4
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs5
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs6
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs7
+        },
+        // odd only
+        {
+            // bits to modify   , value to program
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs0
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs1
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs2
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs3
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs4
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs5
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs6
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs7
+        },
     },
 };
 
@@ -212,22 +259,48 @@ struct fir_registers firs_runtime =
         { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // even only
         { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // odd only
     },
+    .PHY_FIR_ACTION0     = 0x0000000000000000,
+    .PHY_FIR_ACTION1     = 0x0000000000000000,
+    .PHY_FIR_MASK        =
+    {
+        // both even/odd
+        {
+            // bits to modify   , value to program
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs0
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs1
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs2
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs3
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs4
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs5
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs6
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs7
+        },
+        // even only
+        {
+            // bits to modify   , value to program
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs0
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs1
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs2
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs3
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs4
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs5
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs6
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs7
+        },
+        // odd only
+        {
+            // bits to modify   , value to program
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs0
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs1
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs2
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs3
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs4
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs5
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs6
+            { 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF }, // iohs7
+        },
+    },
 };
-
-// PB PTL PSave Constants
-// @TODO RTC209315 Fill in actual psave values
-const uint8_t PB_PSAVE_MODE_CFG_MODE        = 0x00;
-const uint8_t PB_PSAVE_MODE_CFG_WIDTH       = 0x00;
-const uint8_t PB_PSAVE_MODE_CFG_MIN_RAND_UC = 0x00;
-const uint8_t PB_PSAVE_MISC_CFG_HALF_LUC    = 0x00;
-const uint8_t PB_PSAVE_MISC_CFG_HALF_HUC    = 0x00;
-const uint8_t PB_PSAVE_MISC_CFG_HALF_LUT    = 0x00;
-const uint8_t PB_PSAVE_MISC_CFG_HALF_HUT    = 0x00;
-const uint8_t PB_PSAVE_MISC_CFG_QTR_LUC     = 0x00;
-const uint8_t PB_PSAVE_MISC_CFG_QTR_HUC     = 0x00;
-const uint8_t PB_PSAVE_MISC_CFG_QTR_LUT     = 0x00;
-const uint8_t PB_PSAVE_MISC_CFG_QTR_HUT     = 0x00;
-const uint8_t PB_PSAVE_MISC_CFG_WSIZE       = 0x00;
 
 // DL Config Register Enums
 const uint8_t DLP_PHY_CONFIG_DL_SELECT_DLP  = 0x01;
@@ -260,70 +333,10 @@ void p10_smp_link_firs_display(
     FAPI_DBG("  DLP_FIR_ACTION1      = 0x%016llX", i_data.DLP_FIR_ACTION1);
     FAPI_DBG("  DLP_FIR_MASK_AND     = 0x%016llX", i_data.DLP_FIR_MASK_SEL[MASK_AND]);
     FAPI_DBG("  DLP_FIR_MASK_OR      = 0x%016llX", i_data.DLP_FIR_MASK_SEL[MASK_OR]);
-}
-
-/// @brief Configure PB PTL psave registers for dynamic lane reduction
-/// @param[in] i_iohs_target   Reference to iohs target
-/// @param[in] i_iohs_pos      Chiplet position for iohs to configure
-/// @return fapi2::ReturnCode  FAPI2_RC_SUCCESS if success, else error code.
-fapi2::ReturnCode p10_smp_link_firs_psave(
-    const fapi2::Target<fapi2::TARGET_TYPE_IOHS>& i_iohs_target,
-    const fapi2::ATTR_CHIP_UNIT_POS_Type& i_iohs_pos)
-{
-    using namespace scomt;
-    using namespace scomt::pauc;
-
-    uint64_t l_psave_mode_scom_addr;
-    uint64_t l_psave_misc_scom_addr;
-    fapi2::buffer<uint64_t> l_psave_mode_cfg(0x0);
-    fapi2::buffer<uint64_t> l_psave_misc_cfg(0x0);
-
-    l_psave_mode_scom_addr = (i_iohs_pos % 2) ? (PB_PSAVE23_MODE_CFG) : (PB_PSAVE01_MODE_CFG);
-    l_psave_misc_scom_addr = (i_iohs_pos % 2) ? (PB_PSAVE23_MISC_CFG) : (PB_PSAVE01_MISC_CFG);
-
-    auto l_pauc_target = i_iohs_target.getParent<fapi2::TARGET_TYPE_PAUC>();
-
-    if(!i_iohs_target.isFunctional())
-    {
-        goto fapi_try_exit;
-    }
-
-    FAPI_DBG("Configuring PB PSAVE Mode Register");
-
-    FAPI_TRY(getScom(l_pauc_target, l_psave_mode_scom_addr, l_psave_mode_cfg),
-             "Error from getScom (PB_PSAVE_MODE_CFG)");
-
-    scomt::disableRegchk();
-    SET_PB_PSAVE01_MODE_CFG_MODE(PB_PSAVE_MODE_CFG_MODE, l_psave_mode_cfg);
-    SET_PB_PSAVE01_MODE_CFG_WIDTH(PB_PSAVE_MODE_CFG_WIDTH, l_psave_mode_cfg);
-    SET_PB_PSAVE01_MODE_CFG_MIN_RAND_UC(PB_PSAVE_MODE_CFG_MIN_RAND_UC, l_psave_mode_cfg);
-    scomt::enableRegchk();
-
-    FAPI_TRY(putScom(l_pauc_target, l_psave_mode_scom_addr, l_psave_mode_cfg),
-             "Error from putScom (PB_PSAVE_MODE_CFG)");
-
-    FAPI_DBG("Configuring PB PSAVE Misc Register");
-
-    FAPI_TRY(getScom(l_pauc_target, l_psave_misc_scom_addr, l_psave_misc_cfg),
-             "Error from getScom (PB_PSAVE_MISC_CFG)");
-
-    scomt::disableRegchk();
-    SET_PB_PSAVE01_MISC_CFG_HALF_LUC(PB_PSAVE_MISC_CFG_HALF_LUC, l_psave_misc_cfg);
-    SET_PB_PSAVE01_MISC_CFG_HALF_HUC(PB_PSAVE_MISC_CFG_HALF_HUC, l_psave_misc_cfg);
-    SET_PB_PSAVE01_MISC_CFG_HALF_LUT(PB_PSAVE_MISC_CFG_HALF_LUT, l_psave_misc_cfg);
-    SET_PB_PSAVE01_MISC_CFG_HALF_HUT(PB_PSAVE_MISC_CFG_HALF_HUT, l_psave_misc_cfg);
-    SET_PB_PSAVE01_MISC_CFG_QTR_LUC(PB_PSAVE_MISC_CFG_QTR_LUC, l_psave_misc_cfg);
-    SET_PB_PSAVE01_MISC_CFG_QTR_HUC(PB_PSAVE_MISC_CFG_QTR_HUC, l_psave_misc_cfg);
-    SET_PB_PSAVE01_MISC_CFG_QTR_LUT(PB_PSAVE_MISC_CFG_QTR_LUT, l_psave_misc_cfg);
-    SET_PB_PSAVE01_MISC_CFG_QTR_HUT(PB_PSAVE_MISC_CFG_QTR_HUT, l_psave_misc_cfg);
-    SET_PB_PSAVE01_MISC_CFG_WSIZE(PB_PSAVE_MISC_CFG_WSIZE, l_psave_misc_cfg);
-    scomt::enableRegchk();
-
-    FAPI_TRY(putScom(l_pauc_target, l_psave_misc_scom_addr, l_psave_misc_cfg),
-             "Error from putScom (PB_PSAVE_MISC_CFG)");
-
-fapi_try_exit:
-    return fapi2::current_err;
+    FAPI_DBG("  PHY_FIR_ACTION0      = 0x%016llX", i_data.PHY_FIR_ACTION0);
+    FAPI_DBG("  PHY_FIR_ACTION1      = 0x%016llX", i_data.PHY_FIR_ACTION1);
+    FAPI_DBG("  PHY_FIR_MASK_AND     = 0x%016llX", i_data.PHY_FIR_MASK_SEL[MASK_AND]);
+    FAPI_DBG("  PHY_FIR_MASK_OR      = 0x%016llX", i_data.PHY_FIR_MASK_SEL[MASK_OR]);
 }
 
 /// @brief Clear contents of FBC EXT FIR register
@@ -353,7 +366,7 @@ fapi_try_exit:
     return fapi2::current_err;
 }
 
-/// @brief Clear contents of FBC TL FIR registers
+/// @brief Clear contents of FBC TL/PHY FIR registers
 /// @param[in] i_pauc_target   Reference to pauc target
 /// @return fapi2::ReturnCode  FAPI2_RC_SUCCESS if success, else error code.
 fapi2::ReturnCode p10_smp_link_firs_clear(
@@ -375,6 +388,13 @@ fapi2::ReturnCode p10_smp_link_firs_clear(
              "Error from prepScom (PB_PTL_FIR_REG_RW)");
     FAPI_TRY(PUT_PB_PTL_FIR_REG_RW(i_pauc_target, l_zeroes),
              "Error from putScom (PB_PTL_FIR_REG_RW)");
+
+    FAPI_DBG("Clearing PHY FIR register");
+
+    FAPI_TRY(PREP_PHY_SCOM_MAC_FIR_REG_RW(i_pauc_target),
+             "Error from prepScom (PHY_SCOM_MAC_FIR_REG_RW)");
+    FAPI_TRY(PUT_PHY_SCOM_MAC_FIR_REG_RW(i_pauc_target, l_zeroes),
+             "Error from putScom (PHY_SCOM_MAC_FIR_REG_RW)");
 
 fapi_try_exit:
     return fapi2::current_err;
@@ -537,6 +557,48 @@ fapi_try_exit:
     return fapi2::current_err;
 }
 
+/// @brief Configure PHY FIR Registers
+/// @param[in] i_pauc_target   Reference to pauc target
+/// @param[in] i_data          Data to program into fir registers
+/// @return fapi2::ReturnCode  FAPI2_RC_SUCCESS if success, else error code.
+fapi2::ReturnCode p10_smp_link_firs_phy(
+    const fapi2::Target<fapi2::TARGET_TYPE_PAUC>& i_pauc_target,
+    const struct fir_registers& i_data)
+{
+    using namespace scomt;
+    using namespace scomt::pauc;
+
+    if(!i_pauc_target.isFunctional())
+    {
+        goto fapi_try_exit;
+    }
+
+    FAPI_DBG("Configuring PHY Registers");
+
+    FAPI_TRY(PREP_PHY_SCOM_MAC_FIR_ACTION0_REG(i_pauc_target),
+             "Error from prepScom (PHY_SCOM_MAC_FIR_ACTION0_REG)");
+    FAPI_TRY(PUT_PHY_SCOM_MAC_FIR_ACTION0_REG(i_pauc_target, i_data.PHY_FIR_ACTION0),
+             "Error from putScom (PHY_SCOM_MAC_FIR_ACTION0_REG)");
+
+    FAPI_TRY(PREP_PHY_SCOM_MAC_FIR_ACTION1_REG(i_pauc_target),
+             "Error from prepScom (PHY_SCOM_MAC_FIR_ACTION1_REG)");
+    FAPI_TRY(PUT_PHY_SCOM_MAC_FIR_ACTION1_REG(i_pauc_target, i_data.PHY_FIR_ACTION1),
+             "Error from putScom (PHY_SCOM_MAC_FIR_ACTION1_REG)");
+
+    FAPI_TRY(PREP_PHY_SCOM_MAC_FIR_MASK_REG_WO_AND(i_pauc_target),
+             "Error from prepScom (PHY_SCOM_MAC_FIR_MASK_REG_WO_AND)");
+    FAPI_TRY(PUT_PHY_SCOM_MAC_FIR_MASK_REG_WO_AND(i_pauc_target, i_data.PHY_FIR_MASK_SEL[MASK_AND]),
+             "Error from putScom (PHY_SCOM_MAC_FIR_MASK_REG_WO_AND)");
+
+    FAPI_TRY(PREP_PHY_SCOM_MAC_FIR_MASK_REG_WO_OR(i_pauc_target),
+             "Error from putScom (PHY_SCOM_MAC_FIR_MASK_REG_WO_OR)");
+    FAPI_TRY(PUT_PHY_SCOM_MAC_FIR_MASK_REG_WO_OR(i_pauc_target, i_data.PHY_FIR_MASK_SEL[MASK_OR]),
+             "Error from putScom (PHY_SCOM_MAC_FIR_MASK_REG_WO_OR)");
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
 /// See doxygen comments in header file
 fapi2::ReturnCode p10_smp_link_firs(
     const fapi2::Target<fapi2::TARGET_TYPE_IOHS>& i_iohs_target,
@@ -626,11 +688,15 @@ fapi2::ReturnCode p10_smp_link_firs(
 
     l_reg_values.PB_PTL_FIR_MASK_SEL[MASK_AND] = ~l_reg_values.PB_PTL_FIR_MASK[i_sublink][l_iohs_pos][MASK_AND];
     l_reg_values.PB_PTL_FIR_MASK_SEL[MASK_OR] = l_reg_values.PB_PTL_FIR_MASK[i_sublink][l_iohs_pos][MASK_OR]
-            & l_reg_values.FBC_EXT_FIR_MASK[l_iohs_pos][MASK_AND];
+            & l_reg_values.PB_PTL_FIR_MASK[i_sublink][l_iohs_pos][MASK_AND];
 
     l_reg_values.DLP_FIR_MASK_SEL[MASK_AND] = ~l_reg_values.DLP_FIR_MASK[i_sublink][MASK_AND];
     l_reg_values.DLP_FIR_MASK_SEL[MASK_OR] = l_reg_values.DLP_FIR_MASK[i_sublink][MASK_OR]
             & l_reg_values.DLP_FIR_MASK[i_sublink][MASK_AND];
+
+    l_reg_values.PHY_FIR_MASK_SEL[MASK_AND] = ~l_reg_values.PHY_FIR_MASK[i_sublink][l_iohs_pos][MASK_AND];
+    l_reg_values.PHY_FIR_MASK_SEL[MASK_OR] = l_reg_values.PHY_FIR_MASK[i_sublink][l_iohs_pos][MASK_OR]
+            & l_reg_values.PHY_FIR_MASK[i_sublink][l_iohs_pos][MASK_AND];
 
     // Print values to be programmed
     p10_smp_link_firs_display(i_iohs_target, l_reg_values);
@@ -642,10 +708,8 @@ fapi2::ReturnCode p10_smp_link_firs(
              "Error from p10_smp_link_firs_tl");
     FAPI_TRY(p10_smp_link_firs_dl(i_iohs_target, l_reg_values),
              "Error from p10_smp_link_firs_dl");
-
-    // Configure PB PTL Psave (for dynamic lane reduction)
-    FAPI_TRY(p10_smp_link_firs_psave(i_iohs_target, l_iohs_pos),
-             "Error from p10_smp_link_firs_psave");
+    FAPI_TRY(p10_smp_link_firs_phy(l_pauc_target, l_reg_values),
+             "Error from p10_smp_link_firs_phy");
 
 fapi_try_exit:
     FAPI_DBG("Exiting...");
