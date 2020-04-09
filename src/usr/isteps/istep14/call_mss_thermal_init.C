2ee484ba5f908221fc1af0ebde638d8e6a4adbce
Roland Veloz <rveloz@us.ibm.com>
2020-04-09 00:42:52 -0500

    P10: 14.2::Initilaize the Thermal Sensor - updated for P10
    New architecture ... new code to support it.  The thermal sensor
    initialization code has been updated for P10.  Removed all references
    of NIMBUS, CUMULUS and AXONE.
    
    Added an include directive to the isteps/mss/makefile
    because it is pulling in the files that were mirrored
    for this  story.
    
    Change-Id: I4c38fd156c1b254f07935b7085e38f574c17324a
    RTC: 208831
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/94754
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: Roland Veloz <rveloz@us.ibm.com>
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep14/call_mss_thermal_init.C
+++ src/usr/isteps/istep14/call_mss_thermal_init.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2019                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2020                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -22,269 +22,233 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
-#include <errl/errlentry.H>
-#include <errl/errlmanager.H>
-#include <errl/errludtarget.H>
 
-#include <isteps/hwpisteperror.H>
-#include <initservice/isteps_trace.H>
+/**
+ *  @file call_mss_thermal_init.C
+ *
+ *  @details Run Thermal Sensor Initialization on a set of targets
+ *
+ */
 
-//  targeting support
-#include <targeting/common/commontargeting.H>
-#include <targeting/common/util.H>
-#include <targeting/common/utilFilter.H>
+/******************************************************************************/
+// Includes
+/******************************************************************************/
 
-#include    <config.h>
-/* FIXME RTC: 210975
-#include    <fapi2.H>
-#include    <fapi2/plat_hwp_invoker.H>
-#include    <p9_mss_thermal_init.H>
-#include    <p9_throttle_sync.H>
+// Standard headers
+#include <stdint.h>                      // uint32_t
 
-#ifdef CONFIG_AXONE
-    //@TODO RTC:195557 #include    <exp_thermal_init.H>
-    #include    <chipids.H> // for EXPLORER ID
-#endif
-*/
+// Trace
+#include <trace/interface.H>             // TRACFCOMP
+#include <initservice/isteps_trace.H>    // ISTEPS_TRACE::g_trac_isteps_trace
 
+// Error logging
+#include <errl/errlentry.H>              // errlHndl_t
+#include <isteps/hwpisteperror.H>        // IStepError, getErrorHandle
+#include <istepHelperFuncs.H>            // captureError
+
+// Targeting support
+#include <targeting/common/target.H>     // TargetHandleList, getAttr
+#include <targeting/common/utilFilter.H> // getAllChips
+
+// Fapi
+#include <fapi2/target.H>                // fapi2::TARGET_TYPE_OCMB_CHIP
+#include <fapi2/plat_hwp_invoker.H>      // FAPI_INVOKE_HWP
+
+// HWP
+#include <exp_mss_thermal_init.H>        // exp_mss_thermal_init
+#include <p10_throttle_sync.H>           // p10_throttle_sync
+
+// Misc
+#include <chipids.H>                     // POWER_CHIPID::EXPLORER_16
+
+/******************************************************************************/
+// namespace shortcuts
+/******************************************************************************/
 using   namespace   ISTEP;
 using   namespace   ISTEP_ERROR;
 using   namespace   ERRORLOG;
 using   namespace   TARGETING;
 
+
 namespace ISTEP_14
 {
-void nimbus_call_mss_thermal_init(IStepError & io_istepError);
-void cumulus_call_mss_thermal_init(IStepError & io_istepError);
-void axone_call_mss_thermal_init(IStepError & io_istepError);
-void run_proc_throttle_sync(IStepError & io_istepError);
+// Forward declare these methods
+void p10_call_mss_thermal_init(IStepError & io_iStepError);
+void run_proc_throttle_sync(IStepError & io_iStepError);
 
-void* call_mss_thermal_init (void *io_pArgs)
+/**
+ * @brief Run Thermal Sensor Initialization followed by Process
+ *        Throttle Synchronization on a list of targets
+ *
+ * @return nullptr if success, else a handle to an error log
+ */
+void* call_mss_thermal_init (void*)
 {
-    IStepError  l_StepError;
-    TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace, "call_mss_thermal_init entry");
+    IStepError  l_iStepError;
 
-    auto l_procModel = TARGETING::targetService().getProcessorModel();
-    switch (l_procModel)
+    TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+              ENTER_MRK"call_mss_thermal_init");
+
+    // Call HWP to thermal initialization on a list of OCMB chips
+    p10_call_mss_thermal_init(l_iStepError);
+
+    // Do not continue if the HWP call to thermal initialization encounters an
+    // error. Breaking out here will facilitate in the efficiency of the
+    // reconfig loop and not cause confusion for the next HWP call.
+    if ( !l_iStepError.isNull() )
     {
-        case TARGETING::MODEL_CUMULUS:
-            cumulus_call_mss_thermal_init(l_StepError);
-            break;
-        case TARGETING::MODEL_AXONE:
-            axone_call_mss_thermal_init(l_StepError);
-            break;
-        case TARGETING::MODEL_NIMBUS:
-            nimbus_call_mss_thermal_init(l_StepError);
-            break;
-        default:
-            assert(0, "call_mss_thermal_init: Unsupported model type 0x%04X",
-                l_procModel);
-            break;
+        TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, ERR_MRK
+                   "ERROR: call_mss_thermal_init exited early because "
+                   "p10_call_mss_thermal_init had failures" );
+    }
+    else
+    {
+        // If no prior error, then call HWP to processor throttle
+        // synchronization on a list of PROC chips
+        run_proc_throttle_sync(l_iStepError);
     }
 
-    run_proc_throttle_sync(l_StepError);
+    TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+              EXIT_MRK"call_mss_thermal_init");
 
-    TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace, "call_mss_thermal_init exit");
+
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+               EXIT_MRK"call_mss_thermal_init, returning %s",
+               (l_iStepError.isNull()? "success" : "failure") );
 
     // end task, returning any errorlogs to IStepDisp
-    return l_StepError.getErrorHandle();
-}
+    return l_iStepError.getErrorHandle();
+} // call_mss_thermal_init
 
-#ifndef CONFIG_AXONE
-void nimbus_call_mss_thermal_init(IStepError & io_istepError)
+/**
+ * @brief Run Thermal Sensor Initialization on a list Explorer OCMB chips
+ *
+ * param[in/out] io_iStepError - Container for errors if an error occurs
+ */
+void p10_call_mss_thermal_init(IStepError & io_iStepError)
 {
-/* FIXME RTC: 210975
-    errlHndl_t  l_errl  =   nullptr;
+    errlHndl_t l_err(nullptr);
 
-    // -- Nimbus only ---
-    // Get all MCS targets
-    TARGETING::TargetHandleList l_mcsTargetList;
-    getAllChiplets(l_mcsTargetList, TYPE_MCS);
-
-    //  --------------------------------------------------------------------
-    //  run mss_thermal_init on all functional MCS chiplets
-    //  --------------------------------------------------------------------
-    for (const auto & l_pMcs : l_mcsTargetList)
-    {
-        fapi2::Target<fapi2::TARGET_TYPE_MCS> l_fapi_pMcs(l_pMcs);
-
-        // Current run on target
-        TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                   "Running p9_mss_thermal_init HWP on target HUID %.8X",
-                   TARGETING::get_huid(l_pMcs) );
-
-        FAPI_INVOKE_HWP( l_errl, p9_mss_thermal_init, l_fapi_pMcs );
-
-        if ( l_errl )
-        {
-            TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-                      "ERROR 0x%.8X: p9_mss_thermal_init HWP returns error",
-                      l_errl->reasonCode());
-
-            // capture the target data in the elog
-            ErrlUserDetailsTarget(l_pMcs).addToLog( l_errl );
-
-            // Create IStep error log and cross reference
-            // to error that occurred
-            io_istepError.addErrorDetails( l_errl );
-
-            // Commit Error
-            errlCommit( l_errl, HWPF_COMP_ID );
-
-            break;
-        }
-        else
-        {
-            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                       "SUCCESS : p9_mss_thermal_init HWP() on 0x%.8X MCS",
-                       TARGETING::get_huid(l_pMcs) );
-        }
-    } // end MCS loop
-*/
-}
-
-void cumulus_call_mss_thermal_init(IStepError & io_istepError)
-{
-
-}
-#else
-void nimbus_call_mss_thermal_init(IStepError & io_istepError)
-{
-    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-              "Error: Trying to call 'p9_mss_thermal_init' but Nimbus code is not compiled in");
-    assert(0, "Calling wrong Model's HWPs");
-}
-
-void cumulus_call_mss_thermal_init(IStepError & io_istepError)
-{
-    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-              "Error: Trying to call 'p9c_mss_thermal_init' but Cumulus code is not compiled in");
-    assert(0, "Calling wrong Model's HWPs");
-}
-#endif
-
-#ifdef CONFIG_AXONE
-void axone_call_mss_thermal_init(IStepError & io_istepError)
-{
-/* FIXME RTC: 210975
-    errlHndl_t l_err = nullptr;
-
-    // Get all OCMB targets
+    // Get a list of all OCMB chips to run Thermal Sensor Initialization on
     TARGETING::TargetHandleList l_ocmbTargetList;
     getAllChips(l_ocmbTargetList, TYPE_OCMB_CHIP);
 
-    for (const auto & l_ocmb_target : l_ocmbTargetList)
+    for (const auto & l_ocmbTarget : l_ocmbTargetList)
     {
-        // check EXPLORER first as this is most likely the configuration
-        uint32_t chipId = l_ocmb_target->getAttr< TARGETING::ATTR_CHIP_ID>();
-        if (chipId == POWER_CHIPID::EXPLORER_16)
+        // Only run Thermal Sensor Initialization (exp_mss_thermal_init)
+        // on Explorer OCMBs.
+        uint32_t l_chipId = l_ocmbTarget->getAttr<TARGETING::ATTR_CHIP_ID>();
+        if (l_chipId == POWER_CHIPID::EXPLORER_16)
         {
-            fapi2::Target <fapi2::TARGET_TYPE_OCMB_CHIP> l_fapi_ocmb_target
-                (l_ocmb_target);
+            // Convert the TARGETING::Target into a fapi2::Target by passing
+            // l_ocmbTarget into the fapi2::Target constructor
+            fapi2::Target <fapi2::TARGET_TYPE_OCMB_CHIP>
+                                             l_fapiOcmbTarget ( l_ocmbTarget );
 
-            // Dump current run on target
-            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                "Running exp_thermal_init HWP on target HUID %.8X",
-                TARGETING::get_huid(l_ocmb_target));
+            // Calling exp_mss_thermal_init HWP on Explorer chip,
+            // trace out stating so
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, INFO_MRK
+                       "Running exp_mss_thermal_init HWP call on Explorer "
+                       "chip, target HUID 0x%.8X, chipId 0x%.8X",
+                       TARGETING::get_huid(l_ocmbTarget),
+                       l_chipId );
 
-            // call the HWP with each fapi2::Target
-            // @todo RTC 195557 FAPI_INVOKE_HWP(l_err, exp_thermal_init, l_fapi_ocmb_target);
-        }
+            // Call the HWP exp_mss_thermal_init call on each fapi2::Target
+            FAPI_INVOKE_HWP(l_err, exp_mss_thermal_init, l_fapiOcmbTarget);
+
+            if (l_err)
+            {
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, ERR_MRK
+                           "ERROR: exp_mss_thermal_init HWP call on Explorer "
+                           "chip, target HUID 0x%08x, chipId 0x%.8X failed."
+                           TRACE_ERR_FMT,
+                           get_huid(l_ocmbTarget),
+                           l_chipId,
+                           TRACE_ERR_ARGS(l_err) );
+
+                // Capture error, commit and continue, do not break out here.
+                // Continue here and consolidate all the errors that might
+                // occur in a batch before bailing. This will facilitate in the
+                // efficiency of the reconfig loop.
+                captureError(l_err, io_iStepError, HWPF_COMP_ID, l_ocmbTarget);
+            }
+            else
+            {
+                TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, INFO_MRK
+                           "SUCCESS: exp_mss_thermal_init HWP on Explorer "
+                           "chip, target HUID 0x%.8X, chipId 0x%.8X",
+                           TARGETING::get_huid(l_ocmbTarget),
+                           l_chipId );
+            }
+        } // end if (l_chipId == POWER_CHIPID::EXPLORER_16)
         else
         {
-            // Gemini, NOOP
-            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                "Skipping thermal_init HWP on axone target HUID 0x%.8X, chipId 0x%.4X",
-                TARGETING::get_huid(l_ocmb_target), chipId );
+            // Non-Explorer chip, a NOOP operation, trace out stating so
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, INFO_MRK
+                       "Skipping call to exp_mss_thermal_init HWP on target "
+                       "HUID 0x%.8X, chipId 0x%.8X is not an Explorer OCMB",
+                       TARGETING::get_huid(l_ocmbTarget),
+                       l_chipId );
         }
+    } // end for (const auto & l_ocmbTarget : l_ocmbTargetList)
+} // p10_call_mss_thermal_init
 
-        if (l_err)
-        {
-            TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-                      "ERROR 0x%.8X: exp_thermalinit HWP returns error",
-                      l_err->reasonCode());
 
-            // capture the target data in the elog
-            ErrlUserDetailsTarget(l_ocmb_target).addToLog(l_err);
-
-            // Create IStep error log and cross reference to error that
-            // occurred
-            io_istepError.addErrorDetails( l_err );
-
-            // Commit Error
-            errlCommit( l_err, HWPF_COMP_ID );
-
-            break;
-        }
-        else if (chipId == POWER_CHIPID::EXPLORER_16)
-        {
-            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                  "SUCCESS running exp_thermalinit HWP on target HUID %.8X",
-                  TARGETING::get_huid(l_ocmb_target));
-        }
-    } // end OCMB loop
-*/
-}
-#else
-void axone_call_mss_thermal_init(IStepError & io_istepError)
+/**
+ * @brief Run Processor Throttle Synchronization on all functional
+ *        Processor Chip targets
+ *
+ * param[in/out] io_iStepError - Container for errors if an error occurs
+ *
+ */
+void run_proc_throttle_sync(IStepError & io_iStepError)
 {
-    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-              "Error: Trying to call 'exp_thermal_init' but Axone code is not compiled in");
-    assert(0, "Calling wrong Model's HWPs");
-}
-#endif
+    errlHndl_t  l_err(nullptr);
 
-void run_proc_throttle_sync(IStepError & io_istepError)
-{
-/* FIXME RTC: 210975
-    errlHndl_t  l_errl  =   nullptr;
-
-    // Run proc throttle sync
-    // Get all functional proc chip targets
-    // Use targeting code to get a list of all processors
+    // Get a list of all processors to run Throttle Synchronization on
     TARGETING::TargetHandleList l_procChips;
     getAllChips( l_procChips, TARGETING::TYPE_PROC );
 
     for (const auto & l_procChip: l_procChips)
     {
-        //Convert the TARGETING::Target into a fapi2::Target by passing
-        //l_procChip into the fapi2::Target constructor
+        // Convert the TARGETING::Target into a fapi2::Target by passing
+        // l_procChip into the fapi2::Target constructor
         fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>
-                                           l_fapi2CpuTarget((l_procChip));
+                                           l_fapiProcTarget(l_procChip);
 
-        TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                   "Running p9_throttle_sync HWP on target HUID %.8X",
+        // Calling p10_throttle_sync HWP on Explorer chip,
+        // trace out stating so
+        TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, INFO_MRK
+                   "Running p10_throttle_sync HWP on PROC target HUID 0x%.8X",
                    TARGETING::get_huid(l_procChip) );
 
-        // Call p9_throttle_sync
-        FAPI_INVOKE_HWP( l_errl, p9_throttle_sync, l_fapi2CpuTarget );
+        // Call the HWP p10_throttle_sync call on each fapi2::Target
+        FAPI_INVOKE_HWP( l_err, p10_throttle_sync, l_fapiProcTarget );
 
-        if (l_errl)
+        if (l_err)
         {
-            TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
-                      "ERROR 0x%.8X: p9_throttle_sync HWP returns error",
-                      l_errl->reasonCode());
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, ERR_MRK
+                       "ERROR: p10_throttle_sync HWP call on PROC target "
+                       "HUID 0x%08x failed."
+                       TRACE_ERR_FMT,
+                       get_huid(l_procChip),
+                       TRACE_ERR_ARGS(l_err) );
 
-            // Capture the target data in the elog
-            ErrlUserDetailsTarget(l_procChip).addToLog(l_errl);
-
-            // Create IStep error log and cross reference
-            //  to error that occurred
-            io_istepError.addErrorDetails( l_errl );
-
-            // Commit Error
-            errlCommit( l_errl, HWPF_COMP_ID );
-
-            break;
+            // Capture error, commit and continue, do not break out here.
+            // Continue here and consolidate all the errors that might
+            // occur in a batch before bailing. This will facilitate in the
+            // efficiency of the reconfig loop.
+            captureError(l_err, io_iStepError, HWPF_COMP_ID, l_procChip);
         }
         else
         {
-            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
-                       "SUCCESS :  p9_throttle_sync HWP( ) on 0x%.8X processor",
+            TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace, INFO_MRK
+                       "SUCCESS : p10_throttle_sync HWP call on "
+                       "PROC target HUID 0x%08x",
                        TARGETING::get_huid(l_procChip) );
         }
     }
-*/
-}
+} // run_proc_throttle_sync
 
-};
+};  // namespace ISTEP_14
+
