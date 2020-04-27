be22a3c8ab17283356470241ca4643cf235c1fd5
Yun Pan <yun.pan1@ibm.com>
2020-04-27 09:37:10 -0500

    Update Istep 12.9
    - Add HWP to the substep procedure
    
    Change-Id: Ifccc58df52c5b19f7ca24b2a4aa34e8aa2e9c27a
    RTC: 248244
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/95800
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: Christian R Geddes <crgeddes@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep12/call_omi_post_trainadv.C
+++ src/usr/isteps/istep12/call_omi_post_trainadv.C
@@ -39,10 +39,18 @@
 #include    <errl/errludtarget.H>
 
 #include    <initservice/isteps_trace.H>
+#include    <istepHelperFuncs.H>          // captureError
+
+#include    <fapi2/plat_hwp_invoker.H>
+
+//HWP
+#include    <p10_io_omi_post_trainadv.H>
+
 
 using   namespace   ISTEP;
 using   namespace   ISTEP_ERROR;
 using   namespace   ISTEPS_TRACE;
+using   namespace   TARGETING;
 
 namespace ISTEP_12
 {
@@ -50,13 +58,45 @@ namespace ISTEP_12
 void* call_omi_post_trainadv (void *io_pArgs)
 {
     IStepError l_StepError;
+    errlHndl_t l_err = nullptr;
 
-    TRACFCOMP( g_trac_isteps_trace, "call_omi_post_trainadv entry" );
+    TargetHandleList l_procTargetList;
+    getAllChips(l_procTargetList, TYPE_PROC);
+    TRACFCOMP( g_trac_isteps_trace, "call_omi_post_trainadv entry. "
+        "%d PROCs found", l_procTargetList.size());
 
     // 12.9.a p10_io_omi_post_trainadv.C
-    //        - Currently doesn't exist
     //        - Debug routine for IO characterization
-    //        - TODO: RTC 248244
+    for (const auto & l_proc_target : l_procTargetList)
+    {
+        TRACFCOMP( g_trac_isteps_trace,
+            "p10_io_omi_post_trainadv HWP target HUID 0x%.8x",
+            get_huid(l_proc_target));
+
+        //  call the HWP with each target
+        fapi2::Target <fapi2::TARGET_TYPE_PROC_CHIP> l_fapi_proc_target
+                (l_proc_target);
+
+        FAPI_INVOKE_HWP(l_err, p10_io_omi_post_trainadv, l_fapi_proc_target);
+
+        //  process return code.
+        if ( l_err )
+        {
+            TRACFCOMP( g_trac_isteps_trace,
+                "ERROR : call p10_io_omi_post_trainadv HWP(): failed on "
+                "target 0x%08X. " TRACE_ERR_FMT,
+                get_huid(l_proc_target),
+                TRACE_ERR_ARGS(l_err));
+
+            // Capture error
+            captureError(l_err, l_StepError, HWPF_COMP_ID, l_proc_target);
+        }
+        else
+        {
+            TRACFCOMP( g_trac_isteps_trace,
+                     "SUCCESS : p10_io_omi_post_trainadv HWP");
+        }
+    }
 
     TRACFCOMP( g_trac_isteps_trace, "call_omi_post_trainadv exit" );
 
