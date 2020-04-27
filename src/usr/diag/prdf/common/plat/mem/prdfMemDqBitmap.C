f0daff609fe4fd0508eeae730a41c6f1c9655c96
Caleb Palmer <cnpalmer@us.ibm.com>
2020-04-27 14:59:14 -0500

    PRD: Update spare info in mem dq bitmap
    Change-Id: I1afd5e60254cf43736bb55f7cb97034d79b73ccd
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/95825
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Benjamen G Tyner <ben.tyner@ibm.com>
    Reviewed-by: Brian J Stegmiller <bjs@us.ibm.com>
    Reviewed-by: Zane C Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/96125
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>

--- src/usr/diag/prdf/common/plat/mem/prdfMemDqBitmap.C
+++ src/usr/diag/prdf/common/plat/mem/prdfMemDqBitmap.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2013,2019                        */
+/* Contributors Listed Below - COPYRIGHT 2013,2020                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -506,16 +506,14 @@ uint32_t __getSpareInfo( TargetHandle_t i_trgt, MemRank i_rank,
         // Generic/MEM_PORT case
         else
         {
-            /* TODO RTC 192544
-            o_noSpare     = CEN_VPD_DIMM_SPARE_NO_SPARE;
-            o_lowNibble   = CEN_VPD_DIMM_SPARE_LOW_NIBBLE;
-            o_highNibble  = CEN_VPD_DIMM_SPARE_HIGH_NIBBLE;
-            o_spareConfig = CEN_VPD_DIMM_SPARE_NO_SPARE;
+            o_noSpare     = MEM_EFF_DIMM_SPARE_NO_SPARE;
+            o_lowNibble   = MEM_EFF_DIMM_SPARE_LOW_NIBBLE;
+            o_highNibble  = MEM_EFF_DIMM_SPARE_HIGH_NIBBLE;
+            o_spareConfig = MEM_EFF_DIMM_SPARE_NO_SPARE;
             TargetHandle_t memPort = getConnectedChild( i_trgt, TYPE_MEM_PORT,
                                                         i_portSlct );
             o_rc = getDimmSpareConfig<TYPE_MEM_PORT>( memPort, i_rank,
                 i_portSlct, o_spareConfig );
-            */
         }
 
         if( SUCCESS != o_rc )
