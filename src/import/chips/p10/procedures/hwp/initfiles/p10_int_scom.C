c97193de4589afa97c876717e8ba8b348db177e5
Minoru Fernando <Minoru.Fernando@ibm.com>
2020-06-22 13:23:30 -0400

    INT Scan/Scom Initfile Updates
    - Add chip ec attributes for defects
    - Updated attribute EQUALs to reflect DD-levels
    
    Change-Id: I7d153fabc31d8e31a2b81f368651b8dc4d6fdd78
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/99257
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Jenny Huynh <jhuynh@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/99618
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/initfiles/p10_int_scom.C
+++ src/import/chips/p10/procedures/hwp/initfiles/p10_int_scom.C
@@ -29,6 +29,22 @@
 
 using namespace fapi2;
 
+constexpr uint64_t literal_0x2000120076F0007E = 0x2000120076F0007E;
+constexpr uint64_t literal_0b00 = 0b00;
+constexpr uint64_t literal_0b1 = 0b1;
+constexpr uint64_t literal_0x775643213F3F00 = 0x775643213F3F00;
+constexpr uint64_t literal_0x77564321000000 = 0x77564321000000;
+constexpr uint64_t literal_0x0101010101010128 = 0x0101010101010128;
+constexpr uint64_t literal_0x3F7F7F7F1CFFFFFF = 0x3F7F7F7F1CFFFFFF;
+constexpr uint64_t literal_0b11 = 0b11;
+constexpr uint64_t literal_0b10 = 0b10;
+constexpr uint64_t literal_0b01 = 0b01;
+constexpr uint64_t literal_0b110 = 0b110;
+constexpr uint64_t literal_0b101 = 0b101;
+constexpr uint64_t literal_0b100 = 0b100;
+constexpr uint64_t literal_0b010 = 0b010;
+constexpr uint64_t literal_0b011 = 0b011;
+constexpr uint64_t literal_0b001 = 0b001;
 
 fapi2::ReturnCode p10_int_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& TGT0,
                                const fapi2::Target<fapi2::TARGET_TYPE_SYSTEM>& TGT1)
@@ -38,6 +54,75 @@ fapi2::ReturnCode p10_int_scom(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>
         fapi2::ATTR_NAME_Type l_chip_id;
         FAPI_TRY(FAPI_ATTR_GET_PRIVILEGED(fapi2::ATTR_NAME, TGT0, l_chip_id));
         FAPI_TRY(FAPI_ATTR_GET_PRIVILEGED(fapi2::ATTR_EC, TGT0, l_chip_ec));
+        fapi2::buffer<uint64_t> l_scom_buffer;
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x2010802ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<0, 64, 0, uint64_t>(literal_0x2000120076F0007E );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x2010802ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x2010803ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<14, 2, 62, uint64_t>(literal_0b00 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x2010803ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x2010816ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<5, 1, 63, uint64_t>(literal_0b1 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x2010816ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x201091full, l_scom_buffer ));
+
+            l_scom_buffer.insert<0, 56, 0, uint64_t>(literal_0x775643213F3F00 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x201091full, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x2010921ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<0, 56, 0, uint64_t>(literal_0x77564321000000 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x2010921ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x2010925ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<0, 64, 0, uint64_t>(literal_0x0101010101010128 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x2010925ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x2010988ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<0, 1, 63, uint64_t>(literal_0b1 );
+            l_scom_buffer.insert<1, 1, 63, uint64_t>(literal_0b1 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x2010988ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x2010989ull, l_scom_buffer ));
+
+            l_scom_buffer.insert<0, 64, 0, uint64_t>(literal_0x3F7F7F7F1CFFFFFF );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x2010989ull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x2010a1dull, l_scom_buffer ));
+
+            l_scom_buffer.insert<8, 2, 62, uint64_t>(literal_0b11 );
+            l_scom_buffer.insert<10, 2, 62, uint64_t>(literal_0b10 );
+            l_scom_buffer.insert<12, 2, 62, uint64_t>(literal_0b01 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x2010a1dull, l_scom_buffer));
+        }
+        {
+            FAPI_TRY(fapi2::getScom( TGT0, 0x2010a1full, l_scom_buffer ));
+
+            l_scom_buffer.insert<5, 3, 61, uint64_t>(literal_0b110 );
+            l_scom_buffer.insert<9, 3, 61, uint64_t>(literal_0b101 );
+            l_scom_buffer.insert<13, 3, 61, uint64_t>(literal_0b100 );
+            l_scom_buffer.insert<17, 3, 61, uint64_t>(literal_0b010 );
+            l_scom_buffer.insert<21, 3, 61, uint64_t>(literal_0b011 );
+            l_scom_buffer.insert<25, 3, 61, uint64_t>(literal_0b001 );
+            FAPI_TRY(fapi2::putScom(TGT0, 0x2010a1full, l_scom_buffer));
+        }
 
     };
 fapi_try_exit:
