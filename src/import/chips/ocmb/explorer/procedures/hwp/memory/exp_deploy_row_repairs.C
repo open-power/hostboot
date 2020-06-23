30bdce7845b0f29c9f4933ad603d2f913c88cadb
Matthew Hickman <Matthew.Hickman@ibm.com>
2020-06-23 01:02:07 -0400

    Added row repair hwp for explorer
    Change-Id: If17b1d473d8eb6a41fb7067e4b469d1e94b4f88a
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/99225
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Edgar R Cordero <ecordero@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/101613
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_deploy_row_repairs.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/exp_deploy_row_repairs.C
@@ -22,3 +22,84 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file exp_deploy_row_repairs.C
+/// @brief API for row repair HWP
+///
+// *HWP HWP Owner: Matt Hickman <Matthew.Hickman@ibm.com>
+// *HWP HWP Backup: Stephen Glancy <sglancy@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 2
+// *HWP Consumed by: FSP:HB
+// EKB-Mirror-To: hostboot
+
+#include <exp_deploy_row_repairs.H>
+#include <fapi2.H>
+#include <lib/shared/exp_defaults.H>
+#include <lib/shared/exp_consts.H>
+#include <lib/dimm/exp_mrs_traits.H>
+#include <lib/ccs/exp_row_repair.H>
+
+#include <generic/memory/lib/dimm/ddr4/mrs_load_ddr4.H>
+#include <mss_generic_attribute_getters.H>
+#include <mss_explorer_attribute_getters.H>
+#include <generic/memory/lib/utils/c_str.H>
+#include <generic/memory/lib/utils/find.H>
+#include <generic/memory/lib/utils/mss_buffer_utils.H>
+#include <map>
+
+extern "C"
+{
+
+    ///
+    /// @brief Deploy SPPR row repair
+    /// @param[in] i_target_ocmb ocmb target
+    /// @return FAPI2_RC_SUCCESS iff successful
+    ///
+    fapi2::ReturnCode exp_deploy_row_repairs(const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target_ocmb)
+    {
+        uint64_t l_mnfg_flags = 0;
+
+        // This table contains a row repair entry for each DIMM/mrank combination
+        std::map< fapi2::Target<fapi2::TARGET_TYPE_DIMM>, std::vector<mss::row_repair::repair_entry<mss::mc_type::EXPLORER>> >
+                l_row_repairs;
+
+        FAPI_TRY( mss::attr::get_mnfg_flags(l_mnfg_flags) );
+
+        // If mnfg flag is set to test all spare rows, we need to do row repair on all dimm/ranks/DRAMs
+        if (l_mnfg_flags & fapi2::ENUM_ATTR_MNFG_FLAGS_MNFG_TEST_ALL_SPARE_DRAM_ROWS)
+        {
+            return mss::exp::row_repair::activate_all_spare_rows(i_target_ocmb);
+        }
+
+        FAPI_TRY( mss::exp::row_repair::map_repairs_per_dimm(i_target_ocmb, l_row_repairs) );
+
+        // If DRAM repairs are disabled (mnfg flag), we're done (but need to callout DIMM if it has row repair data)
+        if (l_mnfg_flags & fapi2::ENUM_ATTR_MNFG_FLAGS_MNFG_DISABLE_DRAM_REPAIRS)
+        {
+            FAPI_INF("%s DRAM repairs are disabled, so skipping row repair deployment", mss::c_str(i_target_ocmb));
+
+            for (const auto l_pair : l_row_repairs)
+            {
+                const auto& l_dimm = l_pair.first;
+                const auto& l_repairs = l_pair.second;
+
+                // Loops through repairs
+                for (const auto& l_repair : l_repairs)
+                {
+                    // If we have a valid repair, call out this DIMM
+                    FAPI_TRY( mss::exp::row_repair::log_repairs_disabled_errors(l_dimm, l_repair) );
+                }
+            }
+
+            return fapi2::FAPI2_RC_SUCCESS;
+        }
+
+        FAPI_TRY( mss::exp::row_repair::deploy_mapped_repairs(l_row_repairs) );
+
+    fapi_try_exit:
+        return fapi2::current_err;
+    }
+
+}
--- src/import/chips/ocmb/explorer/procedures/hwp/memory/lib/ccs/exp_bad_dq_bitmap_funcs.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/lib/ccs/exp_bad_dq_bitmap_funcs.C
@@ -23,3 +23,86 @@
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
 // EKB-Mirror-To: hostboot
+
+///
+/// @file exp_bad_dq_bitmap_funcs.C
+/// @brief Functions that access the Bad DQ Bitmap.
+///
+/// *HWP HWP Owner: Matt Hickman <Matthew.Hickman@ibm.com>
+/// *HWP HWP Backup: Andre Marin <aamarin@us.ibm.com>
+/// *HWP Team: Memory
+/// *HWP Level: 2
+/// *HWP Consumed by: HB:CI
+/// EKB-Mirror-To: hostboot
+///
+
+#include <fapi2.H>
+#include <lib/shared/exp_defaults.H>
+#include <lib/shared/exp_consts.H>
+#include <lib/dimm/exp_rank.H>
+
+#include <generic/memory/lib/utils/c_str.H>
+#include <generic/memory/lib/utils/shared/mss_generic_consts.H>
+#include <generic/memory/lib/utils/mss_rank.H>
+#include <generic/memory/lib/mss_generic_attribute_getters.H>
+#include <generic/memory/lib/mss_generic_attribute_setters.H>
+#include <lib/ccs/exp_bad_dq_bitmap_funcs.H>
+
+namespace mss
+{
+namespace exp
+{
+
+///
+/// @brief Row Repair Utility function that gets the Bad DQ Bitmap.
+/// @param[in]  i_rank Rank info to get bitmap
+/// @param[in,out] io_data Reference to data where Bad DQ bitmap is copied to
+/// @return FAPI2_RC_SUCCESS iff okay
+///
+fapi2::ReturnCode get_bad_dq_bitmap( const mss::rank::info<>& i_rank,
+                                     uint8_t io_data[BAD_DQ_BYTE_COUNT])
+{
+    using RT = mss::rank::rankTraits<mss::mc_type::EXPLORER>;
+
+    // Get the Bad DQ bitmap by querying ATTR_BAD_DQ_BITMAP.
+    // Use a heap based array to avoid large stack alloc
+    uint8_t l_dqBitmap[RT::MAX_RANKS_PER_DIMM][BAD_DQ_BYTE_COUNT] = {};
+
+    FAPI_TRY( mss::attr::get_bad_dq_bitmap(i_rank.get_dimm_target(), l_dqBitmap) );
+
+    // Write contents of DQ bitmap for specific rank to io_data.
+    std::copy(std::begin(l_dqBitmap[i_rank.get_dimm_rank()]), std::end(l_dqBitmap[i_rank.get_dimm_rank()]), io_data);
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+///
+/// @brief Row Repair Utility function that sets the Bad DQ Bitmap.
+/// @param[in] i_rank Rank info to get bitmap
+/// @param[in] i_data Reference to data where Bad DQ bitmap is copied to
+/// @return FAPI2_RC_SUCCESS iff okay
+///
+fapi2::ReturnCode set_bad_dq_bitmap( const mss::rank::info<>& i_rank,
+                                     const uint8_t i_data[BAD_DQ_BYTE_COUNT])
+{
+    using RT = mss::rank::rankTraits<mss::mc_type::EXPLORER>;
+    const auto& l_dimm = i_rank.get_dimm_target();
+
+    // Get the Bad DQ bitmap by querying ATTR_BAD_DQ_BITMAP.
+    // Use a heap based array to avoid large stack alloc
+    uint8_t l_dqBitmap[RT::MAX_RANKS_PER_DIMM][BAD_DQ_BYTE_COUNT] = {};
+
+    FAPI_TRY( mss::attr::get_bad_dq_bitmap(l_dimm, l_dqBitmap) );
+
+    // Add the rank bitmap to the DIMM bitmap and write the bitmap
+    std::copy(i_data, i_data + BAD_DQ_BYTE_COUNT, std::begin(l_dqBitmap[i_rank.get_dimm_rank()]));
+
+    FAPI_TRY( mss::attr::set_bad_dq_bitmap(l_dimm, l_dqBitmap) );
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+} // namespace exp
+} // namespace mss
