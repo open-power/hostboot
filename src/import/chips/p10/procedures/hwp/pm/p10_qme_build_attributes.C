31a52725b2d2909194ef092698c5f3a513f1a1a3
Jenny Huynh <jhuynh@us.ibm.com>
2020-07-12 17:11:08 -0400

    Enable checkstop on special attentions
    - Unmask spattn propagation via scan to support various IPL types
    - Enable attn bits in HID register via core scom initfile for
      hcode to apply on stop save/restore
    - Also mask core firs used for fused-mode only while we're at it
    
    Change-Id: I7f783f698a93b0208a663b3b4466355c93116716
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/100173
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: SAMUEL D KIRCHHOFF <kirchhoff@us.ibm.com>
    Reviewed-by: DAVID A KING <kingboy@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/100182
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/pm/p10_qme_build_attributes.C
+++ src/import/chips/p10/procedures/hwp/pm/p10_qme_build_attributes.C
@@ -206,6 +206,28 @@ p10_qme_build_attributes(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_ch
 
     {
         uint8_t l_uint8_data = 0;
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_FUSED_CORE_MODE, l_sys_tgt, l_uint8_data),
+                 "Error From FAPI_ATTR_GET For ATTR_FUSED_CORE_MODE");
+
+        FAPI_DBG("ATTR_FUSED_CORE_MODE uint8_ Attribute Value: %x, Copy to Address: %x",
+                 l_uint8_data, i_pQmeAttrTank);
+        *(uint8_t*)i_pQmeAttrTank =  l_uint8_data ;
+        i_pQmeAttrTank = (uint8_t*)i_pQmeAttrTank + 1;
+    }
+
+    {
+        uint8_t l_uint8_data = 0;
+        FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_XSTOP_ON_SPATTN, l_sys_tgt, l_uint8_data),
+                 "Error From FAPI_ATTR_GET For ATTR_XSTOP_ON_SPATTN");
+
+        FAPI_DBG("ATTR_XSTOP_ON_SPATTN uint8_ Attribute Value: %x, Copy to Address: %x",
+                 l_uint8_data, i_pQmeAttrTank);
+        *(uint8_t*)i_pQmeAttrTank =  l_uint8_data ;
+        i_pQmeAttrTank = (uint8_t*)i_pQmeAttrTank + 1;
+    }
+
+    {
+        uint8_t l_uint8_data = 0;
         FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_SYSTEM_IPL_PHASE, l_sys_tgt, l_uint8_data),
                  "Error From FAPI_ATTR_GET For ATTR_SYSTEM_IPL_PHASE");
 
@@ -305,8 +327,8 @@ p10_qme_build_attributes(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_ch
     //  therefore, skip_size = dummy_size - (HIB_size - QI_size)
 
     {
-        const uint32_t TARGET_TYPE_SYSTEM_ATTR_TOTAL_SIZE = 75;
-        const uint32_t TARGET_TYPE_SYSTEM_ATTR_LIMIT_SIZE = 43;
+        const uint32_t TARGET_TYPE_SYSTEM_ATTR_TOTAL_SIZE = 77;
+        const uint32_t TARGET_TYPE_SYSTEM_ATTR_LIMIT_SIZE = 45;
 
         if( l_sys_bytes >= TARGET_TYPE_SYSTEM_ATTR_TOTAL_SIZE )
         {
@@ -321,7 +343,7 @@ p10_qme_build_attributes(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_ch
             if( l_sys_bytes < TARGET_TYPE_SYSTEM_ATTR_LIMIT_SIZE )
             {
                 FAPI_INF("ERROR: TARGET_TYPE_SYSTEM Attribute Co-Req Detected,\
-                         hw_image attr size %x, hcode_image_build attr size 75", l_sys_bytes);
+                         hw_image attr size %x, hcode_image_build attr size 77", l_sys_bytes);
                 FAPI_ASSERT(0, fapi2::QME_META_COREQ_PROTECT_FAIL()
                             .set_HW_IMAGE_ATTR_SIZE(l_sys_bytes)
                             .set_HCD_BUILD_ATTR_SIZE(TARGET_TYPE_SYSTEM_ATTR_TOTAL_SIZE),
@@ -337,7 +359,7 @@ p10_qme_build_attributes(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_ch
         leftover  = (8 - (alignment % 8)) % 8;
         i_pQmeAttrTank = (uint8_t*)i_pQmeAttrTank + skip_size + leftover;
 
-        FAPI_DBG("TARGET_TYPE_SYSTEM Image_Byte %d Build_Byte 75 Skip_Size %d Alignment %d Leftover %d",
+        FAPI_DBG("TARGET_TYPE_SYSTEM Image_Byte %d Build_Byte 77 Skip_Size %d Alignment %d Leftover %d",
                  l_sys_bytes, skip_size, alignment, leftover);
     }
 
