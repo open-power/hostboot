1604481d2af5424ad69b218feb0090f46b280372
Matthew Hickman <Matthew.Hickman@ibm.com>
2020-07-16 14:13:39 -0400

    Added DD1 workaround for DSTLFIR
    Change-Id: I425a92c64e464c5a13c9efa2ea9d7f93f1bc0a1a
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/100738
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Dev-Ready: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Sneha Kadam <sneha.kadam@ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Edgar R Cordero <ecordero@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/101096
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: William G Hoffa <wghoffa@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/memory/lib/workarounds/p10_fir_workarounds.C
+++ src/import/chips/p10/procedures/hwp/memory/lib/workarounds/p10_fir_workarounds.C
@@ -22,3 +22,69 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file p10_fir_workarounds.C
+/// @brief Workarounds for p10 fir workaround
+// *HWP HWP Owner: Matt Hickman <Matthew.Hickman@ibm.com>
+// *HWP HWP Backup: Louis Stermole <stermole@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 2
+// *HWP Consumed by: Memory
+
+#include <cstdarg>
+#include <fapi2.H>
+#include <p10_scom_mcc_b.H>
+#include <lib/shared/p10_consts.H>
+#include <lib/fir/p10_fir_traits.H>
+#include <lib/workarounds/p10_fir_workarounds.H>
+#include <generic/memory/lib/utils/pos.H>
+#include <generic/memory/lib/utils/fir/gen_mss_fir.H>
+#include <generic/memory/lib/utils/shared/mss_generic_consts.H>
+#include <generic/memory/lib/utils/find.H>
+
+namespace mss
+{
+namespace workarounds
+{
+namespace fir
+{
+
+///
+/// @brief Function handling the DD1 workaround for HW511630 and HW520480
+/// @param[in] i_target the MCC target of the DSTL fir
+/// @return FAPI2_RC_SUCCESS iff ok
+///
+fapi2::ReturnCode dstl_dd1_workaround( const fapi2::Target<fapi2::TARGET_TYPE_MCC>& i_target )
+{
+    fapi2::buffer<uint64_t> l_mask_or = 0;
+    fapi2::ReturnCode l_rc = fapi2::FAPI2_RC_SUCCESS;
+    mss::fir::reg<scomt::mcc::DSTL_DSTLFIR_RW> l_mcc_dstlfir_reg(i_target, l_rc);
+    FAPI_TRY(l_rc, "unable to create fir::reg for DSTL_DSTLFIR_RW 0x%08X", scomt::mcc::DSTL_DSTLFIR_RW);
+
+    // Write DSTLFIR register per defect workaround
+    l_rc = l_mcc_dstlfir_reg
+           .masked<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_A_COUNTER_ERROR>()
+           .masked<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_B_COUNTER_ERROR>()
+           .masked<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_A_BUFFER_OVERUSE_ERROR>()
+           .masked<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_B_BUFFER_OVERUSE_ERROR>()
+           .local_checkstop<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_A_FAIL_ACTION>()
+           .local_checkstop<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_B_FAIL_ACTION>()
+           .write();
+
+    FAPI_TRY(l_rc, "unable to write to dstlfir for target %s", mss::c_str(i_target));
+
+    // Set the mask_or reg to remask firs
+    l_mask_or.setBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_A_COUNTER_ERROR>();
+    l_mask_or.setBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_B_COUNTER_ERROR>();
+    l_mask_or.setBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_A_BUFFER_OVERUSE_ERROR>();
+    l_mask_or.setBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_B_BUFFER_OVERUSE_ERROR>();
+    FAPI_TRY( mss::putScom(i_target, scomt::mcc::DSTL_DSTLFIRMASK_WO_OR, l_mask_or) );
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+} // namespace fir
+} // namespace workarounds
+} // namespace mss
