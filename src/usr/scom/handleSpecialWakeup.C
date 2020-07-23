57f8c7f61af43374be68777b2c3b5b43cf20b082
Amit Tendolkar <amit.tendolkar@in.ibm.com>
2020-07-23 06:37:31 -0500

    Remove Temporary Special Wakeup Workaround
    - Simics level a5826f17ffada60c1d9f6254adc23a0bd19c3c9d has fixes
    - Re-enable HBRT Special Wakeup
    
    Change-Id: I2b52fdcaa17d0f1f0fcfa3b85e1beebe2a3dde67
    RTC: 256452
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/100926
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/usr/scom/handleSpecialWakeup.C
+++ src/usr/scom/handleSpecialWakeup.C
@@ -109,7 +109,6 @@ errlHndl_t callWakeupHyp(TARGETING::Target* i_target,
     errlHndl_t l_errl = NULL;
 
 #ifdef __HOSTBOOT_RUNTIME
-#if 0 // FIXME RTC 256452 currently special wakeup causes HBRT issues/hangs
     TARGETING::TYPE l_type = i_target->getAttr<TARGETING::ATTR_TYPE>();
 
     // Check for valid interface function
@@ -255,7 +254,6 @@ errlHndl_t callWakeupHyp(TARGETING::Target* i_target,
             break;
         }
     }
-#endif // FIXME RTC 256452
 #else
 
     assert( false, "Cannot use host wakeup in IPL code!!" );
