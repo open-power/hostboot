fd19f57ac5f4ad058ffa6eb9eeed7d33fe0e3ca7
Dan Crowell <dcrowell@us.ibm.com>
2020-07-28 11:08:45 -0500

    Force MRW coreq to fix CI
    Change-Id: I285f096e385ecc10e9ab640fd264e89c97ae7d74
    CMVC-Coreq: 1112394
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/101221
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- config.mk
+++ config.mk
@@ -5,7 +5,9 @@
 #
 # OpenPOWER HostBoot Project
 #
-# COPYRIGHT International Business Machines Corp. 2010,2014
+# Contributors Listed Below - COPYRIGHT 2010,2020
+# [+] International Business Machines Corp.
+#
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -29,3 +31,4 @@ include $(MKRULESDIR)/util.mk
 include $(MKRULESDIR)/env.mk
 include $(MKRULESDIR)/rules.mk
 include $(MKRULESDIR)/passes.mk
+
