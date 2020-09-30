f77c9d89d08a4cea6a57b35f751ce3383efe5d2b
Luis Fernandez <Luis.Fernandez@ibm.com>
2020-09-30 15:44:50 -0500

    Setup clock_config for SPIM4
    For PIB access of SPIM4 (i.e. TPM), set the
    clock_config register to known working value
    
    Change-Id: I6b65a70f95a6081544969a64c235dc96ad47b7f6
    RTC: 246066
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/105288
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Matt Derksen <mderkse1@us.ibm.com>
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>
    Reviewed-by: NAREN A DEVAIAH <naren.devaiah@in.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/105294
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/spi/p10_sbe_spi_cmd.C
+++ src/import/chips/p10/procedures/hwp/spi/p10_sbe_spi_cmd.C
@@ -97,6 +97,25 @@ fapi2::ReturnCode p10_spi_clock_init (
         FAPI_TRY(putScom(i_spiHandle.target_chip,
                          clockRegAddr, data64));
     }
+    else
+    {
+        // TODO RTC:260631 Support dynamic calculation of SPI TPM clock config
+        // For PIB access of engine 4 (i.e. TPM), the following clock setup register is
+        // known to work.
+        if (i_spiHandle.engine == SPI_ENGINE_TPM)
+        {
+            // SCK_CLOCK_DIVIDER: 0x015
+            //      PAU_freq = 0x0855 MHz in Denali MRW
+            //      spi_clock_freq = 24 MHz
+            //      PIB_frequency = PAU_freq / 2
+            //      divider = ( PIB_frequency / (spi_clock_freq * 2) ) - 1
+            // SCK_RECEIVE_DELAY: 0x40
+            // SCK_ECC_SPIMM_ADDR_CORR_DIS: 0x1  no_ecc_address_correction
+            // SCK_ECC_CONTROL: 0x01  transparent_read
+            data64 = 0x0154000A00000000ULL;
+            FAPI_TRY(putScom(i_spiHandle.target_chip, clockRegAddr, data64));
+        }
+    }
 
 fapi_try_exit:
     return fapi2::current_err;
