d6e52a0b23409a3e30a66a21af2636ef3c0a0dc8
Ilya Smirnov <ismirno@us.ibm.com>
2020-11-19 16:04:03 -0600

    HBBL TPM Driver: tpmIsCommandReady, tpmPollForCommandReady
    This commit introduces two helper functions into the HBBL TPM driver.
    The new functions are a part of the tpmTransmit flow that will be used
    in the future. Both functions originate in src/usr/spi/tpmdd.C
    
    Change-Id: Ie0fe5f9768c0a8cc9371168eb337107b84113be9
    RTC: 208818
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/108107
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Zachary Clark <zach@ibm.com>
    Reviewed-by: Nicholas E Bofferding <bofferdn@us.ibm.com>

--- src/bootloader/bl_tpm_spidd.C
+++ src/bootloader/bl_tpm_spidd.C
@@ -25,6 +25,7 @@
 #include <bl_tpm_spidd.H>
 #include <plat_hwp_invoker.H>
 #include <p10_sbe_spi_cmd.H>
+#include <bl_console.H>
 
 /** @file bl_tpm_spidd.C
  *  @brief Implementations for interfaces for TPM SPI operations in HBBL
@@ -96,6 +97,127 @@ Bootloader::hbblReasonCode tpm_write(const uint32_t i_offset, void* i_buffer, si
         // We wrote more or less than we wanted
         l_rc = Bootloader::RC_TPM_INVALID_WRITE_SIZE;
     }
+
+    return l_rc;
+}
+
+/**
+ * @brief Helper function to read the TPM status register
+ *
+ * @param[out] o_stsReg the value of the status register
+ * @return 0 on success or error code on error
+ */
+Bootloader::hbblReasonCode tpmReadSTSReg(TPMDD::tpm_sts_reg_t& o_stsReg)
+{
+    size_t l_size = sizeof(o_stsReg);
+    return tpm_read(TPMDD::TPM_REG_75x_STS, &o_stsReg, l_size);
+}
+
+/**
+ * @brief Helper function to write a "command ready" status to TPM status reg
+ *
+ * @return 0 on success or error code on error
+ */
+Bootloader::hbblReasonCode tpmWriteCommandReady()
+{
+    TPMDD::tpm_sts_reg_t l_stsReg;
+    l_stsReg.isCommandReady = 1;
+    size_t l_size = sizeof(l_stsReg);
+
+    return tpm_write(TPMDD::TPM_REG_75x_STS, &l_stsReg, l_size);
+}
+
+/**
+ * @brief Helper function to check whether the TPM is ready for a command
+ *
+ * @param[out] o_isReady whether the command is ready
+ * @return 0 on success or error code on error
+ */
+Bootloader::hbblReasonCode tpmIsCommandReady(bool& o_isReady)
+{
+    Bootloader::hbblReasonCode l_rc = Bootloader::RC_NO_ERROR;
+    TPMDD::tpm_sts_reg_t l_stsReg;
+
+    o_isReady = false;
+    l_rc = tpmReadSTSReg(l_stsReg);
+
+    o_isReady = (l_rc == Bootloader::RC_NO_ERROR && l_stsReg.isCommandReady);
+    return l_rc;
+}
+
+/**
+ * @brief Helper function to check for the command ready status in a retry loop
+ *        until the TPM signals that command is ready or there is a timeout.
+ *
+ * @param[out] o_stsReg the value of the TPM status reg
+ * @return 0 on success or error code on error
+ */
+Bootloader::hbblReasonCode tpmCheckCommandReadyStatus(bool& o_commandReady)
+{
+    Bootloader::hbblReasonCode l_rc = Bootloader::RC_NO_ERROR;
+    for(size_t l_delay = 0; l_delay < TPMDD::TPM_TIMEOUT_B; l_delay += 10)
+    {
+        o_commandReady = false;
+        l_rc = tpmIsCommandReady(o_commandReady);
+        if(l_rc || o_commandReady)
+        {
+            break;
+        }
+
+        // Sleep for 10 ns
+        bl_nanosleep(0, 10);
+    }
+
+    return l_rc;
+}
+
+/**
+ * @brief Helper function to poll the TPM for command ready status. If the
+ *        command is not ready the first time, we retry the command once more.
+ *
+ * @return 0 on success or error code on error
+ */
+Bootloader::hbblReasonCode tpmPollForCommandReady()
+{
+    Bootloader::hbblReasonCode l_rc = Bootloader::RC_NO_ERROR;
+    bool l_commandReady = false;
+
+    do {
+
+    // First, poll for command ready status
+    l_rc = tpmCheckCommandReadyStatus(l_commandReady);
+    if(l_rc)
+    {
+        break;
+    }
+
+    if(!l_commandReady)
+    {
+        // The first write to command ready may have just aborted
+        //   an outstanding command, we will write it again and poll once
+        //   more
+        l_rc = tpmWriteCommandReady();
+        if(l_rc)
+        {
+            break;
+        }
+
+        // Poll again
+        l_rc = tpmCheckCommandReadyStatus(l_commandReady);
+        if(l_rc)
+        {
+            break;
+        }
+    }
+
+    if(!l_rc && !l_commandReady)
+    {
+        // No RC reported but also the TPM is not ready for the command
+        l_rc = Bootloader::RC_TPM_COMMAND_NOT_READY;
+        break;
+    }
+
+    }while(0);
 
     return l_rc;
 }
