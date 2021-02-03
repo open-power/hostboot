1ae0745cbf7d03a5e08032fff4eb65ee03ba34b3
Dan Crowell <dcrowell@us.ibm.com>
2021-02-03 14:34:26 -0600

    Make host_omi_init run in parallel
    Modified istep 12.11 host_omi_init to execute the HWP functions
    in parallel.  As part of this, created a simpler infrastructure
    to do the same thing for other isteps.
    
    Change-Id: I2a063f7069d4ba5bd33b0a5e8a3a139d38d635e1
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/110836
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Matt Derksen <mderkse1@us.ibm.com>
    Reviewed-by: Christian R Geddes <crgeddes@us.ibm.com>

---
+++ src/usr/isteps/hwpThread.C
@@ -0,0 +1,145 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/usr/isteps/hwpThread.C $                                  */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2021                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#include <hwpThread.H>
+
+namespace ISTEP
+{
+
+mutex_t HwpWorkItem::cv_stepErrorMutex = MUTEX_INITIALIZER;
+
+// Generically run a single HWP against the given target
+void HwpWorkItem::operator()()
+{
+    errlHndl_t l_err = nullptr;
+
+    // reset watchdog for each omic as this function can be very slow
+    INITSERVICE::sendProgressCode();
+
+    TRACFCOMP( ISTEPS_TRACE::g_trac_isteps_trace,
+               "Start %s on target HUID 0x%.8X",
+               iv_hwpName,
+               get_huid(iv_pTarget) );
+
+    // Call HWP-specific function
+    l_err = run_hwp();
+
+    //  process return code
+    if ( l_err )
+    {
+        TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                  ERR_MRK"call %s HWP: failed on target 0x%08X. "
+                  TRACE_ERR_FMT,
+                  iv_hwpName,
+                  get_huid(iv_pTarget),
+                  TRACE_ERR_ARGS(l_err));
+
+        // addErrorDetails may not be thread-safe.  Protect with mutex.
+        mutex_lock(&cv_stepErrorMutex);
+
+        // Capture the error by default
+        //  can be override by specific classes to do more
+        run_after_failure(l_err);
+
+        mutex_unlock(&cv_stepErrorMutex);
+    }
+    else
+    {
+        TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                  INFO_MRK"SUCCESS running %s HWP on target HUID %.8X. ",
+                  iv_hwpName,
+                  get_huid(iv_pTarget));
+
+        // Optional function to run something after the HWP succeeds
+        run_after_success();
+    }
+}
+
+
+/**
+ * @brief Return an upper bound for the number of threads
+ *        to spawn based on how much memory we have available
+ */
+size_t HwpWorkItem::getMaxThreads( void )
+{
+    // Default to no real artificial limit
+    size_t l_maxThreads = 128;
+
+    // This is where we could put any kind of limitations based on
+    //  our current memory footprint.  For now, assume we don't
+    //  need one
+
+    // If forced to, only allow a single thread.  This results in
+    //  a completely serialized istep.  This can be useful for debug
+    //  scenarios in the lab.
+    auto l_forceSerial = TARGETING::UTIL::assertGetToplevelTarget()->
+      getAttr<TARGETING::ATTR_FORCE_SERIAL_ISTEPS>();
+    if( l_forceSerial != 0 )
+    {
+        l_maxThreads = 1;
+    }
+
+    return l_maxThreads;
+}
+
+/**
+ * @brief Start all threads and wait for completion
+ */
+bool HwpWorkItem::start_threads( Util::ThreadPool<ISTEP::HwpWorkItem>& i_threadPool,
+                                 ISTEP_ERROR::IStepError& i_istepError,
+                                 size_t i_numTargets )
+{
+    bool l_error = false;
+
+    //Don't create more threads than we have targets
+    auto l_numThreads = std::min(ISTEP::HwpWorkItem::getMaxThreads(),
+                                 i_numTargets);
+
+    TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+              INFO_MRK"Starting %llu thread(s) to handle %llu target(s) ",
+              l_numThreads, i_numTargets);
+
+    //Set the number of threads to use in the threadpool
+    Util::ThreadPoolManager::setThreadCount(l_numThreads);
+
+    //create and start worker threads
+    i_threadPool.start();
+
+    //wait for all workitems to complete, then clean up all threads.
+    errlHndl_t l_err = i_threadPool.shutdown();
+    if(l_err)
+    {
+        TRACFCOMP(ISTEPS_TRACE::g_trac_isteps_trace,
+                  ERR_MRK"thread pool returned an error "
+                  TRACE_ERR_FMT,
+                  TRACE_ERR_ARGS(l_err));
+
+        // Capture error
+        captureError(l_err, i_istepError, ISTEP_COMP_ID, nullptr);
+        l_error = true;
+    }
+    return l_error;
+};
+
+}; //namespace ISTEP
--- src/usr/isteps/istep12/call_host_omi_init.C
+++ src/usr/isteps/istep12/call_host_omi_init.C
@@ -28,13 +28,13 @@
  *  Contains the HWP wrapper for Istep 12.11
  *      exp_omi_init
  *      p10_omi_init
- *      p10_disable_ocmb_i2c
  */
 #include    <stdint.h>
 
 #include    <trace/interface.H>
 #include    <initservice/taskargs.H>
 #include    <errl/errlentry.H>
+#include    <hwpThread.H>
 
 #include    <isteps/hwpisteperror.H>
 #include    <errl/errludtarget.H>
@@ -69,13 +69,101 @@ namespace ISTEP_12
 {
 void enableInbandScomsOCMB( TargetHandleList i_ocmbTargetList );
 
+class WorkItem_exp_omi_init: public ISTEP::HwpWorkItem
+{
+  public:
+    WorkItem_exp_omi_init( IStepError& i_stepError,
+                           const Target& i_ocmb )
+    : HwpWorkItem( i_stepError, i_ocmb, "exp_omi_init" )
+    {}
+
+    virtual errlHndl_t run_hwp( void )
+    {
+        errlHndl_t l_err = nullptr;
+        fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP> l_fapi_target(iv_pTarget);
+        FAPI_INVOKE_HWP(l_err, exp_omi_init, l_fapi_target);
+        return l_err;
+    }
+
+    /**
+     * @brief Executes if the HWP returns an error
+     *   Extended in order to check for missing OCMB FW updates
+     *
+     * @param[in] i_err  Error returned from FAPI_INVOKE_HWP
+     */
+    virtual void run_after_failure( errlHndl_t& i_err )
+    {
+        // Capture error if there is no update needed, otherwise mark
+        //  the part for an update
+        captureErrorOcmbUpdateCheck(i_err, *iv_pStepError,
+                                    ISTEP_COMP_ID, iv_pTarget);
+        cv_encounteredHwpError = true;
+    };
+
+    // Remember that we hit a HWP failure.  We can't rely on IStepError because
+    //  logs might have been committed informational in the case where the OCMB
+    //  is downlevel.
+    static bool cv_encounteredHwpError;
+};
+bool WorkItem_exp_omi_init::cv_encounteredHwpError = false;
+
+
+class WorkItem_p10_omi_init: public ISTEP::HwpWorkItem
+{
+  public:
+    WorkItem_p10_omi_init( IStepError& i_stepError,
+                           const Target& i_mcc )
+    : HwpWorkItem( i_stepError, i_mcc, "p10_omi_init" ) {}
+
+    virtual errlHndl_t run_hwp( void )
+    {
+        errlHndl_t l_err = nullptr;
+        fapi2::Target<fapi2::TARGET_TYPE_MCC> l_fapi_target(iv_pTarget);
+        FAPI_INVOKE_HWP(l_err, p10_omi_init, l_fapi_target);
+        return l_err;
+    }
+
+    // Optional function to run something after the HWP succeeds
+    virtual void run_after_success( void )
+    {
+        // Now that the OMI link is active, switch to using inband
+        //  scoms for OCMB access
+        TargetHandleList l_ocmbTargetList;
+        getChildAffinityTargets(l_ocmbTargetList, iv_pTarget,
+                                CLASS_CHIP, TYPE_OCMB_CHIP);
+        enableInbandScomsOCMB(l_ocmbTargetList);
+    }
+
+    /**
+     * @brief Executes if the HWP returns an error
+     *   Extended in order to check for missing OCMB FW updates
+     *
+     * @param[in] i_err  Error returned from FAPI_INVOKE_HWP
+     */
+    virtual void run_after_failure( errlHndl_t& i_err )
+    {
+        // Capture error if there is no update needed, otherwise mark
+        //  the part for an update
+        captureErrorOcmbUpdateCheck(i_err, *iv_pStepError,
+                                    ISTEP_COMP_ID, iv_pTarget);
+        cv_encounteredHwpError = true;
+    };
+
+    // Remember that we hit a HWP failure.  We can't rely on IStepError because
+    //  logs might have been committed informational in the case where the OCMB
+    //  is downlevel.
+    static bool cv_encounteredHwpError;
+};
+bool WorkItem_p10_omi_init::cv_encounteredHwpError = false;
+
+
 void* call_host_omi_init (void *io_pArgs)
 {
     IStepError l_StepError;
-    errlHndl_t l_err = nullptr;
     TRACFCOMP( g_trac_isteps_trace, "call_host_omi_init entry" );
+    Util::ThreadPool<HwpWorkItem> threadpool;
     TargetHandleList l_ocmbTargetList;
-    bool encounteredHwpError = false;
+
     do
     {
         // 12.11.a exp_omi_init.C
@@ -87,45 +175,32 @@ void* call_host_omi_init (void *io_pArgs)
 
         for (const auto & l_ocmb_target : l_ocmbTargetList)
         {
-            TRACFCOMP( g_trac_isteps_trace,
-                "exp_omi_init HWP target HUID 0x%.8x",
-                get_huid(l_ocmb_target) );
+            //  Create a new workitem from this ocmb and feed it to the
+            //  thread pool for processing.  Thread pool handles workitem
+            //  cleanup.
+            threadpool.insert(new WorkItem_exp_omi_init(l_StepError,
+                                                        *l_ocmb_target));
+        }
 
-            //  call the HWP with each target
-            fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP> l_fapi_ocmb_target
-                    (l_ocmb_target);
-
-            FAPI_INVOKE_HWP(l_err, exp_omi_init , l_fapi_ocmb_target);
-
-            //  process return code.
-            if ( l_err )
-            {
-                TRACFCOMP(g_trac_isteps_trace,
-                    "ERROR : call exp_omi_init HWP(): failed on target 0x%08X. "
-                    TRACE_ERR_FMT,
-                    get_huid(l_ocmb_target),
-                    TRACE_ERR_ARGS(l_err));
-
-                // Capture error
-                captureErrorOcmbUpdateCheck(l_err, l_StepError, HWPF_COMP_ID, l_ocmb_target);
-                encounteredHwpError = true;
-            }
-            else
-            {
-                TRACFCOMP( g_trac_isteps_trace,
-                    "SUCCESS : exp_omi_init HWP on target HUID 0x%.8x",
-                    get_huid(l_ocmb_target) );
-            }
-        } // ocmb loop
+        // Start the threads and wait for completion
+        if( ISTEP::HwpWorkItem::start_threads( threadpool,
+                                               l_StepError,
+                                               l_ocmbTargetList.size() ) )
+        {
+            TRACFCOMP(g_trac_isteps_trace,
+                      ERR_MRK"call_host_omi_init: start_threads returned an error for exp_omi_init" );
+            break;
+        }
 
         // Do not continue if an error was encountered
-        if(encounteredHwpError)
+        if(WorkItem_exp_omi_init::cv_encounteredHwpError)
         {
             TRACFCOMP( g_trac_isteps_trace,
                 INFO_MRK "call_host_omi_init exited early because exp_omi_init "
-                "had failures" );
+                "had failures");
             break;
         }
+
 
         // 12.11.b p10_omi_init.C
         //        - Finalize the OMI
@@ -137,98 +212,24 @@ void* call_host_omi_init (void *io_pArgs)
 
         for (const auto & l_mcc_target : l_mccTargetList)
         {
-            TRACFCOMP( g_trac_isteps_trace,
-                "p10_omi_init HWP target HUID 0x%.8x",
-                get_huid(l_mcc_target) );
+            //  Create a new workitem from this mmcc and feed it to the
+            //  thread pool for processing.  Thread pool handles workitem
+            //  cleanup.
+            threadpool.insert(new WorkItem_p10_omi_init(l_StepError,
+                                                        *l_mcc_target));
+        }
 
-            //  call the HWP with each target
-            fapi2::Target<fapi2::TARGET_TYPE_MCC> l_fapi_mcc_target
-                    (l_mcc_target);
 
-            FAPI_INVOKE_HWP(l_err, p10_omi_init, l_fapi_mcc_target);
-
-            //  process return code.
-            if ( l_err )
-            {
-                TRACFCOMP(g_trac_isteps_trace,
-                    "ERROR : call p10_omi_init HWP(): failed on target 0x%08X. "
-                    TRACE_ERR_FMT,
-                    get_huid(l_mcc_target),
-                    TRACE_ERR_ARGS(l_err));
-
-                // Capture error
-                captureErrorOcmbUpdateCheck(l_err, l_StepError, HWPF_COMP_ID, l_mcc_target);
-                encounteredHwpError = true;
-            }
-            else
-            {
-                TRACFCOMP( g_trac_isteps_trace,
-                    "SUCCESS : p10_omi_init HWP on target HUID 0x%.8x, "
-                    "enable scom settings to use inband for all ocmb children",
-                    get_huid(l_mcc_target));
-
-                TargetHandleList l_ocmbTargetList;
-                getChildAffinityTargets(l_ocmbTargetList , l_mcc_target,
-                                        CLASS_CHIP, TYPE_OCMB_CHIP);
-                enableInbandScomsOCMB(l_ocmbTargetList);
-            }
-        } // MCC loop
-
-        // Do not continue if an error was encountered
-        if(encounteredHwpError)
+        // Start the threads and wait for completion
+        if( ISTEP::HwpWorkItem::start_threads( threadpool,
+                                               l_StepError,
+                                               l_mccTargetList.size() ) )
         {
-            TRACFCOMP( g_trac_isteps_trace,
-                INFO_MRK "call_host_omi_init exited early because p10_omi_init "
-                "had failures" );
+            TRACFCOMP(g_trac_isteps_trace,
+                      ERR_MRK"call_host_omi_init: start_threads returned an error for p10_omi_init" );
             break;
         }
 
-        // 12.11.c p10_disable_ocmb_i2c.C
-        //        - Disable i2c access
-        TargetHandleList l_procTargetList;
-        getAllChips(l_procTargetList, TYPE_PROC);
-        TRACFCOMP(g_trac_isteps_trace,
-            "call_host_omi_init: %d PROCs found",
-            l_procTargetList.size());
-
-        // We only want to disable i2c if we are in secure mode
-        const bool shouldForceDisable = false;
-
-        for ( const auto & l_proc : l_procTargetList )
-        {
-            TRACFCOMP( g_trac_isteps_trace,
-                "p10_disable_ocmb_i2c HWP target HUID 0x%.8x",
-                get_huid(l_proc) );
-
-            //  call the HWP with each proc
-            fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> l_fapi_proc_target
-                    (l_proc);
-
-            FAPI_INVOKE_HWP(l_err, p10_disable_ocmb_i2c, l_fapi_proc_target,
-                            shouldForceDisable);
-
-            //  process return code.
-            if ( l_err )
-            {
-                TRACFCOMP(g_trac_isteps_trace,
-                    "ERROR : call p10_disable_ocmb_i2c HWP(): failed on target "
-                    "0x%08X. "
-                    TRACE_ERR_FMT,
-                    get_huid(l_proc),
-                    TRACE_ERR_ARGS(l_err));
-
-                // Once we execute p10_disable_ocmb_i2c on something we won't
-                // be able to use the i2c mode to update.
-                // Just capture error and don't look to do i2c code update recovery
-                captureError(l_err, l_StepError, HWPF_COMP_ID, l_proc);
-            }
-            else
-            {
-                TRACFCOMP( g_trac_isteps_trace,
-                    "SUCCESS : p10_disable_ocmb_i2c HWP on target HUID 0x%.8x",
-                    get_huid(l_proc));
-            }
-        } // proc loop
     } while(0);
 
     // Grab informational Explorer logs (early IPL = false)
