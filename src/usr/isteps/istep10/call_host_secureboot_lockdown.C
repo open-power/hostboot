b4f201ee0bb6f78126622e73144073a22a6d761f
Nick Bofferding <bofferdn@us.ibm.com>
2021-03-09 08:51:55 -0600

    Secure Boot: Disable backup TPM init
    Disables backup TPM init due to instability problems.
    
    Change-Id: I9338866bde8901c4b2fbe1bdcde3284bf7a74b96
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/112395
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep10/call_host_secureboot_lockdown.C
+++ src/usr/isteps/istep10/call_host_secureboot_lockdown.C
@@ -70,6 +70,9 @@ void* call_host_secureboot_lockdown (void *io_pArgs)
 
     do {
 
+    // @TODO RTC 269646: Re-enable backup TPM when there is higher confidence
+    // it will work
+    #if 0
     // Attempt to initialize the backup TPM.
     TRUSTEDBOOT::initBackupTpm();
 
@@ -91,6 +94,7 @@ void* call_host_secureboot_lockdown (void *io_pArgs)
         // TPM and also set the deconfig bit to force fencing if security is enabled
         TRUSTEDBOOT::tpmMarkFailed(backup_tpm, l_err);
     }
+    #endif
 
 #ifdef CONFIG_SECUREBOOT
 
