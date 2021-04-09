2f2ac423f51cbaa832da846d5e92ebde3de4112c
Roland Veloz <rveloz@us.ibm.com>
2021-04-09 00:20:47 -0500

    Added better positive/negative feedback from applying VPD algorithms
    Removed the suppression of the error messages from the git commands.
    This is no longer needed now that the VPD directory is being compiled
    in a single thread. These were needed to suppress the false negatives
    when the VPD directory was being compiled with multi threads. Now only
    true negatives are happening therefore no need to suppress the error
    messages.
    
    Also allow the git fetch command display it's positive feedback
    when retrieving the VPD algorithms as opposed to suppressing them.
    
    Change-Id: Ia3e18612677acbf8e3ad4bb32051b390f69c9431
    RTC:263370
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/114344
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Deborah L McLemore <debmc@us.ibm.com>
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/build/tools/fetchVpdAlgorithms.sh
+++ src/build/tools/fetchVpdAlgorithms.sh
@@ -36,83 +36,43 @@
 # Assume any commnds that run, passed (0)
 RET_VAL=0
 
-# Set this to 1 for verbose tracing
-DEBUG_INFO=0
-
 echo "Fetching and applying the VPD ECC algorithm files:"
 
 while :
 do
-    # Get the absolute path to the repo
-    REPO_HOME=$(git rev-parse --show-toplevel 2>&1)
+    # Get the absolute path to the HB repo
+    REPO_ABSOLUTE_PATH=$(git rev-parse --show-toplevel)
     RET_VAL=$?
 
-    # If could not get absolute path to repo, then exit while loop
+    # If an issue getting the absolute path to the project, then exit while loop
     if [[ $RET_VAL -ne 0 ]]; then
-        if [[ $DEBUG_INFO -ne 0 ]]; then
-            echo "Command 'git rev-parse --show-toplevel' failed:"
-            echo "$REPO_HOME"
-        fi
-
         break
     fi
 
-    # Confirm existence of the VPD directory
-    VPD_DIR="${REPO_HOME}/src/usr/vpd"
-    if [[ ! -d  "$VPD_DIR" ]]; then
-        if [[ $DEBUG_INFO -ne 0 ]]; then
-            echo "Destination directory '$VPD_DIR' does not exist"
-        fi
+    # Confirm existence of the VPD directory within the project via an absolute path
+    VPD_ABSOLUTE_PATH="${PROJECT_ROOT}/src/usr/vpd"
+    if [[ ! -d  "$VPD_ABSOLUTE_PATH" ]]; then
+        echo "VPD directory '$VPD_ABSOLUTE_PATH' does not exist"
 
         # OS error code 2 `perror 2`: No such file or directory
         RET_VAL=2
         break
     fi
 
-    # If any of the files exist, the 'git apply' command below will error out.
-    # This is not necessarily an issue, it is just erroring out because the files exist.
-    # Considering that there is no force option on the 'git apply' command, will
-    # issue a warning to caller because an error message may be misleading.
-    if [[  -f "${VPD_DIR}/vpdecc.c"         || -f "${VPD_DIR}/vpdecc.h"  ||
-           -f "${VPD_DIR}/vpdecc_support.c" || -f "${VPD_DIR}/vpdecc_support.h" ]]; then
-        echo -e "Warning: One, if not all, of these files vpdecc.c, vpdecc.h, vpdecc_support.c\n" \
-                "and/or vpdecc_support.h exists in directory ${VPD_DIR}\n"                       \
-                "Cannot patch any of these files if any one exists.\n"                           \
-                "If patch must be applied then try 'make clean', 'make clobber' or remove files by hand."
-
-        # Exit loop and do not continue
-        RET_VAL=0
-        break
-    fi
-
     ## Attempt to apply files
     #  Fetch the changes
-    RESULTS=$(git fetch ssh://hostboot.gerrit/hostboot refs/changes/54/112054/1 2>&1)
+    `git fetch ssh://hostboot.gerrit/hostboot refs/changes/54/112054/1`
     RET_VAL=$?
     # If an error occurred while fetching the commit, then exit while loop
     if [[ $RET_VAL -ne 0 ]]; then
-        if [[ $DEBUG_INFO -ne 0 ]]; then
-            echo "Command 'git fetch ssh://hostboot.gerrit/hostboot refs/changes/54/112054/1' failed:"
-            echo "$RESULTS"
-        fi
-
-        # No valid OS error code, will use generic code 1 `perror 1`: Operation not permitted
-        RET_VAL=1
         break
     fi
 
     # Create a patch from the FETCH_HEAD reference and apply the patch
-    RESULTS=$(git format-patch -1 --stdout FETCH_HEAD -p | git apply 2>&1)
+    `git format-patch -1 --stdout FETCH_HEAD -p | git apply --verbose`
     RET_VAL=$?
     # If an error occurred while applying the patch, then exit while loop
     if [[ $RET_VAL -ne 0 ]]; then
-        if [[ $DEBUG_INFO -ne 0 ]]; then
-            echo "Command 'git format-patch -1 --stdout FETCH_HEAD -p | git apply ' failed:"
-            echo "$RESULTS"
-        fi
-
-        # No valid OS error code, will use generic code 1 `perror 1`: Operation not permitted
-        RET_VAL=1
         break
     fi
 
@@ -121,15 +81,9 @@ do
     break;
 done
 
-if [[ $RET_VAL -eq  0 ]]; then
-    echo "Patched VPD ECC algorithm files:"
-    echo "    ${VPD_DIR}/vpdecc.c"
-    echo "    ${VPD_DIR}/vpdecc.h"
-    echo "    ${VPD_DIR}/vpdecc_support.c"
-    echo "    ${VPD_DIR}/vpdecc_support.h"
-else
+if [[ $RET_VAL -ne  0 ]]; then
     echo "Unexpected error retrieving the VPD ECC algorithm files:"
     echo "     vpdecc.c, vpdecc.h, vpdecc_support.c and vpdecc_support.h"
 fi
 
-exit 0
+exit $RET_VAL
