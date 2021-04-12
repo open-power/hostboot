f57bd81b8d375caa3d8d292632d2c3b1f015bf14
Matt Derksen <mderkse1@us.ibm.com>
2021-04-12 11:22:30 -0500

    PLDM rsid needs to allow for cores
    Possible cores = 8 chips * 32 core/chip = 2 ^ 8
    Need at least 8 bits for unique core id.
    Also need to use something besides position
    since cores do not have that attribute.
    
    Change-Id: I95f6343444f8c0ab76df570e542e02404f2f347b
    RTC:250699
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/114533
    Reviewed-by: Zachary Clark <zach@ibm.com>
    Reviewed-by: Corey V Swenson <cswenson@us.ibm.com>
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Christian R Geddes <crgeddes@us.ibm.com>

--- src/usr/pldm/extended/pldm_fru.C
+++ src/usr/pldm/extended/pldm_fru.C
@@ -38,70 +38,18 @@
 using namespace PLDM;
 using namespace TARGETING;
 
-enum encoded_target_class_t
-{
-    ENCODED_CLASS_CHIP,          // PROC targets
-    ENCODED_CLASS_LOGICAL_CARD,  // DIMM
-    ENCODED_END                  // sentinel
-};
-
-constexpr int target_class_bits = 3;
-constexpr int target_type_bits = 7;
-constexpr int position_id_bits = 6;
-
-static_assert(ENCODED_END <= (1 << target_class_bits),
-              "Not enough bits in encoded rsid to encoded target class");
+constexpr int target_type_bits  = 7;
+constexpr int ordinal_id_bits   = 9;
 
 static_assert(TYPE_LAST_IN_RANGE <= (1 << target_type_bits),
-              "Too many possible CLASS attribuet values to encode in 4 bits");
-
-struct target_class_encoding
-{
-    ATTR_CLASS_type target_class;
-    encoded_target_class_t encoded_class;
-};
-
-static const target_class_encoding encoded_target_classes[] =
-{
-    { CLASS_CHIP, ENCODED_CLASS_CHIP },
-    { CLASS_LOGICAL_CARD, ENCODED_CLASS_LOGICAL_CARD },
-};
-
-static encoded_target_class_t encodeTargetClass(const TARGETING::CLASS i_class)
-{
-    const auto it = std::find_if(std::begin(encoded_target_classes),
-                                 std::end(encoded_target_classes),
-                                 [i_class](const target_class_encoding& encoding)
-                                 { return encoding.target_class == i_class; });
-
-    assert(it != std::end(encoded_target_classes),
-           "Cannot ENCODE target class %d for Hostboot FRU record set ID",
-           i_class);
-
-    return it->encoded_class;
-}
-
-static ATTR_CLASS_type decodeTargetClass(const encoded_target_class_t i_encoded_class)
-{
-    const auto it = std::find_if(std::begin(encoded_target_classes),
-                                 std::end(encoded_target_classes),
-                                 [i_encoded_class](const target_class_encoding& encoding)
-                                 { return encoding.encoded_class == i_encoded_class; });
-
-    assert(it != std::end(encoded_target_classes),
-           "Cannot DECODE target class %d for Hostboot FRU record set ID",
-           i_encoded_class);
-
-    return it->target_class;
-}
+    "Cannot encode all target types in the given number of bits");
 
 union encoded_rsid_t
 {
     struct
     {
-        encoded_target_class_t encoded_class : 3;
         TARGETING::TYPE target_type : 7;
-        ATTR_POSITION_type position_id : 6;
+        ATTR_ORDINAL_ID_type ordinal_id : 9;
     } __attribute__((packed));
 
     uint16_t packed_rsid;
@@ -110,24 +58,24 @@ union encoded_rsid_t
 static_assert(sizeof(encoded_rsid_t) == sizeof(PLDM::fru_record_set_id_t),
               "Encoded RSID union not packed");
 
+
 PLDM::fru_record_set_id_t PLDM::getTargetFruRecordSetID(const Target* const i_target)
 {
-    const auto target_class = i_target->getAttr<ATTR_CLASS>();
-    const auto target_type = i_target->getAttr<ATTR_TYPE>();
-    const auto position_id = i_target->getAttr<ATTR_POSITION>();
+    const auto l_target_type = i_target->getAttr<ATTR_TYPE>();
+    const auto l_ordinal_id = i_target->getAttr<ATTR_ORDINAL_ID>();
 
-    assert(target_type <= (1 << target_type_bits),
+    assert(l_target_type < (1 << target_type_bits),
            "Target type for FRU Record Set ID is out of range (HUID 0x%08x)",
            get_huid(i_target));
-    assert(position_id <= (1 << position_id_bits),
-           "Position ID for FRU Record Set ID is out of range (HUID 0x%08x)",
-           get_huid(i_target));
+
+    assert(l_ordinal_id < (1 << ordinal_id_bits),
+           "Ordinal ID for FRU Record Set ID is out of range (HUID 0x%08x) - max size: 0x%08x",
+           get_huid(i_target), (1 << ordinal_id_bits)-1);
 
     encoded_rsid_t rsid = { };
 
-    rsid.encoded_class = encodeTargetClass(target_class);
-    rsid.target_type = target_type;
-    rsid.position_id = position_id;
+    rsid.target_type = l_target_type;
+    rsid.ordinal_id = l_ordinal_id;
 
     return rsid.packed_rsid;
 }
@@ -136,11 +84,9 @@ Target* PLDM::getTargetFromHostbootFruRecordSetID(const fru_record_set_id_t i_rs
 {
     encoded_rsid_t encoded_rsid = { .packed_rsid = i_rsid };
 
-    const ATTR_CLASS_type target_class =  decodeTargetClass(encoded_rsid.encoded_class);
-
     TARGETING::TargetHandleList target_list;
     getClassResources(target_list,
-                      target_class,
+                      CLASS_NA,
                       encoded_rsid.target_type,
                       UTIL_FILTER_PRESENT);
 
@@ -148,7 +94,7 @@ Target* PLDM::getTargetFromHostbootFruRecordSetID(const fru_record_set_id_t i_rs
 
     for (Target* const target : target_list)
     {
-        if (target->getAttr<ATTR_POSITION>() == encoded_rsid.position_id)
+        if (target->getAttr<ATTR_ORDINAL_ID>() == encoded_rsid.ordinal_id)
         {
             result_target = target;
             break;
