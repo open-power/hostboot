23395589fc93ad79358deb54253e9ff584d717fe
Caleb Palmer <cnpalmer@us.ibm.com>
2021-06-11 16:26:22 -0500

    PRD: Mask at the chiplet level for RE EQ_CORE_FIR analysis
    Change-Id: I0b81a218ab4a9b6a199220d7fea35b13f0218046
    CQ: SW527151
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/118029
    Reviewed-by: Benjamen G Tyner <ben.tyner@ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Zane C Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/118106
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>

--- src/usr/diag/prdf/common/plat/p10/prdfP10Eq.C
+++ src/usr/diag/prdf/common/plat/p10/prdfP10Eq.C
@@ -92,6 +92,85 @@ int32_t coreUcsSummary( ExtensibleChip * i_chip,
 }
 PRDF_PLUGIN_DEFINE( p10_eq, coreUcsSummary );
 
+/**
+ * @brief  Plugin function for recoverable analysis to the core and then to
+ *         mask the EQ_CORE_FIR off at the EQ chiplet level if at threshold.
+ * @param  i_chip    An EQ chip.
+ * @param  io_sc     The step code data struct.
+ * @param  i_corePos The position of the analyzed core.
+ * @return SUCCESS if analysis to core completes, else the failed return code
+ *         from analysis.
+ */
+int32_t __analyzeCoreFir( ExtensibleChip * i_chip,
+                          STEP_CODE_DATA_STRUCT & io_sc, uint8_t i_corePos )
+{
+    #define PRDF_FUNC "[p10_eq::__analyzeCoreFir] "
+
+    int32_t o_rc = SUCCESS;
+
+    // First, analyze to the core. If analysis is not successful, just return
+    // the failed return code.
+    ExtensibleChip * core = getConnectedChild( i_chip, TYPE_CORE, i_corePos );
+    o_rc = core->Analyze( io_sc, RECOVERABLE );
+    if ( SUCCESS != o_rc )
+    {
+        PRDF_TRAC(PRDF_FUNC "Analysis failed to core 0x%08x", core->getHuid());
+        return o_rc;
+    }
+
+    // Note that we cannot set the mask of recoverable bits that have reached
+    // threshold in the EQ_CORE_FIR. Instead, we will mask the core off at the
+    // EQ_CHIPLET_RE_FIR level here.
+
+    #ifdef __HOSTBOOT_MODULE
+    // If we aren't doing recoverable analysis or are not at threshold, just
+    // return SUCCESS
+    if ( RECOVERABLE != io_sc.service_data->getPrimaryAttnType() ||
+         !io_sc.service_data->IsAtThreshold() )
+    {
+        return o_rc;
+    }
+
+    SCAN_COMM_REGISTER_CLASS * chipletMask =
+        i_chip->getRegister( "EQ_CHIPLET_RE_FIR_MASK" );
+
+    if ( SUCCESS == chipletMask->Read() )
+    {
+        // Bits 5:8 are the bits for analysis to the EQ_CORE_FIR
+        chipletMask->SetBit( 5 + i_corePos );
+
+        if ( SUCCESS != chipletMask->Write() )
+        {
+            PRDF_ERR( PRDF_FUNC "Failed to write EQ_CHIPLET_RE_FIR_MASK on "
+                      "0x%08x", i_chip->getHuid() );
+        }
+    }
+    else
+    {
+        PRDF_ERR( PRDF_FUNC "Failed to read EQ_CHIPLET_RE_FIR_MASK on 0x%08x",
+                  i_chip->getHuid() );
+    }
+    #endif // __HOSTBOOT_MODULE
+
+    return o_rc;
+
+    #undef PRDF_FUNC
+}
+
+
+#define PLUGIN_ANALYZE_CORE_FIR( POS ) \
+int32_t analyzeCoreFir_##POS( ExtensibleChip * i_chip, \
+                              STEP_CODE_DATA_STRUCT & io_sc ) \
+{ \
+    return __analyzeCoreFir( i_chip, io_sc, POS ); \
+} \
+PRDF_PLUGIN_DEFINE( p10_eq, analyzeCoreFir_##POS );
+
+PLUGIN_ANALYZE_CORE_FIR(0);
+PLUGIN_ANALYZE_CORE_FIR(1);
+PLUGIN_ANALYZE_CORE_FIR(2);
+PLUGIN_ANALYZE_CORE_FIR(3);
+
 } // namespace p10_eq
 
 } // namespace PRDF
