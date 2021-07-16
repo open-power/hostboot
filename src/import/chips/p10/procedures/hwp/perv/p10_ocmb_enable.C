4994671f3dd2d0d87e5e78ee59247dc47e6b61ea
Louis Stermole <stermole@us.ibm.com>
2021-07-16 14:56:07 -0400

    Add 500ms delay after toggling OCMB reset
    Change-Id: I21f1a830dab5e1c4420beb6be6c9a5961b527fdf
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/119904
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Geetha Pisapati <geetha.pisapati@ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Daniel Melnikov <daniel.melnikov@ibm.com>
    Dev-Ready: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: Edgar R Cordero <ecordero@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/119915
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/perv/p10_ocmb_enable.C
+++ src/import/chips/p10/procedures/hwp/perv/p10_ocmb_enable.C
@@ -126,6 +126,10 @@ p10_ocmb_enable(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target)
         CLEAR_TP_TPVSB_FSI_W_MAILBOX_FSXCOMP_FSXLOG_ROOT_CTRL0_TPFSI_IO_OCMB_RESET_EN(l_scom_buf);
         FAPI_TRY(PUT_TP_TPVSB_FSI_W_MAILBOX_FSXCOMP_FSXLOG_ROOT_CTRL0_RW(i_target, l_scom_buf));
 
+        // Wait 500 milliseconds - Explorer spec says not to attempt i2c communication until
+        // 500ms after toggling reset
+        fapi2::delay(500 * 1000000, 10000);
+
     }
 
     FAPI_DBG("End");
