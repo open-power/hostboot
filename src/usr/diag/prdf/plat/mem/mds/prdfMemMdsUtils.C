afaf6da7c71500472e4c9de28a58305882b77b6a
Caleb Palmer <cnpalmer@us.ibm.com>
2021-11-01 14:11:58 -0500

    PRD: MDS add utility for clearing media error logs
    Change-Id: Id2fbd3004b538d1b166cfde5a8d509261ed96549
    RTC: 294163
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/124570
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Benjamen G Tyner <ben.tyner@ibm.com>
    Reviewed-by: Austin F Cui <austin.cui@ibm.com>
    Reviewed-by: Zane C Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/125910
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>

--- src/usr/diag/prdf/plat/mem/mds/prdfMemMdsUtils.C
+++ src/usr/diag/prdf/plat/mem/mds/prdfMemMdsUtils.C
@@ -112,13 +112,124 @@ uint32_t readMdsCtlr<TYPE_OCMB_CHIP>( TargetHandle_t i_ocmb, uint32_t i_addr,
 
 //------------------------------------------------------------------------------
 
+template<>
+uint32_t writeMdsCtlr<TYPE_MDS_CTLR>( TargetHandle_t i_mdsCtlr, uint32_t i_addr,
+                                      uint32_t i_data )
+{
+    #define PRDF_FUNC "[writeMdsCtlr] "
+
+    PRDF_ASSERT( nullptr != i_mdsCtlr );
+    PRDF_ASSERT( TYPE_MDS_CTLR == getTargetType(i_mdsCtlr) );
+
+    uint32_t o_rc = SUCCESS;
+
+    // Buffer length must be 4 bytes
+    size_t bufLen = sizeof(uint32_t);
+
+    errlHndl_t errl = nullptr;
+
+    errl = DeviceFW::deviceWrite( i_mdsCtlr, &i_data, bufLen,
+                                  DEVICE_SCOM_ADDRESS(i_addr) );
+    if ( nullptr != errl )
+    {
+        PRDF_ERR( PRDF_FUNC "deviceWrite() failed on i_mdsCtlr=0x%08x "
+                  "i_addr=0x%016llx", getHuid(i_mdsCtlr), i_addr );
+
+        o_rc = PRD_SCANCOM_FAILURE;
+        PRDF_ADD_SW_ERR(errl, o_rc, PRDF_HOM_SCOM, __LINE__);
+
+        bool l_isAbort = false;
+        PRDF_ABORTING(l_isAbort);
+        if (!l_isAbort)
+        {
+            PRDF_SET_ERRL_SEV(errl, ERRL_SEV_INFORMATIONAL);
+            PRDF_COMMIT_ERRL(errl, ERRL_ACTION_HIDDEN);
+        }
+        else
+        {
+            delete errl;
+            errl = nullptr;
+        }
+    }
+
+    return o_rc;
+
+    #undef PRDF_FUNC
+}
+
+template<>
+uint32_t writeMdsCtlr<TYPE_OCMB_CHIP>( TargetHandle_t i_ocmb, uint32_t i_addr,
+                                      uint32_t i_data )
+{
+    PRDF_ASSERT( nullptr != i_ocmb );
+    PRDF_ASSERT( TYPE_OCMB_CHIP == getTargetType(i_ocmb) );
+
+    TargetHandle_t mdsCtlr = getConnectedChild( i_ocmb, TYPE_MDS_CTLR, 0 );
+
+    return writeMdsCtlr<TYPE_MDS_CTLR>(mdsCtlr, i_addr, i_data);
+}
+
+//------------------------------------------------------------------------------
+
 uint32_t clearMediaErrLogs( ExtensibleChip * i_ocmb )
 {
     #define PRDF_FUNC "[clearMediaErrLogs] "
 
+    PRDF_ASSERT( nullptr != i_ocmb );
+    PRDF_ASSERT( TYPE_OCMB_CHIP == i_ocmb->getType() );
+
     uint32_t o_rc = SUCCESS;
 
-    // TODO
+    // Enable mode change registers F1RCCX, F1RCDX, F1RCEX, and F1RCFX to
+    // 0xAB, 0xCD, 0x89, and 0xEF respectively. These registers start at address
+    // 0x0001001B
+    uint32_t savedData = 0;
+    o_rc = readMdsCtlr<TYPE_OCMB_CHIP>( i_ocmb->getTrgt(), 0x0001001B,
+                                        savedData );
+    if ( SUCCESS != o_rc )
+    {
+        PRDF_ERR( PRDF_FUNC "readMdsCtlr(0x%08x, 0x0001001B) failed.",
+                  i_ocmb->getHuid() );
+    }
+
+    uint32_t enableModeChange = 0xABCD89EF;
+    o_rc = writeMdsCtlr<TYPE_OCMB_CHIP>( i_ocmb->getTrgt(), 0x0001001B,
+                                         enableModeChange );
+    if ( SUCCESS != o_rc )
+    {
+        PRDF_ERR( PRDF_FUNC "writeMdsCtlr(0x%08x, 0x0001001B) failed.",
+                  i_ocmb->getHuid() );
+    }
+
+    // Set the media error log clear bit (bit 7 starting at address 0x00202600)
+    uint32_t data = 0;
+    o_rc = readMdsCtlr<TYPE_OCMB_CHIP>( i_ocmb->getTrgt(), 0x00202600,
+                                        data );
+    if ( SUCCESS != o_rc )
+    {
+        PRDF_ERR( PRDF_FUNC "readMdsCtlr(0x%08x, 0x0001001B) failed.",
+                  i_ocmb->getHuid() );
+    }
+
+    data |= 0x01000000;
+    o_rc = writeMdsCtlr<TYPE_OCMB_CHIP>( i_ocmb->getTrgt(), 0x00202600,
+                                         data );
+    if ( SUCCESS != o_rc )
+    {
+        PRDF_ERR( PRDF_FUNC "writeMdsCtlr(0x%08x, 0x0001001B) failed.",
+                  i_ocmb->getHuid() );
+    }
+
+
+    // Set the mode change registers back to what they were set to before, just
+    // in case.
+    o_rc = writeMdsCtlr<TYPE_OCMB_CHIP>( i_ocmb->getTrgt(), 0x0001001B,
+                                         savedData );
+    if ( SUCCESS != o_rc )
+    {
+        PRDF_ERR( PRDF_FUNC "writeMdsCtlr(0x%08x, 0x0001001B) failed.",
+                  i_ocmb->getHuid() );
+    }
 
     return o_rc;
 
