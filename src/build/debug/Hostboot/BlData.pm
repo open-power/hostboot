f83dd927fb3910c6f70ff1ca585399efc6a48b20
Nick Bofferding <bofferdn@us.ibm.com>
2021-11-15 10:10:11 -0600

    Fix bootloader data offset in BlData.pm tool
    This commit fixes an out of date HRMOR offset in
    the BlData.pm tool that was causing the tool to
    attempt parsing of invalid areas of memory.
    
    Change-Id: I2e02f0968769778aeef95e6fcaa377f45cb83d53
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/125040
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: Deborah L McLemore <debmc@us.ibm.com>
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Michael Baiocchi <mbaiocch@us.ibm.com>

--- src/build/debug/Hostboot/BlData.pm
+++ src/build/debug/Hostboot/BlData.pm
@@ -67,7 +67,7 @@ sub main
     # @HBBL_SIZE_SYNC@
     # Data address is bootloader HRMOR + HBBL_MAX_SIZE + 12K exception
     # vectors
-    my $dataAddr = 0xF020B000;
+    my $dataAddr = 0xE020B000;
     my $dataOffset = 0;
 
     # Parse data address from options.
