3653088b92231e57d4de1666f980e2152ca24838
hostboot <hostboot@us.ibm.com>
2021-11-28 23:57:06 -0600

    Update to latest openbmc/libmctp commit e320669
    Change-Id: I946faeb34af8f32b50637df9653c06d9a1b40a54
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/125408
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Christian R Geddes <crgeddes@us.ibm.com>

--- src/subtree/openbmc/libmctp/range.h
+++ src/subtree/openbmc/libmctp/range.h
@@ -3,15 +3,15 @@
 
 #define MIN(a, b)                                                              \
 	({                                                                     \
-		__typeof__(a) _a = a;                                          \
-		__typeof__(b) _b = b;                                          \
+		typeof(a) _a = a;                                              \
+		typeof(b) _b = b;                                              \
 		_a < _b ? _a : _b;                                             \
 	})
 
 #define MAX(a, b)                                                              \
 	({                                                                     \
-		__typeof__(a) _a = a;                                          \
-		__typeof__(b) _b = b;                                          \
+		typeof(a) _a = a;                                              \
+		typeof(b) _b = b;                                              \
 		_a > _b ? _a : _b;                                             \
 	})
 
