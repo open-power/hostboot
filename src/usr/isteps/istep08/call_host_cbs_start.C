ea8f7755901ad093319188cef31fdb8518ea768a
Zach Clark <zach@ibm.com>
2021-12-09 16:29:45 -0600

    Clock failure support: Deconfig proc on clock test fail (istep 8.3) (commit 2/n)
    This commit adds code to deconfigure and fatally guard a processor if
    the clock test fails in istep host_cbs_start (8.3) because the
    processor is not usable if the clock test fails.
    
    Change-Id: I615b6f7dabd62c0d5460ba3ce03ea9f4afc36360
    RTC: 295267
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/125831
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: Isaac Salem <isaac.salem@ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Deborah L McLemore <debmc@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/usr/isteps/istep08/call_host_cbs_start.C
+++ src/usr/isteps/istep08/call_host_cbs_start.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2015,2021                        */
+/* Contributors Listed Below - COPYRIGHT 2015,2022                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -128,6 +128,9 @@ void* call_host_cbs_start(void *io_pArgs)
                           TRACE_ERR_FMT,
                           get_huid(l_cpu_target),
                           TRACE_ERR_ARGS(l_errl));
+
+                l_errl->addHwCallout(l_cpu_target, HWAS::SRCI_PRIORITY_LOW, HWAS::DELAYED_DECONFIG, HWAS::GARD_Predictive);
+
                 captureError(l_errl, l_stepError, HWPF_COMP_ID, l_cpu_target);
                 continue; //Don't continue on this chip if p10_clock_test failed
             }
