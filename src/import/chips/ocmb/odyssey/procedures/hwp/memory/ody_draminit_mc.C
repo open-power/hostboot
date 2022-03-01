35ba74a09d7e527f10fab886899ca4850295dea7
Amita Banchhor <amita.banchhor@ibm.com>
2022-03-01 13:45:17 -0500

    porting ody_draminit_mc code
    Zen:MST-1452
    Change-Id: I1abe15ecf343426fb30d00590ab0a809590499e9
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/128241
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Geetha Pisapati <geetha.pisapati@ibm.com>
    Reviewed-by: Edgar Cordero <ecordero@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/134965
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/ocmb/odyssey/procedures/hwp/memory/ody_draminit_mc.C
+++ src/import/chips/ocmb/odyssey/procedures/hwp/memory/ody_draminit_mc.C
@@ -36,6 +36,15 @@
 #include <fapi2.H>
 #include <ody_draminit_mc.H>
 
+#include <generic/memory/lib/utils/c_str.H>
+#include <generic/memory/lib/utils/find.H>
+#include <generic/memory/lib/utils/count_dimm.H>
+
+#include <lib/mc/ody_port_traits.H>
+#include <generic/memory/lib/utils/mc/gen_mss_port.H>
+#include <generic/memory/mss_git_data_helper.H>
+#include <generic/memory/lib/utils/fir/gen_mss_unmask.H>
+
 extern "C"
 {
 ///
@@ -45,5 +54,60 @@ extern "C"
 ///
     fapi2::ReturnCode ody_draminit_mc( const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target )
     {
+        mss::display_git_commit_info("ody_draminit_mc");
+
+        FAPI_INF( TARGTIDFORMAT " Start ody_draminit_mc", TARGTID );
+
+        //skip this ocmb_chip if we have no DIMM's configured
+        if(mss::count_dimm(i_target) == 0)
+        {
+            FAPI_INF( "No DIMM's configured on " TARGTIDFORMAT " Skipping this OCMB_CHIP.", TARGTID ) ;
+            return fapi2::FAPI2_RC_SUCCESS;
+        }
+
+        // TODO Zen:MST-1405 Power management settings have changed in Odyssey. Need to update generic function
+        // and traits to accommodate changes
+        // Enable Power management based off of mrw_power_control_requested
+        // FAPI_TRY( mss::enable_power_management<mss::mc_type::ODYSSEY>(i_target), "%s Failed to enable power management",
+        //           mss::c_str(i_target) );
+
+        // TODO Zen:MST-1406 Check with design team if we have an "init complete" indicator in Odyssey.
+        // This was assigned to an unused (and unnamed) bit on Explorer PMU8Q.
+        // Set the IML Complete bit. Steve Powell to find a bit in the SRQ to use for this purpose
+        // FAPI_TRY( mss::change_iml_complete<mss::mc_type::ODYSSEY>(i_target, mss::HIGH), "%s Failed to set_ipm_complete",
+        //           mss::c_str(i_target));
+
+        // Set DFI init start requested from Stephen Powell
+        FAPI_TRY( mss::change_dfi_init_start<mss::mc_type::ODYSSEY>(i_target, mss::ON ),
+                  TARGTIDFORMAT " Failed to change_dfi_init_start",
+                  TARGTID );
+
+        // Start the refresh engines by setting MBAREF0Q(0) = 1. Note that the remaining bits in
+        // MBAREF0Q should retain their initialization values.
+        FAPI_TRY( mss::change_refresh_enable<mss::mc_type::ODYSSEY>(i_target, mss::HIGH),
+                  TARGTIDFORMAT " Failed change_refresh_enable",
+                  TARGTID );
+
+        // Trigger the MC to take the DRAMs out of self refresh
+        FAPI_TRY( mss::change_force_str<mss::mc_type::ODYSSEY>(i_target, mss::LOW), TARGTIDFORMAT " Failed change_force_str",
+                  TARGTID );
+
+        // Enable periodic short zq cal
+        FAPI_TRY( mss::enable_zq_cal<mss::mc_type::ODYSSEY>(i_target), TARGTIDFORMAT " Failed enable_zq_cal", TARGTID );
+
+        // Enable periodic mem calibration
+        FAPI_TRY( mss::enable_periodic_cal<mss::mc_type::ODYSSEY>(i_target), TARGTIDFORMAT " Failed enable_periodic_cal",
+                  TARGTID );
+
+        // TODO Zen:MST-1529 Specialize unmask::after_draminit_mc for Odyssey
+        // Unmask registers after draminit_mc
+        // FAPI_TRY(mss::unmask::after_draminit_mc<mss::mc_type::ODYSSEY>(i_target), "%s Failed after_draminit_mc",
+        //          mss::c_str(i_target));
+
+        FAPI_INF( TARGTIDFORMAT " End ody_draminit MC", TARGTID );
         return fapi2::FAPI2_RC_SUCCESS;
+
+    fapi_try_exit:
+        return fapi2::current_err;
     }
+}
