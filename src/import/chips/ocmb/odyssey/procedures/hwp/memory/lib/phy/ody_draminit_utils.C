c1a99fd89b126fb5ff5d1202d66bd0ee803d7916
Stephen Glancy <sglancy@us.ibm.com>
2022-03-21 12:31:25 -0400

    Adds Odyssey draminit start/stop functions
    ZEN:MST-1538
    Change-Id: I961b2e6b270fa99b9712eeb3bc3cba270dfbc52f
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/129090
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Amita Banchhor <amita.banchhor@ibm.com>
    Reviewed-by: Edgar Cordero <ecordero@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/129124
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/ocmb/odyssey/procedures/hwp/memory/lib/phy/ody_draminit_utils.C
@@ -0,0 +1,125 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/ocmb/odyssey/procedures/hwp/memory/lib/phy/ody_draminit_utils.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2022                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+// EKB-Mirror-To: hostboot
+///
+/// @file ody_draminit_utils.C
+/// @brief Odyssey PHY draminit utility functions
+///
+// *HWP HWP Owner: Stephen Glancy <sglancy@us.ibm.com>
+// *HWP HWP Backup: Louis Stermole <stermole@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 2
+// *HWP Consumed by: FSP:HB
+
+#include <fapi2.H>
+
+#include <generic/memory/lib/utils/shared/mss_generic_consts.H>
+
+// TODO:ZEN:MST-1571 Update Odyssey PHY registers when the official values are merged into the EKB
+#include <lib/phy/ody_draminit_utils.H>
+#include <lib/phy/ody_phy_utils.H>
+
+namespace mss
+{
+namespace ody
+{
+namespace phy
+{
+
+// TODO:ZEN:MST-1571 Update Odyssey PHY registers when the official values are merged into the EKB
+// For now using the Synopsys register location documentation
+constexpr uint64_t MIRCORESET = 0x000d0099;
+constexpr uint64_t CALZAP     = 0x00020089;
+
+constexpr uint64_t MIRCORESET_STALLTOMICRO = 60;
+constexpr uint64_t MIRCORESET_RESETTOMICRO = 63;
+
+constexpr uint64_t CALZAP_CALZAP     = 63;
+
+///
+/// @brief Starts the firmware draminit training
+/// @param[in] i_target the target on which to operate
+/// @return fapi2::FAPI2_RC_SUCCESS iff successful
+/// @note Assumes that the firmware binaries and data structures are loaded appropriately
+///
+fapi2::ReturnCode start_training(const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>& i_target)
+{
+    // Per the Synopsys documentation, to start the training, there is a latching sequence
+    // 1. Configure the PHY to allow training access (wait 40 cycles)
+    // 2. Reset and stall the processor (gets it into a base state but does not start it)
+    // 3. Stall the processor (releases reset but does not start it)
+    // 4. Start training (release stall/reset)
+    fapi2::buffer<uint64_t> l_data;
+
+    // 1. Configure the PHY to allow training access (wait 40 cycles after)
+    // Note: OFF_N refers to scom access -> so this enables training access
+    FAPI_TRY(configure_phy_scom_access(i_target, mss::states::OFF_N));
+    // Note: pausing for 100 ns, which should be more than 40 cycles at Odyssey's min frequency
+    FAPI_TRY( fapi2::delay( 100, 40) );
+
+    // 2. Reset and stall the processor (gets it into a base state but does not start it)
+    l_data.setBit<MIRCORESET_STALLTOMICRO>().setBit<MIRCORESET_RESETTOMICRO>();
+    FAPI_TRY(fapi2::putScom(i_target, MIRCORESET, l_data));
+
+    // 3. Stall the processor (releases reset but does not start it)
+    l_data.flush<0>().setBit<MIRCORESET_STALLTOMICRO>();
+    FAPI_TRY(fapi2::putScom(i_target, MIRCORESET, l_data));
+
+    // 4. Start training (release stall/reset)
+    l_data.flush<0>();
+    FAPI_TRY(fapi2::putScom(i_target, MIRCORESET, l_data));
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+///
+/// @brief Cleans up from the firmware draminit training
+/// @param[in] i_target the target on which to operate
+/// @return fapi2::FAPI2_RC_SUCCESS iff successful
+/// @note puts the processor into a stall state
+///
+fapi2::ReturnCode cleanup_training(const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>& i_target)
+{
+    // Per the Synopsys documentation, to cleanup after the training:
+    // 1. Stop the processor (stall it)
+    // 2. Reset the calibration engines to their initial state (cal Zap!)
+    fapi2::buffer<uint64_t> l_data;
+
+    // 1. Stop the processor (stall it)
+    l_data.setBit<MIRCORESET_STALLTOMICRO>();
+    FAPI_TRY(fapi2::putScom(i_target, MIRCORESET, l_data));
+
+    // 2. Reset the calibration engines to their initial state (cal Zap!)
+    l_data.flush<0>().setBit<CALZAP_CALZAP>();
+    FAPI_TRY(fapi2::putScom(i_target, CALZAP, l_data));
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+} // namespace phy
+} // namespace ody
+} // namespace mss
--- src/import/chips/ocmb/odyssey/procedures/hwp/memory/lib/phy/ody_phy_utils.C
+++ src/import/chips/ocmb/odyssey/procedures/hwp/memory/lib/phy/ody_phy_utils.C
@@ -1,7 +1,7 @@
 /* IBM_PROLOG_BEGIN_TAG                                                   */
 /* This is an automatically generated prolog.                             */
 /*                                                                        */
-/* $Source: src/import/chips/ocmb/odyssey/procedures/hwp/memory/lib/phy/ody_draminit_procedure.C $ */
+/* $Source: src/import/chips/ocmb/odyssey/procedures/hwp/memory/lib/phy/ody_phy_utils.C $ */
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
@@ -24,9 +24,8 @@
 /* IBM_PROLOG_END_TAG                                                     */
 // EKB-Mirror-To: hostboot
 ///
-/// @file ody_draminit_procedure.C
-/// @brief Odyssey draminit procedure
-/// @note Using a separate file as simulation might need a different draminit procedure for now
+/// @file ody_phy_utils.C
+/// @brief Odyssey PHY utility functions
 ///
 // *HWP HWP Owner: Stephen Glancy <sglancy@us.ibm.com>
 // *HWP HWP Backup: Louis Stermole <stermole@us.ibm.com>
@@ -36,72 +35,43 @@
 
 #include <fapi2.H>
 
-#include <generic/memory/lib/utils/find.H>
+#include <generic/memory/lib/utils/shared/mss_generic_consts.H>
 
-#include <lib/phy/ody_draminit_utils.H>
 #include <lib/phy/ody_phy_utils.H>
+// TODO:ZEN:MST-1571 Update Odyssey PHY registers when the official values are merged into the EKB
 
 namespace mss
 {
 namespace ody
 {
-
-///
-/// @brief Runs draminit
-/// @param[in] i_target the target on which to operate
-/// @return fapi2::FAPI2_RC_SUCCESS iff successful
-/// @note Assumes PHY init has already been run
-///
-fapi2::ReturnCode draminit(const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>& i_target)
+namespace phy
 {
-    // 1. Loads the IMEM Memory (instructions)
-    // TODO:ZEN:MST-1561 Create code to load IMEM, DMEM, and message block onto Synopsys PHY
 
-    // 2. Loads the DMEM Memory (data)
-    // TODO:ZEN:MST-1561 Create code to load IMEM, DMEM, and message block onto Synopsys PHY
+///
+/// @brief Configure the PHY to allow/disallow register accesses via scom
+/// @param[in] i_target the target on which to operate
+/// @param[in] i_state the state to set the PHY to - either mss::states::ON_N (scom access) or mss::states::OFF_N (training access)
+/// @return fapi2::FAPI2_RC_SUCCESS iff successful
+///
+fapi2::ReturnCode configure_phy_scom_access(const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>& i_target,
+        const mss::states i_state)
+{
+    // TODO:ZEN:MST-1571 Update Odyssey PHY registers when the official values are merged into the EKB
+    // For now using the Synopsys register location documentation
+    constexpr uint64_t MICROCONTMUXSEL = 0x000d0000;
+    constexpr uint64_t MICROCONTMUXSEL_MICROCONTMUXSEL = 63;
 
-    // 3. Configures and loads the message block
-    // TODO:ZEN:MST-1561 Create code to load IMEM, DMEM, and message block onto Synopsys PHY
+    fapi2::buffer<uint64_t> l_data;
+    FAPI_TRY(fapi2::getScom(i_target, MICROCONTMUXSEL, l_data));
 
-    // 4. Starts training
-    FAPI_TRY(mss::ody::phy::start_training(i_target));
+    l_data.writeBit<MICROCONTMUXSEL_MICROCONTMUXSEL>(i_state);
 
-    // 5. Processes and handles training messages (aka poll for completion)
-    // TODO:ZEN:MST-1542 Add base code and control for Synopsys mailbox interaction
-
-    // 6. Cleans up after training
-    FAPI_TRY(mss::ody::phy::cleanup_training(i_target));
-
-    // 7. Read the data structure and set attributes
-    // TODO:ZEN:MST-1567 Create code to process data from the Synopsys message block
-
-    // 8. Error handling
-    // TODO:ZEN:MST-1568 Add Odyssey draminit error processing
+    FAPI_TRY(fapi2::putScom(i_target, MICROCONTMUXSEL, l_data));
 
 fapi_try_exit:
     return fapi2::current_err;
 }
 
-///
-/// @brief Runs draminit
-/// @param[in] i_target the target on which to operate
-/// @return fapi2::FAPI2_RC_SUCCESS iff successful
-/// @note Assumes PHY init has already been run
-///
-fapi2::ReturnCode draminit(const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
-{
-    for(const auto& l_port : mss::find_targets<fapi2::TARGET_TYPE_MEM_PORT>(i_target))
-    {
-        // Note: This will need to be updated to allow training to fail on one port but continue on the second
-        FAPI_TRY(draminit(l_port));
-    }
-
-    // Blame FIRs and unmask FIRs (done on the OCMB chip level)
-    // TODO:ZEN:MST-1530 Specialize unmask::after_draminit_training for Odyssey
-
-fapi_try_exit:
-    return fapi2::current_err;
-}
-
+} // namespace phy
 } // namespace ody
 } // namespace mss
