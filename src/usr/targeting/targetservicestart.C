866277ae0973195cb495b93089cd2611b1c3d93f
Mike Baiocchi <mbaiocch@us.ibm.com>
2022-03-23 22:37:49 -0500

    Enable HW VPD writes on eBMC platforms
    This commit enables HW VPD Writes to the EEPROMs on the eBMC systems by
    setting ATTR_ALLOW_EEPROM_WRITES to true.
    
    Change-Id: I2a50c71607353aaec008c4a075c959329270125e
    RTC:269891
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/129257
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Chen Du <duchen@us.ibm.com>
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>
    Reviewed-by: Nick Bofferding <bofferdn@us.ibm.com>

--- src/usr/targeting/targetservicestart.C
+++ src/usr/targeting/targetservicestart.C
@@ -722,13 +722,7 @@ static void initializeAttributes(TargetService& i_targetService,
         else
         {
             l_pTopLevel->setAttr<ATTR_IS_MPIPL_HB>(0);
-
-            // TODO RTC 269891: Remove this block of code when all platforms
-            // support HW VPD writes; for now, only FSP based systems have this
-            // verified.
-            #ifdef CONFIG_FSP_BUILD
             l_pTopLevel->setAttr<TARGETING::ATTR_ALLOW_EEPROM_WRITES>(true);
-            #endif
 
             // Compute any values that might change based on a remap of memory
             adjustMemoryMap(i_targetService);
