54d40f515f5dcf42f6ba46f0d095579aedbd6ab5
Ilya Smirnov <ismirno@us.ibm.com>
2022-04-11 14:01:31 -0500

    Convert Date/Time in Unflatten
    The date/time in unflatten for IPL flow needs to be converted from
    BCD format to regular date/time format, so that when the flatten
    is called again on the data, the date/time will be correctly
    displayed in the error log.
    
    Change-Id: I8c4eef383b01335464e436eab4c34cf1e1696bf0
    CQ: SW547847
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/130544
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/include/runtime/interface.h
+++ src/include/runtime/interface.h
@@ -862,8 +862,8 @@ typedef struct hostInterfaces
     struct dateTime
     {
         uint16_t year;
-        uint8_t day;
         uint8_t month;
+        uint8_t day;
         uint8_t hour;
         uint8_t minute;
         uint8_t second;
