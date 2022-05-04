491b5a668f39ee7359189850417640b6cd582c80
Amita Banchhor <amita.banchhor@ibm.com>
2022-05-04 13:51:00 -0400

    Creating code to load IMEM, DMEM, and message block onto Synopsys PHY
    Change-Id: If2dba59fffcbba8d660983a2d4a7050602c43c49
    Original-Change-Id: I18c562695fea8de7d756d21b8479f0a7371e2176
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/131392
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: NAREN AMMANDA DEVAIAH <naren.devaiah@in.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/151635
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/ocmb/odyssey/procedures/hwp/memory/tests/ody_draminit_utils_ut.C
+++ src/import/chips/ocmb/odyssey/procedures/hwp/memory/tests/ody_draminit_utils_ut.C
@@ -43,6 +43,8 @@
 #include <generic/memory/lib/utils/count_dimm.H>
 #include <lib/phy/ody_draminit_utils.H>
 
+
+
 namespace mss
 {
 namespace test
@@ -96,7 +98,138 @@ SCENARIO_METHOD(ocmb_chip_target_test_fixture, "DRAMINIT utility unit tests", "[
         REQUIRE(l_rcw_info.iv_rcw_page == 0x56);
         REQUIRE(l_rcw_info.iv_rcw_val == 0x78);
     }
-} // scenario
+
+    GIVEN("Calculate image end addr")
+    {
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x50000, 65536) == 0x57fff);
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x50000, 6553) == 0x50CCC);
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x50000, 66000) == 0x580E7);
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x50000, 1) == 0x50000);
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x50000, 2) == 0x50000);
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x50000, 3) == 0x50001);
+
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x58000, 65536) == 0x5ffff);
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x58000, 6553) == 0x58CCC);
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x58000, 66000) == 0x600E7);
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x58000, 1) == 0x58000);
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x58000, 2) == 0x58000);
+        REQUIRE(mss::ody::phy::calculate_image_end_addr(0x58000, 3) == 0x58001);
+
+    }
+
+    GIVEN("Testing the NULL pointer in load_mem_bin_data()")
+    {
+        FAPI_INF("Testing NULL pointer for DMEM/IMEM image data");
+
+        // Loops over OCMB chip targets that were defined in the associated config
+        for_each_target([](const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
+        {
+            REQUIRE_SPECIFIC_RC_FAIL(mss::ody::phy::load_mem_bin_data(i_target, 0x58000, NULL, 0x400, 0x8000),
+                                     fapi2::RC_ODY_DRAMINIT_START_DATA_PTR_NULL);
+            REQUIRE_SPECIFIC_RC_FAIL(mss::ody::phy::load_mem_bin_data(i_target, 0x50000, NULL, 0x400, 0x8000),
+                                     fapi2::RC_ODY_DRAMINIT_START_DATA_PTR_NULL);
+
+            return 0;
+        });
+    }
+
+    GIVEN("Testing out of bounds check in load_dmem_helper()")
+    {
+        FAPI_INF("Testing out of bounds check for dmem image data");
+
+        // Loops over OCMB chip targets that were defined in the associated config
+        for_each_target([](const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
+        {
+            uint8_t l_dmem_image_data[] = {0x12, 0x34, 0x56, 0x78, 0x9a};
+
+            REQUIRE_SPECIFIC_RC_FAIL(mss::ody::phy::ody_load_dmem_helper(i_target, l_dmem_image_data, 0x5, 0xFFFE),
+                                     fapi2::RC_ODY_DRAMINIT_MEM_ADDR_RANGE_OUT_OF_BOUNDS);
+            REQUIRE_SPECIFIC_RC_FAIL(mss::ody::phy::ody_load_dmem_helper(i_target, l_dmem_image_data, 0x10001, 0),
+                                     fapi2::RC_ODY_DRAMINIT_MEM_ADDR_RANGE_OUT_OF_BOUNDS);
+
+
+            return 0;
+        });
+    }
+
+    GIVEN("Testing pass case scenario for load_dmem_helper()")
+    {
+        FAPI_INF("Testing passing scenario for DMEM image data");
+        // Loops over OCMB chip targets that were defined in the associated config
+        for_each_target([](const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
+        {
+            uint8_t l_dmem_image_data[0x10000] = {};
+
+            REQUIRE_RC_PASS(mss::ody::phy::ody_load_dmem_helper(i_target, l_dmem_image_data, 0x10000, 0));
+            REQUIRE_RC_PASS(mss::ody::phy::ody_load_dmem_helper(i_target, l_dmem_image_data, 0x8000, 0x8000));
+
+            return 0;
+        });
+    }
+
+
+    GIVEN("Testing odd offset check in load_dmem_helper()")
+    {
+        FAPI_INF("Testing offset check for DMEM image data");
+
+        // Loops over OCMB chip targets that were defined in the associated config
+        for_each_target([](const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
+        {
+            REQUIRE_SPECIFIC_RC_FAIL(mss::ody::phy::ody_load_dmem_helper(i_target, NULL, 0x10000, 1),
+                                     fapi2::RC_ODY_DRAMINIT_OFFSET_UNSUPPORTED);
+            return 0;
+        });
+    }
+
+    GIVEN("Testing out of bounds check in load_imem_helper()")
+    {
+        FAPI_INF("Testing out of bounds check for imem image data");
+
+        // Loops over OCMB chip targets that were defined in the associated config
+        for_each_target([](const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
+        {
+            uint8_t l_imem_image_data[] = {0x12, 0x34, 0x56, 0x78, 0x9a};
+
+            REQUIRE_SPECIFIC_RC_FAIL(mss::ody::phy::ody_load_imem_helper(i_target, l_imem_image_data, 0x5, 0xFFFE),
+                                     fapi2::RC_ODY_DRAMINIT_MEM_ADDR_RANGE_OUT_OF_BOUNDS);
+            REQUIRE_SPECIFIC_RC_FAIL(mss::ody::phy::ody_load_imem_helper(i_target, l_imem_image_data, 0x10001, 0),
+                                     fapi2::RC_ODY_DRAMINIT_MEM_ADDR_RANGE_OUT_OF_BOUNDS);
+
+
+            return 0;
+        });
+    }
+
+    GIVEN("Testing pass case scenario for load_imem_helper()")
+    {
+        FAPI_INF("Testing pass scenario for imem image data");
+
+        // Loops over OCMB chip targets that were defined in the associated config
+        for_each_target([](const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
+        {
+            uint8_t l_imem_image_data[0x10000] = {};
+
+            REQUIRE_RC_PASS(mss::ody::phy::ody_load_imem_helper(i_target, l_imem_image_data, 0x10000, 0));
+            REQUIRE_RC_PASS(mss::ody::phy::ody_load_imem_helper(i_target, l_imem_image_data, 0x8000, 0x8000));
+
+            return 0;
+        });
+    }
+
+
+    GIVEN("Testing odd offset check in load_imem_helper()")
+    {
+        FAPI_INF("Testing offset check for imem image data");
+
+        // Loops over OCMB chip targets that were defined in the associated config
+        for_each_target([](const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
+        {
+            REQUIRE_SPECIFIC_RC_FAIL(mss::ody::phy::ody_load_imem_helper(i_target, NULL, 0x10000, 1),
+                                     fapi2::RC_ODY_DRAMINIT_OFFSET_UNSUPPORTED);
+            return 0;
+        });
+    }
+}// scenario
 
 } // end ns test
 } // end ns mss
