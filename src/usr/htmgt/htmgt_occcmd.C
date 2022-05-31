51d7f4dd3075dba9ac4f5fb23462331fa0c3e49b
Chris Cain <cjcain@us.ibm.com>
2022-05-31 12:59:14 -0500

    HTMGT: Commit OCC exceptions as INFO until safe mode
    Current HTMGT code would log all OCC exceptions as unrecoverable.  This
    commit will make the logs information unless the next reset will move
    the system to safe mode.
    It will also prevent the excption data from being truncated.
    
    Change-Id: I1a40249fb509e57ac36c1966c30ffc47846998e5
    CQ: SW551985
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/132330
    Reviewed-by: Martha Broyles <mbroyles@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Sheldon Bailey <baileysh@us.ibm.com>
    Reviewed-by: Nick Bofferding <bofferdn@us.ibm.com>

--- src/usr/htmgt/htmgt_occcmd.C
+++ src/usr/htmgt/htmgt_occcmd.C
@@ -53,8 +53,6 @@ namespace HTMGT
     uint8_t g_cmd = 0;
     uint8_t g_seq = 0;
 
-    const uint16_t MAX_FFDC = 512;
-
     const uint8_t OCC_COMM_ESTABLISHED_CHECKPOINT[2] = { 0x0E, 0xFF };
 
     struct occCircBufferCmd_t
@@ -557,7 +555,7 @@ namespace HTMGT
             {
                 l_errlHndl->addFFDC(HTMGT_COMP_ID,
                                     iv_OccCmd.cmdData,
-                                    std::min(iv_OccCmd.dataLength, MAX_FFDC),
+                                    iv_OccCmd.dataLength,
                                     1,  // version
                                     SUBSEC_OCC_CMD_DATA);
             }
@@ -859,15 +857,21 @@ namespace HTMGT
                      * @devdesc OCC reported exception
                      */
                     errlHndl_t l_excErr = NULL;
+                    ERRORLOG::errlSeverity_t severity =
+                        ERRORLOG::ERRL_SEV_INFORMATIONAL;
+                    if (OCC_RESET_COUNT_THRESHOLD == iv_Occ->iv_resetCount)
+                    {
+                        // commit as unrecoverable if will end up in safe mode
+                        severity = ERRORLOG::ERRL_SEV_UNRECOVERABLE;
+                    }
                     bldErrLog(l_excErr, HTMGT_MOD_HANLDE_OCC_EXCEPTION,
                               (htmgtReasonCode)(OCCC_COMP_ID | exceptionType),
                               exceptionType, exceptionDataLength,
                               iv_Occ->iv_instance, UINT32_GET(&sramRspPtr[5]),
-                              ERRORLOG::ERRL_SEV_UNRECOVERABLE);
+                              severity);
                     l_excErr->addFFDC(OCCC_COMP_ID,
                                       sramRspPtr,
-                                      std::min(exceptionLength,
-                                      (uint32_t)MAX_FFDC),
+                                      exceptionLength,
                                       1,  // version
                                       exceptionType); // subsection
 #ifdef CONFIG_CONSOLE_OUTPUT_OCC_COMM
@@ -1205,7 +1209,7 @@ namespace HTMGT
             // Add full OCC response to user details
             l_errlHndl->addFFDC(HTMGT_COMP_ID,
                                 rspBuffer,
-                                std::min(rspLen, MAX_FFDC),
+                                rspLen,
                                 1,  // version
                                 SUBSEC_OCC_RSP_DATA);
         }
@@ -1356,7 +1360,7 @@ namespace HTMGT
             // Add OCC response data to user details
             l_errlHndl->addFFDC(HTMGT_COMP_ID,
                                 iv_OccRsp.rspData,
-                                std::min(iv_OccRsp.dataLength,MAX_FFDC),
+                                iv_OccRsp.dataLength,
                                 1,  // version
                                 SUBSEC_OCC_RSP_DATA);
         }
