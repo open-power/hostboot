8661c1bb47b82c4e6c7ae185c843a4b15bc5e65b
Thi Tran <thi@us.ibm.com>
2022-06-15 17:48:26 -0400

    Add Odyssey scan/scom/clock_domains
    Change-Id: Ie70575fac2296957952fe11dae8e73900bfa972e
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/132900
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Douglas Holtsinger <douglas.holtsinger@ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/158198
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

---
+++ src/import/chips/ocmb/odyssey/common/scominfo/odyssey_scom_addr.C
@@ -0,0 +1,143 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/ocmb/odyssey/common/scominfo/odyssey_scom_addr.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2018,2023                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file odyssey_scom_addr.C
+/// @brief ODYSSEY chip unit SCOM address platform translation code
+///
+/// HWP HW Maintainer: Thi Tran <thi@us.ibm.com>
+/// HWP FW Maintainer:
+/// HWP Consumed by: Cronus, HB
+///
+
+// includes
+#include "odyssey_scom_addr.H"
+
+#define ODYSSEY_SCOM_ADDR_C
+
+extern "C"
+{
+    /// See function description in header file
+
+    // #####################################
+    bool odyssey_scom_addr::isPervTarget()
+    {
+        bool l_pervTarget = false;
+
+        if (getChipletId() <= 0x1)  // Allow PIB (0x00) and Perv chiplet (0x01)
+        {
+            if (getEndpoint() == PSCOM_ENDPOINT)            // 0x1
+            {
+                if ( (getRingId() == PSCOM_RING_ID) ||    // 0x0
+                     (getRingId() == PERV_RING_ID) )    // 0x1
+                {
+                    l_pervTarget = true;
+                }
+            }
+            else if (getEndpoint() == CLOCK_CTRL_ENDPOINT)  // 0x3
+            {
+                l_pervTarget = true;
+            }
+            // Check if Endpoint is a PERV endpoint
+            else if ( (getEndpoint() == CHIPLET_CTRL_ENDPOINT) ||     // 0x0
+                      (getEndpoint() == FIR_ENDPOINT)          ||     // 0x4
+                      (getEndpoint() == THERMAL_ENDPOINT)      ||     // 0x5
+                      (getEndpoint() == PCBSLV_ENDPOINT) )            // 0xF
+            {
+                if ( getRingId() == PSCOM_RING_ID)                    // 0x0
+                {
+                    l_pervTarget = true;
+                }
+            }
+        }
+
+        return l_pervTarget;
+    }
+
+    // #####################################
+    uint8_t odyssey_scom_addr::getPervTargetInstance()
+    {
+        return getChipletId();
+    }
+
+    // #####################################
+    bool odyssey_scom_addr::isOmiTarget()
+    {
+        bool l_omiTarget = false;
+
+        if ( ( getRingId() == OMI0_RING_ID ||
+               getRingId() == DLX_RING_ID ) && ( getChipletId() == MEM_CHIPLET_ID ) )
+        {
+            l_omiTarget = true;
+        }
+
+        return l_omiTarget;
+    }
+
+    // #####################################
+    uint8_t odyssey_scom_addr::getOmiTargetInstance()
+    {
+        uint8_t l_instance = 0;
+        return l_instance;
+    }
+
+    // #####################################
+    bool odyssey_scom_addr::isMemportTarget()
+    {
+        bool l_memportTarget = false;
+
+        if ( (getChipletId() == MEM_CHIPLET_ID) &&
+             ( (getRingId() == MEMPORT0_RING_ID) ||
+               (getRingId() == MEMPORT1_RING_ID) ||
+               (getRingId() == MEMPORT0_PHY_RING_ID) ||
+               (getRingId() == MEMPORT1_PHY_RING_ID) ))
+        {
+            l_memportTarget = true;
+        }
+
+        return l_memportTarget;
+    }
+
+    // #####################################
+    uint8_t odyssey_scom_addr::getMemportTargetInstance()
+    {
+        uint8_t l_instance = 0;
+
+        if (getRingId() == MEMPORT1_RING_ID)
+        {
+            l_instance = 1;
+        }
+
+        if (getRingId() == MEMPORT1_PHY_RING_ID)
+        {
+            l_instance = 1;
+        }
+
+        return l_instance;
+    }
+
+
+} // extern "C"
+
+#undef ODYSSEY_SCOM_ADDR_C
---
+++ src/import/chips/ocmb/odyssey/common/scominfo/odyssey_scominfo.C
@@ -0,0 +1,201 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/ocmb/odyssey/common/scominfo/odyssey_scominfo.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2018,2023                        */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+///
+/// @file odyssey_scominfo.C
+/// @brief ODYSSEY chip unit SCOM address platform translation code
+///
+/// HWP HW Maintainer: Thi Tran <thi@us.ibm.com>
+/// HWP FW Maintainer:
+/// HWP Consumed by: Cronus, HB
+///
+
+// includes
+#include <stdio.h>
+#include <odyssey_scominfo.H>
+#include <odyssey_scom_addr.H>
+#include <odyssey_cu_utils.H>
+
+#define ODYSSEY_SCOMINFO_C
+
+extern "C"
+{
+
+    // ---------------------------
+    // Internal functions
+    // ---------------------------
+
+    // See header file for function description
+    uint64_t odyssey_scominfo_createChipUnitScomAddr(
+        const odysseyChipUnits_t i_odysseyCU,
+        const uint8_t i_ecLevel,
+        const uint8_t i_chipUnitNum,
+        const uint64_t i_scomAddr,
+        const odysseyTranslationMode_t i_mode)
+    {
+        uint8_t l_rc = 0;
+        odyssey_scom_addr l_scom(i_scomAddr);
+
+        do
+        {
+            // If chip unit type is a chip, return input address
+            if (i_odysseyCU == ODYSSEY_NO_CU)
+            {
+                l_scom.setAddr(i_scomAddr);
+                break;
+            }
+
+            // Make sure i_chipUnitNum is within range
+            l_rc = odyssey_validateChipUnitNum(i_chipUnitNum, i_odysseyCU);
+
+            if (l_rc)
+            {
+                break;
+            }
+
+            // Set other address fields (ringId, satId, etc...)
+            // for Chip unit types that are needed.
+            switch (i_odysseyCU)
+            {
+                case ODYSSEY_PERV_CHIPUNIT:
+                    l_scom.setChipletId(i_chipUnitNum);
+                    break;
+
+                case ODYSSEY_MEMPORT_CHIPUNIT:
+                    {
+                        uint8_t l_ring = l_scom.getRingId();
+
+                        if (l_ring == MEMPORT0_RING_ID || l_ring == MEMPORT1_RING_ID)
+                        {
+                            if (i_chipUnitNum == 0)
+                            {
+                                l_scom.setRingId(MEMPORT0_RING_ID);
+                            }
+                            else
+                            {
+                                l_scom.setRingId(MEMPORT1_RING_ID);
+                            }
+                        }
+
+                        if (l_ring == MEMPORT0_PHY_RING_ID || l_ring == MEMPORT1_PHY_RING_ID)
+                        {
+                            if (i_chipUnitNum == 0)
+                            {
+                                l_scom.setRingId(MEMPORT0_PHY_RING_ID);
+                            }
+                            else
+                            {
+                                l_scom.setRingId(MEMPORT1_PHY_RING_ID);
+                            }
+                        }
+                    }
+                    break;
+
+                case ODYSSEY_OMI_CHIPUNIT:
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+        while (0);
+
+        if (l_rc)
+        {
+            l_scom.setAddr(FAILED_TRANSLATION);
+        }
+
+        return l_scom.getAddr();
+    }
+
+    // See header file for function description
+    uint32_t odyssey_scominfo_isChipUnitScom(const odysseyChipUnits_t i_odysseyCU,
+            const uint8_t i_ecLevel,
+            const uint64_t i_scomAddr,
+            bool& o_chipUnitRelated,
+            std::vector<odyssey_chipUnitPairing_t>& o_chipUnitPairing,
+            const odysseyTranslationMode_t i_mode)
+    {
+        odyssey_scom_addr l_scom(i_scomAddr);
+        o_chipUnitRelated = false;
+        o_chipUnitPairing.clear();
+
+        // All Odyssey SCOM regs are not chip unit related in ENGD
+        if (i_mode == ODYSSEY_ENGD_BUILD_MODE)
+        {
+            return (!l_scom.isValid());
+        }
+
+        // OMI registers
+        if (l_scom.isOmiTarget())
+        {
+            o_chipUnitRelated = true;
+            // ODYSSEY_OMI_CHIPUNIT
+            o_chipUnitPairing.push_back(odyssey_chipUnitPairing_t(ODYSSEY_OMI_CHIPUNIT,
+                                        l_scom.getOmiTargetInstance()));
+        }
+
+        // PERV registers
+        if (l_scom.isPervTarget())
+        {
+            o_chipUnitRelated = true;
+            // ODYSSEY_PERV_CHIPUNIT
+            o_chipUnitPairing.push_back(odyssey_chipUnitPairing_t(ODYSSEY_PERV_CHIPUNIT,
+                                        l_scom.getPervTargetInstance()));
+        }
+
+        // Memport registers
+        if (l_scom.isMemportTarget())
+        {
+            o_chipUnitRelated = true;
+            // ODYSSEY_MEMPORT_CHIPUNIT
+            o_chipUnitPairing.push_back(odyssey_chipUnitPairing_t(ODYSSEY_MEMPORT_CHIPUNIT,
+                                        l_scom.getMemportTargetInstance()));
+        }
+
+        /// Address may be of a chip, let it pass through
+        return (!l_scom.isValid());
+    }
+
+    // See header file for function description
+    uint32_t odyssey_scominfo_fixChipUnitScomAddrOrTarget(const odysseyChipUnits_t i_odysseyCU,
+            const uint8_t i_ecLevel,
+            const uint32_t i_targetChipUnitNum,
+            const uint64_t i_scomaddr,
+            uint64_t& o_modifiedScomAddr,
+            odysseyChipUnits_t& o_odysseyCU,
+            uint32_t& o_modifiedChipUnitNum,
+            const odysseyTranslationMode_t i_mode)
+    {
+        uint32_t l_rc = 0;
+        o_modifiedScomAddr = i_scomaddr;
+        o_odysseyCU = i_odysseyCU;
+        o_modifiedChipUnitNum = i_targetChipUnitNum;
+        return l_rc;
+    }
+
+} // extern "C"
+
+#undef ODYSSEY_SCOMINFO_C
