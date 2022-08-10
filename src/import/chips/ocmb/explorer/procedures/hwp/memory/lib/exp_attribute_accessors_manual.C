8f5fdd3cf3a0cd60edece360ad0c4a1b928415fa
David J Chung <DJ.chung@ibm.com>
2022-08-10 12:30:42 -0400

    Ported has_rcd to generic
    git-coreq:hostboot:I3ff2b5ca49f766679eec3ebbcf7b825bdffa7e5a
    Change-Id: I3ff2b5ca49f766679eec3ebbcf7b825bdffa7e5a
    ZEN:MST-1435
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/135906
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: Geetha Pisapati <geetha.pisapati@ibm.com>
    Reviewed-by: Edgar Cordero <ecordero@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/135976
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/ocmb/explorer/procedures/hwp/memory/lib/exp_attribute_accessors_manual.C
+++ src/import/chips/ocmb/explorer/procedures/hwp/memory/lib/exp_attribute_accessors_manual.C
@@ -42,93 +42,9 @@
 #include <lib/dimm/exp_kind.H>
 #include <mss_p10_attribute_getters.H>
 #include <mss_generic_system_attribute_getters.H>
-#include <generic/memory/lib/generic_attribute_accessors_manual.H>
 
 namespace mss
 {
-
-///
-/// @brief Check if any dimms exist that have RCD enabled - DIMM specialization
-/// @param[in] i_target - the fapi2::Target we are starting from
-/// @param[out] o_has_rcd - true iff any DIMM with RCD detected
-/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS iff ok
-///
-fapi2::ReturnCode has_rcd( const fapi2::Target<fapi2::TARGET_TYPE_DIMM>& i_target,
-                           bool& o_has_rcd )
-{
-    // Assume RCD is not supported at beginning of check
-    o_has_rcd = false;
-
-    uint8_t l_dimm_type = 0;
-    uint8_t l_rcd_supported = 0;
-
-    FAPI_TRY(mss::attr::get_dimm_type(i_target, l_dimm_type));
-    FAPI_TRY(mss::attr::get_supported_rcd(i_target, l_rcd_supported));
-
-    // OR with tmp_rcd to maintain running true/false if RCD on *any* DIMM
-    o_has_rcd |= ((l_dimm_type == fapi2::ENUM_ATTR_MEM_EFF_DIMM_TYPE_RDIMM) ||
-                  (l_dimm_type == fapi2::ENUM_ATTR_MEM_EFF_DIMM_TYPE_LRDIMM));
-
-    o_has_rcd |= (l_rcd_supported == fapi2::ENUM_ATTR_MEM_EFF_SUPPORTED_RCD_RCD_PER_CHANNEL_1);
-
-fapi_try_exit:
-
-    return fapi2::current_err;
-}
-
-///
-/// @brief Check if any dimms exist that have RCD enabled - MEM PORT specialization
-/// @param[in] i_target - the fapi2::Target we are starting from
-/// @param[out] o_has_rcd - true iff any DIMM with RCD detected
-/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS iff ok
-///
-fapi2::ReturnCode has_rcd( const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>& i_target,
-                           bool& o_has_rcd )
-{
-    // Assume RCD is not supported at beginning of check
-    o_has_rcd = false;
-
-    // Loop over all DIMM's and determine if we have an RCD
-    for(const auto& l_dimm : mss::find_targets<fapi2::TARGET_TYPE_DIMM>(i_target))
-    {
-        bool l_current_dimm_rcd = false;
-        FAPI_TRY(has_rcd(l_dimm, l_current_dimm_rcd));
-        o_has_rcd |= l_current_dimm_rcd;
-    }
-
-    return fapi2::FAPI2_RC_SUCCESS;
-
-fapi_try_exit:
-
-    return fapi2::current_err;
-}
-
-///
-/// @brief Check if any dimms exist that have RCD enabled - OCMB CHIP specialization
-/// @param[in] i_target - the fapi2::Target we are starting from
-/// @param[out] o_has_rcd - true iff any DIMM with RCD detected
-/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS iff ok
-///
-fapi2::ReturnCode has_rcd( const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target,
-                           bool& o_has_rcd )
-{
-    // Assume RCD is not supported at beginning of check
-    o_has_rcd = false;
-
-    // Nested for loops to determine DIMM type if DIMMs exist
-    for (const auto& l_port : mss::find_targets<fapi2::TARGET_TYPE_MEM_PORT>(i_target))
-    {
-        bool l_current_port_rcd = false;
-        FAPI_TRY(has_rcd(l_port, l_current_port_rcd));
-        o_has_rcd |= l_current_port_rcd;
-    }
-
-    return fapi2::FAPI2_RC_SUCCESS;
-
-fapi_try_exit:
-
-    return fapi2::current_err;
-}
 
 ///
 /// @brief Gets if the given target has a quad encoded CS - DIMM target specialization
