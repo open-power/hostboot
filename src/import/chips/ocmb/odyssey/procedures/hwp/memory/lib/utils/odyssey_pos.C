37b4f0bcb050440c96fdaa5b083adbb1fcb0c2c0
Stephen Glancy <sglancy@us.ibm.com>
2022-11-28 12:50:12 -0500

    Updates relative pos from port/ocmb chip to proc to use ATTR_BUS_POS
    ZEN:MST-1666
    Change-Id: I4310a90adeec410be8aa98abb69ce31ba4977275
    Original-Change-Id: I63cd237c09e98a1b2ad67e2e9eb531d6a35a7a2f
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/141696
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Sneha Kadam <sneha.kadam1@ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: Geetha Pisapati <geetha.pisapati@ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Dev-Ready: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Edgar Cordero <ecordero@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/143721
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/ocmb/odyssey/procedures/hwp/memory/lib/utils/odyssey_pos.C
+++ src/import/chips/ocmb/odyssey/procedures/hwp/memory/lib/utils/odyssey_pos.C
@@ -35,6 +35,8 @@
 
 #include <generic/memory/lib/utils/pos.H>
 #include <lib/shared/ody_consts.H>
+#include <generic/memory/lib/utils/find.H>
+#include <generic/memory/lib/utils/mss_generic_check.H>
 
 namespace mss
 {
@@ -67,14 +69,21 @@ relative_pos<mc_type::ODYSSEY, fapi2::TARGET_TYPE_OCMB_CHIP>(const fapi2::Target
 ///
 /// @brief Return an MEM_PORT's relative position from an OCMB
 /// @param[in] i_target a target representing the target in question
-/// @return The position relative to chiplet R
+/// @return The position of the memory port relative to the processor chip
 ///
 template<>
 posTraits<fapi2::TARGET_TYPE_MEM_PORT>::pos_type
 relative_pos<mc_type::ODYSSEY, fapi2::TARGET_TYPE_OCMB_CHIP>(const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>&
         i_target)
 {
-    return pos(i_target) % mcTypeTraits<mss::mc_type::ODYSSEY>::PORTS_PER_OCMB;
+    uint8_t l_rel_pos = 0;
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_REL_POS, i_target, l_rel_pos));
+    return l_rel_pos;
+
+fapi_try_exit:
+    fapi2::current_err = fapi2::FAPI2_RC_SUCCESS;
+    FAPI_ERR(TARGTIDFORMAT " Attribute access error!", TARGTID);
+    return 0;
 }
 
 ///
@@ -110,34 +119,39 @@ relative_pos<mc_type::ODYSSEY, fapi2::TARGET_TYPE_MEM_PORT>(const fapi2::Target<
 }
 
 ///
-/// @brief Return a mem_port's relative position from a proc_chip
-/// @param[in] i_target a target representing the target in question
-/// @return The position relative to chiplet R
-///
-template<>
-posTraits<fapi2::TARGET_TYPE_MEM_PORT>::pos_type
-relative_pos<mc_type::ODYSSEY, fapi2::TARGET_TYPE_PROC_CHIP>(const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>&
-        i_target)
-{
-    return fapi_pos(i_target) % (mcTypeTraits<mss::mc_type::ODYSSEY>::PORTS_PER_OCMB *
-                                 mcTypeTraits<mss::mc_type::ODYSSEY>::OCMB_PER_OMI * mcTypeTraits<mss::mc_type::ODYSSEY>::OMI_PER_MCC *
-                                 mcTypeTraits<mss::mc_type::ODYSSEY>::MCC_PER_MI *
-                                 mcTypeTraits<mss::mc_type::ODYSSEY>::MI_PER_MC * mcTypeTraits<mss::mc_type::ODYSSEY>::MC_PER_PROC);
-}
-
-///
 /// @brief Return an OCMB's relative position from a proc_chip
 /// @param[in] i_target a target representing the target in question
-/// @return The position relative to chiplet R
+/// @return The position of the OCMB chip relative to the processor chip
 ///
 template<>
 posTraits<fapi2::TARGET_TYPE_OCMB_CHIP>::pos_type
 relative_pos<mc_type::ODYSSEY, fapi2::TARGET_TYPE_PROC_CHIP>(const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>&
         i_target)
 {
-    return fapi_pos(i_target) % (mcTypeTraits<mss::mc_type::ODYSSEY>::OCMB_PER_OMI *
-                                 mcTypeTraits<mss::mc_type::ODYSSEY>::OMI_PER_MCC * mcTypeTraits<mss::mc_type::ODYSSEY>::MCC_PER_MI *
-                                 mcTypeTraits<mss::mc_type::ODYSSEY>::MI_PER_MC * mcTypeTraits<mss::mc_type::ODYSSEY>::MC_PER_PROC);
+    uint8_t l_bus_pos = 0;
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_BUS_POS, i_target, l_bus_pos));
+    return l_bus_pos;
+
+fapi_try_exit:
+    fapi2::current_err = fapi2::FAPI2_RC_SUCCESS;
+    FAPI_ERR(TARGTIDFORMAT " Attribute access error!", TARGTID);
+    return 0;
+}
+
+///
+/// @brief Return a mem_port's relative position from a proc_chip
+/// @param[in] i_target a target representing the target in question
+/// @return The position of the memory port relative to the processor chip
+///
+template<>
+posTraits<fapi2::TARGET_TYPE_MEM_PORT>::pos_type
+relative_pos<mc_type::ODYSSEY, fapi2::TARGET_TYPE_PROC_CHIP>(const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>&
+        i_target)
+{
+    typedef mcTypeTraits<mss::mc_type::ODYSSEY> TT;
+    const auto& l_ocmb = mss::find_target<fapi2::TARGET_TYPE_OCMB_CHIP>(i_target);
+    return (relative_pos<mc_type::ODYSSEY, fapi2::TARGET_TYPE_OCMB_CHIP>(i_target)) +
+           (relative_pos<mc_type::ODYSSEY, fapi2::TARGET_TYPE_PROC_CHIP>(l_ocmb) * TT::PORTS_PER_OCMB);
 }
 
 ///
