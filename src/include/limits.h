b4bf5ad74828722b1e0aa4eb06b2b3b4748b3c58
Zane Shelley <zshelle@us.ibm.com>
2022-12-09 13:17:25 -0600

    Add support for std::numeric_limits
    Needed by openpower-libhei subtree.
    
    Change-Id: Iae2786abc884bdc02960e43fc72549385a0b5227
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/142419
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Zachary Clark <zach@ibm.com>
    Reviewed-by: Nick Bofferding <bofferdn@us.ibm.com>
    Reviewed-by: Zane C Shelley <zshelle@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/149670
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>

--- src/include/limits.h
+++ src/include/limits.h
@@ -37,7 +37,32 @@
 #define INT_MAX   __INT_MAX__
 #define SHRT_MAX  __SHRT_MAX__
 #define SSIZE_MAX __INT64_MAX__
+#define CHAR_BIT    __CHAR_BIT__
 
-#define CHAR_BIT 8 // Number of bits in a char
+#define SCHAR_MAX   __SCHAR_MAX__
+#define SCHAR_MIN   (-SCHAR_MAX - 1)
+#define UCHAR_MAX   (SCHAR_MAX * 2 + 1)
+
+#ifdef __CHAR_UNSIGNED__
+  #define CHAR_MAX  UCHAR_MAX
+  #define CHAR_MIN  0
+#else
+  #define CHAR_MAX  SCHAR_MAX
+  #define CHAR_MIN  SCHAR_MIN
+#endif
+
+#define SHRT_MIN    (-SHRT_MAX - 1)
+#define USHRT_MAX   (SHRT_MAX * 2 + 1)
+
+#define INT_MIN     (-INT_MAX - 1)
+#define UINT_MAX    (INT_MAX * 2U + 1U)
+
+#define LONG_MAX    __LONG_MAX__
+#define LONG_MIN    (-LONG_MAX - 1L)
+#define ULONG_MAX   (LONG_MAX * 2UL + 1UL)
+
+#define LLONG_MAX   __LONG_LONG_MAX__
+#define LLONG_MIN   (-LLONG_MAX - 1LL)
+#define ULLONG_MAX  (LLONG_MAX * 2ULL + 1ULL)
 
 #endif
