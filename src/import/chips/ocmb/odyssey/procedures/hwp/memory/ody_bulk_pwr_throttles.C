76726d69375a97b21da16e4108609ad0c1976559
Louis Stermole <stermole@us.ibm.com>
2023-02-01 17:17:46 -0500

    Add L2 version of ody_bulk_pwr_throttles HWP
    Zen:MST-1456
    Change-Id: If756efb66f4f7a084a32c57fef1c643ed01cabbf
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/144993
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Sneha Kadam <sneha.kadam1@ibm.com>
    Reviewed-by: ANDRE MARIN <aamarin@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Geetha Pisapati <geetha.pisapati@ibm.com>
    Reviewed-by: Michael D Pardeik <pardeik@us.ibm.com>
    Reviewed-by: Edgar Cordero <ecordero@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/144997
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/ocmb/odyssey/procedures/hwp/memory/ody_bulk_pwr_throttles.C
+++ src/import/chips/ocmb/odyssey/procedures/hwp/memory/ody_bulk_pwr_throttles.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2019,2022                        */
+/* Contributors Listed Below - COPYRIGHT 2019,2023                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -41,6 +41,8 @@
 #include <lib/shared/ody_consts.H>
 #include <generic/memory/lib/utils/find.H>
 #include <generic/memory/lib/utils/count_dimm.H>
+#include <lib/power_thermal/ody_throttle_traits.H>
+#include <lib/power_thermal/ody_throttle.H>
 
 extern "C"
 {
@@ -49,7 +51,7 @@ extern "C"
     /// @param[in] i_targets vector of OCMB chips
     /// @param[in] i_throttle_type thermal boolean to determine whether to calculate throttles based on the power regulator or thermal limits
     /// @return fapi2::ReturnCode - FAPI2_RC_SUCCESS iff get is OK
-    /// @note determines the throttle levels based off of the port's power curve,
+    /// @note determines the throttle levels based off of both ports' power curves,
     /// sets the slot throttles to the same
     /// @note Enums are POWER for power egulator throttles and THERMAL for thermal throttles
     /// @note equalizes the throttles to the lowest of runtime and the lowest slot-throttle value
@@ -57,7 +59,24 @@ extern "C"
     fapi2::ReturnCode ody_bulk_pwr_throttles( const std::vector< fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP> >& i_targets,
             const mss::throttle_type i_throttle_type)
     {
-        return fapi2::FAPI2_RC_SUCCESS;
+        FAPI_INF("Start ody_bulk_pwr_throttles for %s type throttling",
+                 (( i_throttle_type == mss::throttle_type::THERMAL) ? "THERMAL" : "POWER"));
+
+        for ( const auto& l_ocmb : i_targets)
+        {
+            FAPI_TRY(mss::power_thermal::pwr_throttles<mss::mc_type::ODYSSEY>(l_ocmb, i_throttle_type));
+        }
+
+        // Equalizes the throttles to the lowest of runtime and the lowest slot-throttle value
+        FAPI_TRY(mss::power_thermal::equalize_throttles<mss::mc_type::ODYSSEY>(i_targets, i_throttle_type));
+
+        FAPI_INF("End ody_bulk_pwr_throttles");
+        return fapi2::current_err;
+
+    fapi_try_exit:
+        FAPI_ERR("Error calculating ody_bulk_pwr_throttles using %s throttling",
+                 ((i_throttle_type == mss::throttle_type::POWER) ? "power" : "thermal"));
+        return fapi2::current_err;
     }
 
 } //extern C
