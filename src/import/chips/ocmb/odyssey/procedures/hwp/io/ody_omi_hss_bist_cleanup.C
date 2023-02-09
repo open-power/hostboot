b9c020989a8daf49055e21f8c4b4aeffb2ce6655
Josh-Chica <Josh.Chica@ibm.com>
2023-02-09 09:28:01 -0500

    Updated Ody IO BIST Cleanup
    Change-Id: I65bf40920cd41270a2fbb2958d4cc9ce5e5acfc6
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/145538
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Josh Chica <josh.chica@ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/146257
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/ocmb/odyssey/procedures/hwp/io/ody_omi_hss_bist_cleanup.C
+++ src/import/chips/ocmb/odyssey/procedures/hwp/io/ody_omi_hss_bist_cleanup.C
@@ -35,13 +35,40 @@
 // Includes
 //------------------------------------------------------------------------------
 #include <ody_omi_hss_bist_cleanup.H>
+#include <ody_io_ppe_common.H>
 
 //------------------------------------------------------------------------------
 // Function definitions
 //------------------------------------------------------------------------------
 fapi2::ReturnCode ody_omi_hss_bist_cleanup(const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target)
 {
-    FAPI_DBG("Start");
+    FAPI_DBG("Start - BIST Cleanup");
+
+    io_ppe_regs<fapi2::TARGET_TYPE_OCMB_CHIP> l_ppe_regs(PHY_PPE_WRAP0_ARB_CSAR,
+            PHY_PPE_WRAP0_ARB_CSDR,
+            PHY_PPE_WRAP0_XIXCR);
+
+    ody_io::io_ppe_common<fapi2::TARGET_TYPE_OCMB_CHIP> l_ppe_common(&l_ppe_regs);
+
+    uint32_t l_rx_lanes = 0;
+    uint32_t l_tx_lanes = 0;
+    const fapi2::buffer<uint64_t> l_num_threads = 1;
+    fapi2::buffer<uint64_t> l_done = 0;
+    fapi2::buffer<uint64_t> l_fail = 0;
+
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_OMI_RX_LANES, i_target, l_rx_lanes));
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_OMI_TX_LANES, i_target, l_tx_lanes));
+
+    FAPI_TRY(l_ppe_common.bist_cleanup(i_target, l_num_threads, l_rx_lanes, l_tx_lanes, l_done, l_fail),
+             "Failed to run common HSS BIST cleanup");
+
+    if (!l_done || l_fail)
+    {
+        FAPI_ASSERT(l_done,
+                    fapi2::IO_PPE_DONE_POLL_FAILED()
+                    .set_TARGET(i_target),
+                    "IO PPE done poll time-out or ext_cmd_fail seen!");
+    }
 
 fapi_try_exit:
     FAPI_DBG("End");
