5bd54d126ccc8b3904a1867091238f44e21ceeaa
hostboot <hostboot@us.ibm.com>
2023-04-12 16:43:01 -0500

    Reapply libpldm subtree changes made from a80d98b to 4c3a5f3
    Change-Id: I293f9076c7ea67488c054dec4a61d7084662c2aa
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/149771
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Nick Bofferding <bofferdn@us.ibm.com>

--- src/subtree/openbmc/pldm/libpldm/src/bios.c
+++ src/subtree/openbmc/pldm/libpldm/src/bios.c
@@ -242,13 +242,17 @@ int encode_get_bios_table_resp(uint8_t instance_id, uint8_t completion_code,
 
 int encode_get_bios_table_req(uint8_t instance_id, uint32_t transfer_handle,
 			      uint8_t transfer_op_flag, uint8_t table_type,
-			      struct pldm_msg *msg)
+			      struct pldm_msg *msg, size_t payload_length)
 {
 	if (msg == NULL) {
 		return PLDM_ERROR_INVALID_DATA;
 	}
 
-	struct pldm_header_info header = {0};
+        if(payload_length != PLDM_GET_BIOS_TABLE_REQ_BYTES) {
+            return PLDM_ERROR_INVALID_LENGTH;
+        }
+
+        struct pldm_header_info header = {0};
 	header.msg_type = PLDM_REQUEST;
 	header.instance = instance_id;
 	header.pldm_type = PLDM_BIOS;
@@ -326,13 +330,18 @@ int decode_get_bios_table_resp(const struct pldm_msg *msg,
 
 int encode_get_bios_attribute_current_value_by_handle_req(
     uint8_t instance_id, uint32_t transfer_handle, uint8_t transfer_op_flag,
-    uint16_t attribute_handle, struct pldm_msg *msg)
+    uint16_t attribute_handle, struct pldm_msg *msg, size_t payload_length)
 {
 	if (msg == NULL) {
 		return PLDM_ERROR_INVALID_DATA;
 	}
 
-	struct pldm_header_info header = {0};
+	if(payload_length != PLDM_GET_BIOS_ATTR_CURR_VAL_BY_HANDLE_REQ_BYTES) {
+            return PLDM_ERROR_INVALID_LENGTH;
+        }
+
+        struct pldm_header_info header = {0};
+
 	header.msg_type = PLDM_REQUEST;
 	header.instance = instance_id;
 	header.pldm_type = PLDM_BIOS;
