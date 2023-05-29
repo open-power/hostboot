3dd9623ca405cfe2dcd3d9edf800f87d00e4dc9a
hostboot <hostboot@us.ibm.com>
2023-05-28 23:56:26 -0500

    Update to latest openbmc/libmctp commit 4df106d
    Change-Id: I2ed22ce440a72de08df5d2a6c4bc05d63b38020f
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/152529
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Nick Bofferding <bofferdn@us.ibm.com>

--- src/subtree/openbmc/libmctp/astlpc.c
+++ src/subtree/openbmc/libmctp/astlpc.c
@@ -41,10 +41,6 @@ static const char *lpc_path = "/dev/aspeed-lpc-ctrl";
 
 #endif
 
-// Workaround for libmctp since it assumes it picks up
-// the definition of the abstraction to mark a function unused
-#define __unused __attribute__((__unused__))
-
 enum mctp_astlpc_cmd {
 	cmd_initialise = 0x00,
 	cmd_tx_begin = 0x01,
@@ -1489,7 +1485,7 @@ struct mctp_binding_astlpc *mctp_astlpc_init_fileio(void)
 #else
 struct mctp_binding_astlpc *mctp_astlpc_init_fileio(void)
 {
-	// Missing support for file IO
+	mctp_prlog(MCTP_LOG_ERR, "%s: Missing support for file IO", __func__);
 	return NULL;
 }
 
