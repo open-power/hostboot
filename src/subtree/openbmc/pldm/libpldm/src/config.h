07c4adb47c7ce712241af3ad327408d74844b2fc
hostboot <hostboot@us.ibm.com>
2023-06-05 08:32:20 -0500

    Reapply libpldm subtree changes made from 74899a8 to 36021eb
    Change-Id: Ifdcdc225e9dc3f6b9f3ead9b33d4ebb1bab0e8a2
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/153353
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Nick Bofferding <bofferdn@us.ibm.com>

---
+++ src/subtree/openbmc/pldm/libpldm/src/config.h
@@ -0,0 +1,16 @@
+
+/**
+ *  @file Controls which functions are allowed to compile within any file that
+ *     resides in the same directory as this file.  Currently, Hostboot only
+ *     uses stable APIs.  APIs that are being tested or deprecated are not
+ *     allowed.  This file should not be confused with Hostboot's generated
+ *     config.h file.
+ */
+
+#ifndef LIBPLDM_CONFIG_H
+
+#define LIBPLDM_ABI_STABLE
+#define LIBPLDM_ABI_TESTING __attribute__((error("Function is not stable")))
+#define LIBPLDM_ABI_DEPRECATED __attribute__((error("Function is deprecated")))
+
+#endif // LIBPLDM_CONFIG_H
