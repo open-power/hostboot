cc3aa28be7f05bcb68c68110c4c502c4c1b39dfa
Ilya Smirnov <ismirno@us.ibm.com>
2023-07-13 15:20:36 -0500

    Process Attr Mismatch Errors from SPPE
    This commit introduces changes to process the attribute error
    array returned to us from SPPE when we perform attribute sync
    (both "push" and "pull"). If any errors are returned, an informational
    error log is created and committed, containing all of the attribute
    errors communicated by SPPE in its FFDC fields. It was agreed that
    this log be informational and not fail the boot in any scenarios.
    
    Change-Id: Id6d208780bffac582466cac1eb477e29aa5828f1
    JIRA: PFHB-439
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/155037
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/usr/sbeio/sbe_attributeAccess.C
+++ src/usr/sbeio/sbe_attributeAccess.C
@@ -32,6 +32,7 @@
 #include <plat_hwp_invoker.H>
 #include <trace/interface.H>
 #include <errl/errlmanager.H>
+#include <errl/hberrltypes.H>
 #include <sbeio/sbeioif.H>
 #include <sbeio/sbe_utils.H>
 #include "sbe_fifodd.H"
@@ -49,47 +50,68 @@ TRACDCOMP(g_trac_sbeio,"AttributeAccess: " printf_string,##args)
 #define SBE_TRACF(printf_string,args...) \
 TRACFCOMP(g_trac_sbeio,"AttributeAccess: " printf_string,##args)
 
+constexpr bool PUSH = 1;
+constexpr bool PULL = 0;
+
+using namespace errl_util;
 
 namespace SBEIO
 {
-
-    // @TODO JIRA PFHB-258 These chipops are reliant on an SBE provided interface to generate, add, and interpret
-    //                     attribute data to/from the SBE.
-
     /**
-    * @brief @TODO JIRA PFHB-258
-    *
-    * @param[in] i_chipTarget The chip you would like to perform the chipop on
-    *                       NOTE: HB should only be sending this to non-boot procs or Odyssey chips
-    *
-    * @return errlHndl_t Error log handle on failure.
-    *
-    */
-    errlHndl_t sendAttrDumpRequest(TARGETING::Target * i_chipTarget)
+     * @brief This function creates an informational error log and populates it
+     *        with all of the mismatched attributes, as dictated by the input
+     *        attribute error vector. Only one informational log is created;
+     *        all of the mismatched attributes are added into the same log as
+     *        FFDC fields. This function is a no-op if the vector is empty.
+     * @note  The created error log gets committed within the function.
+     *
+     * @param[in] i_errors The vector of detected attribute mismatches.
+     * @param[in] i_ocmb The Odyssey OCMB target.
+     * @param[in] i_push whether an attribute push (1) or pull (0) was performed.
+     */
+    void processSbeAttrErrorsArray(const std::vector<sbeutil::AttrError_t>& i_errors,
+                                   const TARGETING::Target* i_ocmb,
+                                   bool i_push)
     {
-        errlHndl_t errl = nullptr;
-
-        do
+        if(i_errors.size() != 0)
         {
-            // Make sure the target is one of the supported types.
-            errl = sbeioInterfaceChecks(i_chipTarget,
-                                        SbeFifo::SBE_FIFO_CLASS_ATTRIBUTE_MESSAGES,
-                                        SbeFifo::SBE_FIFO_CMD_ATTRIBUTE_DUMP);
-            if(errl)
+            /*@
+             * @errortype
+             * @moduleid         SBEIO_PROCESS_ATTR_ARRAY
+             * @reasoncode       SBEIO_ATTR_MISMATCH_DETECTED
+             * @userdata1        The HUID of the Odyssey chip
+             * @userdata2[0:31]  The size of the attribute error array
+             * @userdata2[32:63] Whether the operation was an attribute push (1) or pull (0)
+             * @devdesc          SPPE responded with an array of mismatches to Host's attribute
+             *                   sync request. See the FFDC field for mismatches.
+             * @custdesc         Informational event.
+             */
+            errlHndl_t l_errl = new ERRORLOG::ErrlEntry(ERRORLOG::ERRL_SEV_INFORMATIONAL,
+                                                        SBEIO_PROCESS_ATTR_ARRAY,
+                                                        SBEIO_ATTR_MISMATCH_DETECTED,
+                                                        TARGETING::get_huid(i_ocmb),
+                                                        SrcUserData(bits{0,31}, i_errors.size(),
+                                                                    bits{32,63}, i_push),
+                                                        ERRORLOG::ErrlEntry::ADD_SW_CALLOUT);
+            for(const auto l_error : i_errors)
             {
-                break;
+                char buff[256] = {};
+                snprintf(buff, sizeof(buff),
+                         "FAPI Target Type: 0x%lx; ATTR_CHIP_UNIT_POS: 0x%x; Attr ID 0x%x; SBE RC: 0x%x",
+                         l_error.iv_targetType, l_error.iv_instance, l_error.iv_attrId, l_error.iv_rc);
+                SBE_TRACF(WARN_MRK"Attribute sync error detected: %s", buff);
+                ERRORLOG::ErrlUserDetailsString(buff).addToLog(l_errl);
             }
-            SBE_TRACF(EXIT_MRK "Skipping unimplemented chipop sendAttrDumpRequest");
-
-
-        }while(0);
-
-        SBE_TRACD(EXIT_MRK "sendAttrDumpRequest");
-        return errl;
-    };
+            l_errl->collectTrace(SBEIO_COMP_NAME);
+            l_errl->collectTrace(FAPI2_COMP_NAME);
+            errlCommit(l_errl, SBEIO_COMP_ID);
+        }
+    }
 
     /**
-    * @brief @TODO JIRA PFHB-258
+    * @brief This function sends a chip-op to SPPE to fetch a binary blob of the
+    *        attributes common between SPPE and Hostboot and applies the results
+    *        to the attributes on Hostboot side.
     *
     * @param[in] i_chipTarget The chip you would like to perform the chipop on
     *                       NOTE: HB should only be sending this to non-boot procs or Odyssey chips
@@ -134,7 +156,7 @@ namespace SBEIO
                             l_response->AttrListResponse,
                             sizeof(l_response->AttrListResponse),
                             l_sbeErrors);
-            // TODO JIRA: PFHB-439 Process the returned l_attrErrors vector
+            processSbeAttrErrorsArray(l_sbeErrors, i_chipTarget, PULL);
             if(errl)
             {
                 SBE_TRACF(ERR_MRK"sendAttrListRequest: could not parse attr blob from SPPE");
@@ -148,10 +170,11 @@ namespace SBEIO
 
         SBE_TRACD(EXIT_MRK "sendAttrListRequest");
         return errl;
-    };
+    }
 
     /**
-    * @brief @TODO JIRA PFHB-258
+    * @brief This function generates a binary blob of attributes common between SPPE
+    *        and Hostboot and sends the blob to SPPE via a chip-op.
     *
     * @param[in] i_chipTarget The chip you would like to perform the chipop on
     *                       NOTE: HB should only be sending this to non-boot procs or Odyssey chips
@@ -208,7 +231,7 @@ namespace SBEIO
                             l_response->AttrUpdateResponse,
                             sizeof(l_response->AttrUpdateResponse),
                             l_attrErrors);
-            // TODO JIRA: PFHB-439 Process the returned l_attrErrors vector
+            processSbeAttrErrorsArray(l_attrErrors, i_chipTarget, PUSH);
             if(errl)
             {
                 SBE_TRACF(ERR_MRK"sendAttrUpdateRequest: error returned from ody_analyze_sbe_attr_response");
@@ -221,6 +244,6 @@ namespace SBEIO
 
         SBE_TRACD(EXIT_MRK "sendAttrUpdateRequest");
         return errl;
-    };
+    }
 } //end namespace SBEIO
 
