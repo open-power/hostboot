1ebbc6d2ee67551ff79d380b10a7b5582dca6465
Matthew Raybuck <matthew.raybuck@ibm.com>
2023-08-02 14:23:53 -0500

    Fix error leak in HDAT when failing to get NVRAM section
    While HDAT is attempting to run hdatFillDataPtrs() there is an attempt
    to get section info for the NVRAM section. In an error case, the log
    which is returned from that attempt is not properly handled leading to a
    leak when the handle is reused later in the function and overwritten.
    
    Change-Id: Ie99a3549d671bf590f1167c9e04a936c8f4dbc23
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/155718
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Deborah L McLemore <debmc@us.ibm.com>
    Reviewed-by: Ilya Smirnov <ismirno@us.ibm.com>

--- src/usr/hdat/hdatspsubsys.C
+++ src/usr/hdat/hdatspsubsys.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2016,2022                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2023                        */
 /* [+] International Business Machines Corp.                              */
 /* [+] Super Micro Computer, Inc.                                         */
 /*                                                                        */
@@ -483,6 +483,8 @@ errlHndl_t HdatSpSubsys::hdatFillDataPtrs()
         {
             HDAT_ERR("PNOR::getSectionInfo returns error. Fill the size as 0");
             iv_mem.hdatHostRamSize = 0;
+            delete l_errlHndl;
+            l_errlHndl = nullptr;
         }
         else
         {
