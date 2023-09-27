44aea21d51f7a686c6c1fc79c57de0452c1fd85e
Matthew Raybuck <matthew.raybuck@ibm.com>
2023-09-27 11:55:36 -0500

    DCE: Update make rule dependencies
    DCE used to use the hbicore.list.bz2 and hbicore_test.list.bz2 files as
    dependencies for their respective rules. These were used as stand-ins for
    the real dependecies but had the unfortunate side-effect of killing make
    if they were missing even if the real dependencies existed.
    
    This commit adds the actual dependencies to their respective make rules
    and will additionally cause the make to error out in a more intuitive
    way than before.
    
    If the dce file was named foo.C then originally with the missing dependency
    you'd get:
    
    "make: *** No rule to make target `foo.dce.test.lid'.  Stop."
    
    Now, you'll get some form of the following depending on which files
    don't exist:
    
    src/build/mkrules/dce.mk:46: *** "Missing the following file(s):
       /esw/san5/mraybuck/repos/p10/hostboot/img/hbicore_test.bin".  Stop.
    
    Additionally, this commit demangles the names in the non-test version of
    the DCE listing file.
    
    Change-Id: I8d203cec7ce02dd78c1d8ae42545a2b11015c771
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/157993
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Zachary Clark <zach@ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/build/mkrules/dce.mk
+++ src/build/mkrules/dce.mk
@@ -34,15 +34,33 @@ DCE_LISTING_FILE := %.dce.lid.debug
 DCE_TEST_LISTING_FILE := %.dce.test.lid.debug
 endif
 
+# Rules for testing if DCE has all required dependencies to make a particular target
+
+IMG_DIR = $(PROJECT_ROOT)/img
+
+HB_DEPENDENCIES :=  $(IMG_DIR)/hbicore.bin $(IMG_DIR)/hbicore_extended.bin $(IMG_DIR)/hbicore.syms.mangled
+HB_TEST_DEPENDENCIES :=  $(IMG_DIR)/hbicore_test.bin $(IMG_DIR)/hbicore_test_extended.bin $(IMG_DIR)/hbicore_test.syms.mangled
+
+HB_MISSING_DEPENDENCIES := $(shell $(foreach f, $(HB_DEPENDENCIES), test -e "$f" || echo "$f";))
+HB_MISSING_TEST_DEPENDENCIES := $(shell $(foreach f, $(HB_TEST_DEPENDENCIES), test -e "$f" || echo "$f";))
+
+# If either of the follow two rules are called then there are missing dependencies. These will print a clearer error
+# message beyond the typical "No rule to make target"
+$(HB_DEPENDENCIES):
+	$(error "Missing the following file(s): $(HB_MISSING_DEPENDENCIES)")
+$(HB_TEST_DEPENDENCIES):
+	$(error "Missing the following file(s): $(HB_MISSING_TEST_DEPENDENCIES)")
+
+
 # Rules for creating the DCE lid and the debug listing file.
 
 .PRECIOUS: %.dce.lid.debug
-%.dce.lid.debug: %.C $(filter %.C %.H, $(DCE_EXTRA_FILES)) $(PROJECT_ROOT)/img/hbicore.list.bz2
+%.dce.lid.debug: %.C $(filter %.C %.H, $(DCE_EXTRA_FILES)) $(HB_DEPENDENCIES)
 	CXXFLAGS="$(filter-out -D__HOSTBOOT_MODULE=% -Werror, $(CXXFLAGS)) $(CXXFLAGS_DCE) -gz" $(ROOTPATH)/src/build/tools/dce/dce-compile "$<" $(filter %.C, $(DCE_EXTRA_FILES)) -o $@ $(INCFLAGS) >/dev/null 2>&1
-	$(OBJDUMP) --source -d $@ > $@.list
+	$(OBJDUMP) --source -d -C $@ > $@.list
 
 .PRECIOUS: %.dce.lid.intermediate
-%.dce.lid.intermediate: %.C $(filter %.C %.H, $(DCE_EXTRA_FILES)) $(PROJECT_ROOT)/img/hbicore.list.bz2
+%.dce.lid.intermediate: %.C $(filter %.C %.H, $(DCE_EXTRA_FILES)) $(HB_DEPENDENCIES)
 	CXXFLAGS="$(filter-out -D__HOSTBOOT_MODULE=% -Werror, $(CXXFLAGS)) $(CXXFLAGS_DCE) -s" $(ROOTPATH)/src/build/tools/dce/dce-compile "$<" $(filter %.C, $(DCE_EXTRA_FILES)) -o $@ $(INCFLAGS)
 	$(ROOTPATH)/src/build/tools/dce/preplib.py $@
 
@@ -60,12 +78,12 @@ endif
 # Test image versions of the above rules, for standalone SIMICS.
 
 .PRECIOUS: %.dce.test.lid.debug
-%.dce.test.lid.debug: %.C $(filter %.C %.H, $(DCE_EXTRA_FILES)) $(PROJECT_ROOT)/img/hbicore_test.list.bz2
+%.dce.test.lid.debug: %.C $(filter %.C %.H, $(DCE_EXTRA_FILES)) $(HB_TEST_DEPENDENCIES)
 	CXXFLAGS="$(filter-out -D__HOSTBOOT_MODULE=% -Werror, $(CXXFLAGS)) $(CXXFLAGS_DCE) -gz" $(ROOTPATH)/src/build/tools/dce/dce-compile "$<" $(filter %.C, $(DCE_EXTRA_FILES)) -o $@ $(INCFLAGS) >/dev/null 2>&1
 	$(OBJDUMP) --source -d -C $@ > $@.list
 
 .PRECIOUS: %.dce.test.lid.intermediate
-%.dce.test.lid.intermediate: %.C $(filter %.C %.H, $(DCE_EXTRA_FILES)) $(PROJECT_ROOT)/img/hbicore_test.list.bz2
+%.dce.test.lid.intermediate: %.C $(filter %.C %.H, $(DCE_EXTRA_FILES)) $(HB_TEST_DEPENDENCIES)
 	CXXFLAGS="$(filter-out -D__HOSTBOOT_MODULE=% -Werror, $(CXXFLAGS)) $(CXXFLAGS_DCE) -s" $(ROOTPATH)/src/build/tools/dce/dce-compile "$<" $(filter %.C, $(DCE_EXTRA_FILES)) -o $@ $(INCFLAGS)
 	HB_DCE_TEST_IMAGE=1 $(ROOTPATH)/src/build/tools/dce/preplib.py $@
 
