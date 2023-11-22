278b539dc5050300b6094f800d48747db34ed27d
David J Chung <DJ.chung@ibm.com>
2023-11-22 12:50:59 -0500

    Adds HWP function for ody_sppe_draminit for a single MEM_PORT target
    ZEN:MST-2446
    Change-Id: I4d34399c674c1b78dc95240f0cd5b6bd60c80f6f
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/ekb/+/160751
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Dev-Ready: David-Jude Chung <dj.chung@ibm.com>
    Reviewed-by: Edgar Cordero <ecordero@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/160752
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/ocmb/odyssey/procedures/hwp/memory/ody_sppe_draminit.C
+++ src/import/chips/ocmb/odyssey/procedures/hwp/memory/ody_sppe_draminit.C
@@ -35,9 +35,11 @@
 
 #include <fapi2.H>
 #include <ody_sppe_draminit.H>
+
 #include <lib/phy/ody_draminit_procedure.H>
 #include <lib/phy/ody_draminit_utils.H>
 #include <generic/memory/mss_git_data_helper.H>
+#include <generic/memory/lib/utils/find.H>
 extern "C"
 {
 ///
@@ -59,5 +61,23 @@ extern "C"
         return fapi2::current_err;
 
     }
+    ///
+    /// @brief Runs the dram initialization on the SPPE on a single port
+    /// @param[in] i_target the controller
+    /// @param[out] o_log_data hwp_data_ostream of streaming log
+    /// @return FAPI2_RC_SUCCESS iff ok
+    ///
+    fapi2::ReturnCode ody_sppe_draminit_single_port(const fapi2::Target<fapi2::TARGET_TYPE_MEM_PORT>& i_port,
+            fapi2::hwp_data_ostream& o_log_data)
+    {
+        mss::display_git_commit_info("ody_sppe_draminit");
+
+        FAPI_TRY(mss::ody::draminit(i_port, o_log_data));
+
+    fapi_try_exit:
+        return fapi2::current_err;
+
+    }
+
 
 }// extern C
