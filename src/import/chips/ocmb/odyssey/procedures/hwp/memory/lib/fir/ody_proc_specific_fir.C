3b05c6472182f9cf8f518160bed79f0348f5c038
Stephen Glancy <sglancy@us.ibm.com>
2024-03-13 12:27:59 -0500

    Moves Odyssey processor specific FIR code to a separate file
    Change-Id: I1e810e77e828ed8e5f4fa414f7d11ce1644acfa2
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/ekb/+/167412
    Reviewed-by: Edgar Cordero <ecordero@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Josh Chica <josh.chica@ibm.com>
    Reviewed-by: Amita Banchhor <amita.banchhor@ibm.com>
    Dev-Ready: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/167436
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>

---
+++ src/import/chips/ocmb/odyssey/procedures/hwp/memory/lib/fir/ody_proc_specific_fir.C
@@ -0,0 +1,225 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: src/import/chips/ocmb/odyssey/procedures/hwp/memory/lib/fir/ody_proc_specific_fir.C $ */
+/*                                                                        */
+/* OpenPOWER HostBoot Project                                             */
+/*                                                                        */
+/* Contributors Listed Below - COPYRIGHT 2024                             */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+// EKB-Mirror-To: hostboot
+///
+/// @file ody_proc_specific_fir.C
+/// @brief Memory subsystem FIR support - for this specific processor only. Not to be mirrored
+///
+// *HWP HWP Owner: Josh Chica <Josh.Chica@ibm.com>
+// *HWP HWP Backup: Stephen Glancy <sglancy@us.ibm.com>
+// *HWP Team: Memory
+// *HWP Level: 2
+// *HWP Consumed by: FSP:HB
+
+#include <fapi2.H>
+#include <generic/memory/lib/utils/find.H>
+#include <generic/memory/lib/utils/fir/gen_mss_fir.H>
+#include <generic/memory/lib/utils/shared/mss_generic_consts.H>
+#include <generic/memory/lib/utils/pos.H>
+#include <lib/fir/ody_fir.H>
+#include <ody_scom_omi.H>
+#include <ody_scom_perv.H>
+#include <p10_scom_omic.H>
+#include <p10_scom_mcc.H>
+
+namespace mss
+{
+namespace check
+{
+
+///
+/// @brief Checks whether any FIRs have lit up on a target
+/// @param[in] i_target - the OCMB_CHIP target on which to operate
+/// @param[in,out] io_rc - the return code for the function
+/// @param[out] o_fir_error - true iff a FIR was hit
+/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS iff ok
+/// @note specialization for ODYSSEY and fir checklist for IO regs
+///
+template <>
+fapi2::ReturnCode bad_fir_bits<mss::mc_type::ODYSSEY, firChecklist::IO_GENERAL>(
+    const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target,
+    fapi2::ReturnCode& io_rc,
+    bool& o_fir_error)
+{
+    /*
+        Check:
+            TCP.LFIR: 3,18
+            TCMC.LFIR: 3
+            IOPPE.FIR: 1,5,13,14,15,16,17,18,20,23,26,27
+    */
+    fapi2::buffer<uint64_t> l_check_mask(0xFFFFFFFFFFFFFFFF);
+    // Start by assuming we do not have a FIR; if true at any point, skip other checks to preserve error
+    o_fir_error = false;
+
+    // Check TCP.LFIR bits [3,18]
+    if(o_fir_error != true)
+    {
+        l_check_mask.flush<1>()
+        .clearBit<scomt::perv::TPCHIP_TPC_LFIR_CC_OTHERS>()
+        .clearBit<scomt::perv::TPCHIP_TPC_LFIR_IN18>();
+        FAPI_TRY(bad_fir_bits_helper_with_mask(i_target,
+                                               scomt::perv::TPCHIP_TPC_LFIR_RW_WCLEAR,
+                                               scomt::perv::TPCHIP_TPC_EPS_MASK_RW_WCLEAR,
+                                               l_check_mask,
+                                               io_rc,
+                                               o_fir_error));
+    }
+
+    // Check TCMC.LFIR: bits [3]
+    if (o_fir_error != true)
+    {
+        l_check_mask.flush<1>()
+        .clearBit<scomt::perv::TCMC_LFIR_CC_OTHERS>();
+        FAPI_TRY(bad_fir_bits_helper_with_mask(i_target,
+                                               scomt::perv::TCMC_LFIR_RW_WCLEAR,
+                                               scomt::perv::TCMC_EPS_MASK_RW_WCLEAR,
+                                               l_check_mask,
+                                               io_rc,
+                                               o_fir_error));
+    }
+
+    // Check IOPPE.FIR: bits [1,5,13,14,15,16,17,18,20,23,26,27]
+    if (o_fir_error != true)
+    {
+        l_check_mask.flush<1>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_IO0_RX_INVALID_STATE_OR_PARITY_ERROR>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_IO0_TX_INVALID_STATE_OR_PARITY_ERROR>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_PPE_INT_HWERROR>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_PPE_EXT_HWERROR>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_PPE_HALT_WATCHDOG_OR_INTERRUPT>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_PPE_HALT_DEBUG>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_PPE_HALTED>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_PPE_WATCHDOG_TIMEOUT>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_PPE_ARB_ARRAY_UNCORRECTABLE_ERROR>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_PPE_CODE_FATAL_ERROR>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_PPE_CODE_RECAL_NOT_RUN>()
+        .clearBit<scomt::omi::PHY_SCOM_MAC0_LFIR_REG_PPE_CODE_THREAD_LOCKED>();
+        FAPI_TRY(bad_fir_bits_helper_with_mask(i_target,
+                                               scomt::omi::PHY_SCOM_MAC0_LFIR_REG_RW_WCLEAR,
+                                               scomt::omi::PHY_SCOM_MAC0_LFIR_MASK_REG_RW_WCLEAR,
+                                               l_check_mask,
+                                               io_rc,
+                                               o_fir_error));
+    }
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+///
+/// @brief Checks whether any FIRs have lit up on a target
+/// @param[in] i_target - the OCMB_CHIP target on which to operate
+/// @param[in,out] io_rc - the return code for the function
+/// @param[out] o_fir_error - true iff a FIR was hit
+/// @return fapi2::ReturnCode FAPI2_RC_SUCCESS iff ok
+/// @note specialization for ODYSSEY and fir checklist for IO_TRAIN regs
+///
+template <>
+fapi2::ReturnCode bad_fir_bits<mss::mc_type::ODYSSEY, firChecklist::IO_TRAIN>(
+    const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>& i_target,
+    fapi2::ReturnCode& io_rc,
+    bool& o_fir_error)
+{
+    /*
+        Check:
+            MC.OMI.FIR.REG: 1,2,6,8,10,11,21,22
+    */
+    //const auto c_omic_target = i_target.getParent<fapi2::TARGET_TYPE_OMI>().getParent<faip2::TARGET_TYPE_OMIC>();
+    //const auto c_mcc_target = i_target.getParent<fapi2::TARGET_TYPE_OMI>().getParent<faip2::TARGET_TYPE_MCC>();
+    const auto c_omic_target = i_target.getParent<fapi2::TARGET_TYPE_OMI>().getParent<fapi2::TARGET_TYPE_OMIC>();
+    const auto c_mcc_target = i_target.getParent<fapi2::TARGET_TYPE_OMI>().getParent<fapi2::TARGET_TYPE_MCC>();
+    //const auto c_omic_target = i_target.getParent<faip2::TARGET_TYPE_OMIC>();
+    //const auto c_mcc_target = i_target.getParent<faip2::TARGET_TYPE_MCC>();
+    fapi2::buffer<uint64_t> l_check_mask(0xFFFFFFFFFFFFFFFF);
+    // Start by assuming we do not have a FIR; if true at any point, skip other checks to preserve error
+    o_fir_error = false;
+
+    // Check MC.OMI.FIR.REG: bits [1,2,6,8,10,11,21,22]
+    if (o_fir_error != true)
+    {
+        l_check_mask.flush<1>()
+        .clearBit<scomt::omi::D_REG_MC_OMI_FIR_DL0_FATAL_ERROR>()
+        .clearBit<scomt::omi::D_REG_MC_OMI_FIR_DL0_DATA_UE>()
+        .clearBit<scomt::omi::D_REG_MC_OMI_FIR_DL0_X4_MODE>()
+        .clearBit<scomt::omi::D_REG_MC_OMI_FIR_DL0_TIMEOUT>()
+        .clearBit<scomt::omi::D_REG_MC_OMI_FIR_DL0_ERROR_RETRAIN>()
+        .clearBit<scomt::omi::D_REG_MC_OMI_FIR_DL0_EDPL_RETRAIN>()
+        .clearBit<scomt::omi::D_REG_MC_OMI_FIR_DL0_SKITTER_ERROR>()
+        .clearBit<scomt::omi::D_REG_MC_OMI_FIR_DL0_SKITTER_DRIFT>();
+        FAPI_TRY(bad_fir_bits_helper_with_mask(i_target,
+                                               scomt::omi::D_REG_MC_OMI_FIR_RW_WCLEAR,
+                                               scomt::omi::D_REG_MC_OMI_FIR_MASK_RW_WCLEAR,
+                                               l_check_mask,
+                                               io_rc,
+                                               o_fir_error));
+    }
+
+    // P10 FIR checks
+    if (o_fir_error != true)
+    {
+        // Check MC.OMI.FIR.REG: bits [0,1,5,7,10]
+        l_check_mask.flush<1>()
+        .clearBit<scomt::omic::MC_OMI_FIR_REG_DL0_FATAL_ERROR>()
+        .clearBit<scomt::omic::MC_OMI_FIR_REG_DL0_DATA_UE>()
+        .clearBit<scomt::omic::MC_OMI_FIR_REG_DL0_X4_MODE>()
+        .clearBit<scomt::omic::MC_OMI_FIR_REG_DL0_TIMEOUT>()
+        .clearBit<scomt::omic::MC_OMI_FIR_REG_DL0_EDPL_RETRAIN>();
+        FAPI_TRY(bad_fir_bits_helper_with_mask(c_omic_target,
+                                               scomt::omic::MC_OMI_FIR_REG_WO_AND,
+                                               scomt::omic::MC_OMI_FIR_MASK_REG_WO_AND,
+                                               l_check_mask,
+                                               io_rc,
+                                               o_fir_error));
+
+        // Check DSTLFIR.REG: bits [8,9,10,11,12,13,16,17,22,23,35,36]
+        if (o_fir_error != true)
+        {
+            l_check_mask.flush<1>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_MCS_DSTL_CMD_PARITY_ERROR>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_RESET_CREDITS_RD_WDF_BUFFER_NONZERO>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_CONFIG_REG_RECOVERABLE_PARITY_ERROR>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_CONFIG_REG_FATAL_PARITY_ERROR>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_A_COUNTER_ERROR>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_B_COUNTER_ERROR>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_A_BUFFER_OVERUSE_ERROR>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_B_BUFFER_OVERUSE_ERROR>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_A_CHANNEL_TIMEOUT>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_B_CHANNEL_TIMEOUT>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_A_PARITY_ERROR>()
+            .clearBit<scomt::mcc::DSTL_DSTLFIR_SUBCHANNEL_B_PARITY_ERROR>();
+            FAPI_TRY(bad_fir_bits_helper_with_mask(c_mcc_target,
+                                                   scomt::mcc::DSTL_DSTLFIR_WO_AND,
+                                                   scomt::mcc::DSTL_DSTLFIRMASK_WO_AND,
+                                                   l_check_mask,
+                                                   io_rc,
+                                                   o_fir_error));
+        }
+    }
+
+fapi_try_exit:
+    return fapi2::current_err;
+}
+
+} // end check ns
+} // end mss ns
