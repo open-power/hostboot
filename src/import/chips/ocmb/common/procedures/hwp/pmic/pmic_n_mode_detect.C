e8cd9af0397ad82e2928866de91ac09610c2396b
akhis023 <akhis023@in.ibm.com>
2024-03-25 11:33:27 -0500

    Remove hw/ppe mirror tag from pmic_n_mode_detect.C
    -This HWP is no longer used by ppe
    
    EWM: 575885
    Change-Id: I601d91483941fffa907dc8aeb6080707c54257f1
    Signed-off-by: akhis023 <akhis023@in.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/ekb/+/171789
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/171779
    Reviewed-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/ocmb/common/procedures/hwp/pmic/pmic_n_mode_detect.C
+++ src/import/chips/ocmb/common/procedures/hwp/pmic/pmic_n_mode_detect.C
@@ -31,8 +31,7 @@
 // *HWP HWP Backup: Louis Stermole <stermole@us.ibm.com>
 // *HWP Team: Memory
 // *HWP Level: 3
-// *HWP Consumed by: SBE
-// EKB-Mirror-To: hw/ppe
+// *HWP Consumed by: HB
 
 #include <fapi2.H>
 #include <pmic_n_mode_detect.H>
