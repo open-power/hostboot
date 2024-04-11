c4f3ba80265eb93220240506f5297e73cdb4653b
Daniel Crowell <dcrowell@us.ibm.com>
2024-04-11 09:16:45 -0500

    Apply P10 OMI DDR4 EDPL changes concurrently
    Calling p10_omi_ddr4_edpl on all Explorer OCMBs during the
    concurrent code update.
    
    Change-Id: I31958a0b1f6b4863b6f2f41448903cf250b0f68c
    STGD: 609280
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/169864
    Reviewed-by: Mike Baiocchi <baiocchi@ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>

--- src/usr/hwplibs/runtime/concurrent_inits.C
+++ src/usr/hwplibs/runtime/concurrent_inits.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2022                             */
+/* Contributors Listed Below - COPYRIGHT 2022,2024                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -34,6 +34,7 @@
 #include <errl/errlmanager.H>
 #include <fapi2/plat_hwp_invoker.H>
 #include <runtime/interface.h>
+#include <p10_omi_ddr4_edpl.H>
 
 extern trace_desc_t* g_trac_hbrt;
 
@@ -41,10 +42,38 @@ void do_concurrent_inits( void )
 {
     TRACFCOMP(g_trac_hbrt,
               ENTER_MRK "do_concurrent_inits" );
+    errlHndl_t l_err = nullptr;
 
     //-----------------------------------------------------
     // Insert HWP calls here
 
+    //-----------------------------------------------------
+    // Apply P10 OMI DDR4 EDPL changes to active systems
+
+    TRACFCOMP(g_trac_hbrt,
+              "Executing p10_omi_ddr4_edpl on all OCMBs" );
+
+    TARGETING::TargetHandleList l_ocmbList;
+    TARGETING::getAllChips( l_ocmbList,
+                            TARGETING::TYPE_OCMB_CHIP,
+                            true );
+    for( const auto & l_ocmb : l_ocmbList )
+    {
+        const fapi2::Target<fapi2::TARGET_TYPE_OCMB_CHIP>
+          l_fapiOcmb( l_ocmb );
+        FAPI_INVOKE_HWP( l_err,
+                         p10_omi_ddr4_edpl,
+                         l_fapiOcmb,
+                         false );
+        if( l_err )
+        {
+            TRACFCOMP(g_trac_hbrt,
+                      "FAILURE: p10_omi_ddr4_edpl on OCMB %.8X",
+                      TARGETING::get_huid( l_ocmb ) );
+            //Commit the log and keep going
+            errlCommit(l_err, HWPF_COMP_ID);
+        }
+    }
     //-----------------------------------------------------
 
 
