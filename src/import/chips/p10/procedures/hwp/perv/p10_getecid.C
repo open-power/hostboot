ea6232ca58fc405419e90babdba944aae602da01
Joe McGill <jmcgill@us.ibm.com>
2024-04-14 20:20:57 -0400

    update getecid to support secondary decode
    remove zero padding on xy display
    
    Change-Id: I908d28fe910def1bfe2c4935eb04912446e33ca7
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/ekb/+/169596
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Dev-Ready: Steven B Janssen <janssens@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/169600
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/perv/p10_getecid.C
+++ src/import/chips/p10/procedures/hwp/perv/p10_getecid.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2016,2020                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2024                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -36,6 +36,7 @@
 #include "p10_scom_perv_1.H"
 #include "p10_scom_perv_7.H"
 #include "p10_scom_perv_b.H"
+#include "p10_scom_perv_d.H"
 
 
 fapi2::ReturnCode p10_getecid(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>& i_target_chip,
@@ -48,6 +49,7 @@ fapi2::ReturnCode p10_getecid(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
     fapi2::buffer<uint64_t> l_ecid_part0_data64 = 0;
     fapi2::buffer<uint64_t> l_ecid_part1_data64 = 0;
     fapi2::buffer<uint64_t> l_ecid_part2_data64 = 0;
+    fapi2::buffer<uint64_t> l_ecid_part3_data64 = 0;
     fapi2::variable_buffer l_fuseString(p10_getecid_fuseString_len);
     FAPI_INF("p10_getecid : Entering ...");
 
@@ -56,19 +58,20 @@ fapi2::ReturnCode p10_getecid(const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP>&
     FAPI_TRY(fapi2::getScom(i_target_chip, SINGLE_OTP_ROM_OTPROM_REG0, l_ecid_part0_data64));
     FAPI_TRY(fapi2::getScom(i_target_chip, SINGLE_OTP_ROM_OTPROM_REG1, l_ecid_part1_data64));
     FAPI_TRY(fapi2::getScom(i_target_chip, SINGLE_OTP_ROM_OTPROM_REG2, l_ecid_part2_data64));
+    FAPI_TRY(fapi2::getScom(i_target_chip, SINGLE_OTP_ROM_OTPROM_REG3, l_ecid_part3_data64));
 
     l_ecid_part0_data64.reverse();
     l_ecid_part1_data64.reverse();
     l_ecid_part2_data64.reverse();
+    l_ecid_part3_data64.reverse();
 
     attr_data[0] = l_ecid_part0_data64();
     attr_data[1] = l_ecid_part1_data64();
 
     FAPI_TRY(l_fuseString.insert(l_ecid_part0_data64(), 0, 64));
-
     FAPI_TRY(l_fuseString.insert(l_ecid_part1_data64(), 64, 64));
-
     FAPI_TRY(l_fuseString.insert(l_ecid_part2_data64(), 128, 64));
+    FAPI_TRY(l_fuseString.insert(l_ecid_part3_data64(), 192, 64));
 
     o_fuseString = l_fuseString;
 
