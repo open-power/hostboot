bd0c3c40d17c164d65574bf735ebe89a7ffa0b3a
Daniel Crowell <dcrowell@us.ibm.com>
2024-04-15 16:16:46 -0500

    Attempt side switch before code update for ody sbe boot error
    It is more efficient to attempt a side switch as the recovery
    for an initial boot failure versus jumping immediately to
    doing a code update.
    
    STGD: 608558
    Change-Id: I5387c6e6f9c64a2e52bb12bba8e5906864799165
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/169866
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins OP HW <op-hw-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Combined Simics CI <combined-simics-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Mike Baiocchi <baiocchi@ibm.com>
    Tested-by: Jenkins OP Build CI <op-jenkins+hostboot@us.ibm.com>

--- src/usr/isteps/ocmbupd/ody_upd_fsm.C
+++ src/usr/isteps/ocmbupd/ody_upd_fsm.C
@@ -255,13 +255,13 @@ state_transitions_t ody_fsm_transitions[] =
                                                                          { ANY_EVENT                   , {internal_error                                                }} } },  // only OCMB_BOOT_ERROR_NO_FFDC can happen when we don't know the fw version
 
     { { no            , no                     , SIDE0  , no          },{{ UPDATE_OMI_FIRMWARE_REACHED
-                                                                           | OCMB_BOOT_ERROR_WITH_FFDC
                                                                            | ATTRS_INCOMPATIBLE
                                                                            | OCMB_HWP_FAIL_OTHER // An OCMB HWP fail includes both FIFO timeouts (SBE crashes) and non-crash HWP fails. We always attempt a code update in either case, because in the former
                                                                                                  // case the timeout handler will catch the problem, and in the latter case the code update could fix the issue.
-                                                                           | OTHER_HW_HWP_FAIL
-                                                                           | OCMB_FLASH_ERROR          , {perform_code_update, switch_to_side_1, retry_check_for_ready  }},
-                                                                         { OCMB_HWP_FAIL_HASH_FAIL     , {switch_to_side_1, retry_check_for_ready                       }},
+                                                                           | OTHER_HW_HWP_FAIL         , {perform_code_update, switch_to_side_1, retry_check_for_ready  }},
+                                                                         { OCMB_BOOT_ERROR_WITH_FFDC
+                                                                           | OCMB_FLASH_ERROR          
+                                                                           | OCMB_HWP_FAIL_HASH_FAIL   , {switch_to_side_1, retry_check_for_ready                       }},
                                                                          { CODE_UPDATE_CHIPOP_FAILURE  , {switch_to_side_golden, retry_check_for_ready                  }} } },
 
     { { no            , no                     , SIDE0  , yes         },{{ OCMB_HWP_FAIL_HASH_FAIL     , {switch_to_side_golden,  retry_check_for_ready                 }},
