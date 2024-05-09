d705179df63d5566a98efa427853a10992f25c43
Josh-Chica <Josh.Chica@ibm.com>
2024-05-09 15:13:28 -0400

    Updated P11 OMI settings
    - Rx iref clock is now 4
    - Rx iref vset dac set to 2
    - Disabled Tx Boost En
    
    Change-Id: Id085fce90b74d70428cdd45e59f150a33a54d762
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/ekb/+/171731
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Chris Steffen <cwsteffen@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/171735
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>

--- src/import/chips/p10/procedures/hwp/memory/p10_omi_init.C
+++ src/import/chips/p10/procedures/hwp/memory/p10_omi_init.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2018,2023                        */
+/* Contributors Listed Below - COPYRIGHT 2018,2024                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -32,11 +32,44 @@
 
 #include <p10_omi_init.H>
 #include <p10_omi_init_scom.H>
+#include <p10_io_lib.H>
 #include <p10_scom_mcc_4.H>
+#include <p10_scom_omi_7.H>
 #include <p10_scom_mcc_d.H>
 #include <lib/fir/p10_fir.H>
 #include <mss_generic_attribute_getters.H>
 #include <generic/memory/lib/utils/find.H>
+
+
+/// @brief                      Builds address to access SCOMs
+/// @param i_base_addr          The chip's base address
+/// @param i_group              The group being accessed
+/// @param i_lane               The lane being accessed
+/// @param i_reg                The reg being accessed & tx direction. Specify register in hex then append 0 (Rx) or 1 (Tx)
+///                                         111111
+///                               0123456789012345
+///                               xxxRRRRRRRRRTxxx
+///
+///                             R : register address bits
+///                             T : TX bit
+///                             x : don't care
+/// @return                     Returns the address that's been built from the input parameters
+inline uint64_t buildAddr(const uint32_t i_base_addr,
+                          const uint8_t i_group,
+                          const uint8_t i_lane,
+                          const uint16_t i_reg)
+{
+    uint64_t r_addr = static_cast<uint64_t>(i_base_addr);
+    r_addr |= static_cast<uint64_t>(0x3F);
+    r_addr |= static_cast<uint64_t>(i_reg & 0x1FF8) << 39;  // 63-21-3=39
+    // 21 is the starting location for Tx, but need to shift back another 3 bits so the
+    //  direction bit of the nibble is in the correct position
+    r_addr |= static_cast<uint64_t>(i_lane & 0x1F) << 32;   // 63-31=32
+    r_addr |= static_cast<uint64_t>(i_group & 0x1F) << 37;   // 63-26=37
+    r_addr |= static_cast<uint64_t>(0x1) << 63;
+
+    return r_addr;
+}
 
 ///
 /// @brief Run initfile to enable templates and set pacing.
@@ -150,6 +183,93 @@ fapi_try_exit:
     return fapi2::current_err;
 }
 
+
+///
+/// @brief Enable P11 performance updates
+///
+/// @param[in] i_target                 p10 channel to work on
+///
+/// @return fapi2:ReturnCode. FAPI2_RC_SUCCESS if success, else error code.
+///
+fapi2::ReturnCode p10_omi_init_performance(const fapi2::Target<fapi2::TARGET_TYPE_MCC>& i_target)
+{
+    constexpr uint64_t c_base_addr = 0x10012c3full;
+
+    constexpr uint16_t c_reg_rx_ctl_mode12_pg = 0x10C0;
+    constexpr uint16_t c_reg_tx_ctl_mode3_pl = 0x0008;
+
+    constexpr uint8_t c_bit_iref_clk_dac = 0; // rx_ctl_mode12_pg
+    constexpr uint8_t c_len_iref_clk = 3;
+    constexpr uint8_t c_bit_iref_vset_dac = 6; // rx_ctl_mode12_pg
+    constexpr uint8_t c_len_iref_vset = 2;
+    constexpr uint8_t c_bit_tx_boost_en = 4; // tx_ctl_mode3_pl
+    constexpr uint8_t c_len_tx = 1;
+    constexpr uint8_t c_max_groups = 2;
+    constexpr uint8_t c_max_lanes = 8;
+    constexpr uint8_t c_indirect_scom_offset = 48;
+
+    auto l_omi_targets = i_target.getChildren<fapi2::TARGET_TYPE_OMI>();
+
+    fapi2::buffer<uint64_t> l_buffer;
+    uint64_t l_addr = 0;
+
+    uint32_t l_num_lanes = P10_IO_LIB_NUMBER_OF_OMI_LANES;
+
+    uint8_t l_group = 0;
+    uint8_t l_lane = 0;
+
+    for (l_group = 0; l_group < c_max_groups; l_group++)
+    {
+        l_addr = buildAddr(c_base_addr, l_group, 0, c_reg_rx_ctl_mode12_pg);
+        FAPI_DBG("RX_CTL_MODE12_PG Address 0x%08X%08X", (l_addr >> 32) & 0xFFFFFFFF, l_addr & 0xFFFFFFFF);
+        FAPI_TRY(putScom(i_target, l_addr, l_buffer),
+                 "Error putscom to address 0x%08X.", l_addr);
+
+        FAPI_DBG("Setting IREF CLOCK DAC to 4");
+        l_buffer.insertFromRight(4, c_bit_iref_clk_dac + c_indirect_scom_offset, c_len_iref_clk);
+
+        FAPI_DBG("Setting IREF VSET DAC to 2");
+        l_buffer.insertFromRight(2, c_bit_iref_vset_dac + c_indirect_scom_offset, c_len_iref_vset);
+
+        FAPI_TRY(putScom(i_target, l_addr, l_buffer),
+                 "Error putscom to address 0x%08X.", l_addr);
+
+        l_buffer.flush<0>();
+
+        for (l_lane = 0; l_lane < c_max_lanes; l_lane++)
+        {
+            l_addr = buildAddr(c_base_addr, l_group, l_lane, c_reg_tx_ctl_mode3_pl);
+            FAPI_DBG("TX_CTL_MODE3_PL Address 0x%08X%08X", (l_addr >> 32) & 0xFFFFFFFF, l_addr & 0xFFFFFFFF);
+            FAPI_TRY(putScom(i_target, l_addr, l_buffer),
+                     "Error putscom to address 0x%08X.", l_addr);
+
+            FAPI_DBG("Setting Tx Boost En to disabled");
+            l_buffer.insertFromRight(0, c_bit_tx_boost_en + c_indirect_scom_offset, c_len_tx);
+
+            FAPI_TRY(putScom(i_target, l_addr, l_buffer),
+                     "Error putscom to address 0x%08X.", l_addr);
+
+            l_buffer.flush<0>();
+        }
+
+    }
+
+    for (auto l_omi_target : l_omi_targets)
+    {
+        FAPI_TRY(p10_io_omi_put_pl_regs(l_omi_target,
+                                        scomt::omi::RXPACKS_0_DEFAULT_RD_RX_BIT_REGS_MODE4_PL,
+                                        scomt::omi::RXPACKS_0_DEFAULT_RD_RX_BIT_REGS_MODE4_PL_PHASE_STEP,
+                                        scomt::omi::RXPACKS_0_DEFAULT_RD_RX_BIT_REGS_MODE4_PL_PHASE_STEP_LEN,
+                                        l_num_lanes,
+                                        0x08));
+    }
+
+fapi_try_exit:
+
+    FAPI_DBG("Exiting with return code : 0x%08X...", (uint64_t) fapi2::current_err);
+    return fapi2::current_err;
+}
+
 // Putting unmask function in mss::unmask for consistency with p9/EXPL/etc.
 namespace mss
 {
@@ -189,11 +309,23 @@ fapi_try_exit:
 ///
 fapi2::ReturnCode p10_omi_init(const fapi2::Target<fapi2::TARGET_TYPE_MCC>& i_target)
 {
+    auto l_proc_target = i_target.getParent<fapi2::TARGET_TYPE_PROC_CHIP>();
+
+    uint32_t l_omi_freq = 0;
     uint8_t l_enable_fir_unmasking = 0;
 
     FAPI_TRY(p10_omi_init_scominit(i_target));
     FAPI_TRY(p10_omi_init_enable_templates(i_target));
     FAPI_TRY(p10_omi_init_enable_lol(i_target));
+
+
+    FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_FREQ_OMI_MHZ, l_proc_target, l_omi_freq));
+
+    // Performance updates are only for 38.4
+    if (l_omi_freq == 38400)
+    {
+        FAPI_TRY(p10_omi_init_performance(i_target));
+    }
 
     // Perform fir unmasking if attribute is set to enabled, default disabled
     FAPI_TRY(FAPI_ATTR_GET(fapi2::ATTR_ENABLE_FIR_UNMASKING, fapi2::Target<fapi2::TARGET_TYPE_SYSTEM>(),
