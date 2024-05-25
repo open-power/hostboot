d7463c6dba1e1d444b0a44605bf064273fd4c0d9
Deb McLemore <debmc@us.ibm.com>
2024-05-24 20:34:26 -0500

    Ignore PMIC Health Check callbacks for DDR4
    The new PMIC Health Check calls (which are for DDR5) are not performed
    for DDR4 (DDR4 4U only runs the Telemetry HWPs); however the timer
    which is used to trigger the periodic health check callbacks needs to be
    filtered and only applied to DDR5 systems.
    
    The terminology of the internals of the PMIC Telemetry and PMIC Health Check
    calls has a very inconsistent naming convention with legacy methods which makes
    some of the external tracing to be confusing, so cleaning up some of the trace
    points to help alleviate any external confusion.
    
    The determination of PMICs being either DDR4 or DDR5 is determined during
    the invocations of each of the Telemetry and Health Check methods.  This
    makes the calls for the Telemetry and Health Check methods required in order
    to identify the type and then the execution of the applicable methods.
    
    STGD: 620612
    Change-Id: Idacae0c22b0f0627a4757bd0523246de33cbeedf
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/172731
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>

--- src/usr/util/runtime/rt_fwnotify.C
+++ src/usr/util/runtime/rt_fwnotify.C
@@ -1095,7 +1095,7 @@ errlHndl_t createPmicHealthCheckCallback(bool i_firstCall)
 
         // Generate a new host callback (in milliseconds)
         TRACFCOMP(g_trac_hbrt,
-            "createPmicHealthCheckCallback: Create host_callback for PMIC health check in %d milliseconds",
+            "createPmicHealthCheckCallback: Create host_callback for PMIC Telemetry in %d milliseconds",
             l_host_callback_timer);
 
         size_t l_msg_size = hostInterfaces::HBRT_FW_MSG_BASE_SIZE;
@@ -1206,7 +1206,7 @@ errlHndl_t createPmicHealthCheckDDR5Callback(bool i_firstCall)
 
         // Generate a new host callback (in milliseconds)
         TRACFCOMP(g_trac_hbrt,
-            "createPmicHealthCheckDDR5Callback: Create host_callback for PMIC health check DDR5 in %d milliseconds",
+            "createPmicHealthCheckDDR5Callback: Create host_callback for PMIC Health Check in %d milliseconds",
             l_host_callback_timer);
 
         size_t l_msg_size = hostInterfaces::HBRT_FW_MSG_BASE_SIZE;
@@ -1664,7 +1664,7 @@ void firmware_notify( uint64_t i_len, void *i_data )
             case hostInterfaces::HBRT_FW_MSG_TYPE_PMIC_HEALTH_CHECK:
             {
                 TRACFCOMP(g_trac_runtime,
-                          "firmware_notify: PMIC health check callback");
+                          "firmware_notify: PMIC Telemetry callback");
 
                 handlePmicHealthCheckCallback();
             }
@@ -1673,7 +1673,7 @@ void firmware_notify( uint64_t i_len, void *i_data )
             case hostInterfaces::HBRT_FW_MSG_TYPE_PMIC_HEALTH_CHECK_DDR5:
             {
                 TRACFCOMP(g_trac_runtime,
-                          "firmware_notify: PMIC health check DDR5 callback");
+                          "firmware_notify: PMIC Health Check callback");
 
                 handlePmicHealthCheckDDR5Callback();
             }
