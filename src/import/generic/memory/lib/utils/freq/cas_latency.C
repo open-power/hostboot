f049ffce6c3fe1674a1cd56e4cfad2e8bd7f61f9
Adithi Ganapathi <adithi.t.ganapathi@ibm.com>
2024-06-26 14:40:28 -0400

    Update CAS latency for P11
    ZEN:MST-1555
    Change-Id: If34798b3f8285134437ef786f90aed48455a352b
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/ekb/+/174552
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: STEPHEN GLANCY <sglancy@us.ibm.com>
    Reviewed-by: Louis Stermole <stermole@us.ibm.com>
    Reviewed-by: Sneha Kadam <sneha.kadam1@ibm.com>
    Reviewed-by: Edgar Cordero <ecordero@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/175084
    Reviewed-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/generic/memory/lib/utils/freq/cas_latency.C
+++ src/import/generic/memory/lib/utils/freq/cas_latency.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2016,2022                        */
+/* Contributors Listed Below - COPYRIGHT 2016,2024                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -56,7 +56,8 @@ const std::vector< uint32_t > CasLatencyTraits<mc_type::EXPLORER>::SUPPORTED_FRE
 const std::vector< uint32_t > CasLatencyTraits<mc_type::ODYSSEY>::SUPPORTED_FREQS =
 {
     DIMM_SPEED_3200,
-    // TODO Zen:MST-1555 Add support for faster DIMM frequencies that P11 supports when we port this to P11
+    DIMM_SPEED_4000,
+    DIMM_SPEED_4800,
 };
 
 }// mss
