1be4fc9a7f6c2c972a717c14c0eb506ea86173da
Prem Shanker Jha <premjha2@in.ibm.com>
2024-09-03 08:34:55 -0500

    PM Elog: Extraction of hcode error log by hostboot during IPL.
    Commit incorporates hcode changes necessary for hostboot to detect
    XGPE and QME error log committed during istep 16.2.
    
    Change-Id: I50a0b3c8f3438847a935a7c3ffbefd2d8bc6546c
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hw/ekb/+/178034
    Reviewed-by: DAVID A KING <kingboy@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Greg Still <stillgs@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: DAVID DU <daviddu@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/178069
    Reviewed-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel M Crowell <dcrowell@us.ibm.com>
    Tested-by: Daniel M Crowell <dcrowell@us.ibm.com>

--- src/import/chips/p10/procedures/ppe/fw_tools/udparsers/b2900/b2900.py
+++ src/import/chips/p10/procedures/ppe/fw_tools/udparsers/b2900/b2900.py
@@ -6,7 +6,7 @@
 #
 # OpenPOWER HostBoot Project
 #
-# Contributors Listed Below - COPYRIGHT 2023
+# Contributors Listed Below - COPYRIGHT 2023,2024
 # [+] International Business Machines Corp.
 #
 #
@@ -34,18 +34,26 @@ import  udparsers.b2900.pmPpe2fsp
 #PARSER Version
 parserVersion = 1.0
 QME_STRING_LID_FILE = "81e0068f.lid"
+ERRL_BIN_XIR_SECT_VER_OFFSET = 153
+ERRL_BIN_XIR_OFFSET = 160
+ERRL_BIN_TRACE_SECTN  = 0x01
+ERRL_BIN_XIR_SECTN  = 0x0b
+PROC_VER_OFFSET = 116
+ELOG_SIZE       = 4096
+MAX_UD_SECTN = 2
+elog_sectn_list = {  'XIR_REG' : 0x0b, 'TRACE_SECTN': 0x01 }
 
 ##############################################################################
 # Function - Functions - Functions - Functions - Functions
 ##############################################################################
 
-#verifies the integrity of trace parser buffer
+# @brief verifies the integrity of trace parser buffer
 def checkForPpeTraceBuff():
     #buffer magic word
     bufferMagicWord = "qme_p10dd"
     bufferMagicWordHexCode = bytes(bufferMagicWord, 'utf-8')
 
-    fBin = open( "/tmp/qmeElogBin.bin", 'rb' )
+    fBin = open( "/tmp/qme.bin", 'rb' )
     data_read = fBin.read()
 
     magicWordOffset = data_read.find( bufferMagicWordHexCode )
@@ -63,49 +71,97 @@ def checkForPpeTraceBuff():
 
     return magicWordOffset
 
+#----------------------------------------------------------------------------------------------------------------------
+
+# @brief    finds start of QME trace buffer
+# @param    content of error log
 def parseQmeUserDataSection(data):
 
-    with open( "/tmp/qmeElogBin.bin", "wb" ) as qmeTraceBinFile:
+    with open( "/tmp/qme.bin", "wb" ) as qmeTraceBinFile:
         qmeTraceBinFile.write( data )
     magicWordOffset = checkForPpeTraceBuff()
-    cmd = "dd skip=" + str(magicWordOffset - 4 ) + " count=" + str(312) + " if=" + "/tmp/qmeElogBin.bin " + " of=" + "/tmp/qmeTrace.bin" + " bs=1 >/dev/null 2>&1"
+    cmd = "dd skip=" + str(magicWordOffset - 4 ) + " count=" + str(1024) + " if=" + "/tmp/qme.bin " + " of=" + "/tmp/qmeTrace.bin" + " bs=1 >/dev/null 2>&1"
     rc = os.system( cmd )
     if( rc ):
         print( "Failed To Extract QME Trace Section. RC : " + str( rc ) )
 
+#----------------------------------------------------------------------------------------------------------------------
 
+# @brief        parses XIR section of error log binary
+# @param[in]    content of error log binary
+def parsePpeRegSection(  ):
+    qmeReg.seek( ERRL_BIN_XIR_OFFSET )
+    qme_xir_val = "[\n"
+    qme_xir_val += "XCR-CTR    :  " + '{:016x}'.format( int.from_bytes(qmeReg.read( 8 ), byteorder='big' )) + "\n"
+    qme_xir_val += "XSR-SPRG0  :  " + '{:016x}'.format( int.from_bytes(qmeReg.read( 8 ), byteorder='big' )) + "\n"
+    qme_xir_val += "IR-EDR     :  " + '{:016x}'.format( int.from_bytes(qmeReg.read( 8 ), byteorder='big' )) + "\n"
+    qme_xir_val += "XSR-IAR    :  " + '{:016x}'.format( int.from_bytes(qmeReg.read( 8 ), byteorder='big' )) + "\n"
+    qme_xir_val += "SRR0-LR    :  " + '{:016x}'.format( int.from_bytes(qmeReg.read( 8 ), byteorder='big' )) + "\n"
+    qme_xir_val += "]\n"
+    qmeReg.seek( 0 )
+    return qme_xir_val
+
+#----------------------------------------------------------------------------------------------------------------------
+# @brief parses QME error log's user data section
+# @param subType userdata section type
+# @param ver     version of user data section
+# @param data    content of uder data section
 def parseUDToJson(subType, ver, data):
-    parseQmeUserDataSection( data )
-    #Execute pmPpe2fsp tool to convert ppe trace to fsp trace.
-    #This provide the right path to the tool as per BMC env
-    ppeFormatFile = "/tmp/qmeTrace.bin"
-    fspFormatFile = "/tmp/qmeTrace_fsp.bin"
-    udparsers.b2900.pmPpe2fsp.get_pm_trace_data_as_string(ppeFormatFile, fspFormatFile)
-
-    with open( fspFormatFile, 'rb' ) as fspFormat:
-        readData = fspFormat.read()
-
     traceDict = dict()
+    qmeReg = open( "/tmp/qme.bin", "wb" )
+    qmeReg.write(data)
+    qmeReg.close()
+    qmeReg = open( "/tmp/qme.bin", "rb" )
 
-    traceStringFile = getLid(QME_STRING_LID_FILE)
+    #print( "Gen = " + chipGen + " " + '{:016x}'.format( procVer ) )
+    size_parsed = ERRL_BIN_XIR_SECT_VER_OFFSET;
+    l_sectn = 0;
+    qme_xir_reg =""
 
-    if traceStringFile == "":
-        traceDict["File not found"] = QME_STRING_LID_FILE
-        traceStr = json.dumps(traceDict)
-        return traceStr
+    while size_parsed < ELOG_SIZE  and l_sectn < MAX_UD_SECTN :
 
-    startPos = 0
-    numTraces = -1
-    ( retVal, traceDataString, warningMessages ) = udparsers.helpers.hostfw_trace.get_binary_trace_data_as_string( readData, startPos, numTraces, traceStringFile )
+        qmeReg.seek( size_parsed )
+        udType = int.from_bytes( qmeReg.read( 1 ), byteorder='big' )
+        udSize = int.from_bytes( qmeReg.read( 2 ), byteorder='big' )
+        udSize += 8
+        size_parsed += udSize
 
-    traceDict["QME Traces" ]  = traceDataString.split( "\n" )
-    traceStr = json.dumps(traceDict)
+        if udType == elog_sectn_list.get( "XIR_REG" ) :
+            qme_xir_reg = parsePpeRegSection( qmeReg )
+            traceDict["XIRs"] = qme_xir_reg.split( "\n" )
 
-    if os.path.exists( "/tmp/qmeElogBin.bin" ) :
-        os.remove( "/tmp/qmeElogBin.bin" )
+        if udType == elog_sectn_list.get( "TRACE_SECTN" ) :
+            parseXgpeTraceDataSection( data )
+            #Execute pmPpe2fsp tool to convert ppe trace to fsp trace.
+            #This provide the right path to the tool as per BMC env
+            ppeFormatFile = "/tmp/qmeTrace.bin"
+            fspFormatFile = "/tmp/qmeTrace_fsp.bin"
+            udparsers.b2900.pmPpe2fsp.get_pm_trace_data_as_string(ppeFormatFile, fspFormatFile)
+
+            with open( fspFormatFile, 'rb' ) as fspFormat:
+                readData = fspFormat.read()
+
+            traceStringFile = getLid(QME_STRING_LID_FILE)
+            print( traceStringFile )
+
+            if traceStringFile == "":
+                traceDict["File not found"] = QME_STRING_LID_FILE
+                traceStr = json.dumps(traceDict)
+                return traceStr
+
+            startPos = 0
+            numTraces = -1
+            ( retVal, traceDataString, warningMessages ) = udparsers.helpers.hostfw_trace.get_binary_trace_data_as_string( readData, startPos, numTraces, traceStringFile )
+
+            traceDict["QME Traces" ]  = traceDataString.split( "\n" )
+
+    traceStr = json.dumps(traceDict, indent = 4, sort_keys=True )
+
     if os.path.exists( "/tmp/qmeTrace_fsp.bin" ) :
         os.remove( "/tmp/qmeTrace_fsp.bin" )
     if os.path.exists( "/tmp/qmeTrace.bin" ):
         os.remove( "/tmp/qmeTrace.bin" )
+    if os.path.exists( "/tmp/qme.bin" ):
+        os.remove( "/tmp/qme.bin" )
 
     return traceStr
--- src/import/chips/p10/procedures/ppe/fw_tools/udparsers/b2f00/b2f00.py
+++ src/import/chips/p10/procedures/ppe/fw_tools/udparsers/b2f00/b2f00.py
@@ -6,7 +6,7 @@
 #
 # OpenPOWER HostBoot Project
 #
-# Contributors Listed Below - COPYRIGHT 2023
+# Contributors Listed Below - COPYRIGHT 2023,2024
 # [+] International Business Machines Corp.
 #
 #
@@ -34,18 +34,26 @@ import  udparsers.b2900.pmPpe2fsp
 #PARSER Version
 parserVersion = 1.0
 XGPE_STRING_LID_FILE = "81e00690.lid"
+ERRL_BIN_XIR_SECT_VER_OFFSET = 153
+ERRL_BIN_XIR_OFFSET = 160
+ERRL_BIN_TRACE_SECTN  = 0x01
+ERRL_BIN_XIR_SECTN  = 0x0b
+PROC_VER_OFFSET = 116
+ELOG_SIZE       = 2048
+MAX_UD_SECTN = 2
+elog_sectn_list = {  'XIR_REG' : 0x0b, 'TRACE_SECTN': 0x01 }
 
 ##############################################################################
 # Function - Functions - Functions - Functions - Functions
 ##############################################################################
 
-#verifies the integrity of trace parser buffer
+# @brief verifies the integrity of trace parser buffer
 def checkForPpeTraceBuff():
     #buffer magic word
     bufferMagicWord = "xgpe_p10dd"
     bufferMagicWordHexCode = bytes(bufferMagicWord, 'utf-8')
 
-    fBin = open( "/tmp/xgpeElogBin.bin", 'rb' )
+    fBin = open( "/tmp/xgpe.bin", 'rb' )
     data_read = fBin.read()
 
     magicWordOffset = data_read.find( bufferMagicWordHexCode )
@@ -63,49 +71,97 @@ def checkForPpeTraceBuff():
 
     return magicWordOffset
 
+# @brief    finds start of XGPE trace buffer
+# @param    content of error log
 def parseXgpeUserDataSection(data):
 
-    with open( "/tmp/xgpeElogBin.bin", "wb" ) as xgpeTraceBinFile:
+    with open( "/tmp/xgpe.bin", "wb" ) as xgpeTraceBinFile:
         xgpeTraceBinFile.write( data )
     magicWordOffset = checkForPpeTraceBuff()
-    cmd = "dd skip=" + str(magicWordOffset - 4 ) + " count=" + str(1080) + " if=" + "/tmp/xgpeElogBin.bin " + " of=" + "/tmp/xgpeTrace.bin" + " bs=1 >/dev/null 2>&1"
+    cmd = "dd skip=" + str(magicWordOffset - 4 ) + " count=" + str(2048) + " if=" + "/tmp/xgpe.bin " + " of=" + "/tmp/xgpeTrace.bin" + " bs=1 >/dev/null 2>&1"
     rc = os.system( cmd )
     if( rc ):
         print( "Failed To Extract XGPE Trace Section. RC : " + str( rc ) )
 
+#----------------------------------------------------------------------------------------------------------------------
 
+# @brief        parses XIR section of error log binary
+# @param[in]    content of error log binary
+def parsePpeRegSection( xgpeReg ):
+    xgpeReg.seek( ERRL_BIN_XIR_OFFSET )
+    xgpe_xir_val = "[\n"
+    xgpe_xir_val += "XCR-CTR    :  " + '{:016x}'.format( int.from_bytes(xgpeReg.read( 8 ), byteorder='big' )) + "\n"
+    xgpe_xir_val += "XSR-SPRG0  :  " + '{:016x}'.format( int.from_bytes(xgpeReg.read( 8 ), byteorder='big' )) + "\n"
+    xgpe_xir_val += "IR-EDR     :  " + '{:016x}'.format( int.from_bytes(xgpeReg.read( 8 ), byteorder='big' )) + "\n"
+    xgpe_xir_val += "XSR-IAR    :  " + '{:016x}'.format( int.from_bytes(xgpeReg.read( 8 ), byteorder='big' )) + "\n"
+    xgpe_xir_val += "SRR0-LR    :  " + '{:016x}'.format( int.from_bytes(xgpeReg.read( 8 ), byteorder='big' )) + "\n"
+    xgpe_xir_val += "]\n"
+    xgpeReg.seek( 0 )
+    return xgpe_xir_val
+
+#----------------------------------------------------------------------------------------------------------------------
+
+# @brief parses XGPE error log's user data section
+# @param subType userdata section type
+# @param ver     version of user data section
+# @param data    content of uder data section
 def parseUDToJson(subType, ver, data):
-    parseXgpeUserDataSection( data )
-    #Execute pmPpe2fsp tool to convert ppe trace to fsp trace.
-    #This provide the right path to the tool as per BMC env
-    ppeFormatFile = "/tmp/xgpeTrace.bin"
-    fspFormatFile = "/tmp/xgpeTrace_fsp.bin"
-    udparsers.b2900.pmPpe2fsp.get_pm_trace_data_as_string(ppeFormatFile, fspFormatFile)
-
-    with open( fspFormatFile, 'rb' ) as fspFormat:
-        readData = fspFormat.read()
-
     traceDict = dict()
+    xgpeReg = open( "/tmp/xgpe.bin", "wb" )
+    xgpeReg.write(data)
+    xgpeReg.close()
+    xgpeReg = open( "/tmp/xgpe.bin", "rb" )
 
-    traceStringFile = getLid(XGPE_STRING_LID_FILE)
+    #print( "Gen = " + chipGen + " " + '{:016x}'.format( procVer ) )
+    size_parsed = ERRL_BIN_XIR_SECT_VER_OFFSET;
+    l_sectn = 0;
+    xgpe_xir_reg =""
 
-    if traceStringFile == "":
-        traceDict["File not found"] = XGPE_STRING_LID_FILE
-        traceStr = json.dumps(traceDict)
-        return traceStr
+    while size_parsed < ELOG_SIZE  and l_sectn < MAX_UD_SECTN :
 
-    startPos = 0
-    numTraces = -1
-    ( retVal, traceDataString, warningMessages ) = udparsers.helpers.hostfw_trace.get_binary_trace_data_as_string( readData, startPos, numTraces, traceStringFile )
+        xgpeReg.seek( size_parsed )
+        udType = int.from_bytes( xgpeReg.read( 1 ), byteorder='big' )
+        udSize = int.from_bytes( xgpeReg.read( 2 ), byteorder='big' )
+        udSize += 8
+        size_parsed += udSize
 
-    traceDict["XGPE Traces" ]  = traceDataString.split( "\n" )
-    traceStr = json.dumps(traceDict)
+        if udType == elog_sectn_list.get( "XIR_REG" ) :
+            xgpe_xir_reg = parsePpeRegSection( xgpeReg )
+            traceDict["XIRs"] = xgpe_xir_reg.split( "\n" )
 
-    if os.path.exists( "/tmp/xgpeElogBin.bin" ) :
-        os.remove( "/tmp/xgpeElogBin.bin" )
+        if udType == elog_sectn_list.get( "TRACE_SECTN" ) :
+            parseXgpeUserDataSection( data )
+            #Execute pmPpe2fsp tool to convert ppe trace to fsp trace.
+            #This provide the right path to the tool as per BMC env
+            ppeFormatFile = "/tmp/xgpeTrace.bin"
+            fspFormatFile = "/tmp/xgpeTrace_fsp.bin"
+            udparsers.b2900.pmPpe2fsp.get_pm_trace_data_as_string(ppeFormatFile, fspFormatFile)
+
+            with open( fspFormatFile, 'rb' ) as fspFormat:
+                readData = fspFormat.read()
+
+            traceStringFile = getLid(XGPE_STRING_LID_FILE)
+            print( traceStringFile )
+
+            if traceStringFile == "":
+                traceDict["File not found"] = XGPE_STRING_LID_FILE
+                traceStr = json.dumps(traceDict)
+                return traceStr
+
+            startPos = 0
+            numTraces = -1
+            ( retVal, traceDataString, warningMessages ) = udparsers.helpers.hostfw_trace.get_binary_trace_data_as_string( readData, startPos, numTraces, traceStringFile )
+
+            traceDict["XGPE Traces" ]  = traceDataString.split( "\n" )
+
+
+    traceStr = json.dumps( traceDict, indent = 4, sort_keys=True )
+
     if os.path.exists( "/tmp/xgpeTrace_fsp.bin" ) :
         os.remove( "/tmp/xgpeTrace_fsp.bin" )
     if os.path.exists( "/tmp/xgpeTrace.bin" ):
         os.remove( "/tmp/xgpeTrace.bin" )
+    if os.path.exists( "/tmp/xgpe.bin" ):
+        os.remove( "/tmp/xgpe.bin" )
 
     return traceStr
