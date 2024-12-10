f3352165d58c06e4e23d398065df59b1428de04c
Caleb Palmer <cnpalmer@us.ibm.com>
2024-12-10 13:56:22 -0600

    PRD: Add new OMI degrade sig for VDD uplift
    Change-Id: I60d7da131146d7b1c47e038ccb0d8671ea3ab9fd
    STGD: 668431
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/182765
    Reviewed-by: Zane C Shelley <zshelle@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/182950

--- src/usr/diag/prdf/common/plat/explorer/prdfExplorerPlugins_common.C
+++ src/usr/diag/prdf/common/plat/explorer/prdfExplorerPlugins_common.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HostBoot Project                                             */
 /*                                                                        */
-/* Contributors Listed Below - COPYRIGHT 2019,2024                        */
+/* Contributors Listed Below - COPYRIGHT 2019,2025                        */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -544,6 +544,21 @@ int32_t CheckKvcoFix( ExtensibleChip * i_chip,
         io_sc.service_data->setSignature(i_chip->getHuid(),
                                          PRDFSIG_OmiDegradeFixOcmb);
     }
+
+    #ifdef __HOSTBOOT_MODULE
+    // Another OMI degrade fix was applied to increase the VDD core voltage.
+    // Check ATTR_MSS_OMI_VDD_UPLIFT_APPLIED on the OCMB and adjust the
+    // signature if that is set to 1. A tryGetAttr is used here in case of
+    // a Hostboot bug that may not be fixed yet that could cause the attribute
+    // to be unavailable to read after a concurrent update.
+    fapi2::ATTR_MSS_OMI_VDD_UPLIFT_APPLIED_Type attr;
+    if (i_chip->getTrgt()->tryGetAttr<ATTR_MSS_OMI_VDD_UPLIFT_APPLIED>(attr) &&
+        (fapi2::ENUM_ATTR_MSS_OMI_VDD_UPLIFT_APPLIED_YES == attr))
+    {
+        io_sc.service_data->setSignature(i_chip->getHuid(),
+                                         PRDFSIG_OmiDegradeVddUpliftOcmb);
+    }
+    #endif
 
     return SUCCESS;
 }
