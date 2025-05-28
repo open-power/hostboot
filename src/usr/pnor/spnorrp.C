08f3d1a96a1f327da0d7dc6085797ec4a800f2a6
Mike Baiocchi <baiocchi@ibm.com>
2025-05-28 17:47:33 -0500

    Remove FSP V3 Security Workarounds - Final Removal
    This commit builds upon prior commits to finally remove the
    workaround that skipped V3 security checks for HBD (Hostboot Data,
    aka targeting and attributes) and WOFDATA PNOR sections for FSP
    systems in spnorrp.C.  It also fixes up a makefile and script
    to allow for the workaround to be removed.
    
    STGD: 702140
    JIRA: PFHB-934
    CMVC-Prereq: 1190885
    Change-Id: I946aa252e909987f09f1691ddb7fd730ff7314bf
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/190796
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Daniel Crowell <dcrowell@us.ibm.com>
    Reviewed-by: Nick Bofferding <bofferdn@us.ibm.com>
    Reviewed-by: Matt Strom <matt.strom@ibm.com>
    Reviewed-by: Mike Baiocchi <baiocchi@ibm.com>

--- src/usr/pnor/spnorrp.C
+++ src/usr/pnor/spnorrp.C
@@ -628,19 +628,6 @@ uint64_t SPnorRP::verifySections(SectionId i_id,
         // sections. More details below.
         bool l_skip_section = false;
 
-        // For FSP builds temporarily skip verification of HBD and WOFDATA
-        // until it's sorted out how to make unique HB_HLLs for every system
-        // @TODO JIRA PFHB-934 remove this workaround when issue resolved
-#ifdef CONFIG_FSP_BUILD
-        if( (i_id == PNOR::HB_DATA) ||
-            (i_id == PNOR::WOFDATA))
-        {
-            TRACFCOMP(g_trac_pnor,"SPnorRP::verifySections: SKIPPING VERIFICATION for section %s",
-                      pPnorString);
-            l_skip_section = true;
-        }
-#endif
-
         // verify while in temp space
         if (SECUREBOOT::enabled() && !l_skip_section)
         {
