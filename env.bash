33808a3e39d830efd93f01a201d6f4bf9f45d3cc
Daniel Crowell <dcrowell@us.ibm.com>
2026-01-08 10:11:24 -0600

    Switch to rchgsa
    Update a few comments and other references to use rchgsa since
    ausgsa is going away.
    Bump sbe repo to pick up a gsa fix there.
    
    JIRA: PFHB-1087
    Change-Id: Ia51750315e3e87b31fb2bc9c05032a9e8a90189d
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/c/hostboot/+/199632
    Reviewed-by: Daniel Crowell <dcrowell@us.ibm.com>
    Tested-by: Daniel Crowell <dcrowell@us.ibm.com>

--- env.bash
+++ env.bash
@@ -5,7 +5,7 @@
 #
 # OpenPOWER HostBoot Project
 #
-# Contributors Listed Below - COPYRIGHT 2010,2023
+# Contributors Listed Below - COPYRIGHT 2010,2026
 # [+] International Business Machines Corp.
 #
 #
@@ -25,18 +25,19 @@
 
 ROOTDIR=.
 
-# Default to austin but optimize based on HOSTNAME
+# Default to Rochester but optimize based on HOSTNAME
 if [ -z "${HOSTBOOT_ENVIRONMENT}" ]; then
-  export HOSTBOOT_ENVIRONMENT=/gsa/ausgsa/projects/h/hostboot/
+  export HOSTBOOT_ENVIRONMENT=/gsa/rchgsa/projects/h/hostboot/
   ARTIFACTFILE=src/build/citest/etc/artifactspaces
   if [ ! -f ${ARTIFACTFILE} ]; then
     echo "Cannot open ${ARTIFACTFILE}, using default space"
   elif [[ "$HOSTNAME" == *"rchland.ibm.com"* ]]; then
+    # this is somewhat redundant with the else but leaving as example for future site moves
     export HOSTBOOT_ENVIRONMENT=`grep rchland ${ARTIFACTFILE} | awk -F, '{print $2}'`
     echo "Using rchland for Hostboot artifacts : $HOSTBOOT_ENVIRONMENT"
   else
-    export HOSTBOOT_ENVIRONMENT=`grep austin ${ARTIFACTFILE} | awk -F, '{print $2}'`
-    echo "Using austin for Hostboot artifacts : $HOSTBOOT_ENVIRONMENT"
+    export HOSTBOOT_ENVIRONMENT=`grep rchland ${ARTIFACTFILE} | awk -F, '{print $2}'`
+    echo "Using rchland for Hostboot artifacts : $HOSTBOOT_ENVIRONMENT"
   fi
 else
   echo "Using custom path for Hostboot artifacts : $HOSTBOOT_ENVIRONMENT"
